f4c8d84caeb18fa01ac9d25fe01d7664f7d90a34
joej0e <36197513+joej0e@users.noreply.github.com>
2019-08-27 18:34:06 +0300

    Update StorageImpl.java

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,15 +2,45 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
-public class StorageImpl<K, V> implements Storage<K,V> {
+public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private Element first;
+
+    public StorageImpl() {
+        first = null;
+    }
 
     @Override
     public void put(K key, V value) {
+        Element element = new Element(key, value);
+        element.next = first;
+        first = element;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        Element current = first;
+        if (current == null) {
+            return null;
+        }
+        while (current.key != key) {
+            if (current.next == null) {
+                return null;
+            } else {
+                current = current.next;
+            }
+        }
+        return (V) current.value;
+    }
+
+    public class Element<K, V> {
+        private K key;
+        private V value;
+        private Element next;
+
+        Element(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }
-
