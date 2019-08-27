86a3477c54aee6a9c90a2a439eab6b8319868428
NikolayKatsai <alaska.rok123@gmail.com>
2019-08-27 21:32:03 +0300

    StorageImpl class finished

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,15 +2,54 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
-public class StorageImpl<K, V> implements Storage<K,V> {
+public class StorageImpl<K, V> implements Storage<K, V> {
+    private K[] keys;
+    private V[] values;
+    private int elementsQ = 0;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[10];
+        values = (V[]) new Object[10];
+    }
 
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < elementsQ; i++) {
+            if ((keys[i] == null && key == null) || keys[i].equals(key)) {
+                values[i] = value;
+                return;
+            }
+        }
+        if (elementsQ == keys.length) {
+            resize(elementsQ * 2);
+        }
+        keys[elementsQ] = key;
+        values[elementsQ] = value;
+        elementsQ++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < elementsQ; i++) {
+            if ((keys[i] == null && key == null) || keys[i].equals(key)) {
+                return values[i];
+            }
+        }
         return null;
+    }
+
+    private void resize(int capacity) {
+        if (capacity < elementsQ) {
+            return;
+        }
+        K[] keysClone = (K[]) new Object[capacity];
+        V[] valuesClone = (V[]) new Object[capacity];
+        for (int i = 0; i < elementsQ; i++) {
+            keysClone[i] = keys[i];
+            valuesClone[i] = values[i];
+        }
+        keys = keysClone;
+        values = valuesClone;
     }
 }
 
