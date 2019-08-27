2a6a07dc340f3d0d579708eab3518d21d7caa430
roman_minevich <283389dv3qsh>
2019-08-27 18:50:35 +0300

    Changed solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -9,14 +9,17 @@ public class StorageImpl<K, V> implements Storage<K,V> {
 
     @Override
     public void put(K key, V value) {
-        if (key == null || key.equals(storage[0][storage[0].length - 1])) {
-            storage[1][storage[1].length - 1] = value;
-        } else {
-            storage[0] = Arrays.copyOf(storage[0], storage[0].length + 1);
-            storage[0][storage[0].length - 1] = key;
-            storage[1] = Arrays.copyOf(storage[1], storage[1].length + 1);
-            storage[1][storage[1].length - 1] = value;
+        for (int i = 0; i < storage[0].length; i++) {
+            if (storage[0][i] == null || storage[0][i].equals(key)) {
+                storage[0][i] = key;
+                storage[1][i] = value;
+                break;
+            }
         }
+        storage[0] = Arrays.copyOf(storage[0], storage[0].length + 1);
+        storage[0][storage[0].length - 1] = key;
+        storage[1] = Arrays.copyOf(storage[1], storage[1].length + 1);
+        storage[1][storage[1].length - 1] = value;
     }
 
     @Override
