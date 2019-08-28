6fe62743ef2e09a6ea62dc1db1f8fd1072e3ac3b
roman_minevich <283389dv3qsh>
2019-08-28 19:54:25 +0300

    Changed solution 3

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,23 +6,24 @@ import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K,V> {
     private Object[][] storage = new Object[2][16];
+    private int size = this.getIndex();
 
     @Override
     public void put(K key, V value) {
-        int index = this.getIndex();
-        for (int i = 0; i < index; i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[0][i] == key || storage[0][i].equals(key)) {
                 storage[1][i] = value;
                 break;
             }
         }
-        storage[0][index] = key;
-        storage[1][index] = value;
+        storage[0][size] = key;
+        storage[1][size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < this.getIndex(); i++) {
+        for (int i = 0; i <size; i++) {
             if (storage[0][i] == key || storage[0][i].equals(key)) {
                 return (V) storage[1][i];
             }
@@ -31,10 +32,8 @@ public class StorageImpl<K, V> implements Storage<K,V> {
     }
 
     private int getIndex() {
-        for (int i = 0; i < storage[1].length; i++) {
-            if (storage[1][i] == null) {
-                return i;
-            }
+        if (size < storage[1].length) {
+            return size;
         }
         storage[0] = Arrays.copyOf(storage[0], storage[0].length * 2);
         storage[1] = Arrays.copyOf(storage[1], storage[1].length * 2);
