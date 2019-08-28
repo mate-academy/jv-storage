ee3dce7bf7a983f53e7a9e87904b473b55fd14fe
Max <skaterweather41@gmail.com>
2019-08-28 15:05:30 +0200

    method containsKey() removed; put(), get() was improved

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -16,42 +16,34 @@ public class StorageImpl<K, V> implements Storage<K,V> {
 
     private void extendCapacity() {
         if (currentCapacity >= storage.length * fillingPercent) {
-            Object[][] extendedStorage = Arrays.copyOfRange(storage,0, storage.length * 2);
+            Object[][] extendedStorage = Arrays.copyOfRange(storage, 0, storage.length * 2);
             storage = extendedStorage;
         }
-    }
-
-    //if storage contains key return it's index, otherwise return -1
-    private int containsKey(K key) {
-        for (int index = 0; index < currentCapacity; index++) {
-            if (key == null && storage[index][0] == null) {
-                return index;
-            }
-            if (storage[index][0].equals(key)) {
-                return index;
-            }
-        }
-        return -1;
     }
 
     @Override
     public void put(K key, V value) {
         extendCapacity();
-        if (containsKey(key) >= 0) {
-            storage[containsKey(key)][1] = value;
-        } else {
-            storage[currentCapacity][0] = key;
-            storage[currentCapacity][1] = value;
-            currentCapacity++;
+        for (int i = 0; i < currentCapacity; i++) {
+            if (storage[i][0].equals(key)) {
+                storage[i][1] = value;
+                return;
+            }
         }
+        storage[currentCapacity][0] = key;
+        storage[currentCapacity++][1] = value;
     }
 
     @Override
     public V get(K key) {
-        if (containsKey(key) >= 0) {
-            return (V) storage[containsKey(key)][1];
+        for (int i = 0; i < currentCapacity; i++) {
+            if (key == null && storage[i][0] == null) {
+                return (V) storage[i][1];
+            }
+            if (storage[i][0].equals(key)) {
+                return (V) storage[i][1];
+            }
         }
         return null;
     }
 }
-
