6e405591ff13cf1e8cbb13d7f0737b09538708d3
roman_minevich <283389dv3qsh>
2019-08-28 21:54:38 +0300

    Changed solution 4

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,17 +6,17 @@ import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K,V> {
     private Object[][] storage = new Object[2][16];
-    private int size;
+    private int size = 0;
 
     @Override
     public void put(K key, V value) {
-        size = this.getIndex();
         for (int i = 0; i < size; i++) {
             if (storage[0][i] == key || storage[0][i].equals(key)) {
                 storage[1][i] = value;
                 break;
             }
         }
+        this.checkSize();
         storage[0][size] = key;
         storage[1][size] = value;
         size++;
@@ -32,12 +32,10 @@ public class StorageImpl<K, V> implements Storage<K,V> {
         return null;
     }
 
-    private int getIndex() {
-        if (size < storage[1].length) {
-            return size;
+    private void checkSize() {
+        if (size == storage[1].length) {
+            storage[0] = Arrays.copyOf(storage[0], storage[0].length * 2);
+            storage[1] = Arrays.copyOf(storage[1], storage[1].length * 2);
         }
-        storage[0] = Arrays.copyOf(storage[0], storage[0].length * 2);
-        storage[1] = Arrays.copyOf(storage[1], storage[1].length * 2);
-        return storage[1].length / 2;
     }
 }
