140f95496a154d7720d22ab668e35c949320a46b
TatianaPirch <pirch.tatiana@gmail.com>
2019-08-28 14:51:09 +0300

    solution for Storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,15 +2,55 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
-public class StorageImpl<K, V> implements Storage<K,V> {
+import java.util.Arrays;
 
-    @Override
-    public void put(K key, V value) {
+public class StorageImpl<K, V> implements Storage<K,V> {
+    private int counter = 0;
+    private Object[] keyArray = new Object[16];
+    private Object[] valueArray = new Object[16];
+
+    public int existsKey(K key) {
+        for (int i = 0; i < counter; i++) {
+            if ((keyArray[i] == null && key == null)
+                    || (keyArray[i] != null && keyArray[i].equals(key))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void resizeArray() {
+        Object[] newKeyArray = Arrays.copyOf(keyArray, keyArray.length * 2);
+        Object[] newValueArray = Arrays.copyOf(valueArray, valueArray.length * 2);
+        keyArray = newKeyArray;
+        keyArray = newValueArray;
     }
 
     @Override
     public V get(K key) {
+        int i = existsKey(key);
+        if (i >= 0) {
+            return (V) valueArray[i];
+        }
         return null;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        if (counter == 0) {
+            keyArray[counter] = key;
+            valueArray[counter] = value;
+        }
+        if (existsKey(key) >= 0) {
+            valueArray[existsKey(key)] = value;
+        } else {
+            if (counter >= keyArray.length) {
+                resizeArray();
+            }
+            keyArray[counter] = key;
+            valueArray[counter] = value;
+            counter++;
+        }
     }
 }
 
