e3518e3e69429d33fe38c6bad7bcc2e941094010
Ruslan <vip.russle@gmail.com>
2019-08-28 17:28:09 +0300

    formatted

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,22 +4,22 @@ import core.basesyntax.Storage;
 import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K,V> {
-    private final int maxSize = 16;
+    private final int initialCapacity = 16;
     private Object[] keys;
     private Object[] values;
     private double loadFactory = 0.75;
-    private int curCapacity;
+    private int currentCapacity;
 
 
     public StorageImpl() {
-        keys = new Object[maxSize];
-        values = new Object[maxSize];
-        curCapacity = 1;
+        keys = new Object[initialCapacity];
+        values = new Object[initialCapacity];
+        currentCapacity = 1;
         keys[0] = null;
     }
 
     private void extCapacity() {
-        if (curCapacity >= loadFactory * maxSize) {
+        if (currentCapacity >= loadFactory * initialCapacity) {
             values = Arrays.copyOf(values, values.length * 2);
             keys = Arrays.copyOf(keys, keys.length * 2);
         }
@@ -29,17 +29,17 @@ public class StorageImpl<K, V> implements Storage<K,V> {
         if (key == null) {
             return 0;
         }
-        for (int i = 1; i < curCapacity; i++) {
+        for (int i = 1; i < currentCapacity; i++) {
             if (key.equals((K) keys[i])) {
                 return i;
             }
         }
-        extCapacity();
-        return curCapacity++;
+        return currentCapacity++;
     }
 
     @Override
     public void put(K key, V value) {
+        extCapacity();
         if (value != null) {
             int num = isKeyThere(key);
             keys[num] = key;
@@ -49,7 +49,7 @@ public class StorageImpl<K, V> implements Storage<K,V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < curCapacity; i++) {
+        for (int i = 0; i < currentCapacity; i++) {
             if (key == null) {
                 return (V) values[0];
             }
