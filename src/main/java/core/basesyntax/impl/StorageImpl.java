646d94ebff96bf426d231d55a1dd2ad7c45c03ff
NikolayKatsai <alaska.rok123@gmail.com>
2019-08-28 11:35:24 +0300

    int elementsQ changed on elementsQuantity, changed default arrays capacity, changed method resize in StorageImpl class

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,35 +2,38 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.Arrays;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
     private K[] keys;
     private V[] values;
-    private int elementsQ = 0;
+    private int elementsQuantity = 0;
+    private static final int DEFAULT_CAPACITY = 16;
 
     public StorageImpl() {
-        keys = (K[]) new Object[10];
-        values = (V[]) new Object[10];
+        keys = (K[]) new Object[DEFAULT_CAPACITY];
+        values = (V[]) new Object[DEFAULT_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < elementsQ; i++) {
+        for (int i = 0; i < elementsQuantity; i++) {
             if ((keys[i] == null && key == null) || keys[i].equals(key)) {
                 values[i] = value;
                 return;
             }
         }
-        if (elementsQ == keys.length) {
-            resize(elementsQ * 2);
+        if (elementsQuantity == keys.length) {
+            resize(elementsQuantity * 2);
         }
-        keys[elementsQ] = key;
-        values[elementsQ] = value;
-        elementsQ++;
+        keys[elementsQuantity] = key;
+        values[elementsQuantity] = value;
+        elementsQuantity++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < elementsQ; i++) {
+        for (int i = 0; i < elementsQuantity; i++) {
             if ((keys[i] == null && key == null) || keys[i].equals(key)) {
                 return values[i];
             }
@@ -39,17 +42,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     }
 
     private void resize(int capacity) {
-        if (capacity < elementsQ) {
-            return;
-        }
-        K[] keysClone = (K[]) new Object[capacity];
-        V[] valuesClone = (V[]) new Object[capacity];
-        for (int i = 0; i < elementsQ; i++) {
-            keysClone[i] = keys[i];
-            valuesClone[i] = values[i];
-        }
-        keys = keysClone;
-        values = valuesClone;
+        keys = Arrays.copyOf(keys, capacity);
+        values = Arrays.copyOf(values, capacity);
     }
 }
 
