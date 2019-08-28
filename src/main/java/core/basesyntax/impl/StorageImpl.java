a77df98d0f30c0a4eb4af76d99fdbced1a5ee135
Iurii <kenu21@gmail.com>
2019-08-28 12:25:14 +0300

    fix hw-6-2

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -7,40 +7,40 @@ import java.util.Arrays;
 public class StorageImpl<K, V> implements Storage<K,V> {
 
     private Object[] keys = new Object[16];
-    private Object[] values =  new Object[16];
-    int topArray = -1;
+    private Object[] values = new Object[16];
+    private int topArray = 0;
 
     @Override
     public void put(K key, V value) {
-        K k;
         for (int i = 0; i < keys.length; i++) {
-            k = (K) keys[i];
-            if (k == key) {
+            if ((K) keys[i] == key) {
                 values[i] = value;
                 return;
             }
         }
-        topArray++;
         if (topArray < keys.length - 1) {
             keys[topArray] = key;
             values[topArray] = value;
         } else {
-            keys = Arrays.copyOf(keys, keys.length * 2);
-            values = Arrays.copyOf(keys, keys.length * 2);
+            keys = resizeAndCopyArr(keys);
+            values = resizeAndCopyArr(values);
             keys[topArray] = key;
             values[topArray] = value;
         }
+        topArray++;
     }
 
     @Override
     public V get(K key) {
-        K k;
         for (int i = 0; i < keys.length; i++) {
-            k = (K) keys[i];
-            if (k == key) {
+            if ((K) keys[i] == key) {
                 return (V) values[i];
             }
         }
         return null;
+    }
+
+    private Object[] resizeAndCopyArr(Object[] input) {
+        return Arrays.copyOf(input, input.length * 2);
     }
 }
