5ae86672c53c0f28ea6d0eb2e5014903d6c117ba
Rosinskyi <sergey22061993@gmail.com>
2019-08-28 19:17:27 +0300

    added solution for jv-storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,24 +2,34 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private K key;
-    private V value;
-    private Map<K, V> map = new HashMap<>();
+    private int index = 0;
+    private int capacity = 16;
+    private K[] keys = (K[]) new Object[capacity];
+    private V[] values = (V[]) new Object[capacity];
 
     @Override
     public void put(K key, V value) {
-        this.key = key;
-        this.value = value;
-        map.put(key, value);
+        for (int i = 0; i < index; i++) {
+            if ((keys[i] == null && key == null) || keys[i].equals(key)) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[index] = key;
+        values[index] = value;
+        index++;
     }
 
     @Override
     public V get(K key) {
-        return map.get(key);
+        for (int i = 0; i < index; i++) {
+            System.out.println(values[i]);
+            if ((key == null && keys[i] == null)
+                    || keys[i].equals(key)) {
+                return values[i];
+            }
+        }
+        return null;
     }
 }
-
