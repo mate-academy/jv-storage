157d0202cc3935b16ef046800a8cb2009837237a
Taras Khalak <tarasulo@gmail.com>
2019-08-29 12:47:41 +0300

    'Solution'

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,14 +2,32 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
-public class StorageImpl<K, V> implements Storage<K,V> {
+public class StorageImpl<K, V> implements Storage<K, V> {
+    private int count;
+    private final int defaultCapacyty = 10;
+    private K[] keys = (K[]) new Object[defaultCapacyty];
+    private V[] values = (V[]) new Object[defaultCapacyty];
 
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < count; i++) {
+            if ((key == null && keys[i] == null) || keys[i].equals(key)) {
+                values[i] = value;
+                continue;
+            }
+        }
+        keys[count] = key;
+        values[count] = value;
+        count++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < count; i++) {
+            if ((key == null && keys[i] == null) || keys[i].equals(key)) {
+                return values[i];
+            }
+        }
         return null;
     }
 }
