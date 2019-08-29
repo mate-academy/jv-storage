8b8db860f7fe1fb0b70eb80b1ae3392da8660d5d
kuhaj <alex.kukhtin.dev@gmail.com>
2019-08-29 08:12:01 +0300

    storage realisation

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,15 +2,59 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
-public class StorageImpl<K, V> implements Storage<K,V> {
+import java.util.Arrays;
+
+public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private static final int DEFAULT_CAPACITY = 16;
+
+    private Object[] keys;
+    private Object[] values;
+    private int index;
+    private int currentCapacity;
+
+    public StorageImpl() {
+        index = 0;
+        currentCapacity = DEFAULT_CAPACITY;
+        keys = new Object[DEFAULT_CAPACITY];
+        values = new Object[DEFAULT_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
+        if (index >= currentCapacity) {
+            resize();
+        }
+        if (contains(key)) {
+            values[indexOf(key)] = value;
+        } else {
+            keys[index] = key;
+            values[index] = value;
+            index++;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return contains(key) ? (V) values[indexOf(key)] : null;
+    }
+
+    private void resize() {
+        currentCapacity = currentCapacity + currentCapacity / 2;
+        keys = Arrays.copyOf(keys, currentCapacity);
+        values = Arrays.copyOf(values, currentCapacity);
+    }
+
+    private boolean contains(K key) {
+        return indexOf(key) >= 0;
+    }
+
+    private int indexOf(K key) {
+        for (int i = 0; i < index; i++) {
+            if (key == null || key.equals(keys[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
-
