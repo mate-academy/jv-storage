f893cee98ea8e8c4ec29606e868ce69b9f4f67ad
Yaroslava Bondarchuk <yaroslava7bondarchuk@gmail.com>
2019-12-10 14:06:18 +0200

    added StorageImpl

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,13 +3,31 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private Object[] keyArr = new Object[10];
+    private Object[] valueArr = new Object[10];
 
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < keyArr.length; i++) {
+            if (key == keyArr[i]) {
+                valueArr[i] = value;
+            } else if (keyArr[i] == null && valueArr[i] == null) {
+                keyArr[i] = key;
+                valueArr[i] = value;
+                break;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keyArr.length; i++) {
+            if (key != null && key.equals((keyArr[i]))) {
+                return (V) valueArr[i];
+            } else if (keyArr[i] == null) {
+                return (V) valueArr[i];
+            }
+        }
         return null;
     }
 }
