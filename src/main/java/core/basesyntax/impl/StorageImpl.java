821664fde1ff26d1b93180cb8226961f26ca7348
Roman Dubovskyi <rdubowskij@gmail.com>
2019-12-10 14:09:34 +0200

    hw6.2 done, please check

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,13 +3,44 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private Object[] valueArr;
+    private int[] keyArr;
+    private int counter;
+
+    public <K, V> StorageImpl() {
+        this.valueArr = new Object[10];
+        this.keyArr = new int[10];
+    }
 
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < keyArr.length; i++) {
+            if (key != null && key.hashCode() == keyArr[i]) {
+                keyArr[i] = key.hashCode();
+                valueArr[i] = value;
+                return;
+            }
+        }
+        if (key != null) {
+            keyArr[counter] = key.hashCode();
+            valueArr[counter] = value;
+            counter++;
+        } else {
+            keyArr[counter] = 0;
+            valueArr[counter] = value;
+            counter++;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keyArr.length; i++) {
+            if (key != null && keyArr[i] == key.hashCode()) {
+                return (V) valueArr[i];
+            } else if (key == null && keyArr[i] == 0) {
+                return (V) valueArr[i];
+            }
+        }
         return null;
     }
 }
