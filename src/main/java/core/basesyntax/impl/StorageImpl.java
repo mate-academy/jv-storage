64cf66fdcfb64c40fac073239cde707ea0cf6710
Mykhailo Kramar <3805033@gmail.com>
2019-12-10 17:35:19 +0200

    hw6.2

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,15 +1,18 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.HashMap;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    HashMap<K, V> storage = new HashMap<>();
 
     @Override
     public void put(K key, V value) {
+        storage.put(key, value);
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return storage.get(key);
     }
 }
