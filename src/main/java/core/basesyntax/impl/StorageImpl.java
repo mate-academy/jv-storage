dbc7f53eaa526fa7f65f4f1f6a2a297a1f579405
Misha <mishabeheka@gmail.com>
2019-12-10 21:13:13 +0200

    Done 5 testes. Test getElementKeyIsObject is failed.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,12 +4,65 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
+    public static StorageImpl [] collection = new StorageImpl[10];
+
+    private K key;
+    private V value;
+
+    public StorageImpl() {
+    }
+
+    public StorageImpl(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < collection.length; ++i){
+            if (collection[i] == null || collection[i].getKey() == key){
+                collection[i] = new StorageImpl<>(key,value);
+                return;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        V val = null;
+
+        for (int i = 0; i < collection.length; ++i){
+            if (collection[i] != null && collection[i].getKey() == key){
+                val = (V) collection[i].value;
+            }
+        }
+        return val;
+    }
+
+    public boolean equals(Object object){
+        if (this == object){
+            return true;
+        }
+        if (object != null || object.getClass() != this.getClass()){
+            return false;
+        }
+        StorageImpl storageImpl = (StorageImpl) object;
+        return key.equals(storageImpl.key) && value.equals(storageImpl.value);
+    }
+
+    public int hashCode(){
+        int prime = 31;
+        int result = 1;
+        result = prime + result * key.hashCode();
+        result = prime + result * value.hashCode();
+        return result;
     }
 }
