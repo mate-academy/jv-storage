f4afb2d8a320756c774fe27ead329b1b78f8f3b3
Alexandr Sashchenko <santys87@gmail.com>
2019-12-10 22:06:56 +0200

    Added mate HW 6.2 Solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,12 +4,37 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
+    private static final int LENGTH = 10;
+    private int arrayItems;
+    private Object[] keysArray;
+    private Object[] valuesArray;
+
+    public StorageImpl() {
+        arrayItems = 0;
+        keysArray = new Object[LENGTH];
+        valuesArray = new Object[LENGTH];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < LENGTH; i++) {
+            if ((keysArray[i] == key) || (keysArray[i] != null && keysArray[i].equals(key))) {
+                valuesArray[i] = value;
+                break;
+            }
+        }
+        keysArray[arrayItems] = key;
+        valuesArray[arrayItems] = value;
+        arrayItems++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < LENGTH; i++) {
+            if ((keysArray[i] == key) || (keysArray[i] != null && keysArray[i].equals(key))) {
+                return (V) valuesArray[i];
+            }
+        }
         return null;
     }
 }
