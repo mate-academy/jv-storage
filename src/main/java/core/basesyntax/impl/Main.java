6c103bfe740e77c4553a39045c8fa13eda68ec96
Roman Dubovskyi <rdubowskij@gmail.com>
2019-12-10 15:53:49 +0200

    tried to simplify get method

--- src/main/java/core/basesyntax/impl/Main.java
+++ src/main/java/core/basesyntax/impl/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         StorageImpl<Integer, String> test = new StorageImpl<>();
         test.put(10, "test1");
-        test.put(1, "test2");
+        test.put(null, "test2");
 
-        System.out.println(test.get(1));
         System.out.println(test.get(10));
+        System.out.println(test.get(null));
     }
 }
