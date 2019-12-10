687e22fe7cf2183484e39aedc315a29ce83529ee
Taraban <Taraban.dmitry@gmail.com>
2019-12-10 22:04:47 +0200

    added storage solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,12 +4,30 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
+    private final int arrayCapacity = 10;
+    private Object[] arrayOfKeys = new Object[arrayCapacity];
+    private Object[] arrayOfValues = new Object[arrayCapacity];
+
+    int size = 1;
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < arrayCapacity - size; i++) {
+            arrayOfKeys[i] = key;
+            arrayOfValues[i] = value;
+            size++;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < arrayOfKeys.length; i++) {
+            if (key != null && key.equals((arrayOfKeys[i]))
+                    || arrayOfKeys[i] == null) {
+                return (V) arrayOfValues[i];
+            }
+        }
         return null;
+
     }
 }
