af8299651b96f469c5c69849281103604b1ab3a6
IegorSoboliev <evsobolev@gmail.com>
2019-12-10 15:41:57 +0200

    Done using HashMap

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,14 +2,23 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.HashMap;
+import java.util.Map;
+//import java.util.Set; for (MapEntry entry : storage.entrySet())
+
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private K key;
+    private V value;
+    private Map<K, V> storage = new HashMap<K, V>();
 
     @Override
     public void put(K key, V value) {
+        storage.put(key, value);
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return storage.get(key);
     }
 }
+
