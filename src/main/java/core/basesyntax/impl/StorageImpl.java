44312dd9951079a1b0dc3528aa6934e4bef436b4
karsv <karsv.dvz@gmail.com>
2019-12-10 14:14:19 +0200

    add solution hw62 with spike)

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,14 +2,69 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
+    List<Entry> list = new ArrayList<>();
+    Entry nullEntry = new Entry(null, null);
 
     @Override
     public void put(K key, V value) {
+        if (key == null) {
+            nullEntry.setValue(value);
+        } else {
+            for (Entry entry : list) {
+                if (key.equals(entry.getKey())) {
+                    entry.setValue(value);
+                }
+            }
+            list.add(new Entry(key, value));
+        }
     }
 
     @Override
     public V get(K key) {
+        if (key == null) {
+            return nullEntry.getValue();
+        } else {
+            for (Entry entry : list) {
+                if (key.equals(entry.getKey())) {
+                    return entry.getValue();
+                }
+            }
+        }
         return null;
+    }
+
+    private class Entry {
+        private K key;
+        private V value;
+
+        Entry(K k, V v) {
+            key = k;
+            value = v;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        public V getValue() {
+            return value;
+        }
+    }
+
+    public static void main(String[] args) {
+        Storage<Integer, String> storage = new StorageImpl<>();
+        String element = "Element";
+
+        storage.put(null, element);
+
+        System.out.println(storage.get(1));
     }
 }
