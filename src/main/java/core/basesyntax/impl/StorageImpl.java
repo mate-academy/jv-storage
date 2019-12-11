9370cf898d349c7d6cd566d2c5b5c11ce31254a1
Riptod <otoval15>
2019-12-11 14:52:33 +0200

    added silution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,13 +3,37 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int size = 10;
+    private int counter = 0;
+    private Object[] keys;
+    private Object[] values;
+
+    public StorageImpl() {
+        keys = new Object[size];
+        values = new Object[size];
+    }
 
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < keys.length; i++) {
+            if (key != null && key.equals(keys[i])) {
+                keys[i] = key;
+                values[i] = value;
+                return;
+            }
+        }
+        keys[counter] = key;
+        values[counter] = value;
+        counter++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] == key || (key != null && key.equals(keys[i]))) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 }
