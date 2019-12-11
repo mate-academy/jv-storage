032b3a8b1ef224672ba32ea81249a7281b0fc836
Dmitrii <zinchukdimaa@gmail.com>
2019-12-11 11:26:45 +0200

    added homeWork-6.2-Solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,12 +4,38 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
+    public static final int STORAGE_SIZE = 10;
+    Object[] keyStorage = new Object[STORAGE_SIZE];
+    Object[] valueStorage = new Object[STORAGE_SIZE];
+    int positionCounter = 0;
+
     @Override
     public void put(K key, V value) {
+        if (key == null) {
+            keyStorage[positionCounter] = null;
+            valueStorage[positionCounter] = value;
+            positionCounter++;
+        } else {
+            for (int i = 0; i < positionCounter; i++) {
+                if (key != null && key.equals(keyStorage[i]) || key == keyStorage[i]) {
+                    keyStorage[i] = key;
+                    valueStorage[i] = value;
+                    positionCounter++;
+                }
+            }
+            keyStorage[positionCounter] = key;
+            valueStorage[positionCounter] = value;
+            positionCounter++;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < positionCounter; i++) {
+            if (key != null && key.equals(keyStorage[i]) || key == keyStorage[i]) {
+                return (V) valueStorage[i];
+            }
+        }
         return null;
     }
 }
