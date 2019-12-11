b91746d6081b2096fb6e7f2c6b2bac02d81c55e5
Mykhailo Kramar <3805033@gmail.com>
2019-12-11 16:48:43 +0200

    hw6.2

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,18 +1,30 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.HashMap;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    HashMap<K, V> storage = new HashMap<>();
+    private static final int ARRAY_SIZE = 10;
+    private Object[] keyStorage = new Object[ARRAY_SIZE];
+    private Object[] valueStorage = new Object[ARRAY_SIZE];
 
     @Override
     public void put(K key, V value) {
-        storage.put(key, value);
+        for (int i = 0; i < valueStorage.length; i++) {
+            if (valueStorage[i] == null & keyStorage[i] == null | keyStorage[i] == key) {
+                this.valueStorage[i] = value;
+                this.keyStorage[i] = key;
+                break;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
-        return storage.get(key);
+        for (int i = 0; i < keyStorage.length; i++) {
+            if (key == null && keyStorage[i] == null || key != null && key.equals(keyStorage[i])) {
+                return (V) valueStorage[i];
+            }
+        }
+        return null;
     }
 }
