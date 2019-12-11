c8a8e4c8411430557909a139af98f0a6dc73653a
Yaroslav <yaroslav2875@i.ua>
2019-12-11 15:07:53 +0200

    added hw 6.2 solution with changes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,34 +4,35 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int CAPACITY = 10;
-    private static Object[] arrayKeys = new Object[CAPACITY];
-    private static Object[] arrayValues = new Object[CAPACITY];
+    private int size;
+    private Object[] arrayKeys;
+    private Object[] arrayValues;
+
+    public StorageImpl() {
+        this.size = 0;
+        this.arrayKeys = new Object[CAPACITY];
+        this.arrayValues = new Object[CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < arrayKeys.length; i++) {
-            if (key == null && arrayKeys[i] == null) {
+        for (int i = 0; i < size + 1; i++) {
+            if (key == arrayKeys[i] && key != null && key.equals(arrayKeys[i])) {
+                arrayValues[i] = value;
+                return;
+            } else if (i == size && size < CAPACITY) {
                 arrayKeys[i] = key;
                 arrayValues[i] = value;
-                return;
-            } else if (key != null && key.equals(arrayKeys[i])) {
-                arrayValues[i] = value;
-                return;
-            } else if (key != null && arrayKeys[i] == null && arrayValues[i] == null) {
-                arrayKeys[i] = key;
-                arrayValues[i] = value;
-                return;
             }
         }
+        size++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < arrayKeys.length; i++) {
-            if (key == null && arrayKeys[i] == null) {
-                return (V)arrayValues[i];
-            } else if (key != null && key.equals(arrayKeys[i])) {
-                return (V)arrayValues[i];
+            if (key == arrayKeys[i] || (key != null && key.equals(arrayKeys[i]))) {
+                return (V) arrayValues[i];
             }
         }
         return null;
