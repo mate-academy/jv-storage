cde56af5c4f9036568441b224c437cacb7523d40
RUSLAN SIMAKOV <UA667766706@GMAIL.COM>
2019-12-11 15:13:40 +0200

    task done

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,12 +4,41 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
+    private int counter = 0;
+    private final int storageSize = 10;
+    private Object[] key = new Object[storageSize];
+    private Object[] value = new Object[storageSize];
+
+    public StorageImpl() {
+    }
+
     @Override
     public void put(K key, V value) {
+
+        for (int i = 0; i < counter; i++) {
+
+            if (this.key[i] == key || this.key[i] != null && (this.key[i]).equals(key)) {
+                this.value[i] = value;
+                return;
+            }
+        }
+        if (counter == storageSize) {
+            return;
+        } else {
+            this.value[counter] = value;
+            this.key[counter] = key;
+            ++counter;
+        }
     }
 
     @Override
     public V get(K key) {
+
+        for (int i = 0; i < counter; i++) {
+            if (this.key[i] == key || this.key[i].equals(key)) {
+                return (V) value[i];
+            }
+        }
         return null;
     }
 }
