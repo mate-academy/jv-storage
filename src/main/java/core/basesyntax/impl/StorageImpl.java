97663b30776f43c0d31b7cf03126e002cca7d1b0
Misha <mishabeheka@gmail.com>
2019-12-11 12:04:18 +0200

    Deleted array StorageImpl and add class Node

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,24 +4,14 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
-    public static StorageImpl[] collection = new StorageImpl[10];
-
-    private K key;
-    private V value;
-
-    public StorageImpl() {
-    }
-
-    private StorageImpl(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
+    public static final int SIZE = 10;
+    private static Node[] collection = new Node[SIZE];
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < collection.length; ++i) {
             if (collection[i] == null || collection[i].key == key) {
-                collection[i] = new StorageImpl<>(key, value);
+                collection[i] = new Node<>(key, value);
                 return;
             }
         }
@@ -39,5 +29,15 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             }
         }
         return val;
+    }
+
+    static class Node<K, V> {
+        private K key;
+        private V value;
+
+        private Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }
