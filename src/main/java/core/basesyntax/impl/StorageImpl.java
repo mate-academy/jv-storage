3457f4d91973db2b8a57820d1fc4f6a503d5d04a
Misha <mishabeheka@gmail.com>
2019-12-11 08:38:57 +0200

    Done HW 6.2. All testes are successful.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,7 +4,7 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
-    public static StorageImpl [] collection = new StorageImpl[10];
+    public static StorageImpl[] collection = new StorageImpl[10];
 
     private K key;
     private V value;
@@ -12,24 +12,16 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public StorageImpl() {
     }
 
-    public StorageImpl(K key, V value) {
+    private StorageImpl(K key, V value) {
         this.key = key;
         this.value = value;
     }
 
-    public K getKey() {
-        return key;
-    }
-
-    public V getValue() {
-        return value;
-    }
-
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < collection.length; ++i){
-            if (collection[i] == null || collection[i].getKey() == key){
-                collection[i] = new StorageImpl<>(key,value);
+        for (int i = 0; i < collection.length; ++i) {
+            if (collection[i] == null || collection[i].key == key) {
+                collection[i] = new StorageImpl<>(key, value);
                 return;
             }
         }
@@ -39,30 +31,13 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public V get(K key) {
         V val = null;
 
-        for (int i = 0; i < collection.length; ++i){
-            if (collection[i] != null && collection[i].getKey() == key){
+        for (int i = 0; i < collection.length; ++i) {
+            if (collection[i] != null
+                    && (collection[i].key == key
+                    || collection[i].key.equals(key))) {
                 val = (V) collection[i].value;
             }
         }
         return val;
-    }
-
-    public boolean equals(Object object){
-        if (this == object){
-            return true;
-        }
-        if (object != null || object.getClass() != this.getClass()){
-            return false;
-        }
-        StorageImpl storageImpl = (StorageImpl) object;
-        return key.equals(storageImpl.key) && value.equals(storageImpl.value);
-    }
-
-    public int hashCode(){
-        int prime = 31;
-        int result = 1;
-        result = prime + result * key.hashCode();
-        result = prime + result * value.hashCode();
-        return result;
     }
 }
