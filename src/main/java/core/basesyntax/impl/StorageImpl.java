e142a29e7c09fde6f026504895aae950537f38ff
Riptod <otoval15>
2019-12-11 15:46:56 +0200

    fixed names)

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,35 +3,35 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int size = 10;
-    private int counter = 0;
-    private Object[] keys;
-    private Object[] values;
+    private static final int CAPACITY = 10;
+    private int Counter = 0;
+    private Object[] Keys;
+    private Object[] Values;
 
     public StorageImpl() {
-        keys = new Object[size];
-        values = new Object[size];
+        Keys = new Object[CAPACITY];
+        Values = new Object[CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < keys.length; i++) {
-            if (key != null && key.equals(keys[i])) {
-                keys[i] = key;
-                values[i] = value;
+        for (int i = 0; i < Keys.length; i++) {
+            if (key != null && key.equals(Keys[i])) {
+                Keys[i] = key;
+                Values[i] = value;
                 return;
             }
         }
-        keys[counter] = key;
-        values[counter] = value;
-        counter++;
+        Keys[Counter] = key;
+        Values[Counter] = value;
+        Counter++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i] == key || (key != null && key.equals(keys[i]))) {
-                return (V) values[i];
+        for (int i = 0; i < Keys.length; i++) {
+            if (Keys[i] == key || (key != null && key.equals(Keys[i]))) {
+                return (V) Values[i];
             }
         }
         return null;
