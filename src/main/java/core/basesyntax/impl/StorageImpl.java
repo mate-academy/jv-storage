964ec0908c703009579f79a8d3562517522d6c85
RUSLAN SIMAKOV <UA667766706@GMAIL.COM>
2019-12-12 00:17:28 +0200

    task done 2

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,27 +2,32 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.Objects;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
 
+    public static final int STORAGE_SIZE = 10;
     private int counter = 0;
-    private final int storageSize = 10;
-    private Object[] key = new Object[storageSize];
-    private Object[] value = new Object[storageSize];
 
-    public StorageImpl() {
+    private Object[] key;
+    private Object[] value;
+
+    public <K, V> StorageImpl() {
+        this.key = new Object[STORAGE_SIZE];
+        this.value = new Object[STORAGE_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
 
         for (int i = 0; i < counter; i++) {
-
-            if (this.key[i] == key || this.key[i] != null && (this.key[i]).equals(key)) {
+            //this.key[i] == key || this.key[i] != null && (this.key[i]).equals(key)
+            if (Objects.equals(this.key[i], key)) {
                 this.value[i] = value;
                 return;
             }
         }
-        if (counter == storageSize) {
+        if (counter == STORAGE_SIZE) {
             return;
         } else {
             this.value[counter] = value;
@@ -35,7 +40,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public V get(K key) {
 
         for (int i = 0; i < counter; i++) {
-            if (this.key[i] == key || this.key[i].equals(key)) {
+            //this.key[i] == key || this.key[i].equals(key)
+            if (Objects.equals(this.key[i], key)) {
                 return (V) value[i];
             }
         }
