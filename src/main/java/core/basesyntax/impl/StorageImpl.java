f3b3799a85acdd6633fb6c564157ad8e1392e2f3
Alexandr Sashchenko <santys87@gmail.com>
2019-12-11 08:33:06 +0200

    Added mate HW 6.2 Solution v2

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -27,7 +27,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         arrayItems++;
     }
 
-    @Override
+    @Override @SuppressWarnings("unchecked")
     public V get(K key) {
         for (int i = 0; i < LENGTH; i++) {
             if ((keysArray[i] == key) || (keysArray[i] != null && keysArray[i].equals(key))) {
