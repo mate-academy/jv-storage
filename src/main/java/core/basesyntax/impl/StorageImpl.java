74115a776883696b51802973024395aac92c5f73
RUSLAN SIMAKOV <UA667766706@GMAIL.COM>
2019-12-12 11:23:24 +0200

    task done 3

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -21,7 +21,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public void put(K key, V value) {
 
         for (int i = 0; i < counter; i++) {
-            //this.key[i] == key || this.key[i] != null && (this.key[i]).equals(key)
+
             if (Objects.equals(this.key[i], key)) {
                 this.value[i] = value;
                 return;
@@ -40,7 +40,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public V get(K key) {
 
         for (int i = 0; i < counter; i++) {
-            //this.key[i] == key || this.key[i].equals(key)
+
             if (Objects.equals(this.key[i], key)) {
                 return (V) value[i];
             }
