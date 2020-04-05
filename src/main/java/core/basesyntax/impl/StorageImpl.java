2d2d38a870c31bbbf51ee1eae36bc3f59600bc7d
Oleksandr Husenko <oleksandrhusenko@gmail.com>
2020-04-06 01:24:49 +0300

    added solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,15 +1,84 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
+    public static final int CAPACITY = 10;
+
+    class Node<K, V> {
+        private K key;
+        private V value;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Node node = (Node) o;
+            return Objects.equals(key, node.key)
+                    && Objects.equals(value, node.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(key, value);
+        }
+    }
+
+    Node<K, V>[] nodes = new Node[CAPACITY];
+    int size = 0;
+
     @Override
     public void put(K key, V value) {
+        boolean isNodeWasRewritten = false;
+
+        for (Node node : nodes) {
+            if (node != null && node.getKey() != null && node.getKey().equals(key)) {
+                node.setValue(value);
+                isNodeWasRewritten = true;
+                break;
+            }
+        }
+
+        if (!isNodeWasRewritten) {
+            nodes[size++] = new Node(key, value);
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        V result = null;
+
+        for (Node node : nodes) {
+            if (node != null
+                    && (node.getKey() == null && key == null
+                    || node.getKey() != null && node.getKey().equals(key))) {
+                result = (V) node.getValue();
+                break;
+            }
+        }
+        return result;
     }
 }
