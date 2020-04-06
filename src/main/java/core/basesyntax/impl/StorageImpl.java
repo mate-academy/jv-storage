2672f1dadea8f0c688c1f9606d2ee185674ffe33
Kseniia Makarova <kseniia.makarova.kyiv@gmail.com>
2020-04-06 17:27:19 +0300

    addded check to if clause in put() so that it would set new value to key null

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -17,7 +17,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] != null && keys[i].equals(key)) {
+            if (keys[i] == null && key == null || keys[i] != null && keys[i].equals(key)) {
                 values[i] = value;
                 return;
             }
