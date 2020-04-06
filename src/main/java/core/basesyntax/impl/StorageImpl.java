a415b65418e50503d5fad4813e3665c531b52f4c
VitaliiLeshchenko <411100253g@gmail.com>
2020-04-06 14:56:49 +0300

    solution with lists

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,15 +1,26 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
+    List<K> listKay = new ArrayList<K>();
+    List<V> listValue = new ArrayList<V>();
+
     @Override
     public void put(K key, V value) {
+        if (listKay.contains(key)) {
+            listValue.add(listKay.indexOf(key), value);
+        } else {
+            listKay.add(key);
+            listValue.add(value);
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return listKay.contains(key) ? listValue.get(listKay.indexOf(key)) : null;
     }
 }
