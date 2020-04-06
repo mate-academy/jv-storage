d3e8ca6e892add73f8f332eb6ee90d06de2f44a4
ihor <krohmal.igor777@gmail.com>
2020-04-06 20:25:03 +0300

    redone class StorageImpl with using array keys and array values

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,68 +6,43 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     private static final int SIZE = 10;
 
-    private K key;
-    private V value;
-    private StorageImpl<K, V>[] storageArray;
-
-    private StorageImpl(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
+    private K[] keys;
+    private V[] values;
 
     public StorageImpl() {
-        storageArray = new StorageImpl[SIZE];
+        keys = (K[]) new Object[SIZE];
+        values = (V[]) new Object[SIZE];
     }
 
     @Override
     public void put(K key, V value) {
-        for (StorageImpl<K, V> storage : storageArray) {
-            if (storage == null) {
-                break;
-            }
-            if (storage.getKey() != null
-                    && storage.getKey().equals(key)) {
-                storage.setValue(value);
+        for (int i = 0; i < SIZE; i++) {
+            if (keys[i] == null && key == null) {
+                values[i] = value;
                 return;
             }
-        }
-        StorageImpl<K, V> storage = new StorageImpl<>(key, value);
-        for (int i = 0; i < storageArray.length; i++) {
-            if (storageArray[i] == null) {
-                storageArray[i] = storage;
-                break;
+            if (keys[i] != null && keys[i].equals(key)) {
+                values[i] = value;
+                return;
+            }
+            if (keys[i] == null && values[i] == null && key != null) {
+                keys[i] = key;
+                values[i] = value;
+                return;
             }
         }
     }
 
     @Override
     public V get(K key) {
-        for (StorageImpl<K, V> storage : storageArray) {
-            if (storage == null) {
-                break;
+        for (int i = 0; i < SIZE; i++) {
+            if (keys[i] == null && key == null) {
+                return values[i];
             }
-            if (storage.getKey() == null && key == null) {
-                return storage.getValue();
-            }
-            if (storage.getKey() == null) {
-                continue;
-            }
-            if (storage.getKey().equals(key)) {
-                return storage.getValue();
+            if (keys[i] != null && keys[i].equals(key)) {
+                return values[i];
             }
         }
         return null;
-    }
-
-    private K getKey() {
-        return key;
-    }
-
-    private V getValue() {
-        return value;
-    }
-
-    private void setValue(V value) {
-        this.value = value;
     }
 }
