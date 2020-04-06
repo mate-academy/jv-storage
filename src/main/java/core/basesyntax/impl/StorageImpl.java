32413c2b990b8347a9daa0799de0fd83f50185ca
Vinglish <khan.ildar94@gmail.com>
2020-04-06 15:17:13 +0300

    storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,15 +1,44 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private Object[] keyStorage = new Object[10];
+    private Object[] valueStorage = new Object[10];
+    private int index;
 
     @Override
     public void put(K key, V value) {
+        if (find(key)) {
+            for (int i = 0; i < keyStorage.length; i++) {
+                if (Objects.equals(keyStorage[i], key)) {
+                    valueStorage[i] = value;
+                }
+            }
+        } else {
+            keyStorage[index] = key;
+            valueStorage[index] = value;
+            index++;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keyStorage.length; i++) {
+            if (Objects.equals(keyStorage[i], key)) {
+                return (V) valueStorage[i];
+            }
+        }
         return null;
+    }
+
+    private boolean find(K key) {
+        for (Object el : keyStorage) {
+            if (Objects.equals(el, key)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
