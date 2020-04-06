fb2866d65e5e33c941f6f05139b254c712b345a3
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-04-06 22:17:13 +0300

    fixed methods put and get

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,38 +3,30 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private Object[] keys = new Object[10];
-    private Object[] values = new Object[10];
+    private K[] keys = (K[]) new Object[10];
+    private V[] values = (V[]) new Object[10];
+    private int index;
 
     @Override
     public void put(K key, V value) {
-        if (get(key) == null) {
-            for (int i = 0; i < keys.length; i++) {
-                if (keys[i] == null & values[i] == null) {
-                    keys[i] = key;
-                    values[i] = value;
-                    break;
-                }
-            }
-        } else {
-            for (int i = 0; i < keys.length; i++) {
-                if (key.equals(keys[i])) {
-                    keys[i] = key;
-                    values[i] = value;
-                    break;
-                }
+        for (int i = 0; i < keys.length; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
+                keys[i] = key;
+                values[i] = value;
+                index++;
+                return;
             }
         }
+        keys[index] = key;
+        values[index] = value;
+        index++;
     }
 
     @Override
     public V get(K key) {
         V value = null;
         for (int i = 0; i < keys.length; i++) {
-            if (key != null && key.equals(keys[i])) {
-                value = (V) values[i];
-                break;
-            } else if (key == keys[i]) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
                 value = (V) values[i];
                 break;
             }
