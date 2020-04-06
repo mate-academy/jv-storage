8c4e3f9d52b4d3c5b97bca44f5c7f9453961fac6
Sumned <vladar91@gmail.com>
2020-04-06 23:12:35 +0300

    total upgrade

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,61 +1,51 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.ArrayList;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private K key;
-    private V value;
-    private ArrayList<K> lsKey = new ArrayList<K>();
-    private ArrayList<V> lsValue = new ArrayList<V>();
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    public StorageImpl() {
+        int length = 10;
+        this.keys = (K[]) new Object[length];
+        this.values = (V[]) new Object[length];
+        size = 0;
+
+    }
+
+    private boolean keyChecker(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] != null ? keys[i].equals(key) : keys[i] == key) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     @Override
     public void put(K key, V value) {
-        this.key = key;
-        this.value = value;
-        if (lsKey.contains(key)) {
-            lsValue.set(lsKey.indexOf(key), value);
+        if (!keyChecker(key)) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
         } else {
-            lsKey.add(key);
-            lsValue.add(value);
+            for (int i = 0; i < size; i++) {
+                if (keys[i] != null ? keys[i].equals(key) : keys[i] == key) {
+                    values[i] = value;
+                }
+            }
         }
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < lsKey.size(); i++) {
-            if (lsKey.get(i) != null ? lsKey.get(i).equals(key) : lsKey.get(i) == key) {
-                return lsValue.get(i);
+        for (int i = 0; i < size; i++) {
+            if (keys[i] != null ? keys[i].equals(key) : keys[i] == key) {
+                return values[i];
             }
         }
         return null;
     }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null || this.getClass() != obj.getClass()) {
-            return false;
-        }
-        StorageImpl<K, V> pair = (StorageImpl) obj;
-        if (key != null ? !key.equals(pair.key) : pair.key != null) {
-            return false;
-        }
-        if (value != null ? !value.equals(pair.value) : pair.value != null) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int primary = 43;
-        int result = primary + (key == null ? 0 : key.hashCode());
-        result = result * primary + (value == null ? 0 : value.hashCode());
-        return result;
-    }
 }
-
-
