f895aa1d54d16a6c28290e07875d62651e79a637
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-04-06 17:35:43 +0300

    added methods put() and get()

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,13 +3,42 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private Object[] keys = new Object[10];
+    private Object[] values = new Object[10];
 
     @Override
     public void put(K key, V value) {
+        if (get(key) == null) {
+            for (int i = 0; i < keys.length; i++) {
+                if (keys[i] == null & values[i] == null) {
+                    keys[i] = key;
+                    values[i] = value;
+                    break;
+                }
+            }
+        } else {
+            for (int i = 0; i < keys.length; i++) {
+                if (key.equals(keys[i])) {
+                    keys[i] = key;
+                    values[i] = value;
+                    break;
+                }
+            }
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        V value = null;
+        for (int i = 0; i < keys.length; i++) {
+            if (key != null && key.equals(keys[i])) {
+                value = (V) values[i];
+                break;
+            } else if (key == keys[i]) {
+                value = (V) values[i];
+                break;
+            }
+        }
+        return value;
     }
 }
