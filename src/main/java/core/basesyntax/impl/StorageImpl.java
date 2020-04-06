59e9f51b6e1c4176a672edeaa2062b4f88f1758f
vladyslav <vlad.bes0711@gmail.com>
2020-04-06 15:09:36 +0300

    added solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,13 +3,57 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int LENGTH = 10;
+    private Object[] keys;
+    private Object[] values;
+    private int index;
+
+    public StorageImpl() {
+        this.keys = new Object[LENGTH];
+        this.values = new Object[LENGTH];
+        this.index = 0;
+    }
 
     @Override
     public void put(K key, V value) {
+        boolean isExist = false;
+        if (key == null) {
+            for (int i = 0; i < index; i++) {
+                if (key == keys[i]) {
+                    keys[i] = key;
+                    values[i] = value;
+                    isExist = true;
+                }
+            }
+        } else {
+            for (int i = 0; i < index; i++) {
+                if (key.equals(keys[i])) {
+                    keys[i] = key;
+                    values[i] = value;
+                    isExist = true;
+                }
+            }
+        }
+        if (!isExist) {
+            keys[index] = key;
+            values[index] = value;
+            index++;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < LENGTH; i++) {
+            if (key == null) {
+                if (key == keys[i]) {
+                    return (V) values[i];
+                }
+            } else {
+                if (key.equals(keys[i])) {
+                    return (V) values[i];
+                }
+            }
+        }
         return null;
     }
 }
