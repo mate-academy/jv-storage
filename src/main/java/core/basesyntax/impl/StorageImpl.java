05dea879854bbd6ed56231b13570949910cc95ed
ana-dav <opa.super.op@gmail.com>
2020-04-06 23:58:54 +0300

    6.2.2

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,13 +3,102 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    static final int DEFAULT_INITIAL_CAPACITY = 10;
+    private final double MAX_LOAD_FACTOR = 0.75;
+    Node<K, V>[] nodes;
+    int size;
+
+    public StorageImpl() {
+        this.nodes = new Node[DEFAULT_INITIAL_CAPACITY];
+        size = 0;
+    }
 
     @Override
     public void put(K key, V value) {
+        boolean valueAdded = false;
+
+        for (Node node : nodes) {
+            if (node != null && node.getKey() != null && node.getKey().equals(key)) {
+                node.setValue(value);
+                valueAdded = true;
+                break;
+            }
+        }
+
+        if (!valueAdded) {
+            nodes[size++] = new Node(key, value);
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        V result = null;
+
+        for (Node node : nodes) {
+            if (node != null
+                    && (node.getKey() == null && key == null
+                    || node.getKey() != null && node.getKey().equals(key))) {
+                result = (V) node.getValue();
+                break;
+            }
+        }
+        return result;
+    }
+
+    private class Node<K, V> {
+        private  K key;
+        private V value;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
