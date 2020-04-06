c47db5476801b3398a6d097885beb6aebc616523
VitaliiLeshchenko <411100253g@gmail.com>
2020-04-06 17:41:33 +0300

    solution with Arrays

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,26 +1,45 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-
-    List<K> listKay = new ArrayList<K>();
-    List<V> listValue = new ArrayList<V>();
+    public static final int ARRAY_MAX_LENGTH = 20;
+    private Object[] keys = new Object[ARRAY_MAX_LENGTH];
+    private Object[] values = new Object[ARRAY_MAX_LENGTH];
+    private int count = 0;
 
     @Override
     public void put(K key, V value) {
-        if (listKay.contains(key)) {
-            listValue.add(listKay.indexOf(key), value);
+        if (this.contain(key) == -1) {
+            keys[count] = key;
+            values[count] = value;
+            count++;
         } else {
-            listKay.add(key);
-            listValue.add(value);
+            values[contain(key)] = value;
         }
     }
 
     @Override
     public V get(K key) {
-        return listKay.contains(key) ? listValue.get(listKay.indexOf(key)) : null;
+
+        return this.contain(key) == -1 ? null : (V) values[this.contain(key)];
+    }
+
+    private int contain(K key) {
+        for (int i = 0; i < count; i++) {
+            if (Objects.equals(keys[i], key)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        StorageImpl<Integer, String> storage = new StorageImpl<>();
+        storage.put(0, "0");
+        storage.put(123, "123");
+        storage.put(null, "null");
+        System.out.println(storage.get(null));
     }
 }
