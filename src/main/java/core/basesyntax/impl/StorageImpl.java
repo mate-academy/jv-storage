07475b3b9ea61d6ddec47f0cd744552694733220
VitaliiLeshchenko <411100253g@gmail.com>
2020-04-06 20:46:25 +0300

    removed Objects.equals and little mistakes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,45 +1,41 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    public static final int ARRAY_MAX_LENGTH = 20;
-    private Object[] keys = new Object[ARRAY_MAX_LENGTH];
-    private Object[] values = new Object[ARRAY_MAX_LENGTH];
+    public static final int ARRAY_MAX_LENGTH = 10;
+    private Object[] keys;
+    private Object[] values;
     private int count = 0;
+
+    public StorageImpl() {
+        keys = new Object[ARRAY_MAX_LENGTH];
+        values = new Object[ARRAY_MAX_LENGTH];
+    }
 
     @Override
     public void put(K key, V value) {
-        if (this.contain(key) == -1) {
+        if (whereContain(key) == -1) {
             keys[count] = key;
             values[count] = value;
             count++;
         } else {
-            values[contain(key)] = value;
+            values[whereContain(key)] = value;
         }
     }
 
     @Override
     public V get(K key) {
 
-        return this.contain(key) == -1 ? null : (V) values[this.contain(key)];
+        return whereContain(key) == -1 ? null : (V) values[whereContain(key)];
     }
 
-    private int contain(K key) {
+    private int whereContain(K key) {
         for (int i = 0; i < count; i++) {
-            if (Objects.equals(keys[i], key)) {
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
                 return i;
             }
         }
         return -1;
-    }
-
-    public static void main(String[] args) {
-        StorageImpl<Integer, String> storage = new StorageImpl<>();
-        storage.put(0, "0");
-        storage.put(123, "123");
-        storage.put(null, "null");
-        System.out.println(storage.get(null));
     }
 }
