25b186d63ebb620ee043ee9a95b2eda036ba72aa
Максим Матуляк <maksymmatuliak@gmail.com>
2020-04-07 16:32:13 +0300

    rewrite

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -15,7 +15,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < keys.length; i++) {
+        for (int i = 0; i < size; i++) {
             if ((keys[i] == null && values == null) || (key != null && key.equals(keys[i]))) {
                 values[i] = value;
                 return;
