dd9e4cfc21f6096786b7b88379dd17431bb33708
SpringWay <44027294+SpringWay@users.noreply.github.com>
2020-04-07 12:41:37 +0300

    solution to hw-6.2.2

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,14 +2,37 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
-public class StorageImpl<K, V> implements Storage<K, V> {
+public class StorageImpl<K,V> implements Storage<K,V> {
+    private static final int STORAGE_LENGTH = 10;
+    private K[] keys;
+    private V[] values;
+    private int size;
 
-    @Override
-    public void put(K key, V value) {
+    public StorageImpl() {
+        keys = (K[]) new Object[STORAGE_LENGTH];
+        values = (V[]) new Object[STORAGE_LENGTH];
+        size = 0;
     }
 
     @Override
-    public V get(K key) {
+    public void put(K keyPut, V valuePut) {
+        for (int i = 0; i < size; i++) {
+            if (keyPut == keys[i] || (keyPut != null && keyPut.equals(keys[i]))) {
+                values[i] = valuePut;
+            }
+        }
+        keys[size] = keyPut;
+        values[size] = valuePut;
+        size++;
+    }
+
+    @Override
+    public V get(K keyGet) {
+        for (int i = 0; i < size; i++) {
+            if (keyGet == keys[i] || (keyGet != null && keyGet.equals(keys[i]))) {
+                return values[i];
+            }
+        }
         return null;
     }
 }
