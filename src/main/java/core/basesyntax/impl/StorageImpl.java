e1461e0548e8f17121b7b6cad5bc3373c9b0551a
Nick97-git <nickarchangel97@gmail.com>
2020-04-07 10:54:15 +0300

    replaced keys.length with index in put method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -22,7 +22,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             index++;
             return;
         }
-        for (int i = 0; i < keys.length; i++) {
+        for (int i = 0; i < index; i++) {
             if (compareKeys(key,keys[i])) {
                 values[i] = value;
             }
