1e7a1ea0eeda712f74fb27f6cfced057f10c3296
Rostislav Levadnyi <rlevadniy@ukr.net>
2020-04-07 13:05:44 +0300

    code was refactored and was added an initialization to class class constructor

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,61 +4,48 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     static final int ARRAY_LENGTH = 10;
-    K[] keyArray = (K[]) new Object[0];
-    V[] valuesArray = (V[]) new Object[0];
+    K[] keyArray;
+    V[] valuesArray;
+    Integer massiveLength;
 
     public StorageImpl() {
+        this.keyArray = (K[]) new Object[ARRAY_LENGTH];
+        this.valuesArray = (V[]) new Object[ARRAY_LENGTH];
+        this.massiveLength = 0;
 
     }
 
     @Override
     public void put(K key, V value) {
-        if (keyArray.length > ARRAY_LENGTH) {
-            if (checking(key) != null) {
-                valuesArray[checking(key)] = value;
+        if (checking(key) == null) {
+            for (int i = massiveLength; i < this.keyArray.length; i++) {
+                if (this.keyArray[i] == null && this.valuesArray[i] == null) {
+                    this.keyArray[i] = key;
+                    this.valuesArray[i] = value;
+                    this.massiveLength += 1;
+                    break;
+                }
             }
         } else {
-            if (checking(key) == null) {
-                K[] cloneOfKeyArray = keyArray.clone();
-                V[] cloneOfValuesArray = valuesArray.clone();
-                keyArray = (K[]) new Object[keyArray.length + 1];
-                valuesArray = (V[]) new Object[valuesArray.length + 1];
-
-                for (int i = 0; i < cloneOfKeyArray.length; i++) {
-                    keyArray[i] = cloneOfKeyArray[i];
-                    valuesArray[i] = cloneOfValuesArray[i];
-                }
-                keyArray[keyArray.length - 1] = key;
-                valuesArray[valuesArray.length - 1] = value;
-            } else {
-                if (checking(key) != null) {
-                    valuesArray[checking(key)] = value;
-                }
-            }
+            valuesArray[checking(key)] = value;
         }
     }
 
     @Override
     public V get(K key) {
         if (checking(key) != null) {
-            return valuesArray[checking(key)];
+            return this.valuesArray[checking(key)];
         } else {
             return null;
         }
     }
 
     public Integer checking(K key) {
-        if (key == null) {
-            for (int i = 0; i < keyArray.length; i++) {
-                if (keyArray[i] == null) {
-                    return i;
-                }
-            }
-            return null;
-        }
-        for (int j = 0; j < keyArray.length; j++) {
-            if (key.equals(keyArray[j])) {
-                return j;
+        for (int i = 0; i < this.keyArray.length; i++) {
+            if (((key == null && this.keyArray[i] == null)
+                    || (key != null && key.equals(this.keyArray[i])))
+                    && this.valuesArray[i] != null) {
+                return i;
             }
         }
         return null;
