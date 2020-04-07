f809fc87e507c1ef6271188f8aba717858b80e99
Yaroslav-Serhii <u.vusochanskiy@gmail.com>
2020-04-07 12:21:24 +0300

    Added constant MAX_ARRAY_SIZE, Corrected methods "put" and "get" according with PULL notes.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,36 +3,34 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ARRAY_SIZE = 10;
     private Node<K, V>[] nodes;
     private int checker;
 
     public StorageImpl() {
         checker = 0;
-        nodes = new Node[10];
+        nodes = new Node[MAX_ARRAY_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
-        boolean notRewrited = true;
         for (int i = 0; i < checker; i++) {
-            if ((key == null && nodes[i].key == null)
+            if ((key == nodes[i].key)
                     || (nodes[i].key != null && nodes[i].key.equals(key))) {
                 nodes[i].value = value;
-                notRewrited = false;
+                return;
             }
         }
-        if (notRewrited) {
-            nodes[checker] = new Node<K, V>();
-            nodes[checker].key = key;
-            nodes[checker].value = value;
-            checker++;
-        }
+        nodes[checker] = new Node<K, V>();
+        nodes[checker].key = key;
+        nodes[checker].value = value;
+        checker++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < checker; i++) {
-            if ((key == null && nodes[i].key == null)
+            if ((key == nodes[i].key)
                     || (nodes[i].key != null && nodes[i].key.equals(key))) {
                 return nodes[i].value;
             }
