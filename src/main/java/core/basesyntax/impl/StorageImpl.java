7e4fab817bd1349fa458b6dbda479259a2d92cec
Serhii Tsyhankov <40259977+fruts@users.noreply.github.com>
2020-04-07 12:16:56 +0300

    change indexOf() method and change put() method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -21,21 +21,13 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             values[index] = value;
             index++;
         }
-        if (key != null) {
-            for (int i = 0; i < keys.length; i++) {
-                if (key.equals(keys[i])) {
-                    values[i] = value;
-                }
-            }
-        }
+        values[indexOf(key)] = value;
     }
 
     public int indexOf(K key) {
-        if (key != null) {
-            for (int i = 0; i < keys.length; i++) {
-                if (key.equals(keys[i])) {
-                    return i;
-                }
+        for (int i = 0; i < keys.length; i++) {
+            if ((key == keys[i]) || (key != null && key.equals(keys[i]))) {
+                return i;
             }
         }
         return index;
