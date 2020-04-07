906307eba1a9058e9eca41bc1ea0de5ca4f18a47
Oleh888 <60585035+Oleh888@users.noreply.github.com>
2020-04-07 11:13:47 +0300

    Update src/main/java/core/basesyntax/impl/StorageImpl.java
    Co-Authored-By: Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -32,7 +32,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     private int findConformity(K key) {
         int number = -1;
-        for (int i = 0; i < keys.length; i++) {
+        for (int i = 0; i < index; i++) {
             if (key == keys[i] || (key != null && key.equals(keys[i]))) {
                 number = i;
             }
