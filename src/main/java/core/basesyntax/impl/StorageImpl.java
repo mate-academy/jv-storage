510dfbd8f18a1db8a3697b2d95b7b5733c0ddd0b
Володимир <matrix.vova@gmail.com>
2020-04-07 12:34:08 +0300

    added solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,12 +4,38 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
+    public static final int ARRAY_MAX_LENGTH = 10;
+    private Object[] keys;
+    private Object[] values;
+    private int count = 0;
+
+    public StorageImpl() {
+        keys = new Object[ARRAY_MAX_LENGTH];
+        values = new Object[ARRAY_MAX_LENGTH];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (whereContain(key) == -1) {
+            keys[count] = key;
+            values[count] = value;
+            count++;
+        } else {
+            values[whereContain(key)] = value;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return whereContain(key) == -1 ? null : (V) values[whereContain(key)];
+    }
+
+    private int whereContain(K key) {
+        for (int i = 0; i < count; i++) {
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
