55bbd6b0194322e57550416c6cf46b08dd9aeda2
vladpetrovskyi <v.petrovskyi98@gmail.com>
2020-04-07 11:47:27 +0300

    updated solution for homework 6.2.2 (moved initialization to constructor; simplified if-statements)

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,39 +4,34 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
-    private int elementNumber = 0;
-    private K[] keysArray = (K[]) new Object[10];
-    private V[] valuesArray = (V[]) new Object[10];
+    private int elementNumber;
+    private K[] keysArray;
+    private V[] valuesArray;
+
+    public StorageImpl() {
+        elementNumber = 0;
+        keysArray = (K[]) new Object[10];
+        valuesArray = (V[]) new Object[10];
+    }
 
     @Override
     public void put(K key, V value) {
-        boolean input = true;
         for (int i = 0; i < keysArray.length - 1; i++) {
-            if (key == null && keysArray[i] == null) {
+            if ((key == null && keysArray[i] == null) || key != null && key.equals(keysArray[i])) {
                 valuesArray[i] = value;
-                input = false;
-            } else if (key != null) {
-                if (key.equals(keysArray[i])) {
-                    valuesArray[i] = value;
-                    input = false;
-                }
+            } else {
+                keysArray[elementNumber] = key;
+                valuesArray[elementNumber] = value;
             }
         }
-        if (input) {
-            keysArray[elementNumber] = key;
-            valuesArray[elementNumber++] = value;
-        }
+        elementNumber++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < keysArray.length - 1; i++) {
-            if (key == null && keysArray[i] == null) {
+            if ((key == null && keysArray[i] == null) || key != null && key.equals(keysArray[i])) {
                 return valuesArray[i];
-            } else if (key != null) {
-                if (key.equals(keysArray[i])) {
-                    return valuesArray[i];
-                }
             }
         }
         return null;
