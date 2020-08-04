a4be5d69afa0e112defd1732249282111156a8c1
jlightgreen <ShvabovichJulia@gmail.com>
2020-08-05 01:12:03 +0300

    hw-7.2

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,9 +4,10 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private Object[][] box;
+    private static final int MAX_CAPACITY = 10;
 
     public StorageImpl() {
-        box = new Object[2][10];
+        box = new Object[2][MAX_CAPACITY];
     }
 
     @Override
@@ -15,14 +16,14 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             return;
         }
         boolean isKeyPresent = false;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < box[0].length; i++) {
             if (box[0][i] == key) {
                 box[1][i] = value;
                 isKeyPresent = true;
             }
         }
         if (!isKeyPresent) {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < box[0].length; i++) {
                 if (box[0][i] == null) {
                     box[0][i] = key;
                     box[1][i] = value;
@@ -34,7 +35,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < box[0].length; i++) {
             if (box[0][i] == key
                     || box[0][i] != null && box[0][i].equals(key)) {
                 return (V) box[1][i];
