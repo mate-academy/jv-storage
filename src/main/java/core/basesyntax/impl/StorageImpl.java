0566e572d40a9de12b179a1680ef3eda0f8a5f0c
Oleksii Tromsa <tromsaalex@gmail.com>
2020-08-04 21:37:25 +0300

    Refactor methods put() and get()

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -21,21 +21,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        if (key == null) {
-            for (int i = 0; i < size; i++) {
-                if (keys[i] == null) {
-                    values[i] = value;
-                    return;
-                }
-            }
-            keys[size] = key;
-            values[size] = value;
-            size++;
-            return;
-        }
 
         for (int i = 0; i < size; i++) {
-            if (key.equals((K)keys[i])) {
+            if (key == null ? keys[i] == null : key.equals((K) keys[i])) {
                 values[i] = value;
                 return;
             }
@@ -47,18 +35,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        if (key == null) {
-            for (int i = 0; i < size; i++) {
-                if (keys[i] == null) {
-                    return (V)values[i];
-                }
-            }
-            return null;
-        }
-
         for (int i = 0; i < size; i++) {
-            if (key.equals((K)keys[i])) {
-                return (V)values[i];
+            if (key == null ? keys[i] == null : key.equals((K) keys[i])) {
+                return (V) values[i];
             }
         }
         return null;
