9dd164beda7061574d6c75dcfeb6d2d8bf20c17b
Mikhail Shevchenko <michaelyshevchenko@gmail.com>
2020-08-04 19:07:15 +0300

    added solution to hw7-2

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,15 +1,51 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
+    private static final int STORAGE_CAPACITY = 10;
+
+    private int storageFullness = 0;
+    private final K[] keyArray;
+    private final V[] valueArray;
+
+    public StorageImpl() {
+        keyArray = (K[]) new Object[STORAGE_CAPACITY];
+        valueArray = (V[]) new Object[STORAGE_CAPACITY];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < STORAGE_CAPACITY; i++) {
+            if (i == storageFullness) {
+                keyArray[i] = key;
+                valueArray[i] = value;
+                storageFullness++;
+                return;
+            } else if (i < storageFullness && ((keyArray[i] == key)
+                    || (keyArray[i] != null && keyArray[i].equals(key)))) {
+                keyArray[i] = key;
+                valueArray[i] = value;
+                return;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < STORAGE_CAPACITY; i++) {
+            if (keyArray[i] == key || (keyArray[i] != null && keyArray[i].equals(key))) {
+                return valueArray[i];
+            }
+        }
         return null;
+    }
+
+    @Override
+    public String toString() {
+        return "StorageImpl{" + "keyArray=" + Arrays.toString(keyArray)
+                + ", valueArray=" + Arrays.toString(valueArray) + '}';
     }
 }
