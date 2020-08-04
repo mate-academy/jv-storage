6d1a59dd78cd6feb0fcc14f9a39a1f09683b8ab2
Alkey <Sirj777sirj@gmail.com>
2020-08-04 16:49:13 +0300

    homework7.2

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,12 +4,39 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
+    private static final int SIZE = 10;
+    private K[] key;
+    private V[] value;
+    private int iterator;
+
+    public StorageImpl() {
+        this.key = (K[]) new Object[SIZE];
+        this.value = (V[]) new Object[SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (iterator > 0) {
+            for (int i = 0; i < this.key.length; i++) {
+                if (((this.key[i] != null) && this.key[i].equals(key))) {
+                    this.value[i] = value;
+                    return;
+                }
+            }
+        }
+        this.key[iterator] = key;
+        this.value[iterator] = value;
+        ++iterator;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < iterator; i++) {
+            if (((this.key[i] != null) && this.key[i].equals(key))
+                    || (key == null && this.key[i] == null)) {
+                return this.value[i];
+            }
+        }
         return null;
     }
 }
