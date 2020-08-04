9a703c6ee3e68548e38f09682eaea4342904031f
aleksandr.hrankin <aleksandr.hrankin@gmail.com>
2020-08-04 16:18:26 +0300

    added solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,12 +4,41 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
+    private static final int ARRAY_SIZE = 10;
+
+    private final Integer[] keys;
+    private final V[] values;
+    private int index;
+
+    public StorageImpl() {
+        keys = new Integer[ARRAY_SIZE];
+        values = (V[]) new Object[ARRAY_SIZE];
+        index = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        int keyHashCode = key == null ? 0 : key.hashCode();
+        for (int i = 0; i < index; i++) {
+            if (keyHashCode == keys[i].hashCode()) {
+                values[i] = value;
+            }
+        }
+        keys[index] = keyHashCode;
+        values[index] = value;
+        index += 1;
     }
 
     @Override
     public V get(K key) {
+        int keyHashCode = key == null ? 0 : key.hashCode();
+        for (int i = 0; i < index; i++) {
+            if (keyHashCode == keys[i].hashCode()) {
+                return values[i];
+            }
+        }
         return null;
     }
 }
+
+
--- src/test/java/core/basesyntax/StorageImplTest.java
+++ src/test/java/core/basesyntax/StorageImplTest.java
@@ -38,10 +38,12 @@ public class StorageImplTest {
         String elementTwo = "Element 2";
         String elementTree = "Element 3";
         String elementFour = "Element 4";
+
         storage.put(1, elementOne);
         storage.put(null, elementTwo);
         storage.put(3, elementTree);
         storage.put(4, elementFour);
+
         Assert.assertEquals(
                 "Test failed! Method get(null) should return value",
                 elementTwo,
