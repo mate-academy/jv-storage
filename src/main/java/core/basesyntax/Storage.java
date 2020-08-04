70f97d68dcc2f683a36f2bb7bdf574b24138f294
jlightgreen <ShvabovichJulia@gmail.com>
2020-08-05 01:10:12 +0300

    hw-7.2

--- src/main/java/core/basesyntax/Storage.java
+++ src/main/java/core/basesyntax/Storage.java
@@ -6,4 +6,3 @@ public interface Storage<K, V> {
 
     V get(K key);
 }
-
--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,13 +3,43 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private Object[][] box;
+
+    public StorageImpl() {
+        box = new Object[2][10];
+    }
 
     @Override
     public void put(K key, V value) {
+        if (value == null) {
+            return;
+        }
+        boolean isKeyPresent = false;
+        for (int i = 0; i < 10; i++) {
+            if (box[0][i] == key) {
+                box[1][i] = value;
+                isKeyPresent = true;
+            }
+        }
+        if (!isKeyPresent) {
+            for (int i = 0; i < 10; i++) {
+                if (box[0][i] == null) {
+                    box[0][i] = key;
+                    box[1][i] = value;
+                    break;
+                }
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < 10; i++) {
+            if (box[0][i] == key
+                    || box[0][i] != null && box[0][i].equals(key)) {
+                return (V) box[1][i];
+            }
+        }
         return null;
     }
 }
