748e66ad7ec6eedb2fbad1f43f1299ba34452f0c
Borisko130 <krimenyukb@gmail.com>
2020-08-05 14:19:33 +0300

    Updated code again

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -8,28 +8,30 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     private K[] keysArray;
     private V[] valuesArray;
+    private int filledSize;
 
     public StorageImpl() {
         keysArray = (K[]) new Object[ARRAY_SIZE];
         valuesArray = (V[]) new Object[ARRAY_SIZE];
+        filledSize = 0;
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < ARRAY_SIZE; i++) {
-            if ((key == keysArray[i]
-                    || (key != null && key.equals(keysArray[i])))
-                    || valuesArray[i] == null) {
-                keysArray[i] = key;
+        for (int i = 0; i < filledSize; i++) {
+            if (key == keysArray[i] || (key != null && key.equals(keysArray[i]))) {
                 valuesArray[i] = value;
-                break;
+                return;
             }
         }
+        keysArray[filledSize] = key;
+        valuesArray[filledSize] = value;
+        filledSize++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < ARRAY_SIZE; i++) {
+        for (int i = 0; i < filledSize; i++) {
             if (key == keysArray[i] || (key != null && key.equals(keysArray[i]))) {
                 return valuesArray[i];
             }
