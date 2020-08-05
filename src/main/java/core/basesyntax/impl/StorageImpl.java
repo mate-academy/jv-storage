2b623f08b1e49998f8360e6ac3cfe95ab7972931
Roman <matviichuk.office@gmail.com>
2020-08-05 06:57:06 +0300

    added soulution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,13 +3,47 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX = 10;
+    private int size;
+    private K[] keys;
+    private V[] values;
+
+    public StorageImpl() {
+        size = 0;
+        keys = (K[]) new Object[MAX];
+        values = (V[]) new Object[MAX];
+    }
 
     @Override
     public void put(K key, V value) {
+        if (isKeyExist(key)) {
+            for (int i = 0; i < size; i++) {
+                if (isKeyExist(keys[i])) {
+                    values[i] = value;
+                }
+            }
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == null ? key == keys[i] : key.equals(keys[i])) {
+                return values[i];
+            }
+        }
         return null;
+    }
+
+    private boolean isKeyExist(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == null ? key == keys[i] : key.equals(keys[i])) {
+                return true;
+            }
+        }
+        return false;
     }
 }
