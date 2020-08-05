306268116f6e2f54083b18d4656eccd466e1fb22
Roman <matviichuk.office@gmail.com>
2020-08-05 16:36:09 +0300

    found and deleted repeated iteration deleted method isKeyExist created constructor

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,16 +2,21 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
-public class StorageImpl<K, V, M> implements Storage<K, V> {
+public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX = 10;
     private int size = 0;
-    private K[] keys = (K[]) new Object[MAX];
-    private V[] values = (V[]) new Object[MAX];
+    private K[] keys;
+    private V[] values;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX];
+        values = (V[]) new Object[MAX];
+    }
 
     @Override
     public void put(K key, V value) {
-        if (isKeyExist(key)) {
-            for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
+            if (key == null ? key == keys[i] : key.equals(keys[i])) {
                 values[i] = value;
                 return;
             }
@@ -29,14 +34,5 @@ public class StorageImpl<K, V, M> implements Storage<K, V> {
             }
         }
         return null;
-    }
-
-    private boolean isKeyExist(K key) {
-        for (int i = 0; i < size; i++) {
-            if (key == null ? key == keys[i] : key.equals(keys[i])) {
-                return true;
-            }
-        }
-        return false;
     }
 }
