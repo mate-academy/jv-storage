5a29a0125fcf5722bd4dd96aadcc817337c7d8e3
Marina <maryna.voloshyna.11@gmail.com>
2020-08-05 20:51:21 +0300

    the last

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -9,17 +9,17 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private int score;
 
     public StorageImpl() {
-        keys = new Object[10];
-        array = new Object[10];
-        score = 0;
+        keys = (K[]) new Object[10];
+        array = (V[]) new Object[10];
     }
 
     @Override
     public void put(K key, V value) {
-        for (int a = 0; a < keys.length; a++) {
+        for (int a = 0; a < score; a++) {
             if (key != null ? key.equals(keys[a])
                     : keys[a] == null) {
                 array[a] = value;
+                return;
             }
         }
         keys[score] = key;
@@ -29,7 +29,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int a = 0; a < keys.length; a++) {
+        for (int a = 0; a < score; a++) {
             if (key != null ? key.equals(keys[a])
                     : keys[a] == null) {
                 return (V) array[a];
