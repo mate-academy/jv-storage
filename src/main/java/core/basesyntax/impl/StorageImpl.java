cf67f293157b4778bf7350418f5c6d9ec07b022e
vladd <vladdyachkin11@gmail.com>
2020-08-05 20:58:09 +0300

    fixed 2

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,7 +3,7 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    static final int SIZE = 10;
+    private static final int SIZE = 10;
 
     private Object[] keys;
     private Object[] values;
