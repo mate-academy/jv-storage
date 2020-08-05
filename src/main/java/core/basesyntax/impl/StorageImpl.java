c0df2d43da196484633507df7f42d7b7d39b1c4f
ROMASHSX <andriiromash@gmail.com>
2020-08-05 10:42:19 +0300

    solution is updated

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,37 +4,37 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int SIZE = 10;
-    private int flag;
+    private int currentSize;
     private K[] keyArray;
     private V[] valueArray;
 
     public StorageImpl() {
         keyArray = (K[]) new Object[SIZE];
         valueArray = (V[]) new Object[SIZE];
-        flag = 0;
+        currentSize = 0;
     }
 
-    private boolean checkKey(K key1, K key2) {
+    private boolean keysAreEqual(K key1, K key2) {
         return key1 == key2 || key1 != null && key1.equals(key2);
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < flag; i++) {
-            if (checkKey(key, keyArray[i])) {
+        for (int i = 0; i < currentSize; i++) {
+            if (keysAreEqual(key, keyArray[i])) {
                 valueArray[i] = value;
-                break;
+                return;
             }
         }
-        keyArray[flag] = key;
-        valueArray[flag] = value;
-        flag++;
+        keyArray[currentSize] = key;
+        valueArray[currentSize] = value;
+        currentSize++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < flag; i++) {
-            if (checkKey(key, keyArray[i])) {
+        for (int i = 0; i < currentSize; i++) {
+            if (keysAreEqual(key, keyArray[i])) {
                 return valueArray[i];
             }
         }
