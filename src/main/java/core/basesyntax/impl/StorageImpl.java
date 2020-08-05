d8daa30242d61d4c4ad5dbae41e97d4e7fe7f07a
Ilya <ilya03449@gmail.com>
2020-08-05 17:58:04 +0300

    fixed

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,10 +5,11 @@ import core.basesyntax.Storage;
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_STORAGE_SIZE = 10;
     private int realSize;
-    private MyPair<K, V>[] myStorage = new MyPair[MAX_STORAGE_SIZE];
+    private MyPair<K, V>[] myStorage;
 
     public StorageImpl() {
-        this.realSize = 0;
+        realSize = 0;
+        myStorage = new MyPair[MAX_STORAGE_SIZE];
     }
 
     @Override
