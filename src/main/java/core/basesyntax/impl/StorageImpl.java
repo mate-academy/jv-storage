eaece1a44e09bcf9bfd4524a545ea472a7224714
Dubovaya <dubovaya.java@gmail.com>
2020-08-05 21:51:01 +0300

    solution StorageLera

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,13 +3,36 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int QUANTITY = 10;
+    private int quantum = 0;
+    private K[] keys;
+    private V[] values;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[QUANTITY];
+        values = (V[]) new Object[QUANTITY];
+    }
 
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < quantum; i++) {
+            if (key == null ? key == keys[i] : key.equals(keys[i])) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[quantum] = key;
+        values[quantum] = value;
+        quantum++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < quantum; i++) {
+            if (key == null ? key == keys[i] : key.equals(keys[i])) {
+                return values[i];
+            }
+        }
         return null;
     }
 }
