99f0ade0e3d5d5e56794a3bab87f1a2e246dc8f3
SetMyDream <56842438+SetMyDream@users.noreply.github.com>
2020-08-05 16:31:30 +0300

    Initial commit

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,13 +3,34 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private Object[] storage;
+    private int currentSize = 0;
+
+    public StorageImpl() {
+        storage = new Object[20];
+
+    }
 
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < currentSize; i++) {
+            if (key == storage[i] || (key != null && key.equals(storage[i]))) {
+                storage[i + 10] = value;
+                return;
+            }
+        }
+        storage[currentSize] = key;
+        storage[currentSize + 10] = value;
+        currentSize++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < currentSize; i++) {
+            if (key != null && key.equals(storage[i]) || key == storage[i]) {
+                return (V) storage[i + 10];
+            }
+        }
         return null;
     }
 }
