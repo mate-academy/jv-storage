0b7fbd17166b2049b80b3802a3c0bfe2801e4fef
glones <polivanchuk2013@gmail.com>
2020-08-05 11:22:36 +0300

    corrected

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,32 +4,28 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
-    private static final int arrayLength = 10;
+    private static final int ARRAY_LENGTH = 10;
     private int counter;
     private K[] keyArray;
     private V[] valueArray;
 
     public StorageImpl() {
-        keyArray = (K[]) new Object[arrayLength];
-        valueArray = (V[]) new Object[arrayLength];
+        keyArray = (K[]) new Object[ARRAY_LENGTH];
+        valueArray = (V[]) new Object[ARRAY_LENGTH];
         counter = 0;
     }
 
     @Override
     public void put(K key, V value) {
-        boolean checkKay = true;
         for (int i = 0; i < counter; i++) {
             if ((key == keyArray[i]) || (key != null && key.equals(keyArray[i]))) {
                 valueArray[i] = value;
-                checkKay = false;
-                break;
+                return;
             }
         }
-        if (checkKay) {
-            keyArray[counter] = key;
-            valueArray[counter] = value;
-            counter++;
-        }
+        keyArray[counter] = key;
+        valueArray[counter] = value;
+        counter++;
     }
 
     @Override
