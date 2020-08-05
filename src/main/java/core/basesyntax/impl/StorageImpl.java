d30200453651b9223481173bf4949925919375d7
Pavlo Khomiak <pavlo.khomiak93@gmail.com>
2020-08-05 11:21:05 +0300

    Refactor code

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,23 +3,34 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int STORAGE_SIZE = 10;
-    private static final Object[] KEY_ARRAY = new Object[STORAGE_SIZE];
-    private static final Object[] VALUE_ARRAY = new Object[STORAGE_SIZE];
+    private static final int STORAGE_CAPACITY = 10;
+    private int storageSize = 0;
+    private Object[] keyArray = null;
+    private Object[] valueArray = null;
+
+    public StorageImpl() {
+        keyArray = new Object[STORAGE_CAPACITY];
+        valueArray = new Object[STORAGE_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < KEY_ARRAY.length; i++) {
-            if (KEY_ARRAY[i] == null && VALUE_ARRAY[i] != null) {
-                continue;
-            }
-            if (KEY_ARRAY[i] == null) {
-                KEY_ARRAY[i] = key;
-                VALUE_ARRAY[i] = value;
+        for (int i = 0; i < keyArray.length; i++) {
+            if (storageSize == STORAGE_CAPACITY) {
                 break;
             }
-            if (KEY_ARRAY[i].equals(key)) {
-                VALUE_ARRAY[i] = value;
+            if (keyArray[i] == null && valueArray[i] != null) {
+                continue;
+            }
+            if (keyArray[i] == null) {
+                keyArray[i] = key;
+                valueArray[i] = value;
+                storageSize++;
+                break;
+            }
+            if (keyArray[i].equals(key)) {
+                valueArray[i] = value;
+                storageSize++;
                 break;
             }
         }
@@ -27,12 +38,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < KEY_ARRAY.length; i++) {
-            if (key == null && KEY_ARRAY[i] == null) {
-                return (V) VALUE_ARRAY[i];
-            }
-            if (KEY_ARRAY[i] != null && KEY_ARRAY[i].equals(key)) {
-                return (V) VALUE_ARRAY[i];
+        for (int i = 0; i < keyArray.length; i++) {
+            if ((key == null && keyArray[i] == null)
+                    || (keyArray[i] != null && keyArray[i].equals(key))) {
+                return (V) valueArray[i];
             }
         }
         return null;
