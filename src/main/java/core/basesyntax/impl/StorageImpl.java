ffdfbf82ec2f9d0fa55f08c172f76e2aa0168f7f
Martynice <marticiy@ukr.net>
2020-08-05 12:30:19 +0300

    another solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,34 +5,35 @@ import core.basesyntax.Storage;
 public class StorageImpl<K, V> implements Storage<K, V> {
 
     private static final int SIZE = 10;
-    private static final Object[] KEYS = new Object[SIZE];
-    private static final Object[] VALUES = new Object[SIZE];
+
+    private Object[] keys = new Object[SIZE];
+    private Object[] values = new Object[SIZE];
+    private int currentSize = 0;
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < SIZE; i++) {
-            if (KEYS[i] == null) {
-                KEYS[i] = key;
-                VALUES[i] = value;
-                return;
-            }
-            if (KEYS[i].equals(key)) {
-                VALUES[i] = value;
-                return;
+        for (int i = 0; i < currentSize; i++) {
+            if (keyCheck(key, i)) {
+                keys[i] = key;
+                values[i] = value;
             }
         }
+        keys[currentSize] = key;
+        values[currentSize] = value;
+        currentSize++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < SIZE; i++) {
-            if (key == null) {
-                return (V) VALUES[i + 1];
-            }
-            if (KEYS[i] != null && KEYS[i].equals(key)) {
-                return (V) VALUES[i];
+        for (int i = 0; i < currentSize; i++) {
+            if (keyCheck(key, i)) {
+                return (V) values[i];
             }
         }
         return null;
+    }
+
+    public boolean keyCheck(K key, int i) {
+        return key == null ? keys[i] == null : key.equals(keys[i]);
     }
 }
