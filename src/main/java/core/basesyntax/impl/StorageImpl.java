fdd2529f24d4b080b33ad2dfd1cfc9101be1e8fd
Vlad Kuzmin <vladkuzmin22@gmail.com>
2020-08-05 14:29:30 +0300

    hw7-2 fixxxed

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,43 +1,37 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     public static final int ARRAY_LENGTH = 10;
 
     public Object[] keyStorage;
     public Object[] valueStorage;
+    private int size;
 
     public StorageImpl() {
         keyStorage = (K[]) new Object[ARRAY_LENGTH];
         valueStorage = (V[]) new Object[ARRAY_LENGTH];
+        size = 0;
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < ARRAY_LENGTH; i++) {
-            if (keyStorage[i] == null && valueStorage[i] == null
-                    & !Arrays.stream(keyStorage).anyMatch(check -> check == key)) {
-                keyStorage[i] = key;
+        for (int i = 0; i < size; i++) {
+            if (key == keyStorage[i] || key != null && key.equals(keyStorage[i])) {
                 valueStorage[i] = value;
-            }
-            if (keyStorage[i] == key
-                    && !Arrays.stream(valueStorage).anyMatch(check -> check == value)) {
-                valueStorage[i] = value;
+                return;
             }
         }
+        keyStorage[size] = key;
+        valueStorage[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < ARRAY_LENGTH; i++) {
-            if (key == keyStorage[i]) {
-                return (V) valueStorage[i];
-            }
-        }
-        for (int i = 0; i < ARRAY_LENGTH; i++) {
-            if (key.equals(keyStorage[i])) {
+        for (int i = 0; i < size; i++) {
+            if (key == keyStorage[i] || key != null && key.equals(keyStorage[i])) {
                 return (V) valueStorage[i];
             }
         }
