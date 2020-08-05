ae1937d5f68a5fdd3a7e2b3f28b0c10df7043829
Andriy <andriy.lagun@gmail.com>
2020-08-05 11:25:32 +0300

    Implemented Pair class Implemented get,put methods for StorageImpl class

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,15 +1,49 @@
 package core.basesyntax.impl;
 
+import core.basesyntax.Pair;
 import core.basesyntax.Storage;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
+    private static final int MAXIMUM_SIZE = 10;
+    private final Pair<K, V>[] storageArray;
+    private int size = 0;
+
+    public StorageImpl() {
+        storageArray = new Pair[MAXIMUM_SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        boolean isKeyExist = storageArray[0] != null && Arrays.stream(storageArray)
+                .filter(Objects::nonNull)
+                .noneMatch(x -> x.getFirst() != key);
+        if (!isKeyExist) {
+            for (int i = 0; i < storageArray.length; i++) {
+                if (i == size) {
+                    storageArray[i] = Pair.of(key, value);
+                    size++;
+                    return;
+                }
+            }
+        } else {
+            Arrays.stream(storageArray)
+                    .filter(x -> x.getFirst().equals(key))
+                    .findFirst()
+                    .ifPresent(x -> x.setSecond(value));
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return storageArray[0] == null ? null : Arrays.stream(storageArray)
+                .filter(x -> x.getFirst() == key
+                        || x.getFirst() != null && x.getFirst().equals(key))
+                .findFirst()
+                .get()
+                .getSecond();
     }
 }
+
