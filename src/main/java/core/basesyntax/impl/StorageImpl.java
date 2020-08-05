cd1e63975067628b85e68cba2ece286bfaf0e4b6
Pavlo Khomiak <pavlo.khomiak93@gmail.com>
2020-08-05 18:02:10 +0300

    Change the painter

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,8 +5,8 @@ import core.basesyntax.Storage;
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int STORAGE_CAPACITY = 10;
     private int storageSize = 0;
-    private Object[] keyArray = null;
-    private Object[] valueArray = null;
+    private Object[] keyArray;
+    private Object[] valueArray;
 
     public StorageImpl() {
         keyArray = new Object[STORAGE_CAPACITY];
@@ -15,30 +15,24 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < keyArray.length; i++) {
-            if (storageSize == STORAGE_CAPACITY) {
-                break;
-            }
+        for (int i = 0; i < storageSize; i++) {
             if (keyArray[i] == null && valueArray[i] != null) {
                 continue;
-            }
-            if (keyArray[i] == null) {
-                keyArray[i] = key;
-                valueArray[i] = value;
-                storageSize++;
-                break;
             }
             if (keyArray[i].equals(key)) {
                 valueArray[i] = value;
                 storageSize++;
-                break;
+                return;
             }
         }
+        keyArray[storageSize] = key;
+        valueArray[storageSize] = value;
+        storageSize++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keyArray.length; i++) {
+        for (int i = 0; i < storageSize; i++) {
             if ((key == null && keyArray[i] == null)
                     || (keyArray[i] != null && keyArray[i].equals(key))) {
                 return (V) valueArray[i];
