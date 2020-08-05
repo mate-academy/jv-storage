f623cdabb11cb217bcc43ec48b2a409235877119
Fedia <fedia.rudiachenko@gmail.com>
2020-08-05 16:01:10 +0300

    added solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,7 +3,7 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    static final int MAX_SIZE = 10;
+    private static final int MAX_SIZE = 10;
     private K[] keyArray;
     private V[] valueArray;
     private int size;
