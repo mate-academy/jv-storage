638422bb32540ede95fabc23f0e64696667ba0dc
aleksandr.hrankin <aleksandr.hrankin@gmail.com>
2020-08-05 13:14:34 +0300

    rafactoring

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -14,29 +14,28 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        int keyHashCode = key == null ? 0 : key.hashCode();
+        Pair<K, V> pair = Pair.of(key, value);
         for (int i = 0; i < index; i++) {
-            int pairKeyHashCode = pairs[i].getKey() == null ? 0 : pairs[i].getKey().hashCode();
-            if (keyHashCode == pairKeyHashCode) {
-                pairs[i].setValue(value);
+            if (comparisonKeys(key, pairs[i].getKey())) {
+                pairs[i] = pair;
                 return;
             }
         }
-        Pair<K, V> pair = Pair.of(key, value);
-        pairs[index] = pair;
-        index += 1;
+        pairs[index++] = pair;
     }
 
     @Override
     public V get(K key) {
-        int keyHashCode = key == null ? 0 : key.hashCode();
         for (int i = 0; i < index; i++) {
-            int pairKeyHashCode = pairs[i].getKey() == null ? 0 : pairs[i].getKey().hashCode();
-            if (keyHashCode == pairKeyHashCode) {
+            if (comparisonKeys(key, pairs[i].getKey())) {
                 return pairs[i].getValue();
             }
         }
         return null;
+    }
+
+    private boolean comparisonKeys(K firstKey, K secondKey) {
+        return firstKey != null ? firstKey.equals(secondKey) : secondKey == null;
     }
 
     private static class Pair<K, V> {
@@ -56,35 +55,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             return value;
         }
 
-        public void setValue(V value) {
-            this.value = value;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            Pair<?, ?> pair = (Pair<?, ?>) o;
-            return (key != null ? key.equals(pair.key) : pair.key == null)
-                    && (value != null ? value.equals(pair.value) : pair.value == null);
-        }
-
-        @Override
-        public int hashCode() {
-            int result = 17;
-            result = 31 * result + (key != null ? key.hashCode() : 0);
-            result = 31 * result + (value != null ? value.hashCode() : 0);
-            return result;
-        }
-
         public static <K, V> Pair of(K key, V value) {
             return new Pair(key, value);
         }
     }
 }
-
-
