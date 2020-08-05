ed3f1fc6f58c9eea82fc7994c4f98927753581a3
YaTut1901 <59802427+YaTut1901@users.noreply.github.com>
2020-08-05 13:03:00 +0300

    hw

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,12 +4,40 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
+    private Object[] keys = new Object[10];
+    private Object[] values = new Object[10];
+    private int count = 0;
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < count; i++) {
+            if (check(key, keys[i])) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[count] = key;
+        values[count] = value;
+        count++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < count; i++) {
+            if (check(key, keys[i])) {
+                return (V) values[i];
+            }
+        }
         return null;
+    }
+
+    private boolean check(Object newKey, Object keyFromArray) {
+        if (newKey == null && newKey == keyFromArray) {
+            return true;
+        }
+        if (newKey != null && newKey.equals(keyFromArray)) {
+            return true;
+        }
+        return false;
     }
 }
