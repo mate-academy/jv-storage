e8b1bf81f7339d7ed95f9836cfbfe649138c6dc3
bogdan <b.kostevich@gmail.com>
2020-12-06 01:26:40 +0200

    Put and get value in a storage.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,13 +3,39 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int STORAGE_CAPACITY = 10;
+    private K[] key;
+    private V[] value;
+
+    public StorageImpl() {
+        this.key = (K[]) new Object[STORAGE_CAPACITY];
+        this.value = (V[]) new Object[STORAGE_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < STORAGE_CAPACITY; i++) {
+            if (this.key[i] == null && this.value[i] == null) {
+                this.key[i] = key;
+                this.value[i] = value;
+                break;
+            }
+            if (this.key[i] == key) {
+                this.value[i] = value;
+                break;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < STORAGE_CAPACITY; i++) {
+            if (key == null && this.key[i] == null) {
+                return this.value[i];
+            } else if (key != null && key.equals(this.key[i])) {
+                return this.value[i];
+            }
+        }
         return null;
     }
 }
