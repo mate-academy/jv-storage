dc85e4468cb959206d123e6c09fb4dea0e17ad42
antoniukivan <ivan.antonuk1992@gmail.com>
2020-12-06 06:28:36 +0200

    implement the Storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,15 +1,25 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final Object[][] STORAGE = new Object[2][10];
+    private int index = 0;
 
-    @Override
     public void put(K key, V value) {
+        STORAGE[0][index] = key;
+        STORAGE[1][index++] = value;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        V value = null;
+        for (int i = 0; i < index; i++) {
+            if (Objects.equals(STORAGE[0][i], key)) {
+                value = (V) STORAGE[1][i];
+            }
+        }
+        return value;
     }
 }
