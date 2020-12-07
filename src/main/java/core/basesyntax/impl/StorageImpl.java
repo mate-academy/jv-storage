327b0446e9a4e6f351f6e3b7abf76e398fc95edd
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-07 16:35:25 +0200

    methods put, get, isEmpty, getNextEmptyIndex, isExist implemented

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,12 +4,65 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
+    public static final int SIZE = 10;
+    private V[] storageValues;
+    private K[] storageKeys;
+
+    public StorageImpl() {
+        storageKeys = (K[]) new Object[SIZE];
+        storageValues = (V[]) new Object[SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        int isExist = isExist(key);
+        int index = isExist == -1 ? isExist : getNextEmptyIndex();
+        storageKeys[index] = key;
+        storageValues[index] = value;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < SIZE; i++) {
+            if (storageKeys[i].equals(key)) {
+                return storageValues[i];
+            }
+        }
         return null;
+    }
+
+    public boolean isEmpty() {
+        if (storageValues == null) {
+            return true;
+        }
+        for (int i = 0; i < SIZE; i++) {
+            if (storageValues[i] != null || storageKeys[i] != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /*
+    Method returns first not empty index in the warehouse
+    Empty if key and value are null
+    If warehouse is full returns -1
+     */
+    public int getNextEmptyIndex() {
+        for (int i = 0; i < SIZE; i++) {
+            if (storageValues[i] == null && storageKeys == null) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int isExist(K key) {
+        for (int i = 0; i < SIZE; i++) {
+            if (storageKeys[i].equals(key)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
