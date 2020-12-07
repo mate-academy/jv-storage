0d8223ccf6691d6785ea8dcc50b5b67a06a0719c
antoniukivan <ivan.antonuk1992@gmail.com>
2020-12-07 20:46:09 +0200

    add constants

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,19 +5,21 @@ import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final Object[][] STORAGE = new Object[2][10];
+    private static final int KEY = 0;
+    private static final int VALUE = 1;
     private int index = 0;
 
     public void put(K key, V value) {
-        STORAGE[0][index] = key;
-        STORAGE[1][index++] = value;
+        STORAGE[KEY][index] = key;
+        STORAGE[VALUE][index++] = value;
     }
 
     @Override
     public V get(K key) {
         V value = null;
         for (int i = 0; i < index; i++) {
-            if (Objects.equals(STORAGE[0][i], key)) {
-                value = (V) STORAGE[1][i];
+            if (Objects.equals(STORAGE[KEY][i], key)) {
+                value = (V) STORAGE[VALUE][i];
             }
         }
         return value;
