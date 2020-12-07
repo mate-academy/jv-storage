8ea8b332c01e9a5a1e3842fc5f5007a048542ae2
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-07 19:33:04 +0200

    removed all methods except get and put

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,67 +2,41 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.Arrays;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
 
     public static final int SIZE = 10;
-    private V[] storageValues;
-    private K[] storageKeys;
+    private V[] values;
+    private K[] keys;
 
     public StorageImpl() {
-        storageKeys = (K[]) new Object[SIZE];
-        storageValues = (V[]) new Object[SIZE];
+        keys = (K[]) new Object[SIZE];
+        values = (V[]) new Object[SIZE];
     }
 
     @Override
     public void put(K key, V value) {
-        int isExist = isExist(key);
-        int index = isExist == -1 ? getNextEmptyIndex() : isExist;
-        storageKeys[index] = key;
-        storageValues[index] = value;
+        for (int i = 0; i < SIZE; i++) {
+            if (keys[i] == null && values[i] == null
+                    || key.equals(keys[i])) {
+                keys[i] = key;
+                values[i] = value;
+                break;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < SIZE; i++) {
-            if (storageKeys[i].equals(key)) {
-                return storageValues[i];
+            if (key == null && keys[i] == null) {
+                return values[i];
+            }
+            if (key.equals(keys[i])) {
+                return values[i];
             }
         }
         return null;
-    }
-
-    public boolean isEmpty() {
-        for (int i = 0; i < SIZE; i++) {
-            if (storageValues[i] != null || storageKeys[i] != null) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /*
-    Method returns first not empty index in the warehouse
-    Empty if key and value are null
-    If warehouse is full returns -1
-     */
-    public int getNextEmptyIndex() {
-        for (int i = 0; i < SIZE; i++) {
-            if (storageKeys[i] == null) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    public int isExist(K key) {
-        for (int i = 0; i < SIZE; i++) {
-            if (storageValues[i] == null && storageKeys[i] == null) {
-                continue;
-            }
-            if (storageKeys[i].equals(key)) {
-                return i;
-            }
-        }
-        return -1;
     }
 }
