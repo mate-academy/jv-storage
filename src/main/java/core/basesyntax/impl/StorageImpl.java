21243df45cc5e5b03e305d61cf1debe65341c725
antonpanchishko <PanchishkoMayor@gmail.com>
2020-12-08 20:15:41 +0200

    1) solved all tasks and pass tests

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,13 +3,40 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int ARRAY_LENGTH = 10;
+    private Object[] keys = new Object[ARRAY_LENGTH];
+    private Object[] values = new Object[ARRAY_LENGTH];
+    private int index = 0;
 
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i <= index; i++) {
+            if (keys[i] == null && values[i] != null) {
+                continue;
+            }
+            if ((keys[i] == null && values[i] == null)
+                    || (keys[i].equals(key) && values[i] != null)) {
+                keys[i] = key;
+                values[i] = value;
+                break;
+            }
+        }
+        index++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i <= index; i++) {
+            if (key == null && keys[i] == null) {
+                return (V)values[i];
+            }
+            if (keys[i] == null) {
+                continue;
+            }
+            if (keys[i].equals(key)) {
+                return (V)values[i];
+            }
+        }
         return null;
     }
 }
