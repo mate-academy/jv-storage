ae18db89d6a836bed4176f5eb2dc5abc1ac5a18b
Ivan Marchenko <51205960+IwalkInSilence@users.noreply.github.com>
2020-12-08 20:35:11 +0200

    implement class StorageImpl, add get and put methods when key.equalse(keys[i]) was NullPointerExeption so I compare it's key == keys[i]

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,13 +3,33 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int ARRAY_LENGTH = 10;
+    private K[] keys = (K[]) new Object[ARRAY_LENGTH];
+    private V[] values = (V[]) new Object[ARRAY_LENGTH];
 
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < keys.length; i++) {
+            if ((key == keys[i] && key != null)) {
+                values[i] = value;
+                break;
+            } else if (keys[i] == null && values[i] == null) {
+                keys[i] = key;
+                values[i] = value;
+                break;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if ((key != null && key.equals(keys[i])) || key == keys[i]) {
+                return values[i];
+            } else if (key == null && keys[i] == null) {
+                return values[i];
+            }
+        }
         return null;
     }
 }
