921f061de99721a85d9f4da9d94a23dbbd025872
Roman Saviak <saviak.roman@gmail.com>
2020-12-08 11:32:24 +0200

    Task to implement put and get from Storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,13 +3,36 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int STORAGE_LENGTH = 10;
+
+    private K [] keyArray = (K[]) new Object[STORAGE_LENGTH];
+    private V [] valueArray = (V[]) new Object[STORAGE_LENGTH];
 
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < STORAGE_LENGTH; i++) {
+            if (keyArray[i] == null && valueArray[i] == null) {
+                keyArray [i] = key;
+                valueArray[i] = value;
+                break;
+            }
+            if (keyArray[i] == key) {
+                valueArray[i] = value;
+                break;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < STORAGE_LENGTH; i++) {
+            if (keyArray[i] == null && key == null) {
+                return valueArray[i];
+            }
+            if (key != null && key.equals(keyArray[i])) {
+                return valueArray[i];
+            }
+        }
         return null;
     }
 }
