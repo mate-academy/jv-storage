1bd17445f5c15e6fd5da2188fb59181b2171ee7d
Serhii Semenykhin <semyonich@users.noreply.github.com>
2020-12-08 16:49:42 +0200

    StorageImpl class implemented: with hashfunction

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,13 +3,22 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ARRAY_LENGTH = 10;
+    V[] valuesArray = (V[]) new Object[MAX_ARRAY_LENGTH];
+    V nullKeyElement;
 
     @Override
     public void put(K key, V value) {
+        if (key == null) {
+            nullKeyElement = value;
+        } else {
+            valuesArray[Math.abs(key.hashCode() % MAX_ARRAY_LENGTH)] = value;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return (key != null) ? valuesArray[Math.abs(key.hashCode() % MAX_ARRAY_LENGTH)]
+                : nullKeyElement;
     }
 }
