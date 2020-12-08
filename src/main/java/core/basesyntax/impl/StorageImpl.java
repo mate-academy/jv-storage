1e8016f82ea13ea2a744ccbf4c1923a6f3bedfec
Vitalik Kopach <apple_air@ukr.net>
2020-12-08 16:02:40 +0200

    implemented method put() and get(K key) from interface Storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,13 +3,39 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    static final int DEFAULT_CAPACITY = 10;
+    private V[] values = (V[]) new Object[DEFAULT_CAPACITY];
+    private K[] keys = (K[]) new Object[DEFAULT_CAPACITY];
 
     @Override
     public void put(K key, V value) {
+        for(int i = 0; i < DEFAULT_CAPACITY; i++) {
+
+            if(keys[i] == null && values[i] == null) {
+                keys[i] = key;
+                values[i] = value;
+                break;
+            }
+
+            if(keys[i] == key) {
+                values[i] = value;
+                break;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < DEFAULT_CAPACITY; i++) {
+
+            if (keys[i] != null && keys[i].equals(key)) {
+                return values[i];
+            }
+
+            if(keys[i] == null && key == null){
+                return values[i];
+            }
+        }
         return null;
     }
 }
