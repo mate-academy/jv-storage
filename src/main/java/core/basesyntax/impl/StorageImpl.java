a92719f3d9f87910afcddbbbdb5a072ccbea45c0
sanek98cv <73754789+sanek98cv@users.noreply.github.com>
2020-12-09 13:28:05 +0200

    Fixed: added constructor and if()

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,19 +4,27 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int ARRAY_LENGTH = 10;
-    private final K[] keyArray = (K[]) new Object[ARRAY_LENGTH];
-    private final V[] valueArray = (V[]) new Object[ARRAY_LENGTH];
-    private int index = 0;
+    private final K[] keyArray;
+    private final V[] valueArray;
+    private int index;
+
+    public StorageImpl() {
+        this.keyArray = (K[]) new Object[ARRAY_LENGTH];
+        this.valueArray = (V[]) new Object[ARRAY_LENGTH];
+        this.index = 0;
+    }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < ARRAY_LENGTH; i++) {
+        for (int i = 0; i < index; i++) {
             if (keyArray[i] != null && keyArray[i].equals(key) || keyArray[i] == key) {
                 valueArray[i] = value;
             }
         }
-        keyArray[index] = key;
-        valueArray[index++] = value;
+        if (keyArray[index] == null && valueArray[index] == null) {
+            keyArray[index] = key;
+            valueArray[index++] = value;
+        }
     }
 
     @Override
