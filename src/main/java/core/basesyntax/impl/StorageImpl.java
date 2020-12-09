bab0c8e91e4c4c13335b55b2d004c9062a14403e
KostyantynProskura <kostyantyn.proskura@gmail.com>
2020-12-09 16:34:09 +0200

    1) Commit

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,12 +4,41 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
+    private static final int ARRAY_LENGTH = 10;
+    private final K[] keys;
+    private final V[] values;
+    private int indexCurrent;
+
+    public StorageImpl() {
+        this.keys = (K[]) new Object[ARRAY_LENGTH];
+        this.values = (V[]) new Object[ARRAY_LENGTH];
+        this.indexCurrent = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < ARRAY_LENGTH; i++) {
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
+                values[i] = value;
+            }
+        }
+        keys[indexCurrent] = key;
+        values[indexCurrent] = value;
+        indexCurrent++;
     }
 
     @Override
     public V get(K key) {
+
+        for (int i = 0; i < ARRAY_LENGTH; i++) {
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
+                return values[i];
+            } else if (key == null && keys[i] == null) {
+                return values[i];
+            }
+
+        }
         return null;
     }
 }
+
