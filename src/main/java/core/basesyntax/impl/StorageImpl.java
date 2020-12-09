48cf98fe1166a28bbec9b7209e3d3a1ca33281fe
Elena Samsonova <elenasamsonova08@gmail.com>
2020-12-09 13:17:39 +0200

    added the possibility to put value by null key, combined 2 if checks in one

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,7 +6,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ARR_SIZE = 10;
     private Object[] keys;
     private Object[] values;
-    private int addedElements = 0;
+    private int size = 0;
 
     public StorageImpl() {
         keys = new Object[MAX_ARR_SIZE];
@@ -17,30 +17,28 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public void put(K key, V value) {
         int sameKeyIndex = -1;
         for (int i = 0; i < MAX_ARR_SIZE; i++) {
-            if (key != null && values[i] != null && key.equals((K) keys[i])) {
+            if (values[i] != null && (key != null && key.equals((K) keys[i])
+                    || key == null && keys[i] == null)) {
                 sameKeyIndex = i;
             }
         }
         if (sameKeyIndex == -1) {
-            keys[addedElements] = key;
-            values[addedElements] = value;
-            addedElements++;
+            keys[size] = key;
+            values[size] = value;
+            size++;
         } else {
             values[sameKeyIndex] = value;
         }
-
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < MAX_ARR_SIZE; i++) {
-            if (key != null && key.equals((K) keys[i])) {
-                return (V) values[i];
-            }
-            if (key == null && keys[i] == null) {
+            if (key != null && key.equals((K) keys[i]) || key == null && keys[i] == null) {
                 return (V) values[i];
             }
         }
         return null;
     }
+
 }
