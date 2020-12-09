103da7469fccd9b739b5da1802951d7ed9ed51b5
Sashk52 <a.tsven@gmail.com>
2020-12-09 12:09:15 +0200

    made solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,12 +4,38 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
-    @Override
+    public static final int MAX_ARRAY_SIZE = 10;
+    public K[] keyArray = (K[]) new Object[MAX_ARRAY_SIZE];
+    public V[] valueArray = (V[]) new Object[MAX_ARRAY_SIZE];
+    int iterator = 0;
+
     public void put(K key, V value) {
+        for (int i = 0; i <= iterator; i++) {
+            if (keyArray[i] == null && key == null) {
+                valueArray[i] = (V) value;
+            } else if (key != null && keyArray[i] != null && keyArray[i].equals(key)) {
+                valueArray[i] = (V) value;
+                keyArray[i] = (K) key;
+            } else if (keyArray[i] == null && valueArray[i] == null && key != null) {
+                valueArray[i] = (V) value;
+                keyArray[i] = (K) key;
+            }
+        }
+        iterator++;
     }
 
-    @Override
     public V get(K key) {
-        return null;
+        V resultValue = valueArray[0];
+        for (int i = 0; i <= iterator; i++) {
+            if (key == null && keyArray[i] == null) {
+                return valueArray[i];
+            } else if (key != null && keyArray[i] != null && keyArray[i].equals(key)) {
+                return valueArray[i];
+            }
+        }
+        return resultValue;
     }
 }
+
+
+
