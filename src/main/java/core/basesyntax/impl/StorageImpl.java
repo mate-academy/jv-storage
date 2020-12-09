ee668c35c892e14fbc8ac742bf63c4b93fe14ef9
Igor Bochkor <igorbochkor@gmail.com>
2020-12-09 07:30:53 +0200

    added solution storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,13 +3,44 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private Object[] keys;
+    private Object[] boxes;
+    private int cellNumber;
+
+    public StorageImpl() {
+        keys = new Object[MAX_ITEMS_NUMBER];
+        boxes = new Object[MAX_ITEMS_NUMBER];
+        cellNumber = 0;
+    }
 
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < cellNumber; i++) {
+            if (key != null && key.equals(keys[i])) {
+                keys[i] = key;
+                boxes[i] = value;
+            }
+        }
+        keys[cellNumber] = key;
+        boxes[cellNumber] = value;
+        cellNumber++;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        V box = null;
+        for (int i = 0; i < cellNumber; i++) {
+            if (key == null && keys[i] == null) {
+                box = (V) boxes[i];
+            }
+            if (keys[i] == null) {
+                continue;
+            }
+            if (keys[i].equals(key)) {
+                box = (V) boxes[i];
+            }
+        }
+        return box;
     }
 }
