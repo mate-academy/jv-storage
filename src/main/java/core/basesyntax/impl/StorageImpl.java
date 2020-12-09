3c878d73ce67fb4defafd434c3265b217640801c
Sashk52 <a.tsven@gmail.com>
2020-12-09 17:44:27 +0200

    recover clearline in Storage interface & deleted clearline in the end & removed iterator as not necesary element

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -7,24 +7,24 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public static final int MAX_ARRAY_SIZE = 10;
     public K[] keyArray = (K[]) new Object[MAX_ARRAY_SIZE];
     public V[] valueArray = (V[]) new Object[MAX_ARRAY_SIZE];
-    private int iterator = 0;
 
     public void put(K key, V value) {
-        for (int i = 0; i <= iterator; i++) {
+        for (int i = 0; i < MAX_ARRAY_SIZE; i++) {
             if (keyArray[i] == key || key != null
                     && keyArray[i] != null && keyArray[i].equals(key)) {
                 valueArray[i] = (V) value;
                 keyArray[i] = (K) key;
+                return;
             } else if (keyArray[i] == valueArray[i] && key != null) {
                 valueArray[i] = (V) value;
                 keyArray[i] = (K) key;
+                return;
             }
         }
-        iterator++;
     }
 
     public V get(K key) {
-        for (int i = 0; i <= iterator; i++) {
+        for (int i = 0; i < MAX_ARRAY_SIZE; i++) {
             if (key == keyArray[i] || key != null
                     && keyArray[i] != null && keyArray[i].equals(key)) {
                 return valueArray[i];
@@ -33,5 +33,4 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         return null;
     }
 }
-
 
