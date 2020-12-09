eaf34648f066e60d001a563ce712a508da139df5
ShemsheyMaxim <shanhaymax@gmail.com>
2020-12-09 02:45:32 +0200

    add correct realised methods put and get for storageImpl

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,13 +3,42 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int STORAGE_SIZE = 10;
+    private K[] keys;
+    private V[] values;
+    private int length;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[STORAGE_SIZE];
+        values = (V[]) new Object[STORAGE_SIZE];
+    }
 
     @Override
     public void put(K key, V value) {
+        if (length == 0) {
+            keys[length] = key;
+            values[length] = value;
+            length++;
+            return;
+        }
+        for (int i = 0; i < length; i++) {
+            if (keys[i] != null && keys[i].equals(key)) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[length] = key;
+        values[length] = value;
+        length++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] == key || (keys[i] != null && keys[i].equals(key))) {
+                return values[i];
+            }
+        }
         return null;
     }
 }
