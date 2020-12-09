946461f530fd1cbc3f68a03e466ddc8e80cb8cf9
Tonitrus <grom4er@gmail.com>
2020-12-09 12:28:02 +0200

    Fix: Create constructor with initialization field. Fix logic put method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -7,12 +7,16 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int ELEMENT_SIZE = 2;
     private static final int KEY = 0;
     private static final int VALUE = 1;
-    private final Object[][] array = new Object[STORAGE_CAPACITY][ELEMENT_SIZE];
+    private final Object[][] array;
+
+    public StorageImpl(){
+        array = new Object[STORAGE_CAPACITY][ELEMENT_SIZE];
+    }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < STORAGE_CAPACITY; i++) {
-            if (array[i][VALUE] == null) {
+            if (array[i][VALUE] == null && array[i][KEY] == null) {
                 array[i][KEY] = key;
                 array[i][VALUE] = value;
                 break;
