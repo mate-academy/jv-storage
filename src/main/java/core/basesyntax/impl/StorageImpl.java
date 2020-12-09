d92db1163f2f32b4daec153213c43572e1dc827d
Vitalik Kopach <apple_air@ukr.net>
2020-12-09 14:57:03 +0200

    added new var 'index, indexExistKey' and changed methods put() and get()

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,35 +3,35 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    static final int DEFAULT_CAPACITY = 10;
-    private V[] values = (V[]) new Object[DEFAULT_CAPACITY];
-    private K[] keys = (K[]) new Object[DEFAULT_CAPACITY];
+    private static final int DEFAULT_CAPACITY = 10;
+    private V[] values;
+    private K[] keys;
+    private int index;
+    private int indexExistKey;
+
+    public StorageImpl() {
+        values = (V[]) new Object[DEFAULT_CAPACITY];
+        keys = (K[]) new Object[DEFAULT_CAPACITY];
+        index = 0;
+        indexExistKey = 0;
+    }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < DEFAULT_CAPACITY; i++) {
-
-            if (keys[i] == null && values[i] == null) {
-                keys[i] = key;
-                values[i] = value;
-                return;
-            }
-            if (key == keys[i] || key != null && key.equals(keys[i])) {
-                values[i] = value;
-                return;
-            }
+        if (get(key) != null) {
+            values[indexExistKey] = value;
+            return;
+        } else {
+            values[index] = value;
+            keys[index++] = key;
         }
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < DEFAULT_CAPACITY; i++) {
-
-            if (keys[i] != null && keys[i].equals(key)) {
-                return values[i];
-            }
-
-            if (keys[i] == null && key == null) {
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
+                indexExistKey = i;
                 return values[i];
             }
         }
