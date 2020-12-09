1d378d0a821045b030c558e521b3010838beb30e
Alex <koleksandr4@gmail.com>
2020-12-09 12:11:49 +0200

    added requested changes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,48 +4,49 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAXIMUM_NUMBER = 10;
-    private final K[] key;
-    private final V[] value;
-    private int index = 0;
+    private final K[] keys;
+    private final V[] values;
+    private int index;
 
     public StorageImpl() {
-        this.key = (K[]) new Object[MAXIMUM_NUMBER];
-        this.value = (V[]) new Object[MAXIMUM_NUMBER];
+        keys = (K[]) new Object[MAXIMUM_NUMBER];
+        values = (V[]) new Object[MAXIMUM_NUMBER];
+        index = 0;
     }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < index; i++) {
-            if (isKeyEquals(key, this.key[i])) {
-                this.value[i] = value;
+            if (isKeyEquals(key, keys[i])) {
+                values[i] = value;
                 return;
             }
         }
-        this.key[index] = key;
-        this.value[index] = value;
+        keys[index] = key;
+        values[index] = value;
         index++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < index; i++) {
-            if (isKeyEquals(key, this.key[i])) {
-                return this.value[i];
+            if (isKeyEquals(key, keys[i])) {
+                return values[i];
             }
         }
         return null;
     }
 
-    public K[] getKey() {
-        return key;
+    public K[] getKeys() {
+        return keys;
     }
 
-    public V[] getValue() {
-        return value;
+    public V[] getValues() {
+        return values;
     }
 
     private boolean isKeyEquals(K key, K key2) {
-        return (key == null && key2 == null)
+        return (key == key2)
                 || (key != null && key.equals(key2));
     }
 }
