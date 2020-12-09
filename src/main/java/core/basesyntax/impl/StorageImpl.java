26d64c2fddd6bf752aacfee054c6a385f751b48f
engineer027 <dp270597dma@gmail.com>
2020-12-09 14:29:48 +0200

    public void put fiz, and rename: keyObject, valueObject;

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,33 +5,41 @@ import core.basesyntax.Storage;
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int LENGTH_ARRAY = 10;
     private int counter;
-    private Object[] key;
-    private Object[] value;
+    private Object[] keyObject;
+    private Object[] valueObject;
 
     public StorageImpl() {
         this.counter = 0;
-        this.key = new Object[LENGTH_ARRAY];
-        this.value = new Object[LENGTH_ARRAY];
+        this.keyObject = new Object[LENGTH_ARRAY];
+        this.valueObject = new Object[LENGTH_ARRAY];
     }
 
     @Override
     public void put(K key, V value) {
+        if (counter == 0) {
+            this.keyObject[counter] = key;
+            this.valueObject[counter] = value;
+            counter++;
+        }
         for (int i = 0; i < counter; i++) {
-            if (this.key[i] == key || key != null && key.equals(this.key[i])) {
-                this.value[i] = value;
+            if (this.keyObject[i] == key || key != null && key.equals(this.keyObject[i])) {
+                this.valueObject[i] = value;
                 break;
             }
+            if ((i == counter - 1) && !(this.keyObject[i] == key || key != null
+                    && key.equals(this.keyObject[i]))) {
+                this.keyObject[counter] = key;
+                this.valueObject[counter] = value;
+                counter++;
+            }
         }
-        this.key[counter] = key;
-        this.value[counter] = value;
-        counter++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < counter; i++) {
-            if (this.key[i] == key || key != null && key.equals(this.key[i])) {
-                return (V) this.value[i];
+            if (this.keyObject[i] == key || key != null && key.equals(this.keyObject[i])) {
+                return (V) this.valueObject[i];
             }
         }
         return null;
