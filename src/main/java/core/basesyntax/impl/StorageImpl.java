8c329a706c2fc680808553379ef50d73cfa71937
YuraBondarenko <yuriybondarenko2001@gmail.com>
2020-12-09 08:55:43 +0200

    rename array of "key" and "value" to "keys" and "values" and remove "this." in places where it doesn't necessary replaced "==" to "equals", created a constructor in that assigned values for array "key" and "value" implemented the Storage of key-value type

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,23 +4,23 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static int MAX_NUMBER_OF_ELEMENTS_IN_STORAGE = 10;
-    private K[] key;
-    private V[] value;
+    private K[] keys;
+    private V[] values;
 
     public StorageImpl() {
-        key = (K[]) new Object[MAX_NUMBER_OF_ELEMENTS_IN_STORAGE];
-        value = (V[]) new Object[MAX_NUMBER_OF_ELEMENTS_IN_STORAGE];
+        keys = (K[]) new Object[MAX_NUMBER_OF_ELEMENTS_IN_STORAGE];
+        values = (V[]) new Object[MAX_NUMBER_OF_ELEMENTS_IN_STORAGE];
     }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < MAX_NUMBER_OF_ELEMENTS_IN_STORAGE; i++) {
-            if (this.key[i] == null && this.value[i] == null) {
-                this.key[i] = key;
-                this.value[i] = value;
+            if (keys[i] == null && values[i] == null) {
+                keys[i] = key;
+                values[i] = value;
                 break;
-            } else if (key != null && key.equals(this.key[i])) {
-                this.value[i] = value;
+            } else if (key != null && key.equals(keys[i])) {
+                values[i] = value;
                 break;
             }
         }
@@ -29,8 +29,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < MAX_NUMBER_OF_ELEMENTS_IN_STORAGE; i++) {
-            if ((key != null && key.equals(this.key[i])) || (key == null && this.key[i] == null)) {
-                return this.value[i];
+            if ((key != null && key.equals(keys[i])) || (key == null && keys[i] == null)) {
+                return values[i];
             }
         }
         return null;
