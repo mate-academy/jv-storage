8ff449e898e41ddaf34adbcbc6038696ef2006f4
engineer027 <dp270597dma@gmail.com>
2020-12-09 13:21:46 +0200

    initialise all fields in the constructor

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,29 +4,32 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int LENGTH_ARRAY = 10;
-    private int counter = 0;
-    private Object[] key = new Object[LENGTH_ARRAY];
-    private Object[] value = new Object[LENGTH_ARRAY];
+    private int counter;
+    private Object[] key;
+    private Object[] value;
+
+    public StorageImpl() {
+        this.counter = 0;
+        this.key = new Object[LENGTH_ARRAY];
+        this.value = new Object[LENGTH_ARRAY];
+    }
 
     @Override
     public void put(K key, V value) {
-        boolean check = false;
-        for (int i = 0; i < LENGTH_ARRAY; i++) {
+        for (int i = 0; i < counter; i++) {
             if (this.key[i] == key || key != null && key.equals(this.key[i])) {
                 this.value[i] = value;
-                check = true;
+                break;
             }
         }
-        if (!check) {
-            this.key[counter] = key;
-            this.value[counter] = value;
-            counter++;
-        }
+        this.key[counter] = key;
+        this.value[counter] = value;
+        counter++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < LENGTH_ARRAY; i++) {
+        for (int i = 0; i < counter; i++) {
             if (this.key[i] == key || key != null && key.equals(this.key[i])) {
                 return (V) this.value[i];
             }
