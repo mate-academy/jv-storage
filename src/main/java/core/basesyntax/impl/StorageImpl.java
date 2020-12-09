22ab26f976dea0894211130cf6accaf80bd6256c
OleksandrPokotylenko <aln.nko.of@gmail.com>
2020-12-09 13:00:35 +0200

    removed arrays' initialization from fields to constructor; rewrote comparison in put() method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,17 +4,19 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_LENGTH = 10;
-    private K[] keys = (K[]) new Object[MAX_LENGTH];
-    private V[] values = (V[]) new Object[MAX_LENGTH];
+    private K[] keys;
+    private V[] values;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_LENGTH];
+        values = (V[]) new Object[MAX_LENGTH];
+    }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < MAX_LENGTH; i++) {
-            if (keys[i] == key) {
-                values[i] = value;
-                break;
-            }
-            if (keys[i] == null && values[i] == null) {
+            if ((keys[i] == key) || (keys[i] != null && keys[i].equals(key))
+            || (keys[i] == null && values[i] == null)) {
                 keys[i] = key;
                 values[i] = value;
                 break;
