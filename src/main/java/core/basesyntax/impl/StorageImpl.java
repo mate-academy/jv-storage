003120b91bc3f430c09fec1531205ee7f74e911c
RuslanIbIsh <ruslan8ishankulov@gmail.com>
2020-12-09 16:06:44 +0200

    done

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,21 +3,21 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int INITIAL_CAPACITY = 10;
+    private static final int INITIAL_CAPACITY_KEYS = 10;
+    private static final int INITIAL_CAPACITY_VALUES = 2;
     private static final int KEY_INDEX = 0;
     private static final int VALUE_INDEX = 1;
     private int incrementedIndex = 0;
     private Object[][] storage;
 
     public StorageImpl() {
-        storage = new Object[INITIAL_CAPACITY][INITIAL_CAPACITY];
+        storage = new Object[INITIAL_CAPACITY_KEYS][INITIAL_CAPACITY_VALUES];
     }
 
     @Override
     public void put(K key, V value) {
-        Object[] result = isContains(key);
-        if ((boolean) result[0] == true) {
-            storage[(int) result[1]][VALUE_INDEX] = value;
+        if (indexOf(key) != -1) {
+            storage[indexOf(key)][VALUE_INDEX] = value;
         } else {
             storage[incrementedIndex][KEY_INDEX] = key;
             storage[incrementedIndex][VALUE_INDEX] = value;
@@ -27,22 +27,19 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        Object[] result = isContains(key);
-        return (V) result[2];
+        if (indexOf(key) == -1) {
+            return null;
+        }
+        return (V) storage[indexOf(key)][VALUE_INDEX];
     }
 
-    private Object[] isContains(K key) {
-        Object[] result = new Object[3];
-        result[0] = false;
-        result[1] = 0;
-        result[2] = null;
+    private int indexOf(K key) {
+        int index = -1;
         for (int i = 0; i < storage.length; i++) {
             if (key != null ? key.equals(storage[i][KEY_INDEX]) : storage[i][KEY_INDEX] == key) {
-                result[0] = true;
-                result[1] = i;
-                result[2] = storage[i][VALUE_INDEX];
+                index = i;
             }
         }
-        return result;
+        return index;
     }
 }
