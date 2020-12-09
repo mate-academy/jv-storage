98b3d085177fd431c0dd7c228984dbad4095b253
Vitalik Kopach <apple_air@ukr.net>
2020-12-09 16:06:55 +0200

    added private method getIndex(K key)

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -7,20 +7,17 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private V[] values;
     private K[] keys;
     private int index;
-    private int indexExistKey;
 
     public StorageImpl() {
         values = (V[]) new Object[DEFAULT_CAPACITY];
         keys = (K[]) new Object[DEFAULT_CAPACITY];
         index = 0;
-        indexExistKey = 0;
     }
 
     @Override
     public void put(K key, V value) {
         if (get(key) != null) {
-            values[indexExistKey] = value;
-            return;
+            values[getIndex(key)] = value;
         } else {
             values[index] = value;
             keys[index++] = key;
@@ -31,10 +28,19 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public V get(K key) {
         for (int i = 0; i < keys.length; i++) {
             if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
-                indexExistKey = i;
                 return values[i];
             }
         }
         return null;
+    }
+
+    private int getIndex(K key1) {
+        int indexOfKey = 0;
+        for (int i = 0; i < keys.length; i++) {
+            if (key1.equals(keys[i])) {
+                return indexOfKey;
+            }
+        }
+        return -1;
     }
 }
