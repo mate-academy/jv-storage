8ea96e4d9a6a56f84a5b5eb8f99649d60b5c25df
egorka <entertainment.veks@gmail.com>
2020-12-09 13:09:23 +0200

    yegor sereda first commit I know that it's bad code. I'll join to the QnA for help.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,12 +4,52 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
+    private static final int MAX_STORAGE_SIZE = 10;
+
+    K[] keyArr = (K[]) new Object[MAX_STORAGE_SIZE];
+    V[] valueArr = (V[]) new Object[MAX_STORAGE_SIZE];
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < MAX_STORAGE_SIZE; i++) {
+            if (keyArr[i] == null) {
+                if (keyArr[i] == key) {
+                    valueArr[i] = value;
+                }
+            } else {
+                if (keyArr[i].equals(key)) {
+                    valueArr[i] = value;
+                }
+            }
+        }
+
+        int i = findLastFilled(keyArr);
+        keyArr[i] = key;
+        valueArr[i] = value;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < MAX_STORAGE_SIZE; i++) {
+            if (keyArr[i] == null) {
+                if (keyArr[i] == key) {
+                    return valueArr[i];
+                }
+            } else {
+                if (keyArr[i].equals(key)) {
+                    return valueArr[i];
+                }
+            }
+        }
         return null;
+    }
+
+    private int findLastFilled(K[] arr) {
+        for (int i = 0; i < MAX_STORAGE_SIZE; i++) {
+            if (arr[i] == null) {
+                return i;
+            }
+        }
+        throw new RuntimeException("Array is full");
     }
 }
