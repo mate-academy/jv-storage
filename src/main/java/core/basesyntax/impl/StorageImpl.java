c2a0eec043aa9c3141c9e215462c6d5fbc98d217
Maksim <max.freedman.main@gmail.com>
2021-04-01 16:57:54 +0300

    added solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,20 +1,46 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    public static final int SIZE = 10;
+
+    private Object[] keys;
+    private Object[] values;
+    private int count;
+
+    public StorageImpl() {
+        keys = new Object[SIZE];
+        values = new Object[SIZE];
+        count = 0;
+    }
 
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < count; i++) {
+            if (Objects.equals(key, keys[i])) {
+                values[i] = value;
+                return;
+            }
+        }
+        values[count] = value;
+        keys[count] = key;
+        count++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < count; i++) {
+            if (Objects.equals(key, keys[i])) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return count;
     }
 }
