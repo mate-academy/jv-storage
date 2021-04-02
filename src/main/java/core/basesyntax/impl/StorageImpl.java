9dcda55476e2d7bc0d97c462dd55700549e177a6
dvdovynets <vdovinec@gmail.com>
2021-04-02 18:13:02 +0300

    added solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,18 +3,38 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int ARRAY_SIZE = 10;
+    private Object[] storageArray;
+
+    public StorageImpl() {
+        storageArray = new Object[ARRAY_SIZE];
+    }
 
     @Override
     public void put(K key, V value) {
+        storageArray[getIndex(key)] = value;
+    }
+
+    public int getIndex(K key) {
+        if (key == null) {
+            return 0;
+        }
+        return Math.abs(key.hashCode() % ARRAY_SIZE);
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return (V) storageArray[getIndex(key)];
     }
 
     @Override
     public int size() {
-        return -1;
+        int count = 0;
+        for (Object element : storageArray) {
+            if (element != null) {
+                count++;
+            }
+        }
+        return count;
     }
 }
