8eb1ede772b3d36f9d6e0a9db49a6b99d8a74cc2
fotomassimo <maksym.pavliuchenko@gmail.com>
2021-04-05 18:17:03 +0300

    Task jv-storage, first commit

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,18 +3,54 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_STORAGE_SIZE = 10;
+    private final Box<K, V>[] storage;
+
+    public StorageImpl() {
+        storage = new Box[MAX_STORAGE_SIZE];
+    }
+
+    private boolean areBothKeysNullOrEqual(K firstKey, K secondKey) {
+        return (firstKey == null && secondKey == null)
+                || (secondKey != null && firstKey != null && secondKey.equals(firstKey));
+    }
 
     @Override
     public void put(K key, V value) {
+        Box<K, V> box = new Box<>(key, value);
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = box;
+                return;
+            }
+            if (areBothKeysNullOrEqual(storage[i].getKey(), key)) {
+                storage[i] = box;
+                return;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                return null;
+            }
+            if (areBothKeysNullOrEqual(storage[i].getKey(), key)) {
+                return storage[i].getValue();
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        int count = 0;
+        for (Box<K, V> boxInStorage : storage) {
+            if (boxInStorage != null) {
+                count++;
+            }
+        }
+        return count;
     }
 }
