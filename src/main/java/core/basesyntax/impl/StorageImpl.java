e248e2e1fb2a4df9120bd1fbea25fa932657a1f2
IhorRome <ihor.rome@gmail.com>
2021-04-06 18:35:10 +0300

    fixed

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,54 +4,52 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int ARRAY_LENGTH = 10;
-    private final K[] keyArray = (K[]) new Object[ARRAY_LENGTH];
-    private final V[] valueArray = (V[]) new Object[ARRAY_LENGTH];
-    private int index = 0;
+    private final K[] keys;
+    private final V[] values;
+    private int size;
+
+    public StorageImpl() {
+        this.keys = (K[]) new Object[ARRAY_LENGTH];
+        this.values = (V[]) new Object[ARRAY_LENGTH];
+        this.size = 0;
+    }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < ARRAY_LENGTH; i++) {
+        for (int i = 0; i < size + 1; i++) {
             if (key == null) {
-                if (keyArray[i] == null && valueArray[i] != null) {
-                    index--;
+                if (keys[i] == null && values[i] != null) {
+                    size--;
                 }
                 break;
             }
-            if (equals(key, keyArray[i])) {
-                index--;
+            if (key.equals(keys[i])) {
+                size--;
             }
         }
-        keyArray[index] = key;
-        valueArray[index] = value;
-        index++;
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        int indexOfValue = 0;
-        for (int i = 0; i < ARRAY_LENGTH; i++) {
-            if (equals(keyArray[i], key)) {
-                indexOfValue = i;
+        for (int i = 0; i < size; i++) {
+            if (key == null) {
+                if (keys[i] == null) {
+                    return values[i];
+                }
+                continue;
             }
-            if (valueArray[i + 1] == null) {
-                break;
+            if (key.equals(keys[i])) {
+                return values[i];
             }
         }
-        return valueArray[indexOfValue];
+        return values[0];
     }
 
     @Override
     public int size() {
-        return index;
-    }
-
-    public boolean equals(K firstKey, K secondKey) {
-        if (firstKey == secondKey) {
-            return true;
-        }
-        if (firstKey == null || secondKey == null) {
-            return false;
-        }
-        return firstKey.equals(secondKey);
+        return size;
     }
 }
