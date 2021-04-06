9aface0c999637bf5881cd68b137ce20ffa02e4d
KseniaObertii <ksenia.obertii@users.noreply.github.com>
2021-04-06 13:44:55 +0300

    fixed mistakes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,34 +4,33 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int ARRAY_SIZE = 10;
-    private final Object[] arrayK;
-    private final Object[] arrayV;
+    private final Object[] keys;
+    private final Object[] values;
     private int counter;
 
     public StorageImpl() {
-        arrayK = new Object[ARRAY_SIZE];
-        arrayV = new Object[ARRAY_SIZE];
-        counter = 0;
+        keys = new Object[ARRAY_SIZE];
+        values = new Object[ARRAY_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < counter; i++) {
-            if (key != null && key.equals(arrayK[i]) || key == arrayK[i]) {
-                arrayV[i] = value;
+            if (key != null && key.equals(keys[i]) || key == keys[i]) {
+                values[i] = value;
                 return;
             }
         }
-        arrayK[counter] = key;
-        arrayV[counter] = value;
+        keys[counter] = key;
+        values[counter] = value;
         counter++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < counter; i++) {
-            if (key == null && arrayK[i] == null || key != null && key.equals(arrayK[i])) {
-                return (V) arrayV[i];
+            if (key == null && keys[i] == null || key != null && key.equals(keys[i])) {
+                return (V) values[i];
             }
         }
         return null;
