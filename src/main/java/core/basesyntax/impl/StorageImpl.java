649946555d20fc324e2e16d3508273ca7dd7e0ab
dvdovynets <vdovinec@gmail.com>
2021-04-06 16:45:23 +0300

    made solution with 2 arrays

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,34 +4,41 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int ARRAY_SIZE = 10;
+    private static final int NO_SUCH_INDEX = -1;
     private final Object[] storageArray;
+    private final Object[] indexArray;
     private int elementsCount;
 
     public StorageImpl() {
         storageArray = new Object[ARRAY_SIZE];
+        indexArray = new Object[ARRAY_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
-        if (storageArray[getIndex(key)] == null) {
-            storageArray[getIndex(key)] = value;
+        int index = getIndex(key);
+        if (index == NO_SUCH_INDEX) {
+            indexArray[elementsCount] = key;
+            storageArray[elementsCount] = value;
             elementsCount++;
         } else {
-            storageArray[getIndex(key)] = value;
+            indexArray[index] = key;
+            storageArray[index] = value;
         }
-
     }
 
     public int getIndex(K key) {
-        if (key == null) {
-            return 0;
+        for (int i = 0; i < elementsCount; i++) {
+            if (indexArray[i] != null && indexArray[i].equals(key) || indexArray[i] == key) {
+                return i;
+            }
         }
-        return Math.abs(key.hashCode() % ARRAY_SIZE);
+        return NO_SUCH_INDEX;
     }
 
     @Override
     public V get(K key) {
-        return (V) storageArray[getIndex(key)];
+        return getIndex(key) == NO_SUCH_INDEX ? null : (V) storageArray[getIndex(key)];
     }
 
     @Override
