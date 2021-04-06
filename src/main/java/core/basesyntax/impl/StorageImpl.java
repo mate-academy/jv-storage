000374a189545da4f8410579be0cdb9d3f388f0d
Vlad-Andr <andreev.v99@gmail.com>
2021-04-06 20:55:10 +0300

    changed method body and remove reducant variable

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,44 +1,48 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int MAX_RANGE = 10;
-    private static final int WRONG_RANGE = 11;
     private static final String NULL_EXPRESSION = "null";
-    private int size = 0;
-    private final String[] arrayOfKeys = new String[MAX_RANGE];
-    private final String[] arrayOfValues = new String[MAX_RANGE];
+    private static final int MAX_RANGE = 10;
+    private int size;
+    private final K[] arrayOfKeys = (K[]) new Object[MAX_RANGE];
+    private final V[] arrayOfValues = (V[]) new Object[MAX_RANGE];
+    private boolean nullWasCreatedOnce = false;
 
     @Override
     public void put(K key, V value) {
-        int numberOfSame = checkIfUniqueKey(key);
-
-        if (numberOfSame == WRONG_RANGE) {
-            if (key != null) {
-                arrayOfKeys[size] = key.toString();
-            } else {
-                arrayOfKeys[size] = NULL_EXPRESSION;
+        boolean isUnique = true;
+        for (int i = 0; i < arrayOfKeys.length; i++) {
+            if (arrayOfKeys[i] != null && key != null && arrayOfKeys[i].equals(key.toString())) {
+                arrayOfValues[i] = (V) (value != null ? value.toString() : NULL_EXPRESSION);
+                isUnique = false;
+            } else if (key == null && arrayOfKeys[i] == null && isUnique) {
+                arrayOfValues[i] = (V) (value != null ? value.toString() : NULL_EXPRESSION);
+                isUnique = false;
+                if (nullWasCreatedOnce == false) {
+                    size++;
+                    nullWasCreatedOnce = true;
+                }
             }
-            arrayOfValues[size] = value != null ? value.toString() : NULL_EXPRESSION;
+        }
+        if (isUnique) {
+            if (key == null) {
+                key = (K) NULL_EXPRESSION;
+            }
+            arrayOfKeys[size] = (K) key.toString();
+            arrayOfValues[size] = (V) (value != null ? value.toString() : NULL_EXPRESSION);
             size++;
-        } else {
-            arrayOfValues[numberOfSame] = value.toString();
         }
     }
 
-    private int checkIfUniqueKey(K key) {
-        int indexOfSame = WRONG_RANGE;
+    public void getAK() {
+        System.out.println(Arrays.toString(arrayOfKeys));
+    }
 
-        if (key == null) {
-            key = (K) NULL_EXPRESSION;
-        }
-        for (int i = 0; i < arrayOfKeys.length; i++) {
-            if (key.toString().equals(arrayOfKeys[i])) {
-                indexOfSame = i;
-            }
-        }
-        return indexOfSame;
+    public void getAV() {
+        System.out.println(Arrays.toString(arrayOfValues));
     }
 
     @Override
@@ -55,11 +59,12 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             if (count != 0) {
                 return valueOfKey;
             }
-            if (arrayOfKeys[i].equals(NULL_EXPRESSION) && key == null) {
-                valueOfKey = (V) arrayOfValues[i];
+            if (arrayOfKeys[i] == null && key == null) {
+                valueOfKey = arrayOfValues[i];
                 count++;
-            } else if (key != null && arrayOfKeys[i].equals(key.toString())) {
-                valueOfKey = (V) arrayOfValues[i];
+            } else if (key != null && arrayOfKeys[i] != null
+                    && arrayOfKeys[i].equals(key.toString())) {
+                valueOfKey = arrayOfValues[i];
                 count++;
             }
         }
@@ -71,12 +76,15 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         int count = 0;
 
         for (int i = 0; i < arrayOfKeys.length; i++) {
-            if (arrayOfKeys[i] == null) {
-                continue;
-            } else if (arrayOfKeys[i].equals(NULL_EXPRESSION) && key == null) {
+            if (arrayOfKeys[i] == null && key == null) {
                 count++;
                 break;
-            } else if (key != null && arrayOfKeys[i].equals(key.toString())) {
+            } else if (key == null && arrayOfKeys[i] != null
+                    && arrayOfKeys[i].equals(NULL_EXPRESSION)) {
+                count++;
+                break;
+            } else if (key != null && arrayOfKeys[i] != null
+                    && arrayOfKeys[i].equals(key.toString())) {
                 count++;
                 break;
             }
