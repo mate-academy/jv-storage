7ea49721a68d7348b7a4c8cf9f0ccd806d49b1a1
peps <pepsithedoge@gmail.com>
2021-04-06 23:57:14 +0300

    added solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,18 +3,48 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int ARRAY_LENGTH = 10;
+    private static final int INDEX_NOT_FOUND = -1;
+    private Object[] keys = new Object[ARRAY_LENGTH];
+    private Object[] values = new Object[ARRAY_LENGTH];
+    private Integer count = 0;
 
     @Override
     public void put(K key, V value) {
+        int oldIndex = getIndexOf(key);
+        if (oldIndex != INDEX_NOT_FOUND) {
+            keys[oldIndex] = key;
+            values[oldIndex] = value;
+            return;
+        }
+        keys[count] = key;
+        values[count] = value;
+        count++;
     }
 
     @Override
     public V get(K key) {
+        int oldIndex = getIndexOf(key);
+        if (oldIndex != INDEX_NOT_FOUND) {
+            return (V) values[oldIndex];
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return count;
+    }
+
+    private int getIndexOf(K key) {
+        for (int i = 0; i < count; i++) {
+            if (keys[i] == null && key == null) {
+                return i;
+            }
+            if (keys[i] != null && keys[i].equals(key)) {
+                return i;
+            }
+        }
+        return INDEX_NOT_FOUND;
     }
 }
