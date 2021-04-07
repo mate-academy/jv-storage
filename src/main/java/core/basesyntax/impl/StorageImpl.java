4b57fe25c166a60eceeee9ec88781089ed0eca52
the5tranger <mishaantonyuk00@gmail.com>
2021-04-07 16:34:58 +0300

    I done it v1.1

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,47 +3,38 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private K[] keys;
-    private V[] values;
+    private K[] keys = (K[]) new Object[0];
+    private V[] values = (V[]) new Object[0];
 
     @Override
     public void put(K key, V value) {
         K[] tempKeys = keys;
         V[] tempValues = values;
-        boolean isFind = false;
-        int position = 0;
-        if (keys == null) {
-            keys = (K[]) new Object[1];
-            values = (V[]) new Object[1];
-            keys[0] = key;
-            values[0] = value;
-            return;
-        }
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i] == key) {
-                isFind = true;
-                position = i;
-            } else if (key != null) {
-                if (key.equals(keys[i])) {
-                    isFind = true;
-                    position = i;
+
+        if (keys != null) {
+            for (int i = 0; i < keys.length; i++) {
+                if (keys[i] == key) {
+                    keys[i] = key;
+                    values[i] = value;
+                    return;
+                } else if (key != null) {
+                    if (key.equals(keys[i])) {
+                        keys[i] = key;
+                        values[i] = value;
+                        return;
+                    }
                 }
             }
         }
-        if (!isFind) {
-            keys = (K[]) new Object[keys.length + 1];
-            values = (V[]) new Object[values.length + 1];
-            for (int i = 0; i < tempKeys.length; i++) {
-                keys[i] = tempKeys[i];
-                values[i] = tempValues[i];
 
-            }
-            keys[keys.length - 1] = key;
-            values[values.length - 1] = value;
-        } else {
-            keys[position] = key;
-            values[position] = value;
+        keys = (K[]) new Object[keys.length + 1];
+        values = (V[]) new Object[values.length + 1];
+        for (int i = 0; i < keys.length - 1; i++) {
+            keys[i] = tempKeys[i];
+            values[i] = tempValues[i];
         }
+        keys[keys.length - 1] = key;
+        values[values.length - 1] = value;
     }
 
     @Override
