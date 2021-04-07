0790490b69c9bcad81c4c764607dc1c30d06fd60
roman <nashpatsa@gmai.com>
2021-04-07 16:26:39 +0300

    changed common mistakes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,9 +4,14 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int SIZE_OF_ARRAY = 10;
-    private K[] arrayOfKey = (K[]) new Object[SIZE_OF_ARRAY];
-    private V[] arrayOfValue = (V[]) new Object[SIZE_OF_ARRAY];
+    private K[] arrayOfKey;
+    private V[] arrayOfValue;
     private int indexCounter = 0;
+
+    public StorageImpl() {
+        arrayOfKey = (K[]) new Object[SIZE_OF_ARRAY];
+        arrayOfValue = (V[]) new Object[SIZE_OF_ARRAY];
+    }
 
     @Override
     public void put(K key, V value) {
