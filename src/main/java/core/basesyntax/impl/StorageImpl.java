aed9a3a70f2a1723b114af9ad0158e685f2d90b2
Darina <to.darina.to>
2021-04-07 22:00:00 +0300

    review1 done

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,35 +4,35 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int LENGTH_STORAGE = 10;
-    private K[] itemK;
-    private V[] itemV;
+    private K[] keys;
+    private V[] values;
+    private int size = 1;
 
     public StorageImpl() {
-        this.itemK = (K[]) new Object[LENGTH_STORAGE];
-        this.itemV = (V[]) new Object[LENGTH_STORAGE];
+        this.keys = (K[]) new Object[LENGTH_STORAGE];
+        this.values = (V[]) new Object[LENGTH_STORAGE];
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < LENGTH_STORAGE; i++) {
-            if (itemV[i] != null && check(itemK[i], key)) {
-                itemV[i] = value;
+        for (int i = 0; i < size; i++) {
+            if (values[i] != null && (key == keys[i]) || (keys[i] != null
+                    && keys[i].equals(key))
+                    || (keys[i] == null && values[i] == null)) {
+                size++;
+                values[i] = value;
+                keys[i] = key;
                 return;
-            }
-            if (itemK[i] == null && itemV[i] == null) {
-                itemK[i] = key;
-                itemV[i] = value;
-                return;
-
             }
         }
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < itemK.length; i++) {
-            if (itemV[i] != null && check(itemK[i], key)) {
-                return itemV[i];
+        for (int i = 0; i < keys.length; i++) {
+            if (values[i] != null && (key == keys[i]) || (keys[i] != null
+                    && keys[i].equals(key))) {
+                return values[i];
             }
         }
         return null;
@@ -41,16 +41,11 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         int result = 0;
-        for (int i = 0; i < itemV.length; i++) {
-            if (itemV[i] != null) {
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] != null) {
                 result++;
             }
         }
         return result;
-    }
-
-    public boolean check(K itemK, K key) {
-        return ((key == itemK) || (itemK != null
-                && itemK.equals(key)));
     }
 }
