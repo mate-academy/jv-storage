b2d7656834b98946ff8fab2fbcbc41df23fe2750
vuyko23 <mykolasakulych@gmail.com>
2021-04-07 11:04:50 +0300

    fixed version

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,11 +2,9 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
-import java.util.Objects;
-
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ITEMS_NUMBER = 10;
-    private static int arrayIndex;
+    private int arrayIndex;
     private Object[] keyArray;
     private Object[] valueArray;
 
