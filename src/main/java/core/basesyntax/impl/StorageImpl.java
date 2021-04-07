7e7d70ddad096ba40c10e305d20dfff6ebc4f270
Den4eg007 <68178197+Den4eg007@users.noreply.github.com>
2021-04-07 16:12:02 +0300

    Implemented StorageImpl class

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,18 +3,51 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int DEFAULT_INITIAL_CAPACITY = 10;
+    private final DataUnit<K, V>[] storage;
+
+    public StorageImpl() {
+        storage = new DataUnit[DEFAULT_INITIAL_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && compareKeys(key, storage[i].getKey())) {
+                storage[i].setValue(value);
+                return;
+            }
+            if (storage[i] == null) {
+                storage[i] = new DataUnit<>(key, value);
+                return;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (DataUnit<K, V> kvDataUnit : storage) {
+            if (kvDataUnit != null && compareKeys(key, kvDataUnit.getKey())) {
+                return kvDataUnit.getValue();
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        int result = 0;
+        for (DataUnit<K, V> kvDataUnit : storage) {
+            if (kvDataUnit != null) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+    private boolean compareKeys(K firstKey, K secondKey) {
+        return (firstKey == secondKey)
+                || (secondKey != null && secondKey.equals(firstKey));
     }
 }
