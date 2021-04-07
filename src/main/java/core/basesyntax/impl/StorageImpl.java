16842542f3a7542bce19f21d3d256cf369357f5d
roman <nashpatsa@gmai.com>
2021-04-07 16:14:58 +0300

    mothod put and get done

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,18 +3,39 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int SIZE_OF_ARRAY = 10;
+    private K[] arrayOfKey = (K[]) new Object[SIZE_OF_ARRAY];
+    private V[] arrayOfValue = (V[]) new Object[SIZE_OF_ARRAY];
+    private int indexCounter = 0;
 
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < arrayOfKey.length; i++) {
+            if (arrayOfKey[i] != null && arrayOfKey[i].equals(key) && arrayOfValue[i] != null
+                    || arrayOfKey[i] == null && arrayOfKey[i] == key
+                    && arrayOfValue[i] != null) {
+                arrayOfValue[i] = value;
+                return;
+            }
+        }
+        arrayOfKey[indexCounter] = key;
+        arrayOfValue[indexCounter] = value;
+        indexCounter++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < arrayOfKey.length; i++) {
+            if (arrayOfKey[i] != null && arrayOfKey[i].equals(key)
+                    || arrayOfKey[i] == null && arrayOfKey[i] == key) {
+                return arrayOfValue[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return indexCounter;
     }
 }
