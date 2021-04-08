a6a01fc64337809b0eca63b09ef2ead7e91452da
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-04-08 14:10:20 +0300

    Ready for review.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,18 +3,47 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAXIMUM_CAPACITY = 10;
+
+    private K[] keys;
+    private V[] values;
+    private int currentSize;
+
+    public StorageImpl() {
+        this.keys = (K[]) new Object[MAXIMUM_CAPACITY];
+        this.values = (V[]) new Object[MAXIMUM_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
+        boolean isFound = false;
+        if (currentSize < MAXIMUM_CAPACITY) {
+            for (int i = 0; i < currentSize; i++) {
+                if (keys[i] == key || (keys[i] != null && keys[i].equals(key))) {
+                    values[i] = value;
+                    isFound = true;
+                }
+            }
+            if (!isFound && value != null) {
+                keys[currentSize] = key;
+                values[currentSize] = value;
+                currentSize++;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < currentSize; i++) {
+            if (keys[i] == key || (keys[i] != null && keys[i].equals(key))) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return currentSize;
     }
 }
