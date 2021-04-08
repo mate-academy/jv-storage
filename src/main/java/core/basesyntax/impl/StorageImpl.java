b9aec0f749f26f8d4aef98ed26c8f63a30093e24
anufriiev <dimaanufriev1012@gmail.com>
2021-04-08 12:08:19 +0300

    Deleted Objects.equals method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,28 +1,27 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     public static final int MAX_NUMBER = 10;
     private int size;
-    private final K[] keyArray;
+    private final K[] keysArray;
     private final V[] valueArray;
 
     public StorageImpl() {
-        this.keyArray = (K[]) new Object[MAX_NUMBER];
+        this.keysArray = (K[]) new Object[MAX_NUMBER];
         this.valueArray = (V[]) new Object[MAX_NUMBER];
     }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(key, keyArray[i])) {
+            if (key == keysArray[i] || key != null && key.equals(keysArray[i])) {
                 valueArray[i] = value;
                 return;
             }
         }
-        keyArray[size] = key;
+        keysArray[size] = key;
         valueArray[size] = value;
         size++;
 
@@ -30,8 +29,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keyArray.length; i++) {
-            if (Objects.equals(key, keyArray[i])) {
+        for (int i = 0; i < keysArray.length; i++) {
+            if (key == keysArray[i] || key != null && key.equals(keysArray[i])) {
                 return valueArray[i];
             }
         }
