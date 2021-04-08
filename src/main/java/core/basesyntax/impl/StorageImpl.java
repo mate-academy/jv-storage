9b9230ca960eb9fc37330945a947509f013fd3e0
redxp <redxpertus1@gmail.com>
2021-04-08 14:00:21 +0300

    storage v.03. Yeap! In this case code looks more readable and clearly :)

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,9 +3,9 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int ARRAY_SIZE = 10;
-    private final Object[] keyArray = new Object[ARRAY_SIZE];
-    private final Object[] valuesArray = new Object[ARRAY_SIZE];
+    private static final int MAX_SIZE = 10;
+    private final Object[] keyArray = new Object[MAX_SIZE];
+    private final Object[] valuesArray = new Object[MAX_SIZE];
     private int arraySize = 0;
 
     @Override
@@ -23,40 +23,16 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        V value = null;
-        boolean check;
-        for (int i = 0; i < ARRAY_SIZE; i++) {
-            if (key == null && valuesArray[i] != null) {
-                check = true;
-            } else {
-                assert key != null;
-                check = key.equals(keyArray[i]);
-            }
-            if (check) {
-                if (keyArray[i] == key && valuesArray[i] == null) {
-                    return null;
-                } else if (keyArray[i] == key && valuesArray[i] != null
-                        || keyArray[i].equals(key) && valuesArray[i] != null) {
-                    value = (V) valuesArray[i];
-                    break;
-                } else if (keyArray[i].equals(key) && valuesArray[i] == null) {
-                    return null;
-                }
+        for (int i = 0; i < arraySize; i++) {
+            if (key != null && key.equals(keyArray[i]) || key == keyArray[i]) {
+                return (V) valuesArray[i];
             }
         }
-        return value;
+        return null;
     }
 
     @Override
     public int size() {
-        int storageSize = 0;
-        int countOfMatches = 0;
-        while (countOfMatches < ARRAY_SIZE) {
-            if (valuesArray[countOfMatches] != null) {
-                storageSize++;
-            }
-            countOfMatches++;
-        }
-        return storageSize;
+        return arraySize;
     }
 }
