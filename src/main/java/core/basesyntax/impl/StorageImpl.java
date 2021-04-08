ac6074bf16f3545075aebbc08908c88f12f1365f
Darina <to.darina.to>
2021-04-08 11:20:53 +0300

    review2

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,16 +6,16 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int LENGTH_STORAGE = 10;
     private K[] keys;
     private V[] values;
-    private int size = 1;
+    private int size = 0;
 
     public StorageImpl() {
-        this.keys = (K[]) new Object[LENGTH_STORAGE];
-        this.values = (V[]) new Object[LENGTH_STORAGE];
+        keys = (K[]) new Object[LENGTH_STORAGE];
+        values = (V[]) new Object[LENGTH_STORAGE];
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i <= size; i++) {
             if (values[i] != null && (key == keys[i]) || (keys[i] != null
                     && keys[i].equals(key))
                     || (keys[i] == null && values[i] == null)) {
