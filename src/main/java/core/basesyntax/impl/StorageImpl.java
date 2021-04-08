0da6212037290ad99eca42ae0ad3f27d029711e1
apyavchik <81320602+apyavchik@users.noreply.github.com>
2021-04-08 17:58:16 +0300

    implement hw jv-storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,18 +3,48 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_CAPACITY = 10;
+    private K[] keysArray;
+    private V[] valuesArray;
+    private int size;
+
+    public StorageImpl() {
+        keysArray = (K[]) new Object[MAX_CAPACITY];
+        valuesArray = (V[]) new Object[MAX_CAPACITY];
+        size = 0;
+    }
 
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (key == keysArray[i]) {
+                valuesArray[i] = value;
+                return;
+            } else if (key != null && key.equals(keysArray[i])) {
+                valuesArray[i] = value;
+                return;
+            }
+        }
+        keysArray[size] = key;
+        valuesArray[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key != keysArray[i]) {
+                if (key == null || !key.equals(keysArray[i])) {
+                    continue;
+                }
+            }
+            return valuesArray[i];
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
