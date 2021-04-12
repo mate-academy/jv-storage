3a93ceba4dd62fc7e2b9f0865de6c6d4444c24a4
RomanDubovskyi <58385506+RomanDubovskyi@users.noreply.github.com>
2021-04-12 15:13:11 +0300

    added test for static size variable (#283)
    * added test for static size variable
    
    * Update src/test/java/core/basesyntax/StorageImplTest.java

--- src/test/java/core/basesyntax/StorageImplTest.java
+++ src/test/java/core/basesyntax/StorageImplTest.java
@@ -7,6 +7,22 @@ import org.junit.Test;
 public class StorageImplTest {
 
     @Test
+    public void checkSizeIsNonStatic() {
+        Storage<Integer, String> firstInstance = new StorageImpl<>();
+        firstInstance.put(1, "Element 1");
+        firstInstance.put(2, "Element 2");
+        firstInstance.put(3, "Element 3");
+
+        Assert.assertEquals("With three elements added to the storage, its size should be 3",
+              3, firstInstance.size());
+
+        Storage<Integer, String> secondInstances = new StorageImpl<>();
+
+        Assert.assertEquals("After creating second empty storage instance its' size should be 0",
+              0, secondInstances.size());
+    }
+
+    @Test
     public void addElementsToStorage() {
         Storage<Integer, String> storage = new StorageImpl<>();
         String elementOne = "Element 1";
