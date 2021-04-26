01817f2606fb936b818db27463fa64c350e41a85
apyavchik <81320602+apyavchik@users.noreply.github.com>
2021-04-26 12:36:23 +0300

    simplify methods get and put

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,6 +1,7 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_CAPACITY = 10;
@@ -17,10 +18,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (key == keysArray[i]) {
-                valuesArray[i] = value;
-                return;
-            } else if (key != null && key.equals(keysArray[i])) {
+            if (Objects.equals(key, keysArray[i])) {
                 valuesArray[i] = value;
                 return;
             }
@@ -33,12 +31,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (key != keysArray[i]) {
-                if (key == null || !key.equals(keysArray[i])) {
-                    continue;
-                }
+            if (Objects.equals(key, keysArray[i])) {
+                return valuesArray[i];
             }
-            return valuesArray[i];
         }
         return null;
     }
