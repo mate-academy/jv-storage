f06923285000e04211f4348615ff5a301ade386e
apyavchik <81320602+apyavchik@users.noreply.github.com>
2021-04-27 11:40:48 +0300

    implement own comperation instead of Objects

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,6 +1,7 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+
 import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
@@ -18,7 +19,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(key, keysArray[i])) {
+            if (key != null && key.equals(keysArray[i]) || key == keysArray[i]) {
                 valuesArray[i] = value;
                 return;
             }
