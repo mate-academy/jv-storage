6ff158a7eef65875f7b8662e8a1bfb062b724124
forsfakel <81535727+forsfakel@users.noreply.github.com>
2021-05-28 00:51:03 +0300

    keys -- array keys values -- array values; put -- method add pair (keys and values) or change value if keys equals; get -- return еkey value; size -- return the number of used cells in the array;

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,18 +3,41 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private final K[] keys = (K[]) new Object[10];
+    private final V[] values = (V[]) new Object[10];
 
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < 10; i++) {
+            if (keys[i] == null && (values[i] == null || key == null)) {
+                keys[i] = key;
+                values[i] = value;
+                break;
+            } else if (keys[i] != null && keys[i].equals(key)) {
+                keys[i] = key;
+                values[i] = value;
+                break;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < 10; i++) {
+            if ((key == null && keys[i] == null) || (keys[i] != null && keys[i].equals(key))) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        for (int i = 0; i < 10; i++) {
+            if (keys[i] == null && values[i] == null) {
+                return (i);
+            }
+        }
+        return 0;
     }
 }
