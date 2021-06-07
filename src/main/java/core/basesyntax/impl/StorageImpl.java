12ca6ea63ab5ccf577d084fa248830281824dd92
forsfakel <81535727+forsfakel@users.noreply.github.com>
2021-06-07 13:39:26 +0300

    added constant DEFAULT_LENGTH - start length array; edited sting "if ((key == null && keys[i] == null) || (keys[i] != null && keys[i].equals(key)))" to "if ((key == keys[i]) || (keys[i] != null && keys[i].equals(key)))"

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,18 +3,18 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private final K[] keys = (K[]) new Object[10];
-    private final V[] values = (V[]) new Object[10];
+    private static final int DEFAULT_LENGTH = 10;
+    private final K[] keys = (K[]) new Object[DEFAULT_LENGTH];
+    private final V[] values = (V[]) new Object[DEFAULT_LENGTH];
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < DEFAULT_LENGTH; i++) {
             if (keys[i] == null && (values[i] == null || key == null)) {
                 keys[i] = key;
                 values[i] = value;
                 break;
             } else if (keys[i] != null && keys[i].equals(key)) {
-                keys[i] = key;
                 values[i] = value;
                 break;
             }
@@ -23,8 +23,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < 10; i++) {
-            if ((key == null && keys[i] == null) || (keys[i] != null && keys[i].equals(key))) {
+        for (int i = 0; i < DEFAULT_LENGTH; i++) {
+            if ((key == keys[i]) || (keys[i] != null && keys[i].equals(key))) {
                 return values[i];
             }
         }
@@ -33,7 +33,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < DEFAULT_LENGTH; i++) {
             if (keys[i] == null && values[i] == null) {
                 return (i);
             }
