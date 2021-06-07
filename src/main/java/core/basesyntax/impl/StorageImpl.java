92e780c469e55cbd5dcc17f809e13873147729f1
aaananas <anastasiia.len11@gmail.com>
2021-06-07 23:04:31 +0200

    Implemented StorageImpl.java, fixed some Code style in test file (caused error while 'mvn clean package')

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,50 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int CAPACITY = 10;
+    private final Object[] keys;
+    private final Object[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = new Object[CAPACITY];
+        values = new Object[CAPACITY];
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = 0;
+        for (Object currentKey : keys) {
+            if (key == currentKey || (key != null && key.equals(currentKey))) {
+                values[index] = value;
+                if (size <= index) {
+                    size++;
+                }
+                return;
+            }
+            index++;
+        }
+        if (size != CAPACITY) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
     }
 
     @Override
     public V get(K key) {
+        int index = 0;
+        for (Object currentKey : keys) {
+            if (currentKey == key || (key != null && key.equals(currentKey))) {
+                return (V) values[index];
+            }
+            index++;
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
