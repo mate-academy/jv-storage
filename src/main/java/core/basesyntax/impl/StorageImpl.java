c6987cc333598a65e8c6ff447b4588236def2269
Tamara-Paskalova <forptv@gmail.com>
2021-06-08 18:30:13 +0300

    added logic at get(), put(), size()

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,49 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int ARRAY_LENGTH = 10;
+    private final K[] keyStorage = (K[]) new Object[ARRAY_LENGTH];
+    private final V[] valueStorage = (V[]) new Object[ARRAY_LENGTH];
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < keyStorage.length; i++) {
+            if (keyStorage[i] == null && valueStorage[i] == null
+                    || key == null && keyStorage[i] == null && !valueStorage[i].equals(value)
+                    || keyStorage[i] != null && keyStorage[i].equals(key)) {
+                keyStorage[i] = key;
+                valueStorage[i] = value;
+                return;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int index = 0;
+        if (key == null) {
+            for (int i = 0; i < keyStorage.length; i++) {
+                if (keyStorage[i] == null && valueStorage[i] != null) {
+                    return valueStorage[i];
+                }
+            }
+        }
+        for (int i = 0; i < keyStorage.length; i++) {
+            if (keyStorage[i] != null && keyStorage[i].equals(key)) {
+                index = i;
+            }
+        }
+        return valueStorage[index];
     }
 
     @Override
     public int size() {
-        return -1;
+        int count = 0;
+        for (V element : valueStorage) {
+            if (element != null) {
+                count++;
+            }
+        }
+        return count;
     }
 }
