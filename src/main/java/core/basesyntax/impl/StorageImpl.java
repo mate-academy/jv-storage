7e9d38bde0d19a0a491e47c48bc71ab346ae83c7
yuriiadam <dr.damba@outlook.com>
2021-06-08 18:02:06 +0300

    A key-value store was implemented, parameterized by two data types

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,41 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_VALUE_OF_STORAGE = 10;
+    private final Object[] keys;
+    private final Object[] values;
+    private int sizeStore;
+
+    public StorageImpl() {
+        keys = new Object[MAX_VALUE_OF_STORAGE];
+        values = new Object[MAX_VALUE_OF_STORAGE];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < sizeStore; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[sizeStore] = key;
+        values[sizeStore] = value;
+        sizeStore++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < MAX_VALUE_OF_STORAGE; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return sizeStore;
     }
 }
