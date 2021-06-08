06dd8a6127daf95c83f6109f3a65a465029c16ff
nikdagaev <nikdagaev@gmail.com>
2021-06-08 16:53:15 +0300

    StorageImpl class was implemented. Tests passed.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,49 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private Object[] keysArray;
+    private Object[] valuesArray;
+
     @Override
     public void put(K key, V value) {
+        if (keysArray == null) {
+            keysArray = new Object[MAX_ITEMS_NUMBER];
+            valuesArray = new Object[MAX_ITEMS_NUMBER];
+        }
+        for (int i = 0; i < MAX_ITEMS_NUMBER; i++) {
+            if (valuesArray[i] == null || keysArray[i] == key
+                    || (keysArray[i] != null && keysArray[i].equals(key))) {
+                keysArray[i] = key;
+                valuesArray[i] = value;
+                return;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        if (keysArray == null) {
+            return null;
+        }
+        for (int i = 0; i < MAX_ITEMS_NUMBER; i++) {
+            if (keysArray[i] == key || (keysArray[i] != null && keysArray[i].equals(key))) {
+                return (V) valuesArray[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        if (keysArray == null) {
+            return 0;
+        }
+        for (int i = 0; i < MAX_ITEMS_NUMBER; i++) {
+            if (valuesArray[i] == null) {
+                return i;
+            }
+        }
+        return MAX_ITEMS_NUMBER;
     }
 }
