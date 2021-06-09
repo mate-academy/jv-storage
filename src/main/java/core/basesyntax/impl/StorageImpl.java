aca51d7801db74f286bc772236c7ddb7cf5ac1d0
Tamara-Paskalova <forptv@gmail.com>
2021-06-09 11:41:38 +0300

    changed logic at get(), put()

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,39 +1,38 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int ARRAY_LENGTH = 10;
+    private static final int MAX_ARRAY_LENGTH = 10;
     private final K[] keyStorage;
     private final V[] valueStorage;
-    private int count = 0;
+    private int count;
 
     public StorageImpl() {
-        keyStorage = (K[]) new Object[ARRAY_LENGTH];
-        valueStorage = (V[]) new Object[ARRAY_LENGTH];
+        keyStorage = (K[]) new Object[MAX_ARRAY_LENGTH];
+        valueStorage = (V[]) new Object[MAX_ARRAY_LENGTH];
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < keyStorage.length; i++) {
-            if (keyStorage[i] == null && valueStorage[i] == null) {
-                keyStorage[i] = key;
-                valueStorage[i] = value;
-                count++;
-                return;
-            } else if (Objects.equals(keyStorage[i], key)) {
+        for (int i = 0; i < count; i++) {
+            if (key == keyStorage[i]
+                    || key != null && key.equals(keyStorage[i])) {
                 keyStorage[i] = key;
                 valueStorage[i] = value;
                 return;
             }
         }
+        keyStorage[count] = key;
+        valueStorage[count] = value;
+        count++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keyStorage.length; i++) {
-            if (Objects.equals(keyStorage[i], key)) {
+        for (int i = 0; i < count; i++) {
+            if (key == keyStorage[i]
+                    || key != null && key.equals(keyStorage[i])) {
                 return valueStorage[i];
             }
         }
