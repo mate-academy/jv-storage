63b9d22f2fc5d3ca8ff3956cca872ef23b692b8b
sSlonIk <kryachok@gmail.com>
2021-06-09 12:06:18 +0300

    removed object and mmethods

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,7 +1,6 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_STORAGE_NUMBER = 10;
@@ -17,7 +16,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(key, keys[i])) {
+            if((key == keys[i])||(key != null && key.equals(keys[i]))){
                 values[i] = value;
                 return;
             }
@@ -30,7 +29,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(key, keys[i])) {
+            if ((key == keys[i])||(key != null && key.equals(keys[i]))) {
                 return (V) values[i];
             }
         }
