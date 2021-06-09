d8a5a41434a95196ea17dc6683add64e1dae5112
Denis <deniskichan@gmail.com>
2021-06-09 09:30:33 +0300

    implemented methods to put and get elements from storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,41 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int ARRAY_SIZE = 10;
+    private int currentIndex = 0;
+    private K[] keys;
+    private V[] values;
+
+    public StorageImpl() {
+        this.keys = (K[]) new Object[ARRAY_SIZE];
+        this.values = (V[]) new Object[ARRAY_SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < currentIndex; i++) {
+            if (key != null && key.equals(keys[i]) || key == keys[i]) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[currentIndex] = key;
+        values[currentIndex] = value;
+        currentIndex++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < currentIndex; i++) {
+            if (key != null && key.equals(keys[i]) || key == keys[i]) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return currentIndex;
     }
 }
