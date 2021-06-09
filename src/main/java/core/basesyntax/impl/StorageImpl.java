f122e8fa50c0ca8cb67ab7a64495a63198491560
vKutik <84785564+vKutik@users.noreply.github.com>
2021-06-09 15:41:11 +0300

    fix checkstyle

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,34 +4,33 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE_ARRAY = 10;
-    private int lengthArray = 0;
-    private K[] key;
-    private V[] value;
+    private int lengthArray;
+    private K[] keys;
+    private V[] values;
 
     public StorageImpl() {
-        key = (K[]) new Object[MAX_SIZE_ARRAY];
-        value = (V[]) new Object[MAX_SIZE_ARRAY];
+        keys = (K[]) new Object[MAX_SIZE_ARRAY];
+        values = (V[]) new Object[MAX_SIZE_ARRAY];
     }
 
     @Override
     public void put(K key, V value) {
-
         for (int i = 0; i < lengthArray; i++) {
-            if (this.key[i] == key || (key != null && key.equals(this.key[i]))) {
-                this.value[i] = value;
+            if (keys[i] == key || (key != null && key.equals(keys[i]))) {
+                values[i] = value;
                 return;
             }
         }
-        this.key[lengthArray] = key;
-        this.value[lengthArray] = value;
+        this.keys[lengthArray] = key;
+        this.values[lengthArray] = value;
         lengthArray++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < lengthArray; i++) {
-            if (this.key[i] == key || (key != null && key.equals(this.key[i]))) {
-                return this.value[i];
+            if (keys[i] == key || (key != null && key.equals(keys[i]))) {
+                return values[i];
             }
         }
         return null;
