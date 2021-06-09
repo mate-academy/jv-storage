181dfc3683c57d7fff5c7d454f8483132da5108f
PashaTerletskyi <p.terletsky@gmail.com>
2021-06-09 14:22:10 +0300

    initial commit

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,16 +4,13 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_NUMBER_OF_ELEMENTS = 10;
-    private final K[] arrayOfKeys = (K[]) new Object[MAX_NUMBER_OF_ELEMENTS];
-    private final V[] arrayOfValues = (V[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+    private K[] arrayOfKeys = (K[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+    private V[] arrayOfValues = (V[]) new Object[MAX_NUMBER_OF_ELEMENTS];
     private int sizeOfStorage = 0;
 
     @Override
     public void put(K key, V value) {
-        if (key == null) {
-
-        }
-        for (int i = 0; i < MAX_NUMBER_OF_ELEMENTS; i++) {
+        for (int i = 0; i < sizeOfStorage; i++) {
             if (key == arrayOfKeys[i] || key != null && key.equals(arrayOfKeys[i])) {
                 arrayOfValues[i] = value;
                 return;
@@ -26,7 +23,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < MAX_NUMBER_OF_ELEMENTS; i++) {
+        for (int i = 0; i < sizeOfStorage; i++) {
             if (key == arrayOfKeys[i] || key != null && key.equals(arrayOfKeys[i])) {
                 return arrayOfValues[i];
             }
