a46f49d43b5a6c878ae0cda57d04b3e71045aae0
Basilius the Third <champfockes@gmail.com>
2021-06-09 13:58:55 +0300

    created constructor, improved method put logic

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,28 +6,31 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_STORAGE_SIZE = 10;
     private static final int KEY_INDEX = 0;
     private static final int VALUE_INDEX = 1;
-    private Object[][] storage = new Object[MAX_STORAGE_SIZE][2];
-    private int actualStorageSize = 0;
+    private Object[][] storage;
+    private int actualStorageSize;
+
+    public StorageImpl() {
+        storage = new Object[MAX_STORAGE_SIZE][2];
+        actualStorageSize = 0;
+    }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < MAX_STORAGE_SIZE; i++) {
-            if (storage[i][KEY_INDEX] == null && storage[i][VALUE_INDEX] == null) {
-                actualStorageSize++;
-                storage[i][KEY_INDEX] = key;
-                storage[i][VALUE_INDEX] = value;
-                break;
-            } else if (key == null && storage[i][KEY_INDEX] == null
+        for (int i = 0; i <= actualStorageSize; i++) {
+            if (storage[i][VALUE_INDEX] != null && storage[i][KEY_INDEX] == key
                     || key != null && key.equals(storage[i][KEY_INDEX])) {
                 storage[i][VALUE_INDEX] = value;
-                break;
+                return;
             }
         }
+        storage[actualStorageSize][KEY_INDEX] = key;
+        storage[actualStorageSize][VALUE_INDEX] = value;
+        actualStorageSize++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < MAX_STORAGE_SIZE; i++) {
+        for (int i = 0; i < actualStorageSize; i++) {
             if (key == null && storage[i][KEY_INDEX] == null
                     || key != null && key.equals(storage[i][KEY_INDEX])) {
                 return (V) storage[i][VALUE_INDEX];
