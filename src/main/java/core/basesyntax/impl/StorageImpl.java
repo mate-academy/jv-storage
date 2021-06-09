26bce1ac3500478cc2e724f2d8fb225642f4976e
Nikitozin <nikitozin91@gmail.com>
2021-06-09 09:39:07 +0300

    change_variable

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -28,7 +28,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < MAX_CAPACITY; i++) {
+        for (int i = 0; i < currentCapacity; i++) {
             if (key == keys[i] || key != null && key.equals(keys[i])) {
                 return (V) values[i];
             }
