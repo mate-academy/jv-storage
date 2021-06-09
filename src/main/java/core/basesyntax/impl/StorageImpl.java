22bac11e9dc5dbb9937ad418f923e68b0fcb3160
Yura Lug <LUV97@ukr.net>
2021-06-09 09:39:27 +0300

    Fix style, loop and logic

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,33 +4,32 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int CAPACITY = 10;
-    private int size = 0;
-    private Object [] keys = new Object [CAPACITY];
-    private Object [] values = new Object [CAPACITY];
+    private int size;
+    private Object[] keys;
+    private Object[] values;
+
+    public StorageImpl() {
+        keys = new Object[CAPACITY];
+        values = new Object[CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
-        int counterSameKeys = 0;
         for (int i = 0; i < size; i++) {
             if (key == keys[i] || key != null && key.equals(keys[i])) {
-                counterSameKeys++;
                 values[i] = value;
+                return;
             }
         }
-        if (counterSameKeys == 0) {
-            keys[size] = key;
-            values[size] = value;
-            size++;
-        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if ((keys[i] == null) && (key == null)) {
-                return (V) values[i];
-            }
-            if ((keys[i] != null) && (keys[i].equals(key))) {
+            if ((keys[i] == null) && (key == null) || (keys[i] != null) && (keys[i].equals(key))) {
                 return (V) values[i];
             }
         }
