676c2cd00207b0b150a001cd4b3fde4ab65d9c19
PashaTerletskyi <p.terletsky@gmail.com>
2021-06-09 13:59:49 +0300

    preintial commit

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,39 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_NUMBER_OF_ELEMENTS = 10;
+    private final K[] arrayOfKeys = (K[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+    private final V[] arrayOfValues = (V[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+    private int sizeOfStorage = 0;
+
     @Override
     public void put(K key, V value) {
+        if (key == null) {
+
+        }
+        for (int i = 0; i < MAX_NUMBER_OF_ELEMENTS; i++) {
+            if (key == arrayOfKeys[i] || key != null && key.equals(arrayOfKeys[i])) {
+                arrayOfValues[i] = value;
+                return;
+            }
+        }
+        arrayOfKeys[sizeOfStorage] = key;
+        arrayOfValues[sizeOfStorage] = value;
+        sizeOfStorage++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < MAX_NUMBER_OF_ELEMENTS; i++) {
+            if (key == arrayOfKeys[i] || key != null && key.equals(arrayOfKeys[i])) {
+                return arrayOfValues[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return sizeOfStorage;
     }
 }
