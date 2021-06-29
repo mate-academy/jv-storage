2539f44d16577e28177be45dc74b8ecc00eab4d2
Alex Pravda <kabinkos@gmail.com>
2021-06-29 15:44:53 +0300

    - JV Homework Generics, Storage task - separate method to check KEY duplication - Arrays to store data - expandable, implemented via Array copy (can I use Array.copyOf here since IDEA gives warning "Manual array copy"?)

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,64 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int ARRAY_INITIAL_SIZE = 0;
+    private K key;
+    private V value;
+
+    private K[] keysArray = (K[]) new Object[ARRAY_INITIAL_SIZE];
+    private V[] valuesArray = (V[]) new Object[ARRAY_INITIAL_SIZE];
+
+    public void expandValuesArray() {
+        V[] newArray = (V[]) new Object[valuesArray.length + 1];
+        for (int i = 0; i < valuesArray.length; i++) {
+            newArray[i] = valuesArray[i];
+        }
+        valuesArray = newArray;
+    }
+
+    public void expandKeysArray() {
+        K[] newArray = (K[]) new Object[keysArray.length + 1];
+        for (int i = 0; i < keysArray.length; i++) {
+            newArray[i] = keysArray[i];
+        }
+        keysArray = newArray;
+    }
+
     @Override
     public void put(K key, V value) {
+        this.key = key;
+        this.value = value;
+
+        if (!checkKeyDuplicate(key)) {
+            expandKeysArray();
+            expandValuesArray();
+            keysArray[keysArray.length - 1] = this.key;
+            valuesArray[valuesArray.length - 1] = this.value;
+        }
+    }
+
+    private boolean checkKeyDuplicate(K key) {
+        for (int i = 0; i < keysArray.length; i++) {
+            if (key == keysArray[i] || key != null && key.equals(keysArray[i])) {
+                valuesArray[i] = this.value;
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keysArray.length; i++) {
+            if (key == keysArray[i] || key != null && key.equals(keysArray[i])) {
+                return valuesArray[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return keysArray.length;
     }
 }
