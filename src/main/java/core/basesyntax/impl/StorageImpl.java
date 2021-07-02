0ac19a6aabd5da908067bf2c636e8dc4885c8464
Alex Pravda <kabinkos@gmail.com>
2021-07-02 11:58:34 +0300

    - removed key / value variables inside of class - re-done with 2D array

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,57 +3,32 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int ARRAY_INITIAL_SIZE = 0;
-    private K key;
-    private V value;
+    private static final int ARRAY_MAX_SIZE = 10;
+    private int size = 0;
 
-    private K[] keysArray = (K[]) new Object[ARRAY_INITIAL_SIZE];
-    private V[] valuesArray = (V[]) new Object[ARRAY_INITIAL_SIZE];
-
-    private void expandValuesArray() {
-        V[] newArray = (V[]) new Object[valuesArray.length + 1];
-        for (int i = 0; i < valuesArray.length; i++) {
-            newArray[i] = valuesArray[i];
-        }
-        valuesArray = newArray;
-    }
-
-    private void expandKeysArray() {
-        K[] newArray = (K[]) new Object[keysArray.length + 1];
-        for (int i = 0; i < keysArray.length; i++) {
-            newArray[i] = keysArray[i];
-        }
-        keysArray = newArray;
-    }
+    private final Object[][] resultArray = new Object[ARRAY_MAX_SIZE][2];
 
     @Override
     public void put(K key, V value) {
-        this.key = key;
-        this.value = value;
-
-        if (!checkKeyDuplicate(key)) {
-            expandKeysArray();
-            expandValuesArray();
-            keysArray[keysArray.length - 1] = this.key;
-            valuesArray[valuesArray.length - 1] = this.value;
-        }
-    }
-
-    private boolean checkKeyDuplicate(K key) {
-        for (int i = 0; i < keysArray.length; i++) {
-            if (key == keysArray[i] || key != null && key.equals(keysArray[i])) {
-                valuesArray[i] = this.value;
-                return true;
+        for (int i = 0; i < size; i++) {
+            if (key == null && resultArray[i][0] == null
+                    || key != null && key.equals(resultArray[i][0])) {
+                resultArray[i][0] = key;
+                resultArray[i][1] = value;
+                return;
             }
         }
-        return false;
+        resultArray[size][0] = key;
+        resultArray[size][1] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keysArray.length; i++) {
-            if (key == keysArray[i] || key != null && key.equals(keysArray[i])) {
-                return valuesArray[i];
+        for (int i = 0; i < resultArray.length; i++) {
+            if (key == resultArray[i][0] || key != null && key.equals(resultArray[i][0])) {
+                //noinspection unchecked
+                return (V) resultArray[i][1];
             }
         }
         return null;
@@ -61,6 +36,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return keysArray.length;
+        return size;
     }
 }
