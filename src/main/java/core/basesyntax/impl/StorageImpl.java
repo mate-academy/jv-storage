22a2c6600bfbe3830dd18f3ddccdf0a940dc2ecc
alexpogrebniy <aleksandr_pogreb@ukr.net>
2021-07-08 23:38:42 +0300

    1 commit

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,41 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_LENGTH = 10;
+    private Object[] keysArr;
+    private Object[] valuesArr;
+    private int capacity;
+
+    public StorageImpl() {
+        keysArr = new Object[MAX_LENGTH];
+        valuesArr = new Object[MAX_LENGTH];
+    }
+
     @Override
     public void put(K key, V value) {
+        keysArr[capacity] = key;
+        valuesArr[capacity] = value;
+        capacity++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < capacity; i++) {
+            if (key == null && keysArr[i] == null && capacity < 3
+                    || keysArr[i] != null && keysArr[i].equals(keysArr[i + 1])) {
+                capacity = 1;
+                return (V) valuesArr[i + 1];
+            }
+
+            if (keysArr[i] != null && key != null && key.equals(keysArr[i]) || keysArr[i] == key) {
+                return (V) valuesArr[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return capacity;
     }
 }
