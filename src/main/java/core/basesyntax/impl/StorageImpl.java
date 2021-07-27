b9777992de1921c5e7fa68a3a1a16be39a33a19b
dmitriy tverdokhlibov <dmitriytverdokhlibov@yahoo.com>
2021-07-27 17:20:17 +0200

    completed Solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,68 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private static final int DEFAULT_CAPACITY = 10;
+
+    private Pair[] data = new Pair[DEFAULT_CAPACITY];
+
     @Override
     public void put(K key, V value) {
+        Pair newPair = new Pair(key, value);
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == null) {
+                data[i] = newPair;
+                break;
+            }
+
+            if (data[i] != null) {
+                if (data[i].getKey() == null && newPair.getKey() == null) {
+                    data[i] = newPair;
+                    break;
+                }
+
+                if (data[i].getKey() != null && data[i].getKey().equals(newPair.getKey())) {
+                    data[i] = newPair;
+                    break;
+                }
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        if (key != null) {
+            for (Pair pair : data) {
+                if (pair != null && pair.getKey() != null) {
+                    if (pair.getKey().equals(key)) {
+                        return (V) pair.getValue();
+                    }
+                }
+            }
+        }
+
+        if (key == null) {
+            for (Pair pair : data) {
+                if (pair != null) {
+                    if (pair.getKey() == null) {
+                        return (V) pair.getValue();
+                    }
+
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        int size = 0;
+        for (int j = 0; j < data.length; j++) {
+            if (data[j] != null) {
+                size++;
+            }
+        }
+
+        return size;
     }
 }
