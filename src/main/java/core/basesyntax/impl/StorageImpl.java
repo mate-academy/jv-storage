dc31d2a0b044ac8e06dd55f9851a17b2f0c80a49
Dima Petrushenko <dmytro.petrushenko@gmail.com>
2021-08-19 10:33:59 +0300

    teacher's notices fixed

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,34 +3,25 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private K key;
-    private V value;
     private final Object[][] array = new Object[10][2];
     private int size = 0;
 
     @Override
     public void put(K key, V value) {
-        this.key = key;
-        this.value = value;
-        if (check(this.key, this.value)) {
-            addNew(this.key, this.value);
+        if (checkStorage(key, value)) {
+            addNew(key, value);
         }
     }
 
     @Override
     public V get(K key) {
-        if (key == null) {
-            for (Object[] objects : array) {
-                if (objects[0] == null && objects[1] != null) {
-                    return (V) objects[1];
-                }
+        for (int i = 0; i < size; i++) {
+            if (key == null && array[i][0] == null && array[i][1] != null) {
+                return (V) array[i][1];
             }
-        } else {
-            for (Object[] objects : array) {
-                if (objects[0] != null) {
-                    if (objects[0].equals(key)) {
-                        return (V) objects[1];
-                    }
+            if (array[i][0] != null) {
+                if (array[i][0].equals(key)) {
+                    return (V) array[i][1];
                 }
             }
         }
@@ -47,27 +38,22 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             if (array[i][0] == null && array[i][1] == null) {
                 array[i][0] = key;
                 array[i][1] = value;
-                this.size++;
+                size++;
                 break;
             }
         }
     }
 
-    private boolean check(K key, V value) {
-        if (key == null) {
-            for (int i = 0; i < array.length; i++) {
-                if (array[i][0] == null && array[i][1] != null) {
+    private boolean checkStorage(K key, V value) {
+        for (int i = 0; i < array.length; i++) {
+            if (key == null && array[i][0] == null && array[i][1] != null) {
+                array[i][1] = value;
+                return false;
+            }
+            if (array[i][0] != null) {
+                if (array[i][0].equals(key)) {
                     array[i][1] = value;
                     return false;
-                }
-            }
-        } else {
-            for (int i = 0; i < array.length; i++) {
-                if (array[i][0] != null && array[i][1] != null) {
-                    if (array[i][0].equals(key)) {
-                        array[i][1] = value;
-                        return false;
-                    }
                 }
             }
         }
