5b1ce74eda898872e45ee4dbd03c993d102b3e73
Dima Petrushenko <dmytro.petrushenko@gmail.com>
2021-08-19 13:07:11 +0300

    last notices was fixed(put and check)

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -9,20 +9,17 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         if (checkStorage(key, value)) {
-            addNew(key, value);
+            array[size][0] = key;
+            array[size][1] = value;
+            size++;
         }
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (key == null && array[i][0] == null && array[i][1] != null) {
+            if (key == array[i][0] || key != null && key.equals(array[i][0])) {
                 return (V) array[i][1];
-            }
-            if (array[i][0] != null) {
-                if (array[i][0].equals(key)) {
-                    return (V) array[i][1];
-                }
             }
         }
         return null;
@@ -31,17 +28,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return this.size;
-    }
-
-    private void addNew(K key, V value) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i][0] == null && array[i][1] == null) {
-                array[i][0] = key;
-                array[i][1] = value;
-                size++;
-                break;
-            }
-        }
     }
 
     private boolean checkStorage(K key, V value) {
