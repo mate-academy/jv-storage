02435d5dbdedaf077f15ca5612b4e24a95832836
Sinelife <ya.serzhan@gmail.com>
2021-08-21 02:16:50 +0300

    add realisation of get, put, size methods to class StorageImpl. add Box class for StorageImpl realization.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,81 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private int size = 0;
+    private Box<K, V>[] boxes;
+
+    public StorageImpl() {
+        boxes = new Box[MAX_ITEMS_NUMBER];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (isEmpty()) {
+            putElementWithNotExistingKey(key, value);
+        } else {
+            if (containsKey(key)) {
+                putElementWithExistingKey(key, value);
+            } else {
+                putElementWithNotExistingKey(key, value);
+            }
+        }
+    }
+
+    private void putElementWithExistingKey(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(key, boxes[i].key)) {
+                boxes[i].setValue(value);
+            }
+        }
+    }
+
+    private void putElementWithNotExistingKey(K key, V value) {
+        boxes[size++] = new Box<>(key, value);
+    }
+
+    private boolean containsKey(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(key, boxes[i].key)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(key, boxes[i].key)) {
+                return boxes[i].value;
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    static class Box<K, V> {
+        private K key;
+        private V value;
+
+        public Box(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
     }
 }
