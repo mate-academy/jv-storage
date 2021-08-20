003a81663568079ef92011a7db13acf69b8b2c73
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2021-08-20 12:16:43 +0300

    deleted complicated methods, made check for available slot inside put method. added size variable

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,22 +6,31 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ARRAY_LENGTH = 10;
     private K[] keys;
     private V[] values;
+    private int size;
 
     public StorageImpl() {
         keys = (K[]) new Object[MAX_ARRAY_LENGTH];
         values = (V[]) new Object[MAX_ARRAY_LENGTH];
+        size = 0;
     }
 
     @Override
     public void put(K key, V value) {
-        keys[indexOfSuitableSlot(key)] = key;
-        values[indexOfSuitableSlot(key)] = value;
+        for (int i = 0; i < size; i++) {
+            if (equalsCheck(keys[i], key)) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < MAX_ARRAY_LENGTH; i++) {
-            if (equalsCheck(keys[i], values[i], key)) {
+            if (equalsCheck(keys[i], key)) {
                 return values[i];
             }
         }
@@ -30,24 +39,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        for (int i = 0; i < MAX_ARRAY_LENGTH; i++) {
-            if (values[i] == null) {
-                return i;
-            }
-        }
-        return 0;
+        return size;
     }
 
-    private int indexOfSuitableSlot(K key) {
-        for (int i = 0; i < MAX_ARRAY_LENGTH; i++) {
-            if (equalsCheck(keys[i], values[i], key) || values[i] == null) {
-                return i;
-            }
-        }
-        return 0;
-    }
-
-    private boolean equalsCheck(K key, V value, K keyToCompare) {
+    private boolean equalsCheck(K key, K keyToCompare) {
         return (keyToCompare == key) || (key != null && key.equals(keyToCompare));
     }
 }
