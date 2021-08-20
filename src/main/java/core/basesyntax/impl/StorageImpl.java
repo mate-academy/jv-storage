3876177efbc5a6975f1cf43e2d2de4dc65f4e647
Viktor Yukhno <pingvinomania@gmail.com>
2021-08-20 11:45:17 +0300

    added fix after mentor review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,17 +6,17 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ARRAY_SIZE = 10;
     private K[] keys;
     private V[] values;
-    private int sizeOfStorage = 0;
+    private int sizeOfStorage;
 
     public StorageImpl() {
-        this.keys = (K[]) new Object[MAX_ARRAY_SIZE];
-        this.values = (V[]) new Object[MAX_ARRAY_SIZE];
+        keys = (K[]) new Object[MAX_ARRAY_SIZE];
+        values = (V[]) new Object[MAX_ARRAY_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
         if (sizeOfStorage >= MAX_ARRAY_SIZE) {
-            throw new ArrayIndexOutOfBoundsException("Adding isn't possible. The storage is full.");
+            throw new RuntimeException("Adding isn't possible. The storage is full.");
         }
         for (int i = 0; i < sizeOfStorage; i++) {
             if ((keys[i] == key) || (key != null && key.equals(keys[i]))) {
