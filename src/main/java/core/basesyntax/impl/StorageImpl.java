79a43e8651ab0b7c87fc3522110357295a696834
Roman Hrechanyi <grechoroma@gmail.com>
2021-08-22 11:27:29 +0300

    My homework jv-storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,51 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private K[] keysArray;
+    private V[] valuesArray;
+    private int sizeOfArray;
+
+    public StorageImpl() {
+        keysArray = (K[])(new Object[MAX_ITEMS_NUMBER]);
+        valuesArray = (V[])(new Object[MAX_ITEMS_NUMBER]);
+        sizeOfArray = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        if (sizeOfArray < MAX_ITEMS_NUMBER) {
+            for (int i = 0; i < sizeOfArray; i++) {
+                if (Objects.equals(key, keysArray[i])) {
+                    valuesArray[i] = value;
+                    return;
+                }
+            }
+            keysArray[sizeOfArray] = key;
+            valuesArray[sizeOfArray] = value;
+            sizeOfArray++;
+        } else {
+            System.out.println("Your storage is full!");
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        V result = null;
+        for (int i = 0; i < sizeOfArray; i++) {
+            if (Objects.equals(key, keysArray[i])) {
+                result = valuesArray[i];
+                break;
+            }
+        }
+        return result;
     }
 
     @Override
     public int size() {
-        return -1;
+        return sizeOfArray;
     }
 }
