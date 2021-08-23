9e949ac9cc4e7d48d702efaa6462fd547eaf0eb6
Yehor Zapara <egor.zapara1994@gmail.com>
2021-08-23 23:29:03 +0300

    StorageImpl was fixed

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,50 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE_OF_ARRAY = 10;
+    private int sizeOfArray;
+    private final K[] arrayOfKeys;
+    private final V[] arrayOfValues;
+
+    public StorageImpl() {
+        arrayOfKeys = (K[]) new Object[MAX_SIZE_OF_ARRAY];
+        arrayOfValues = (V[]) new Object[MAX_SIZE_OF_ARRAY];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < MAX_SIZE_OF_ARRAY; i++) {
+            if (i == sizeOfArray) {
+                arrayOfKeys[sizeOfArray] = key;
+                arrayOfValues[sizeOfArray] = value;
+                sizeOfArray++;
+                return;
+            }
+            if (isEquals(key, arrayOfKeys[i])) {
+                arrayOfKeys[i] = key;
+                arrayOfValues[i] = value;
+                return;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < arrayOfKeys.length; i++) {
+            if (isEquals(key, arrayOfKeys[i])) {
+                return arrayOfValues[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return sizeOfArray;
+    }
+
+    private boolean isEquals(K inputKey, K keyInArray) {
+        return inputKey != null && inputKey.equals(keyInArray)
+                || inputKey == keyInArray;
     }
 }
