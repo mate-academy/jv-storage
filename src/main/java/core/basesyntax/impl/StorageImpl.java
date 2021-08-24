1c73475cf039a10efaf2deb98d76f9d3313f9d49
vadim9482 <vadim9482@gmail.com>
2021-08-25 00:21:36 +0300

    First commit, were created methods put and get, some problems with NPE

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,18 +2,42 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.Arrays;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE = 10;
+    Object keys[];
+    Object values[];
+    int putCount;
+
+    public StorageImpl() {
+        keys = new Object[MAX_SIZE];
+        values = new Object[MAX_SIZE];
+        putCount = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        if (value != null && !Arrays.asList(keys).contains(key)) {
+            keys[putCount] = key;
+            values[putCount] = value;
+            putCount++;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        V value = null;
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i].equals(key)) {
+                value = (V) values[i];
+            }
+        }
+        return value;
     }
 
     @Override
     public int size() {
-        return -1;
+        return putCount;
     }
 }
