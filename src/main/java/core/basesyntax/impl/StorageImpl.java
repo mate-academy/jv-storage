26ae18b6976f252ac3d17acbf2e6e415b6588411
Kostia Dementiev <konstde00@gmail.com>
2021-08-25 11:42:31 +0300

    fixed getIndex method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -61,7 +61,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     private int getIndex(K key) {
         for (int i = 0; i < size; i++) {
-            if (key != null && key.equals(keys[i]) || (key == keys[i])) {
+            if ((key == keys[i]) || key != null && key.equals(keys[i])) {
                 return i;
             }
         }
