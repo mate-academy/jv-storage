70e03b1bcd646def12ad70ff5a13f64c8af37823
mishamolnar <michaellmuller423@gmail.com>
2021-08-25 12:18:34 +0300

    LENGTH_TO_INDEX and INDEX_FOUND  removed

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,8 +4,6 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int NO_KEY_FOUND = -1;
-    private static final int KEY_FOUND = 0;
-    private static final int LENGTH_TO_INDEX = 1;
     private K[] keys = (K[]) new Object[10];
     private V[] values = (V[]) new Object[10];
     private int size = 0;
@@ -13,17 +11,17 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         if (getIndexOfKey(key) == NO_KEY_FOUND) {
+            keys[size] = key;
+            values[size] = value;
             size++;
-            keys[size - LENGTH_TO_INDEX] = key;
-            values[size - LENGTH_TO_INDEX] = value;
-        } else if (getIndexOfKey(key) >= KEY_FOUND) {
+        } else if (getIndexOfKey(key) >= 0) {
             values[getIndexOfKey(key)] = value;
         }
     }
 
     @Override
     public V get(K key) {
-        return (getIndexOfKey(key) >= KEY_FOUND) ? values[getIndexOfKey(key)] : null;
+        return (getIndexOfKey(key) >= 0) ? values[getIndexOfKey(key)] : null;
     }
 
     @Override
@@ -38,11 +36,11 @@ public class StorageImpl<K, V> implements Storage<K, V> {
                     return i;
                 }
             }
-            return NO_KEY_FOUND;
-        }
-        for (int i = 0; i < size; i++) {
-            if (key.equals(keys[i])) {
-                return i;
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (key.equals(keys[i])) {
+                    return i;
+                }
             }
         }
         return NO_KEY_FOUND;
