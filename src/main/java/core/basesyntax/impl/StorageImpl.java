990fdcea3b09f37916f9e5e330c5b40569fa2374
Stas Shepet <milka33349@gmail.com>
2021-08-26 23:16:08 +0300

    Storage implemented

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,45 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_NUMBER_OF_ELEMENTS = 10;
+    private K[] arrayWithKeys;
+    private V[] arrayWithValue;
+    private int size;
+
+    public StorageImpl() {
+        arrayWithKeys = (K[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+        arrayWithValue = (V[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (keysEquals(arrayWithKeys[i], key)) {
+                arrayWithValue[i] = value;
+                return;
+            }
+        }
+        arrayWithKeys[size] = key;
+        arrayWithValue[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keysEquals(arrayWithKeys[i], key)) {
+                return arrayWithValue[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    private boolean keysEquals(K firstKey, K secondKey) {
+        return firstKey == secondKey || firstKey != null && firstKey.equals(secondKey);
     }
 }
