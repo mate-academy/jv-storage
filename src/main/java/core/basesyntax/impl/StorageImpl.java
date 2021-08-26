2ef61b292afd0ddc23703be74b2c939e38b503de
Serhii Blakh <s.a.blakh@ukr.net>
2021-08-26 12:12:50 +0300

    added suggested change and checking size in method put()

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,39 +5,40 @@ import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
-    private Object[] keyStorage = new Object[MAX_SIZE];
-    private Object[] valueStorage = new Object[MAX_SIZE];
-    private int counter = 0;
+    private K[] keys = (K[]) new Object[MAX_SIZE];
+    private V[] values = (V[]) new Object[MAX_SIZE];
+    private int size = 0;
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < counter; i++) {
-            if (Objects.equals(key, keyStorage[i])) {
-                valueStorage[i] = value;
+        if (size >= MAX_SIZE) {
+            throw new IndexOutOfBoundsException("We can't put data!"
+                    + " We can put only 10 pairs to our storage");
+        }
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(key, keys[i])) {
+                values[i] = value;
                 return;
             }
         }
-        keyStorage[counter] = key;
-        valueStorage[counter] = value;
-        counter++;
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < counter; i++) {
-            if (key == null && keyStorage[i] == null) {
-                return (V) valueStorage[i];
-            }
-            if (key != null && keyStorage[i] != null && key.equals(keyStorage[i])) {
-                return (V) valueStorage[i];
+        for (int i = 0; i < size; i++) {
+            if (key == null && keys[i] == null
+                    || (key != null && keys[i] != null && key.equals(keys[i]))) {
+                return (V) values[i];
             }
         }
-        counter++;
         return null;
     }
 
     @Override
     public int size() {
-        return counter;
+        return size;
     }
 }
