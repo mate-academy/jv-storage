2426310a55c1dcf3cbe4a7493d20c8f3e68d933b
vadim9482 <vadim9482@gmail.com>
2021-08-26 20:23:28 +0300

    Change searching algorythm, ternar operators were appended

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,42 +2,42 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
-import java.util.Arrays;
-
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
-    Object keys[];
-    Object values[];
-    int putCount;
+    private Object[] keys;
+    private Object[] values;
+    private int quantity;
 
     public StorageImpl() {
         keys = new Object[MAX_SIZE];
         values = new Object[MAX_SIZE];
-        putCount = 0;
     }
 
     @Override
     public void put(K key, V value) {
-        if (value != null && !Arrays.asList(keys).contains(key)) {
-            keys[putCount] = key;
-            values[putCount] = value;
-            putCount++;
+        for (int i = 0; i < quantity; i++) {
+            if (key == null ? keys[i] == null : key.equals(keys[i])) {
+                values[i] = value;
+                return;
+            }
         }
+        keys[quantity] = key;
+        values[quantity] = value;
+        quantity++;
     }
 
     @Override
     public V get(K key) {
-        V value = null;
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i].equals(key)) {
-                value = (V) values[i];
+        for (int i = 0; i < quantity; i++) {
+            if (key == null ? keys[i] == null : key.equals(keys[i])) {
+                return (V) values[i];
             }
         }
-        return value;
+        return null;
     }
 
     @Override
     public int size() {
-        return putCount;
+        return quantity;
     }
 }
