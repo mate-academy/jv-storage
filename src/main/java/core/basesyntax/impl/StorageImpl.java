d96873eea37c75741bc0b1742b24f586c73d973a
Andrii Demchenko <avd998ne@gmail.com>
2021-08-26 23:30:44 +0300

    added jv-storage solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,47 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private K[] keys;
+    private V[] values;
+    private int storageSize;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_ITEMS_NUMBER];
+        values = (V[]) new Object[MAX_ITEMS_NUMBER];
+        storageSize = 0;
+    }
+
+    private boolean areEqual(K existingKey, K givenKey) {
+        return existingKey == givenKey || (existingKey != null
+                && existingKey.equals(givenKey));
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < storageSize; i++) {
+            if (areEqual(keys[i], key)) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[storageSize] = key;
+        values[storageSize] = value;
+        storageSize++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < storageSize; i++) {
+            if (areEqual(keys[i], key)) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return storageSize;
     }
 }
