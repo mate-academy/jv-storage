168aa76a968502108376c5a3747f24d12179e674
vitalii_yelinskyi <yelinskyizp@gmail.com>
2021-08-26 09:32:55 +0300

    were renamed values; added the check size > array.length

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,38 +3,41 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int FIRST_NUMBER_FOR_ARRAY = 10;
-    private static final int SECOND_NUMBER_FOR_ARRAY = 2;
-    private static final int POSITION_KEY = 0;
-    private static final int POSITION_VALUE = 1;
-    private int counter;
+    private static final int SIZE_FIRST_ARRAY = 10;
+    private static final int SIZE_SECOND_ARRAY = 2;
+    private static final int KEY_POSITION = 0;
+    private static final int VALUE_POSITION = 1;
+    private int size;
     private final Object[][] arrayStorage;
 
     public StorageImpl() {
         arrayStorage =
-                new Object[FIRST_NUMBER_FOR_ARRAY][SECOND_NUMBER_FOR_ARRAY];
+                new Object[SIZE_FIRST_ARRAY][SIZE_SECOND_ARRAY];
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < counter; i++) {
-            if ((arrayStorage[i][POSITION_KEY] == key)
-                    || (key != null && key.equals(arrayStorage[i][POSITION_KEY]))) {
-                arrayStorage[i][POSITION_VALUE] = value;
+        if (size > SIZE_FIRST_ARRAY) {
+            throw new RuntimeException("Array is full");
+        }
+        for (int i = 0; i < size; i++) {
+            if ((arrayStorage[i][KEY_POSITION] == key)
+                    || (key != null && key.equals(arrayStorage[i][KEY_POSITION]))) {
+                arrayStorage[i][VALUE_POSITION] = value;
                 return;
             }
         }
-        arrayStorage[counter][POSITION_KEY] = key;
-        arrayStorage[counter][POSITION_VALUE] = value;
-        counter++;
+        arrayStorage[size][KEY_POSITION] = key;
+        arrayStorage[size][VALUE_POSITION] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < counter; i++) {
-            if (key == arrayStorage[i][POSITION_KEY]
-                    || (key != null && key.equals(arrayStorage[i][POSITION_KEY]))) {
-                return (V) arrayStorage[i][POSITION_VALUE];
+        for (int i = 0; i < size; i++) {
+            if (key == arrayStorage[i][KEY_POSITION]
+                    || (key != null && key.equals(arrayStorage[i][KEY_POSITION]))) {
+                return (V) arrayStorage[i][VALUE_POSITION];
             }
         }
         return null;
@@ -42,6 +45,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return counter;
+        return size;
     }
 }
