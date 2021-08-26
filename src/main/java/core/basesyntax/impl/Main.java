898c1167ba2d8130c8290c55e539a294e5612486
Yuliia Mysova-Shreder <yuliiamysova@gmail.com>
2021-08-26 16:51:06 +0300

    StorageImpl class was created

---
+++ src/main/java/core/basesyntax/impl/Main.java
@@ -0,0 +1,18 @@
+/*package core.basesyntax.impl;
+
+import core.basesyntax.Storage;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Storage<Integer, Box> storage = new StorageImpl<>();
+        Box box = new Box(1);
+        storage.put(22, box);
+        Box value = storage.get(22); // returns the Box
+        int size = storage.size();
+    }
+}
+*/
+
+
