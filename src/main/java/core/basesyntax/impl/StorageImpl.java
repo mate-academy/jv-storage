98d2de2fe4fa8884fbab803e7cd4fb458a373218
Roman Novikov <novikovr121281@gmail.com>
2021-08-27 16:15:54 +0300

    Home work is done

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,62 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private int counter = 0;
+    private int size = 0;
+    private Entry<K, V>[] kyeValues = new Entry[0];
+    private Entry<K, V>[] kyeValuesCopy;
+
     @Override
     public void put(K key, V value) {
+        if (size < MAX_ITEMS_NUMBER) {
+            if (counter == 0) {
+                fillInKyeValues(key, value);
+                return;
+            }
+            for (int i = 0; i < kyeValues.length; i++) {
+                if (key == null && key == kyeValues[i].getKey()) {
+                    fillInKyeValues(key, value, i);
+                    return;
+                } else if (key != null && key.equals(kyeValues[i].getKey())) {
+                    fillInKyeValues(key, value, i);
+                    return;
+                }
+            }
+            fillInKyeValues(key, value);
+        }
+    }
+
+    private void fillInKyeValues(K key, V value) {
+        kyeValuesCopy = Arrays.copyOf(kyeValues, ++size);
+        kyeValuesCopy[counter] = new Entry<>(key, value);
+        kyeValues = kyeValuesCopy;
+        counter++;
+    }
+
+    private void fillInKyeValues(K key, V value, int iter) {
+        kyeValuesCopy = Arrays.copyOf(kyeValues, size);
+        kyeValuesCopy[iter] = new Entry<>(key, value);
+        kyeValues = kyeValuesCopy;
     }
 
     @Override
     public V get(K key) {
+        for (Entry en : kyeValues) {
+            if (!(en.getKey() == null) && (en.getKey().equals(key))) {
+                return (V) en.getValue();
+            } else if (en.getKey() == null && key == null) {
+                return (V) en.getValue();
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
