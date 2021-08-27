d996126dd7770ceb02b51d29fbfbbe2c5bb71d79
Stas Shepet <milka33349@gmail.com>
2021-08-27 11:07:05 +0300

    redunant prefixs removed, added if for check size

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,33 +4,35 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_NUMBER_OF_ELEMENTS = 10;
-    private K[] arrayWithKeys;
-    private V[] arrayWithValue;
+    private K[] keys;
+    private V[] values;
     private int size;
 
     public StorageImpl() {
-        arrayWithKeys = (K[]) new Object[MAX_NUMBER_OF_ELEMENTS];
-        arrayWithValue = (V[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+        keys = (K[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+        values = (V[]) new Object[MAX_NUMBER_OF_ELEMENTS];
     }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (keysEquals(arrayWithKeys[i], key)) {
-                arrayWithValue[i] = value;
+            if (keysEquals(keys[i], key)) {
+                values[i] = value;
                 return;
             }
         }
-        arrayWithKeys[size] = key;
-        arrayWithValue[size] = value;
-        size++;
+        if (size <= MAX_NUMBER_OF_ELEMENTS) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (keysEquals(arrayWithKeys[i], key)) {
-                return arrayWithValue[i];
+            if (keysEquals(keys[i], key)) {
+                return values[i];
             }
         }
         return null;
