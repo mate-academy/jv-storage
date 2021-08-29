a05b0f947d59e05ddad4d4e74a870b2b824b4a6a
Gleb Kreshchuk <gleb.net@gmail.com>
2021-08-29 23:51:28 +0300

    jv-storage task completed according requirements

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,42 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS = 10;
+    private int index = 0;
+    private K[] keys = (K[]) new Object[MAX_ITEMS];
+    private V[] values = (V[]) new Object[MAX_ITEMS];
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < index; i++) {
+            if (checkIfEqual(key, keys[i])) {
+                values[i] = value;
+                return;
+            }
+        }
+        values[index] = value;
+        keys[index] = key;
+        index++;
     }
 
     @Override
     public V get(K key) {
+        int localIndex = 0;
+        for (K localKey : keys) {
+            if (checkIfEqual(key, localKey)) {
+                return values[localIndex];
+            }
+            localIndex++;
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return index;
+    }
+
+    private boolean checkIfEqual(K newKey, K localKey) {
+        return (localKey == newKey || localKey != null && localKey.equals(newKey));
     }
 }
