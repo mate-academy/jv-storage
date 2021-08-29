5424eb0cde7a270bbe85b65dbf0097d4280fd1b4
Rostyslav Fostyk <rostyslavfostykjob@gmail.com>
2021-08-29 15:03:42 +0300

    implemented keyEquals method and constructor

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,18 +1,22 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ARRAY_SIZE = 10;
-    private K[] keys = (K[]) new Object[MAX_ARRAY_SIZE];
-    private V[] values = (V[]) new Object[MAX_ARRAY_SIZE];
+    private K[] keys;
+    private V[] values;
     private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_ARRAY_SIZE];
+        values = (V[]) new Object[MAX_ARRAY_SIZE];
+    }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(key, keys[i])) {
+            if (keyEquals(key, keys[i])) {
                 values[i] = value;
                 return;
             }
@@ -25,7 +29,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(key, keys[i])) {
+            if (keyEquals(key, keys[i])) {
                 return values[i];
             }
         }
@@ -35,5 +39,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return size;
+    }
+
+    private boolean keyEquals(K key, K keys) {
+        return key == keys || (key != null && key.equals(keys));
     }
 }
