f784931117113dfea3067dc2e5139635c7d8456e
taras-oleksiuk <taras.oleksiuk@gmail.com>
2021-08-30 15:45:42 +0300

    added solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,47 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_NUMBER_OF_ELEMENTS = 10;
+    private static int actualNumberOfElements;
+    private final K[] arrayKeys;
+    private final V[] arrayValues;
+
+    public StorageImpl() {
+        //noinspection unchecked
+        arrayKeys = (K[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+        //noinspection unchecked
+        arrayValues = (V[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+        actualNumberOfElements = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < actualNumberOfElements; i++) {
+            if (Objects.equals(key, arrayKeys[i])) {
+                arrayValues[i] = value;
+                return;
+            }
+        }
+        arrayKeys[actualNumberOfElements] = key;
+        arrayValues[actualNumberOfElements] = value;
+        actualNumberOfElements++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < actualNumberOfElements; i++) {
+            if (Objects.equals(key, arrayKeys[i])) {
+                return arrayValues[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return actualNumberOfElements;
     }
 }
