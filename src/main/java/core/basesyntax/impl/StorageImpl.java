d157e4d4546c1e55d9bba24f3f41223aa4f45190
Roman Radikhovskiy <radihovskiyroman@gmail.com>
2021-08-30 12:24:05 +0300

    added throwing an Exception

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,16 +3,16 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private K[] keys = (K[]) new Object[10];
-    private V[] values = (V[]) new Object[10];
+    private static final int MAX = 10;
+    private K[] keys = (K[]) new Object[MAX];
+    private V[] values = (V[]) new Object[MAX];
     private int size;
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (size == 10) {
-                System.out.println("Invalid array. Array must be less than 10");
-                return;
+            if (size == MAX) {
+                throw new RuntimeException("Invalid array. Array must be less than 10");
             }
             if ((keys[i] == key) || (keys[i] != null && keys[i].equals(key))) {
                 values[i] = value;
