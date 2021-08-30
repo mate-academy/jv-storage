3c618f703fd2152143450828b0a5da5546b004cc
Dmytro Heleta <dmytroheleta@gmail.com>
2021-08-30 13:51:08 +0300

    implement storage class

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,53 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_STORAGE_SIZE = 9;
+    private K[] keys;
+    private V[] values;
+    private int currentStorageSize = 0;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_STORAGE_SIZE];
+        values = (V[]) new Object[MAX_STORAGE_SIZE];
+    }
+
+    private void increaseStorageSize() {
+        currentStorageSize++;
+    }
+
+    public int getCurrentStorageSize() {
+        return currentStorageSize;
+    }
+
+    private boolean isKeyAlreadyPresentInStorage(K newKey, K currentKey) {
+        return (currentKey == newKey || (currentKey != null && currentKey.equals(newKey)));
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < getCurrentStorageSize(); i++) {
+            if (isKeyAlreadyPresentInStorage(key, keys[i])) {
+                values[i] = value;
+                return;
+            }
+        }
+        values[getCurrentStorageSize()] = value;
+        keys[getCurrentStorageSize()] = key;
+        increaseStorageSize();
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < getCurrentStorageSize(); i++) {
+            if (isKeyAlreadyPresentInStorage(key,keys[i])) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return getCurrentStorageSize();
     }
 }
