d22b9bed90b7bdb88595bf4682206a368cf52ef8
The Reaper <dimonnumber1@gmail.com>
2021-08-30 10:45:59 +0300

    Changes applied

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,24 +4,31 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int NO_KEY_FOUND = -1;
-    private K[] keys = (K[]) new Object[10];
-    private V[] values = (V[]) new Object[10];
+    private K[] keys;
+    private V[] values;
     private int size = 0;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[10];
+        values = (V[]) new Object[10];
+    }
 
     @Override
     public void put(K key, V value) {
-        if (getIndexOfKey(key) == NO_KEY_FOUND) {
+        int keyIndex = getIndexOfKey(key);
+        if (keyIndex == NO_KEY_FOUND) {
             keys[size] = key;
             values[size] = value;
             size++;
-        } else if (getIndexOfKey(key) >= 0) {
-            values[getIndexOfKey(key)] = value;
+        } else if (keyIndex >= 0) {
+            values[keyIndex] = value;
         }
     }
 
     @Override
     public V get(K key) {
-        return (getIndexOfKey(key) >= 0) ? values[getIndexOfKey(key)] : null;
+        int keyIndex = getIndexOfKey(key);
+        return (keyIndex >= 0) ? values[keyIndex] : null;
     }
 
     @Override
