1226f1595212c2f125840d55966426620d9ec303
Denys Smei <denys.smei@artcodevision.com>
2021-08-30 16:45:19 +0300

    indexOfKey(K key) remark

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -42,7 +42,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     private int indexOfKey(K key) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] == null && key == null
+            if ((keys[i] == key)
                     || ((keys[i] != null) && keys[i].equals(key))) {
                 return i;
             }
