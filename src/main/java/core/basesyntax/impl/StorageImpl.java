4f063695a2e460ddca3432f8e4242d7562647fdf
Roman Novikov <novikovr121281@gmail.com>
2021-09-03 19:01:04 +0300

    changed the filling of the method "put()"

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,49 +1,38 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int MAX_ITEMS_NUMBER = 10;
+
     private int counter = 0;
-    private int size = 0;
-    private Entry<K, V>[] kyeValues = new Entry[0];
-    private Entry<K, V>[] kyeValuesCopy;
+    private int size = 1;
+    private Entry<K, V>[] kyeValues = new Entry[10];
 
     @Override
     public void put(K key, V value) {
-        if (counter == 0) {
-            fillInKyeValues(key, value);
-            return;
-        }
         for (int i = 0; i < size; i++) {
-            if (key == null && key == kyeValues[i].getKey()) {
-                fillInKyeValues(key, value, i);
-                return;
-            } else if (key != null && key.equals(kyeValues[i].getKey())) {
-                fillInKyeValues(key, value, i);
+            if ((kyeValues[i] == null)
+                    || (key == null && kyeValues[i].getKey() == null)
+                    || (key != null && kyeValues[i].getKey() != null
+                    && kyeValues[i].getKey().equals(key))) {
+                if (kyeValues[i] == null) {
+                    counter++;
+                }
+                kyeValues[i] = new Entry<>(key, value);
                 return;
             }
         }
-        fillInKyeValues(key, value);
-    }
-
-    private void fillInKyeValues(K key, V value) {
-        kyeValuesCopy = Arrays.copyOf(kyeValues, ++size);
-        kyeValuesCopy[counter] = new Entry<>(key, value);
-        kyeValues = kyeValuesCopy;
+        size++;
+        kyeValues[counter] = new Entry<>(key, value);
         counter++;
-    }
-
-    private void fillInKyeValues(K key, V value, int iter) {
-        kyeValuesCopy = Arrays.copyOf(kyeValues, size);
-        kyeValuesCopy[iter] = new Entry<>(key, value);
-        kyeValues = kyeValuesCopy;
     }
 
     @Override
     public V get(K key) {
         for (Entry en : kyeValues) {
+            if (en == null) {
+                return null;
+            }
             if ((en.getKey() == null && key == null)
                     || ((key != null && en.getKey() != null)
                     && en.getKey().equals(key))) {
@@ -55,6 +44,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return size;
+        return counter;
     }
 }
