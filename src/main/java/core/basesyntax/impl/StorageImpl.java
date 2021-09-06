1ec4b374464d638a8e965c3f297954bacf9d84a4
Alex Hazniuk <ohazniuk@gmail.com>
2021-09-06 15:50:18 +0300

    implemented task - storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,47 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ELEMENTS_NUMBER = 10;
+    private K[] elementsK;
+    private V[] elementsV;
+
+    public StorageImpl() {
+        elementsK = (K[]) new Object[MAX_ELEMENTS_NUMBER];
+        elementsV = (V[]) new Object[MAX_ELEMENTS_NUMBER];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < MAX_ELEMENTS_NUMBER; i++) {
+            if (key == null && elementsK[i] == null
+                    || key != null && key.equals(elementsK[i])
+                    || elementsK[i] == null && elementsV[i] == null) {
+                elementsK[i] = key;
+                elementsV[i] = value;
+                break;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < MAX_ELEMENTS_NUMBER; i++) {
+            if (key == null && elementsK[i] == null
+                    || key != null && key.equals(elementsK[i])) {
+                return elementsV[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        int size = 0;
+        for (int i = 0; i < MAX_ELEMENTS_NUMBER; i++) {
+            if (elementsV[i] != null) {
+                size++;
+            }
+        }
+        return size;
     }
 }
