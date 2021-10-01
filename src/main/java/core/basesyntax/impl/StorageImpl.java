48ab6cf107962db79f2bd67c0f2ea7fd07acc536
Mykola Skrypka <mykolaskrypka@gmail.com>
2021-10-01 08:20:04 +0300

    created class KeyValue for storing data in array, implemented methods get, put, size for working with storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,52 @@
 package core.basesyntax.impl;
 
+import core.basesyntax.KeyValue;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private final Integer storageSize = 10;
+    private final KeyValue<K,V>[] storage = new KeyValue[storageSize];
+
     @Override
     public void put(K key, V value) {
+        for (int ind = 0; ind < storageSize; ind++) {
+            if (storage[ind] == null) {
+                KeyValue<K, V> newKeyValue = new KeyValue<>(key, value);
+                storage[ind] = newKeyValue;
+                break;
+            } else if (storage[ind].getKey() == key ||
+                    (storage[ind].getKey() != null && storage[ind].getKey().equals(key))) {
+                storage[ind].setValue(value);
+                break;
+            }
+        }
+
     }
 
     @Override
     public V get(K key) {
+        for (KeyValue<K, V> kv : storage) {
+            if (kv == null) {
+                continue;
+            }
+            if (kv.getKey() == key) {
+                return kv.getValue();
+            }
+            if (kv.getKey() != null && kv.getKey().equals(key)) {
+                return kv.getValue();
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        int size = 0;
+        for (KeyValue<K, V> kv : storage) {
+            if (kv != null) {
+                size++;
+            }
+        }
+        return size;
     }
 }
