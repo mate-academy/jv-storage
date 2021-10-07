85f59a25d833785e21c846d3e7f51718057823a2
Angela <angelamatiiva@outlook.com>
2021-10-07 11:15:09 +0300

    refactoring

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -15,20 +15,20 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public void put(K key, V value) {
         Entry<K, V> entry = new Entry<>(key, value);
         for (int i = 0; i < length; i++) {
-            if ((items[i].getKey() == null && key == null)
+            if ((items[i].getKey() == key)
                     || (items[i].getKey() != null && items[i].getKey().equals(key))) {
-                items[i] = entry; // если одинаковые объекты - обновляем(перезаписываем)
+                items[i] = entry;
                 return;
             }
         }
-        items[length] = entry; // если разные объекты записываем
+        items[length] = entry;
         length++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < length; i++) {
-            if ((items[i].getKey() == null && key == null)
+            if ((items[i].getKey() == key)
                     || (items[i].getKey() != null && items[i].getKey().equals(key))) {
                 return items[i].getValue();
             }
