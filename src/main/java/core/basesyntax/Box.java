9a427d318386ada8fd0654460dc46dec5af1c37e
Ruslan Malyavsky <ruslan-gorod@ukr.net>
2021-10-20 00:48:31 +0300

    Implemented StorageImpl class and created classes Box and Main

---
+++ src/main/java/core/basesyntax/Box.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public class Box {
+    String name;
+
+    public Box() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,18 +2,64 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
+
+    List<Element<K, V>> list = new ArrayList<>();
+
     @Override
     public void put(K key, V value) {
+        Element<K, V> element = findElement(key);
+        if (element != null) {
+            element.setValue(value);
+        } else {
+            list.add(new Element<>(key, value));
+        }
     }
 
     @Override
     public V get(K key) {
+        Element<K, V> element = findElement(key);
+        return element != null ? element.getValue() : null;
+    }
+
+    private Element<K, V> findElement(K key) {
+        for (Element<K, V> element : list) {
+            if (key == null && element.getKey() == null) {
+                return element;
+            } else if (element.getKey() != null && element.getKey().equals(key)) {
+                return element;
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return list.size();
+    }
+
+    static class Element<K, V> {
+        private final K key;
+        private V value;
+
+        public Element(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
     }
 }
