69a85767b064c443e335995f2b76ff8a3f2585d0
Anton Volskyi <antonvolskyi1234@gmail.com>
2021-10-26 18:41:29 +0300

    implemented all methods / all tests passed / mvn cp is success

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,44 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static int ELEMENTS_COUNT = 10;
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    {
+        keys = (K[]) new Object[ELEMENTS_COUNT];
+        values = (V[]) new Object[ELEMENTS_COUNT];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(keys[i], key)) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(key,keys[i])) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
