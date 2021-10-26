65f092742512ee3111979e118e5f56552b434220
nishchenkokateryna <kateryna.nishchenko@gmail.com>
2021-10-26 16:16:23 +0300

    Added implementation of methods

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,36 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private K[] keysStorage = (K[]) new Object[MAX_ITEMS_NUMBER];
+    private V[] valuesStorage = (V[]) new Object[MAX_ITEMS_NUMBER];
+    private int size;
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (keysStorage[i] == key || keysStorage[i] != null && keysStorage[i].equals(key)) {
+                valuesStorage[i] = value;
+                return;
+            }
+        }
+        keysStorage[size] = key;
+        valuesStorage[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keysStorage[i] == key || keysStorage[i] != null && keysStorage[i].equals(key)) {
+                return valuesStorage[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
