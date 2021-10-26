ff8cac8952d0a7536d176fe6dcd73058768fc231
Andrey Sidorkin <andrei.sidorkin@gmail.com>
2021-10-27 00:18:41 +0300

    All tests passed ok

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,59 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int FULLSIZE = 10;
+    private StorageImpl[] bigStorage = new StorageImpl[FULLSIZE];
+    private K key;
+    private V value;
+
+    public StorageImpl() {
+    }
+
+    public StorageImpl(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public StorageImpl(V value) {
+        this.value = value;
+    }
+
+    public static <K, V> StorageImpl<K, V> of(K key, V value) {
+        return new StorageImpl<>(key, value);
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < FULLSIZE; i++) {
+            if (bigStorage[i] == null || (bigStorage[i].key != null
+                    && bigStorage[i].key.equals(key))
+                    || bigStorage[i].key == null && bigStorage[i].key == key) {
+                bigStorage[i] = StorageImpl.of(key, value);
+                break;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < FULLSIZE; i++) {
+            if (bigStorage[i] != null && bigStorage[i].key != null
+                    && bigStorage[i].key.equals(key)) {
+                return (V) bigStorage[i].value;
+            } else if (bigStorage[i] != null && bigStorage[i].key == key) {
+                return (V) bigStorage[i].value;
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        for (int i = 0; i < FULLSIZE; i++) {
+            if (bigStorage[i] == null) {
+                return i;
+            }
+        }
+        return 0;
     }
 }
