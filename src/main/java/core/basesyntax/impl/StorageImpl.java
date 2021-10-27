b24641fb19dd10406188594b9f8cdcbcddbc5fab
Roman Polulikh <polilikh.r@gmail.com>
2021-10-27 13:32:49 +0300

    fixed reviewer's remarks

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -16,7 +16,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (equalKeys(keys[i], key)) {
+            if (areKeysEqual(keys[i], key)) {
                 values[i] = value;
                 return;
             }
@@ -29,7 +29,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (equalKeys(keys[i], key)) {
+            if (areKeysEqual(keys[i], key)) {
                 return values[i];
             }
         }
@@ -41,7 +41,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         return size;
     }
 
-    public boolean equalKeys(K firstKey, K secondKey) {
+    private boolean areKeysEqual(K firstKey, K secondKey) {
         return (firstKey == null ? secondKey == null : firstKey.equals(secondKey));
     }
 }
