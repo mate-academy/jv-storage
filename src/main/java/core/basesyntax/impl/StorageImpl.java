78f4e465b9d4e6d11a07c608b7c442da791dbbe6
Oleh Mazurkov <o.mazurkov@gmail.com>
2021-10-29 01:55:02 +0300

    solving hw-6 "storage"

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,46 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_NUMBER_STORAGE_ELEMENTS = 10;
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_NUMBER_STORAGE_ELEMENTS];
+        values = (V[]) new Object[MAX_NUMBER_STORAGE_ELEMENTS];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (size < MAX_NUMBER_STORAGE_ELEMENTS) {
+            for (int i = 0; i < size; i++) {
+                if ((key == keys[i]) || (key != null && key.equals(keys[i]))) {
+                    values[i] = value;
+                    return;
+                }
+            }
+            keys[size] = key;
+            values[size] = value;
+            size++;
+            return;
+        } else {
+            return;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if ((key == keys[i]) || (key != null && key.equals(keys[i]))) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
