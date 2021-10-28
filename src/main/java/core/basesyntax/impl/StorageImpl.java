9efc8b6c07004c8f05d8e83fc6f957000447279e
Dzmitry Kozel <dmitrokoz88@gmail.com>
2021-10-28 22:20:33 +0300

    static initialization black uzed redunant fields deleted if statements reduced

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,43 +4,23 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ITEMS_NUMBER = 10;
-    private K key;
-    private V value;
+    private static final Object[] arrayOfKeys;
+    private static final Object[] arrayOfValues;
+
+    static {
+        arrayOfKeys = new Object[MAX_ITEMS_NUMBER];
+        arrayOfValues = new Object[MAX_ITEMS_NUMBER];
+    }
+
     private int currentSize = 0;
-    private Object[] arrayOfKeys = new Object[MAX_ITEMS_NUMBER];
-    private Object[] arrayOfValues = new Object[MAX_ITEMS_NUMBER];
-
-    public K getKey() {
-        return key;
-    }
-
-    public void setKey(K key) {
-        this.key = key;
-    }
-
-    public V getValue() {
-        return value;
-    }
-
-    public void setValue(V value) {
-        this.value = value;
-    }
 
     @Override
     public void put(K key, V value) {
-        if (key == null) {
-            for (int i = 0; i < currentSize; i++) {
-                if (arrayOfKeys[i] == null) {
-                    arrayOfValues[i] = value;
-                    return;
-                }
-            }
-        } else {
-            for (int i = 0; i < currentSize; i++) {
-                if (key.equals(arrayOfKeys[i])) {
-                    arrayOfValues[i] = value;
-                    return;
-                }
+        for (int i = 0; i < currentSize; i++) {
+            if ((key == null && arrayOfKeys[i] == null)
+                    || (key != null && key.equals(arrayOfKeys[i]))) {
+                arrayOfValues[i] = value;
+                return;
             }
         }
         arrayOfKeys[currentSize] = key;
