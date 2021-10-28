248967b5a8e5849d5c430d3a0d7f75e756f4d469
KostyNikiforov <www.kosta.kon@gmail.com>
2021-10-28 15:53:07 +0300

    Generic class StorageImpl was implemented.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,45 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
+@SuppressWarnings("unchecked")
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE = 10;
+    private K[] keys = (K[]) new Object[MAX_SIZE];
+    private final V[] values = (V[]) new Object[MAX_SIZE];
+    private int size;
+
     @Override
     public void put(K key, V value) {
+        int keyNumber = getNumberIfExists(key);
+        if (keyNumber != -1) {
+            keys[keyNumber] = key;
+            values[keyNumber] = value;
+        } else {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int keyNumber = getNumberIfExists(key);
+        return keyNumber != -1 ? values[keyNumber] : null;
     }
 
     @Override
     public int size() {
+        return size;
+    }
+
+    private int getNumberIfExists(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(keys[i], key)) {
+                return i;
+            }
+        }
         return -1;
     }
 }
