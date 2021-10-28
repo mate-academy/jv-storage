41e8a1a61759d0551fe6965938653100f8e7c624
Andrey Sidorkin <andrei.sidorkin@gmail.com>
2021-10-28 21:28:05 +0300

    homework is rewritten completely according to review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,46 +4,30 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int FULLSIZE = 10;
-    private StorageImpl[] bigStorage = new StorageImpl[FULLSIZE];
-    private K key;
-    private V value;
-
-    public StorageImpl() {
-    }
-
-    public StorageImpl(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    public StorageImpl(V value) {
-        this.value = value;
-    }
-
-    public static <K, V> StorageImpl<K, V> of(K key, V value) {
-        return new StorageImpl<>(key, value);
-    }
+    private Object[] keys = new Object[FULLSIZE];
+    private Object[] values = new Object[FULLSIZE];
+    private int size = 0;
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < FULLSIZE; i++) {
-            if (bigStorage[i] == null || (bigStorage[i].key != null
-                    && bigStorage[i].key.equals(key))
-                    || bigStorage[i].key == null && bigStorage[i].key == key) {
-                bigStorage[i] = StorageImpl.of(key, value);
-                break;
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == null && key == null && values[i] != null
+                    || keys[i] != null && keys[i].equals(key)) {
+                values[i] = value;
+                return;
             }
         }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < FULLSIZE; i++) {
-            if (bigStorage[i] != null && bigStorage[i].key != null
-                    && bigStorage[i].key.equals(key)) {
-                return (V) bigStorage[i].value;
-            } else if (bigStorage[i] != null && bigStorage[i].key == key) {
-                return (V) bigStorage[i].value;
+        for (int i = 0; i < size; i++) {
+            if (keys[i] != null && key != null && keys[i].equals(key)
+                    || key == null && keys[i] == key) {
+                return (V) values[i];
             }
         }
         return null;
@@ -51,11 +35,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        for (int i = 0; i < FULLSIZE; i++) {
-            if (bigStorage[i] == null) {
-                return i;
-            }
-        }
-        return 0;
+        return size;
     }
 }
