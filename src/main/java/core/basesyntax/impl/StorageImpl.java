03e97931aeac54c45c429ac929a5a1755e5f9f09
Olha Tankevych <freakystyley85@gmail.com>
2021-10-28 22:20:19 +0300

    Implemented storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,50 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static int NUMBER_OF_OBJECT = 10;
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[NUMBER_OF_OBJECT];
+        values = (V[]) new Object[NUMBER_OF_OBJECT];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (get(key) == null) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        } else {
+            values[getIndex(key)] = value;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (((keys[i] != null) && (keys[i].equals(key)))
+                    || ((keys[i] == null) && (key == null))) {
+                return values[i];
+            }
+        }
         return null;
+    }
+
+    private int getIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (((keys[i] != null) && (keys[i].equals(key))) || ((keys[i] == null)
+                    && (key == null))) {
+                return i;
+            }
+        }
+        return 0;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
