8ea1eeb4475efaef7a51d6d56e1ce7c7b2d0ae6d
Olha Tankevych <freakystyley85@gmail.com>
2021-10-29 14:17:28 +0300

    added final to constant, changed put and get methods

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,7 +3,7 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static int NUMBER_OF_OBJECT = 10;
+    private static final int NUMBER_OF_OBJECT = 10;
     private K[] keys;
     private V[] values;
     private int size;
@@ -15,21 +15,20 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        if (get(key) == null) {
+        int index = getIndex(key);
+        if (index >= 0) {
+            values[getIndex(key)] = value;
+        } else {
             keys[size] = key;
             values[size] = value;
             size++;
-        } else {
-            values[getIndex(key)] = value;
         }
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < size; i++) {
-            if (keys[i] != null && keys[i].equals(key) || keys[i] == key) {
-                return values[i];
-            }
+        if (getIndex(key) != -1) {
+            return values[getIndex(key)];
         }
         return null;
     }
