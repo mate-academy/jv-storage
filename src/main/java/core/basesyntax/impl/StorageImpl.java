50c888bc5110fa2e9774e3fed7143ea5c1549107
Tyvoniuk Dmytro <dmytro.tyvoniuk@gmail.com>
2021-10-29 13:23:03 +0300

    jv - storage, with fixes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -17,10 +17,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         for (int i = 0; i < size; i++) {
             if (products[i].key == key
                     || products[i].key != null && products[i].key.equals(key)) {
-                if (products[i].value == value
-                        || products[i].value != null && products[i].value.equals(value)) {
-                    return;
-                }
                 products[i].value = value;
                 return;
             }
