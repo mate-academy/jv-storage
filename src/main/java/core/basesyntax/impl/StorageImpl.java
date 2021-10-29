5141a3e5e940297f34a8dc988a858c6fade6b969
Dzmitry Kozel <dmitrokoz88@gmail.com>
2021-10-29 13:44:21 +0300

    key == arrayOfKeys[i] || key != null && key.equals(arrayOfKeys[i])

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -16,8 +16,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < currentSize; i++) {
-            if (key == null && arrayOfKeys[i] == null
-                    || key != null && key.equals(arrayOfKeys[i])) {
+            if (key == arrayOfKeys[i] || key != null && key.equals(arrayOfKeys[i])) {
                 arrayOfValues[i] = value;
                 return;
             }
@@ -30,8 +29,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < currentSize; i++) {
-            if (key == null && arrayOfKeys[i] == null
-                    || key != null && key.equals(arrayOfKeys[i])) {
+            if (key == arrayOfKeys[i] || key != null && key.equals(arrayOfKeys[i])) {
                 return (V) arrayOfValues[i];
             }
         }
