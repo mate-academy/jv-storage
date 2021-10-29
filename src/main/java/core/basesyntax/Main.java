d17d3e7055212eadce1b09304dac65c3de075dde
VitaliiMarchenko <vitalbka.01@gmail.com>
2021-10-29 19:49:47 +0300

    added solution

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+import core.basesyntax.impl.StorageImpl;
+
+public class Main {
+    public static void main(String[] args) {
+        Storage<Integer, Box> storage = new StorageImpl<>();
+        Box box = new Box();
+        storage.put(22, box);
+        Box value = storage.get(22); // returns the Box
+        int size = storage.size(); // returns storage size
+
+        System.out.println(value + "value");
+        System.out.println(size + " size");
+    }
+}
--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,73 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private Object[] keys;
+    private Object[] values;
+    private int size = 0;
+
+    public StorageImpl() {
+        keys = new Object[MAX_ITEMS_NUMBER];
+        values = new Object[MAX_ITEMS_NUMBER];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (size == MAX_ITEMS_NUMBER) {
+            System.out.println("Storage is full");
+            return;
+        }
+        if (key == null) {
+            for (int i = 0; i < size; i++) {
+                if (keys[i] == null) {
+                    values[i] = value;
+                    return;
+                }
+            }
+        }
+        if (key != null) {
+            for (int i = 0; i < size; i++) {
+                if (key.equals(keys[i])) {
+                    values[i] = value;
+                    return;
+                }
+            }
+        }
+        keys[size] = key;
+        values[size] = value;
+        this.size++;
     }
 
     @Override
     public V get(K key) {
+        if (key == null) {
+            return getWhenKeyIsNull();
+        } else {
+            return getWhenKeyIsNotNull(key);
+        }
+    }
+
+    private V getWhenKeyIsNull() {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == null) {
+                return (V) values[i];
+            }
+        }
+        return null;
+    }
+
+    private V getWhenKeyIsNotNull(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key.equals(keys[i])) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return this.size;
     }
+
 }
