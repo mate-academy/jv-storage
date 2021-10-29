6510036c645b9c39abe1e750ea3e708a88b10870
Olha Tankevych <freakystyley85@gmail.com>
2021-10-29 13:29:41 +0300

    fixed the condition in getIndex

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -35,8 +35,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     private int getIndex(K key) {
         for (int i = 0; i < size; i++) {
-            if (((keys[i] != null) && (keys[i].equals(key))) || ((keys[i] == null)
-                    && (key == null))) {
+            if (keys[i] != null && keys[i].equals(key) || keys[i] == key) {
                 return i;
             }
         }
