3dec12881d8b1228490db95525ef722aa92d855c
Oleksandr Ostapchuk <shurion.ua@gmail.com>
2021-10-31 21:09:57 +0200

    The First Task implemented

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,49 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int KEY_ROW = 0;
+    private static final int VALUE_ROW = 1;
+    private static final int NUMBER_OF_ELEMENTS = 10;
+    private static final int NUMBER_OF_ROW = 2;
+    private Object[][] storage;
+    private int storageLength;
+
+    public StorageImpl() {
+        storage = new Object[NUMBER_OF_ELEMENTS][NUMBER_OF_ROW];
+        storageLength = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        int counter = Math.min(getCounterByKey(key), storageLength);
+        storage[counter][KEY_ROW] = key;
+        storage[counter][VALUE_ROW] = value;
+        storageLength += getCounterByKey(key) < storageLength ? 0 : 1;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int counter = getCounterByKey(key);
+        return (V) storage[counter][VALUE_ROW];
     }
 
     @Override
     public int size() {
-        return -1;
+        return storageLength;
+    }
+
+    private int getCounterByKey(K key) {
+        int counter = 0;
+        for (int i = 0; i < storageLength; i++) {
+            if ((storage[i][KEY_ROW] == null
+                       && storage[i][VALUE_ROW] != null
+                       && key == null)
+                    || (storage[i][KEY_ROW] != null
+                       && storage[i][KEY_ROW].equals(key))) {
+                return counter;
+            }
+            counter++;
+        }
+        return counter;
     }
 }
