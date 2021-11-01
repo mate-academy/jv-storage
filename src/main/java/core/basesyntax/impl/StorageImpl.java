30c57c1f81d7cdbf4ff1bc63d199867e52d85542
Kate Deshchinska <katerinka9636@gmail.com>
2021-11-01 15:10:26 +0200

    added changes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,13 +1,12 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ITEMS_NUMBER = 10;
     private final Object[] keys;
     private final Object[] values;
-    private int numberOfKeys = 0;
+    private int size;
 
     public StorageImpl() {
         keys = new Object[MAX_ITEMS_NUMBER];
@@ -16,24 +15,22 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        if (numberOfKeys == MAX_ITEMS_NUMBER) {
-            return;
-        }
-        numberOfKeys++;
-        keys[numberOfKeys - 1] = key;
-        values[numberOfKeys - 1] = value;
-        for (int i = 0; i < numberOfKeys; i++) {
-            if ((keys[i] == keys[i + 1]) || (keys[i] != null && keys[i].equals(keys[i + 1]))) {
+        for (int i = 0; i < size; i++) {
+            if ((keys[i] == key) || (keys[i] != null && keys[i].equals(key))) {
                 values[i] = value;
+                return;
             }
         }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        for (Object o : keys) {
-            if ((o == null && key == null) || (o != null && o.equals(key))) {
-                return (V) values[Arrays.asList(keys).indexOf(key)];
+        for (int i = 0; i < keys.length; i++) {
+            if ((keys[i] == key) || (keys[i] != null && keys[i].equals(key))) {
+                return (V) values[i];
             }
         }
         return null;
@@ -41,11 +38,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        for (int i = 0; i < numberOfKeys - 1; i++) {
-            if ((keys[i] == keys[i + 1]) || (keys[i] != null && keys[i].equals(keys[i + 1]))) {
-                numberOfKeys--;
-            }
-        }
-        return numberOfKeys;
+        return size;
     }
 }
