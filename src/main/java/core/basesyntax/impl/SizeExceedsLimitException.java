94ca220aa60cb782ded0187210efe1ba0df6eaff
Oleh Mazurkov <o.mazurkov@gmail.com>
2021-11-01 13:19:46 +0200

    solving hw-6 v2 "storage"

---
+++ src/main/java/core/basesyntax/impl/SizeExceedsLimitException.java
@@ -0,0 +1,7 @@
+package core.basesyntax.impl;
+
+public class SizeExceedsLimitException extends RuntimeException {
+    public SizeExceedsLimitException(String message) {
+        super(message);
+    }
+}
--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -15,21 +15,29 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        if (size < MAX_NUMBER_STORAGE_ELEMENTS) {
-            for (int i = 0; i < size; i++) {
-                if ((key == keys[i]) || (key != null && key.equals(keys[i]))) {
-                    values[i] = value;
-                    return;
-                }
-            }
-            keys[size] = key;
-            values[size] = value;
-            size++;
-            return;
-        } else {
-            return;
+        if (size > MAX_NUMBER_STORAGE_ELEMENTS) {
+            throw new SizeExceedsLimitException("Size of storage exceedsmi the limit");
         }
+        for (int i = 0; i < size; i++) {
+            if ((key == keys[i]) || (key != null && key.equals(keys[i]))) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
+        return;
     }
+    /*
+else краще прибрати і модифікувати if.
+    Якщо size > max number то викинути ексепшин, що сторедж заповнений.
+    В такому випадку ми будемо мати більш очевидну роботу класу.
+    Зараз, коли ми додаємо елемент в заповнений сторедж нічого
+    не відбувається і це виглядає так ніби елемент успішно
+    був доставлений в сторедж,
+    що може викликати труднощі при пошуку проблеми
+    */
 
     @Override
     public V get(K key) {
