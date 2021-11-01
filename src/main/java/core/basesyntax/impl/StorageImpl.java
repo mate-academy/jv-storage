4ef06ed7767df301a8bc2d3c59030d33e1394105
Oleksandr Ostapchuk <shurion.ua@gmail.com>
2021-11-01 09:55:50 +0200

    Two arrays created

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -7,26 +7,28 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int VALUE_ROW = 1;
     private static final int NUMBER_OF_ELEMENTS = 10;
     private static final int NUMBER_OF_ROW = 2;
-    private Object[][] storage;
+    private Object[] keys;
+    private Object[] values;
     private int storageLength;
 
     public StorageImpl() {
-        storage = new Object[NUMBER_OF_ELEMENTS][NUMBER_OF_ROW];
+        keys = new Object[NUMBER_OF_ELEMENTS];
+        values = new Object[NUMBER_OF_ELEMENTS];
         storageLength = 0;
     }
 
     @Override
     public void put(K key, V value) {
         int counter = Math.min(getCounterByKey(key), storageLength);
-        storage[counter][KEY_ROW] = key;
-        storage[counter][VALUE_ROW] = value;
+        keys[counter] = key;
+        values[counter] = value;
         storageLength += getCounterByKey(key) < storageLength ? 0 : 1;
     }
 
     @Override
     public V get(K key) {
         int counter = getCounterByKey(key);
-        return (V) storage[counter][VALUE_ROW];
+        return (V) values[counter];
     }
 
     @Override
@@ -37,11 +39,11 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private int getCounterByKey(K key) {
         int counter = 0;
         for (int i = 0; i < storageLength; i++) {
-            if ((storage[i][KEY_ROW] == null
-                       && storage[i][VALUE_ROW] != null
+            if ((keys[i] == null
+                       && values[i] != null
                        && key == null)
-                    || (storage[i][KEY_ROW] != null
-                       && storage[i][KEY_ROW].equals(key))) {
+                    || (keys[i] != null
+                       && keys[i].equals(key))) {
                 return counter;
             }
             counter++;
