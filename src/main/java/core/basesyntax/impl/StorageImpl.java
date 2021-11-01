2e3874359dff4ac1fa121830e175a573bf0d2734
Oleksandr Ostapchuk <shurion.ua@gmail.com>
2021-11-01 13:07:31 +0200

    1. Extra constants deleted 2. "i" and "counter" changed by "index" 3. Other proposed changes was implemented

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,10 +3,7 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int KEY_ROW = 0;
-    private static final int VALUE_ROW = 1;
     private static final int NUMBER_OF_ELEMENTS = 10;
-    private static final int NUMBER_OF_ROW = 2;
     private Object[] keys;
     private Object[] values;
     private int storageLength;
@@ -14,21 +11,20 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public StorageImpl() {
         keys = new Object[NUMBER_OF_ELEMENTS];
         values = new Object[NUMBER_OF_ELEMENTS];
-        storageLength = 0;
     }
 
     @Override
     public void put(K key, V value) {
-        int counter = Math.min(getCounterByKey(key), storageLength);
-        keys[counter] = key;
-        values[counter] = value;
-        storageLength += getCounterByKey(key) < storageLength ? 0 : 1;
+        int index = Math.min(getCounterByKey(key), storageLength);
+        keys[index] = key;
+        values[index] = value;
+        storageLength += index < storageLength ? 0 : 1;
     }
 
     @Override
     public V get(K key) {
-        int counter = getCounterByKey(key);
-        return (V) values[counter];
+        int index = getCounterByKey(key);
+        return (V) values[index];
     }
 
     @Override
@@ -37,17 +33,13 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     }
 
     private int getCounterByKey(K key) {
-        int counter = 0;
-        for (int i = 0; i < storageLength; i++) {
-            if ((keys[i] == null
-                       && values[i] != null
-                       && key == null)
-                    || (keys[i] != null
-                       && keys[i].equals(key))) {
-                return counter;
+        int index = 0;
+        for (; index < storageLength; index++) {
+            ////(keys[i] == null && values[i] != null && key == null)
+            if (keys[index] == key || (keys[index] != null && keys[index].equals(key))) {
+                return index;
             }
-            counter++;
         }
-        return counter;
+        return index;
     }
 }
