a6a22a06214cf23ff2570d95557c86362d720bbe
Dima Barshay <barshayerum8@gmail.com>
2021-11-01 15:23:18 +0200

    implemented storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,38 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private final Pair<K, V>[] storage = new Pair[MAX_ITEMS_NUMBER];
+    private int currentElementsNumber = 0;
+
     @Override
     public void put(K key, V value) {
+        if (currentElementsNumber < MAX_ITEMS_NUMBER) {
+            for (int i = 0; i < currentElementsNumber; i++) {
+                if (key == storage[i].getKey()
+                        || (key != null && key.equals(storage[i].getKey()))) {
+                    storage[i].setValue(value);
+                    return;
+                }
+            }
+            storage[currentElementsNumber] = new Pair<>(key, value);
+            currentElementsNumber++;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (Pair<K, V> element : storage) {
+            if (element != null
+                    && (key == element.getKey() || (key != null && key.equals(element.getKey())))) {
+                return element.getValue();
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return currentElementsNumber;
     }
 }
