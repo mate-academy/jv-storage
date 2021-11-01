a448f8110be3761b71e9ccb5f7c437e465bdb660
Kate Deshchinska <katerinka9636@gmail.com>
2021-11-01 14:10:27 +0200

    Implement the Storage of key-value type

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,13 +1,13 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-
 import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ITEMS_NUMBER = 10;
-    private Object[] keys;
-    private Object[] values;
+    private final Object[] keys;
+    private final Object[] values;
+    private int numberOfKeys = 0;
 
     public StorageImpl() {
         keys = new Object[MAX_ITEMS_NUMBER];
@@ -16,17 +16,36 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        keys = new Object[]{key};
-        values = new Object[]{value};
+        if (numberOfKeys == MAX_ITEMS_NUMBER) {
+            return;
+        }
+        numberOfKeys++;
+        keys[numberOfKeys - 1] = key;
+        values[numberOfKeys - 1] = value;
+        for (int i = 0; i < numberOfKeys; i++) {
+            if ((keys[i] == keys[i + 1]) || (keys[i] != null && keys[i].equals(keys[i + 1]))) {
+                values[i] = value;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
-        return (V) values[Arrays.asList(keys).indexOf(key)];
+        for (Object o : keys) {
+            if ((o == null && key == null) || (o != null && o.equals(key))) {
+                return (V) values[Arrays.asList(keys).indexOf(key)];
+            }
+        }
+        return null;
     }
 
     @Override
     public int size() {
-        return keys.length;
+        for (int i = 0; i < numberOfKeys - 1; i++) {
+            if ((keys[i] == keys[i + 1]) || (keys[i] != null && keys[i].equals(keys[i + 1]))) {
+                numberOfKeys--;
+            }
+        }
+        return numberOfKeys;
     }
 }
