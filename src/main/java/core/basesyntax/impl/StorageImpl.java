5bee8e84c672132c8d47f292e80e703dc5e3fc7c
Mykola Voronov <www.kolyavoronov@gmail.com>
2021-11-01 09:27:31 +0200

    key and value variables were renamed to keys and values

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,34 +4,34 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     public static final int MAX_STORAGE_SIZE = 10;
-    private Object[] key;
-    private Object[] value;
+    private Object[] keys;
+    private Object[] values;
     private int size;
 
     public StorageImpl() {
-        this.key = new Object[MAX_STORAGE_SIZE];
-        this.value = new Object[MAX_STORAGE_SIZE];
-        this.size = 0;
+        keys = new Object[MAX_STORAGE_SIZE];
+        values = new Object[MAX_STORAGE_SIZE];
+        size = 0;
     }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (this.key[i] == key || this.key[i] != null && this.key[i].equals(key)) {
-                this.value[i] = value;
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
+                values[i] = value;
                 return;
             }
         }
-        this.key[size] = key;
-        this.value[size] = value;
+        keys[size] = key;
+        values[size] = value;
         size++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (this.key[i] == key || this.key[i] != null && this.key[i].equals(key)) {
-                return (V) value[i];
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
+                return (V) values[i];
             }
         }
         return null;
