ad00b12582e87cdbdb816de9364c354916760f1c
Mykhailo Tereshchenkov <mtwib.re@gmail.com>
2021-11-01 12:14:44 +0200

    Methods were overriden, storage was implemented

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,36 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_STORAGE_SIZE = 10;
+    private int size;
+    private K[] keysStorage = (K[]) new Object[MAX_STORAGE_SIZE];
+    private V[] valuesStorage = (V[]) new Object[MAX_STORAGE_SIZE];
+    
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (keysStorage[i] == key || keysStorage[i] != null && keysStorage[i].equals(key)) {
+                valuesStorage[i] = value;
+                return;
+            }
+        }
+        valuesStorage[size] = value;
+        keysStorage[size] = key;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keysStorage[i] == key || keysStorage[i] != null && keysStorage[i].equals(key)) {
+                return valuesStorage[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
