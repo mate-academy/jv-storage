04aebf3ef38f258ec6cbc48b3102feb313216bd6
Vladyslav Chyver <lione2009lione@gmail.com>
2021-11-01 15:06:21 +0200

    code have lgic, but don't work

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,30 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    public static final int STORAGE_SIZE = 10;
+    private Object[] storageKeys = new Object[STORAGE_SIZE];
+    private Object[] values = new Object[STORAGE_SIZE];
+    private int indexOfArray = 0;
+
     @Override
     public void put(K key, V value) {
+        this.storageKeys[indexOfArray] = key;
+        this.values[indexOfArray] = value;
+        indexOfArray++;
     }
 
     @Override
     public V get(K key) {
+        for (int indexOfArray = 0; indexOfArray < STORAGE_SIZE; indexOfArray++) {
+            if (storageKeys.equals(key)) {
+                return (V) values[indexOfArray];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return STORAGE_SIZE;
     }
 }
