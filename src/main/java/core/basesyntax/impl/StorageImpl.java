473ad6c541bb8be087550b9ed1e93549155c9dbd
Dany Nesterov <nesterov1200997@gmail.com>
2021-11-01 11:13:28 +0200

    changes have been added

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,14 +3,14 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int MAX_VALUE = 10;
+    private static final int STORAGE_SIZE = 10;
     private K[] keys;
     private V[] values;
     private int size;
 
     public StorageImpl() {
-        keys = (K[]) new Object[MAX_VALUE];
-        values = (V[]) new Object[MAX_VALUE];
+        keys = (K[]) new Object[STORAGE_SIZE];
+        values = (V[]) new Object[STORAGE_SIZE];
     }
 
     @Override
