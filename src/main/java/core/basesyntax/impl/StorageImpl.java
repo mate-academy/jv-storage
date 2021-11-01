50dbaba462fa68bc5aa3daf782b934cbf8ae4b26
Kate Deshchinska <katerinka9636@gmail.com>
2021-11-01 09:56:51 +0200

    first commit

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,18 +2,31 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.Arrays;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private Object[] keys;
+    private Object[] values;
+
+    public StorageImpl() {
+        keys = new Object[MAX_ITEMS_NUMBER];
+        values = new Object[MAX_ITEMS_NUMBER];
+    }
+
     @Override
     public void put(K key, V value) {
+        keys = new Object[]{key};
+        values = new Object[]{value};
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return (V) values[Arrays.asList(keys).indexOf(key)];
     }
 
     @Override
     public int size() {
-        return -1;
+        return keys.length;
     }
 }
