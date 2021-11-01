4ab512e798c00f9339840cbd8d2b4578cc1270d1
Svitlana <wsv3ta@gmail.com>
2021-11-01 16:50:52 +0200

    created methods to work with two types of data

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,40 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int ELEMENT_MAX_NUMBER = 10;
+    private final K[] keys = (K[]) new Object[ELEMENT_MAX_NUMBER];
+    private final V[] values = (V[]) new Object[ELEMENT_MAX_NUMBER];
+    private int size;
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(keys[i], key)) {
+                keys[i] = key;
+                values[i] = value;
+                return;
+            }
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(keys[i], key)) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
