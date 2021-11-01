82a6c73c30b07afdafb132f946fffdb2f039d002
Vladyslav Chyver <lione2009lione@gmail.com>
2021-11-01 16:35:52 +0200

    rename variable storageKeys to keys, STORAGE_SIZE to private

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,20 +3,20 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    public static final int STORAGE_SIZE = 10;
-    private Object[] storageKeys = new Object[STORAGE_SIZE];
+    private static final int STORAGE_SIZE = 10;
+    private Object[] keys = new Object[STORAGE_SIZE];
     private Object[] values = new Object[STORAGE_SIZE];
     private int size;
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (storageKeys[i] == key || key != null && key.equals(storageKeys[i])) {
+            if (keys[i] == key || key != null && key.equals(keys[i])) {
                 values[i] = value;
                 return;
             }
         }
-        this.storageKeys[size] = key;
+        this.keys[size] = key;
         this.values[size] = value;
         size++;
     }
@@ -24,7 +24,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int j = 0; j < size; j++) {
-            if (key == storageKeys[j] || key != null && key.equals(storageKeys[j])) {
+            if (key == keys[j] || key != null && key.equals(keys[j])) {
                 return (V) values[j];
             }
         }
