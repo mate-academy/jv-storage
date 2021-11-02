774e33844bc4b8ab186ed43fe178b5a349e67dc3
Viktor Stadnik <stadhik.victor@gmail.com>
2021-11-02 12:03:02 +0200

    Fixed remark: 1.delete return in put method 2.Rename of final variable and variable "size"

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,31 +3,30 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int ELEMENTS = 10;
-    private K[] boxKey = (K[]) new Object[ELEMENTS];
-    private V[] boxValue = (V[]) new Object[ELEMENTS];
-    private int newSize;
+    private static final int MAX_SIZE = 10;
+    private K[] boxKey = (K[]) new Object[MAX_SIZE];
+    private V[] boxValue = (V[]) new Object[MAX_SIZE];
+    private int size;
 
     @Override
     public void put(K key, V value) {
-        if (newSize == ELEMENTS) {
+        if (size == MAX_SIZE) {
             throw new RuntimeException("Size is long");
         }
-        for (int i = 0; i < newSize; i++) {
+        for (int i = 0; i < size; i++) {
             if (boxKey[i] == key || key != null && key.equals(boxKey[i])) {
                 boxValue[i] = value;
                 return;
             }
         }
-        boxKey[newSize] = key;
-        boxValue[newSize] = value;
-        newSize++;
-        return;
+        boxKey[size] = key;
+        boxValue[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < newSize; i++) {
+        for (int i = 0; i < size; i++) {
             if (boxKey[i] == key || key != null && key.equals(boxKey[i])) {
                 return boxValue[i];
             }
@@ -37,6 +36,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return newSize;
+        return size;
     }
 }
