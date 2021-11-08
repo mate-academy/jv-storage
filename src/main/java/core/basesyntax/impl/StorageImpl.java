5127e2702bc3dcb91dc513a75b93f09f43480683
Serhii Pedchenko <djmolot@gmail.com>
2021-11-08 20:49:00 +0200

    removed keyword this in method size()

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -43,7 +43,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return this.size;
+        return size;
     }
 
     private int getKeyIndex(K key) {
