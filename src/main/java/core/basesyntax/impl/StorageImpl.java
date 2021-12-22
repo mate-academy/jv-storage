231a9aa29c39716a189b14bc1d1b944a33f97ead
maksim <maksim.sokur90@gmail.com>
2021-12-22 22:03:13 +0200

    jv-storage-sokur_m-V_1.0 1. Created class Pair vs Generic types, constructors, and base methods. 2. Implement in class StorageImpl interface Storage methods. 3. Create array of Pair vs final size 10 and dynamic value size. 4. Create base constructor StorageImpl. 5. Implement method put and verify data at key and set value in if statement and return in loop or create new object and add to array. 6. Implement method get and verify data at key and return typed value from array or return null. 7. Implement method size and return size.
    Maven test checked.
     Thank you.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,44 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_NUMBERS_OF_ELEMENTS = 10;
+    private Pair[] pairArray;
+    private int size;
+
+    public StorageImpl() {
+        pairArray = new Pair[MAX_NUMBERS_OF_ELEMENTS];
+        size = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (key == pairArray[i].getKey()
+                    || key != null && key.equals(pairArray[i].getKey())) {
+                pairArray[i].setValue(value);
+                return;
+            }
+        }
+        Pair<K, V> pair = new Pair<>();
+        pair.setKey(key);
+        pair.setValue(value);
+        pairArray[size] = pair;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == pairArray[i].getKey()
+                    || key != null && key.equals(pairArray[i].getKey())) {
+                return (V) pairArray[i].getValue();
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
