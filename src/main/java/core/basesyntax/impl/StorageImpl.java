c9ca7b5a83193a62617491081bc0259733466cea
oleg-new <9213195@gmail.com>
2021-12-24 18:51:59 +0200

    added variable "size", to determine duplicate "key" method "get" is used, only one "if" is used in the get method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,15 +3,17 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private Object[][] objectArray = new Object[2][10];
+    int size = 10;
+    private Object[][] objectArray = new Object[2][size];
+
+
 
     @Override
     public void put(K key, V value) {
 
-        for (int i = 0; i < objectArray[0].length; i++) {
+        for (int i = 0; i < size; i++) {
             if ((objectArray[0][i] == null && objectArray[1][i] == null)
-                    || (objectArray[0][i] == null && key == null)
-                    || (objectArray[0][i] != null && objectArray[0][i].equals(key))) {
+                || (get(key) != null)){
                 objectArray[0][i] = key;
                 objectArray[1][i] = value;
                 return;
@@ -21,11 +23,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < objectArray[0].length; i++) {
-            if (objectArray[0][i] != null && objectArray[0][i].equals(key)) {
-                return (V) objectArray[1][i];
-            }
-            if (key == null && objectArray[0][i] == null && objectArray[1][i] != null) {
+        for (int i = 0; i < size; i++) {
+            if ((objectArray[0][i] != null && objectArray[0][i].equals(key)) || (key == null && objectArray[0][i] == null && objectArray[1][i] != null)) {
                 return (V) objectArray[1][i];
             }
         }
