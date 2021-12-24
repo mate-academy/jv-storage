831b35ec0b8bc464a6b64dac24e51284df0cc160
Vladyslav Kondiievskyi <80918559+Vladyslav-Kondiievskyi@users.noreply.github.com>
2021-12-24 20:51:19 +0200

    task was completed but has conflict compiler with my

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,51 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private int size;
+    private final int max = 40;
+    private K [] keys;
+    private V [] values;
+
+    public StorageImpl() {
+        keys =(K[]) new Object[max];
+        values = (V[]) new Object[max];
+    }
+
     @Override
     public void put(K key, V value) {
+
+        if (size > max) {
+            throw new RuntimeException("Arrays are full");
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(keys[i], key)) {
+                keys[i] = key;
+                values[i] = value;
+                return;
+            }
+        }
+        values[size] = value;
+        keys[size] = key;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(keys[i], key)) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
