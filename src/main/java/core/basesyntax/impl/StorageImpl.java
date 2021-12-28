d822bd6bd1146008e66a8d30bfaf10cc140e4f22
Vitalii Moisieiev <vitalij.mojsejiv@gmail.com>
2021-12-28 23:48:35 +0200

    Corrected syntax and indentation errors

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,8 +3,8 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private Pair<K, V>[] pairs;
     private static final int MAX_SIZE = 10;
+    private Pair<K, V>[] pairs;
 
     public StorageImpl() {
         pairs = new Pair[MAX_SIZE];
@@ -15,8 +15,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         for (int i = 0; i < size(); i++) {
             if (key == null && pairs[i].getKey() == null
                     || pairs[i].getKey() != null && pairs[i].getKey().equals(key)) {
-                 pairs[i].setValue(value);
-                 return;
+                pairs[i].setValue(value);
+                return;
             }
         }
         pairs[size()] = new Pair<>(key, value);
