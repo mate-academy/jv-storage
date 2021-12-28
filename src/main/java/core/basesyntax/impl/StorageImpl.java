00efe08c2350d2bf9ac2fdf2c6c013069aed8185
Vitaliy Zahrenko <vzagorenko@gmail.com>
2021-12-28 18:40:55 +0200

    Storage has been impleneted

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,46 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private K[] keyArray = (K[]) new Object[10];
+    private V[] valArray = (V[]) new Object[10];
+    private int sizeArray = 0;
+
     @Override
     public void put(K key, V value) {
+
+        for (int i = 0;i < this.sizeArray;i++) {
+            if (keyEquals(i,key)) {
+                keyArray[i] = key;
+                valArray[i] = value;
+                return;
+            }
+        }
+        keyArray[this.sizeArray] = key;
+        valArray[this.sizeArray] = value;
+        this.sizeArray++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < this.sizeArray;i++) {
+            if (keyEquals(i,key)) {
+                return valArray[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return this.sizeArray;
+    }
+
+    private boolean keyEquals(int index,K key) {
+
+        if (this.keyArray[index] != null) {
+            return this.keyArray[index].equals(key);
+        }
+        return (key == null);
     }
 }
