ec2b4286f1b7025a042096e9b5341accb6c0172f
oleg-new <9213195@gmail.com>
2021-12-29 22:20:03 +0200

    Added constant MAX_SIZE, simplified the size method, get and put methods changed

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,21 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private int size = 10;
-    private Object[][] objectArray = new Object[2][size];
+    private static final int MAX_SIZE = 10;
+    private int size = 0;
+    private Object[][] objectArray = new Object[2][MAX_SIZE];
 
     @Override
     public void put(K key, V value) {
 
-        for (int i = 0; i < size; i++) {
-            if ((objectArray[0][i] == null && objectArray[1][i] == null)
-                    || (get(key) != null)) {
+        for (int i = 0; i < MAX_SIZE - 1; i++) {
+            if ((get(key) != null)) {
+                objectArray[1][i] = value;
+                return;
+            } else if (objectArray[0][i] == null && objectArray[1][i] == null) {
                 objectArray[0][i] = key;
                 objectArray[1][i] = value;
+                size++;
                 return;
             }
         }
@@ -23,7 +27,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public V get(K key) {
         for (int i = 0; i < size; i++) {
             if ((objectArray[0][i] != null && objectArray[0][i].equals(key))
-                    || (key == null && objectArray[0][i] == null && objectArray[1][i] != null)) {
+                    || (key == objectArray[0][i])) {
                 return (V) objectArray[1][i];
             }
         }
@@ -32,12 +36,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        int result = 0;
-        for (int i = 0; i < objectArray[0].length; i++) {
-            if (objectArray[1][i] != null || objectArray[0][i] != null) {
-                result++;
-            }
-        }
-        return result;
+
+        return size;
     }
 }
