514dacaa90fcf6293b47a5874ff5aa1b8fd5096e
Vitaliy Zahrenko <vzagorenko@gmail.com>
2021-12-30 11:13:11 +0200

    constat storage capacity, remove (this.), keyEquals metod refactored

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,28 +4,28 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
-    private K[] keyArray = (K[]) new Object[10];
-    private V[] valArray = (V[]) new Object[10];
+    private static final int STORAGE_CAPACITY = 10;
+    private K[] keyArray = (K[]) new Object[STORAGE_CAPACITY];
+    private V[] valArray = (V[]) new Object[STORAGE_CAPACITY];
     private int sizeArray = 0;
 
     @Override
     public void put(K key, V value) {
 
-        for (int i = 0;i < this.sizeArray;i++) {
+        for (int i = 0;i < sizeArray;i++) {
             if (keyEquals(i,key)) {
-                keyArray[i] = key;
                 valArray[i] = value;
                 return;
             }
         }
-        keyArray[this.sizeArray] = key;
-        valArray[this.sizeArray] = value;
-        this.sizeArray++;
+        keyArray[sizeArray] = key;
+        valArray[sizeArray] = value;
+        sizeArray++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < this.sizeArray;i++) {
+        for (int i = 0; i < sizeArray;i++) {
             if (keyEquals(i,key)) {
                 return valArray[i];
             }
@@ -35,14 +35,11 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return this.sizeArray;
+        return sizeArray;
     }
 
     private boolean keyEquals(int index,K key) {
 
-        if (this.keyArray[index] != null) {
-            return this.keyArray[index].equals(key);
-        }
-        return (key == null);
+        return key == keyArray[index] || key != null && key.equals(keyArray[index]);
     }
 }
