1df7352765b19445a28e4ce445bc055622e2d83a
Maksym Pokras <mxpokras@gmail.com>
2022-01-01 13:09:32 +0200

    StorageImpl.Node internal class created

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,6 +3,24 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private int size = 0;
+
+    private class Node <K,V> {
+        private K key;
+        private V value;
+        Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+    }
     @Override
     public void put(K key, V value) {
     }
