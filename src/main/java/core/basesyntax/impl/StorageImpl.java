20ac388b4d5bb381d2565d49b2711d40c51c3859
Maksym Pokras <mxpokras@gmail.com>
2022-01-01 14:05:26 +0200

    StorageImpl is made with Object[] array as container

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,36 +2,71 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
-public class StorageImpl<K, V> implements Storage<K, V> {
-    private int size = 0;
+import java.util.Objects;
 
-    private class Node <K,V> {
-        private K key;
-        private V value;
-        Node(K key, V value) {
+public class StorageImpl<K, V> implements Storage<K, V> {
+    private static int CAPACITY = 10;
+    private int size = 0;
+    private Object[] array = new Object[CAPACITY]; //
+
+    private class Element<K1,V1> {
+        private K1 key;
+        private V1 value;
+        Element(K1 key, V1 value) {
             this.key = key;
             this.value = value;
         }
 
-        public K getKey() {
+        public K1 getKey() {
             return key;
         }
 
-        public V getValue() {
+        public V1 getValue() {
             return value;
         }
+
+        public void setValue(V1 value) {
+            this.value = value;
+        }
     }
+
     @Override
     public void put(K key, V value) {
+        Element <K,V> element = getElement(key);
+        if (element == null) {
+            element = new Element<>(key, value);
+            array[size] = element;
+            size++;
+            return;
+        }
+        element.setValue(value);
     }
 
     @Override
     public V get(K key) {
+        Element <K,V> element = getElement(key);
+        if (element != null) {
+            return element.getValue();
+        }
+        return null;
+    }
+
+    private Element<K,V> getElement(K key) {
+        for (int i = 0; i < size; i++) {
+            Object bufObj = array[i];
+            if (bufObj == null) {
+                continue;
+            }
+            Element<K,V> element = (Element<K,V>) bufObj;
+            if (Objects.equals(element.getKey(), key)) {
+                return element;
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
