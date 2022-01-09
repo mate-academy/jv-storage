13cf44e7ce4370df2cd0a85fb41cfff3de622f59
Oleksandr Semytskyi <ssemitskiy@gmail.com>
2022-01-09 22:37:45 +0200

    Solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,40 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int maxItemsNumber = 9;
+    private static int size = 0;
+    private static Object[] keys = new Object[maxItemsNumber];
+    private Object[] values = new Integer[maxItemsNumber];
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == key || keys[i] != null) {
+                values[i] = value;
+                return;
+            }
+        }
+        if (size > maxItemsNumber) {
+            System.out.println("the storage has run out of space ):");
+            return;
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0;i < size;i++) {
+            if (keys[i] == key && key != null) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
