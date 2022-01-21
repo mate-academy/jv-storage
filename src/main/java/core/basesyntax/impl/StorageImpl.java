5bd881f624617d97d62ce4bc555b0e42898072d1
LinkClink <shekaqwe@gmail.com>
2022-01-21 23:13:19 +0200

    add class StrorageIm with meyhods
    realisation methods put + get + size

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,33 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE = 10;
+    private Pair[] pairs = new Pair[MAX_SIZE];
+    private int size;
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (key == pairs[i].getKey() || key != null && key.equals(pairs[i].getKey())) {
+                pairs[i].setValue(value);
+                return;
+            }
+        }
+        pairs[size++] = new Pair().setValue(value).setKey(key);
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == pairs[i].getKey() || key != null && key.equals(pairs[i].getKey())) {
+                return (V) pairs[i].getValue();
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
