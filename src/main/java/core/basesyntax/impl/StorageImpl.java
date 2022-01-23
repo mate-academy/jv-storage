fe526aad7371ad0509e376edf2e0fb94c1f909c2
Liliia Ponomarenko <liliya97ua@gmail.com>
2022-01-23 16:07:30 +0200

    implement the Storage of key-value type

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,34 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_STORAGE_SIZE = 10;
+    private final K[] keys = (K[]) new Object[MAX_STORAGE_SIZE];
+    private final V[] items = (V[]) new Object[MAX_STORAGE_SIZE];
+    private int itemsCode;
+
     @Override
     public void put(K key, V value) {
+        if (get(key) == null) {
+            keys[itemsCode] = key;
+            items[itemsCode] = value;
+            itemsCode++;
+        } else {
+            items[itemsCode - 1] = value;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < itemsCode; i++) {
+            if (keys[i] == key || key != null && key.equals(keys[i])) {
+                return items[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return itemsCode;
     }
 }
