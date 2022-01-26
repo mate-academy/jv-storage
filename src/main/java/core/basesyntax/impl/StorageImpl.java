198e80ae7edf586df4b15c523768768d7eb13485
sviat <svyatoslav.grinik@gmail.com>
2022-01-26 20:58:19 +0200

    code was refactored to improve readability

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,50 +4,33 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int DEFAULT_CAPACITY = 10;
-    private static final int MAX_NULL_KEYS = 1;
     private final K[] keys;
     private final V[] values;
+    private int size;
 
     public StorageImpl() {
         keys = (K[]) new Object[DEFAULT_CAPACITY];
         values = (V[]) new Object[DEFAULT_CAPACITY];
+        size = 0;
     }
 
     @Override
     public void put(K key, V value) {
-        if (key == null) {
-            for (int i = 0; i < keys.length; i++) {
-                if (keys[i] == null) {
-                    keys[i] = null;
-                    values[i] = value;
-                    return;
-                }
-            }
-        }
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i] == null && values[i] == null
-                    || key != null && key.equals(keys[i])) {
-                keys[i] = key;
-                values[i] = value;
-                return;
-            }
+        if (get(key) == null) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        } else {
+            keys[size - 1] = key;
+            values[size - 1] = value;
         }
     }
 
     @Override
     public V get(K key) {
-        if (key == null) {
-            for (int i = 0; i < keys.length; i++) {
-                if (keys[i] == null && values[i] != null) {
-                    return values[i];
-                } else if (keys[i] == null && values[i] == null) {
-                    return null;
-                }
-            }
-        }
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i] == null && values[i] == null) {
-                return null;
+        for (int i = 0; i < size; i++) {
+            if (key == null && keys[i] == null && values[i] != null) {
+                return values[i];
             } else if (key != null && key.equals(keys[i])) {
                 return values[i];
             }
@@ -57,14 +40,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        int size = 0;
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i] != null) {
-                size++;
-            } else if (keys[i] == null && values[i] != null) {
-                return MAX_NULL_KEYS;
-            }
-        }
         return size;
     }
 }
