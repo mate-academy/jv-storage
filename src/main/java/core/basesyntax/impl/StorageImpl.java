8d615b4ea62c07b1621506489a9a379f4e89c0c5
Dima Semeniuk <semeniuk.dima@ukr.net>
2022-01-27 12:06:58 +0200

    Changed arrays names from key and value to keys and values

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,31 +4,31 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int STORAGE_SIZE = 10;
-    private K[] key;
-    private V[] value;
+    private K[] keys;
+    private V[] values;
     private int size;
 
     public StorageImpl() {
-        key = (K[]) new Object[STORAGE_SIZE];
-        value = (V[]) new Object[STORAGE_SIZE];
+        keys = (K[]) new Object[STORAGE_SIZE];
+        values = (V[]) new Object[STORAGE_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
         if (get(key) == null) {
-            this.key[size] = key;
-            this.value[size] = value;
+            keys[size] = key;
+            values[size] = value;
             size++;
         } else {
-            this.value[size - 1] = value;
+            values[size - 1] = value;
         }
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (this.key[i] == key || this.key[i] != null && this.key[i].equals(key)) {
-                return this.value[i];
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
+                return values[i];
             }
         }
         return null;
