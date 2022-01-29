bf4d833d06cf7b3ae31025aa3399f50757daa5df
Oleh Mytiai <mytiai.ov@icloud.com>
2022-01-29 16:08:11 +0200

    implemented methods: put, get and size

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,51 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private final K[] keys;
+    private final V[] values;
+    private int size;
+    private int index;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_ITEMS_NUMBER];
+        values = (V[]) new Object[MAX_ITEMS_NUMBER];
+        size = 0;
+        index = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        if (size > 0) {
+            for (int i = 0; i < size; i++) {
+                if (isEqual(keys[i], key)) {
+                    values[i] = value;
+                    return;
+                }
+            }
+        }
+        keys[index] = key;
+        values[index] = value;
+        index++;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (isEqual(keys[i], key)) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    private boolean isEqual(K first, K second) {
+        return first == second || first != null && first.equals(second);
     }
 }
