0b885f27bc2297e6d5c18dbc5fccd4ac490865a1
Ivan Pavluchuk <java.python10@gmail.com>
2022-01-31 15:13:49 +0200

    readded solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,32 +3,36 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int SIZE = 10;
-    private int value;
+    private static final int MAXIMUM_SIZE = 10;
+    private int size;
     private K[] key;
     private V[] values;
 
     public StorageImpl() {
-        key = (K[]) new Object[SIZE];
-        values = (V[]) new Object[SIZE];
+        key = (K[]) new Object[MAXIMUM_SIZE];
+        values = (V[]) new Object[MAXIMUM_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < this.value; i++) {
+        for (int i = 0; i < this.size; i++) {
             if (get(key) != null) {
                 values[i] = value;
                 return;
             }
         }
-        this.key[this.value] = key;
-        values[this.value] = value;
-        this.value++;
+        if (get(key) == null) {
+            this.key[this.size] = key;
+            values[this.size] = value;
+            this.size++;
+        } else {
+            values[this.size - 1] = value;
+        }
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < value; i++) {
+        for (int i = 0; i < size; i++) {
             if (this.key[i] == key || this.key[i] != null && this.key[i].equals(key)) {
                 return values[i];
             }
@@ -38,6 +42,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return value;
+        return size;
     }
 }
