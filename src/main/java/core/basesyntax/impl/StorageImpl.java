9f15057bb1a0a5f80cdb838116a0a265a09e26e8
Natalia Kurhanska <kurhnatali@gmail.com>
2022-02-04 23:33:51 +0200

    changed get()

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -23,7 +23,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (key == keys[i] || key != null && keys[i].equals(key)) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
                 return (V) values[i];
             }
         }
