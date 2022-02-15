2dec51b1aa3a6c405635beef95117eaa02fd553e
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2022-02-15 02:29:07 +0200

    Create methods

--- src/main/java/core/basesyntax/Storage.java
+++ src/main/java/core/basesyntax/Storage.java
@@ -1,12 +1,11 @@
 package core.basesyntax;
 
 public interface Storage<K, V> {
+
     void put(K key, V value);
 
     V get(K key);
 
     int size();
 
-    K[] keyArray = new K[10];
-    V[] valueArray = new V[10];
 }
--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,8 +3,34 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private K key;
+    private V value;
+    private static int MAX_ARRAY_VALUE = 10;
+    K[] keyArray = (K[]) new Object[MAX_ARRAY_VALUE];;
+    V[] valueArray = (V[]) new Object[MAX_ARRAY_VALUE];
+    private int storageSize;
+
+    private StorageImpl(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public <K, V> void StorageCount(K key, V value) {
+        Storage<K, V> storage = new StorageImpl<>(key, value);
+
+       for(int i = 0; i < MAX_ARRAY_VALUE - 1; i++) {
+           if(storage.get(key).equals(value)) {
+               storage.put(key, value);
+
+           }
+       }
+
+    }
+
+
     @Override
     public void put(K key, V value) {
+        K[i] = key;
     }
 
     @Override
@@ -14,6 +40,34 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return -1;
+        return storageSize;
     }
+/*
+    @Override
+    public boolean equals(Object storageImpl) {
+        if (this == storageImpl) {
+            return true;
+        }
+        if (storageImpl == null) {
+            return false;
+        }
+        if (!storageImpl.getClass().equals(StorageImpl.class)) {
+            return false;
+        }
+        StorageImpl<K, V> testStorageImpl = (StorageImpl<K, V>) storageImpl;
+        return (((key == testStorageImpl.key) || (key != null
+                && key.equals(testStorageImpl.key))) && ((value == testStorageImpl.value)
+                || ((value != null && value.equals(testStorageImpl.value)))));
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 71;
+        result = 43 * result + (key == null ? 0 : key.hashCode());
+        result = 43 * result + (value == null ? 0 : value.hashCode());
+        return result;
+    }
+
+ */
 }
