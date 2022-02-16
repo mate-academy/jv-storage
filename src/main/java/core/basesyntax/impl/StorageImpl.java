bd82fc4a1e72f5013d372a7047d9f8bc17c9ed75
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2022-02-16 14:19:57 +0200

    All tasks complete. Mavens comments corrected. Let's check

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,73 +1,59 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static int MAX_ARRAY_VALUE = 10;
     private K key;
     private V value;
-    private static int MAX_ARRAY_VALUE = 10;
-    K[] keyArray = (K[]) new Object[MAX_ARRAY_VALUE];;
-    V[] valueArray = (V[]) new Object[MAX_ARRAY_VALUE];
-    private int storageSize;
+    private K[] keyArray = (K[]) new Object[MAX_ARRAY_VALUE];
+    private V[] valueArray = (V[]) new Object[MAX_ARRAY_VALUE];
+    private int storageValue = 0;
 
-    private StorageImpl(K key, V value) {
+    public StorageImpl(K key, V value) {
         this.key = key;
         this.value = value;
     }
 
-    public <K, V> void StorageCount(K key, V value) {
-        Storage<K, V> storage = new StorageImpl<>(key, value);
-
-       for(int i = 0; i < MAX_ARRAY_VALUE - 1; i++) {
-           if(storage.get(key).equals(value)) {
-               storage.put(key, value);
-
-           }
-       }
+    public StorageImpl() {
 
     }
 
-
     @Override
     public void put(K key, V value) {
-        K[i] = key;
+        if (storageValue == 0) {
+            keyArray[0] = key;
+            valueArray[0] = value;
+            storageValue++;
+        } else {
+            for (int i = 0; i < MAX_ARRAY_VALUE - 1; i++) {
+                if (Objects.equals(key, keyArray[i])) {
+                    valueArray[i] = value;
+                    return;
+                } else {
+                    keyArray[storageValue] = key;
+                    valueArray[storageValue] = value;
+                    storageValue++;
+                    break;
+                }
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int k = 0; k < MAX_ARRAY_VALUE - 1; k++) {
+            if (Objects.equals(key, keyArray[k])) {
+                return valueArray[k];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return storageSize;
-    }
-/*
-    @Override
-    public boolean equals(Object storageImpl) {
-        if (this == storageImpl) {
-            return true;
-        }
-        if (storageImpl == null) {
-            return false;
-        }
-        if (!storageImpl.getClass().equals(StorageImpl.class)) {
-            return false;
-        }
-        StorageImpl<K, V> testStorageImpl = (StorageImpl<K, V>) storageImpl;
-        return (((key == testStorageImpl.key) || (key != null
-                && key.equals(testStorageImpl.key))) && ((value == testStorageImpl.value)
-                || ((value != null && value.equals(testStorageImpl.value)))));
-
+        return storageValue;
     }
 
-    @Override
-    public int hashCode() {
-        int result = 71;
-        result = 43 * result + (key == null ? 0 : key.hashCode());
-        result = 43 * result + (value == null ? 0 : value.hashCode());
-        return result;
-    }
-
- */
 }
