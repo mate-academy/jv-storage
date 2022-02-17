ee5488e5df7518c6ff4e60122ab3bde908eb7f45
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2022-02-17 22:23:56 +0200

    All up to date

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,48 +5,35 @@ import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static int MAX_ARRAY_VALUE = 10;
-    private K key;
-    private V value;
-    private K[] keyArray = (K[]) new Object[MAX_ARRAY_VALUE];
-    private V[] valueArray = (V[]) new Object[MAX_ARRAY_VALUE];
+    private K[] keys = (K[]) new Object[MAX_ARRAY_VALUE];
+    private V[] values = (V[]) new Object[MAX_ARRAY_VALUE];
     private int storageValue = 0;
-    // Next try
-
-    public StorageImpl(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
 
     public StorageImpl() {
+    }
 
+    private void storageAdd(int currentCell, K key, V value) {
+        keys[currentCell] = key;
+        values[currentCell] = value;
+        storageValue++;
     }
 
     @Override
     public void put(K key, V value) {
-        if (storageValue == 0) {
-            keyArray[0] = key;
-            valueArray[0] = value;
-            storageValue++;
-        } else {
-            for (int i = 0; i < MAX_ARRAY_VALUE - 1; i++) {
-                if (Objects.equals(key, keyArray[i])) {
-                    valueArray[i] = value;
-                    return;
-                } else {
-                    keyArray[storageValue] = key;
-                    valueArray[storageValue] = value;
-                    storageValue++;
-                    break;
-                }
+        for (int i = 0; i < storageValue; i++) {
+            if (Objects.equals(key, keys[i])) {
+                values[i] = value;
+                return;
             }
         }
+        storageAdd(storageValue, key, value);
     }
 
     @Override
     public V get(K key) {
         for (int k = 0; k < MAX_ARRAY_VALUE - 1; k++) {
-            if (Objects.equals(key, keyArray[k])) {
-                return valueArray[k];
+            if (Objects.equals(key, keys[k])) {
+                return values[k];
             }
         }
         return null;
@@ -56,5 +43,4 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public int size() {
         return storageValue;
     }
-
 }
