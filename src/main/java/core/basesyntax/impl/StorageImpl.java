8857028b2fdce09ceb7572486d239733e69fd06f
Kate Chernyavska <ccarts.kpi@gmail.com>
2022-02-20 16:39:33 +0200

    criated two arrays equal length for storing keys and values in constructor they were initialized. In method put in arrays fill values and check values if they didn't equal to null. In method get key was replaced value. Method size return size Storage pair.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,45 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
+@SuppressWarnings("unchecked")
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private final K[] itemsKey;
+    private final V[] itemsValye;
+    private int lengthPair;
+
+    public StorageImpl() {
+        itemsKey = (K[]) new Object[MAX_ITEMS_NUMBER];
+        itemsValye = (V[]) new Object[MAX_ITEMS_NUMBER];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < itemsKey.length; i++) {
+            if (Objects.equals(itemsKey[i], key) && get(key) != null) {
+                itemsValye[i] = value;
+                return;
+            }
+        }
+        itemsKey[lengthPair] = key;
+        itemsValye[lengthPair] = value;
+        lengthPair++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < itemsKey.length; i++) {
+            if (Objects.equals(itemsKey[i], key)) {
+                return itemsValye[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return lengthPair;
     }
 }
