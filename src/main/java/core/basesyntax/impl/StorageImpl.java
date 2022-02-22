e2ad67f0e41580de894c23225d78d5ba2e9a8e1c
Dmytro Lemeshchenko <71040933+LowPassenger@users.noreply.github.com>
2022-02-22 23:59:05 +0200

    Jv 6 hw storage (#1)
    * Create arrays
    
    * Create methods
    
    * All tasks complete. Mavens comments corrected. Let's check
    
    * Next try
    
    * Next try
    
    * All up to date
    
    * Refactor 1.0
    
    * Delete redundant constructor and two variables
    
    * Rename variable storageValue to size
    Remove Objects.equals

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,40 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static int MAX_ARRAY_VALUE = 10;
+    private K[] keys = (K[]) new Object[MAX_ARRAY_VALUE];
+    private V[] values = (V[]) new Object[MAX_ARRAY_VALUE];
+    private int size = 0;
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
+                values[i] = value;
+                return;
+            }
+        }
+        storageAdd(size, key, value);
     }
 
     @Override
     public V get(K key) {
+        for (int k = 0; k < size; k++) {
+            if ((key == null && keys[k] == null) || (key != null && key.equals(keys[k]))) {
+                return values[k];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    private void storageAdd(int currentCell, K key, V value) {
+        keys[currentCell] = key;
+        values[currentCell] = value;
+        size++;
     }
 }
