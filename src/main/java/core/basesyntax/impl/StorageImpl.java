32635bda0394c3a556929d3fb8d4f06513de39ee
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2022-02-22 23:53:01 +0200

    Rename variable storageValue to size Remove Objects.equals

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,38 +1,28 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static int MAX_ARRAY_VALUE = 10;
     private K[] keys = (K[]) new Object[MAX_ARRAY_VALUE];
     private V[] values = (V[]) new Object[MAX_ARRAY_VALUE];
-    private int storageValue = 0;
-
-    public StorageImpl() {
-    }
-
-    private void storageAdd(int currentCell, K key, V value) {
-        keys[currentCell] = key;
-        values[currentCell] = value;
-        storageValue++;
-    }
+    private int size = 0;
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < storageValue; i++) {
-            if (Objects.equals(key, keys[i])) {
+        for (int i = 0; i < size; i++) {
+            if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
                 values[i] = value;
                 return;
             }
         }
-        storageAdd(storageValue, key, value);
+        storageAdd(size, key, value);
     }
 
     @Override
     public V get(K key) {
-        for (int k = 0; k < MAX_ARRAY_VALUE - 1; k++) {
-            if (Objects.equals(key, keys[k])) {
+        for (int k = 0; k < size; k++) {
+            if ((key == null && keys[k] == null) || (key != null && key.equals(keys[k]))) {
                 return values[k];
             }
         }
@@ -41,6 +31,12 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return storageValue;
+        return size;
+    }
+
+    private void storageAdd(int currentCell, K key, V value) {
+        keys[currentCell] = key;
+        values[currentCell] = value;
+        size++;
     }
 }
