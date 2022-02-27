5fe9cb88401de8d160e901474bf280ef98a41406
Liubomyr <liubomyr.stepanenko@ukma.edu.ua>
2022-02-27 12:21:13 +0200

    StorageImpl is completed

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,48 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private K[] keyArray = (K[]) new Object[10];
+    private V[] valueArray = (V[]) new Object[10];
+    private int freeIndex = 0;
+
     @Override
     public void put(K key, V value) {
+        int i = 0;
+        for (; i < freeIndex; i++) {
+            if ((keyArray[i] != null
+                    && keyArray[i].equals(key))
+                    || (keyArray[i] == null
+                    && keyArray[i] == key)) {
+                valueArray[i] = value;
+                return;
+            }
+        }
+        keyArray[freeIndex] = key;
+        valueArray[freeIndex++] = value;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int i = 0;
+        while (i < freeIndex) {
+            if ((keyArray[i] != null
+                    && keyArray[i].equals(key))
+                    || (keyArray[i] == null
+                    && keyArray[i] == key)) {
+                break;
+            } else {
+                i++;
+            }
+        }
+        if (i >= freeIndex) {
+            return null;
+        }
+        return valueArray[i];
     }
 
     @Override
     public int size() {
-        return -1;
+        return freeIndex;
     }
 }
