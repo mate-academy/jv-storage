b5f204fec38bfebf886db02c208eabc10ea63d79
Misha Polunin <polunin1986@gmail.com>
2022-02-28 18:09:59 +0200

    - fixed

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,7 +1,6 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static int MAX_ARRAY_VALUE = 10;
@@ -30,7 +29,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(keys[i],key)) {
+            if ((keys[i] != null && keys[i].equals(key)) || keys[i] == key) {
                 return values[i];
             }
         }
