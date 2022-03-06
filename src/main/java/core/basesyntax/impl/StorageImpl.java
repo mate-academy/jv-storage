67c95fba74b5ff8183ea19ea42bc81666fd32681
Mariana Trinko <marjanavv@gmail.com>
2022-03-06 18:01:03 +0100

    Storage - the first solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,13 +4,14 @@ import core.basesyntax.Storage;
 import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int length = 10;
     private K[] keys;
     private V[] values;
     private int size;
 
     public StorageImpl() {
-        keys = (K[]) new Object[10];
-        values = (V[]) new Object[10];
+        keys = (K[]) new Object[length];
+        values = (V[]) new Object[length];
         size = 0;
     }
 
