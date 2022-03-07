e3fdba2f5c4baacf57a8dfa9f8c89b1863a8886c
dmytro.sylenok <ulinmob@gmail.com>
2022-03-07 16:57:13 +0200

    changes requested on PR

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -7,24 +7,29 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public static final int MAX_SIZE = 10;
     private K[] keyArray = (K[]) new Object[MAX_SIZE];
     private V[] valueArray = (V[]) new Object[MAX_SIZE];
-    private int count;
+    private int size;
 
     @Override
     public void put(Object key, Object value) {
-        for (int i = 0; i < count; i++) {
+        if (checkIfAlreadyExists(key, (V) value)) return;
+        keyArray[size] = (K) key;
+        valueArray[size] = (V) value;
+        size++;
+    }
+
+    private boolean checkIfAlreadyExists(Object key, V value) {
+        for (int i = 0; i < size; i++) {
             if (keyArray[i] == key || key != null && key.equals(keyArray[i])) {
-                valueArray[i] = (V) value;
-                return;
+                valueArray[i] = value;
+                return true;
             }
         }
-        keyArray[count] = (K) key;
-        valueArray[count] = (V) value;
-        count++;
+        return false;
     }
 
     @Override
     public Object get(Object key) {
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < size; i++) {
             if (keyArray[i] == key || key != null && key.equals(keyArray[i])) {
                 return valueArray[i];
             }
@@ -34,6 +39,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return count;
+        return size;
     }
 }
