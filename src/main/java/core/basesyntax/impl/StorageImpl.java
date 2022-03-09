34d20efc8cd0890008d7c6faf933b937abdad5ed
Mariana Trinko <marjanavv@gmail.com>
2022-03-09 08:36:15 +0100

    Storage , name of constant

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,14 +4,14 @@ import core.basesyntax.Storage;
 import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int length = 10;
+    private static final int MAX_STORAGE_SIZE = 10;
     private K[] keys;
     private V[] values;
     private int size;
 
     public StorageImpl() {
-        keys = (K[]) new Object[length];
-        values = (V[]) new Object[length];
+        keys = (K[]) new Object[MAX_STORAGE_SIZE];
+        values = (V[]) new Object[MAX_STORAGE_SIZE];
     }
 
     @Override
