8bb0d769aa383d0fb660921a0db669f605c833b5
Iryna Hrynda <hryndairyna1@gmail.com>
2022-03-16 09:51:45 +0200

    - added KeyValue class - implemented methods: put, get and size in class StorageImpl

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,51 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE = 10;
+    private int size = 0;
+    private KeyValue<K, V>[] storage;
+
+    public StorageImpl() {
+        storage = new KeyValue[MAX_SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (size > 0) {
+            for (int i = 0; i < size; i++) {
+                if ((storage[i].getKey() == null && key == null)
+                        || (storage[i].getKey() != null && storage[i].getKey().equals(key))) {
+                    storage[i].setValue(value);
+                    return;
+                }
+            }
+        }
+
+        KeyValue<K, V> keyValue = new KeyValue<>(key, value);
+
+        if (size < MAX_SIZE) {
+            storage[size++] = keyValue;
+        }
     }
 
     @Override
     public V get(K key) {
+        if (size == 0) {
+            return null;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if ((storage[i].getKey() == null && key == null)
+                    || (storage[i].getKey() != null && storage[i].getKey().equals(key))) {
+                return storage[i].getValue();
+            }
+        }
+
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
