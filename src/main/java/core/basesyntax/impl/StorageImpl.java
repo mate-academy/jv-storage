9d4ce912cb32d8a1931460062ce089261682dd62
Oleksandr Domnich <dmnichz@gmail.com>
2022-03-16 16:30:57 +0200

    added solution for jv-storage task

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,51 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    static final int MAX_CAPACITY = 20;
+    private Object[] array;
+
+    public StorageImpl() {
+        this.array = new Object[0];
+    }
+
     @Override
     public void put(K key, V value) {
+        boolean isPresent = false;
+        if (array.length == 0) {
+            array = new Object[array.length + 2];
+            array[0] = key;
+            array[1] = value;
+        }
+        for (int i = 0; i < array.length; i = i + 2) {
+            if (array[i] == key || array[i] != null && array[i].equals(key)) {
+                array[i + 1] = value;
+                isPresent = true;
+            }
+        }
+        if (!isPresent) {
+            if (array.length < MAX_CAPACITY) {
+                Object[] newArray;
+                newArray = array;
+                array = new Object[array.length + 2];
+                System.arraycopy(newArray, 0, array, 0, newArray.length);
+                array[array.length - 2] = key;
+                array[array.length - 1] = value;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < array.length; i = i + 2) {
+            if (array[i] == key || array[i] != null && array[i].equals(key)) {
+                return (V) array[i + 1];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return array.length / 2;
     }
 }
