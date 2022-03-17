dc7125dda20dd94c755692ac38d865291aa1a69d
Oleksandr Domnich <dmnichz@gmail.com>
2022-03-17 12:06:52 +0200

    fixed solution for jv-storage task after review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,44 +3,34 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    static final int MAX_CAPACITY = 20;
-    private Object[] array;
+    private static final int MAX_CAPACITY = 10;
+    private K[] keys;
+    private V[] values;
+    private int size;
 
     public StorageImpl() {
-        this.array = new Object[0];
+        this.keys = (K[]) new Object[MAX_CAPACITY];
+        this.values = (V[]) new Object[MAX_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
-        boolean isPresent = false;
-        if (array.length == 0) {
-            array = new Object[array.length + 2];
-            array[0] = key;
-            array[1] = value;
-        }
-        for (int i = 0; i < array.length; i = i + 2) {
-            if (array[i] == key || array[i] != null && array[i].equals(key)) {
-                array[i + 1] = value;
-                isPresent = true;
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
+                values[i] = value;
+                return;
             }
         }
-        if (!isPresent) {
-            if (array.length < MAX_CAPACITY) {
-                Object[] newArray;
-                newArray = array;
-                array = new Object[array.length + 2];
-                System.arraycopy(newArray, 0, array, 0, newArray.length);
-                array[array.length - 2] = key;
-                array[array.length - 1] = value;
-            }
-        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < array.length; i = i + 2) {
-            if (array[i] == key || array[i] != null && array[i].equals(key)) {
-                return (V) array[i + 1];
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
+                return values[i];
             }
         }
         return null;
@@ -48,6 +38,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return array.length / 2;
+        return size;
     }
 }
