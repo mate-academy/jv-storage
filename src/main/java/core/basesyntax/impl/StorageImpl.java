3e5a175e0b1802e2c39a3555dcad0dea80b2967d
Denys.Sh <7860denis@gmail.com>
2022-03-18 09:35:23 +0200

    fix

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -16,7 +16,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         int indexArray = getIndexArray(key);
-        if (get(key) == null) {
+        if (indexArray == -1) {
             storage[index] = new Pair(key, value);
             index++;
         } else {
