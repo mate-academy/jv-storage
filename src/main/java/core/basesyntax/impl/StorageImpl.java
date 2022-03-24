4484b42ab304bc92a3fdf60b9dee139f9cce8f34
JadeRowan <rougesoul.m@gmail.com>
2022-03-24 23:48:35 +0200

    Methods are implemented. All goals completed

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,45 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int CAPACITY = 10;
+    private K[] keys = ((K[]) new Object[CAPACITY]);
+    private V[] values = ((V[]) new Object[CAPACITY]);
+    private int size = 0;
+
     @Override
     public void put(K key, V value) {
+        int keyIndex = keyIndex(key);
+        if (keyIndex == -1) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        } else {
+            values[keyIndex] = value;
+        }
+
     }
 
     @Override
     public V get(K key) {
+        int keyIndex = keyIndex(key);
+        if (keyIndex != -1) {
+            return values[keyIndex];
+        }
         return null;
     }
 
     @Override
     public int size() {
+        return size;
+    }
+
+    private int keyIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            K storedKey = keys[i];
+            if (storedKey != null && storedKey.equals(key) || storedKey == key) {
+                return i;
+            }
+        }
         return -1;
     }
 }
