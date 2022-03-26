96882505f21f89242e4ebf4b23b51d77f804ffa3
Dmytro Skarzhynets <d.skarzh@protonmail.com>
2022-03-26 16:54:28 +0200

    Storage HW Skarzhynets

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,44 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
+
+@SuppressWarnings("unchecked")
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int NOT_FOUND = -1;
+    private static final int MAX_SIZE = 10;
+    private int size;
+    private Pair<K, V>[] items = new Pair[MAX_SIZE];
+
     @Override
     public void put(K key, V value) {
+        int index;
+        if ((index = findIndexByKey(key)) != NOT_FOUND) {
+            items[index].setValue(value);
+            return;
+        }
+        items[size] = new Pair<>(key, value);
+        size++;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int index;
+        return (index = findIndexByKey(key)) == NOT_FOUND ? null : items[index].getValue();
+    }
+
+    private int findIndexByKey(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(key, items[i].getKey())) {
+                return i;
+            }
+        }
+        return NOT_FOUND;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
