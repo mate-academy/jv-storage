78fdc2acf6883add80ff0625add4a890a2f590b1
Dmytro Skarzhynets <d.skarzh@protonmail.com>
2022-03-28 17:59:44 +0300

    Refactor: various minor improvements

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -8,12 +8,16 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int NOT_FOUND = -1;
     private static final int MAX_SIZE = 10;
     private int size;
-    private Pair<K, V>[] items = new Pair[MAX_SIZE];
+    private Pair<K, V>[] items;
+
+    public StorageImpl() {
+        items = new Pair[MAX_SIZE];
+    }
 
     @Override
     public void put(K key, V value) {
-        int index;
-        if ((index = findIndexByKey(key)) != NOT_FOUND) {
+        int index = findIndexByKey(key);
+        if (index != NOT_FOUND) {
             items[index].setValue(value);
             return;
         }
@@ -23,14 +27,14 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        int index;
-        return (index = findIndexByKey(key)) == NOT_FOUND ? null : items[index].getValue();
+        int index = findIndexByKey(key);
+        return index == NOT_FOUND ? null : items[index].getValue();
     }
 
     private int findIndexByKey(K key) {
         for (int i = 0; i < size; i++) {
-            if ((items[i].getKey() == key)
-                    || (items[i].getKey() != null && items[i].getKey().equals(key))) {
+            if (items[i].getKey() == key
+                    || items[i].getKey() != null && items[i].getKey().equals(key)) {
                 return i;
             }
         }
