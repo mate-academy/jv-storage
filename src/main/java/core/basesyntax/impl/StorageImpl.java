3b74c03f8aa3f534008c0dd3cd04d6910fdbae98
DanyloTiazhev <finckreg@gmail.com>
2022-03-29 16:35:44 +0300

    Done?

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,18 +3,52 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    int b = 0;
+    private static final int STORAGE_CAPACITY = 10;
+
+    private K[] keyData = (K[]) new Object[STORAGE_CAPACITY];
+    private V[] valueData = (V[]) new Object[STORAGE_CAPACITY];
+    private int size = 0;
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (key != null && key.equals(keyData[i]) || key == keyData[i]) {
+                size--;
+                keyData[i] = key;
+                valueData[i] = value;
+            }
+        }
+        keyData[size] = key;
+        valueData[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (key != null && key.equals(keyData[i]) || key == keyData[i]) {
+                return (V) valueData[i];
+            }
+        }   return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
+
+/*
+@Override
+    public void put(K key, V value) {
+        if (!found(key)) {
+            keyData[size] = key;
+            valueData[size] = value;
+            size++;
+        }
+        keyData[(int) key] = key;
+        valueData[(int) key] = value;
+        size++;
+
+    }
+ */
