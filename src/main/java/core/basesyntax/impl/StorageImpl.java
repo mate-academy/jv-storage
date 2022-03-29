b1ec0f2da7f80bb38e8a18885519cf36cc0e33a0
Volodymyr Klimyuk <klimyuk054@gmail.com>
2022-03-29 16:57:14 +0300

    Implemented without Map

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,24 +1,46 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.HashMap;
-import java.util.Map;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private final Map<K, V> map = new HashMap<>();
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private Object[] arrayKey = new Object[MAX_ITEMS_NUMBER];
+    private Object[] arrayValue = new Object[MAX_ITEMS_NUMBER];
+    private int count = 0;
 
     @Override
     public void put(K key, V value) {
-        map.put(key, value);
+        for (int i = 0; i < count; i++) {
+            if (key == null && arrayKey[i] == null || key != null && key.equals(arrayKey[i])) {
+                arrayValue[i] = value;
+                return;
+            }
+        }
+        arrayKey[count] = key;
+        arrayValue[count] = value;
+        count++;
     }
 
     @Override
     public V get(K key) {
-        return map.get(key);
+        for (int i = 0; i < count; i++) {
+            if (key == null && arrayKey[i] == null || key != null && key.equals(arrayKey[i])) {
+                return (V) arrayValue[i];
+            }
+        }
+        return null;
     }
 
     @Override
     public int size() {
-        return map.size();
+        return count;
+    }
+    private boolean isRight(K key) {
+        for (int i = 0; i < count; i++) {
+            if (key == null && arrayKey[i] == null || key != null && key.equals(arrayKey[i])) {
+            return true;
+            }
+        }
+        return false;
     }
 }
