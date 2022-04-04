27de43724b29bb7629eaefd5a85b45d49be9710f
dombrovskiy2850@gmail.com <dombrovskiy2850@gmail.com>
2022-04-04 15:59:12 +0300

    fixed some mistakes after Q&A

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,44 +3,38 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private Object[] objectsK = new Object[10];
-    private Object[] objectsV = new Object[10];
+    private static final int MAX_SIZE = 10;
+    private Object[] objectsK = new Object[MAX_SIZE];
+    private Object[] objectsV = new Object[MAX_SIZE];
     private int size = 0;
 
     @Override
     public void put(K key, V value) {
-        objectsK[size] = (key == null) ? 1000 : key;
+        for (int i = 0; i < size; i++) {
+            if ((key == objectsK[i]) || (key != null && key.equals(objectsK[i]))) {
+                objectsV[i] = value;
+                return;
+            }
+        }
+        objectsK[size] = key;
         objectsV[size] = value;
         size++;
     }
 
     @Override
     public V get(K key) {
-        size++;
         Object result = new Object();
         Object cast = (Object) key;
-        if (key == null) {
-            for (int j = 0; j < size; j++) {
-                if (objectsK[j].equals(1000)) {
-                    result = objectsV[j];
-                    size--;
-                }
-            }
-            return (V)result;
-        }
-        result = null;
-        for (int j = 0; j < objectsK.length; j++) {
-            if (cast.equals(objectsK[j])) {
-                result = objectsV[j];
-                size--;
+        for (int j = 0; j < size; j++) {
+            if ((key == objectsK[j]) || (key != null && key.equals(objectsK[j]))) {
+                return (V)objectsV[j];
             }
         }
-        return (V)result;
+        return null;
     }
 
     @Override
     public int size() {
         return size;
     }
-
 }
