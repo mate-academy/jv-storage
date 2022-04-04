a5abfe335e174626880ff0978232a9d361d63e07
OLEH VIESHAIEV <sred_a@i.ua>
2022-04-04 14:54:20 +0300

    fixed checkstyle. access modifier in method getIndex() was changed and method was moved after public methods.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,8 +4,8 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_NUMBER_ELEMENTS = 10;
-    private K [] keys;
-    private V [] values;
+    private K[] keys;
+    private V[] values;
     private int size;
 
     public StorageImpl() {
@@ -16,7 +16,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         int index = getIndex(key);
-        if (index >= 0) {
+        if (index != -1) {
             values[index] = value;
             return;
         }
@@ -25,19 +25,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         size++;
     }
 
-    public int getIndex(K key) {
-        for (int i = 0; i < size; i++) {
-            if (key == keys[i] || key != null && key.equals(keys[i])) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
     @Override
     public V get(K key) {
         int index = getIndex(key);
-        if (index >= 0) {
+        if (index != -1) {
             return values[index];
         }
         return null;
@@ -46,5 +37,14 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return size;
+    }
+
+    private int getIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
