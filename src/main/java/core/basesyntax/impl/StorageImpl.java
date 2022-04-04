135e6a2159e4a0daeace1caaf8dfc27f0da362cf
Dmytro Koliada <dmitrikstar@gmail.com>
2022-04-04 09:38:46 +0300

    rename and transferd private  method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,48 +4,48 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_VALUE = 10;
-    private int resultSize;
-    private K[] keyResult;
-    private V[] valueResult;
+    private int size;
+    private K[] keys;
+    private V[] values;
 
     public StorageImpl() {
-        keyResult = ((K[]) new Object[MAX_VALUE]);
-        valueResult = ((V[]) new Object[MAX_VALUE]);
-    }
-
-    private int keySize(K key) {
-        for (int i = 0; i < resultSize; i++) {
-            K keyArrays = keyResult[i];
-            if (keyArrays == key || keyArrays != null && keyArrays.equals(key)) {
-                return i;
-            }
-        }
-        return -1;
+        keys = ((K[]) new Object[MAX_VALUE]);
+        values = ((V[]) new Object[MAX_VALUE]);
     }
 
     @Override
     public void put(K key, V value) {
-        int keySize = keySize(key);
+        int keySize = getSize(key);
         if (keySize == -1) {
-            keyResult[resultSize] = key;
-            valueResult[resultSize] = value;
-            resultSize++;
+            keys[size] = key;
+            values[size] = value;
+            size++;
         } else {
-            valueResult[keySize] = value;
+            values[keySize] = value;
         }
     }
 
     @Override
     public V get(K key) {
-        int keySize = keySize(key);
+        int keySize = getSize(key);
         if (keySize != -1) {
-            return valueResult[keySize];
+            return values[keySize];
         }
         return null;
     }
 
     @Override
     public int size() {
-        return resultSize;
+        return size;
+    }
+
+    private int getSize(K key) {
+        for (int i = 0; i < size; i++) {
+            K keyArrays = keys[i];
+            if (keyArrays == key || keyArrays != null && keyArrays.equals(key)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
