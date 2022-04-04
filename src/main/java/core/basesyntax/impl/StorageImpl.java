8baac133baf3c74a99936cfdfbe0e5ced75c0408
Dmytro Skrypnyk <skripnik139@gmail.com>
2022-04-04 10:20:18 +0300

    Add constructor

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,13 +5,18 @@ import core.basesyntax.Storage;
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_LENGTH = 10;
     private static final int NOT_FOUND = -1;
-    private K[] keysArray = (K[]) new Object[MAX_LENGTH];
-    private V[] valuesArray = (V[]) new Object[MAX_LENGTH];
-    private int size = 0;
+    private K[] keysArray;
+    private V[] valuesArray;
+    private int size;
+
+    public StorageImpl() {
+        keysArray = (K[]) new Object[MAX_LENGTH];
+        valuesArray = (V[]) new Object[MAX_LENGTH];
+    }
 
     @Override
     public void put(K key, V value) {
-        int currentIndex = indexKey(key);
+        int currentIndex = foundIndexKey(key);
         if (currentIndex == NOT_FOUND) {
             keysArray[size] = key;
             valuesArray[size] = value;
@@ -23,7 +28,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        int currentIndex = indexKey(key);
+        int currentIndex = foundIndexKey(key);
         if (currentIndex != NOT_FOUND) {
             return valuesArray[currentIndex];
         }
@@ -35,7 +40,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         return size;
     }
 
-    public int indexKey(K key) {
+    public int foundIndexKey(K key) {
         for (int i = 0; i < size; i++) {
             if ((keysArray[i] == key) || (keysArray[i] != null && keysArray[i].equals(key))) {
                 return i;
