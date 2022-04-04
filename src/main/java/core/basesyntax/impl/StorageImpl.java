02d6b221c0ea24729e22dc98a2690dec0dec6c4d
Nickolai Vakulich <nickolai.vakulich@gmail.com>
2022-04-04 11:58:42 +0300

    fixes PR

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,18 +1,22 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_NUMBER_OF_ELEMENTS = 10;
-    private K[] keys = (K[]) new Object[MAX_NUMBER_OF_ELEMENTS];
-    private V[] values = (V[]) new Object[MAX_NUMBER_OF_ELEMENTS];
-    private int size = 0;
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+        values = (V[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+    }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(key, keys[i])) {
+            if ((key == keys[i]) || (key != null && key.equals(keys[i]))) {
                 values[i] = value;
                 return;
             }
@@ -25,7 +29,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < keys.length; i++) {
-            if (Objects.equals(key, keys[i])) {
+            if ((key == keys[i]) || (key != null && key.equals(keys[i]))) {
                 return values[i];
             }
         }
