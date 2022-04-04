2564d11e38504c8262a01ab46cc9957dacf53999
Dmytro Koliada <dmitrikstar@gmail.com>
2022-04-04 08:08:25 +0300

    remove list and set, added arrays

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,41 +1,51 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private List<K> keyResult = new ArrayList<>();
-    private List<V> valueResult = new ArrayList<>();
+    private static final int MAX_VALUE = 10;
+    private int resultSize;
+    private K[] keyResult;
+    private V[] valueResult;
+
+    public StorageImpl() {
+        keyResult = ((K[]) new Object[MAX_VALUE]);
+        valueResult = ((V[]) new Object[MAX_VALUE]);
+    }
+
+    private int keySize(K key) {
+        for (int i = 0; i < resultSize; i++) {
+            K keyArrays = keyResult[i];
+            if (keyArrays == key || keyArrays != null && keyArrays.equals(key)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
     @Override
     public void put(K key, V value) {
-        valueResult.add(value);
-        keyResult.add(key);
-        for (int i = 0; i < keyResult.size(); i++) {
-            if (key == keyResult.get(i) || key != null && key.equals(keyResult.get(i))) {
-                valueResult.set(i, value);
-            }
+        int keySize = keySize(key);
+        if (keySize == -1) {
+            keyResult[resultSize] = key;
+            valueResult[resultSize] = value;
+            resultSize++;
+        } else {
+            valueResult[keySize] = value;
         }
-        Set<V> set = new LinkedHashSet<>(valueResult);
-        valueResult.clear();
-        valueResult.addAll(set);
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keyResult.size(); i++) {
-            if (key == keyResult.get(i) || key != null && key.equals(keyResult.get(i))) {
-                return valueResult.get(i);
-            }
+        int keySize = keySize(key);
+        if (keySize != -1) {
+            return valueResult[keySize];
         }
         return null;
     }
 
     @Override
     public int size() {
-        return valueResult.size();
+        return resultSize;
     }
 }
