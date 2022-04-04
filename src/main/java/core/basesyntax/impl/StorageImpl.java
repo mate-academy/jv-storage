5d658bed3b5e5637799b886cfee3d7a1917622dd
Andrii Fabersky <Andriifabersky@gmail.com>
2022-04-04 19:28:18 +0300

    I did homework

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,48 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_STORAGE_NUM = 10;
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_STORAGE_NUM];
+        values = (V[]) new Object[MAX_STORAGE_NUM];
+    }
+
+    private int keyIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            K storedKey = keys[i];
+            if (storedKey != null && storedKey.equals(key) || storedKey == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     @Override
     public void put(K key, V value) {
+        int keyIndex = keyIndex(key);
+        if (keyIndex == -1) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        } else {
+            values[keyIndex] = value;
+        }
     }
 
     @Override
     public V get(K key) {
+        if (keyIndex(key) != -1) {
+            return values[keyIndex(key)];
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
