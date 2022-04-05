c16199ecfd341a9eeea0b519e1615bc2f81a2ff3
Vladyslav Fomenko <with.god.victorius@gmail.com>
2022-04-05 15:18:02 +0300

    fixed mistakes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,20 +3,20 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int SIZE = 10;
+    private static final int MAX_SIZE_STORAGE = 10;
     private Object[] keys;
     private Object[] values;
     private int size;
 
     public StorageImpl() {
-        keys = new Object[SIZE];
-        values = new Object[SIZE];
+        keys = new Object[MAX_SIZE_STORAGE];
+        values = new Object[MAX_SIZE_STORAGE];
     }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if ((key == keys[i]) || (key != null && key.equals(keys[i]))) {
+            if (key == keys[i] || (key != null && key.equals(keys[i]))) {
                 values[i] = value;
                 return;
             }
@@ -29,7 +29,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if ((key == keys[i]) || (key != null && key.equals(keys[i]))) {
+            if (key == keys[i] || (key != null && key.equals(keys[i]))) {
                 return (V) values[i];
             }
         }
