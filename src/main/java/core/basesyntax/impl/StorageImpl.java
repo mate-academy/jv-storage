666d64048072a810cd38b5cfc17af883b8e30a5b
GrabenkoVitaliy <grabenkovitaliy7@gmail.com>
2022-04-05 16:29:25 +0300

    hw was added

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,49 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private static final int ARRAY_LENGTH = 10;
+
+    private K[] keyArray;
+    private V[] valueArray;
+    private int size = 0;
+
+    public StorageImpl() {
+        this.keyArray = (K[]) new Object[ARRAY_LENGTH];
+        this.valueArray = ( V[]) new Object[ARRAY_LENGTH];
+    }
+
+    private int getIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keyArray[i] == key || (keyArray[i] != null && keyArray[i].equals(key))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     @Override
     public void put(K key, V value) {
+        if (getIndex(key) == -1) {
+            valueArray[size] = value;
+            keyArray[size] = key;
+            size++;
+        } else {
+            int index = getIndex(key);
+            valueArray[index] = value;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        if (getIndex(key) == -1) {
+            return null;
+        }
+        return valueArray[getIndex(key)];
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
