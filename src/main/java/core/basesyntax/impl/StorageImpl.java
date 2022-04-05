8fc630dee08b87668123351313bb6944cece164f
Gigawatt94 <101740223+Gigawatt94@users.noreply.github.com>
2022-04-05 16:59:41 +0300

    rename index

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -15,9 +15,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        int i = getIndex(key);
-        if (i >= 0) {
-            values[i] = value;
+        int index = getIndex(key);
+        if (index >= 0) {
+            values[index] = value;
             return;
         }
         keys[size] = key;
@@ -27,9 +27,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        int i = getIndex(key);
-        if (i >= 0) {
-            return values[i];
+        int index = getIndex(key);
+        if (index >= 0) {
+            return values[index];
         }
         return null;
     }
