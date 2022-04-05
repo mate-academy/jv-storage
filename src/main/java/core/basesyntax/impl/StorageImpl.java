e9b60942a2a69787f0fe28c90b2a9a390607621f
Andrii Fabersky <Andriifabersky@gmail.com>
2022-04-05 13:07:01 +0300

    I fix mistake

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -13,24 +13,16 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         values = (V[]) new Object[MAX_STORAGE_NUM];
     }
 
-    private int getKeyIndex(K key) {
-        for (int i = 0; i < size; i++) {
-            if (keys[i] != null && keys[i].equals(key) || keys[i] == key) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
     @Override
     public void put(K key, V value) {
-        if (getKeyIndex(key) == -1) {
+        int keyIndex = getKeyIndex(key);
+        if (keyIndex == -1) {
             keys[size] = key;
             values[size] = value;
             size++;
             return;
         }
-        values[getKeyIndex(key)] = value;
+        values[keyIndex] = value;
     }
 
     @Override
@@ -44,5 +36,16 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return size;
+    }
+
+    private int getKeyIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            K keyIndex = keys[i];
+            if (keyIndex != null && keyIndex.equals(key)
+                    || keyIndex == key) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
