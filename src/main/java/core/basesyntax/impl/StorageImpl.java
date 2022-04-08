4f075a1264bcd56ce491055c62ac5d1c5552d429
Mark Shapka <mark.shapka1@gmail.com>
2022-04-08 14:34:22 +0300

    - was implemented the Storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,46 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_VALUE_SIZE = 10;
+    private Object[] keys = new Object[MAX_VALUE_SIZE];
+    private Object[] values = new Object[MAX_VALUE_SIZE];
+    private int count = 0;
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < count; i++) {
+            if ((key == null && key == keys[i])) {
+                values[i] = value;
+                return;
+            } else if (key == null) {
+                break;
+            } else if (key == keys[i] || key.equals(keys[i])) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[count] = key;
+        values[count] = value;
+        count++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < count; i++) {
+            if (key == null) {
+                if (key == keys[i]) {
+                    return (V) values[i];
+                }
+                continue;
+            } else if (key == keys[i] || key.equals(keys[i])) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return count;
     }
 }
