4f85287bc21f6ca8c9857512c0faffac0f7e9ed4
Sergii Turchyn <turchyn13@gmail.com>
2022-04-10 18:50:26 +0300

    initial commit

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,66 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_STORAGE_SIZE = 10;
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_STORAGE_SIZE];
+        values = (V[]) new Object[MAX_STORAGE_SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] == null) {
+                if (values[i] == null) {
+                    keys[i] = key;
+                    values[i] = value;
+                    size++;
+                    break;
+                } else if (key == null) {
+                    values[i] = value;
+                } else {
+                    continue;
+                }
+            } else if (keys[i] == key || keys[i].equals(key)) {
+                values[i] = value;
+                break;
+            }
+
+
+//            if (keys[i] != null && values[i] != null) {
+//                if (keys[i].equals(key)) {
+//                    values[i] = value;
+//                    break;
+//                }
+//            } else if (keys[i] == null && values[i] != null) {
+//                values[i] = value;
+//                break;
+//            } else {
+//                keys[i] = key;
+//                values[i] = value;
+//                size++;
+//                break;
+//            }
+        }
     }
 
     @Override
     public V get(K key) {
+        V var = null;
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] == key || (keys[i] != null && keys[i].equals(key))) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
