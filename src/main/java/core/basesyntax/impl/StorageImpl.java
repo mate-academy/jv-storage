17924911d1e181e0e8df92df084d164bbdfd11c3
SerhiiPankov <pankoff.serg@gmail.com>
2022-04-11 12:04:23 +0300

    fix bags for jv-storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,32 +3,34 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int h = 5;
-    private K key;
-    private V value;
-    private final Object[][] storage = new Object[2][10];
+    private static final int h = 10;
+    private int size;
+    private final K[] storageK = (K[]) new Object[h];
+    private final V[] storageV = (V[]) new Object[h];
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i <= size(); i++) {
-            if (storage[0][i] != null && storage[0][i].equals(key)) {
-                storage[1][i] = value;
+            if (storageK[i] != null && storageK[i].equals(key)) {
+                storageV[i] = value;
                 break;
             }
-            if (storage[0][i] == null && storage[1][i] == null) {
-                storage[0][i] = key;
-                storage[1][i] = value;
+            if (storageK[i] == null && storageV[i] == null) {
+                storageK[i] = key;
+                storageV[i] = value;
+                size++;
                 break;
             }
             if (key == null && value != null
-                    && storage[0][i] == null && storage[1][i] != null
-                    && !storage[1][i].equals(value)) {
-                storage[0][i] = key;
-                storage[1][i] = value;
+                    && storageK[i] == null && storageV[i] != null
+                    && !storageV[i].equals(value)) {
+                storageK[i] = key;
+                storageV[i] = value;
                 break;
             }
-            if (key == null && value != null && storage[0][i] == null && storage[1][i] == null) {
-                storage[1][i] = value;
+            if (key == null && value != null && storageK[i] == null && storageV[i] == null) {
+                storageV[i] = value;
+                size++;
                 break;
             }
 
@@ -38,11 +40,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i <= size(); i++) {
-            if (storage[0][i] != null && storage[0][i].equals(key)) {
-                return (V) storage[1][i];
-            }
-            if (key == null && storage[0][i] == null && storage[1][i] != null) {
-                return (V) storage[1][i];
+            if ((storageK[i] != null && storageK[i].equals(key))
+                    || (key == null && storageK[i] == null && storageV[i] != null)) {
+                return storageV[i];
             }
         }
         return null;
@@ -50,12 +50,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        int size = 0;
-        for (int i = 0; i < h; i++) {
-            if (storage[0][i] != null || storage[1][i] != null) {
-                size++;
-            }
-        }
         return size;
     }
 }
