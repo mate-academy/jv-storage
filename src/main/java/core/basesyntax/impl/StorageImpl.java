67a1e7974e7afb9445adfbff44573965c86a749e
Dima Davydenko <dmytro.davydenko89@gmail.com>
2022-04-14 09:43:47 +0300

    the solution was implemented

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,57 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private Object[] keys;
+    private Object[] values;
+
+    public StorageImpl() {
+        keys = new Object[MAX_ITEMS_NUMBER];
+        values = new Object[MAX_ITEMS_NUMBER];
+    }
+
     @Override
     public void put(K key, V value) {
+        int i = 0;
+        while (i < MAX_ITEMS_NUMBER) {
+            if (keys[i] == null && values[i] == null
+                    || keys[i] == null && key == null
+                    || keys[i] != null && keys[i].equals(key)) {
+                keys[i] = key;
+                values[i] = value;
+                break;
+            } else {
+                i++;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int j = 0;
+        while (j < MAX_ITEMS_NUMBER) {
+            if (keys[j] != null && keys[j].equals(key)) {
+                break;
+            } else if (keys[j] == null && key == null) {
+                break;
+            } else {
+                j++;
+            }
+        }
+        return j == MAX_ITEMS_NUMBER ? null : (V) values[j];
     }
 
     @Override
     public int size() {
-        return -1;
+        int i = 0;
+        while (i < MAX_ITEMS_NUMBER) {
+            if (keys[i] != null || values[i] != null) {
+                i++;
+            } else {
+                break;
+            }
+        }
+        return i;
     }
 }
