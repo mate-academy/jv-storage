f9d70ea11dfd5b5f642cc911473207f36e542a1b
Vitalii Smahlenko <smaglenko@gmail.com>
2022-04-15 14:31:04 +0300

    Changes after mentor's advice

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,46 +4,35 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAXIMUM_CAPACITY = 10;
-    private K[] key;
-    private V[] value;
-    private int storageSpaceCounter;
+    private K[] keys;
+    private V[] values;
+    private int size;
 
     public StorageImpl() {
-        this.key = (K[]) new Object[MAXIMUM_CAPACITY];
-        this.value = (V[]) new Object[MAXIMUM_CAPACITY];
+        keys = (K[]) new Object[MAXIMUM_CAPACITY];
+        values = (V[]) new Object[MAXIMUM_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < this.key.length; i++) {
-            if (key != null && this.key[i] != null) {
-                if (this.key[i].equals(key)) {
-                    this.value[i] = value;
-                    storageSpaceCounter--;
-                }
-            }
-            if (this.key[i] == key && this.value[i] != null) {
-                this.value[i] = value;
-                break;
-            }
-            if (this.key[i] == null && this.value[i] == null) {
-                this.key[i] = key;
-                this.value[i] = value;
-                storageSpaceCounter++;
-                break;
+        for (int i = 0; i < this.keys.length; i++) {
+            if ((this.keys[i] != null && this.keys[i].equals(key))
+                    || (this.keys[i] == key && this.values[i] != null)) {
+                this.values[i] = value;
+                size--;
             }
         }
+        this.keys[size] = key;
+        this.values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < this.key.length; i++) {
-            if (key != null && this.key[i] != null) {
-                if (this.key[i].equals(key)) {
-                    return value[i];
-                }
-            } else if (this.key[i] == key) {
-                return value[i];
+        for (int i = 0; i < this.keys.length; i++) {
+            if ((this.keys[i] != null && this.keys[i].equals(key))
+                    || this.keys[i] == key) {
+                return values[i];
             }
         }
         return null;
@@ -51,6 +40,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return storageSpaceCounter;
+        return size;
     }
 }
