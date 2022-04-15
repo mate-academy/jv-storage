e7492305dfbe9a717729a4dc91ad1610198021b3
Alex <alexoriobel@gmail.com>
2022-04-15 23:55:18 +0300

    After re review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,24 +1,54 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.HashMap;
-import java.util.Map;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private Map<K, V> data = new HashMap<>();
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private Object[] keys;
+    private Object[] values;
+    private int count;
+
+    public StorageImpl() {
+        keys = new Object[MAX_ITEMS_NUMBER];
+        values = new Object[MAX_ITEMS_NUMBER];
+        count = 0;
+    }
 
     @Override
     public void put(K key, V value) {
-        data.put(key, value);
+        boolean temp = false;
+        int index = 0;
+        for (int i = 0; i < keys.length; i++) {
+            if ((keys[i] == null ? keys[i] == key : keys[i].equals(key))
+                    && values[i] != null) {
+                temp = true;
+                index = i;
+            }
+        }
+        if (!temp) {
+            keys[count] = key;
+            values[count] = value;
+            count++;
+        } else {
+            keys[index] = key;
+            values[index] = value;
+        }
     }
 
     @Override
     public V get(K key) {
-        return data.get(key);
+        int index = 0;
+        for (int i = 0; i < keys.length; i++) {
+            if ((keys[i] == null ? keys[i] == key : keys[i].equals(key))
+                    && values[i] != null) {
+                index = i;
+            }
+        }
+        return (V) values[index];
     }
 
     @Override
     public int size() {
-        return data.size();
+        return count;
     }
 }
