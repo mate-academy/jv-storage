66daffb9ad22f7cc5ddf4373b38087d9b8e5c13e
agash <alexgash8@gmail.com>
2022-04-16 09:53:55 +0300

    ready

--- src/main/java/core/basesyntax/Storage.java
+++ src/main/java/core/basesyntax/Storage.java
@@ -2,6 +2,8 @@ package core.basesyntax;
 
 public interface Storage<K, V> {
     void put(K key, V value);
+
     V get(K key);
+
     int size();
 }
--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,41 +2,59 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_CAPACITY = 10;
-    private K[] keys;
-    private V[] values;
+    private Object[] keys;
+    private Object[] values;
 
     public StorageImpl() {
-        keys = new K(MAX_CAPACITY);
-        values = new V[MAX_CAPACITY];
+        keys = (K[])new Object[MAX_CAPACITY];
+        values = (V[]) new Object[MAX_CAPACITY];
     }
+
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < 10; i++) {
             if (keys[i] == null & values[i] == null) {
                 keys[i] = key;
                 values[i] = value;
+                break;
+            }
+            if (keys[i] == null & key == null) {
+                values[i] = value;
+                break;
+            }
+            if (keys[i] != null) {
+                if (keys[i].equals(key)) {
+                    values[i] = value;
+                    break;
+                }
             }
         }
     }
+
     @Override
     public V get(K key) {
         for (int i = 0; i < 10; i++) {
-            if (keys[i] == key) {
-                return values[i];
+            if (keys[i] == null) {
+                if (key == null) {
+                    return (V) values[i];
+                } else {
+                    continue;
+                }
+            }
+            if (keys[i].equals(key)) {
+                return (V) values[i];
             }
         }
         return null;
     }
+
     @Override
     public int size() {
         int counter = 0;
         for (int i = 0; i < MAX_CAPACITY; i++) {
-            if (keys[i] != null & values[i] != null) {
+            if (keys[i] != null || values[i] != null) {
                 counter++;
             }
         }
