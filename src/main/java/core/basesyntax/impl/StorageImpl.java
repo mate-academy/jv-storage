fb451b010a7bfa7178f6f0f75ee32d77b085c5e2
Vasiliy Sheretko <vasil.sheretko@yahoo.com>
2022-04-16 20:33:12 +0300

    Added StorageImpl class implementation

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,35 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int LIST_CAPACITY = 10;
+    private List<K> keys = new ArrayList<>(LIST_CAPACITY);
+    private List<V> values = new ArrayList<>(LIST_CAPACITY);
+
+    public StorageImpl() {
+    }
+
     @Override
     public void put(K key, V value) {
+        if (keys.contains(key)) {
+            values.remove(keys.indexOf(key));
+            values.add(keys.indexOf(key), value);
+        } else {
+            keys.add(key);
+            values.add(value);
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return keys.contains(key) ? values.get(keys.indexOf(key)) : null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return (keys.size() == values.size() ? keys.size() : 0);
     }
 }
