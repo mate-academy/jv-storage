c68d2e98fc73ffab08e4ae4c3ccd692edfb79147
Vasiliy Sheretko <vasil.sheretko@yahoo.com>
2022-04-17 13:15:07 +0300

    Implemented alternative solution using arrays

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,35 +1,52 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.ArrayList;
-import java.util.List;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int LIST_CAPACITY = 10;
-    private List<K> keys = new ArrayList<>(LIST_CAPACITY);
-    private List<V> values = new ArrayList<>(LIST_CAPACITY);
+    private static final int MAX_CAPACITY = 10;
+    private K[] keys;
+    private V[] values;
+    private int size;
 
     public StorageImpl() {
+        keys = (K[])new Object[MAX_CAPACITY];
+        values = (V[])new Object[MAX_CAPACITY];
+        size = 0;
     }
 
     @Override
     public void put(K key, V value) {
-        if (keys.contains(key)) {
-            values.remove(keys.indexOf(key));
-            values.add(keys.indexOf(key), value);
-        } else {
-            keys.add(key);
-            values.add(value);
+        for (int i = 0; i < size; i++) {
+            if (key == null && keys[i] == null && values[i] != null
+                    || key != null && key.equals(keys[i])) {
+                values[i] = value;
+                return;
+            }
+            if (key == keys[i] && keys[i] == null && values[i] == null) {
+                values[i] = value;
+                size++;
+                return;
+            }
         }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        return keys.contains(key) ? values.get(keys.indexOf(key)) : null;
+        V value = null;
+        for (int i = 0; i < size; i++) {
+            if (key == null && key == keys[i] || key != null && key.equals(keys[i])) {
+                value = values[i];
+                break;
+            }
+        }
+        return value;
     }
 
     @Override
     public int size() {
-        return (keys.size() == values.size() ? keys.size() : 0);
+        return size;
     }
 }
