8b8f3abfaf88919d0da23a87c2ae0af67f4f2354
Alex <alexoriobel@gmail.com>
2022-04-20 18:39:05 +0300

    After re review v2

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,49 +6,41 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ITEMS_NUMBER = 10;
     private Object[] keys;
     private Object[] values;
-    private int count;
+    private int size;
 
     public StorageImpl() {
         keys = new Object[MAX_ITEMS_NUMBER];
         values = new Object[MAX_ITEMS_NUMBER];
-        count = 0;
     }
 
     @Override
     public void put(K key, V value) {
-        boolean temp = false;
-        int index = 0;
         for (int i = 0; i < keys.length; i++) {
             if ((keys[i] == null ? keys[i] == key : keys[i].equals(key))
                     && values[i] != null) {
-                temp = true;
-                index = i;
+                keys[i] = key;
+                values[i] = value;
+                return;
             }
         }
-        if (!temp) {
-            keys[count] = key;
-            values[count] = value;
-            count++;
-        } else {
-            keys[index] = key;
-            values[index] = value;
-        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        int index = 0;
         for (int i = 0; i < keys.length; i++) {
             if ((keys[i] == null ? keys[i] == key : keys[i].equals(key))
                     && values[i] != null) {
-                index = i;
+                return (V) values[i];
             }
         }
-        return (V) values[index];
+        return null;
     }
 
     @Override
     public int size() {
-        return count;
+        return size;
     }
 }
