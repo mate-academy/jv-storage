5e65e7d2e346ac11d6f09d0424b13e39ccf24115
Nikita Melnikov <bam19921303@gmail.com>
2022-04-26 20:01:19 +0300

    implementation StorageImpl class.  Add constructor  void method put() and method get() for recording and get value by key.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,46 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int SIZE_BOX = 10;
+    private final K[] keyBox;
+    private final V[] valueBox;
+    private int size = 0;
+
+    public StorageImpl() {
+        this.keyBox = (K[]) new Object[SIZE_BOX];
+        this.valueBox = (V[]) new Object[SIZE_BOX];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if ((key == null && key == keyBox[i]) || (key != null && key.equals(keyBox[i]))) {
+                valueBox[i] = value;
+                return;
+            }
+        }
+        keyBox[size] = key;
+        valueBox[size] = value;
+        size++;
+        System.out.println(Arrays.toString(keyBox));
+        System.out.println(Arrays.toString(valueBox));
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keyBox.length; i++) {
+            if (key == keyBox[i] || (keyBox[i] != null && keyBox[i].equals(key))) {
+                return valueBox[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
