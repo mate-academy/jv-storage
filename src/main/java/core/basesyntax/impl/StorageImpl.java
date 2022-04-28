2c6bebff3d2f70d4a54063fa48f3127ee079e98c
l_osyash <los.nazar24@gmail.com>
2022-04-28 15:49:40 +0300

    Added fixes after review.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,10 +3,15 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int MAX_LENGTH = 10;
-    private final Object[] values = new Object[MAX_LENGTH];
-    private final Object[] keys = new Object[MAX_LENGTH];
-    private int index = 0;
+    private static final int STORAGE_MAX_LENGTH = 10;
+    private final Object[] values;
+    private final Object[] keys;
+    private int index;
+
+    public StorageImpl() {
+        this.values = new Object[STORAGE_MAX_LENGTH];
+        this.keys = new Object[STORAGE_MAX_LENGTH];
+    }
 
     @Override
     public void put(K key, V value) {
