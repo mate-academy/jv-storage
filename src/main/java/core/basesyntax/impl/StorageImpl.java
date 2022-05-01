cd5a1d4de06da6ed05c422c9de5a1e13b5e0fb8b
Oleksandr Petrunovskyi <o.petrunovskyi@gmail.com>
2022-05-01 08:47:46 +0300

    the jv-storage task is completed and ready for check

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,39 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_STORAGE_VOLUME = 10;
+    private final Object[] keyArray = new Object[MAX_STORAGE_VOLUME];
+    private final Object[] valueArray = new Object[MAX_STORAGE_VOLUME];
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < keyArray.length; i++) {
+            if ((keyArray[i] == null && valueArray[i] == null)
+                    || (keyArray[i] == key || (keyArray[i] != null && keyArray[i].equals(key)))) {
+                keyArray[i] = key;
+                valueArray[i] = value;
+                break;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keyArray.length; i++) {
+            if (keyArray[i] == key || (keyArray[i] != null && keyArray[i].equals(key))) {
+                return (V) valueArray[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        for (int i = 0; i < valueArray.length; i++) {
+            if (valueArray[i] == null) {
+                return i;
+            }
+        }
+        return valueArray.length;
     }
 }
