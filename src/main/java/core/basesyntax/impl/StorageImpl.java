7d6179accb999df755756704b3bfd739d0f71d1f
Maksym <Maksym_Personal@ip-192-168-0-72.eu-central-1.compute.internal>
2022-05-05 22:35:04 +0300

    tests and stylechecks passed

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,43 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private final int maxArraySize = 10;
+    private K[] keys = (K[]) new Object[maxArraySize];
+    private V[] values = (V[]) new Object[maxArraySize];
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] == null && values[i] == null) {
+                keys[i] = key;
+                values[i] = value;
+                break;
+            } else if (keys[i] == key || (key != null && keys[i] != null && keys[i].equals(key))) {
+                values[i] = value;
+                break;
+            } else if (i == maxArraySize) {
+                System.out.println("You can't place a new key, dictionary is full");
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] == key || (key != null && keys[i] != null && key.equals(keys[i]))) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        for (int i = 0; i < keys.length; i++) {
+            if (values[i] == null) {
+                return i;
+            }
+        }
+        return maxArraySize;
     }
 }
