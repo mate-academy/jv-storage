6c400d9a31589aecd10e56a46f63d87d81e8495f
Fokin <Fokin1@ukr.net>
2022-05-07 11:55:08 +0200

    create class StorageImpl it creates an array of keys value

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,53 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int SIZE = 10;
+    private int count = 0;
+    private K[] keyBox = (K[]) new Object[SIZE];
+    private V[] keyValue = (V[]) new Object[SIZE];
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < keyBox.length; i++) {
+            if (keyValue[i] == null) {
+                keyBox[i] = key;
+                keyValue[i] = value;
+                break;
+            }
+            if (keyValue[i] != null && key == (K) keyBox[i]) {
+                keyValue[i] = value;
+                break;
+            }
+            if (key != null && key.equals((K) (keyBox[i]))) {
+                keyValue[i] = value;
+                break;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        V result = null;
+        for (int i = 0; i < keyBox.length; i++) {
+            if (key != null && key.equals((K) keyBox[i])) {
+                result = keyValue[i];
+                break;
+            }
+            if (key == (K) keyBox[i]) {
+                result = keyValue[i];
+                break;
+            }
+        }
+        return result;
     }
 
     @Override
     public int size() {
-        return -1;
+        for (int i = 0; i < keyBox.length; i++) {
+            if (keyValue[i] != null) {
+                count++;
+            }
+        }
+        return count;
     }
 }
