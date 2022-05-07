bc6c5854ea2141d74d353250931701c319f700ef
Maksym <Maksym_Personal@Dibrovs-MacBook-Pro.local>
2022-05-07 19:10:11 +0300

    ifs combined, else and zero length checks removed

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,36 +3,25 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int STORAGE_SIZE  = 10;
+    private static final int STORAGE_SIZE = 10;
     private int size;
-    private K[] keys = (K[]) new Object[maxArraySize];
-    private V[] values = (V[]) new Object[maxArraySize];
+    private K[] keys = (K[]) new Object[STORAGE_SIZE];
+    private V[] values = (V[]) new Object[STORAGE_SIZE];
 
     @Override
     public void put(K key, V value) {
-        if (size == 0) {
-            keys[size] = key;
-            values[size] = value;
-            size++;
-            return;
-        }
-
         for (int i = 0; i < size; i++) {
-            if (key == keys[i]) {
+            if (key == keys[i]
+                    || (key != null && keys[i] != null) && keys[i].equals(key)) {
                 keys[i] = key;
                 values[i] = value;
-                return;
-            } else if ((key != null && keys[i] != null) && keys[i].equals(key)) {
-                keys[i] = key;
-                values[i] = value;
-                return;
-            } else {
-                keys[size] = key;
-                values[size] = value;
-                size++;
                 return;
             }
         }
+        keys[size] = key;
+        values[size] = value;
+        size++;
+        return;
     }
 
     @Override
