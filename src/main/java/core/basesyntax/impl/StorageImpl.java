1a6ccfffa8b514a870f34ad78fbd18d41092256e
Fokin <Fokin1@ukr.net>
2022-05-13 06:22:34 +0200

    optimized the put, get, size method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,53 +3,36 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int SIZE = 10;
-    private int count = 0;
-    private K[] keyBox = (K[]) new Object[SIZE];
-    private V[] keyValue = (V[]) new Object[SIZE];
+    private static final int MAX_STORAGE_SIZE = 10;
+    private int count;
+    private K[] keys = (K[]) new Object[MAX_STORAGE_SIZE];
+    private V[] values = (V[]) new Object[MAX_STORAGE_SIZE];
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < keyBox.length; i++) {
-            if (keyValue[i] == null) {
-                keyBox[i] = key;
-                keyValue[i] = value;
-                break;
-            }
-            if (keyValue[i] != null && key == (K) keyBox[i]) {
-                keyValue[i] = value;
-                break;
-            }
-            if (key != null && key.equals((K) (keyBox[i]))) {
-                keyValue[i] = value;
-                break;
+        for (int i = 0; i < count; i++) {
+            if (key == (K) keys[i] || (key != null && key.equals((keys[i])))) {
+                values[i] = value;
+                return;
             }
         }
+        keys[count] = key;
+        values[count] = value;
+        count++;
     }
 
     @Override
     public V get(K key) {
-        V result = null;
-        for (int i = 0; i < keyBox.length; i++) {
-            if (key != null && key.equals((K) keyBox[i])) {
-                result = keyValue[i];
-                break;
-            }
-            if (key == (K) keyBox[i]) {
-                result = keyValue[i];
-                break;
+        for (int i = 0; i < keys.length; i++) {
+            if (key == (K) keys[i] || (key != null && key.equals(keys[i]))) {
+                return values[i];
             }
         }
-        return result;
+        return null;
     }
 
     @Override
     public int size() {
-        for (int i = 0; i < keyBox.length; i++) {
-            if (keyValue[i] != null) {
-                count++;
-            }
-        }
         return count;
     }
 }
