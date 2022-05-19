011c131f15c251223a79318408cd3b529ba07463
Mykola Dubich <nikolay19888@hotmail.com>
2022-05-19 21:39:43 +0300

    Implement class StorageImpl. Added  methods put(), get(), size(), setData()

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,59 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_STORAGE_SIZE = 10;
+    private Object[] keysStorage;
+    private Object[] valueStorage;
+    private int index;
+
+    public StorageImpl() {
+        keysStorage = new Object[MAX_STORAGE_SIZE];
+        valueStorage = new Object[MAX_STORAGE_SIZE];
+        index = 0;
+    }
+
+    private void setData(K key, V value, int index) {
+        keysStorage[index] = key;
+        valueStorage[index] = value;
+    }
+
     @Override
     public void put(K key, V value) {
+
+        if (index == 0) {
+            setData(key, value, index);
+            index++;
+            return;
+        }
+        for (int i = 0; i < index; i++) {
+            if (key == null && keysStorage[i] == null) {
+                setData(key, value, i);
+                return;
+            }
+            if (keysStorage[i] != null && keysStorage[i].equals(key)) {
+                setData(key, value, i);
+                return;
+            }
+        }
+        setData(key, value, index);
+        index++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < index; i++) {
+            if (keysStorage[i] == null && key == null) {
+                return (V) valueStorage[i];
+            }
+            if (keysStorage[i] != null && keysStorage[i].equals(key)) {
+                return (V) valueStorage[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return index;
     }
 }
