9a315f1ea8a33460b9cb905b321e816c29e7eac6
Mykola Dubich <nikolay19888@hotmail.com>
2022-05-20 11:40:04 +0300

    Fix variables names. Fixing loops in methods put() and get()

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,37 +2,35 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.Arrays;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_STORAGE_SIZE = 10;
-    private Object[] keysStorage;
-    private Object[] valueStorage;
+    private Object[] keys;
+    private Object[] values;
     private int index;
 
     public StorageImpl() {
-        keysStorage = new Object[MAX_STORAGE_SIZE];
-        valueStorage = new Object[MAX_STORAGE_SIZE];
+        keys = new Object[MAX_STORAGE_SIZE];
+        values = new Object[MAX_STORAGE_SIZE];
         index = 0;
     }
 
     private void setData(K key, V value, int index) {
-        keysStorage[index] = key;
-        valueStorage[index] = value;
+        keys[index] = key;
+        values[index] = value;
     }
 
     @Override
     public void put(K key, V value) {
-
         if (index == 0) {
             setData(key, value, index);
             index++;
             return;
         }
         for (int i = 0; i < index; i++) {
-            if (key == null && keysStorage[i] == null) {
-                setData(key, value, i);
-                return;
-            }
-            if (keysStorage[i] != null && keysStorage[i].equals(key)) {
+            if (key == null && keys[i] == null ||
+                    keys[i] != null && keys[i].equals(key)) {
                 setData(key, value, i);
                 return;
             }
@@ -44,11 +42,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < index; i++) {
-            if (keysStorage[i] == null && key == null) {
-                return (V) valueStorage[i];
-            }
-            if (keysStorage[i] != null && keysStorage[i].equals(key)) {
-                return (V) valueStorage[i];
+            if (keys[i] == null && key == null ||
+                    keys[i] != null && keys[i].equals(key)) {
+                return (V) values[i];
             }
         }
         return null;
