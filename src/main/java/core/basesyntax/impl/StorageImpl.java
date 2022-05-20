0be76105b1bf413085f636882175eb9d7bbbf8cc
Lev Ber <levberezov01@gmail.com>
2022-05-20 19:42:18 +0300

    removed excess loop and if statement, rename variables, add number 10 as constant field;

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,62 +3,43 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-
-    private K[] keyArray = (K[]) new Object[10];
-    private V[] valueArray = (V[]) new Object[10];
-    private int count = 0;
+    private static final int ARRAYS_SIZE = 10;
+    private K[] keys = (K[]) new Object[ARRAYS_SIZE];
+    private V[] values = (V[]) new Object[ARRAYS_SIZE];
+    private int size;
 
     @Override
     public void put(K key, V value) {
-        keyArray[count] = key;
-        valueArray[count] = value;
-        if (key != null) {
-            for (int i = 0; i < count; i++) {
-                if (keyArray[i] != null && keyArray[i].equals(keyArray[count])) {
-                    keyArray[i] = key;
-                    valueArray[i] = value;
-                    valueArray[count] = null;
-                    keyArray[count] = null;
-                    count--;
-                    break;
-                }
-            }
-        } else {
-            for (int i = 0; i < count; i++) {
-                if (keyArray[i] == keyArray[count]) {
-                    keyArray[i] = key;
-                    valueArray[i] = value;
-                    valueArray[count] = null;
-                    keyArray[count] = null;
-                    count--;
-                    break;
-                }
+        keys[size] = key;
+        values[size] = value;
+
+        for (int i = 0; i < size; i++) {
+            if ((keys[i] == null && keys[i] == keys[size]) || (keys[i]
+                    != null && keys[i].equals(keys[size]))) {
+                keys[i] = key;
+                values[i] = value;
+                values[size] = null;
+                keys[size] = null;
+                size--;
+                break;
             }
         }
-        count++;
+        size++;
     }
 
     @Override
     public V get(K key) {
         int find = 0;
-        if (key != null) {
-            for (; find < size(); find++) {
-                if (keyArray[find] != null && keyArray[find].equals(key)) {
-                    break;
-                }
-            }
-        } else {
-            for (; find < size(); find++) {
-                if (keyArray[find] == null) {
-                    break;
-                }
+        for (; find < size(); find++) {
+            if (keys[find] == key || keys[find] != null && keys[find].equals(key)) {
+                break;
             }
         }
-        return valueArray[find];
+        return values[find];
     }
 
     @Override
     public int size() {
-        return this.count;
+        return size;
     }
 }
