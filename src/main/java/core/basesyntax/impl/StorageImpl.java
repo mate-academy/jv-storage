312fa74c2682f2a75856cf4fe815bd6532bfc02d
Konstantin Miroshnichenko <bigfishnotbad@gmail.com>
2022-05-22 09:05:30 +0300

    methods put, get and size were implemented

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,18 +2,91 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.Objects;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private K[] keys;
+    private V[] values;
+
+    public StorageImpl() {
+        keys = (K[]) (new Object[10]);
+        values = (V[]) (new Object[10]);
+    }
+
     @Override
     public void put(K key, V value) {
+        if (key == null) {
+            int index = getIndexWithNullKey(null);
+            if (index != -1) {
+                values[index] = value;
+                return;
+            }
+        }
+        int index = getIndexByKey(key);
+        if (index != -1) {
+            keys[index] = key;
+            values[index] = value;
+            return;
+        }
+        if (size() == 10) { // max quantity elements in our storage
+            System.out.println("storage overloaded");
+            return;
+        }
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] == null && values[i] == null) {
+                keys[i] = key;
+                values[i] = value;
+                break;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        if (key == null) {
+            int index = getIndexWithNullKey(null);
+            if (index != -1) {
+                return values[index];
+            }
+            return null;
+        }
+        int index = getIndexByKey(key);
+        if (index != -1) {
+            return values[index];
+        }
         return null;
     }
 
     @Override
     public int size() {
+        int count = 0;
+        int index = getIndexWithNullKey(null);
+        if (index != -1) {
+            count++;
+        }
+        for (K key : keys) {
+            if (key != null) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private int getIndexWithNullKey(K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] == null && values[i] != null) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private int getIndexByKey(K key) {  //clause: keys were reviewed for null
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] != null && Objects.equals(keys[i], key)) {
+                return i;
+            }
+        }
         return -1;
     }
 }
