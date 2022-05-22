f8e3ed554e7a9fbf75abbaf5a44ec60e94bca6bb
Pavlo99-gif <kurulchyk7355608@gmail.com>
2022-05-22 14:51:41 +0300

    jv-storage-task-done

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,56 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private K[] key;
+    private V[] value;
+    private int size;
+
+    private void increaseArrays() {
+        size++;
+        if (size == 1) {
+            key = (K[]) new Object[size];
+            value = (V[]) new Object[size];
+        }
+        key = (K[]) Arrays.copyOf(key, size);
+        value = (V[]) Arrays.copyOf(value, size);;
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = keyIndex(key);
+        if (index == -1) {
+            increaseArrays();
+            this.key[size - 1] = key;
+            this.value[size - 1] = value;
+        } else {
+            this.value[index] = value;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int index = keyIndex(key);
+        if (index == -1) {
+            return null;
+        }
+        return value[index];
     }
 
     @Override
     public int size() {
+        return size;
+    }
+
+    private int keyIndex(K findKey) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(key[i], findKey)) {
+                return i;
+            }
+        }
         return -1;
     }
 }
