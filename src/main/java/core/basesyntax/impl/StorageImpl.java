a5c1c540b1efdabae1090743a61ece1b0aa841b6
Ivan Yuriv <yuriv1024@gmail.com>
2022-05-22 17:58:47 +0300

    methods get(), put() and size() was implemented.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,41 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAXIMUM_NUMBER_OF_ELEMENTS = 10;
+    private int size = 0;
+    private final Object[] keys;
+    private final Object[] values;
+
+    public StorageImpl() {
+        keys = new Object[MAXIMUM_NUMBER_OF_ELEMENTS];
+        values = new Object[MAXIMUM_NUMBER_OF_ELEMENTS];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == key || (key != null && key.equals(keys[i]))) {
+                values[i] = value;
+                return;
+            }
+        }
+        values[size] = value;
+        keys[size] = key;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == key || (key != null && key.equals(keys[i]))) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
