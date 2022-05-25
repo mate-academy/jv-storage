aa8f60b04ec5d3305e108141c460d497f5daddc6
Zinchuk-Yevhenii-Mate <zenya87@gmail.com>
2022-05-25 19:11:57 +0300

    used get(key)

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -16,15 +16,12 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < size; i++) {
-            if (key == keyArray[i] || (keyArray[i] != null && keyArray[i].equals(key))) {
-                valueArray[i] = value;
-                return;
-            }
+        if (get(key) == null) {
+            keyArray[size] = key;
+            valueArray[size] = value;
+            size++;
         }
-        keyArray[size] = key;
-        valueArray[size] = value;
-        size++;
+        valueArray[size - 1] = value;
     }
 
     @Override
