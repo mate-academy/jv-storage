2e0bd080b7ca9d032cd616acc129dd532f8c2524
Lev Ber <levberezov01@gmail.com>
2022-05-26 14:07:51 +0300

    added fixes: removed redundant variable, removed unnecessary overwriting of array elements

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,39 +3,34 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int ARRAYS_SIZE = 10;
-    private K[] keys = (K[]) new Object[ARRAYS_SIZE];
-    private V[] values = (V[]) new Object[ARRAYS_SIZE];
+    private static final int STORAGE_SIZE = 10;
+    private K[] keys = (K[]) new Object[STORAGE_SIZE];
+    private V[] values = (V[]) new Object[STORAGE_SIZE];
     private int size;
 
     @Override
     public void put(K key, V value) {
-        keys[size] = key;
-        values[size] = value;
-
         for (int i = 0; i < size; i++) {
-            if ((keys[i] == null && keys[i] == keys[size]) || (keys[i]
-                    != null && keys[i].equals(keys[size]))) {
+            if ((keys[i] == null && keys[i] == key) || (keys[i]
+                    != null && keys[i].equals(key))) {
                 keys[i] = key;
                 values[i] = value;
-                values[size] = null;
-                keys[size] = null;
-                size--;
-                break;
+                return;
             }
         }
+        keys[size] = key;
+        values[size] = value;
         size++;
     }
 
     @Override
     public V get(K key) {
-        int find = 0;
-        for (; find < size(); find++) {
-            if (keys[find] == key || keys[find] != null && keys[find].equals(key)) {
-                break;
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
+                return values[i];
             }
         }
-        return values[find];
+        return null;
     }
 
     @Override
