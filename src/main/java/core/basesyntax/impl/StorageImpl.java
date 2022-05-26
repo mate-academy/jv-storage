7899c48034d3c18ede8c2835114cd715a975d2bb
Maksym Karamavrov <makcumkaka@gmail.com>
2022-05-26 10:54:21 +0300

    Method get() was implemented to put method. Objects are not used. Downcasting is not used anymore.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,37 +1,35 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_STORAGE = 10;
-    private K[] keyArray;
-    private V[] valueArray;
-    private int count = 0;
+    private Object[] keyArray;
+    private Object[] valueArray;
+    private int count;
 
     public StorageImpl() {
-        keyArray = (K[]) new Object[MAX_STORAGE];
-        valueArray = (V[]) new Object[MAX_STORAGE];
+        keyArray = new Object[MAX_STORAGE];
+        valueArray = new Object[MAX_STORAGE];
+        count = 0;
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < count; i++) {
-            if (Objects.equals(key, keyArray[i])) {
-                valueArray[i] = value;
-                return;
-            }
+        if (get(key) == null) {
+            keyArray[count] = key;
+            valueArray[count] = value;
+            count++;
+        } else {
+            valueArray[count - 1] = value;
         }
-        keyArray[count] = key;
-        valueArray[count] = value;
-        count++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keyArray.length; i++) {
-            if (Objects.equals(key, keyArray[i])) {
-                return valueArray[i];
+        for (int i = 0; i < count; i++) {
+            if (key != null && key.equals(keyArray[i]) || key == keyArray[i]) {
+                return (V) valueArray[i];
             }
         }
         return null;
