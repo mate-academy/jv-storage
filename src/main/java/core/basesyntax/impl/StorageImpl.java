749d9e0c5c6f698bf5f660953daa40e3da950ceb
olyaklipa <olyaklipa@gmail.com>
2022-05-28 19:11:38 +0300

    The class StorageImpl was implemented.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,60 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int EMPTY = 0;
+    private static final int ROOM = 1;
+    private Object[] keys;
+    private Object[] values;
+
+    public StorageImpl() {
+        keys = new Object[EMPTY];
+        values = new Object[EMPTY];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (keys.length == EMPTY && values.length == EMPTY) {
+            add(key, value);
+        } else {
+            boolean exist = false;
+            for (int i = 0; i < keys.length; i++) {
+                if (key == null ? keys[i] == null : key.equals((K) keys[i])) {
+                    values[i] = value;
+                    exist = true;
+                    break;
+                }
+            }
+            if (!exist) {
+                add(key, value);
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if (key == null ? keys[i] == null : key.equals((K) keys[i])) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return keys.length;
+    }
+
+    private void add(K key, V value) {
+        Object[] newKeys = new Object[keys.length + ROOM];
+        Object[] newValues = new Object[values.length + ROOM];
+        for (int i = 0; i < keys.length; i++) {
+            newKeys[i] = keys[i];
+            newValues[i] = values[i];
+        }
+        newKeys[keys.length] = key;
+        newValues[values.length] = value;
+        keys = newKeys;
+        values = newValues;
     }
 }
