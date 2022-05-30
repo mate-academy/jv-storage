9dbed6fc5056b77eaddb521ccdf549dc5b333321
elenapokaliuk <100062370+elenapokaliuk@users.noreply.github.com>
2022-05-30 13:15:47 +0300

    added implementation for method get to check the storage already contains given key added implementation for method put to put new value and key to storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,44 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAXIMUM_STORAGE_ELEMENTS = 10;
+    private Object[] keys;
+    private Object[] values;
+    private int size;
+    private int getKeyIndex;
+
+    public StorageImpl() {
+        keys = new Object[MAXIMUM_STORAGE_ELEMENTS];
+        values = new Object[MAXIMUM_STORAGE_ELEMENTS];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (get(key) == null) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        } else {
+            values[getKeyIndex] = value;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(keys[i], key)) {
+                getKeyIndex = i;
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
