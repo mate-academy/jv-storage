0c7a65507b457e2c1af3f60e16241ef62e10a9a4
Ivan Kunitskiy <kunitskivan@gmail.com>
2022-05-30 11:38:05 +0300

    The names of the arrays were changed

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,30 +6,30 @@ import java.util.Objects;
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ARRAY_VALUE = 10;
     private int size = 0;
-    private final Object[] keyArray;
-    private final Object[] valueArray;
+    private final Object[] keys;
+    private final Object[] values;
 
     public StorageImpl() {
-        keyArray = new Object[MAX_ARRAY_VALUE];
-        valueArray = new Object[MAX_ARRAY_VALUE];
+        keys = new Object[MAX_ARRAY_VALUE];
+        values = new Object[MAX_ARRAY_VALUE];
     }
 
     @Override
     public void put(K key, V value) {
         if (get(key) == null) {
-            keyArray[size] = key;
-            valueArray[size] = value;
+            keys[size] = key;
+            values[size] = value;
             size++;
         } else {
-            valueArray[size - 1] = value;
+            values[size - 1] = value;
         }
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(key, keyArray[i])) {
-                return (V) valueArray[i];
+            if (Objects.equals(key, keys[i])) {
+                return (V) values[i];
             }
         }
         return null;
