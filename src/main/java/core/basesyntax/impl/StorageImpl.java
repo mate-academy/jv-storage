7e2c5dead0a661ce68514fa7609bbaa78a2dd0fa
Nikita Semisoshenko <jaderussell197@gmail.com>
2022-05-30 17:43:04 +0300

    all commented moments was implemented. Build success

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,24 +1,39 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int ARRAY_LENGTH = 10;
-    private Object[] keys = new Object[ARRAY_LENGTH];
-    private Object[] values = new Object[ARRAY_LENGTH];
+    private static Object SIZE_OBJECT;
+    private static final int LENGTH = 10;
+    private Object[] keys = new Object[LENGTH];
+    private Object[] values = new Object[LENGTH];
 
     @Override
     public void put(K key, V value) {
-        new PutValue<>(key, value, keys, values);
+        for (int i = 0; i < keys.length; i++) {
+            if ((values[i] == null)
+                    || (key == keys[i] && values[i] != null)
+                    || (key != null && key.equals(keys[i]))) {
+                keys[i] = key;
+                values[i] = value;
+                SIZE_OBJECT = value;
+                break;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
-        return (V) new GetValue<>(key, this.keys, this.values).valueOf();
+        try {
+            return (V) values[Arrays.asList(keys).indexOf(key)];
+        } catch (IndexOutOfBoundsException e) {
+            return null;
+        }
     }
 
     @Override
     public int size() {
-        return new GetSize(this.values).size();
+        return Arrays.asList(values).indexOf(SIZE_OBJECT) + 1;
     }
 }
