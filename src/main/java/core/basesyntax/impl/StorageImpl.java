26cc585560a478f51aeab77ca2c6b63a39b8479e
Sergii Uglovyi <s.uglovyi@gmail.com>
2022-06-03 20:44:37 +0300

    add solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,18 +2,47 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+@SuppressWarnings("unchecked")
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_PAIR_NUMBER = 10;
+    private int size = 0;
+    private final K[] keys;
+    private final V[] values;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_PAIR_NUMBER];
+        values = (V[]) new Object[MAX_PAIR_NUMBER];
+    }
+
     @Override
     public void put(K key, V value) {
+        int indexFoundKey = -1;
+        for (int i = 0; i < size; i++) {
+            if (keys[i] != null && keys[i].equals(key) || keys[i] == key) {
+                values[i] = value;
+                indexFoundKey = i;
+                break;
+            }
+        }
+        if (indexFoundKey < 0) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] != null && keys[i].equals(key) || keys[i] == key) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
