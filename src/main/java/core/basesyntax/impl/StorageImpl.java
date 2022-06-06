f5f16da1baf842a368457134ce13ec7f239bfbe6
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-06-06 16:58:53 +0300

    created and debugged StorageImpl

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,93 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_LENGTH = 10;
+    private StorageImpl[] arrayPair = new StorageImpl[MAX_LENGTH];
+    private int nextIndex;
+    private K key;
+    private V value;
+
+    public StorageImpl(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public StorageImpl() {
+
+    }
+
+    public int getKeyHashCode(K key) {
+        int result = 1618;
+        result = result + (key == null ? 0 : key.hashCode());
+        return result;
+    }
+
+    public boolean testArray(StorageImpl[] array) {
+        boolean result = false;
+        for (StorageImpl element : array) {
+            if (element != null) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    public boolean[] searchedKey(K key) {
+        boolean[] comparisonTable = new boolean[MAX_LENGTH];
+        if (testArray(arrayPair)) {
+            for (int index = 0; index < nextIndex; index++) {
+                int arrayKeyHash = getKeyHashCode((K) arrayPair[index].key);
+                int keyHash = getKeyHashCode(key);
+                if (arrayKeyHash == keyHash) {
+                    comparisonTable[index] = true;
+                } else {
+                    comparisonTable[index] = false;
+                }
+            }
+        }
+        return comparisonTable;
+    }
+
     @Override
     public void put(K key, V value) {
+        int currentIndex;
+        boolean checkPresent = false;
+        if (!testArray(arrayPair)) {
+            currentIndex = 0;
+            nextIndex = 0;
+            arrayPair[currentIndex] = new StorageImpl(key, value);
+            nextIndex++;
+        } else {
+            boolean[] comparisonTable = searchedKey(key);
+            for (currentIndex = 0; currentIndex < nextIndex; currentIndex++) {
+                if (comparisonTable[currentIndex]) {
+                    arrayPair[currentIndex].value = value;
+                    checkPresent = true;
+                }
+            }
+            if (!checkPresent && (nextIndex < MAX_LENGTH)) {
+                arrayPair[currentIndex] = new StorageImpl(key, value);
+                nextIndex++;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        if (testArray(arrayPair)) {
+            boolean[] comparisonTable = searchedKey(key);
+            int index;
+            for (index = 0; index < nextIndex; index++) {
+                if (comparisonTable[index]) {
+                    return (V) arrayPair[index].value;
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return nextIndex;
     }
 }
