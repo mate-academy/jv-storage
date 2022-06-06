7c4dba677682d7f41c9c64e59f45816ea2158464
Mark Godnyi <libertyinprogresslife@gmail.com>
2022-06-06 10:30:02 +0200

    Created getIndex method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,7 +4,6 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_STORAGE_SIZE = 10;
-    private static int pairCount;
     private int size;
     @SuppressWarnings("unchecked")
     private final Pair<K, V>[] pairs = new Pair[MAX_STORAGE_SIZE];
@@ -21,8 +20,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        if (get(key) != null) {
-            pairs[pairCount].value = value;
+        if (getIndex(key) != -1) {
+            pairs[getIndex(key)].value = value;
             return;
         }
         pairs[size] = new Pair<>(key, value);
@@ -31,15 +30,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        pairCount = -1;
-        StorageImpl<K, V> storage = new StorageImpl<>();
-        for (int i = 0; i < size; i++) {
-            pairCount++;
-            if (storage.compareKeys(pairs[i].key, key)) {
-                return pairs[i].value;
-            }
+        if (getIndex(key) == -1) {
+            return null;
         }
-        return null;
+        return pairs[getIndex(key)].value;
     }
 
     @Override
@@ -49,6 +43,16 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     private boolean compareKeys(K key, K otherKey) {
         return (key == null && otherKey == null) || (key != null && key.equals(otherKey));
+    }
+
+    private int getIndex(K key) {
+        StorageImpl<K, V> storage = new StorageImpl<>();
+        for (int i = 0; i < size; i++) {
+            if (storage.compareKeys(pairs[i].key, key)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
 
