1a311f0547cc336fc880e713ec2241b5d2cb6512
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-06-08 18:08:06 +0300

    corrected arrays

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,15 +2,15 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
-import java.security.Key;
-
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_LENGTH = 10;
     private int size;
     private K key;
     private V value;
-    private GenericArray<K> arrayKeys = new GenericArray(MAX_LENGTH);
-    private GenericArray<V> arrayValues = new GenericArray(MAX_LENGTH);
+    //private GenericArray<K> arrayKeys = new GenericArray(MAX_LENGTH);
+    //private GenericArray<V> arrayValues = new GenericArray(MAX_LENGTH);
+    private K[] keys = (K[]) new Object[MAX_LENGTH];
+    private V[] values = (V[]) new Object[MAX_LENGTH];
 
     public StorageImpl(K key, V value) {
         this.key = key;
@@ -21,33 +21,32 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     }
 
-    private class GenericArray<T> {
-        private Object[] arrayT;
+    //private class GenericArray<T> {
+        //private Object[] arrayT;
 
-        public GenericArray(int size) {
-            arrayT = new Object[size];
-        }
+        //public GenericArray(int size) {
+            //arrayT = new Object[size];
+        //}
 
-        public T getElement(int index) {
-            return (T) arrayT[index];
-        }
+        //public T getElement(int index) {
+            //return (T) arrayT[index];
+        //}
 
-        public void setElement(int index, T element) {
-            arrayT[index] = element;
-        }
-    }
+        //public void setElement(int index, T element) {
+            //arrayT[index] = element;
+        //}
+    //}
 
     public Integer searchedKey(K key) {
-        boolean condition;
         Integer result = null;
-            for (int index = 0; index < size; index++) {
-                if ((arrayKeys.getElement(index) == null || key == null)
-                    && arrayKeys.getElement(index) == key ) {
-                    result = index;
-                } else if (arrayKeys.getElement(index).equals(key)) {
-                    result = index;
-                }
+        for (int index = 0; index < size; index++) {
+            if ((keys[index] == null || key == null)
+                    && keys[index] == key) {
+                result = index;
+            } else if (keys[index].equals(key)) {
+                result = index;
             }
+        }
         return result;
     }
 
@@ -55,20 +54,19 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public void put(K key, V value) {
         Integer searchedIndex = searchedKey(key);
         if (searchedIndex != null) {
-            arrayValues.setElement(searchedIndex, value);
+            values[searchedIndex] = value;
         } else if (size < MAX_LENGTH) {
-            arrayKeys.setElement(size, key);
-            arrayValues.setElement(size, value);
+            keys[size] = key;
+            values[size] = value;
             size++;
-            }
+        }
     }
 
     @Override
     public V get(K key) {
         Integer searchedIndex = searchedKey(key);
         if (searchedIndex != null) {
-            V result = arrayValues.getElement(searchedIndex);
-            return result;
+            return (V) values[searchedIndex];
         }
         return null;
     }
