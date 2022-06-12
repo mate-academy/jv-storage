7641cf68e76f158737c56b0033631d5164eefd43
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2022-06-12 13:29:35 +0300

    made home work

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,31 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.HashMap;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private HashMap<K, V> storage = new HashMap<K, V>();
+
+    public HashMap<K, V> getStorage() {
+        return storage;
+    }
+
+    public void setStorage(HashMap<K, V> storage) {
+        this.storage = storage;
+    }
+
     @Override
     public void put(K key, V value) {
+        storage.put(key, value);
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return getStorage().get(key);
     }
 
     @Override
     public int size() {
-        return -1;
+        return storage.size();
     }
 }
