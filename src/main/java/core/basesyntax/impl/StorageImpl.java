453393225ddf3a9495cc4bd02e822b8fdc54833b
Nata <natserbo@gmail.com>
2022-06-20 18:27:51 +0300

    add put, get & size realisation

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,18 +2,33 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.ArrayList;
+
+
 public class StorageImpl<K, V> implements Storage<K, V> {
+    ArrayList<K> kArray = new ArrayList<>();
+    ArrayList<V> vArray = new ArrayList<>();
+
     @Override
     public void put(K key, V value) {
+        if (!kArray.contains(key)) {
+            kArray.add(key);
+            vArray.add(value);
+        } else {
+            vArray.set(kArray.indexOf(key), value);
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        if (!kArray.contains(key)) {
+            return null;
+        }
+        return vArray.get(kArray.indexOf(key));
     }
 
     @Override
     public int size() {
-        return -1;
+        return kArray.size();
     }
 }
