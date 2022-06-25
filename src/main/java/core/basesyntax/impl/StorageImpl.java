794597253b3743e6742f0289a3265e04c9256478
Roman Voitalik <voitalik1@gmail.com>
2022-06-25 17:19:56 +0300

    implement methods

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,53 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_NUMBER_OF_ELEMENTS = 10;
+    private K[] keysArray;
+    private V[] valuesArray;
+    private int currentSize;
+
+    public StorageImpl() {
+        keysArray = (K[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+        valuesArray = (V[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+    }
+
     @Override
     public void put(K key, V value) {
+        // check if array is empty, true -> add element and exit
+        if (currentSize == 0) {
+            keysArray[currentSize] = key;
+            valuesArray[currentSize] = value;
+            currentSize++;
+            return;
+        }
+        // if element exists -> replace it with new value
+        for (int i = 0; i < currentSize; i++) {
+            if (keysArray[i] == key || keysArray[i] != null && keysArray[i].equals(key)) {
+                valuesArray[i] = value;
+                return;
+            }
+        }
+        // if element does not exist -> add it into array
+        if (currentSize < 10) {
+            keysArray[currentSize] = key;
+            valuesArray[currentSize] = value;
+            currentSize++;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        V result = null;
+        for (int i = 0; i < currentSize; i++) {
+            if (keysArray[i] == key || keysArray[i] != null && keysArray[i].equals(key)) {
+                result = valuesArray[i];
+            }
+        }
+        return result;
     }
 
     @Override
     public int size() {
-        return -1;
+        return currentSize;
     }
 }
