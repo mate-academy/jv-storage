bf1482f16dcbfcf95c32964dbd19b248673ed1bd
Roman Voitalik <voitalik1@gmail.com>
2022-06-27 14:41:21 +0300

    fix requests

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,28 +4,26 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_NUMBER_OF_ELEMENTS = 10;
-    private K[] keysArray;
-    private V[] valuesArray;
+    private K[] keys;
+    private V[] values;
     private int currentSize;
 
     public StorageImpl() {
-        keysArray = (K[]) new Object[MAX_NUMBER_OF_ELEMENTS];
-        valuesArray = (V[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+        keys = (K[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+        values = (V[]) new Object[MAX_NUMBER_OF_ELEMENTS];
     }
 
     @Override
     public void put(K key, V value) {
-        // if element exists -> replace it with new value
         for (int i = 0; i < currentSize; i++) {
-            if (keysArray[i] == key || keysArray[i] != null && keysArray[i].equals(key)) {
-                valuesArray[i] = value;
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
+                values[i] = value;
                 return;
             }
         }
-        // if element does not exist -> add it into array
         if (currentSize < MAX_NUMBER_OF_ELEMENTS) {
-            keysArray[currentSize] = key;
-            valuesArray[currentSize] = value;
+            keys[currentSize] = key;
+            values[currentSize] = value;
             currentSize++;
         }
     }
@@ -34,8 +32,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public V get(K key) {
         V result = null;
         for (int i = 0; i < currentSize; i++) {
-            if (keysArray[i] == key || keysArray[i] != null && keysArray[i].equals(key)) {
-                result = valuesArray[i];
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
+                result = values[i];
             }
         }
         return result;
