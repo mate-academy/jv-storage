00831d60bd836fd55e52ca46cd0915f926ebc427
Roman Pylypchuk <roman.pylypchuk@yahoo.com>
2022-06-27 21:07:20 +0300

    added constants

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,9 +2,18 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.Objects;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_NUMBER_OF_ELEMENTS = 10;
+    private final K[] keys = (K[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+    private final V[] values = (V[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+    private int size;
+
     @Override
     public void put(K key, V value) {
+
+        }
     }
 
     @Override
