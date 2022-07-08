96127fcdbf67bcdae6d32cce48faf5945dd35ce5
Serhii Buria <sergeiburya@gmail.com>
2022-07-08 11:07:53 +0300

    added Box class, implemented key-value method with key validationÑÑ

---
+++ src/main/java/core/basesyntax/Box.java
@@ -0,0 +1,18 @@
+package core.basesyntax;
+
+public class Box {
+    private Object value;
+
+    public Box() {
+
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+}
