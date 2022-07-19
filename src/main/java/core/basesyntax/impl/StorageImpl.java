03df4592d4d010b91222403deebcd53653132443
Serhii Davydov <serhii.davydov@gmail.com>
2022-07-19 23:12:27 +0300

    Add first realisation

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,58 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE_STORAGE = 10;
+    private static int sizeStorage;
+    private ElementStorage<K, V>[] storage;
+
+    public StorageImpl() {
+        this.storage = new ElementStorage[MAX_SIZE_STORAGE];
+        this.sizeStorage = 0;
+    }
+
+    class ElementStorage<K, V> {
+        private K key;
+        private V value;
+
+        public ElementStorage(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = containsKey(key);
+        if (index >= 0) {
+            storage[index] = new ElementStorage(key, value);
+        } else {
+            storage[sizeStorage++] = new ElementStorage(key, value);
+        }
+    }
+
+    private int containsKey(K key) {
+        if (this.size() == 0) {
+            return -1;
+        }
+        for (int i = 0; i < this.size(); i++) {
+            if (storage[i].key == null && key == null) {
+                return i;
+            }
+            if (storage[i].key != null && storage[i].key.equals(key)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int index = containsKey(key);
+        return index >= 0 ? storage[index].value : null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return sizeStorage;
     }
 }
