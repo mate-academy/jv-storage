fa5994f840dbd8fdd6a546a847a7b1485d8783ea
Serhii Davydov <serhii.davydov@gmail.com>
2022-07-20 11:02:26 +0300

    Refactoring: change names, modificators, remove empty lines

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,19 +4,19 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE_STORAGE = 10;
-    private static int sizeStorage;
-    private ElementStorage<K, V>[] storage;
+    private int size;
+    private Element<K, V>[] storage;
 
     public StorageImpl() {
-        this.storage = new ElementStorage[MAX_SIZE_STORAGE];
-        this.sizeStorage = 0;
+        storage = new Element[MAX_SIZE_STORAGE];
+        this.size = 0;
     }
 
-    class ElementStorage<K, V> {
+    private class Element<K, V> {
         private K key;
         private V value;
 
-        public ElementStorage(K key, V value) {
+        private Element(K key, V value) {
             this.key = key;
             this.value = value;
         }
@@ -24,37 +24,31 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        int index = containsKey(key);
+        int index = getIndex(key);
         if (index >= 0) {
-            storage[index] = new ElementStorage(key, value);
+            storage[index] = new Element(key, value);
         } else {
-            storage[sizeStorage++] = new ElementStorage(key, value);
+            storage[size++] = new Element(key, value);
         }
-    }
-
-    private int containsKey(K key) {
-        if (this.size() == 0) {
-            return -1;
-        }
-        for (int i = 0; i < this.size(); i++) {
-            if (storage[i].key == null && key == null) {
-                return i;
-            }
-            if (storage[i].key != null && storage[i].key.equals(key)) {
-                return i;
-            }
-        }
-        return -1;
     }
 
     @Override
     public V get(K key) {
-        int index = containsKey(key);
+        int index = getIndex(key);
         return index >= 0 ? storage[index].value : null;
     }
 
     @Override
     public int size() {
-        return sizeStorage;
+        return size;
+    }
+
+    private int getIndex(K key) {
+        for (int i = 0; i < this.size; i++) {
+            if ((storage[i].key != null && storage[i].key.equals(key)) || storage[i].key == key) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
