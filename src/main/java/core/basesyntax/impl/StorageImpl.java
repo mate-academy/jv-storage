38175ff497edace04a5feaf35a1590169de64d05
Andrii Pylypenko <pilipenko1975@gmail.com>
2022-07-25 19:08:53 +0300

    Task completed v.2.0

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,15 +3,27 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private Pair[] pairs = new Pair[10];
+    private final Pair[] pairs = new Pair[10];
     private byte size;
 
-    private class Pair<K, V> {
-        K key;
-        V value;
+    private static class Pair<K, V> {
+        private K key;
+        private V value;
 
         public Pair(K key, V value) {
             this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
             this.value = value;
         }
     }
@@ -23,9 +35,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public void put(K key, V value) {
         for (int i = 0; i < pairs.length; i++) {
             if (pairs[i] != null
-                    && ((key == null && pairs[i].key == null)
-                    || (pairs[i].key != null && pairs[i].key.equals(key)))) {
-                pairs[i].value = value;
+                    && ((key == null && pairs[i].getKey() == null)
+                    || (pairs[i].getKey() != null && pairs[i].getKey().equals(key)))) {
+                pairs[i].setValue(value);
                 break;
             }
             if (pairs[i] == null) {
@@ -40,9 +52,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public V get(K key) {
         for (Pair<K, V> currentItem : pairs) {
             if (currentItem != null
-                    && (currentItem.key == key
-                    || currentItem.key != null && currentItem.key.equals(key))) {
-                return currentItem.value;
+                    && (currentItem.getKey() == key
+                    || currentItem.getKey() != null && currentItem.getKey().equals(key))) {
+                return currentItem.getValue();
             }
         }
         return null;
