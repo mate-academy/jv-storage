da1f57f810005cd704cc5ad7c62e54fd82486d28
Maksym Kulakov <kulakovmaksym0@gmail.com>
2022-07-25 12:15:40 +0200

    StorageImpl_1ver

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,58 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_PAIRS_NUMBER = 10;
+    private K key;
+    private V value;
+    private StorageImpl<K, V>[] pairs;
+
+    public StorageImpl() {
+        pairs = new StorageImpl[MAX_PAIRS_NUMBER];
+    }
+
+    public StorageImpl(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
     @Override
     public void put(K key, V value) {
+        if (getIndex(key) < size()) {
+            pairs[getIndex(key)].value = value;
+        }
+        pairs[getIndex(key)] = new StorageImpl<K, V>(key, value);
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return (getIndex(key) < size()) ? pairs[getIndex(key)].value : null;
     }
 
     @Override
     public int size() {
+        int count = 0;
+        for (int index = 0; index < MAX_PAIRS_NUMBER; index++) {
+            if (pairs[index] == null) {
+                count++;
+            }
+        }
+        return MAX_PAIRS_NUMBER - count;
+    }
+
+    public int getIndex(K key) {
+        for (int index = 0; index < MAX_PAIRS_NUMBER; index++) {
+            if (pairs[index] != null && pairs[index].key == key) {
+                return index;
+            } else if (pairs[index] != null && pairs[index].key != null
+                    && pairs[index].key.equals(key)) {
+                return index;
+            } else if (pairs[index] == null) {
+                return index;
+            }
+        }
         return -1;
     }
 }
+
+
+
