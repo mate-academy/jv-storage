a151996db14fa364467d1302cf8bfea957606cb2
Andrii Malenko <andmalenko@gmail.com>
2022-07-25 12:33:29 +0300

    All requested changes has been done

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,24 +4,23 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ARRAY_SIZE = 10;
-    private int sizeCounter;
+    private int size;
     private Object[] keys;
     private Object[] values;
 
     public StorageImpl() {
         keys = new Object[MAX_ARRAY_SIZE];
         values = new Object[MAX_ARRAY_SIZE];
-        sizeCounter = 0;
     }
 
     @Override
     public void put(K key, V value) {
         if (get(key) == null) {
-            this.keys[sizeCounter] = key;
-            this.values[sizeCounter] = value;
-            sizeCounter++;
+            this.keys[size] = key;
+            this.values[size] = value;
+            size++;
         } else {
-            for (int i = 0; i < sizeCounter; i++) {
+            for (int i = 0; i < size; i++) {
                 if ((this.keys[i] == key) || (this.keys[i] != null) && (this.keys[i].equals(key))) {
                     this.values[i] = value;
                 }
@@ -31,7 +30,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < sizeCounter; i++) {
+        for (int i = 0; i < size; i++) {
             if ((this.keys[i] == key) || (this.keys[i] != null) && (this.keys[i].equals(key))) {
                 return (V) values[i];
             }
@@ -41,6 +40,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return sizeCounter;
+        return size;
     }
 }
