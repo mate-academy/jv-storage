3c93e8d7452ff42ae942d1e2d38d9c6f1f280f48
Maksym Kulakov <kulakovmaksym0@gmail.com>
2022-07-25 20:21:15 +0200

    Objects.equals & two Object arrays used

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,57 +1,52 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_PAIRS_NUMBER = 10;
     private K key;
     private V value;
-    private StorageImpl<K, V>[] pairs;
+    private Object[] keys;
+    private Object[] values;
 
     public StorageImpl() {
-        pairs = new StorageImpl[MAX_PAIRS_NUMBER];
-    }
-
-    private StorageImpl(K key, V value) {
-        this.key = key;
-        this.value = value;
+        keys = new Object[MAX_PAIRS_NUMBER];
+        values = new Object[MAX_PAIRS_NUMBER];
     }
 
     @Override
     public void put(K key, V value) {
-        if (getIndex(key) < size()) {
-            pairs[getIndex(key)].value = value;
+        for (int index = 0; index < MAX_PAIRS_NUMBER; index++) {
+            if (Objects.equals(keys[index], key)) {
+                values[index] = value;
+                break;
+            } else if (keys[index] == null && values[index] == null) {
+                keys[index] = key;
+                values[index] = value;
+                break;
+            }
         }
-        pairs[getIndex(key)] = new StorageImpl<K, V>(key, value);
     }
 
     @Override
     public V get(K key) {
-        return (getIndex(key) < size()) ? pairs[getIndex(key)].value : null;
+        for (int index = 0; index < MAX_PAIRS_NUMBER; index++) {
+            if (Objects.equals(keys[index], key)) {
+                return (V) values[index];
+            }
+        }
+        return null;
     }
 
     @Override
     public int size() {
         int count = 0;
         for (int index = 0; index < MAX_PAIRS_NUMBER; index++) {
-            if (pairs[index] == null) {
+            if (keys[index] == null && values[index] == null) {
                 count++;
             }
         }
         return MAX_PAIRS_NUMBER - count;
-    }
-
-    public int getIndex(K key) {
-        for (int index = 0; index < MAX_PAIRS_NUMBER; index++) {
-            if (pairs[index] != null && pairs[index].key == key) {
-                return index;
-            } else if (pairs[index] != null && pairs[index].key != null
-                    && pairs[index].key.equals(key)) {
-                return index;
-            } else if (pairs[index] == null) {
-                return index;
-            }
-        }
-        return -1;
     }
 }
