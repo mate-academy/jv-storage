860def6d74143727c0966865180d08bab21f76d7
Andrii Pylypenko <pilipenko1975@gmail.com>
2022-07-26 13:55:56 +0300

    Task completed v.2.2 Changes suggested by the mentor have been made

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -32,7 +32,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if ((key == pairs[i].getKey() )
+            if ((key == pairs[i].getKey())
                     || (pairs[i].getKey() != null && pairs[i].getKey().equals(key))) {
                 pairs[i].setValue(value);
                 return;
