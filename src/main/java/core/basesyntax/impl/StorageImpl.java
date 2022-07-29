294b982b3c18aea1ea56d0ef0912afddf2df0636
Dmytro Vyshnivskyi <d.vyshnivskyi@gmail.com>
2022-07-29 23:48:20 +0300

    Implemented the Storage.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,46 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    @Override
-    public void put(K key, V value) {
+    private static final int MAX_NUMBER_OF_ELEMENTS = 10;
+    private static final int NUMBER_OF_LINES = 2;
+    private final Object[][] keys = new Object[MAX_NUMBER_OF_ELEMENTS][NUMBER_OF_LINES];
+    private int size;
+
+    private int getIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(key, keys[i][0])) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
+    public void put(K key, V value) {
+        int index = getIndex(key);
+        if (index == -1) {
+            keys[size][0] = key;
+            keys[size++][1] = value;
+        } else {
+            keys[index][1] = value;
+        }
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
     public V get(K key) {
-        return null;
+        int index = getIndex(key);
+        if (index == -1) {
+            return null;
+        }
+        return (V) keys[index][1];
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
