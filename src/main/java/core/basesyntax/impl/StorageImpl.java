4441a160bce3364f6227bf7732f4899460951cb2
Dmytro Vyshnivskyi <d.vyshnivskyi@gmail.com>
2022-07-30 00:03:40 +0300

    Added constants

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,14 +4,16 @@ import core.basesyntax.Storage;
 import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int MAX_NUMBER_OF_ELEMENTS = 10;
+    private static final int NUMBER_OF_ELEMENTS = 10;
     private static final int NUMBER_OF_LINES = 2;
-    private final Object[][] keys = new Object[MAX_NUMBER_OF_ELEMENTS][NUMBER_OF_LINES];
+    private static final int INDEX_OF_KEY = 0;
+    private static final int INDEX_OF_VALUE = 1;
+    private final Object[][] keys = new Object[NUMBER_OF_ELEMENTS][NUMBER_OF_LINES];
     private int size;
 
     private int getIndex(K key) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(key, keys[i][0])) {
+            if (Objects.equals(key, keys[i][INDEX_OF_KEY])) {
                 return i;
             }
         }
@@ -22,10 +24,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public void put(K key, V value) {
         int index = getIndex(key);
         if (index == -1) {
-            keys[size][0] = key;
-            keys[size++][1] = value;
+            keys[size][INDEX_OF_KEY] = key;
+            keys[size++][INDEX_OF_VALUE] = value;
         } else {
-            keys[index][1] = value;
+            keys[index][INDEX_OF_VALUE] = value;
         }
     }
 
@@ -36,7 +38,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         if (index == -1) {
             return null;
         }
-        return (V) keys[index][1];
+        return (V) keys[index][INDEX_OF_VALUE];
     }
 
     @Override
