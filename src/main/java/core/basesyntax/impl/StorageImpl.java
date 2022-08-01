fbdfd7d1c8e45a7883c1ebb3e4feb7051657fb37
Dmytro Vyshnivskyi <d.vyshnivskyi@gmail.com>
2022-08-01 16:07:57 +0300

    corrected comments

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,30 +4,21 @@ import core.basesyntax.Storage;
 import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int NUMBER_OF_ELEMENTS = 10;
-    private static final int NUMBER_OF_LINES = 2;
+    private static final int MAX_NUMBER_OF_ELEMENTS = 10;
+    private static final int NUMBER_OF_ROWS = 2;
     private static final int INDEX_OF_KEY = 0;
     private static final int INDEX_OF_VALUE = 1;
-    private final Object[][] keys = new Object[NUMBER_OF_ELEMENTS][NUMBER_OF_LINES];
+    private final Object[][] elements = new Object[MAX_NUMBER_OF_ELEMENTS][NUMBER_OF_ROWS];
     private int size;
-
-    private int getIndex(K key) {
-        for (int i = 0; i < size; i++) {
-            if (Objects.equals(key, keys[i][INDEX_OF_KEY])) {
-                return i;
-            }
-        }
-        return -1;
-    }
 
     @Override
     public void put(K key, V value) {
         int index = getIndex(key);
         if (index == -1) {
-            keys[size][INDEX_OF_KEY] = key;
-            keys[size++][INDEX_OF_VALUE] = value;
+            elements[size][INDEX_OF_KEY] = key;
+            elements[size++][INDEX_OF_VALUE] = value;
         } else {
-            keys[index][INDEX_OF_VALUE] = value;
+            elements[index][INDEX_OF_VALUE] = value;
         }
     }
 
@@ -38,11 +29,20 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         if (index == -1) {
             return null;
         }
-        return (V) keys[index][INDEX_OF_VALUE];
+        return (V) elements[index][INDEX_OF_VALUE];
     }
 
     @Override
     public int size() {
         return size;
+    }
+
+    private int getIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(key, elements[i][INDEX_OF_KEY])) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
