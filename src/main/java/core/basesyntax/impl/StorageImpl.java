28c3a0684baa1d36c23f77836c919b66b8e7893b
Ivan Aldokhin <vanoald2601@gmail.com>
2022-08-01 20:33:36 +0300

    Add method findByKey and overrides several methods( put, get, size,equals,hashCode).

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,30 +3,23 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-
     private static final int MAX_ITEMS_NUMBER = 10;
     private final Object[] items = new Object[MAX_ITEMS_NUMBER];
-    private K key;
-    private V value;
-    private int size;
-    private int findKey = -1;
-
-    public K getKey() {
-        return key;
-    }
-
-    public V getValue() {
-        return value;
-    }
 
     @Override
     public void put(K key, V value) {
-        get(key);
-        if (findKey >= 0) {
-            size--;
-        } 
-        items[size] = new Object[]{key,value};
-        size++;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                items[i] = new Object[] {key,value};
+                break;
+            } else {
+                Object[] tmp = (Object[]) items[i];
+                if (key == tmp[0] || key != null && key.equals(tmp[0])) {
+                    items[i] = new Object[] {key,value};
+                    break;
+                }
+            }
+        }
     }
 
     @Override
@@ -34,11 +27,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         for (Object item: items) {
             if (item != null) {
                 Object[] tmp = (Object[]) item;
-                if (key == tmp[0]
-                        || key != null
-                        && key.equals(tmp[0])) {
-                    findKey++;
-                    return (V) tmp[1];
+                if (key == tmp[0] || key != null && key.equals(tmp[0])) {
+                    return (V)tmp[1];
                 }
             }
         }
@@ -47,29 +37,12 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
+        int size = 0;
+        for (Object item: items) {
+            if (item != null) {
+                size++;
+            }
+        }
         return size;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
-        }
-        if (obj == null || getClass() != obj.getClass()) {
-            return false;
-        }
-        StorageImpl<K, V> storage = (StorageImpl<K, V>) obj;
-        return (storage.getKey() == key
-                || (storage.getKey() != null
-                && storage.getKey().equals(key)))
-                && (storage.getValue() == value
-                || (storage.getValue() != null
-                && storage.getValue().equals(value)));
-    }
-
-    @Override
-    public int hashCode() {
-        return (key == null ? 0 : key.hashCode() << 16)
-                & (value == null ? 0 : value.hashCode());
     }
 }
