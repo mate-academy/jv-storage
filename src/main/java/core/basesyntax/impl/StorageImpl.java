53312adf154d9d58d1e84b87d0e75d7829392a3c
Igor Petrichenko <i.petrichenko@gmail.com>
2022-08-02 18:16:03 +0300

    Homework jv-storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,38 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.ArrayList;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private final ArrayList<V> valueArrays;
+    private final ArrayList<K> keyArrays;
+
+    public StorageImpl() {
+        valueArrays = new ArrayList<>();
+        keyArrays = new ArrayList<>();
+    }
+
     @Override
     public void put(K key, V value) {
+        if (valueArrays.size() == 0 || !keyArrays.contains(key)) {
+            valueArrays.add(value);
+            keyArrays.add(key);
+        } else {
+            int index = keyArrays.indexOf(key);
+            valueArrays.set(index, value);
+        }
     }
 
     @Override
     public V get(K key) {
+        if (keyArrays.contains(key)) {
+            return valueArrays.get(keyArrays.indexOf(key));
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return valueArrays.size();
     }
 }
