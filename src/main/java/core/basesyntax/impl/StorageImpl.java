491d1a9f1bdf2a3ed2c704eef346390053bdddd6
Veers <raadch3011@gmail.com>
2022-08-02 12:01:57 +0300

    implements methods, create fields, create one more method, complete logic

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,50 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ARRAY_SIZE = 10;
+    private final Object [] keys;
+    private final Object [] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = new Object[MAX_ARRAY_SIZE];
+        values = new Object[MAX_ARRAY_SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = getKeyIndex(key);
+        if (index == -1) {
+            this.keys[size] = key;
+            this.values[size] = value;
+            size++;
+        } else {
+            this.keys[index] = key;
+            this.values[index] = value;
+        }
+    }
+
+    private int getKeyIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if ((keys[i] == key)
+                    || (keys[i] != null && key != null && key.equals(keys[i]))) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int index = getKeyIndex(key);
+        if (index == -1) {
+            return null;
+        }
+        return (V) values[index];
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
