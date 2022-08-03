b19850a22be219fd2cef5ec1b74471b4308b5e5e
Andrii Nesterenko <nesterenkoandrii23@gmail.com>
2022-08-03 08:14:15 +0300

    changed the name of constant MAX_STORAGE_NUM to MAX_STORAGE_NUMBER

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,14 +3,14 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int MAX_STORAGE_NUM = 10;
+    private static final int MAX_STORAGE_NUMBER = 10;
     private K[] keys;
     private V[] values;
     private int size;
 
     public StorageImpl() {
-        keys = (K[]) new Object[MAX_STORAGE_NUM];
-        values = (V[]) new Object[MAX_STORAGE_NUM];
+        keys = (K[]) new Object[MAX_STORAGE_NUMBER];
+        values = (V[]) new Object[MAX_STORAGE_NUMBER];
     }
 
     @Override
