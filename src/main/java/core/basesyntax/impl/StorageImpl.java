63557b3703485f5e0eb6c5527d2c08465ad0b548
Andriy Linkov <linkoff.as@gmail.com>
2022-08-03 20:47:34 +0300

    Implemented class StorageImpl

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,56 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private Object[] keys = new Object[MAX_ITEMS_NUMBER];
+    private Object[] values = new Object[MAX_ITEMS_NUMBER];
+    private int amount = 0;
+
     @Override
     public void put(K key, V value) {
+        if (amount == 0) {
+            addKeyValue(key, value, 0);
+            return;
+        }
+        for (int i = 0; i < amount; i++) {
+            if (isEqualsKeys((K) keys[i], key)) {
+                values[i] = value;
+                return;
+            }
+        }
+        if (amount < MAX_ITEMS_NUMBER) {
+            addKeyValue(key, value, amount);
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < MAX_ITEMS_NUMBER; i++) {
+            if (isEqualsKeys((K) keys[i], key)) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return amount;
+    }
+
+    private boolean isEqualsKeys(K firstKey, K secondKey) {
+        if (firstKey == null && secondKey == null) {
+            return true;
+        }
+        if (firstKey != null && firstKey.equals(secondKey)) {
+            return true;
+        }
+        return false;
+    }
+
+    private void addKeyValue(K key, V value, int index) {
+        keys[index] = (Object) key;
+        values[index] = (Object) value;
+        amount += 1;
     }
 }
