605195cde003d148b561f6628152260760564f30
Igor Petrichenko <i.petrichenko@gmail.com>
2022-08-03 21:34:51 +0300

    Homework jv-storage without ArrayList

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,38 +1,54 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.ArrayList;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private final ArrayList<V> valueArrays;
-    private final ArrayList<K> keyArrays;
+    private static final int MAX_AMOUNT_STORAGE = 10;
+    private final V[] valueArrays;
+    private final K[] keyArrays;
+    private int size;
+    private int indexValue;
 
+    @SuppressWarnings("unchecked")
     public StorageImpl() {
-        valueArrays = new ArrayList<>();
-        keyArrays = new ArrayList<>();
+        valueArrays = (V[]) new Object[MAX_AMOUNT_STORAGE];
+        keyArrays = (K[]) new Object[MAX_AMOUNT_STORAGE];
     }
 
     @Override
     public void put(K key, V value) {
-        if (valueArrays.size() == 0 || !keyArrays.contains(key)) {
-            valueArrays.add(value);
-            keyArrays.add(key);
+        if (!isKey(keyArrays, key)) {
+            valueArrays[size] = value;
+            keyArrays[size] = key;
+            size++;
         } else {
-            int index = keyArrays.indexOf(key);
-            valueArrays.set(index, value);
+            valueArrays[indexValue] = value;
         }
+    }
+
+    private boolean isKey(K[] keyArrays, K key) {
+        for (int i = 0; i < keyArrays.length; i++) {
+            if (keyArrays[i] == null && valueArrays[i] == null) {
+                return false;
+            } else if (key == null && keyArrays[i] == null && valueArrays[i] != null
+                    || keyArrays[i] != null && keyArrays[i].equals(key)) {
+                indexValue = i;
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public V get(K key) {
-        if (keyArrays.contains(key)) {
-            return valueArrays.get(keyArrays.indexOf(key));
+        if (isKey(keyArrays, key)) {
+            return valueArrays[indexValue];
         }
         return null;
     }
 
     @Override
     public int size() {
-        return valueArrays.size();
+        return size;
     }
 }
