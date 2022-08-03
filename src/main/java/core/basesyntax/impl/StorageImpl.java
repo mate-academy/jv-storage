ee815220c43936342e489df1fe1ffe6e1a168b25
Nazar <litvik.12@gmail.com>
2022-08-03 15:49:59 +0300

    test

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,28 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int SIZE = 10;
+    private final Object[] keys;
+    private final Object[] values;
+
+    public StorageImpl() {
+        keys = new Object[SIZE];
+        values = new Object[SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+
     }
 
     @Override
     public V get(K key) {
+
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return 0;
     }
 }
