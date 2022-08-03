2320518b5aabea1b992ff32a3f57634a3fba7294
amarkelovUA <java.developer.aleksandr@gmail.com>
2022-08-03 16:15:18 +0300

    The case of entering null values is added

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,8 +2,7 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
-import java.sql.Array;
-import java.util.Objects;
+import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
@@ -40,11 +39,20 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     }
 
     private <K> int indexOfKey(K key) {
-        for (int i = 0; i < size; i++) {
-            if (keys[i].equals(key)) {
-                return i;
+        if (key == null) {
+            for (int i = 0; i < size; i++) {
+                if (keys[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (key.equals(keys[i])) {
+                    return i;
+                }
             }
         }
+
         return -1;
     }
 }
