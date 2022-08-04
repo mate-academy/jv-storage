2919861be3d31384822c155030eec25fb494049d
Artem Bordichuk <artyombordichuk@gmail.com>
2022-08-04 16:57:18 +0300

    All recommendations implemented

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,31 +3,32 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int BOUND = 10;
+    private final K[] keys = (K[]) new Object[BOUND];
+    private final V[] values = (V[]) new Object[BOUND];
     private int size = 0;
-    private K[] keysArray = (K[]) new Object[10];
-    private V[] valueArray = (V[]) new Object[10];
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if ((keysArray[i] == null && key == null)
-                    || (keysArray[i] != null && keysArray[i].equals(key))) {
-                valueArray[i] = value;
+            if ((keys[i] == null && key == null)
+                    || (keys[i] != null && keys[i].equals(key))) {
+                values[i] = value;
                 return;
             }
         }
 
-        keysArray[size] = key;
-        valueArray[size] = value;
+        keys[size] = key;
+        values[size] = value;
         size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keysArray.length; i++) {
-            if ((keysArray[i] == null && key == null)
-                    || (keysArray[i] != null && keysArray[i].equals(key))) {
-                return valueArray[i];
+        for (int i = 0; i < keys.length; i++) {
+            if ((keys[i] == null && key == null)
+                    || (keys[i] != null && keys[i].equals(key))) {
+                return values[i];
             }
         }
         return null;
