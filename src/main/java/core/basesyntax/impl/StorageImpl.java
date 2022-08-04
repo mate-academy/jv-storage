2f91564892c81c3a3b7cd6e1d889ef7b69010d4a
Alexhqolma <66127994+Alexhqolma@users.noreply.github.com>
2022-08-04 19:32:10 +0300

    Add realise of storage task

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,54 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private static final int ARRAY_SIZE = 10;
+    private K key;
+    private V value;
+    private final Object[] keys = new Object[ARRAY_SIZE];
+    private final Object[] values = new Object[ARRAY_SIZE];
+    private int exist = -1;
+    private int keysSize = -1;
+
     @Override
     public void put(K key, V value) {
+        if (keysSize == -1) {
+            keysSize++;
+            this.keys[keysSize] = key;
+            this.values[keysSize] = value;
+        } else {
+            for (int i = 0; i < keysSize + 1; i++) {
+                if (key != null && key.equals(keys[i]) || key == keys[i]) {
+                    exist = i;
+                }
+            }
+            if (exist != -1) {
+                this.keys[exist] = key;
+                this.values[exist] = value;
+            } else {
+                keysSize++;
+                this.keys[keysSize] = key;
+                this.values[keysSize] = value;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if (key != null && key.equals(keys[i]) || key == keys[i]) {
+                return (V) values[i];
+            }
+        }
         return null;
+    }
+
+    public V getValue() {
+        return value;
     }
 
     @Override
     public int size() {
-        return -1;
+        return keysSize + 1;
     }
 }
