4029d17fa7414fedabfaa3db75de26ff11b0ab74
Igor Petrichenko <i.petrichenko@gmail.com>
2022-08-04 13:22:41 +0300

    my version implementation hw-storage. I remove global variable size and index with override methods

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,52 +3,52 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int MAX_AMOUNT_STORAGE = 10;
-    private final V[] valueArrays;
-    private final K[] keyArrays;
-    private int size;
-    private int indexValue;
+    private static final int STORAGE_SIZE = 10;
+    private final V[] values;
+    private final K[] keys;
 
     @SuppressWarnings("unchecked")
     public StorageImpl() {
-        valueArrays = (V[]) new Object[MAX_AMOUNT_STORAGE];
-        keyArrays = (K[]) new Object[MAX_AMOUNT_STORAGE];
+        values = (V[]) new Object[STORAGE_SIZE];
+        keys = (K[]) new Object[STORAGE_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
-        if (!isKey(keyArrays, key)) {
-            valueArrays[size] = value;
-            keyArrays[size] = key;
-            size++;
+        if (getKey(key) < 0) {
+            int index = size();
+            values[index] = value;
+            keys[index] = key;
         } else {
-            valueArrays[indexValue] = value;
+            values[getKey(key)] = value;
         }
-    }
-
-    private boolean isKey(K[] keyArrays, K key) {
-        for (int i = 0; i < keyArrays.length; i++) {
-            if (keyArrays[i] == null && valueArrays[i] == null) {
-                return false;
-            } else if (key == null && keyArrays[i] == null && valueArrays[i] != null
-                    || keyArrays[i] != null && keyArrays[i].equals(key)) {
-                indexValue = i;
-                return true;
-            }
-        }
-        return false;
     }
 
     @Override
     public V get(K key) {
-        if (isKey(keyArrays, key)) {
-            return valueArrays[indexValue];
-        }
-        return null;
+        return getKey(key) >= 0 ? values[getKey(key)] : null;
     }
 
     @Override
     public int size() {
+        int size = 0;
+        for(V value: values) {
+            if (value != null) {
+                size++;
+            }
+        }
         return size;
+    }
+
+    private int getKey(K key) {
+        for (int index = 0; index < keys.length; index++) {
+            if (keys[index] == null && values[index] == null) {
+                return -1;
+            } else if (key == null && keys[index] == null && values[index] != null
+                    || keys[index] != null && keys[index].equals(key)) {
+                return index;
+            }
+        }
+        return -1;
     }
 }
