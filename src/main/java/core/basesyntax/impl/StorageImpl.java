c34f816eafd2b770a817caffe24000bdd7fe9850
Vasyl Pankiv <wasilius2@gmail.com>
2022-08-04 17:11:21 +0300

    add constructor and all necessary fields

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,7 +2,20 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.lang.reflect.Array;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ARRAY_LENGTH = 10;
+    private K[] keyArray;
+    private V[] valueArray;
+    private int firstEmptyIndex;
+
+    public StorageImpl() {
+        firstEmptyIndex = 0;
+        K[] keyArray = (K[]) Array.newInstance(Object.class, MAX_ARRAY_LENGTH);
+        V[] valueArray = (V[]) Array.newInstance(Object.class, MAX_ARRAY_LENGTH);
+    }
+
     @Override
     public void put(K key, V value) {
     }
