9d838b3960094b038f322638682b78d5c294525e
max-bolotin <realprof.mx@gmail.com>
2022-08-04 23:55:53 +0300

    using a null marker constant to distinguish null keys from default null values of empty array elements

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,49 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private K[] keys = (K[]) new Object[10];
+    private V[] values = (V[]) new Object[10];
+    private static final String NULL_MARKER = "Null marker";
+
     @Override
     public void put(K key, V value) {
+        key = nullCheck(key);
+        for (int i = 0; i < keys.length; i++) {
+            if (Objects.equals(key, keys[i]) || keys[i] == null) {
+                keys[i] = key;
+                values[i] = value;
+                break;
+            }
+        }
+    }
+
+    private K nullCheck(K key) {
+        if (key == null) {
+            key = (K) NULL_MARKER;
+        }
+        return key;
     }
 
     @Override
     public V get(K key) {
+        key = nullCheck(key);
+        for (int i = 0; i < keys.length; i++) {
+            if (Objects.equals(keys[i], key)) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        int count = 0;
+        while (keys[count] != null) {
+            count++;
+        }
+        return count;
     }
 }
