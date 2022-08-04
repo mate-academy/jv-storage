c09eacf2af875380afdbc2f9f995f669684603ab
Artem Bordichuk <artyombordichuk@gmail.com>
2022-08-04 19:54:23 +0300

    Constant name was changed. Class constructor was implemented.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,10 +3,15 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int BOUND = 10;
-    private K[] keys = (K[]) new Object[BOUND];
-    private V[] values = (V[]) new Object[BOUND];
+    private static final int MAXIMUM_CAPACITY = 10;
+    private K[] keys;
+    private V[] values;
     private int size = 0;
+
+    public StorageImpl() {
+        this.keys = (K[]) new Object[MAXIMUM_CAPACITY];
+        this.values = (V[]) new Object[MAXIMUM_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
@@ -26,7 +31,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < keys.length; i++) {
-            if ((keys[i] == null && key == null)
+            if ((keys[i] == key)
                     || (keys[i] != null && keys[i].equals(key))) {
                 return values[i];
             }
