64e9cada7e5cf68eaec6f02f5a3c887881e6c311
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-08-04 15:35:16 +0200

    add arrays for our pair

---
+++ src/main/java/core/basesyntax/Box.java
@@ -0,0 +1,1 @@
+package core.basesyntax;public class Box {}
--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,8 +1,18 @@
 package core.basesyntax.impl;
 
+import core.basesyntax.Box;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_CAPACITY = 10;
+    private Object[] keys;
+    private Object[] values;
+
+    public StorageImpl() {
+        keys = new Object[MAX_CAPACITY];
+        values = new Object[MAX_CAPACITY];
+    }
+
     @Override
     public void put(K key, V value) {
     }
@@ -15,5 +25,14 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return -1;
+    }
+
+    public static void main(String[] args) {
+        //создаем наше хранилище
+        Storage<Integer, Box> storage = new StorageImpl<>();
+        //создаем объект с валью
+        Box box = new Box();
+        //заполнили наше хранилище первой парой
+        storage.put(22, box);
     }
 }
