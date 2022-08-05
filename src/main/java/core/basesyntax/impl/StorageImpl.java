25f565c59de0fccdab69f0fa4196774ea0d1c00a
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-08-05 18:28:05 +0300

    made implementation with catacity

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,28 +4,38 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int DEFAULT_CAPACITY = 10;
-    private static final Object[] KEYS = new Object[DEFAULT_CAPACITY];
-    private static final Object[] VALUES = new Object[DEFAULT_CAPACITY];
-    private int size = 0;
+    private Object[] keys;
+    private Object[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = new Object[DEFAULT_CAPACITY];
+        values = new Object[DEFAULT_CAPACITY];
+    }
+
+    public StorageImpl(int catacity) {
+        keys = new Object[catacity];
+        values = new Object[catacity];
+    }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if ((key == KEYS[i]) || (key != null && key.equals(KEYS[i]))) {
-                VALUES[i] = value;
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
+                values[i] = value;
                 return;
             }
         }
-        KEYS[size] = key;
-        VALUES[size] = value;
+        keys[size] = key;
+        values[size] = value;
         size++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if ((key == KEYS[i]) || (key != null && key.equals(KEYS[i]))) {
-                return (V) VALUES[i];
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
+                return (V) values[i];
             }
         }
         return null;
