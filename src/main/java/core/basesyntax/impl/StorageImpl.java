796b15f7a733f33289ef0b666159c83d4f0f1997
Nazar <litvik.12@gmail.com>
2022-08-05 22:05:55 +0300

    added solution with using two arrays

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,46 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_VALUE = 10;
+    private final K[] keys;
+    private final V[] values;
+    private int sizeOfStorage;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_VALUE];
+        values = (V[]) new Object[MAX_VALUE];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (getIndex(key) == -1) {
+            keys[sizeOfStorage] = key;
+            values[sizeOfStorage] = value;
+            sizeOfStorage++;
+        } else {
+            values[getIndex(key)] = value;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        if (getIndex(key) == -1) {
+            return null;
+        }
+        return values[getIndex(key)];
     }
 
     @Override
     public int size() {
+        return sizeOfStorage;
+    }
+
+    private int getIndex(K key) {
+        for (int i = 0; i < sizeOfStorage; i++) {
+            if ((keys[i] != null && keys[i].equals(key)) || keys[i] == key) {
+                return i;
+            }
+        }
         return -1;
     }
 }
