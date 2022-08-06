d607f9c81ae438c71fe43e94b4474a544b9f29eb
Anna Popova <anitapopova1996@gmail.com>
2022-08-06 09:42:00 +0300

    Refactored put() and get() methods.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -10,6 +10,35 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public StorageImpl() {
     }
 
+    @Override
+    public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].key != null && storage[i].key.equals(key) || storage[i].key == key) {
+                storage[i] = new Node(key, value);
+                return;
+            }
+        }
+
+        storage[size] = new Node(key, value);
+        size++;
+
+    }
+
+    @Override
+    public V get(K key) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].key != null && storage[i].key.equals(key) || storage[i].key == key) {
+                return (V) storage[i].value;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
     private static class Node<K, V> {
         private K key;
         private V value;
@@ -19,15 +48,15 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             this.value = value;
         }
 
-        public final K getKey() {
+        public K getKey() {
             return key;
         }
 
-        public final V getValue() {
+        public V getValue() {
             return value;
         }
 
-        public final String toString() {
+        public String toString() {
             return key + "=" + value;
         }
 
@@ -40,8 +69,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             if (obj instanceof Node) {
                 Node entry = (Node) obj;
 
-                return key.equals(entry.getKey())
-                        && value.equals(entry.getValue());
+                return (this.key == entry.getKey()
+                        || this.key != null && this.key.equals(entry.getKey()))
+                        && (this.value == entry.getValue()
+                        || this.value != null && this.value.equals(entry.getValue()));
             }
             return false;
         }
@@ -53,38 +84,5 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             hash = 17 * hash + ((value == null) ? 0 : value.hashCode());
             return hash;
         }
-    }
-
-    @Override
-    public void put(K key, V value) {
-        for (int i = 0; i < STORAGE_SIZE; i++) {
-            if (storage[i] != null) {
-                if (storage[i].key != null && storage[i].key.equals(key) || storage[i].key == key) {
-                    storage[i] = new Node(key, value);
-                    break;
-                }
-            } else {
-                storage[i] = new Node(key, value);
-                size++;
-                break;
-            }
-        }
-    }
-
-    @Override
-    public V get(K key) {
-        for (Node node : storage) {
-            if (node != null) {
-                if (node.key != null && node.key.equals(key) || node.key == key) {
-                    return (V) node.value;
-                }
-            }
-        }
-        return null;
-    }
-
-    @Override
-    public int size() {
-        return size;
     }
 }
