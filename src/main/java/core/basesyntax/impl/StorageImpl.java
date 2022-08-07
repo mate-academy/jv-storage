a3198a26f95390fd95cd47585cd75402716ce954
Serhii Nazarenko <nasarenko82@gmail.com>
2022-08-07 14:17:26 +0300

    jv-storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,11 +1,13 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ELEMENTS_NUMBER = 10;
     private K[] arrayKey = (K[]) new Object[MAX_ELEMENTS_NUMBER];
     private V[] arrayValue = (V[]) new Object[MAX_ELEMENTS_NUMBER];
+    private int size;
 
     @Override
     public void put(K key, V value) {
@@ -16,6 +18,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             } else if (arrayKey[i] == null && arrayValue[i] == null) {
                 arrayKey[i] = key;
                 arrayValue[i] = value;
+                size++;
                 break;
             } else if (arrayKey[i] != null) {
                 if (arrayKey[i].equals(key)) {
@@ -28,17 +31,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < arrayKey.length; i++) {
-            if (arrayKey[i] == key && arrayValue[i] != null) {
+        for (int i = 0; i < size(); i++) {
+            if (Objects.equals(arrayKey[i], key)) {
                 return arrayValue[i];
-            } else if (arrayKey[i] == null) {
-                if (arrayKey[i] == key && arrayValue[i] != null) {
-                    return arrayValue[i];
-                }
-            } else {
-                if (arrayKey[i].equals(key)) {
-                    return arrayValue[i];
-                }
             }
         }
         return null;
@@ -46,12 +41,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        int counter = 0;
-        for (V sizeKey : arrayValue) {
-            if (sizeKey != null) {
-                counter++;
-            }
-        }
-        return counter;
+        return size;
     }
 }
