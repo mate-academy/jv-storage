1045cffea1ef80ee37aba54193b5c6d1c25a668e
Myroslav_Voskoboinyk <maestro.of.pain@gmail.com>
2022-08-07 14:14:45 +0300

    All overridden methods was implemented

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,41 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAXIMUM_NUMBERS_OF_ELEMENTS = 10;
+    private final Object[] arrayOfKeys = new Object[MAXIMUM_NUMBERS_OF_ELEMENTS];
+    private final Object[] arrayOfValues = new Object[MAXIMUM_NUMBERS_OF_ELEMENTS];
+    private int index = 0;
+
     @Override
     public void put(K key, V value) {
+        if (getIndex(key) == -1) {
+            arrayOfKeys[index] = key;
+            arrayOfValues[index] = value;
+            index++;
+        } else {
+            arrayOfValues[getIndex(key)] = value;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return getIndex(key) == -1 ? null : (V) arrayOfValues[getIndex(key)];
     }
 
     @Override
     public int size() {
+        return index;
+    }
+
+    private int getIndex(K key) {
+        for (int i = 0; i < index; i++) {
+            if (Objects.equals(arrayOfKeys[i], key)) {
+                return i;
+            }
+        }
         return -1;
     }
 }
