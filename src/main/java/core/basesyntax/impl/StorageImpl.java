6a9ecff33861ed08a6de1c22eccedac4d3ed52a5
EagleAndrew <tortloveduck4@gmail.com>
2022-08-07 23:07:17 +0200

    storage-task

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,37 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_RANGE = 10;
+    private int size;
+    private final K[] keys = (K[]) new Object[MAX_RANGE];
+    private final V[] values = (V[]) new Object[MAX_RANGE];
+
     @Override
     public void put(K key, V value) {
+        if (get(key) == null) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        } else {
+            values[size - 1] = value;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < MAX_RANGE; i++) {
+            if (Objects.equals(keys[i], key)) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
