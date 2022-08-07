1e6718e31b945ee2718ba3aaf741aacad620c0db
Aptypio13 <aptypio668@gmail.com>
2022-08-07 21:52:54 +0300

    Revert "implement a StorageImpl.java"
    This reverts commit 1fbbf44fd1b541a00bef8aadbd8a2ba3f2c3647c.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,42 +1,19 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int MAX_NUMBER_OF_ELEMENTS = 10;
-    private final K[] keys;
-    private final V[] values;
-    private int size;
-
-    public StorageImpl() {
-        keys = (K[]) new Object[MAX_NUMBER_OF_ELEMENTS];
-        values = (V[]) new Object[MAX_NUMBER_OF_ELEMENTS];
-    }
-
     @Override
     public void put(K key, V value) {
-        if (get(key) != null) {
-            values[size - 1] = value;
-        } else {
-            keys[size] = key;
-            values[size] = value;
-            size++;
-        }
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < size; i++) {
-            if (Objects.equals(key, keys[i])) {
-                return values[i];
-            }
-        }
         return null;
     }
 
     @Override
     public int size() {
-        return size;
+        return -1;
     }
 }
