19bcd4f52bfd273d96df70bd0fb5969f18a4a1a4
Sofiia <sofiakazzz@gmail.com>
2022-08-07 21:25:26 +0300

    Implemented the Storage of key-value

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,52 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_CAPACITY = 10;
+    private Object[] keys;
+    private Object[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = new Object[MAX_CAPACITY];
+        values = new Object[MAX_CAPACITY];
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = getIndex(key);
+        if (index == -1) {
+            values[size] = value;
+            keys[size] = key;
+            size++;
+        } else {
+            values[index] = value;
+        }
     }
 
     @Override
     public V get(K key) {
+        int index = getIndex(key);
+        if (index != -1) {
+            return (V) values[index];
+        }
         return null;
     }
 
     @Override
     public int size() {
+        return size;
+    }
+
+    private int getIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (!Objects.equals(keys[i], key)) {
+                continue;
+            }
+            return i;
+        }
         return -1;
     }
 }
