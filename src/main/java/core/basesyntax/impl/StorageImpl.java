f3048b94db5f65fca1eff92e37e2ac0354662df2
IraHurko <iragurko19@gmail.com>
2022-08-08 15:50:10 +0300

    Implements Storage with two types

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,50 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_VALUE = 10;
+    private final K[] keyArray;
+    private final V[] valueArray;
+
+    public StorageImpl() {
+        keyArray = (K[]) new Object[MAX_ITEMS_VALUE];
+        valueArray = (V[]) new Object[MAX_ITEMS_VALUE];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < keyArray.length; i++) {
+            if (Objects.equals(key, keyArray[i])) {
+                keyArray[i] = key;
+                valueArray[i] = value;
+                return;
+            }
+        }
+        int length = size();
+        keyArray[length] = key;
+        valueArray[length] = value;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keyArray.length; i++) {
+            if (Objects.equals(key, keyArray[i])) {
+                return valueArray[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        int size = 0;
+        for (int i = 0; i < keyArray.length; i++) {
+            if (keyArray[i] != null || valueArray[i] != null) {
+                size++;
+            }
+        }
+        return size;
     }
 }
