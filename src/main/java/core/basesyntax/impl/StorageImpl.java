8cca4b63a23a56addf027ea62ad3ec87b1b8bd1b
Heorhiy Nezbrytskiy <georgiy.nezbritskiy@gmail.com>
2022-08-08 16:59:36 +0300

    implement methods put and get

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,44 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAXIMUM_CAPACITY = 10;
+    private K key;
+    private V value;
+    private final Object[] arrayKey = new Object[MAXIMUM_CAPACITY];
+    private final Object[] arrayValue = new Object[MAXIMUM_CAPACITY];
+    private int size = 0;
+
+    public StorageImpl() {
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (((key != null) && key.equals(arrayKey[i])) || (key == null && arrayKey[i] == key)) {
+                arrayValue[i] = value;
+                return;
+            }
+        }
+        arrayKey[size] = key;
+        arrayValue[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        if (size == 0 && (key == null && arrayKey[size] == key)) {
+            return (V) arrayValue[size];
+        }
+        for (int i = 0; i < size; i++) {
+            if (key == null && arrayKey[i] == null || key != null && key.equals(arrayKey[i])) {
+                return (V) arrayValue[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
