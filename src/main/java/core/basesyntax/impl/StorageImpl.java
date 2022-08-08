e3a88872110a82a94dcc5a4cb3fc76834caa0f59
LubusUzh <lubus.uzh@gmail.com>
2022-08-08 10:30:47 +0300

    Added fixes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,6 +5,8 @@ import core.basesyntax.Storage;
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ROWS_SIZE = 10;
     private static final int COLUMNS_SIZE = 2;
+    private static final int FIRST_COLUMN_INDEX = 0;
+    private static final int SECOND_COLUMN_INDEX = 1;
     private Object[][] keysAndValues;
     private int size;
 
@@ -12,24 +14,14 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         keysAndValues = new Object[MAX_ROWS_SIZE][COLUMNS_SIZE];
     }
 
-    private int getIndex(K key) {
-        for (int i = 0; i < size; i++) {
-            if (keysAndValues[i][0] == key || (keysAndValues[i][0] != null
-                    && keysAndValues[i][0].equals(key))) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
     @Override
     public void put(K key, V value) {
         int index = getIndex(key);
         if (index != -1) {
-            keysAndValues[index][1] = value;
+            keysAndValues[index][SECOND_COLUMN_INDEX] = value;
         } else {
-            keysAndValues[size][0] = key;
-            keysAndValues[size][1] = value;
+            keysAndValues[size][FIRST_COLUMN_INDEX] = key;
+            keysAndValues[size][SECOND_COLUMN_INDEX] = value;
             size++;
         }
     }
@@ -38,7 +30,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public V get(K key) {
         int index = getIndex(key);
         if (index != -1) {
-            return (V) keysAndValues[index][1];
+            return (V) keysAndValues[index][SECOND_COLUMN_INDEX];
         }
         return null;
     }
@@ -46,5 +38,16 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return size;
+    }
+
+    private int getIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keysAndValues[i][FIRST_COLUMN_INDEX] == key
+                    || (keysAndValues[i][FIRST_COLUMN_INDEX] != null
+                    && keysAndValues[i][FIRST_COLUMN_INDEX].equals(key))) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
