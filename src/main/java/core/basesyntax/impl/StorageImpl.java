e2b6ecd81437bf282fe959bfdb57d500ae77a148
Thousandfold <you@example.com>
2022-08-08 16:28:55 +0300

    create_strorage_methods

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,41 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int LENGTH = 10;
+    private Object[] keys;
+    private Object[] values;
+    private int size;
+
+    public StorageImpl() {
+        this.keys = new Object[LENGTH];
+        this.values = new Object[LENGTH];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size;i++) {
+            if (keys[i] == key || (keys[i] != null && keys[i].equals(key))) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size;i++) {
+            if (keys[i] == key || (keys[i] != null && keys[i].equals(key))) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
