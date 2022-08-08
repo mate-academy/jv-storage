0751e4246906db8047840e684b48b466abbe08a0
ArturZhadan <artur.zhadan.17@gmail.com>
2022-08-08 16:44:35 +0300

    changed constant modificator, renamed filds, removed size initialization, changed array initialization, added check to get() method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,45 +1,47 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.lang.reflect.Array;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    public static final int STORAGE_CAPACITY = 10;
-    private K [] keyStorage;
-    private V [] valueStorage;
-    private int size = 0;
+    private static final int STORAGE_CAPACITY = 10;
+    private K [] keys;
+    private V [] values;
+    private int size;
 
     public StorageImpl() {
-        keyStorage = (K[]) Array.newInstance(Object.class, STORAGE_CAPACITY);
-        valueStorage = (V[]) Array.newInstance(Object.class, STORAGE_CAPACITY);
+        keys = (K[]) new Object[STORAGE_CAPACITY];;
+        values = (V[]) new Object[STORAGE_CAPACITY];;
     }
 
     @Override
     public void put(K key, V value) {
         int keyIndex = getIndex(key);
-        if (keyIndex != size) {
-            keyStorage[keyIndex] = key;
-            valueStorage[keyIndex] = value;
+        if (keyIndex != -1) {
+            keys[keyIndex] = key;
+            values[keyIndex] = value;
             return;
         }
-        keyStorage[keyIndex] = key;
-        valueStorage[keyIndex] = value;
+        keys[size] = key;
+        values[size] = value;
         size++;
     }
 
     public int getIndex(K key) {
         for (int i = 0; i < size; i++) {
-            if (keyStorage[i] == key || keyStorage[i] != null && keyStorage[i].equals(key)) {
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
                 return i;
             }
         }
-        return size;
+        return -1;
     }
 
     @Override
     public V get(K key) {
+        if (getIndex(key) == -1) {
+            return null;
+        }
         int keyIndex = getIndex(key);
-        return valueStorage[keyIndex];
+        return values[keyIndex];
     }
 
     @Override
