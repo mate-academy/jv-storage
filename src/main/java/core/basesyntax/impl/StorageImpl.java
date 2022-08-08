3117dda1271bb00ec486d52233f9dad31f3f9c6c
Alexhqolma <66127994+Alexhqolma@users.noreply.github.com>
2022-08-08 11:02:14 +0300

    1) Changed name of key and value -1 to 0 2) Created new method for indication of index 3) Changed if-box to correct 4) Used size instead of keys.length() 5) Object inside class Cat - not mine addition (if I am not mistake), it is look like it added from tested side

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -10,35 +10,31 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private final Object[] keys = new Object[ARRAY_SIZE];
     private final Object[] values = new Object[ARRAY_SIZE];
     private int exist = -1;
-    private int keysSize = -1;
+    private int size = 0;
 
     @Override
     public void put(K key, V value) {
-        if (keysSize == -1) {
-            keysSize++;
-            this.keys[keysSize] = key;
-            this.values[keysSize] = value;
+        if (size == 0) {
+            this.keys[size] = key;
+            this.values[size] = value;
+            size++;
         } else {
-            for (int i = 0; i < keysSize + 1; i++) {
-                if (key != null && key.equals(keys[i]) || key == keys[i]) {
-                    exist = i;
-                }
-            }
+            getIndex(key);
             if (exist != -1) {
                 this.keys[exist] = key;
                 this.values[exist] = value;
             } else {
-                keysSize++;
-                this.keys[keysSize] = key;
-                this.values[keysSize] = value;
+                this.keys[size] = key;
+                this.values[size] = value;
+                size++;
             }
         }
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keys.length; i++) {
-            if (key != null && key.equals(keys[i]) || key == keys[i]) {
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
                 return (V) values[i];
             }
         }
@@ -51,6 +47,15 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return keysSize + 1;
+        return size;
+    }
+
+    private int getIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key != null && key.equals(keys[i]) || key == keys[i]) {
+                exist = i;
+            }
+        }
+        return exist;
     }
 }
