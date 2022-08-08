6406b62886060b730ee067b00877dd4eedb2ca60
Thousandfold <you@example.com>
2022-08-08 16:40:32 +0300

    fix_checkstyle_storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -9,13 +9,13 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private int size;
 
     public StorageImpl() {
-        this.keys = new Object[LENGTH];
-        this.values = new Object[LENGTH];
+        keys = new Object[LENGTH];
+        values = new Object[LENGTH];
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < size;i++) {
+        for (int i = 0; i < size; i++) {
             if (keys[i] == key || (keys[i] != null && keys[i].equals(key))) {
                 values[i] = value;
                 return;
@@ -28,7 +28,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < size;i++) {
+        for (int i = 0; i < size; i++) {
             if (keys[i] == key || (keys[i] != null && keys[i].equals(key))) {
                 return (V) values[i];
             }
