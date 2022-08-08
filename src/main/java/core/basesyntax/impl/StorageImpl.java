1cc1fe6946c680312868e9f2a67a7b4c4a9d6d67
Azarov Artur <azarovartyr@gmail.com>
2022-08-09 01:08:11 +0300

    implemented methods get, put, size in class StorageImpl.java

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,55 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    public static final String EMPTY_KEY = "empty";
+    private final Object[] keys = new Object[10];
+    private final Object[] values = new Object[10];
+
+    {
+        Arrays.fill(keys,EMPTY_KEY);
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < keys.length; i++) {
+            if ((key == null && key == keys[i]) || (keys[i] != null && keys[i].equals(key))) {
+                values[i] = value;
+                break;
+            }
+            if (keys[i] != null && keys[i].equals(EMPTY_KEY)) {
+                keys[i] = key;
+                values[i] = value;
+                break;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if (key == null) {
+                if (key == keys[i]) {
+                    return (V)values[i];
+                }
+                continue;
+            }
+            if (keys[i] != null && keys[i].equals(key)) {
+                return (V)values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] != null && keys[i].equals(EMPTY_KEY)) {
+                return i;
+            }
+        }
+        return keys.length;
     }
 }
