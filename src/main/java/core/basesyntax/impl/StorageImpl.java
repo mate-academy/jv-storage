3e97a451824821c53ee57c2c886b2b08714d0991
Azarov Artur <azarovartyr@gmail.com>
2022-08-11 16:36:07 +0300

    fixed all the additional errors mentioned

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,13 +6,12 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_STORAGE_SIZE = 10;
     private final K[] keys;
     private final V[] values;
-    ....
-    
+    private int size;
+
     public StorageImpl() {
         keys = (K[]) new Object[MAX_STORAGE_SIZE];
         values = (V[]) new Object[MAX_STORAGE_SIZE];
     }
-    private int size;
 
     @Override
     public void put(K key, V value) {
