d9c1ce08e2a238e7e3e005cc97a94959c17405c4
Azarov Artur <azarovartyr@gmail.com>
2022-08-11 15:39:44 +0300

    fixed all the errors mentioned

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,42 +1,37 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    public static final String EMPTY_KEY = "empty";
-    private final Object[] keys = new Object[10];
-    private final Object[] values = new Object[10];
-
-    {
-        Arrays.fill(keys,EMPTY_KEY);
-    }
+    private static final int MAX_STORAGE_SIZE = 10;
+    private final Object[] keys = new Object[MAX_STORAGE_SIZE];
+    private final Object[] values = new Object[MAX_STORAGE_SIZE];
+    private int size;
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < size ; i++) {
-            if ((key  == keys[i]) || (keys[i] != null && keys[i].equals(key))) {
+        for (int i = 0; i < size; i++) {
+            if ((key == keys[i]) || (keys[i] != null && keys[i].equals(key))) {
                 values[i] = value;
                 return;
             }
         }
-                keys[size] = key;
-                values[size] = value;
-                size++;
-            }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keys.length; i++) {
+        for (int i = 0; i < size; i++) {
             if (key == null) {
                 if (key == keys[i]) {
-                    return (V)values[i];
+                    return (V) values[i];
                 }
                 continue;
             }
             if (keys[i] != null && keys[i].equals(key)) {
-                return (V)values[i];
+                return (V) values[i];
             }
         }
         return null;
@@ -44,11 +39,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i] != null && keys[i].equals(EMPTY_KEY)) {
-                return i;
-            }
-        }
-        return keys.length;
+        return size;
     }
 }
