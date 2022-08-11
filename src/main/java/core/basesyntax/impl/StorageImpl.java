2353bf97b2e3ce97d600148322e6a0b1d8cd19a2
Slava Kobzar <slvkobzar@gmail.com>
2022-08-11 18:15:17 +0300

    Another solution of jv-storage homework after first review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,33 +6,34 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ITEMS_NUMBER = 10;
     private Object[] keys = new Object[MAX_ITEMS_NUMBER];
     private Object[] values = new Object[MAX_ITEMS_NUMBER];
+    private int size = 0;
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < MAX_ITEMS_NUMBER; i++) {
-            if (key == null) {
-                if (keys[i] == null) {
-                    values[i] = value;
-                    break;
-                } else {
-                    continue;
+        if (get(key) == null) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (key == null) {
+                    if (keys[i] == null) {
+                        values[i] = value;
+                        break;
+                    } else {
+                        continue;
+                    }
                 }
-            }
-            if (key.equals(keys[i])) {
-                values [i] = value;
-                break;
-            }
-            if (keys[i] == null && values[i] == null) {
-                keys[i] = key;
-                values[i] = value;
-                break;
+                if (key.equals(keys[i])) {
+                    values[i] = value;
+                }
             }
         }
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < MAX_ITEMS_NUMBER; i++) {
+        for (int i = 0; i < size; i++) {
             if (key == null) {
                 if (keys[i] == null) {
                     return (V) values[i];
@@ -49,12 +50,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        int storageSize = 0;
-        for (int i = 0; i < MAX_ITEMS_NUMBER; i++) {
-            if (keys[i] != null || values[i] != null) {
-                storageSize++;
-            }
-        }
-        return storageSize;
+        return size;
     }
 }
