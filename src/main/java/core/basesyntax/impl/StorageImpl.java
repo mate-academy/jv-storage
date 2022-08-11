174c0e1bad1ecf439afc92cd6d1b5465c63add30
azarovartur <99978155+azarovartur@users.noreply.github.com>
2022-08-11 16:33:25 +0300

    Update src/main/java/core/basesyntax/impl/StorageImpl.java
    Co-authored-by: Anastasia Len <81781479+aaananas@users.noreply.github.com>

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -17,7 +17,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if ((key == keys[i]) || (keys[i] != null && keys[i].equals(key))) {
+            if (key == keys[i] || (keys[i] != null && keys[i].equals(key))) {
                 values[i] = value;
                 return;
             }
