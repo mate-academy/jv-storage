ff437ae00ee832c5915d6793f10ec76be6c03a13
Slava Kobzar <slvkobzar@gmail.com>
2022-08-12 11:35:27 +0300

    Methods simplifying and constructor creating

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,44 +4,35 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ITEMS_NUMBER = 10;
-    private Object[] keys = new Object[MAX_ITEMS_NUMBER];
-    private Object[] values = new Object[MAX_ITEMS_NUMBER];
-    private int size = 0;
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_ITEMS_NUMBER];
+        values = (V[]) new Object[MAX_ITEMS_NUMBER];
+    }
 
     @Override
     public void put(K key, V value) {
-        if (get(key) == null) {
+        boolean keyIsPresent = false;
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || (key != null && key.equals(keys[i]))) {
+                values[i] = value;
+                keyIsPresent = true;
+            }
+        }
+        if (keyIsPresent == false) {
             keys[size] = key;
             values[size] = value;
             size++;
-        } else {
-            for (int i = 0; i < size; i++) {
-                if (key == null) {
-                    if (keys[i] == null) {
-                        values[i] = value;
-                        break;
-                    } else {
-                        continue;
-                    }
-                }
-                if (key.equals(keys[i])) {
-                    values[i] = value;
-                }
-            }
         }
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (key == null) {
-                if (keys[i] == null) {
-                    return (V) values[i];
-                } else {
-                    continue;
-                }
-            }
-            if (key.equals(keys[i])) {
+            if (key == keys[i] || (key != null && key.equals(keys[i]))) {
                 return (V) values[i];
             }
         }
