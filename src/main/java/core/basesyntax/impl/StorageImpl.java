ec4768f2c4d8ae4464b567c52336621080085618
Oleksandr Gotin <alekander.gotin@gmail.com>
2022-08-20 01:27:14 +0300

    code after changes requested by mentors

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -15,8 +15,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        int index = getIndex(key);
-        if (index != -1) {
+        Integer index = getIndex(key);
+        if (index != null) {
             values[index] = value;
         } else {
             keys[size] = key;
@@ -27,8 +27,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        int index = getIndex(key);
-        return index != 0 ? (V) values[index] : null;
+        Integer index = getIndex(key);
+        return index != null ? (V) values[index] : null;
     }
 
     @Override
@@ -36,13 +36,14 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         return size;
     }
 
-    private int getIndex(K key) {
+    private Integer getIndex(K key) {
+        Integer result = null;
         for (int i = 0; i < size; i++) {
-            if (keys[i] == keys
-                    || keys != null && keys[i].equals(key)) {
-                return i;
+            if ((keys[i] != null && keys[i].equals(key))
+                    || (keys[i] == key && keys[i] == null)) {
+                result = i;
             }
         }
-        return -1;
+        return result;
     }
 }
