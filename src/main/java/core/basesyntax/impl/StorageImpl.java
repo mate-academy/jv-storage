9a36b40c31285ddd6c2208a1bc69d5aed304d9d8
Kostiantyn Sacheiev <dynamofan1927@gmail.com>
2022-08-23 17:25:45 +0200

    Solution was added 1. Added constructor 2. Was realized method size() 3. Created method findKeyIndex which return index of the key on array or return - 1 if key don't find 4. Was realized methods put() and get()

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,18 +2,47 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.Objects;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private final K[] keys;
+    private final V[] values;
+    private int size;
+
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_ITEMS_NUMBER];
+        values = (V[]) new Object[MAX_ITEMS_NUMBER];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (findKeyIndex(key) == -1) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        } else {
+            values[findKeyIndex(key)] = value;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return findKeyIndex(key) == -1 ? null : (V) values[findKeyIndex(key)];
     }
 
     @Override
     public int size() {
+        return size;
+    }
+
+    private int findKeyIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(keys[i], key)) {
+                return i;
+            }
+        }
         return -1;
     }
 }
