24786c7689ed0f479b4ab20efa3d13fe44fd4c2d
Volodymyr Sibiruk <arthes46@gmail.com>
2022-09-01 20:26:14 +0300

    added implementation of methods "put", "get" and "size"

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,57 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_NUMBER_OF_ELEMENTS = 10;
+    private int size;
+    private K[] keyStorage;
+    private V[] valueStorage;
+
+    public StorageImpl() {
+        keyStorage = (K[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+        valueStorage = (V[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = size;
+        for (int i = 0; i <= size; i++) {
+            if (key == null && keyStorage[i] == null) {
+                index = i;
+                break;
+            }
+            if (key != null && key.equals(keyStorage[i])) {
+                index = i;
+            }
+        }
+        keyStorage[index] = key;
+        valueStorage[index] = value;
+
+        if (index == size) {
+            size++;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int index = 0;
+        for (int i = 0; i <= size; i++) {
+            if (key == null && keyStorage[i] == null) {
+                index = i;
+                break;
+            }
+            if (key != null && key.equals(keyStorage[i])) {
+                index = i;
+            }
+        }
+        return valueStorage[index];
     }
 
     @Override
     public int size() {
-        return -1;
+        if (keyStorage[0] == null) {
+            return size;
+        }
+
+        return size;
     }
 }
