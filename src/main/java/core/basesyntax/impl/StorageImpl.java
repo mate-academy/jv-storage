583cc4a8446b96309024ffed2359a1f3eecc0e78
Alkayde <morozovnikita5899@gmail.com>
2022-09-02 01:51:56 +0300

    Filled in storage implementation using lists

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,33 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private final List<K> listOfKeys = new ArrayList<>();
+    private final List<V> listOfValues = new ArrayList<>();
+
     @Override
     public void put(K key, V value) {
+        if (listOfKeys.contains(key)) {
+            listOfValues.set(listOfKeys.indexOf(key), value);
+            return;
+        }
+        listOfKeys.add(key);
+        listOfValues.add(value);
     }
 
     @Override
     public V get(K key) {
-        return null;
+        if (!listOfKeys.contains(key)) {
+            return null;
+        }
+        return listOfValues.get(listOfKeys.indexOf(key));
     }
 
     @Override
     public int size() {
-        return -1;
+        return listOfKeys.size();
     }
 }
