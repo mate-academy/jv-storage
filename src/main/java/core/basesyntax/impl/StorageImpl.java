af018757f1fba593a6c1de226e20dba586d2d7c4
Mykyta Hulianytskyi <yadiver82@gmail.com>
2022-09-03 18:28:29 +0300

    v2.0 totally changed methods which put values to storage, get values from storage and returns size of filled storage! Added "size" value which monitors actual realtime size of the filled storage.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,34 +2,47 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
-@SuppressWarnings("unchecked") // do not remove this line
+@SuppressWarnings("unchecked")
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ARRAY_ELEMENTS = 10;
-    private K key;
-    private V value;
-    private K[] keyArray = (K[]) new Object[MAX_ARRAY_ELEMENTS];
-    private V[] valueArray = (V[]) new Object[MAX_ARRAY_ELEMENTS];
+    private int size = 0;
+    private K[] keyArray;
+    private V[] valueArray;
+
+    public StorageImpl() {
+        keyArray = (K[]) new Object[MAX_ARRAY_ELEMENTS];
+        valueArray = (V[]) new Object[MAX_ARRAY_ELEMENTS];
+    }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < keyArray.length; i++) {
-            if ((keyArray[i] == null && valueArray[i] == null)
-                    || (key != null && key.equals(keyArray[i]))
-                    || (key == null && keyArray[i] == null)) {
-                keyArray[i] = key;
+        if (size == 0) {
+            keyArray[size] = key;
+            valueArray[size] = value;
+            size++;
+        }
+        for (int i = 0; i < size; i++) {
+            if ((key != null && key.equals(keyArray[i]))
+                    || (key == keyArray[i])) {
                 valueArray[i] = value;
                 return;
             }
         }
-        throw new RuntimeException("Can't put the data into a Storage - Storage is full ");
+        if (size < MAX_ARRAY_ELEMENTS) {
+            keyArray[size] = key;
+            valueArray[size] = value;
+            size++;
+        } else {
+            throw new RuntimeException("Can't put the data into a Storage - Storage is full ");
+        }
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keyArray.length; i++) {
-            if (key != null && key.equals(keyArray[i])
-                    || (key == null && keyArray[i] == null)) {
+        for (int i = 0; i < size; i++) {
+            if ((key != null && key.equals(keyArray[i]))
+                    || (key == keyArray[i])) {
                 return valueArray[i];
             }
         }
@@ -38,11 +51,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        for (int i = 0; i < valueArray.length; i++) {
-            if (valueArray[i] == null) {
-                return i;
-            }
-        }
-        return 0;
+        return size;
     }
 }
