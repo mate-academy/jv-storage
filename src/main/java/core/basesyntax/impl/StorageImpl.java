7793439e79a03f520c7dae38218b25f4f33d3b4f
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-09-06 22:14:20 +0300

    Rename size variable

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,20 +6,20 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int KEY_INDEX = 0;
     private static final int VALUE_INDEX = 1;
     private final Object[][] objects;
-    private int positionIndex;
+    private int size;
 
     public StorageImpl() {
         this.objects = new Object[2][10];
-        this.positionIndex = 0;
+        this.size = 0;
     }
 
     @Override
     public void put(K key, V value) {
         int index = this.getKeyIndex(key);
         if (index == -1) {
-            objects[KEY_INDEX][positionIndex] = key;
-            objects[VALUE_INDEX][positionIndex] = value;
-            positionIndex++;
+            objects[KEY_INDEX][size] = key;
+            objects[VALUE_INDEX][size] = value;
+            size++;
         } else {
             objects[VALUE_INDEX][index] = value;
         }
@@ -33,11 +33,11 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return positionIndex;
+        return size;
     }
 
     private int getKeyIndex(K key) {
-        for (int i = 0; i < positionIndex; i++) {
+        for (int i = 0; i < size; i++) {
             if (key == null) {
                 if (objects[KEY_INDEX][i] == null) {
                     return i;
