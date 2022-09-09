aeeee11fbff59bebbbc0bde9d6728a0f13b90381
Andrii Kovalskyi <kovalskyi.andrii90@gmail.com>
2022-09-09 11:06:44 +0200

    Storage impliments correctly

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,37 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private static final int MAX_STORAGE_SIZE = 10;
+    private int size;
+    private K[] keyStorage = (K[]) new Object[MAX_STORAGE_SIZE];
+    private V[] valueStorage = (V[]) new Object[MAX_STORAGE_SIZE];
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (key == keyStorage[i] || key != null && key.equals(keyStorage[i])) {
+                valueStorage[i] = value;
+                return;
+            }
+        }
+        keyStorage[size] = key;
+        valueStorage[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == keyStorage[i] || key != null && key.equals(keyStorage[i])) {
+                return (V) valueStorage[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
