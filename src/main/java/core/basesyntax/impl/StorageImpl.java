cf61efa1d9de30023aaf9913a3a30684e4832d36
Oleksandr Kolisnyk <kolisnykol@gmail.com>
2022-09-17 16:24:29 +0300

    Storage Solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,50 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE_STORAGE = 10;
+    private int sizeStorage;
+    private K[] keys;
+    private V[] values;
+
+    public StorageImpl() {
+        sizeStorage = 0;
+        keys = (K[]) new Object[MAX_SIZE_STORAGE];
+        values = (V[]) new Object[MAX_SIZE_STORAGE];
+    }
+
     @Override
     public void put(K key, V value) {
+        int indexForPut = sizeStorage;
+        boolean isKeyExist = false;
+        for (int i = 0; i < sizeStorage; i++) {
+            if (Objects.equals(key,(keys[i]))) {
+                indexForPut = i;
+                isKeyExist = true;
+                break;
+            }
+        }
+        values[indexForPut] = value;
+        if (!isKeyExist) {
+            keys[indexForPut] = key;
+            sizeStorage++;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < sizeStorage; i++) {
+            if (Objects.equals(key,(keys[i]))) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return sizeStorage;
     }
 }
