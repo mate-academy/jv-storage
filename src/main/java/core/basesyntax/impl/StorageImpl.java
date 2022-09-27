7d4a641bd21c28f5cb361f51b79b28a5f0760814
vdziadevych <vdziadevych@magnite.com>
2022-09-27 18:26:31 +0300

    add refactoring by comments

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -15,27 +15,15 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i] == null && key == null) {
-                if (values[i] == null) {
-                    size++;
-                }
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == key || key != null && key.equals(keys[i])) {
                 values[i] = value;
-                break;
+                return;
             }
-            if (keys[i] == null && key != null && values[i] == null) {
-                keys[i] = key;
-                values[i] = value;
-                size++;
-                break;
-            }
-            if (keys[i] != null && keys[i].equals(key)) {
-                values[i] = value;
-                break;
-            }
-
         }
-
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
