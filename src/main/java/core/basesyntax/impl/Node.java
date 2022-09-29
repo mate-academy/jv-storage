ea630a900e9562af2f8003b49e7317ecaea74b5e
EvgenyTyshchenko <evgeny.tyshchenko@gmail.com>
2022-09-29 12:56:35 +0300

    V1 storage implementation

---
+++ src/main/java/core/basesyntax/impl/Node.java
@@ -0,0 +1,23 @@
+package core.basesyntax.impl;
+
+public class Node<K, V> {
+    private final K key;
+    private V value;
+
+    public Node(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return this.key;
+    }
+
+    public V getValue() {
+        return this.value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+}
