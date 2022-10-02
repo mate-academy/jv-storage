d6cf67dde910fd331f0af1a6307745594bbd4da6
Volodymyr Oliinyk <volodymyr.oliinyk89@gmail.com>
2022-10-02 14:22:50 +0300

    implemented task

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,47 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private int storageSize = 0;
+    private final K[] keyStorage;
+    private final V[] valueStorage;
+
+    public StorageImpl() {
+        keyStorage = (K[]) new Object[MAX_ITEMS_NUMBER];
+        valueStorage = (V[]) new Object[MAX_ITEMS_NUMBER];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (get(key) != null) {
+            valueStorage[getIndex(key)] = value;
+            return;
+        }
+        keyStorage[storageSize] = key;
+        valueStorage[storageSize] = value;
+        storageSize++;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return getIndex(key) == storageSize ? null : valueStorage[getIndex(key)];
     }
 
     @Override
     public int size() {
-        return -1;
+        return storageSize;
+    }
+
+    private int getIndex(K key) {
+        for (int i = 0; i < storageSize; i++) {
+            if (equalsObjects(keyStorage[i], key)) {
+                return i;
+            }
+        }
+        return storageSize;
+    }
+
+    private boolean equalsObjects(Object a, Object b) {
+        return a == b || (a != null && a.equals(b));
     }
 }
