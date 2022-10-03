5e21019965c6d68925ba9dedb51fc0ae5eae5a4c
Alex Anofriev <alex.linkoln7391@gmail.com>
2022-10-04 01:04:03 +0300

    Task solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,18 +2,21 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
-public class StorageImpl<K, V> implements Storage<K, V> {
+public class StorageImpl<K, S> implements Storage<K, S> {
+    private final StorageData<K, S> data = new StorageData<>();
+
     @Override
-    public void put(K key, V value) {
+    public void put(K key, S value) {
+        data.setKeysAndValue(key,value);
     }
 
     @Override
-    public V get(K key) {
-        return null;
+    public S get(K key) {
+        return data.getByKeys(key);
     }
 
     @Override
     public int size() {
-        return -1;
+        return data.getSizeOfArray();
     }
 }
