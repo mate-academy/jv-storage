e256813415167d394d2ea64bdf7fb5060b569af9
Bohdan Vyshnevskyy <yksvenhsiv@gmail.com>
2022-10-04 15:16:56 +0300

    solution was added

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,44 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int NOT_FOUND = -1;
+    private int size;
+    private Pair<K, V> [] items;
+
+    public StorageImpl() {
+        items = new Pair[10];
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = findIndexByKey(key);
+        if (index != NOT_FOUND) {
+            items[index].setValue(value);
+            return;
+        }
+        items[size] = new Pair<>(key, value);
+        size++;
+    }
+
+    private int findIndexByKey(K key) {
+        for (int i = 0; i < size; i++) {
+            if (items[i].getKey() == key
+                    || items[i].getKey() != null
+                    && items[i].getKey().equals(key)) {
+                return i;
+            }
+        }
+        return NOT_FOUND;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int index = findIndexByKey(key);
+        return index == NOT_FOUND ? null : items[index].getValue();
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
