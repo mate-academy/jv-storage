eee7847b765d5f69736e4949af2d6f49b45dcd8f
Anastasia Vasylyk <anastasiavasylykv@gmail.com>
2022-10-06 12:34:43 +0300

    Implement Storage solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,48 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int SIZE = 10;
+    private Object[] keys;
+    private Object[] values;
+    private int elementQuantity = 0;
+
+    public StorageImpl() {
+        this.keys = new Object[SIZE];
+        this.values = new Object[SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = -1;
+        for (int i = 0; i < elementQuantity; i++) {
+            if (Objects.equals(keys[i], key)) {
+                index = i;
+                values[index] = value;
+                break;
+            }
+        }
+        if (index == -1) {
+            values[elementQuantity] = value;
+            keys[elementQuantity] = key;
+            elementQuantity++;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < elementQuantity; i++) {
+            if (Objects.equals(keys[i], key)) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return elementQuantity;
     }
 }
