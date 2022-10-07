a5bb64e37dacf7e7eede5ba8850755a60c6e9b95
Pavlo Hryhorash <pavlo.hryhorash@gmail.com>
2022-10-07 16:55:16 +0300

    Solved jv-storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,53 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE = 10;
+    private int size = 0;
+    private K[] keyArray;
+    private V[] valueArray;
+
+    public StorageImpl() {
+        keyArray = (K[]) new Object[MAX_SIZE];
+        valueArray = (V[]) new Object[MAX_SIZE];
+    }
+
+    public int findIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(key, keyArray[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = findIndex(key);
+        if (findIndex(key) >= 0) {
+            valueArray[index] = value;
+            return;
+        }
+        if (size < MAX_SIZE) {
+            keyArray[size] = key;
+            valueArray[size] = value;
+            size++;
+        }
     }
 
     @Override
     public V get(K key) {
+        int index = findIndex(key);
+        if (index >= 0) {
+            return valueArray[index];
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
