8c3eeb29fd9ed63af62115b735b5f75cdc45b6d3
MuzykaVP <muzykavp.work@gmail.com>
2022-10-07 18:25:26 +0300

    implemented business logic to storage program

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,29 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.ArrayList;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private final ArrayList<V> valuesList = new ArrayList<>();
+    private final ArrayList<K> keysList = new ArrayList<>();
+
     @Override
     public void put(K key, V value) {
+        if (keysList.contains(key)) {
+            valuesList.set(keysList.indexOf(key), value);
+            return;
+        }
+        valuesList.add(value);
+        keysList.add(key);
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return keysList.contains(key) ? valuesList.get(keysList.indexOf(key)) : null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return valuesList.size();
     }
 }
