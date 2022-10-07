31516bb02395fb40cfa69d789309dce106b71638
Anastasia Vasylyk <anastasiavasylykv@gmail.com>
2022-10-07 14:08:27 +0300

    added fixes after 2nd review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,7 +1,6 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int SIZE = 10;
@@ -16,7 +15,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     private int compareKeys(K key) {
         for (int i = 0; i < elementQuantity; i++) {
-            if (Objects.equals(keys[i], key)) {
+            if ((keys[i] == key) || (keys[i] != null && keys[i].equals(key))) {
                 return i;
             }
         }
