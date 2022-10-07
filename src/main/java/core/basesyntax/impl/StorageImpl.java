48f82417474883e9657db9c62acdf31eb405d717
Artem Kurinniy <artem.kyrinniy@gmail.com>
2022-10-07 13:14:34 +0300

    returned trigger for null

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,6 +4,7 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_STORAGE_SIZE = 10;
+    private static final String TRIGGER_FOR_NULL = "null";
     private final K[] keys = (K[]) new Object[MAX_STORAGE_SIZE];
     private final V[] values = (V[]) new Object[MAX_STORAGE_SIZE];
     private int index;
@@ -12,7 +13,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         if (key == null) {
-            keys[index] = (K) "null";
+            keys[index] = (K) TRIGGER_FOR_NULL;
         } else {
             keys[index] = key;
         }
@@ -24,7 +25,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         if (key == null) {
-            key = (K) "null";
+            key = (K) TRIGGER_FOR_NULL;
         }
         for (int i = MAX_STORAGE_SIZE; i > 0; i--) {
             if (key.equals(keys[i - 1])) {
