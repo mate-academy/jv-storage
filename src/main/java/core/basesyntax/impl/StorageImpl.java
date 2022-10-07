028daff0b4deeb8656a72ccd7032ec4b5ac6c4ca
Serhii <romanchuk552@gmail.com>
2022-10-07 18:18:37 +0300

    jv-storage completed

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,72 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_LENGTH = 10;
+    private final K[] keys;
+    private final V[] values;
+    private int counter;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_LENGTH];
+        values = (V[]) new Object[MAX_LENGTH];
+    }
+
     @Override
     public void put(K key, V value) {
+        boolean flag = false;
+        int neededIndex = 0;
+        if (key == null) {
+            for (int i = 0; i < counter; i++) {
+                if (keys[i].equals("null")) {
+                    flag = true;
+                    neededIndex = i;
+                }
+            }
+        } else {
+            for (int i = 0; i < counter; i++) {
+                if (keys[i].equals(key)) {
+                    flag = true;
+                    neededIndex = i;
+                }
+            }
+        }
+        if (flag) {
+            if (key == null) {
+                values[neededIndex] = value;
+            } else {
+                values[neededIndex] = value;
+            }
+        } else {
+            if (key == null) {
+                keys[counter] = (K) "null";
+            } else {
+                keys[counter] = key;
+            }
+            values[counter] = value;
+            counter++;
+        }
     }
 
     @Override
     public V get(K key) {
+        if (key == null) {
+            for (int i = 0; i < counter; i++) {
+                if (keys[i] == "null") {
+                    return values[i];
+                }
+            }
+        } else {
+            for (int i = 0; i < counter; i++) {
+                if (keys[i].equals(key)) {
+                    return values[i];
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return counter;
     }
 }
