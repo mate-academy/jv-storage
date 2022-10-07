5c440c89fe53937af197de9009978297e0a6bb29
Andrii Matveev <matveev.tesla@gmail.com>
2022-10-07 13:35:50 +0300

    homework has been done

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,61 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private K key;
+    private V value;
+    private StorageImpl[] storages = new StorageImpl[10];
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public K getKey() {
+        return this.key;
+    }
+
     @Override
     public void put(K key, V value) {
+        for (StorageImpl<K, V> str : storages) {
+            if (str != null && Objects.equals(str.getKey(), key)) {
+                str.value = value;
+                return;
+            }
+        }
+        for (int i = 0; i < storages.length; i++) {
+            if (storages[i] == null) {
+                storages[i] = new StorageImpl<K, V>();
+                storages[i].setKey(key);
+                storages[i].setValue(value);
+                return;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (StorageImpl<K, V> str : storages) {
+            if (str != null && Objects.equals(str.key, key)) {
+                return str.value;
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        int size = 0;
+        for (StorageImpl<K, V> str : storages) {
+            if (str != null) {
+                size++;
+            }
+        }
+        return size;
     }
 }
