317f57e59be3c288f47d8c9b1364f4a414a5b33d
Andrew Miroshnikov <giggle2594@gmail.com>
2022-10-10 01:44:10 +0300

    Put my brain in Storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,43 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int START_CAPACITY = 10;
+    private final K[] keys = (K[]) new Object[START_CAPACITY];
+    private final V[] values = (V[]) new Object[START_CAPACITY];
+    private int size;
+
+    public int indexOf(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
     @Override
     public void put(K key, V value) {
+        if (size == START_CAPACITY || get(key) == null) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
+
+        values[indexOf(key)] = value;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        if (indexOf(key) == -1) {
+            return null;
+        }
+        return values[indexOf(key)];
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
+
 }
