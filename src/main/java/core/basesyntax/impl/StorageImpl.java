2fe2007e6d800ee3b3925a8b8016708e417e2c7e
Veronika Tretiak <nikatretiak9@gmail.com>
2022-10-09 21:22:59 +0200

    implemented task

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,47 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private final K[] keys;
+    private final V[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[DEFAULT_CAPACITY];
+        values = (V[]) new Object[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (size == DEFAULT_CAPACITY || get(key) == null) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        } else {
+            values[getIndex(key)] = value;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        if (getIndex(key) == -1) {
+            return null;
+        } else {
+            return values[getIndex(key)];
+        }
+    }
+
+    public int getIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
