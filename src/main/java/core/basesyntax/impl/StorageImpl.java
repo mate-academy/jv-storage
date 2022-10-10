3127c3c3a405e67407297afbaafb75081224008f
AlinaOryshak <alinaoryshak@gmail.com>
2022-10-10 16:03:07 +0300

    constant was changed, getFreeCellIndex() was deleted, put() and getKeyIndexInStorage() were changed

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,7 +4,7 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ITEMS_NUMBER = 10;
-    private static final int NOTHING_WAS_FOUND = -1;
+    private static final int NOT_FOUND_KEY = -1;
     private K[] keys;
     private V[] values;
     private int size;
@@ -18,10 +18,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         int keyIndex = getKeyIndexInStorage(key);
-        if (keyIndex == NOTHING_WAS_FOUND) {
-            int cellIndex = getFreeCellIndex();
-            keys[cellIndex] = key;
-            values[cellIndex] = value;
+        if (keyIndex == NOT_FOUND_KEY) {
+            keys[size] = key;
+            values[size] = value;
             size++;
         } else {
             size += values[keyIndex] == null ? 1 : 0;
@@ -31,12 +30,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < MAX_ITEMS_NUMBER; i++) {
-            if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
-                return values[i];
-            }
-        }
-        return null;
+        int keyIndex = getKeyIndexInStorage(key);
+        return keyIndex > -1 ? values[keyIndex] : null;
     }
 
     @Override
@@ -46,19 +41,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     public int getKeyIndexInStorage(K key) {
         for (int i = 0; i < MAX_ITEMS_NUMBER; i++) {
-            if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
+            if ((key == keys[i]) || (key != null && key.equals(keys[i]))) {
                 return i;
             }
         }
-        return NOTHING_WAS_FOUND;
-    }
-
-    private int getFreeCellIndex() {
-        for (int i = 0; i < MAX_ITEMS_NUMBER; i++) {
-            if (keys[i] == null && values[i] == null) {
-                return i;
-            }
-        }
-        return NOTHING_WAS_FOUND;
+        return NOT_FOUND_KEY;
     }
 }
