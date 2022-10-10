adfe3fc5afaf1d11b6b710b6c7be235b6c6d4c14
Oleksandr <oleksandrkorniychuk98@gmail.com>
2022-10-10 19:44:47 +0300

    Removed using class Objects {{{(>_<)}}}

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,7 +1,6 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_VALUE = 10;
@@ -39,7 +38,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     private Integer toFindIndex(K key) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(keys[i], key)) {
+            if ((keys[i] == key) || (keys[i] != null && keys[i].equals(key))) {
                 return i;
             }
         }
