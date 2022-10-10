ca5426eda54d583020e3a9aaa292d51fe31e650b
Artem Kurinniy <artem.kyrinniy@gmail.com>
2022-10-10 14:24:23 +0300

    optimized readable

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,51 +3,73 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int MAX_STORAGE_SIZE = 10;
-    private final K[] keys = (K[]) new Object[MAX_STORAGE_SIZE];
-    private final V[] values = (V[]) new Object[MAX_STORAGE_SIZE];
-    private int index;
+    private static final int MAX_SIZE = 10;
+    private static final int ARRAYS_ARE_EMPTY = 0;
+    private final Object[] keys;
+    private final Object[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = new Object[MAX_SIZE];
+        values = new Object[MAX_SIZE];
+    }
 
     @Override
     public void put(K key, V value) {
-        if (key != null) {
-            if (key.equals(keys[index == 0 ? index : index - 1])) {
-                values[index - 1] = value;
-                keys[index - 1] = key;
-                return;
-            }
+        if (canAddWithSameKey(key, value)) {
+            return;
         }
-        values[index] = value;
-        keys[index] = key;
-        index++;
-        getDefaultIndex();
-    }
-
-    private void getDefaultIndex() {
-        if (keys[0] == null & values[0] != null) {
-            index = 1;
-        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        if (size == ARRAYS_ARE_EMPTY) {
+            return null;
+        }
         if (key != null) {
-            for (int i = 0; i < MAX_STORAGE_SIZE; i++) {
-                if (key.equals(keys[i])) {
-                    return values[i];
+            for (int i = size; i >= 0; i--) {
+                if (key.equals(keys[i - 1])) {
+                    return (V) values[i - 1];
                 }
             }
         }
-        for (int i = MAX_STORAGE_SIZE; i > 0; i--) {
-            if (keys[i - 1] == null & values[i - 1] != null) {
-                return values[i - 1];
-            }
-        }
-        return null;
+        return (V) values[findValueWithNullKey(key)];
     }
 
     @Override
     public int size() {
+        return size;
+    }
+
+    private boolean canAddWithSameKey(K key, V value) {
+        if (size != ARRAYS_ARE_EMPTY) {
+            if (key != null) {
+                if (key.equals(keys[size - 1])) {
+                    keys[size - 1] = key;
+                    values[size - 1] = value;
+                    return true;
+                }
+            } else {
+                if (keys[size - 1] == null) {
+                    values[size] = value;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private int findValueWithNullKey(K key) {
+        int index = size;
+        for (int i = size - 1; i > 0; i--) {
+            if (keys[i] == key & values[i] != null) {
+                index = i;
+                break;
+            }
+        }
         return index;
     }
 }
