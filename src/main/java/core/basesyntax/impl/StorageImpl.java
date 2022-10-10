886bf55d21f94a0d1b3745913994a3f9bfeeb10a
Artem Kurinniy <artem.kyrinniy@gmail.com>
2022-10-10 14:31:06 +0300

    optimized readable

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,16 +2,17 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+@SuppressWarnings("unchecked")
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
     private static final int ARRAYS_ARE_EMPTY = 0;
-    private final Object[] keys;
-    private final Object[] values;
+    private final K[] keys;
+    private final V[] values;
     private int size;
 
     public StorageImpl() {
-        keys = new Object[MAX_SIZE];
-        values = new Object[MAX_SIZE];
+        keys = (K[]) new Object[MAX_SIZE];
+        values = (V[]) new Object[MAX_SIZE];
     }
 
     @Override
@@ -32,11 +33,11 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         if (key != null) {
             for (int i = size; i >= 0; i--) {
                 if (key.equals(keys[i - 1])) {
-                    return (V) values[i - 1];
+                    return values[i - 1];
                 }
             }
         }
-        return (V) values[findValueWithNullKey(key)];
+        return values[findValueWithNullKey(key)];
     }
 
     @Override
