eba2283170482d90c5d8e7edaa33f0a5b31b8eb5
Pavlo.Polovyi <pasha32662@gmail.com>
2022-10-11 13:51:53 +0300

    Changed code according to mentor review, in some places made it easier to read. Splited put and get method. Removed unnecessary hashCode field in Entry class. Made correct matching of keys in get method. Made correct putting of entries in bucket. Made Entry class private.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,113 +3,36 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int DEFAULT_STORAGE_SIZE = 15;
-    private static final int DEFAULT_STORAGE_GROW_FACTOR = 1;
-    private static final double DEFAULT_LOAD_CAPACITY = 0.5;
-    private int storageSize;
+    private static final int STORAGE_SIZE = 15;
     private int currentSize;
-    private boolean isGrowable;
-    private Object[] entries;
+    private final Object[] entries;
 
     public StorageImpl() {
-        storageSize = DEFAULT_STORAGE_SIZE;
         currentSize = 0;
-        entries = new Object[DEFAULT_STORAGE_SIZE];
-        isGrowable = false;
+        entries = new Object[STORAGE_SIZE];
     }
 
-    public StorageImpl(boolean isGrowable) {
-        this();
-        this.isGrowable = isGrowable;
-    }
-
-    public StorageImpl(int storageSize) {
-        this.storageSize = storageSize;
-        currentSize = 0;
-        this.entries = new Object[storageSize];
-        isGrowable = false;
-    }
-
-    static class Entry<K, V> {
-        private final K key;
-        private final Integer hashCode;
-        private V value;
-        private Entry<K, V> next;
-
-        Entry(K key, V value, Integer hashCode) {
-            this.key = key;
-            this.value = value;
-            this.hashCode = hashCode;
-            next = null;
-        }
-
-        public Integer getHashCode() {
-            return hashCode;
-        }
-
-        public V getValue() {
-            return value;
-        }
-
-        public K getKey() {
-            return key;
-        }
-
-        public void setValue(V value) {
-            this.value = value;
-        }
-
-        public Entry<K, V> getNext() {
-            return next;
-        }
-
-        public void setNext(Entry<K, V> next) {
-            this.next = next;
-        }
-    }
-
-    @SuppressWarnings("unchecked")
     @Override
     public void put(K key, V value) {
-        Entry<K, V> entry;
         checkCapacity();
         int hashCode = getHashCode(key);
-        int index = Math.abs(hashCode % storageSize);
+        int index = Math.abs(hashCode % STORAGE_SIZE);
         if (entries[index] != null) {
-            Entry<K, V> existed = (Entry<K, V>) entries[index];
-            if (existed.getHashCode().equals(hashCode)) {
-                existed.setValue(value);
-            } else {
-                entry = new Entry<>(key, value, hashCode);
-                existed.setNext(entry);
-                currentSize++;
-            }
+            addOrUpdateEntry(key, value, index);
         } else {
-            entry = new Entry<>(key, value, hashCode);
-            entries[index] = entry;
-            currentSize++;
+            addNewEntry(key, value, index);
         }
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public V get(K key) {
-        Entry<K, V> entry;
-        Integer hashCode = getHashCode(key);
-        int index = Math.abs(hashCode % storageSize);
+        int hashCode = getHashCode(key);
+        int index = Math.abs(hashCode % STORAGE_SIZE);
         if (entries[index] != null) {
-            entry = (Entry<K, V>) entries[index];
-            if (entry.getHashCode().equals(hashCode)) {
-                return entry.getValue();
+            if (key == null) {
+                return getNullKey(index);
             } else {
-                Entry<K, V> next = entry.getNext();
-                while (next != null) {
-                    if (next.getHashCode().equals(hashCode)) {
-                        return next.getValue();
-                    }
-                    next = next.getNext();
-                }
-                return null;
+                return getNonNullKey(key, index);
             }
         }
         return null;
@@ -120,43 +43,100 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         return currentSize;
     }
 
-    private Integer getHashCode(K key) {
+    private int getHashCode(K key) {
         return key == null ? 0 : key.hashCode();
     }
 
     private void checkCapacity() {
-        if (isGrowable) {
-            double currentLoad = (double) currentSize / storageSize;
-            if (currentLoad >= DEFAULT_LOAD_CAPACITY) {
-                reformateStorage();
-            }
+        int spaceLeft = STORAGE_SIZE - currentSize;
+        if (spaceLeft == 0) {
+            throw new RuntimeException("Storage is full! "
+                    + "Parameter <isGrowable> was set to false. "
+                    + "Max size is " + STORAGE_SIZE);
+        }
+    }
+
+    private void addNewEntry(K key, V value, int index) {
+        Entry<K, V> entry = new Entry<>(key, value);
+        entries[index] = entry;
+        currentSize++;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void addOrUpdateEntry(K key, V value, int index) {
+        Entry<K, V> existed = (Entry<K, V>) entries[index];
+        if (key == null && existed.getKey() == null) {
+            existed.setValue(value);
+        } else if (existed.getKey().equals(key)) {
+            existed.setValue(value);
         } else {
-            int left = storageSize - currentSize;
-            if (left == 0) {
-                throw new RuntimeException("Storage is full! "
-                        + "Parameter <isGrowable> was set to false. "
-                        + "Max size is " + storageSize);
+            Entry<K, V> entry = new Entry<>(key, value);
+            while (true) {
+                Entry<K, V> temporary = existed.getNext();
+                if (temporary == null) {
+                    existed.setNext(entry);
+                    break;
+                } else {
+                    existed = temporary;
+                }
             }
+            currentSize++;
         }
     }
 
     @SuppressWarnings("unchecked")
-    private void reformateStorage() {
-        storageSize = storageSize << DEFAULT_STORAGE_GROW_FACTOR;
-        Object[] newEntries = new Object[storageSize];
-        Object[] oldEntries = entries;
-        entries = newEntries;
-        currentSize = 0;
-        for (Object oldEntry : oldEntries) {
-            if (oldEntry != null) {
-                Entry<K, V> entry = (Entry<K, V>) oldEntry;
-                put(entry.getKey(), entry.getValue());
-                Entry<K, V> next = entry.getNext();
-                while (next != null) {
-                    put(next.getKey(), next.getValue());
-                    next = next.getNext();
-                }
+    private V getNullKey(int index) {
+        Entry<K, V> entry = (Entry<K, V>) entries[index];
+        while (entry != null) {
+            if (entry.getKey() == null) {
+                return entry.getValue();
             }
+            entry = entry.getNext();
+        }
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    private V getNonNullKey(K key, int index) {
+        Entry<K, V> entry = (Entry<K, V>) entries[index];
+        while (entry != null) {
+            if (entry.getKey().equals(key)) {
+                return entry.getValue();
+            }
+            entry = entry.getNext();
+        }
+        return null;
+    }
+
+    private static class Entry<K, V> {
+        private final K key;
+        private V value;
+        private Entry<K, V> next;
+
+        Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+            next = null;
+        }
+
+        private V getValue() {
+            return value;
+        }
+
+        private K getKey() {
+            return key;
+        }
+
+        private void setValue(V value) {
+            this.value = value;
+        }
+
+        private Entry<K, V> getNext() {
+            return next;
+        }
+
+        private void setNext(Entry<K, V> next) {
+            this.next = next;
         }
     }
 }
