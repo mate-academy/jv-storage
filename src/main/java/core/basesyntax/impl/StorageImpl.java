332da1e2ed44c763099d9261cba7ccfcd52eebc4
MuzykaVP <muzykavp.work@gmail.com>
2022-10-11 15:49:48 +0300

    implemented mentors suggestion

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,36 +3,39 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private int size = 0;
-    private Object[] valuesList = new Object[10];
-    private Object[] keysList = new Object[10];
+    private static final int ARRAY_SIZE = 10;
+    private int size;
+    private final Object[] valuesList;
+    private final Object[] keysList;
 
-    @Override
+    public StorageImpl() {
+        size = 0;
+        valuesList = new Object[ARRAY_SIZE];
+        keysList = new Object[ARRAY_SIZE];
+    }
+
+    private Integer indexOfKey(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keysList[i] == key || keysList[i] != null && keysList[i].equals(key)) {
+                return i;
+            }
+        }
+        return size;
+    }
+
     public void put(K key, V value) {
-        for (int i = 0; i < keysList.length; i++) {
-            if (keysList[i] == null && valuesList[i] != null && keysList[i] == key
-                    || keysList[i] != null && keysList[i].equals(key)) {
-                valuesList[i] = value;
-                break;
-            }
-            if (keysList[i] == null && valuesList[i] == null) {
-                keysList[i] = key;
-                valuesList[i] = value;
-                size++;
-                break;
-            }
+        if (indexOfKey(key) != size) {
+            valuesList[indexOfKey(key)] = value;
+        } else {
+            keysList[size] = key;
+            valuesList[size] = value;
+            size++;
         }
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keysList.length; i++) {
-            if (keysList[i] == key && valuesList[i] != null
-                    || keysList[i] != null && keysList[i].equals(key)) {
-                return (V) valuesList[i];
-            }
-        }
-        return null;
+        return (V) valuesList[indexOfKey(key)];
     }
 
     @Override
