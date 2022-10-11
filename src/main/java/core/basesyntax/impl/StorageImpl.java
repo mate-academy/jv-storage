4d762a5f7e93caba6041e42a3c28d353768c9e26
Sidir0m <romangolovatyi@yahoo.com>
2022-10-11 13:19:55 +0300

    Corrected code logic with DRY pattern after mentors remark

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,37 +4,44 @@ import core.basesyntax.Storage;
 import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-
     private static final int MAX_ITEMS_NUMBER = 10;
     private K[] keys;
     private V[] values;
-    private int sizeOfStorage = 0;
+    private int sizeOfStorage;
 
     public StorageImpl() {
         this.keys = (K[]) new Object[MAX_ITEMS_NUMBER];
-        this.values = (V[]) new Object[MAX_ITEMS_NUMBER];;
+        this.values = (V[]) new Object[MAX_ITEMS_NUMBER];
+        sizeOfStorage = 0;
+    }
+
+    private Integer indexOfKey(K key) {
+        for (int i = 0; i < sizeOfStorage; i++) {
+            if (key == null && keys[i] == null || key != null && key.equals(keys[i])) {
+                return i;
+            }
+        }
+        return null;
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < sizeOfStorage; i++) {
-            if (key == null && keys[i] == null || key != null && key.equals(keys[i])) {
-                this.values[i] = value;
-                this.keys[i] = key;
-                return;
-            }
+        Integer index = indexOfKey(key);
+        if (index != null) {
+            this.values[index] = value;
+            this.keys[index] = key;
+        } else if (sizeOfStorage < MAX_ITEMS_NUMBER) {
+            keys[sizeOfStorage] = key;
+            values[sizeOfStorage] = value;
+            sizeOfStorage++;
         }
-        keys[sizeOfStorage] = key;
-        values[sizeOfStorage] = value;
-        sizeOfStorage++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < sizeOfStorage; i++) {
-            if (key == null && keys[i] == null || key != null && key.equals(keys[i])) {
-                return values[i];
-            }
+        Integer index = indexOfKey(key);
+        if (index != null) {
+            return values[index];
         }
         return null;
     }
