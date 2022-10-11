ce647d9da7f62bc560f5d4c54dde339849f2d947
Artem Kurinniy <artem.kyrinniy@gmail.com>
2022-10-11 13:57:12 +0300

    added one validation

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,6 +6,7 @@ import core.basesyntax.Storage;
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
     private static final int START_INDEX = 1;
+    private static final int UNEXPECTED_KEY = -1;
     private final K[] keys;
     private final V[] values;
     private int size = 1;
@@ -18,13 +19,15 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         int i = indexOFkey(key);
-        if (key == null && value != null) {
-            values[i] = value;
-            return;
-        }
-        if (key != null && key.equals(keys[i])) {
-            values[i] = value;
-            return;
+        if (i != UNEXPECTED_KEY) {
+            if (key == null && value != null) {
+                values[i] = value;
+                return;
+            }
+            if (key != null && key.equals(keys[i])) {
+                values[i] = value;
+                return;
+            }
         }
         keys[size] = key;
         values[size] = value;
@@ -33,7 +36,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        return values[indexOFkey(key)];
+        if (indexOFkey(key) != UNEXPECTED_KEY) {
+            return values[indexOFkey(key)];
+        }
+        return null;
     }
 
     @Override
@@ -51,6 +57,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
                     return i;
                 }
             }
+            return UNEXPECTED_KEY;
         }
         return 0;
     }
