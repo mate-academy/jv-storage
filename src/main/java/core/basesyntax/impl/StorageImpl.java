80d2600ad3e50f9eda72c0311a3d6f91c78c8a03
apc10bbe <poka4ailo.oleg@gmail.com>
2022-10-11 16:05:25 +0300

    added getIndex method and changed get and put methods

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,7 +4,7 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
-    private static final int NULL_SIZE = 1;
+    private static final int NULL_POINT = -1;
     private int count;
     private K[] keys;
     private V[] values;
@@ -15,43 +15,38 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         values = (V[]) new Object[MAX_SIZE];
     }
 
-    public boolean checkValue(K key, int i) {
-        if (key != null && key.equals(keys[i])) {
-            return true;
-        }
-        return key == null && key == keys[i];
-    }
-
     @Override
     public void put(K key, V value) {
-        boolean putOrNot = false;
-        for (int i = 0; i < keys.length; i++) {
-            if (checkValue(key, i)) {
-                values[i] = value;
-                putOrNot = true;
-            }
-        }
-        if (!putOrNot) {
+        if (getIndex(key) == NULL_POINT) {
             keys[count] = key;
             values[count] = value;
             count++;
         } else {
-            count = NULL_SIZE;
+            values[getIndex(key)] = value;
         }
+
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keys.length; i++) {
-            if (checkValue(key, i)) {
-                return values[i];
-            }
-        }
-        return null;
+        return getIndex(key) > NULL_POINT ? values[getIndex(key)] : null;
     }
 
     @Override
     public int size() {
         return count;
+    }
+
+    private boolean checkValue(K key, int i) {
+        return ((key == keys[i]) || (key != null && key.equals(keys[i])));
+    }
+
+    private int getIndex(K key) {
+        for (int i = 0; i < count; i++) {
+            if (checkValue(key, i)) {
+                return i;
+            }
+        }
+        return NULL_POINT;
     }
 }
