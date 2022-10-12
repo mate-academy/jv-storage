84c597cbe494f17896f41704d918ad1b2b807fda
Valerii <kulibchenkovalera@gmail.com>
2022-10-12 11:01:07 +0300

    Second commit

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,51 +4,46 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     public static final int MAX_ELEMENT = 10;
-    private K[] keys = (K[]) new Object[MAX_ELEMENT];
-    private V[] values = (V[]) new Object[MAX_ELEMENT];
+    private final K[] keys;
+    private final V[] values;
     private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_ELEMENT];
+        values = (V[]) new Object[MAX_ELEMENT];
+        size = 0;
+    }
 
     @Override
     public void put(K key, V value) {
-        if (size == 0) {
-            keys[0] = key;
-            values[0] = value;
-            size++;
-        } else {
-            for (int i = 0; i < size; i++) {
-                if (key == null) {
-                    if (keys[i] == null) {
-                        values[i] = value;
-                        return;
-                    }
-                } else if (key.equals(keys[i])) {
-                    values[i] = value;
-                    System.out.println(value);
-                    return;
-                }
-            }
-            keys[size] = key;
-            values[size] = value;
+        int index = index(key);
+        keys[index] = key;
+        values[index] = value;
+        if (index == size) {
             size++;
         }
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < size; i++) {
-            if (key == null) {
-                if (keys[i] == null) {
-                    return values[i];
-                }
-            } else if (key.equals(keys[i])) {
-                return values[i];
-            }
-        }
-        return null;
+        return values[index(key)];
     }
 
     @Override
     public int size() {
+        return size;
+    }
+
+    private int index(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == null) {
+                if (keys[i] == null) {
+                    return i;
+                }
+            } else if (key.equals(keys[i])) {
+                return i;
+            }
+        }
         return size;
     }
 }
