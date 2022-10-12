9bebe22891f75dc03f294728f06761f015b63c3d
Sachamunn <denhackgaming@gmail.com>
2022-10-12 18:26:27 +0100

    Main task implemented

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,37 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_STORAGE_SIZE = 10;
+    private Object[] key = new Object[MAX_STORAGE_SIZE];
+    private Object[] value = new Object[MAX_STORAGE_SIZE];
+    private int storageSize;
+
     @Override
     public void put(K key, V value) {
+        int findIndex = findIndex(key);
+        this.value[findIndex] = value;
+        this.key[findIndex] = key;
+        if (findIndex == storageSize) {
+            storageSize++;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return (V) value[findIndex(key)];
     }
 
     @Override
     public int size() {
-        return -1;
+        return storageSize;
+    }
+
+    private int findIndex(K key) {
+        for (int i = 0; i < MAX_STORAGE_SIZE; i++) {
+            if (this.key[i] == key || key != null && key.equals(this.key[i])) {
+                return i;
+            }
+        }
+        return size();
     }
 }
