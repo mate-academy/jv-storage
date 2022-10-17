18e2122d9ea0997009939aedd5379ae5d4a9c8dd
Katya Humeniuk <kapelka.boo@gmail.com>
2022-10-17 19:23:16 +0300

    Solution was added.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,43 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int COUNT_OF_ARRAY = 10;
+    private K [] keys = (K[]) new Object[COUNT_OF_ARRAY];
+    private V [] values = (V[]) new Object[COUNT_OF_ARRAY];
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < keys.length; i++) {
+            if ((keys[i] == null && values[i] == null)
+                    || (keys[i] == null && key == null && values[i] != null)
+                    || (keys[i] != null && keys[i].equals(key))) {
+                keys[i] = key;
+                values[i] = value;
+                break;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] != null && keys[i].equals(key)) {
+                return values[i];
+            } else if (key == null && keys[i] == null) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        int storageSize = 0;
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] != null || values[i] != null) {
+                storageSize++;
+            }
+        }
+        return storageSize;
     }
 }
