bb647e6e788fe321a2ab67f0d24818573ead2618
DY-JS <delo1604@ukr.net>
2022-10-17 12:52:42 +0300

    Solution storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,47 +1,33 @@
 package core.basesyntax.impl;
 
-import core.basesyntax.Box;
 import core.basesyntax.Storage;
 import java.util.ArrayList;
 import java.util.List;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
-    private List<Box> storage = new ArrayList<>();
-
-    public int getIndex(K key) {
-        if (storage.size() <= 0) {
-            return -1;
-        }
-        for (int i = 0; i < storage.size(); i++) {
-            if (storage.get(i).getKey() != null && storage.get(i).getKey().equals(key)
-                    || storage.get(i).getKey() == null && key == null) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
+    private List<Pair> storage = new ArrayList<>();
     @Override
     public void put(K key, V value) {
-        Box box = new Box(key, value);
-        if (box.getValue() == null) {
+        final byte MAX_SIZE = 10;
+        Pair pair = new Pair(key, value);
+        if (pair.getValue() == null) {
             return;
         }
-        if (storage.size() == 10) {
+        if (storage.size() == MAX_SIZE) {
             System.out.println("Sorry, storage is full! Can't add item!");
         }
         if (getIndex(key) >= 0) {
             int i = getIndex(key);
-            storage.set(i, box);
+            storage.set(i, pair);
         } else {
-            storage.add(box);
+            storage.add(pair);
         }
     }
 
     @Override
     public V get(K key) {
-        for (Box item : storage) {
+        for (Pair item : storage) {
             if (item.getKey() != null && item.getKey().equals(key)
                     || item.getKey() == key && key == null) {
                 return (V) item.getValue();
@@ -53,5 +39,15 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return storage.size();
+    }
+
+    private int getIndex(K key) {
+        for (int i = 0; i < storage.size(); i++) {
+            if (storage.get(i).getKey() != null && storage.get(i).getKey().equals(key)
+                    || storage.get(i).getKey() == null && key == null) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
