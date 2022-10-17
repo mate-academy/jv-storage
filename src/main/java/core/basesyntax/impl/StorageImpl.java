3c2b8cd74c5ba0bb35de74b066cd842c6ec79f43
sasha <radovietsoleksandr@gmail.com>
2022-10-17 22:42:34 +0300

    storag11

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,22 +4,60 @@ import core.basesyntax.Storage;
 
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int SIZE = 10;
+    private final K[] arrayKey;
+    private final V[] arrayValue;
+    private  int sizeElementsInArray = 0;
+
+    public StorageImpl() {
+        arrayKey = (K[]) new Object[SIZE];
+        arrayValue = (V[]) new Object[SIZE];
+    }
 
     @Override
     public void put(K key, V value) {
 
+        for (int i = 0; i < arrayKey.length; i++) {
+            if ((key == null && arrayKey[i] == null)) {
+                arrayKey[i] = key;
+                arrayValue[i] = value;
+                break;
+            }
+            if ((key != null) && key.equals(arrayKey[i])) {
+                arrayKey[i] = key;
+                arrayValue[i] = value;
+                break;
+            }
+            if (key != null && arrayValue[i] == null) {
+                arrayKey[i] = key;
+                arrayValue[i] = value;
+                sizeElementsInArray++;
+                break;
+            }
+        }
     }
 
 
     @Override
     public V get(K key) {
-        return null;
+        V value = null;
+        for (int i = 0; i < arrayKey.length; i++) {
+            if (key == null && arrayKey[i] == null){
+                value = arrayValue[i];
+                break;
+            }
+            if(key != null && key.equals(arrayKey[i])){
+                value = arrayValue[i];
+                break;
+            }
+        }
+        return  value;
     }
 
 
     @Override
-    public int size() {
-        return -1;
+    public int size(){
+        return sizeElementsInArray;
     }
 
 }
