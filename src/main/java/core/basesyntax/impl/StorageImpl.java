c87883e82ee8e8f8e528bb0b2a65334d89b0854b
Katya Humeniuk <kapelka.boo@gmail.com>
2022-10-18 13:55:01 +0300

    Fixed after review.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,26 +6,28 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int COUNT_OF_ARRAY = 10;
     private K [] keys = (K[]) new Object[COUNT_OF_ARRAY];
     private V [] values = (V[]) new Object[COUNT_OF_ARRAY];
+    private int storageSize = 0;
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < keys.length; i++) {
-            if ((keys[i] == null && values[i] == null)
-                    || (keys[i] == null && key == null && values[i] != null)
+        for (int i = 0; i <= storageSize; i++) {
+            if (keys[i] == null && key == null && values[i] != null
                     || (keys[i] != null && keys[i].equals(key))) {
                 keys[i] = key;
                 values[i] = value;
                 break;
             }
+            keys[storageSize] = key;
+            values[storageSize] = value;
+            storageSize++;
+            break;
         }
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i] != null && keys[i].equals(key)) {
-                return values[i];
-            } else if (key == null && keys[i] == null) {
+        for (int i = 0; i < storageSize; i++) {
+            if ((keys[i] != null && keys[i].equals(key)) || (key == null && keys[i] == null)) {
                 return values[i];
             }
         }
@@ -34,12 +36,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        int storageSize = 0;
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i] != null || values[i] != null) {
-                storageSize++;
-            }
-        }
         return storageSize;
     }
 }
