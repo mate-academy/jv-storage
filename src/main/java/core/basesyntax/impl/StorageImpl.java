11265577946c92bc7965a3ae43406eef5ef63a48
Olexii Melnik <alexmelnikk@gmail.com>
2022-10-18 10:38:10 +0300

    all test done. Storage works!!!

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,9 +6,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ITEMS_NUMBER = 10;
     private K[] keys;
     private V[] values;
-    private int indexKey = 0;
-    private int indexValue = 0;
-    private int countNull = 0;
+    private int size;
 
     public StorageImpl() {
         keys = (K[]) new Object[MAX_ITEMS_NUMBER];
@@ -17,47 +15,23 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        int marker = 0;
-        if (key == null) {
-            countNull = countNull + 1;
-        }
-        for (int i = 0; i < MAX_ITEMS_NUMBER; i++) {
-            if (key != null && key.equals(keys[i])) {
-                values [i] = value;
-                marker = marker + 1;
-            }
-            if (countNull > 1) {
-                values [i] = value;
-                break;
-
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == key || key != null && key.equals(keys[i])) {
+                values[i] = value;
+                return;
             }
         }
-        if (keys[indexKey] == null) {
-            keys[indexKey] = key;
-        } else if (marker < 1) {
-            indexKey++;
-            keys[indexKey] = key;
-        }
-        if (values[indexValue] == null) {
-            values[indexValue] = value;
-        } else if (marker < 1) {
-            indexValue++;
-            values[indexValue] = value;
-            if (values[indexValue] != null) {
-                indexKey++;
-            }
-        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
         V some = null;
-        for (int i = 0; i < MAX_ITEMS_NUMBER; i++) {
-            if (values[i] != null && key == null && keys[i] == null) {
-                some = values[i];
-                break;
-            } else if (keys[i] != null && key != null && key.equals(keys[i])) {
-                some = values[i];
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == key || key != null && key.equals(keys[i])) {
+                some = values [i];
             }
         }
         return some;
@@ -65,16 +39,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        int countLength = 0;
-        if (countNull > 1) {
-            return 1;
-        } else {
-            for (int i = 0; i < MAX_ITEMS_NUMBER; i++) {
-                if (keys[i] != null || values[i] != null) {
-                    countLength = countLength + 1;
-                }
-            }
-        }
-        return countLength;
+        return size;
     }
 }
