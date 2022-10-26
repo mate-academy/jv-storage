1b81bbd7a28fec44d65e6b56964b93f0a84953e4
Vyacheslav Sukhov <makasin_12@hotmail.com>
2022-10-26 18:39:47 +0200

    26/10 commit

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,16 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int START_CAPACITY = 10;
+    private static int SIZE;
     private final K[] keys;
     private final V[] values;
-    private static final int START_CAPACITY = 10;
-    private int SIZE;
 
     public StorageImpl() {
         keys = (K[]) new Object[START_CAPACITY];
         values = (V[]) new Object[START_CAPACITY];
         SIZE = 0;
     }
-
 
     @Override
     public void put(K key, V value) {
