e3511f8181bb2d1a96b995c765c7a2811271f4e6
AndriySichniy <89684976+SichniyAndriy@users.noreply.github.com>
2022-10-27 21:01:33 +0300

    Update src/main/java/core/basesyntax/impl/StorageImpl.java
    Co-authored-by: Zakhar Chorny <90692308+zachar-chorny@users.noreply.github.com>

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,7 +6,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_LENGTH = 10;
     private final K[] keys = (K[])new Object[MAX_LENGTH];
     private final V[] values = (V[])new Object[MAX_LENGTH];
-    private int size = 0;
+    private int size;
 
     @Override
     public void put(K key, V value) {
