deb9d92c2ec7a5bda64a25c864c0da4228ab5723
maksim-untilov <maksimuntilov7@gmail.com>
2022-10-28 20:33:32 +0300

    removed methods of class Objects

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,12 +1,11 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
     private static final int DEFAULT_CAPACITY = 10;
-    private static int size;
+    private int size;
     private K[] keys;
     private V[] values;
 
@@ -17,22 +16,23 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i] == null && values[i] == null || Objects.equals(keys[i], key)) {
-                keys[i] = key;
+        for (int i = 0; i < size; i++) {
+            if ((keys[i] == null && key == null) || key != null && key.equals(keys[i])) {
                 values[i] = value;
-                break;
+                return;
             }
         }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
         V value = null;
         for (int i = 0; i < keys.length; i++) {
-            if (Objects.equals(keys[i], key)) {
-                value = values[i];
-                break;
+            if ((keys[i] == null && key == null) || key != null && key.equals(keys[i])) {
+                return (V) values[i];
             }
         }
         return value;
@@ -40,12 +40,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        size = 0;
-        for (int i = 0; i < keys.length; i++) {
-            if (values[i] != null) {
-                size++;
-            }
-        }
         return size;
     }
 }
