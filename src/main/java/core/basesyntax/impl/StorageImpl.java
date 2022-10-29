ff98ade376665bc84d92800f27433042d2a8563a
Roman Yuskevych <yuskevych@gmail.com>
2022-10-29 22:54:38 +0300

    added fixes after review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,59 +6,44 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_STORAGE_SIZE = 10;
     private final K[] keys;
     private final V[] values;
-    private int storageSize;
+    private int size;
 
     public StorageImpl() {
         keys = (K[]) new Object[MAX_STORAGE_SIZE];
         values = (V[]) new Object[MAX_STORAGE_SIZE];
-        storageSize = 0;
-    }
-
-    private K getKeyByIndex(int index) {
-        return keys[index];
-    }
-
-    private void setKeyByIndex(int index, K key) {
-        keys[index] = key;
-    }
-
-    private V getValueByIndex(int index) {
-        return values[index];
-    }
-
-    private void setValueByIndex(int index, V value) {
-        values[index] = value;
     }
 
     @Override
     public void put(K key, V value) {
         int i;
-        for (i = 0; i < storageSize; i++) {
-            if (key == null && getKeyByIndex(i) == null
-                    || key != null && key.equals(getKeyByIndex(i))) {
-                setValueByIndex(i, value);
+        for (i = 0; i < size; i++) {
+            if (isKeysEqual(key, keys[i])) {
+                values[i] = value;
                 return;
             }
         }
-        setKeyByIndex(i, key);
-        setValueByIndex(i, value);
-        storageSize += 1;
+        keys[i] = key;
+        values[i] = value;
+        size += 1;
     }
 
     @Override
     public V get(K key) {
         int i;
-        for (i = 0; i < storageSize; i++) {
-            if (key == null && getKeyByIndex(i) == null
-                    || key != null && key.equals(getKeyByIndex(i))) {
+        for (i = 0; i < size; i++) {
+            if (isKeysEqual(key, keys[i])) {
                 break;
             }
         }
-        return getValueByIndex(i);
+        return values[i];
     }
 
     @Override
     public int size() {
-        return storageSize;
+        return size;
+    }
+
+    private boolean isKeysEqual(K keyOne, K keyTwo) {
+        return keyOne == null && keyTwo == null || keyOne != null && keyOne.equals(keyTwo);
     }
 }
