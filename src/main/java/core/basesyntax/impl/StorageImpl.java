b92f68955b390ea6909538b50dbb26239ff254f2
Roman Yuskevych <yuskevych@gmail.com>
2022-10-29 18:41:49 +0300

    added jv-storage homework solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,62 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_STORAGE_SIZE = 10;
+    private final K[] keys;
+    private final V[] values;
+    private int storageSize;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_STORAGE_SIZE];
+        values = (V[]) new Object[MAX_STORAGE_SIZE];
+        storageSize = 0;
+    }
+
+    private K getKeyByIndex(int index) {
+        return keys[index];
+    }
+
+    private void setKeyByIndex(int index, K key) {
+        keys[index] = key;
+    }
+
+    private V getValueByIndex(int index) {
+        return values[index];
+    }
+
+    private void setValueByIndex(int index, V value) {
+        values[index] = value;
+    }
+
     @Override
     public void put(K key, V value) {
+        int i;
+        for (i = 0; i < storageSize; i++) {
+            if (key == null && getKeyByIndex(i) == null
+                    || key != null && key.equals(getKeyByIndex(i))) {
+                setValueByIndex(i, value);
+                return;
+            }
+        }
+        setKeyByIndex(i, key);
+        setValueByIndex(i, value);
+        storageSize += 1;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int i;
+        for (i = 0; i < storageSize; i++) {
+            if (key == null && getKeyByIndex(i) == null
+                    || key != null && key.equals(getKeyByIndex(i))) {
+                break;
+            }
+        }
+        return getValueByIndex(i);
     }
 
     @Override
     public int size() {
-        return -1;
+        return storageSize;
     }
 }
