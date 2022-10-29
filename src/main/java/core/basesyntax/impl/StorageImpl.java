e5a7944e69eb1a14d091355234b4adfddb1a5687
Victoria Kharlanchuk <kaplabetty@gmail.com>
2022-10-29 23:47:23 +0200

    rewriting value correction

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -8,16 +8,19 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private K key;
     private V value;
     private StorageImpl[] mas = new StorageImpl[MAX_ARRAY];
-    private StorageImpl[] arrayOfElements;
     
     @Override
     public void put(K key, V value) {
         StorageImpl<K, V> storage = new StorageImpl<>();
         storage.key = key;
         storage.value = value;
+       
         if (!isIn(key)) {
             mas[count] = storage;
             count++;
+        }
+        if (isIn(key)) {
+            changeValue(key, value);
         }
     }
     
@@ -38,7 +41,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
                 }
             }
         }
-        System.out.println(value);
         return value;
     }
     
@@ -56,8 +58,29 @@ public class StorageImpl<K, V> implements Storage<K, V> {
                 if (key.equals(storage.key)) {
                     return true;
                 }
+            } else {
+                if (key == (storage.key)) {
+                    return true;
+                }
             }
         }
         return false;
+    }
+    
+    public void changeValue(K key, V value) {
+        for (int i = 0; i < mas.length; i++) {
+            if (mas[i] == null) {
+                continue;
+            }
+            if (key != null) {
+                if (key.equals(mas[i].key)) {
+                    mas[i].value = value;
+                }
+            } else {
+                if (key == (mas[i].key)) {
+                    mas[i].value = value;
+                }
+            }
+        }
     }
 }
