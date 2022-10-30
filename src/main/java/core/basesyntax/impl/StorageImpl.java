c0e28353791f2d246f0123adaa983186c5d5e94b
Denis Kalyakin <denis.kalyakin999@gmail.com>
2022-10-30 21:52:35 +0200

    in process

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,30 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final byte MAX_ITEMS_NUMBER = 10;
+    private K[] arrayKeys = (K[]) new Object[MAX_ITEMS_NUMBER];
+    private V[] arrayValues = (V[]) new Object[MAX_ITEMS_NUMBER];
+    private byte index = 0;
     @Override
     public void put(K key, V value) {
+        if (index == MAX_ITEMS_NUMBER) {index = 0;}
+        arrayKeys[index] = key;
+        arrayValues[index] = value;
+        index++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < arrayKeys.length; i++) {
+            if (arrayKeys[i] != null && arrayKeys[i].equals(key)) {
+                return arrayValues[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return index;
     }
 }
