d77fca6660d23ddf46e550be13c014ff2bcd5cbe
Denis Kalyakin <denis.kalyakin999@gmail.com>
2022-10-31 21:33:33 +0200

    stuck at this stage & need help

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,20 +4,45 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final byte MAX_ITEMS_NUMBER = 10;
-    private K[] arrayKeys = (K[]) new Object[MAX_ITEMS_NUMBER];
-    private V[] arrayValues = (V[]) new Object[MAX_ITEMS_NUMBER];
+    private K[] arrayKeys;
+    private V[] arrayValues;
     private byte index = 0;
+
+    public StorageImpl() {
+        arrayKeys = (K[]) new Object[MAX_ITEMS_NUMBER];
+        arrayValues = (V[]) new Object[MAX_ITEMS_NUMBER];
+    }
+
     @Override
     public void put(K key, V value) {
         if (index == MAX_ITEMS_NUMBER) {index = 0;}
-        arrayKeys[index] = key;
-        arrayValues[index] = value;
-        index++;
+        /**for (int i = 0; i < arrayKeys.length; i++) {
+            if (arrayKeys[i].equals(key)) {
+                arrayValues[i] = value;
+            }
+        } **/
+        if (key == null && arrayKeys[index] == null) {
+            arrayValues[index] = value;
+            index++;
+        }
+        if (key == null) {
+            arrayKeys[index] = null;
+            arrayValues[index] = value;
+            index++;
+        }
+        if (key != null) {
+            arrayKeys[index] = key;
+            arrayValues[index] = value;
+            index++;
+        }
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < arrayKeys.length; i++) {
+            if (key == null && arrayKeys[i] == null) {
+                return arrayValues[i];
+            }
             if (arrayKeys[i] != null && arrayKeys[i].equals(key)) {
                 return arrayValues[i];
             }
