e8475230705f03b3eaf0cd130a01d0036213e1bc
Yurii Trokhymenko <uta1965_@ukr.net>
2022-10-31 16:26:49 +0100

    edit StorageImpl

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,18 +2,71 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+@SuppressWarnings("unchecked")
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_NUMBER = 10;
+    private Object[] keys;
+    private Object[] values;
+    private K key;
+    private V value;
+
+    public int getIndex(K key) {
+        int volume = size();
+        int result = -1;
+        if (key == null) {
+            for (int i = 0; i < volume; i++) {
+                if (keys[i] == null) {
+                    result = i;
+                }
+            }
+        } else {
+            for (int i = 0; i < volume; i++) {
+                if (key.equals(keys[i])) {
+                    result = i;
+                }
+            }
+        }
+        return result;
+    }
+
     @Override
     public void put(K key, V value) {
+        int volume = size();
+        if (volume == 0) {
+            keys = new Object[MAX_NUMBER];
+            values = new Object[MAX_NUMBER];
+        }
+        int index = getIndex(key);
+        if (index < 0) {
+            if (volume <= MAX_NUMBER) {
+                keys[volume] = key;
+                values[volume] = value;
+            }
+        } else {
+            values[index] = value;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int index = getIndex(key);
+        if (index < 0) {
+            return null;
+        }
+        return (V) values[index];
     }
 
     @Override
     public int size() {
-        return -1;
+        if (values == null) {
+            return 0;
+        }
+        int result = 0;
+        for (int i = 0; i < MAX_NUMBER; i++) {
+            if (values[i] != null) {
+                result = result + 1;
+            }
+        }
+        return result;
     }
 }
