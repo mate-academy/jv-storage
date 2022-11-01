893b222b740c3a5296ee4675008645b7c93dc7e7
Yurii Trokhymenko <uta1965_@ukr.net>
2022-11-01 19:04:40 +0100

    added fixed after review 4

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,18 +2,50 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+@SuppressWarnings("unchecked")
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_NUMBER = 10;
+    private Object[] keys;
+    private Object[] values;
+    private int size = 0;
+
+    public StorageImpl() {
+        keys = new Object[MAX_NUMBER];
+        values = new Object[MAX_NUMBER];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (equalsKey(key, (K) keys[i])) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (equalsKey(key, (K) keys[i])) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    public boolean equalsKey(K firstKey, K secondKey) {
+        if ((firstKey == secondKey) || ((firstKey != null) && firstKey.equals(secondKey))) {
+            return true;
+        }
+        return false;
     }
 }
