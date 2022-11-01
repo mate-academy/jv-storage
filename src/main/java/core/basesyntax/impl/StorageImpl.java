d61a3163e1510c841482722c425cd531f0fcd39f
Yurii Trokhymenko <uta1965_@ukr.net>
2022-11-01 13:44:15 +0100

    added fixed after review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -7,66 +7,60 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_NUMBER = 10;
     private Object[] keys;
     private Object[] values;
-    private K key;
-    private V value;
+    private int size = 0;
 
-    public int getIndex(K key) {
-        int volume = size();
-        int result = -1;
+    public StorageImpl() {
+        keys = new Object[MAX_NUMBER];
+        values = new Object[MAX_NUMBER];
+    }
+
+    public boolean equalsKey(K key, Object o) {
         if (key == null) {
-            for (int i = 0; i < volume; i++) {
-                if (keys[i] == null) {
-                    result = i;
-                }
-            }
-        } else {
-            for (int i = 0; i < volume; i++) {
-                if (key.equals(keys[i])) {
-                    result = i;
-                }
-            }
+            return (o == null);
         }
-        return result;
+        if (o == null) {
+            return false;
+        }
+        return o.equals(key);
     }
 
     @Override
     public void put(K key, V value) {
-        int volume = size();
-        if (volume == 0) {
-            keys = new Object[MAX_NUMBER];
-            values = new Object[MAX_NUMBER];
-        }
-        int index = getIndex(key);
-        if (index < 0) {
-            if (volume <= MAX_NUMBER) {
-                keys[volume] = key;
-                values[volume] = value;
+        int ind = -1;
+        for (int i = 0; i < size; i++) {
+            if (equalsKey(key, keys[i])) {
+                ind = i;
+                break;
             }
-        } else {
-            values[index] = value;
+        }
+        if (size < MAX_NUMBER) {
+            if (ind < 0) {
+                keys[size] = key;
+                values[size] = value;
+                size = size + 1;
+            } else {
+                values[ind] = value;
+            }
         }
     }
 
     @Override
     public V get(K key) {
-        int index = getIndex(key);
-        if (index < 0) {
+        int ind = -1;
+        for (int i = 0; i < size; i++) {
+            if (equalsKey(key, keys[i])) {
+                ind = i;
+                break;
+            }
+        }
+        if (ind < 0) {
             return null;
         }
-        return (V) values[index];
+        return (V) values[ind];
     }
 
     @Override
     public int size() {
-        if (values == null) {
-            return 0;
-        }
-        int result = 0;
-        for (int i = 0; i < MAX_NUMBER; i++) {
-            if (values[i] != null) {
-                result = result + 1;
-            }
-        }
-        return result;
+        return size;
     }
 }
