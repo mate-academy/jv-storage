d543a1e448925e0cbded8aa44da772a639226ade
Victoria Kharlanchuk <kaplabetty@gmail.com>
2022-11-02 19:23:28 +0100

    task generic over again

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,84 +3,41 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    static final int MAX_ARRAY = 10;
-    private int count = 0;
-    private K key;
-    private V value;
-    private StorageImpl[] mas = new StorageImpl[MAX_ARRAY];
+    private static final int ARRAY_SIZE = 10;
+    private final K[] keys;
+    private final V[] values;
+    private int size;
+    
+    public StorageImpl() {
+        this.keys = (K[]) new Object[ARRAY_SIZE];
+        this.values = (V[]) new Object[ARRAY_SIZE];
+    }
     
     @Override
     public void put(K key, V value) {
-        StorageImpl<K, V> storage = new StorageImpl<>();
-        storage.key = key;
-        storage.value = value;
-       
-        if (!isIn(key)) {
-            mas[count] = storage;
-            count++;
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
+                values[i] = value;
+                size--;
+            }
         }
-        if (isIn(key)) {
-            changeValue(key, value);
-        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
     
     @Override
     public V get(K key) {
-        
-        for (int i = 0; i < mas.length; i++) {
-            if (mas[i] == null) {
-                continue;
-            }
-            if (key != null) {
-                if (key.equals(mas[i].key)) {
-                    value = (V) mas[i].value;
-                }
-            } else {
-                if (key == (mas[i].key)) {
-                    value = (V) mas[i].value;
-                }
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || (key != null && key.equals(keys[i]))) {
+                return values[i];
             }
         }
-        return value;
+        return null;
     }
     
     @Override
     public int size() {
-        return count;
-    }
-    
-    public boolean isIn(K key) {
-        for (StorageImpl storage : mas) {
-            if (storage == null) {
-                continue;
-            }
-            if (key != null) {
-                if (key.equals(storage.key)) {
-                    return true;
-                }
-            } else {
-                if (key == (storage.key)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    
-    public void changeValue(K key, V value) {
-        for (int i = 0; i < mas.length; i++) {
-            if (mas[i] == null) {
-                continue;
-            }
-            if (key != null) {
-                if (key.equals(mas[i].key)) {
-                    mas[i].value = value;
-                }
-            } else {
-                if (key == (mas[i].key)) {
-                    mas[i].value = value;
-                }
-            }
-        }
+        return size;
     }
 }
