129b306ca6e900dfe966d67bfc03d121a35edf49
Oksana Rudenko <0ksaha_rudenko@ukr.net>
2022-11-03 23:51:37 +0200

    jv-storage solution fixed put method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,31 +4,27 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int DEFAULT_CAPACITY = 10;
-    private static int size;
+    private int size;
     private K[] keys;
     private V[] values;
 
     public StorageImpl() {
         keys = (K[]) new Object[DEFAULT_CAPACITY];
         values = (V[]) new Object[DEFAULT_CAPACITY];
-        size = 0;
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i] == null && values[i] == null) {
-                keys[i] = key;
-                values[i] = value;
-                size++;
-                break;
-            }
+        for (int i = 0; i < size; i++) {
             if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
                 keys[i] = key;
                 values[i] = value;
-                break;
+                return;
             }
         }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
