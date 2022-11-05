20552145b5a6fb8f27259e0d5ea8836888cf7660
Sergei Matiko <adgodst@gmail.com>
2022-11-05 16:03:50 +0200

    fixed issues

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,36 +4,33 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int ARRAY_SIZE = 10;
-    private K[] keyArray;
-    private V[] valueArray;
+    private K[] key;
+    private V[] value;
     private int size;
 
     public StorageImpl() {
-        this.keyArray = (K[]) new Object[ARRAY_SIZE];
-        this.valueArray = (V[]) new Object[ARRAY_SIZE];
+        this.key = (K[]) new Object[ARRAY_SIZE];
+        this.value = (V[]) new Object[ARRAY_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (isEquals(keyArray[i], key)) {
-                valueArray[i] = value;
-                if (i == 0 && size == 0) {
-                    size++;
-                }
+            if (isEquals(this.key[i], key)) {
+                this.value[i] = value;
                 return;
             }
         }
-        keyArray[size] = key;
-        valueArray[size] = value;
+        this.key[size] = key;
+        this.value[size] = value;
         size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keyArray.length; i++) {
-            if (isEquals(keyArray[i], key)) {
-                return valueArray[i];
+        for (int i = 0; i < this.key.length; i++) {
+            if (isEquals(this.key[i], key)) {
+                return value[i];
             }
         }
         return null;
