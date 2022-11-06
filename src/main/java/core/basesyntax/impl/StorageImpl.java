c973ee67a94113d28f6b99d1268298c8d05f9eea
Denis Kalyakin <denis.kalyakin999@gmail.com>
2022-11-06 15:56:13 +0200

    added fixes after review again

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -7,7 +7,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private final K[] keys;
     private final V[] values;
     private int size;
-    private boolean nullMarker;
 
     public StorageImpl() {
         values = (V[]) new Object[MAX_ITEMS_NUMBER];
@@ -16,30 +15,15 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i] != null && keys[i].equals(key)) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
                 values[i] = value;
                 return;
             }
         }
-        if (key == null) {
-            if (nullMarker) {
-                for (int i = 0; i < keys.length; i++) {
-                    if (keys[i] == null && size > 0) {
-                        values[i] = value;
-                        return;
-                    }
-                }
-            }
-            values[size] = value;
-            nullMarker = true;
-            size++;
-        }
-        if (key != null) {
-            keys[size] = key;
-            values[size] = value;
-            size++;
-        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
