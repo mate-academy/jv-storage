6c27b2fa866d792700a27022429c7dc783d26e24
Vadym Valchak <vadym.valchak@gmail.com>
2022-11-08 19:58:29 +0200

    Add put and get functionality

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,43 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_KEYS_AND_VALUES_ARRAYS_SIZE = 10;
+    private K[] keys = (K[]) new Object[MAX_KEYS_AND_VALUES_ARRAYS_SIZE];
+    private V[] values = (V[]) new Object[MAX_KEYS_AND_VALUES_ARRAYS_SIZE];
+    private int cursor = 0;
+
     @Override
     public void put(K key, V value) {
+        int index = keyExist(key);
+        if (index != -1) {
+            values[index] = value;
+        } else {
+            keys[cursor] = key;
+            values[cursor] = value;
+            cursor++;
+        }
     }
 
     @Override
     public V get(K key) {
+        int index = keyExist(key);
+        if (index != -1) {
+            return values[index];
+        }
         return null;
     }
 
     @Override
     public int size() {
+        return cursor;
+    }
+
+    private int keyExist(K key) {
+        for (int i = 0; i < size(); i++) {
+            if (keys[i] == key || key != null && key.equals(keys[i])) {
+                return i;
+            }
+        }
         return -1;
     }
 }
