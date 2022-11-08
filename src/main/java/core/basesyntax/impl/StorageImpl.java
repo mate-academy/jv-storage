7758ac449f83a0be38141680924912e4bdc3be71
Anton Kalenskyi <anton_kalenskiy@ukr.net>
2022-11-08 11:30:39 +0200

    Added implementations of some methods: put(),get(),size(). Added one more method checkKey()

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,50 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private final K[] keys = (K[]) new Object[MAX_ITEMS_NUMBER];
+    private final V[] values = (V[]) new Object[MAX_ITEMS_NUMBER];
+    private int indexAddedObj = 0;
+
     @Override
     public void put(K key, V value) {
+        if (checkKey(key,value)) {
+            keys[indexAddedObj] = key;
+            values[indexAddedObj] = value;
+            indexAddedObj++;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] != null && keys[i].equals(key)
+                    || (keys[i] == null && key == null && values[i] != null)) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        int size = 0;
+        for (V value : values) {
+            if (value != null) {
+                size++;
+            }
+        }
+        return size;
+    }
+
+    private boolean checkKey(K key, V value) {
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] != null && keys[i].equals(key)
+                    || (keys[i] == null && key == null && values[i] != null)) {
+                values[i] = value;
+                return false;
+            }
+        }
+        return true;
     }
 }
