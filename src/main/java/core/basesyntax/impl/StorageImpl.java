ec940eb2fbb031256d53b6c366b62d73e85dbb62
Volodymyr Mykychak <leadervlo.d@gmail.com>
2022-11-09 22:01:19 +0200

    change variables & add constructor

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,12 +4,17 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int DEFAULT_CAPACITY = 10;
-    private K[] keys = (K[]) new Object[DEFAULT_CAPACITY];
-    private V[] values = (V[]) new Object[DEFAULT_CAPACITY];
-    private int size = 0;
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[DEFAULT_CAPACITY];
+        values = (V[]) new Object[DEFAULT_CAPACITY];
+    }
 
     @Override
-    public void put(K key, V value) { // key = null value = fjk
+    public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
             if (key == keys[i] || keys[i] != null && keys[i].equals(key)) {
                 values[i] = value;
