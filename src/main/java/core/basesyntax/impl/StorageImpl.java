7f84bbc37241c2e1d838e78578dbff0482e923ea
Vadym Valchak <vadym.valchak@gmail.com>
2022-11-09 17:33:40 +0200

    Update src/main/java/core/basesyntax/impl/StorageImpl.java
    Co-authored-by: Leonid Sivko <rommel@ukr.net>

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -11,7 +11,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public StorageImpl() {
         keys = (K[]) new Object[MAX_ITEMS_NUMBER];
         values = (V[]) new Object[MAX_ITEMS_NUMBER];
-        cursor = 0;
     }
 
     @Override
