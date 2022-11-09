b18ec508eaa30d267a08fb5317acc833744aafd2
Volodymyr Mykychak <leadervlo.d@gmail.com>
2022-11-09 19:34:05 +0200

    change mvn clean pkg

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,7 +3,7 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private final int DEFAULT_CAPACITY = 10;
+    private static final int DEFAULT_CAPACITY = 10;
     private K[] keys = (K[]) new Object[DEFAULT_CAPACITY];
     private V[] values = (V[]) new Object[DEFAULT_CAPACITY];
     private int size = 0;
