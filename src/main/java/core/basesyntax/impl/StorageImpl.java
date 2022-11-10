0b4e776ede1f473baaa8fd100dc10483cc4ddeaa
fantom <mironovanton5511@gmail.com>
2022-11-10 10:34:25 +0200

    fix according to comments

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -28,7 +28,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keys.length; i++) {
+        for (int i = 0; i < count; i++) {
             if ((keys[i] == null && key == null) || key != null && key.equals(keys[i])) {
                 return (V) values[i];
             }
