d50cab3e2a50b87818d768b675a6b5a7cca20534
Danylo Borysenko <borysenkodanyl@gmail.com>
2022-11-15 20:52:42 +0200

    method findExistingKeyIndex(K key) was changed and moved

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -24,15 +24,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             size += 1;
         }
     }
-    
-    private int findExistingKeyIndex(K key) {
-        for (int i = 0; i < size; i++) {
-            if (keys[i] != null && keys[i].equals(key) || keys[i] == key) {
-                return i;
-            }
-        }
-        return -1;
-    }
 
     @Override
     public V get(K key) {
@@ -46,5 +37,14 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return size;
+    }
+
+    private int findExistingKeyIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
