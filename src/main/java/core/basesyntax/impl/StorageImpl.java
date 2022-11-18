ab36d2b9ed12fe4a6404f5d989f6815f5ce11961
Alona Vyrodova <alenavirodova@gmail.com>
2022-11-18 16:34:05 +0200

    create  array

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,46 +1,62 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.ArrayList;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int DEFOULT_SIZE = 10;
+    private Pairs[] pairs;
+    private Pairs[] massiv;
+    private int index = 0;
 
-    private ArrayList<Pairs> pairs = new ArrayList<>();
+    public StorageImpl() {
+        pairs = new Pairs[DEFOULT_SIZE];
+        massiv = new Pairs[index];
+    }
 
     @Override
     public void put(K key, V value) {
         Pairs pair = findPair(key);
         if (pair == null) {
-            pair = new Pairs(key, value);
-            pairs.add(pair);
+            Pairs pairToPut = new Pairs(key, value);
+            pairs[index] = pairToPut;
+            massiv = createReternMassive();
+            index++;
         } else {
             pair.setValue(value);
         }
     }
 
-    private Pairs<K, V> findPair(K key) {
-        Pairs<K, V> unicPair = null;
-        for (int i = 0; i < pairs.size(); i++) {
-            Pairs<K, V> pair = pairs.get(i);
-            if (pair.getKey() != null && pair.getKey().equals(key)) {
+    private Pairs findPair(K key) {
+        Pairs unicPair = null;
+        for (int i = 0; i < pairs.length; i++) {
+            Pairs pair = pairs[i];
+            if (pairs[i] != null && pair.getKey() != null && pair.getKey().equals(key)) {
                 unicPair = pair;
             }
-            if ((pair.getKey() == null) && (key == null)) {
+            if ((pairs[i] != null && pair.getKey() == null) && (key == null)) {
                 unicPair = pair;
             }
         }
         return unicPair;
     }
 
+    public Pairs[] createReternMassive() {
+        Pairs[] pairsResive = new Pairs[index + 1];
+        for (int k = 0; k < pairsResive.length; k++) {
+            pairsResive[k] = pairs[k];
+        }
+        return pairsResive;
+    }
+
     @Override
     public V get(K key) {
         V value = null;
-        for (int i = 0; i < pairs.size(); i++) {
-            if (pairs.get(i).getKey() != null && pairs.get(i).getKey().equals(key)) {
-                value = (V) pairs.get(i).getValue();
+        for (int i = 0; i < massiv.length; i++) {
+            if (massiv[i].getKey() != null && massiv[i].getKey().equals(key)) {
+                value = (V) massiv[i].getValue();
             }
-            if (pairs.get(i).getKey() == null && key == null) {
-                value = (V) pairs.get(i).getValue();
+            if (massiv[i].getKey() == null && key == null) {
+                value = (V) massiv[i].getValue();
             }
         }
         return value;
@@ -48,6 +64,33 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return pairs.size();
+
+        return massiv.length;
+    }
+
+    private class Pairs<K, V> {
+        private K key;
+        private V value;
+
+        public Pairs(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public void setKey(K key) {
+            this.key = key;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
     }
 }
