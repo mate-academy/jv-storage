2f7e93007266f95c0e41a05967e7ba7aa7d8a440
Sander <alex.kapusniak@gmail.com>
2022-11-21 22:16:13 +0200

    first working version

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,18 +2,45 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ARRAY_LENGTH = 10;
+
+    private int size;
+    private K[] keys = (K[]) new Object[MAX_ARRAY_LENGTH];
+    private V[] values = (V[]) new Object[MAX_ARRAY_LENGTH];
+
     @Override
     public void put(K key, V value) {
+        int checkedIndex = checkIndex(key);
+        if (checkedIndex >= 0) {
+            values[checkedIndex] = value;
+        } else {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
+    }
+
+    private int checkIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public V get(K key) {
+        if (checkIndex(key) >= 0) {
+            return values[checkIndex(key)];
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
