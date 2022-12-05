6b033fc8ff2e06386dfdb97d3c5385c8993c30ba
Nazar Hnatyuk <stim1.one@gmail.com>
2022-12-05 16:55:53 +0200

    JV-Storage homework solution.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,44 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE = 10;
+    private int size;
+    private final K[] keyArray;
+    private final V[] valueArray;
+
+    public StorageImpl() {
+        this.keyArray = (K[]) new Object[MAX_SIZE];
+        this.valueArray = (V[]) new Object[MAX_SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(keyArray[i], key)) {
+                valueArray[i] = value;
+                return;
+            }
+        }
+        keyArray[size] = key;
+        valueArray[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(keyArray[i], key)) {
+                return valueArray[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
