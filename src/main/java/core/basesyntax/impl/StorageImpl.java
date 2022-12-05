7368f2e523fe6a4e2c10ce0bc314cc1136b0c8ea
Tymofii Skrypko <skrypko270901@gmail.com>
2022-12-05 23:45:56 +0100

    renamed fields, added required changes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,38 +3,29 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int MAX_SIZE = 10;
-    private final K[] key;
-    private final V[] value;
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private final K[] keys;
+    private final V[] values;
     private int size;
 
     public StorageImpl() {
-        this.key = (K[]) new Object[MAX_SIZE];
-        this.value = (V[]) new Object[MAX_SIZE];
-    }
-
-    private int getPosition(K key) {
-        for (int i = 0; i < size; i++) {
-            if ((this.key[i] == key) || (this.key[i] != null && this.key[i].equals(key))) {
-                return i;
-            }
-        }
-        return -1;
+        this.keys = (K[]) new Object[MAX_ITEMS_NUMBER];
+        this.values = (V[]) new Object[MAX_ITEMS_NUMBER];
     }
 
     @Override
     public void put(K key, V value) {
         int keyPosition = getPosition(key);
         if (keyPosition == -1) {
-            if (this.size < MAX_SIZE) {
-                this.key[size] = key;
-                this.value[size] = value;
+            if (this.size < MAX_ITEMS_NUMBER) {
+                keys[size] = key;
+                values[size] = value;
                 this.size++;
             } else {
                 System.out.println("Storage is full!");
             }
         } else {
-            this.value[keyPosition] = value;
+            values[keyPosition] = value;
         }
     }
 
@@ -44,11 +35,20 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         if (keyPosition == -1) {
             return null;
         }
-        return this.value[keyPosition];
+        return values[keyPosition];
     }
 
     @Override
     public int size() {
         return this.size;
+    }
+
+    private int getPosition(K key) {
+        for (int i = 0; i < size; i++) {
+            if ((keys[i] == key) || (keys[i] != null && keys[i].equals(key))) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
