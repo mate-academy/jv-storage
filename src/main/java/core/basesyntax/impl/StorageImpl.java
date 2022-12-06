017c1ac2332eb006e6f000f6975e79e0610dd808
Bogdan Pokhilko <bogdanp1701@gmail.com>
2022-12-06 12:37:59 +0200

    In StorageImpl Class created 2 arrays, constant int for storage length and arraysize.
    created the constructor for StorageImpl.
    implemented put() method, where with help of for loop I've managed to check if key == null and if there is already a key value in the keyArr.
    implemented get() method, with the for loop where and the same logic as put method.
    implemented size() method, with for loop to check if elements in the array are not equal to null and return the size of the array.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,46 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_STORAGE_LENGTH = 10;
+    private int arrSize = 0;
+    private K[] keyArr;
+    private V[] valueArr;
+
+    public StorageImpl() {
+        this.keyArr = (K[]) new Object[MAX_STORAGE_LENGTH];
+        this.valueArr = (V[]) new Object[MAX_STORAGE_LENGTH];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < arrSize; i++) {
+            if (key == keyArr[i] || key != null && key.equals(keyArr[i])) {
+                valueArr[i] = value;
+                return;
+            }
+        }
+        keyArr[arrSize] = key;
+        valueArr[arrSize] = value;
+        arrSize++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keyArr.length; i++) {
+            if (key == keyArr[i] || key != null && key.equals(keyArr[i])) {
+                return valueArr[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        for (K key : keyArr) {
+            if (key != null) {
+                return arrSize;
+            }
+        }
+        return arrSize;
     }
 }
