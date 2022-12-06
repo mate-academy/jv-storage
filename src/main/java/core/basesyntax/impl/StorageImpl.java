b656fc1e0fdaec95bcbae1cb2111a419e7d739d0
Dmytro M <dmytro.malinnikov@gmail.com>
2022-12-06 18:14:44 +0200

    create storage interface implementation

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,36 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ELEMENTS_NUMBER = 10;
+    private Object[] keys = new Object[MAX_ELEMENTS_NUMBER];
+    private Object[] values = new Object[MAX_ELEMENTS_NUMBER];
+    private int elementsCount = 0;
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < elementsCount; i++) {
+            if (key == null && keys[i] == null || key != null && key.equals(keys[i])) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[elementsCount] = key;
+        values[elementsCount] = value;
+        elementsCount++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < elementsCount; i++) {
+            if (key == null && keys[i] == null || key != null && key.equals(keys[i])) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return elementsCount;
     }
 }
