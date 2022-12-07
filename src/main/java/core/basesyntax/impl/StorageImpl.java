4502db72a4b1f21c146bfe239968769ec5b55ad7
Ivan Vovnyanko <moonkit.dior@gmail.com>
2022-12-07 16:01:49 +0300

    homework solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,43 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    Element[] elements;
+    int size = 0;
+    {
+        elements = new Element[10];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (size != 0) {
+            for (int i = 0; i < size; i++) {
+                if (elements[i].getKey() == key
+                        || elements[i].getKey() != null && elements[i].getKey().equals(key)) {
+                    elements[i].setValue(value);
+                    return;
+                }
+            }
+        }
+        Element element = new Element(key, value);
+        elements[size] = element;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        if (size != 0) {
+            for (int i = 0; i < size; i++) {
+                if (elements[i].getKey() == key
+                        || elements[i].getKey() != null && elements[i].getKey().equals(key)) {
+                    return (V) elements[i].getValue();
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
