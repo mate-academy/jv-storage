a4284c82d88fc23eff3afdcd33f6128cc7ad28eb
Ivan Vovnyanko <moonkit.dior@gmail.com>
2022-12-07 22:36:52 +0300

    fixed bugs (from mentor)

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,21 +4,19 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private Element[] elements;
-    private int size = 0;
+    private int size;
 
-    {
+    public StorageImpl(){
         elements = new Element[10];
     }
 
     @Override
     public void put(K key, V value) {
-        if (size != 0) {
-            for (int i = 0; i < size; i++) {
-                if (elements[i].getKey() == key
-                        || elements[i].getKey() != null && elements[i].getKey().equals(key)) {
-                    elements[i].setValue(value);
-                    return;
-                }
+        for (int i = 0; i < size; i++) {
+            if (elements[i].getKey() == key
+                    || elements[i].getKey() != null && elements[i].getKey().equals(key)) {
+                elements[i].setValue(value);
+                return;
             }
         }
         Element element = new Element(key, value);
@@ -28,12 +26,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        if (size != 0) {
-            for (int i = 0; i < size; i++) {
-                if (elements[i].getKey() == key
-                        || elements[i].getKey() != null && elements[i].getKey().equals(key)) {
-                    return (V) elements[i].getValue();
-                }
+        for (int i = 0; i < size; i++) {
+            if (elements[i].getKey() == key
+                    || elements[i].getKey() != null && elements[i].getKey().equals(key)) {
+                return (V) elements[i].getValue();
             }
         }
         return null;
@@ -43,4 +39,47 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public int size() {
         return size;
     }
+
+    public Element[] getElements() {
+        return elements;
+    }
+
+    public void setElements(Element[] elements) {
+        this.elements = elements;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
+
+class Element<K, V> {
+    private K key;
+    private V value;
+
+    public Element(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+}
+
