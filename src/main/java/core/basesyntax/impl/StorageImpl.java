882b587a6fd2d90c024c4bb0d8ba22dfe5b8ed40
Inna Diachkina <innadyachkina@gmail.com>
2022-12-13 09:08:31 +0200

    override mmethods

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,43 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ARRAY_NUMBER = 10;
+    private final K[] keys;
+    private final V[] values;
+    private int size;
+
+    public StorageImpl() {
+        this.keys = (K[]) new Object[MAX_ARRAY_NUMBER];
+        this.values = (V[]) new Object[MAX_ARRAY_NUMBER];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (key != null && key.equals(keys[i]) || keys[i] == key) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        V value = null;
+        for (int i = 0; i < size; i++) {
+            if (key != null && key.equals(keys[i]) || keys[i] == key) {
+                value = values[i];
+            }
+        }
+        return value;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
+
 }
