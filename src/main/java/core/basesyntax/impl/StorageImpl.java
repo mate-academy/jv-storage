19c6142885f1c22e484f32a11e0c5fd3c7b9cc28
user <graban2253@gmail.com>
2022-12-17 13:24:42 +0200

    Finished

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,51 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_STORAGE_NUM = 10;
+    private K[] keys;
+    private V[] values;
+    private int size = 0;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_STORAGE_NUM];
+        values = (V[]) new Object[MAX_STORAGE_NUM];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (checkKeys(keys[i], key)) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (checkKeys(keys[i], key)) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    public boolean checkKeys(Object key1, Object key2) {
+        if (key1 == null && key2 == null) {
+            return true;
+        }
+        if (key1 == null || key2 == null) {
+            return false;
+        }
+        return key1.equals(key2);
     }
 }
