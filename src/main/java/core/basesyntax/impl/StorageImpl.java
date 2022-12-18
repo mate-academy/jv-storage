cd6b0e9063cce11d922436c42ee0270d2c837d92
ydikalo <dikalotel@gmail.com>
2022-12-18 23:10:11 +0200

    added annotation @SuppressWarnings("unchecked")

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,6 +3,8 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 import java.util.Arrays;
 
+
+@SuppressWarnings("unchecked")
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int CAPACITY = 10;
     private int size = 0;
