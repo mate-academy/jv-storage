a5134996a47e310ab32d8bdd73440dd6341fc823
Oleksii Bilko <lucheus@gmail.com>
2022-12-18 15:27:52 +0200

    Implemented the Storage with generic

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,43 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE = 10;
+    private final K[] keys = (K[]) new Object[MAX_SIZE];
+    private final V[] values = (V[]) new Object[MAX_SIZE];
+    private int size = 0;
+
+    private boolean objectsEquals(K o1, K o2) {
+        return o1 == o2 || (o1 != null && o1.equals(o2));
+    }
+
     @Override
     public void put(K key, V value) {
+        boolean newKey = true;
+        for (int i = 0; i < size; i++) {
+            if (objectsEquals(key, keys[i])) {
+                values[i] = value;
+                newKey = false;
+            }
+        }
+        if (newKey) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (objectsEquals(key, keys[i])) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
