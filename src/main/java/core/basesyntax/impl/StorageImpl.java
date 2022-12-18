2c2429f254d5f524a70f70351c83136b41c75dbf
Mykola Radzivon <kolian201497@gmail.com>
2022-12-18 15:58:03 +0200

    solved task, implements storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,47 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_LENGTH = 10;
+    private final Object[] arrayOfKey = new Object[MAX_LENGTH];
+    private final Object[] arrayOfValue = new Object[MAX_LENGTH];
+    private int size = 0;
+
     @Override
     public void put(K key, V value) {
+        arrayOfKey[size] = key;
+        arrayOfValue[size] = value;
+        size++;
+
     }
 
     @Override
     public V get(K key) {
+        int count = 0;
+        int k = -1;
+        for (int i = 0; i < size; i++) {
+            if (arrayOfKey[i] == null && key == null) {
+                k = i;
+                count++;
+            }
+            if (arrayOfKey[i] == null) {
+                continue;
+            }
+            if (arrayOfKey[i].equals(key)) {
+                k = i;
+                count++;
+            }
+        }
+        if (count >= 2) {
+            size--;
+        }
+        if (k >= 0) {
+            return (V) arrayOfValue[k];
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
