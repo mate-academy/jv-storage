90276a98e8ba6a89b0105a81767877b02ffa3747
Solomiia Voloshyn <solomiiavoloshyn@gmail.com>
2022-12-19 15:32:17 +0200

    changed the name of constant

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,14 +3,14 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int MAX_NUMBER = 10;
+    private static final int MAX_CAPACITY = 10;
     private int size;
     private final K[] keys;
     private final V[] values;
 
     public StorageImpl() {
-        this.keys = (K[]) new Object[MAX_NUMBER];
-        this.values = (V[]) new Object[MAX_NUMBER];
+        this.keys = (K[]) new Object[MAX_CAPACITY];
+        this.values = (V[]) new Object[MAX_CAPACITY];
     }
 
     @Override
