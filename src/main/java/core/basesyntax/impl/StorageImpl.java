bc79b97e18957d532f7b2cd7be7480ede5eb6824
Bohdan Halitskyi <b.a.galitskiy@gmail.com>
2022-12-19 18:49:26 +0200

    Implemented methods for building pairs of key-values

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,48 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_LENGTH = 10;
+    private K[] keyStorage;
+    private V[] valueStorage;
+    private int size;
+
+    public StorageImpl() {
+        this.keyStorage = (K[]) new Object[MAX_LENGTH];
+        this.valueStorage = (V[]) new Object[MAX_LENGTH];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (getKeyIndex(key) != null) {
+            valueStorage[getKeyIndex(key)] = value;
+            return;
+        }
+        keyStorage[size] = key;
+        valueStorage[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        if (getKeyIndex(key) != null) {
+            return valueStorage[getKeyIndex(key)];
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
+
+    private Integer getKeyIndex(K key) {
+        Integer index = null;
+        for (int i = 0; i < size; i++) {
+            if (key == keyStorage[i] || key != null && key.equals(keyStorage[i])) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
 }
