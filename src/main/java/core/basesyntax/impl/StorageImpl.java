9cfcdd23f803afa37a5047265d80625734cd961e
Dmitriy Krasnopir <dmitriy.krasnopir@gmail.com>
2022-12-19 23:00:39 +0200

    third commit

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,20 +3,20 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int MAX_STORAGE_COUNT = 10;
+    private static final int MAX_ELEMENTS_NUMBER = 10;
     private final K[] keys;
     private final V[] values;
     private int size;
 
     public StorageImpl() {
-        keys = (K[]) new Object[MAX_STORAGE_COUNT];
-        values = (V[]) new Object[MAX_STORAGE_COUNT];
+        keys = (K[]) new Object[MAX_ELEMENTS_NUMBER];
+        values = (V[]) new Object[MAX_ELEMENTS_NUMBER];
     }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] == key || key != null && key.equals(keys[i])) {
+            if (areKeysEqual(keys[i], key)) {
                 values[i] = value;
                 return;
             }
@@ -29,7 +29,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] == key || key != null && key.equals(keys[i])) {
+            if (areKeysEqual(keys[i], key)) {
                 return values[i];
             }
         }
@@ -39,5 +39,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return size;
+    }
+
+    private boolean areKeysEqual(Object firstKey, Object secondKey) {
+        return firstKey == secondKey || secondKey != null && secondKey.equals(firstKey);
     }
 }
