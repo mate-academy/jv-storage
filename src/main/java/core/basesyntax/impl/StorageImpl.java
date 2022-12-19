bec12e9dce15a3673b24f417eab8750e47e6b7d0
ydikalo <dikalotel@gmail.com>
2022-12-19 16:09:18 +0200

    renamed constants and non-static fields, reworked put and get methods

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,36 +1,38 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Arrays;
 
 @SuppressWarnings("unchecked")
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int CAPACITY = 10;
+    private static final int MAX_CAPACITY = 10;
     private int size;
-    private Object[] keySet;
-    private Object[] valueSet;
+    private Object[] keys;
+    private Object[] values;
 
     public StorageImpl() {
-        keySet = new Object[CAPACITY];
-        valueSet = new Object[CAPACITY];
+        keys = new Object[MAX_CAPACITY];
+        values = new Object[MAX_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
-        if (Arrays.asList(keySet).contains(key)
-                && Arrays.asList(keySet).indexOf(key) <= size - 1) {
-            valueSet[Arrays.asList(keySet).indexOf(key)] = value;
-        } else {
-            keySet[size] = key;
-            valueSet[size] = value;
-            size++;
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
+                values[i] = value;
+                return;
+            }
         }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        if (Arrays.asList(keySet).contains(key)) {
-            return (V) valueSet[Arrays.asList(keySet).indexOf(key)];
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
+                return (V) values[i];
+            }
         }
         return null;
     }
