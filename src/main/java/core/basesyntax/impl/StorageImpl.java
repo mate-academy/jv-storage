3334f6ead7333c6423163053c6ad1c558b0d89c4
Mykola Radzivon <kolian201497@gmail.com>
2022-12-19 21:00:02 +0200

    change my solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -10,34 +10,21 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        arrayOfKey[size] = key;
-        arrayOfValue[size] = value;
-        size++;
-
+        if (!checkedEquals(key, value)) {
+            arrayOfKey[size] = key;
+            arrayOfValue[size] = value;
+            size++;
+        } else {
+            checkedEquals(key, value);
+        }
     }
 
     @Override
     public V get(K key) {
-        int count = 0;
-        int k = -1;
         for (int i = 0; i < size; i++) {
-            if (arrayOfKey[i] == null && key == null) {
-                k = i;
-                count++;
+            if (arrayOfKey[i] == key || (arrayOfKey[i] != null && arrayOfKey[i].equals(key))) {
+                return (V) arrayOfValue[i];
             }
-            if (arrayOfKey[i] == null) {
-                continue;
-            }
-            if (arrayOfKey[i].equals(key)) {
-                k = i;
-                count++;
-            }
-        }
-        if (count >= 2) {
-            size--;
-        }
-        if (k >= 0) {
-            return (V) arrayOfValue[k];
         }
         return null;
     }
@@ -45,5 +32,27 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return size;
+    }
+
+    private boolean checkedEquals(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (arrayOfKey[i] == null && key == null) {
+                change(i, key, value);
+                return true;
+            }
+            if (arrayOfKey[i] == null) {
+                continue;
+            }
+            if (arrayOfKey[i].equals(key)) {
+                change(i, key, value);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void change(int i, K key, V value) {
+        arrayOfKey[i] = key;
+        arrayOfValue[i] = value;
     }
 }
