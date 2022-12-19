ce2695595e2de141a77e709578ad15ea67116bb6
Oleksii Lytvynenko <oleksiila@gmail.com>
2022-12-19 19:04:14 +0000

    Initial commit: Homework jv-storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,42 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ARRAY_SIZE = 10;
+    private K[] keyArray = (K[]) new Object[MAX_ARRAY_SIZE];
+    private V[] valueArray = (V[]) new Object[MAX_ARRAY_SIZE];
+    private int size = 0;
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (key == keyArray[i] || key != null && key.equals(keyArray[i])) {
+                valueArray[i] = value;
+                return;
+            }
+        }
+        keyArray[size] = key;
+        valueArray[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keyArray[i] == key || keyArray[i] != null && keyArray[i].equals(key)) {
+                return valueArray[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        int size = 0;
+        for (V element : valueArray) {
+            if (element != null) {
+                size++;
+            }
+        }
+        return size;
     }
 }
