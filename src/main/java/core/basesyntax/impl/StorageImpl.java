5adbaf96ac5e92ff22af0b714ebe51c9b15d278f
Gleb Shevchenko <glieb.shevchenko@gmail.com>
2022-12-20 17:01:44 +0200

    Methods are created and work

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -10,22 +10,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private final K[] keys;
     private final V[] values;
 
-    public K getKey() {
-        return key;
-    }
-
-    public void setKey(K key) {
-        this.key = key;
-    }
-
-    public V getValue() {
-        return value;
-    }
-
-    public void setValue(V value) {
-        this.value = value;
-    }
-
     public StorageImpl() {
         keys = (K[]) new Object[MAX_CAPACITY];
         values = (V[]) new Object[MAX_CAPACITY];
@@ -33,22 +17,21 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        keys[size + 1] = getKey();
-        values[size + 1] = getValue();
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        if (size == 0) {
-            keys[0] = key;
-            values[0] = value;
-        }
-        for (int i = 0; i < size; i++) {
-            if (!key.equals(keys[i])) {
-                put(key, value);
-                return values[size];
-            } else {
-                values[i] = value;
+        for (int i = 0; i < keys.length; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
                 return values[i];
             }
         }
@@ -57,12 +40,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        int count = 0;
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i] != null) {
-                count++;
-            }
-        }
-        return count;
+        return size;
     }
 }
