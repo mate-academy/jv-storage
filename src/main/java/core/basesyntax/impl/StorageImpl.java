a3b3395f39dfd4cb88356fca4ef587bdbf2a9fce
Gleb Shevchenko <glieb.shevchenko@gmail.com>
2022-12-20 16:41:50 +0200

    Does not work

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,66 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_CAPACITY = 10;
+    private K key;
+    private V value;
+    private int size = size();
+    private final K[] keys;
+    private final V[] values;
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_CAPACITY];
+        values = (V[]) new Object[MAX_CAPACITY];
+    }
+
     @Override
     public void put(K key, V value) {
+        keys[size + 1] = getKey();
+        values[size + 1] = getValue();
     }
 
     @Override
     public V get(K key) {
+        if (size == 0) {
+            keys[0] = key;
+            values[0] = value;
+        }
+        for (int i = 0; i < size; i++) {
+            if (!key.equals(keys[i])) {
+                put(key, value);
+                return values[size];
+            } else {
+                values[i] = value;
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        int count = 0;
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] != null) {
+                count++;
+            }
+        }
+        return count;
     }
 }
