41e86854e8a41d45bda48c9b3366745c7dd1da77
Maksim Surzhik <kabaneri15@gmail.com>
2022-12-20 11:39:43 +0200

    tests completed, StorageImpl was changed

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,65 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int LIMIT_OF_ARRAY = 10;
+    private static final int VALUE_OF_NULL = -1;
+    private static final int KEY_INDEX = 0;
+    private static final int VALUE_INDEX = 1;
+    private final Object[][] obj = new Object[LIMIT_OF_ARRAY][2];
+    private int size = 0;
+
+    public StorageImpl() {
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < obj.length; i++) {
+            if (obj[i][KEY_INDEX] != null) {
+                if (obj[i][KEY_INDEX].equals(VALUE_OF_NULL) && key == null) {
+                    obj[i][VALUE_INDEX] = value;
+                    size--;
+                    break;
+                }
+                if (obj[i][KEY_INDEX].equals(key)) {
+                    obj[i][VALUE_INDEX] = value;
+                    size--;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < obj.length; i++) {
+            if (key == null && obj[i][KEY_INDEX] == null && obj[i][VALUE_INDEX] == null) {
+                obj[i][KEY_INDEX] = VALUE_OF_NULL;
+                obj[i][VALUE_INDEX] = value;
+                size++;
+                break;
+            }
+            if (obj[i][KEY_INDEX] == null && obj[i][VALUE_INDEX] == null) {
+                obj[i][KEY_INDEX] = key;
+                obj[i][VALUE_INDEX] = value;
+                size++;
+                break;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (Object[] objects : obj) {
+            if (objects[KEY_INDEX] != null) {
+                if (objects[KEY_INDEX].equals(VALUE_OF_NULL) && key == null) {
+                    return (V) objects[VALUE_INDEX];
+                }
+                if (objects[KEY_INDEX].equals(key)) {
+                    return (V) objects[VALUE_INDEX];
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
