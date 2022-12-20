b29c9c880d1c48241637b68bb1cd5c1a7e2a9f7f
mykoladymchenko <mykola.dymchenko@insightsoftware.com>
2022-12-20 12:56:05 +0200

    added solution to the class StorageImpl: - added implementation to the exists methods: put(), get(), size() - added two another methods: addToArrays(), is exists(); the first one adds data to the arrays and another one checks about existing values in the array

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,58 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int START_INDEX = 0;
+    private static final int SIZE = 5;
+    private Object[] keys = new Object[SIZE];
+    private Object[] values = new Object[SIZE];
+    private int arrayIndex = 0;
     @Override
     public void put(K key, V value) {
+        if (arrayIndex == START_INDEX) {
+            addToArrays(key, value, arrayIndex);
+            arrayIndex++;
+        } else if (arrayIndex < SIZE) {
+            if (!isExist(key, value)) {
+                addToArrays(key, value, arrayIndex);
+                arrayIndex++;
+            }
+        } else {
+            throw new RuntimeException("You can't add data because storage is full!");
+        }
+    }
+
+    private void addToArrays(K key, V value, int index) {
+        this.keys[index] = key;
+        this.values[index] = value;
+    }
+
+    private boolean isExist(K key, V value) {
+        for (int i = 0; i < arrayIndex; i++) {
+            if (keys[i] == key) {
+                addToArrays(key, value, i);
+                return true;
+            } else if (keys[i] != null && keys[i].equals(key)) {
+                addToArrays(key, value, i);
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < arrayIndex; i++) {
+            if (keys[i] == key) {
+                return (V) values[i];
+            } else if (keys[i] != null && keys[i].equals(key)) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return arrayIndex;
     }
 }
