68b109355d80868e7108e70c8bc71d5dbf211fc8
Your NameMykola <mykola.osolinskyi@gmail.com>
2022-12-20 17:38:50 +0100

    changes after first students review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,15 +3,15 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int MAXIMUM_NUMBER_OF_ELEMENTS_IN_STORAGE = 10;
+    private static final int MAX_CAPACITY = 10;
     private static final int INDEX_UNEXIST_KEY = -1;
     private int size;
     private final K[] keys;
     private final V[] values;
 
     public StorageImpl() {
-        keys = (K[]) new Object[MAXIMUM_NUMBER_OF_ELEMENTS_IN_STORAGE];
-        values = (V[]) new Object[MAXIMUM_NUMBER_OF_ELEMENTS_IN_STORAGE];
+        keys = (K[]) new Object[MAX_CAPACITY];
+        values = (V[]) new Object[MAX_CAPACITY];
     }
 
     @Override
@@ -29,7 +29,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         int index = getKeyIndex(key);
-        return index != INDEX_UNEXIST_KEY ? values[index] : null;
+        return index == INDEX_UNEXIST_KEY ? values[index];
     }
 
     @Override
