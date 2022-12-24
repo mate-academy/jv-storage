ac0e5a6f09128d7f0fbc6941d1c04d7ca8e29d0f
Igor Puhach <igorpuhach21@gmail.com>
2022-12-24 11:49:00 +0200

    added two arrays, implement methods put() and get()

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,36 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_NUMBER = 10;
+    private K[] arrayKeys = (K[]) new Object [MAX_NUMBER];
+    private V [] arrayValues = (V[]) new Object [MAX_NUMBER];
+    private int size = 0;
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (key == arrayKeys[i] || key != null && key.equals(arrayKeys[i])) {
+                arrayValues[i] = value;
+                return;
+            }
+        }
+        arrayKeys[size] = key;
+        arrayValues[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < arrayKeys.length; i++) {
+            if (arrayKeys[i] == key || arrayKeys[i] != null && arrayKeys[i].equals(key)) {
+                return arrayValues[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
