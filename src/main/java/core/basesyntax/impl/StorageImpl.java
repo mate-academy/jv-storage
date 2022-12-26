b41f0b71bdd67ab6d7d565e35907e501bda5bfc6
Artem Otsabera <eskvair86@gmail.com>
2022-12-26 23:38:41 +0200

    implements three methods

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,49 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE_STORAGE = 10;
+    private final K[] keyArray;
+    private final V[] valueArray;
+    private int size;
+
+    public StorageImpl() {
+        keyArray = (K[]) new Object[MAX_SIZE_STORAGE];
+        valueArray = (V[]) new Object[MAX_SIZE_STORAGE];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i <= size; i++) {
+            if ((key == null && keyArray[i] == null && valueArray[i] != null)
+                   || (key != null && key.equals(keyArray[i]))) {
+                keyArray[i] = key;
+                valueArray[i] = value;
+                return;
+            }
+        }
+        keyArray[size] = key;
+        valueArray[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if ((key == keyArray[i]) || (key != null && key.equals(keyArray[i]))) {
+                return valueArray[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        int count = 0;
+        for (V elements : valueArray) {
+            if (elements != null) {
+                count++;
+            }
+        }
+        return count;
     }
 }
