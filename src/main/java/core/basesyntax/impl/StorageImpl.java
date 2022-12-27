9452d689c1d0cf9ad2608ba62f6bbe982cb28843
Nazarii Kharyshyn <nazariykharyshyn@gmail.com>
2022-12-27 14:09:11 +0200

    Created two arrays instead of one 2d array, initialized them in constructor

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,36 +2,38 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.Objects;
+
 @SuppressWarnings("unchecked")
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ELEMENT_NUMBER = 10;
-    private static final int ELEMENT_OF_PAIR_NUMBER = 2;
-    private static final int NUMBER_OF_KEY = 0;
-    private static final int NUMBER_OF_VALUE = 1;
-    private final Object[][] storages = new Object[MAX_ELEMENT_NUMBER][ELEMENT_OF_PAIR_NUMBER];
+    private final K[] keyStorages;
+    private final V[] valueStorages;
     private int counter = 0;
+
+    public StorageImpl() {
+        keyStorages = (K[]) new Object[MAX_ELEMENT_NUMBER];
+        valueStorages = (V[]) new Object[MAX_ELEMENT_NUMBER];
+    }
 
     @Override
     public void put(K key, V value) {
-        int replacedElements = 0;
         for (int i = 0; i < counter; i++) {
             if (keyAlreadyExist(key, i)) {
-                storages[i][NUMBER_OF_VALUE] = value;
-                replacedElements++;
+                valueStorages[i] = value;
+                return;
             }
         }
-        if (replacedElements == 0) {
-            storages[counter][NUMBER_OF_KEY] = key;
-            storages[counter][NUMBER_OF_VALUE] = value;
-            counter++;
-        }
+        keyStorages[counter] = key;
+        valueStorages[counter] = value;
+        counter++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < counter; i++) {
             if (keyAlreadyExist(key, i)) {
-                return (V) storages[i][NUMBER_OF_VALUE];
+                return valueStorages[i];
             }
         }
         return null;
@@ -43,8 +45,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     }
 
     private boolean keyAlreadyExist(K key, int i) {
-        return storages[i][NUMBER_OF_KEY] == key
-                || storages[i][NUMBER_OF_KEY] != null
-                && storages[i][NUMBER_OF_KEY].equals(key);
+        return Objects.equals(keyStorages[i], key);
     }
 }
