744281ff04109eb834f043b4de5194f1c9f81216
Serhii Kopervas <sergkopervas@gmail.com>
2023-01-02 21:13:46 +0100

    Final solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,78 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_CAPACITY = 10;
+    private int size;
+    private K[] keys;
+    private V[] values;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_CAPACITY];
+        values = (V[]) new Object[MAX_CAPACITY];
+        size = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        int i = 0;
+        boolean success = false;
+        while (success == false) {
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
+                values[i] = value;
+                success = true;
+                size++;
+            } else if (keys[i] == null && values[i] == null) {
+                keys[i] = key;
+                values[i] = value;
+                success = true;
+                size++;
+            }
+            i++;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        int counter = 0;
+        for (int i = 0; i < MAX_CAPACITY; i++) {
+            if (keys[i] != null || values[i] != null) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public K[] getKeys() {
+        return keys;
+    }
+
+    public void setKeys(K[] keys) {
+        this.keys = keys;
+    }
+
+    public V[] getValues() {
+        return values;
+    }
+
+    public void setValues(V[] values) {
+        this.values = values;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
     }
 }
