2a69e90c4260d05033650909cec2c516c4ea412f
Oleh B <oleg.busygin@gmail.com>
2023-01-09 18:58:58 +0200

    Homework jv-storage done

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,24 +3,51 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private final int maxSize = 10;
-    private Pair[] pairArr;
+    private static final int MAX_SIZE = 10;
+    private Pair<K, V>[] pairArray;
     private int actualSize;
 
     public StorageImpl() {
-        this.pairArr = new Pair[maxSize];
+        this.pairArray = new Pair[MAX_SIZE];
+    }
+
+    public class Pair<K, V> {
+        private K key;
+        private V value;
+
+        public Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Pair<?, ?> pair = (Pair<?, ?>) o;
+
+            if (key != null ? !key.equals(pair.key) : pair.key != null) return false;
+            return value != null ? value.equals(pair.value) : pair.value == null;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = key != null ? key.hashCode() : 0;
+            result = 31 * result + (value != null ? value.hashCode() : 0);
+            return result;
+        }
     }
 
     @Override
     public void put(K key, V value) {
-
-        for (int i = 0; i < pairArr.length; i++) {
-            if (pairArr[i] != null && ((pairArr[i].getKey() == key)
-                    || (pairArr[i].getKey() != null && pairArr[i].getKey().equals(key)))) {
-                pairArr[i] = new Pair<>(key, value);
+        for (int i = 0; i <= actualSize ; i++) {
+            if (pairArray[i] != null && ((pairArray[i].key == key)
+                    || (pairArray[i].key != null && pairArray[i].key.equals(key)))) {
+                pairArray[i] = new Pair<>(key, value);
                 return;
-            } else if (pairArr[i] == null) {
-                pairArr[i] = new Pair<>(key, value);
+            } else if (pairArray[i] == null) {
+                pairArray[i] = new Pair<>(key, value);
                 actualSize++;
                 return;
             }
@@ -30,9 +57,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < actualSize; i++) {
-            if (pairArr[i] != null && (pairArr[i].getKey() == null && key == null)
-                    || (pairArr[i].getKey() != null && pairArr[i].getKey().equals(key))) {
-                return (V) this.pairArr[i].getValue();
+            if (pairArray[i] != null && (pairArray[i].key == key)
+                    || (pairArray[i].key != null && pairArray[i].key.equals(key))) {
+                return (V) this.pairArray[i].value;
             }
         }
         return null;
