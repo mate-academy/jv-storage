c2eab2418b924b1e3004ea882317b237cd9b492a
Mik911 <911Sokol@gmail.com>
2023-01-11 23:04:09 +0200

    mik-jv-storage solution sent for review ch1, 11.01.23  22-59

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,55 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private final int maxNumber = 10;
+    private int realSize = -1;
+    private K[] keys = (K[]) new Object[maxNumber];
+    private V[] values = (V[]) new Object[maxNumber];
+
     @Override
     public void put(K key, V value) {
+        int foundKey = -1;
+        for (int i = 0; i <= realSize; i++) {
+            if (key == null && keys[i] == null) {
+                foundKey = i;
+                break;
+            }
+            if (keys[i] != null && keys[i].equals(key)) {
+                foundKey = i;
+                break;
+            }
+        }
+        if (foundKey == -1) {
+            realSize++;
+            keys[realSize] = key;
+            values[realSize] = value;
+        } else {
+            values[foundKey] = value;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int foundKey = -1;
+        for (int i = 0; i <= realSize; i++) {
+            if (key == null && keys[i] == null) {
+                foundKey = i;
+                break;
+            }
+            if (keys[i] != null && keys[i].equals(key)) {
+                foundKey = i;
+                break;
+            }
+        }
+        if (foundKey == -1) {
+            return null;
+        } else {
+            return values[foundKey];
+        }
     }
 
     @Override
     public int size() {
-        return -1;
+        return realSize + 1;
     }
 }
