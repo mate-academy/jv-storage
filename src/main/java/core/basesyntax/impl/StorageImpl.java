076815d7e0f075ff420ece763f5f1dd1220f9750
Mik911 <911Sokol@gmail.com>
2023-01-12 21:03:47 +0200

    12.01.23 -mik-jv-storage solution sent for review ch2, 11.01.23  22-59

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,46 +3,37 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private final int maxNumber = 10;
-    private int realSize = -1;
-    private K[] keys = (K[]) new Object[maxNumber];
-    private V[] values = (V[]) new Object[maxNumber];
+    private final int DEFAULT_CAPACITY = 10;
+    private int realSize = 0;
+    private K[] keys = (K[]) new Object[DEFAULT_CAPACITY];
+    private V[] values = (V[]) new Object[DEFAULT_CAPACITY];
 
     @Override
     public void put(K key, V value) {
-        int foundKey = -1;
-        for (int i = 0; i <= realSize; i++) {
-            if (key == null && keys[i] == null) {
-                foundKey = i;
-                break;
-            }
-            if (keys[i] != null && keys[i].equals(key)) {
-                foundKey = i;
-                break;
-            }
-        }
+        int foundKey = getIndex(key);
         if (foundKey == -1) {
-            realSize++;
             keys[realSize] = key;
             values[realSize] = value;
+            realSize++;
         } else {
             values[foundKey] = value;
         }
     }
 
-    @Override
-    public V get(K key) {
+    private int getIndex(K key) {
         int foundKey = -1;
-        for (int i = 0; i <= realSize; i++) {
-            if (key == null && keys[i] == null) {
-                foundKey = i;
-                break;
-            }
-            if (keys[i] != null && keys[i].equals(key)) {
+        for (int i = 0; i < realSize; i++) {
+            if ((key == null && keys[i] == null) || (keys[i] != null && keys[i].equals(key))) {
                 foundKey = i;
                 break;
             }
         }
+        return foundKey;
+    }
+
+    @Override
+    public V get(K key) {
+        int foundKey = getIndex(key);
         if (foundKey == -1) {
             return null;
         } else {
@@ -52,6 +43,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return realSize + 1;
+        return realSize;
     }
 }
