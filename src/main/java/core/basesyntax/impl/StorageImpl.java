807e242a6b202512af2ae61cbcd628c5f3d890dd
Mik911 <911Sokol@gmail.com>
2023-01-12 22:25:20 +0200

    ch.5 - 12.01.23 -mik-jv-storage solution sent for review ch3, 11.01.23  22-59

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,17 +4,23 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int DEFAULT_CAPACITY = 10;
-    private int realSize = 0;
-    private K[] keys = (K[]) new Object[DEFAULT_CAPACITY];
-    private V[] values = (V[]) new Object[DEFAULT_CAPACITY];
+    private int size = 0;
+    private K[] keys;
+    private V[] values;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[DEFAULT_CAPACITY];
+
+        values = (V[]) new Object[DEFAULT_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
         int foundKey = getIndex(key);
         if (foundKey == -1) {
-            keys[realSize] = key;
-            values[realSize] = value;
-            realSize++;
+            keys[size] = key;
+            values[size] = value;
+            size++;
         } else {
             values[foundKey] = value;
         }
@@ -22,7 +28,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     private int getIndex(K key) {
         int foundKey = -1;
-        for (int i = 0; i < realSize; i++) {
+        for (int i = 0; i < size; i++) {
             if ((key == null && keys[i] == null) || (keys[i] != null && keys[i].equals(key))) {
                 foundKey = i;
                 break;
@@ -43,6 +49,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return realSize;
+        return size;
     }
 }
