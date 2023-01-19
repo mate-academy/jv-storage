91de9385d7f9b5aa5f25a0cf98700dd33ebc3a3d
Kostia Vershynin <vershynin.kostia@gmail.com>
2023-01-19 22:27:48 +0200

    realisation class StorageImpl

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,47 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private int size = 0;
+    private static final int MAX_SIZE = 10;
+    private K[] keys = (K[]) new Object[MAX_SIZE];
+    private V[] values = (V[]) new Object[MAX_SIZE];
+
+    public StorageImpl() {
+    }
+
     @Override
     public void put(K key, V value) {
+        if (size == 0) {
+            values[0] = value;
+            keys[0] = key;
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (Objects.equals(key, keys[i]) || key == keys[i]) {
+                    values[i] = value;
+                    return;
+                }
+                keys[size] = key;
+                values[size] = value;
+            }
+        }
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if (Objects.equals(key, keys[i])) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
