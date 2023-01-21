5cb9fc2da4a857d25c3e721752346262cd39df43
Kostia Vershynin <vershynin.kostia@gmail.com>
2023-01-21 17:52:07 +0200

    fixed realisation class StorageImpl

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,39 +1,35 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
-    private int size = 0;
-    private K[] keys = (K[]) new Object[MAX_SIZE];
-    private V[] values = (V[]) new Object[MAX_SIZE];
+    private int size;
+    private final K[] keys;
+    private final V[] values;
 
     public StorageImpl() {
+        this.keys = (K[]) new Object[MAX_SIZE];
+        this.values = (V[]) new Object[MAX_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
-        if (size == 0) {
-            values[0] = value;
-            keys[0] = key;
-        } else {
-            for (int i = 0; i < size; i++) {
-                if (Objects.equals(key, keys[i]) || key == keys[i]) {
-                    values[i] = value;
-                    return;
-                }
-                keys[size] = key;
-                values[size] = value;
+        for (int i = 0; i < size; i++) {
+            if (isEquals(key, keys[i])) {
+                values[i] = value;
+                return;
             }
         }
+        keys[size] = key;
+        values[size] = value;
         size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keys.length; i++) {
-            if (Objects.equals(key, keys[i])) {
+        for (int i = 0; i < size; i++) {
+            if (isEquals(key, keys[i])) {
                 return values[i];
             }
         }
@@ -43,5 +39,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return size;
+    }
+
+    private boolean isEquals(K firstKey, K secondKey) {
+        return firstKey == secondKey || (firstKey != null && firstKey.equals(secondKey));
     }
 }
