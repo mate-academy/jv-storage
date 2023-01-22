6c6c81a9a0e254ab1d44361cfe4e8e3518b3a88d
Igor Parovenko <igorparovenko@yahoo.com>
2023-01-22 13:23:50 +0100

    second impl.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,37 +3,57 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private K[] keys = (K[]) new Object[10];
-    private V[] values = (V[]) new Object[10];
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private final K[] keys = (K[]) new Object[MAX_ITEMS_NUMBER];
+    private final V[] values = (V[]) new Object[MAX_ITEMS_NUMBER];
+
+    private int counter = 0;
+
+    public boolean keyCheck(K key) {
+        counter = 0;
+        for (int i = 0; i < this.values.length; i++) {
+            if (this.keys[i] == null && this.values[i] != null
+                    && key == null || this.keys[i] != null
+                    && this.keys[i].equals(key)) {
+                counter = i;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isEmpty() {
+        counter = 0;
+        for (int i = 0; i < this.values.length; i++) {
+            if (this.keys[i] == null && this.values[i] == null) {
+                counter = i;
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public void put(K key, V value) {
-        for (int j = 0; j < values.length; j++) {
-            if (keys[j] == null && values[j] == null || keys[j] == null && key == null
-                    && values[j] != null || keys[j] != null && keys[j].equals(key)) {
-                keys[j] = key;
-                values[j] = value;
-                break;
-            }
+        if (keyCheck(key) || isEmpty()) {
+            this.keys[counter] = key;
+            this.values[counter] = value;
         }
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < values.length; i++) {
-            if (values[i] != null) {
-                if (keys[i] == null && key == null || keys[i] != null && keys[i].equals(key)) {
-                    return values[i];
-                }
-            }
-            }
+        if (keyCheck(key)) {
+            return this.values[counter];
+        }
         return null;
     }
 
     @Override
     public int size() {
         int counter = 0;
-        for (int i = 0; i < values.length; i++) {
-            if (keys[i] != null | values[i] != null) {
+        for (Object value : this.values) {
+            if (value != null) {
                 counter++;
             }
         }
