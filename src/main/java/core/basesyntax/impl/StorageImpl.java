8fcfaa67cec129cf700d21e8d77bf13a620928b5
Mykhailo Dolia <mndolia@gmail.com>
2023-01-23 22:26:07 +0000

    implementation hw storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,23 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.HashMap;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private HashMap<K,V> storageList = new HashMap<>();
+
     @Override
     public void put(K key, V value) {
+        storageList.put(key, value);
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return storageList.get(key);
     }
 
     @Override
     public int size() {
-        return -1;
+        return storageList.size();
     }
 }
