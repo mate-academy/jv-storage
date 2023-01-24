bdb619585e48b973e6be32742e14320bd51d74bd
Yaroslav Ometiukh <120783204+yaroslav-ometiukh@users.noreply.github.com>
2023-01-24 08:17:45 +0200

    Update src/main/java/core/basesyntax/impl/StorageImpl.java
    Co-authored-by: Ivan Kosovych <41166578+Ivan95kos@users.noreply.github.com>

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,7 +6,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ELEMENTS = 10;
     private final K[] keys;
     private final V[] values;
-    private int now;
+    private int size;
 
     public StorageImpl() {
         keys = (K[]) new Object[MAX_ELEMENTS];
