9e636036bc1b20b10eb6047a1cffb19c8a94121c
Mykhailo Dolia <mndolia@gmail.com>
2023-01-25 15:23:41 +0000

    implementation hw storage (changed HashMap on Array)

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,23 +1,47 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.HashMap;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private HashMap<K,V> storageList = new HashMap<>();
+    class Pair<K,V> {
+        private K key;
+        private V value;
+
+        public Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private static final int MAX_LENGTH = 10;
+    private Pair<K,V>[] pair = new Pair[MAX_LENGTH];
+    //private V[] values = (V[]) new Object[MAX_LENGTH];
+    private int sizeArr;
 
     @Override
     public void put(K key, V value) {
-        storageList.put(key, value);
+        for (int i = 0; i < sizeArr; i++) {
+            if (pair[i].key != null && pair[i].key.equals(key) || pair[i].key == key) {
+                pair[i].value = value;
+                return;
+            }
+        }
+        pair[sizeArr] = new Pair<>(key, value);
+        sizeArr++;
     }
 
     @Override
     public V get(K key) {
-        return storageList.get(key);
+        for (int i = 0; i < sizeArr; i++) {
+            if (pair[i].key != null && pair[i].key.equals(key) || pair[i].key == key) {
+                return pair[i].value;
+            }
+        }
+        return null;
     }
 
     @Override
     public int size() {
-        return storageList.size();
+        return sizeArr;
     }
 }
