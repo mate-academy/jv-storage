32fc28921595c3ea12486e4e46994f78e220b93b
Mykhailo Dolia <mndolia@gmail.com>
2023-01-26 00:26:58 +0000

    implementation hw storage (fixed)

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,36 +3,29 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    class Pair<K,V> {
-        private K key;
-        private V value;
-
-        public Pair(K key, V value) {
-            this.key = key;
-            this.value = value;
-        }
-    }
-
     private static final int MAX_LENGTH = 10;
-    private Pair<K,V>[] pair = new Pair[MAX_LENGTH];
-    //private V[] values = (V[]) new Object[MAX_LENGTH];
-    private int sizeArr;
+    private Pair<K,V>[] pair;
+    private int size;
+
+    public StorageImpl() {
+        this.pair = new Pair[MAX_LENGTH];
+    }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < sizeArr; i++) {
+        for (int i = 0; i < size; i++) {
             if (pair[i].key != null && pair[i].key.equals(key) || pair[i].key == key) {
                 pair[i].value = value;
                 return;
             }
         }
-        pair[sizeArr] = new Pair<>(key, value);
-        sizeArr++;
+        pair[size] = new Pair<>(key, value);
+        size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < sizeArr; i++) {
+        for (int i = 0; i < size; i++) {
             if (pair[i].key != null && pair[i].key.equals(key) || pair[i].key == key) {
                 return pair[i].value;
             }
@@ -42,6 +35,16 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return sizeArr;
+        return size;
+    }
+
+    private class Pair<K,V> {
+        private K key;
+        private V value;
+
+        public Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }
