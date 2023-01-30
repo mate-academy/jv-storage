7c2cca1d622b6ad90dbf51efe9de35e70132ff21
Maxim Skory <devisrush@gmail.com>
2023-01-30 02:18:02 +0200

    StorageImpl class logic was implemented

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,61 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private static final int ARRAY_SIZE = 10;
+    private final Object[] keys;
+    private final Object[] values;
+
+    public StorageImpl() {
+        keys = new Object[ARRAY_SIZE];
+        values = new Object[ARRAY_SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] == null && values[i] == null) {
+                keys[i] = key;
+                values[i] = value;
+                return;
+            }
+            if (key == null) {
+                while (keys[i] != null) {
+                    i++;
+                }
+                values[i] = value;
+                return;
+            }
+            if (key.equals(keys[i])) {
+                values[i] = value;
+                return;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if (key == null) {
+                while (keys[i] != null) {
+                    i++;
+                }
+                return (V) values[i];
+            }
+            if (key.equals(keys[i])) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        for (int i = 0; i < keys.length; i++) {
+            if (values[i] == null) {
+                return i;
+            }
+        }
+        return ARRAY_SIZE;
     }
 }
