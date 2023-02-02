94ce64a2433b20f0757b3088b1e172558370192b
Aleksandr Zahorodnii <git.hub.zagar@gmail.com>
2023-02-02 18:59:22 +0100

    fixed method put and delete method containsKey after review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,25 +4,31 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_LENGTH_STORAGE = 10;
-    private K[] keyArray = (K[]) new Object[MAX_LENGTH_STORAGE];
-    private V[] valueArray = (V[]) new Object[MAX_LENGTH_STORAGE];
-    private int size = 0;
+    private K[] keyArray;;
+    private V[] valueArray ;
+    private int size;
+
+    public StorageImpl() {
+        keyArray = (K[]) new Object[MAX_LENGTH_STORAGE];
+        valueArray = (V[]) new Object[MAX_LENGTH_STORAGE];
+    }
 
     @Override
     public void put(K key, V value) {
-        if (containsKey(key)) {
+        int index = indexOf(key);
+        if (index != -1) {
             valueArray[indexOf(key)] = value;
         } else {
             keyArray[size] = key;
             valueArray[size] = value;
             size++;
         }
-
     }
 
     @Override
     public V get(K key) {
-        if (containsKey(key)) {
+        int index = indexOf(key);
+        if (index != -1) {
             return valueArray[indexOf(key)];
         }
         return null;
@@ -40,14 +46,5 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             }
         }
         return -1;
-    }
-
-    private boolean containsKey(K key) {
-        for (int i = 0; i < size; i++) {
-            if (key == null && keyArray[i] == null || (key != null && key.equals(keyArray[i]))) {
-                return true;
-            }
-        }
-        return false;
     }
 }
