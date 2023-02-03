1e9b85466169a883e7e2e337415ebd99c787f3bf
Mykola Bobyliak <bobimyko@gmail.com>
2023-02-03 15:31:44 +0200

    HW storage after second review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -10,31 +10,21 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        boolean isTheSameKey = keyExists(key, value);
-        for (int i = 0; i < size; i++) {
-            if (keyArray[i] != null && keyArray[i].equals(key)) {
+        for (int i = 0; i < size; ++i) {
+            if (keyExists(key, keyArray[i])) {
                 valueArray[i] = value;
-                isTheSameKey = true;
+                return;
             }
         }
-        if (!isTheSameKey) {
-            keyArray[size] = key;
-            valueArray[size] = value;
-            size++;
-        }
+        keyArray[size] = key;
+        valueArray[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        if (key == null) {
-            for (int i = 0; i < size; i++) {
-                if (keyArray[i] == null) {
-                    return valueArray[i];
-                }
-            }
-        }
         for (int i = 0; i < size; i++) {
-            if (keyArray[i] != null && keyArray[i].equals(key)) {
+            if (keyExists(key, keyArray[i])) {
                 return valueArray[i];
             }
         }
@@ -46,16 +36,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         return size;
     }
 
-    private boolean keyExists(K key1, V key2) {
-        boolean isTheSameKey = false;
-        if (key1 == null) {
-            for (int i = 0; i < size; i++) {
-                if (keyArray[i] == null) {
-                    valueArray[i] = key2;
-                    isTheSameKey = true;
-                }
-            }
-        }
-        return isTheSameKey;
+    private boolean keyExists(K key1, K key2) {
+        return key1 == key2 || key1 != null && key1.equals(key2);
     }
 }
