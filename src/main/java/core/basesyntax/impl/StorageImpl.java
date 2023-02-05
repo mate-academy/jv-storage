a7cc1550d907aa9fc12bfdaef1db8fd4026ca3a6
slaybrute <slaybrute@gmail.com>
2023-02-05 21:41:58 +0200

    solved task

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,45 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.ArrayList;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private static final int MAX_STORAGE_VALUE = 10;
+    private int createdStorages;
+
+    private ArrayList<K> keyArrayList = new ArrayList<>();
+    private ArrayList<V> valueArrayList = new ArrayList<>();
+
+    private K key;
+    private V value;
+
     @Override
     public void put(K key, V value) {
+        if (createdStorages <= MAX_STORAGE_VALUE) {
+            if (keyArrayList.contains(key)) {
+                valueArrayList.remove(valueArrayList
+                        .get(keyArrayList.indexOf(key)));
+                keyArrayList.remove(key);
+            }
+            this.key = key;
+            this.value = value;
+            keyArrayList.add(0, key);
+            valueArrayList.add(0, value);
+            createdStorages++;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        if (!keyArrayList.contains(key)) {
+            return null;
+        }
+        return valueArrayList.get(keyArrayList.indexOf(key));
     }
 
     @Override
     public int size() {
-        return -1;
+        return this.valueArrayList.size();
     }
 }
