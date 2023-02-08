b1d65bec2fe0c3a82964170edf8897b497d2ee3c
Yuriy Kononchuk <y_kononchuk@ukr.net>
2023-02-08 19:52:49 +0200

    fixed StorageImpl after 1st review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,31 +1,43 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_STORAGE_SIZE = 10;
-    private final K[] keyItem = (K[]) new Object[MAX_STORAGE_SIZE];
-    private final V[] itemPair = (V[]) new Object[MAX_STORAGE_SIZE];
-    private int elementNumber = 0;
+    private K[] keyItem;
+    private V[] itemPair;
+    private int size;
+
+    public StorageImpl() {
+        keyItem = (K[]) new Object[MAX_STORAGE_SIZE];
+        itemPair = (V[]) new Object[MAX_STORAGE_SIZE];
+    }
+
+    private boolean isKeyTheSame(K key, K[] keyItem, int i) {
+        boolean isKeyNull = (key == null && keyItem[i] == null);
+        boolean isKeyNotNull = (key != null && key.equals(keyItem[i]));
+        return isKeyNull || isKeyNotNull;
+    }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < elementNumber; i++) {
-            if (Objects.equals(key, keyItem[i])) {
+        for (int i = 0; i < size; i++) {
+            boolean isKeyTheSame = isKeyTheSame(key, keyItem, i);
+            if (isKeyTheSame) {
                 itemPair[i] = value;
-                elementNumber--;
+                size--;
             }
         }
-        keyItem[elementNumber] = key;
-        itemPair[elementNumber] = value;
-        elementNumber++;
+        keyItem[size] = key;
+        itemPair[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keyItem.length; i++) {
-            if (Objects.equals(key, keyItem[i])) {
+        for (int i = 0; i < size; i++) {
+            boolean isKeyTheSame = isKeyTheSame(key, keyItem, i);
+            if (isKeyTheSame) {
                 return itemPair[i];
             }
         }
@@ -34,6 +46,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return elementNumber--;
+        return size;
     }
 }
