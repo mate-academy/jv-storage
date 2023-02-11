8f6afd3f3f66150b92766615e0ef3b7dd4fbf328
“Ymishok321 <kkoiku3@gmail.com>
2023-02-11 11:21:26 +0200

    Implemented StorageImpl

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,48 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int ARRAY_MAX_LENGTH = 10;
+    private K[] keysStore;
+    private V[] valuesStore;
+
+    private int length;
+
+    public StorageImpl() {
+        this.keysStore = (K[]) new Object[ARRAY_MAX_LENGTH];
+        this.valuesStore = (V[]) new Object[ARRAY_MAX_LENGTH];
+        length = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < length; i++) {
+            if ((keysStore[i] == null && key == null)
+                    ^ (keysStore[i] != null && keysStore[i].equals(key))) {
+                valuesStore[i] = value;
+                return;
+            }
+        }
+        keysStore[length] = key;
+        valuesStore[length] = value;
+        length++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < length; i++) {
+            if ((keysStore[i] == null && key == null)
+                    ^ (keysStore[i] != null && keysStore[i].equals(key))) {
+                return valuesStore[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return length;
     }
 }
