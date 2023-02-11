da7a0be69cced8cb33fa620c5706084fb2c23da2
Roman Omeliukh <romik3513@gmail.com>
2023-02-11 14:04:37 +0200

    StorageImpl

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,52 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private final int maxCapacity = 10;
+    private final Object[] arrKey = new Object [maxCapacity];
+    private final Object[] arrValue = new Object [maxCapacity];
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < maxCapacity; i++) {
+            if (key != null) {
+                if (arrKey[i] == null && arrValue[i] == null || key.equals(arrKey[i])) {
+                    arrKey[i] = key;
+                    arrValue[i] = value;
+                    break;
+                }
+            }
+            if (arrKey[i] == key) {
+                arrKey[i] = key;
+                arrValue[i] = value;
+                break;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < maxCapacity; i++) {
+            if (key != null) {
+                if (arrKey[i] == null && arrValue[i] == null || key.equals(arrKey[i])) {
+                    return (V) arrValue[i];
+                }
+            }
+            if (arrKey[i] == key) {
+                return (V) arrValue[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        int size = 0;
+        for (int i = 0; i < maxCapacity; i++) {
+            if (arrValue[i] != null) {
+                size += 1;
+            }
+        }
+        return size;
     }
 }
