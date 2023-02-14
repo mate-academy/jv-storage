3a2f494a2ae7fe6f35dae720310eb297aacc0e14
Denys Dominskyi <dominskyj@gmail.com>
2023-02-14 22:57:23 +0200

    Initial commit

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,49 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int START_CAPACITY = 10;
+    private final KeyValuePair<K, V>[] array = new KeyValuePair[START_CAPACITY];
+    private int size = 0;
+
     @Override
     public void put(K key, V value) {
+        boolean isExist = false;
+        KeyValuePair<K, V> instanceOfKeyValue = new KeyValuePair<>(key, value);
+        for (int i = 0; i < size; i++) {
+            if (array[i].getKey() != null) {
+                if (array[i].getKey().equals(key)) {
+                    array[i] = instanceOfKeyValue;
+                    isExist = true;
+                    break;
+                }
+            } else if (array[i].getKey() == key) {
+                array[i] = instanceOfKeyValue;
+                isExist = true;
+                break;
+            }
+        }
+        if (!isExist) {
+            array[size] = instanceOfKeyValue;
+            size++;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].getKey() != null) {
+                if (array[i].getKey().equals(key)) {
+                    return array[i].getValue();
+                }
+            } else if (array[i].getKey() == key) {
+                return array[i].getValue();
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
