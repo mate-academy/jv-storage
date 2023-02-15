378a5ed4b52a778810d64e7513efa3221d7c2ea9
Potseluiko Vitalii <vetaliy063@gmail.com>
2023-02-15 23:03:54 +0200

    Implemented StorageImpl class and its methods.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,52 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_CAPACITY = 10;
+    private Object[] keys;
+    private Object[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = new Object[MAX_CAPACITY];
+        values = new Object[MAX_CAPACITY];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (size == 0) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        } else {
+            for (int i = 0; i < size && size <= MAX_CAPACITY; i++) {
+                if (key == null && keys[i] == null) {
+                    values[i] = value;
+                    return;
+                } else if (keys[i] != null && keys[i].equals(key)) {
+                    values[i] = value;
+                    return;
+                }
+            }
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == null && keys[i] == null) {
+                return (V) values[i];
+            } else if (keys[i] != null && keys[i].equals(key)) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
