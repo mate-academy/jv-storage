e79cf0e022d57352ca86a7793baabdaa0ed11593
kromchk <kromchyk@yahoo.com>
2023-02-15 23:14:21 +0200

    refactored task with consider to remarks again

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,45 +6,42 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_RANGE = 10;
     private K[] keysRange;
     private V[] valuesRange;
+    private int index;
 
     public StorageImpl() {
         keysRange = (K[]) new Object[MAX_RANGE];
         valuesRange = (V[]) new Object[MAX_RANGE];
+        index = 0;
     }
 
     @Override
     public void put(K key, V value) {
         int i = 0;
-        for (; i < MAX_RANGE; i++) {
-            if (keysRange[i] == key || keysRange[i] == valuesRange[i]
-                    || (keysRange[i] != null && keysRange[i].equals(key))) {
-                keysRange[i] = key;
+        for (; i < index; i++) {
+            if (keysRange[i] == key || (keysRange[i] != null && keysRange[i].equals(key))) {
                 valuesRange[i] = value;
                 break;
             }
+        }
+        if (i == index) {
+            keysRange[index] = key;
+            valuesRange[index++] = value;
         }
     }
 
     @Override
     public V get(K key) {
         int i = 0;
-        for (; i < MAX_RANGE; i++) {
-            if (keysRange[i] == key || keysRange[i] == valuesRange[i]
-                    || (keysRange[i] != null && keysRange[i].equals(key))) {
+        for (; i < index; i++) {
+            if (keysRange[i] == key || (keysRange[i] != null && keysRange[i].equals(key))) {
                 break;
             }
         }
-        return valuesRange[i];
+        return i < index ? valuesRange[i] : null;
     }
 
     @Override
     public int size() {
-        int i = 0;
-        for (;i < MAX_RANGE; i++) {
-            if (keysRange[i] == valuesRange[i]) {
-                break;
-            }
-        }
-        return i;
+        return index;
     }
 }
