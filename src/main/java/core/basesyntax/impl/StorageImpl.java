5055ecb8e73175999e2149f37d8ab9ebdff74b9d
Denys Dominskyi <dominskyj@gmail.com>
2023-02-16 12:18:11 +0200

    1. Generated constructor of StorageImpl and initialize new KeyValuePair there 2. Removed field isExist 3. Checked text in RunTimeException

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,29 +4,28 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int START_CAPACITY = 10;
-    private final KeyValuePair<K, V>[] array = new KeyValuePair[START_CAPACITY];
+    private KeyValuePair<K, V>[] array;
     private int size = 0;
+
+    public StorageImpl() {
+        this.array = (KeyValuePair<K, V>[]) new KeyValuePair[START_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
-        boolean isExist = false;
         KeyValuePair<K, V> instanceOfKeyValue = new KeyValuePair<>(key, value);
         for (int i = 0; i < size; i++) {
             if (array[i].getKey() == key || array[i].getKey() != null
                     && array[i].getKey().equals(key)) {
                 array[i] = instanceOfKeyValue;
-                isExist = true;
-                break;
+                return;
             }
         }
-        if (!isExist) {
-            if (size == START_CAPACITY) {
-                throw new RuntimeException(
-                        "The storage is full, you can replace an existing element!");
-            }
-            array[size] = instanceOfKeyValue;
-            size++;
+        if (size == START_CAPACITY) {
+            throw new RuntimeException("The storage is full, you can replace an existing"
+                    + " element!");
         }
+        array[size++] = instanceOfKeyValue;
     }
 
     @Override
