57af81ca167edd9575c657a2b24a716e4c20a9ce
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2023-02-17 12:54:31 +0200

    implemented Storage with service methods

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,54 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_CAPACITY = 10;
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_CAPACITY];
+        values = (V[]) new Object[MAX_CAPACITY];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (isKeyIdentical(key, i)) {
+                values[i] = value;
+                return;
+            }
+        }
+        if (isStorageFull()) {
+            throw new RuntimeException("Can't add key: " + key + ", value: " + value + "."
+                    + " Storage is full");
+        }
+        keys[size] = key;
+        values[size++] = value;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (isKeyIdentical(key, i)) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    private boolean isKeyIdentical(K key, int i) {
+        return keys[i] == key || keys[i] != null && keys[i].equals(key);
+    }
+
+    private boolean isStorageFull() {
+        return size == MAX_CAPACITY;
     }
 }
+
+
