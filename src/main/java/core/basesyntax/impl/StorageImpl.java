d95b6dc267a6a78943e8530c3f415c64cef6b92b
Viacheslav Kyslyi <kyslyiviacheslav@gmail.com>
2023-02-17 18:20:31 +0200

    Solved task Implemented storage of key-value type; Implemented methods put,get,size.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,50 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    public static final int MAX_STORAGE_PLACE = 10;
+    public static final int NUMBER_OF_ITEMS = 2;
+    private final Object[][] valueStorage;
+    private int currentSize;
+
+    public StorageImpl() {
+        valueStorage = new Object[MAX_STORAGE_PLACE][NUMBER_OF_ITEMS];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < currentSize; i++) {
+            if (equal(key,valueStorage[i][0])) {
+                valueStorage[i][1] = value;
+                return;
+            }
+        }
+        valueStorage[currentSize][0] = key;
+        valueStorage[currentSize][1] = value;
+        currentSize++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < currentSize; i++) {
+            if (equal(key,valueStorage[i][0])) {
+                return (V) valueStorage[i][1];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return currentSize;
+    }
+
+    private boolean equal(Object a, Object b) {
+        if (a == b) {
+            return true;
+        }
+        if (a == null || b == null || a.getClass() != b.getClass()) {
+            return false;
+        }
+        return (a == b) || (a != null && a.equals(b));
     }
 }
