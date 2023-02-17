a88fa5f5da20e758956d6f49b726b50b773298ab
Vadym Kovalenko <axeshock322@gmail.com>
2023-02-17 21:46:36 +0200

    rename constant name of storage lenght, simplify methods

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,22 +1,23 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int MAX_NUMBER = 10;
+    private static final int MAX_STORAGE_LENGTH = 10;
     private K[] keys;
     private V[] values;
     private int size;
 
     public StorageImpl() {
-        keys = (K[]) new Object[MAX_NUMBER];
-        values = (V[]) new Object[MAX_NUMBER];
+        keys = (K[]) new Object[MAX_STORAGE_LENGTH];
+        values = (V[]) new Object[MAX_STORAGE_LENGTH];
     }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (keyCheck(key, i)) {
+            if (Objects.equals(key, keys[i])) {
                 values[i] = value;
                 return;
             }
@@ -29,7 +30,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (keyCheck(key, i)) {
+            if (Objects.equals(key, keys[i])) {
                 return values[i];
             }
         }
@@ -39,9 +40,5 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return size;
-    }
-
-    public boolean keyCheck(K key, int index) {
-        return key == keys[index] || key != null && key.equals(keys[index]);
     }
 }
