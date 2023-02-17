c7bb8d1f745a8a9e9436912c8a7c4f3e079e04cc
Denys <denchik9026@gmail.com>
2023-02-17 16:55:06 +0200

    implement storage logic

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,52 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private final StorageItem[] storageItems;
+    private int indexCounter = 0;
+
+    public StorageImpl() {
+        this.storageItems = new StorageItem[MAX_ITEMS_NUMBER];
+    }
+
     @Override
     public void put(K key, V value) {
+        StorageItem<K, V> existingStorageItem = getStorageItemByKey(key);
+        if (existingStorageItem != null) {
+            existingStorageItem.setValue(value);
+            return;
+        }
+        StorageItem<K, V> storageItem = new StorageItem<>(key, value);
+        try {
+            storageItems[indexCounter] = storageItem;
+            indexCounter++;
+        } catch (IndexOutOfBoundsException e) {
+            throw new RuntimeException("Storage is FULL, you can't add new item", e);
+        }
     }
 
     @Override
     public V get(K key) {
+        StorageItem<K, V> existingStorageItem = getStorageItemByKey(key);
+        if (existingStorageItem != null) {
+            return existingStorageItem.getValue();
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return indexCounter;
+    }
+
+    private StorageItem<K, V> getStorageItemByKey(K key) {
+        for (StorageItem<K, V> item : storageItems) {
+            if (item != null) {
+                if (item.getKey() == key || (item.getKey() != null && item.getKey().equals(key))) {
+                    return item;
+                }
+            }
+        }
+        return null;
     }
 }
