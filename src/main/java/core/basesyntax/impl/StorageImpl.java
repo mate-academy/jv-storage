1ad26b75abb7ff7c1652589327b0333237594da7
De1eF <jatfun58@gmail.com>
2023-02-18 12:23:29 +0200

    Added storage implementation. Maybe there is another way to do a null check on cells in put & get. I am ready for critiques

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,18 +2,48 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
+    List<K> keyList = new ArrayList<>();
+    List<V> valueList = new ArrayList<>();
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < keyList.size(); i++) {
+            if (key == null && keyList.get(i) == null) {
+                valueList.set(i, value);
+                return;
+            }
+            if (keyList.get(i) != null && keyList.get(i).equals(key)) {
+                valueList.set(i, value);
+                return;
+            }
+        }
+        addCell(key, value);
+    }
+
+    private void addCell(K key, V value) {
+        keyList.add(key);
+        valueList.add(value);
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keyList.size(); i++) {
+            if (key == null && keyList.get(i) == null) {
+                return valueList.get(i);
+            }
+            if (keyList.get(i) != null && keyList.get(i).equals(key)) {
+                return valueList.get(i);
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return keyList.size();
     }
 }
