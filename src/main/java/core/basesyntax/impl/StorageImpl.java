4727679a348a32957c418a764b4841a674e9b175
Nickolay Voloshchuk <124300295+NickolayVoloshchuk@users.noreply.github.com>
2023-02-19 23:59:53 +0200

    HomeWork JavaCore/Storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,40 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final short MAX_LENGTH_STORAGE = 10;
+    private final K[] keys = (K[]) new Object[MAX_LENGTH_STORAGE];
+    private final V[] values = (V[]) new Object[MAX_LENGTH_STORAGE];
+    private int length;
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < length; i++) {
+            if (Objects.equals(key, keys[i])) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[length] = key;
+        values[length] = value;
+        length++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < length; i++) {
+            if (Objects.equals(key, keys[i])) {
+                return values[i];
+            }
+        }
+        System.out.println("Can't find record with key: " + key);
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return length;
     }
 }
