42fec6338b1209ba3fdec20903bc73e303a4ca94
Volodymyr Prysiazhniuk <verticalsyu@gmail.com>
2023-02-19 18:21:19 +0200

    Implement the Storage of key-value type that is parameterized with two types of data

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,46 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int STORAGE_SIZE = 10;
+
+    private K[] keys;
+    private V[] values;
+    private int space;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[STORAGE_SIZE];
+        values = (V[]) new Object[STORAGE_SIZE];
+    }
+
+    public boolean verify(K k, K v) {
+        return (k != null && k.equals(v) || k == v);
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < space; i++) {
+            if (verify(keys[i], key)) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[space] = key;
+        values[space] = value;
+        space++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < space; i++) {
+            if (verify(keys[i], key)) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return space;
     }
 }
