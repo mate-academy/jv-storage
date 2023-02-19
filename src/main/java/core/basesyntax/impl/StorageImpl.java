ba28fdec0bd5467fe4e29e48a67842902ab0755e
Oleksandr Krasnov <redman2775@gmail.com>
2023-02-19 14:35:52 +0200

    realized StorageImpl; added 2 arrays; added comparisonKey() method; methods get(), size(), put() -  was override; maven checkstyle - BUILD SUCCESS;

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,46 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_VALUES_NUMBERS = 10;
+
+    private K[] keysInStorage;
+    private V[] valuesInStorage;
+    private int length;
+
+    public StorageImpl() {
+        keysInStorage = (K[]) new Object[MAX_VALUES_NUMBERS];
+        valuesInStorage = (V[]) new Object[MAX_VALUES_NUMBERS];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < length; i++) {
+            if (comparisonKey(keysInStorage[i], key)) {
+                valuesInStorage[i] = value;
+                return;
+            }
+        }
+        keysInStorage[length] = key;
+        valuesInStorage[length] = value;
+        length++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < length; i++) {
+            if (comparisonKey(keysInStorage[i], key)) {
+                return valuesInStorage[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return length;
+    }
+
+    private boolean comparisonKey(K firstKey, K secondKey) {
+        return (firstKey == secondKey) || (firstKey != null && firstKey.equals(secondKey));
     }
 }
