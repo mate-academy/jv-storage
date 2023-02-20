49f8b8231f9d79a4b3c47ee949f040e29d9d344a
yuriy49 <yuriy121991@gmail.com>
2023-02-20 16:29:08 +0200

    BUILD SUCCESS  create private fields  add constructor and cast to V/K[] type objects  implement methods below

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,56 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    @Override
-    public void put(K key, V value) {
+    private static final int MAX_SIZE = 10;
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_SIZE];
+        values = (V[]) new Object[MAX_SIZE];
+        size = 0;
     }
 
-    @Override
+    public void put(K key, V value) {
+        int index = findIndex(key);
+        if (index >= 0) {
+            values[index] = value;
+        } else {
+            if (size >= MAX_SIZE) {
+                throw new IllegalStateException("Storage is full");
+            }
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
+    }
+
     public V get(K key) {
+        int index = findIndex(key);
+        if (index >= 0) {
+            return values[index];
+        }
         return null;
     }
 
-    @Override
     public int size() {
+        return size;
+    }
+
+    private int findIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (notNull(keys[i],key)) {
+                return i;
+            }
+        }
         return -1;
+    }
+
+    private boolean notNull(K key1,K key2) {
+        return Objects.equals(key1, key2);
     }
 }
