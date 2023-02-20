cd0aed06b8cdd861a1e68e1792c975a1991408cf
Maksym <maxim.yashyn@gmail.com>
2023-02-20 15:24:35 +0200

    implement StorageImpl

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,44 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int SIZE_OF_ARRAY = 10;
+    private K[] arrayK;
+    private V[] arrayV;
+    private int size;
+
+    public StorageImpl() {
+        arrayV = (V[]) new Object[SIZE_OF_ARRAY];
+        arrayK = (K[]) new Object[SIZE_OF_ARRAY];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i <= size; i++) {
+            if ((arrayK[i] == arrayV[i])
+                    || (arrayK[i] == key)
+                    || (arrayK[i] != null && arrayK[i].equals(key))) {
+                if (arrayK[i] == arrayV[i]) {
+                    size++;
+                }
+                arrayK[i] = key;
+                arrayV[i] = value;
+                break;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i <= size; i++) {
+            if (arrayK[i] == key || (arrayK[i] != null && arrayK[i].equals(key))) {
+                return arrayV[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
