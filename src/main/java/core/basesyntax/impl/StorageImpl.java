b35cf3624abce6d1acee16da8ec71a4ae92e670e
Danilo Kozakov <tosterworld@gmail.com>
2023-02-20 18:07:59 +0200

    Storage implemented at StorageImpl

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,47 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    public static final int MAX_SIZE = 10;
+    private K[] keys;
+    private V[] values;
+    private int count = 0;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_SIZE];
+        values = (V[]) new Object[MAX_SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < count; i++) {
+            if ((keys[i] == null && key == null)
+                    || (keys[i] != null && keys[i].equals(key))) {
+                values[i] = value;
+                return;
+            }
+        }
+        if (count < MAX_SIZE) {
+            keys[count] = key;
+            values[count] = value;
+            count++;
+        } else {
+            throw new RuntimeException("Cannot add more then 10 values!");
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < count; i++) {
+            if ((keys[i] == null && key == null)
+                    || (keys[i] != null && keys[i].equals(key))) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return count;
     }
 }
