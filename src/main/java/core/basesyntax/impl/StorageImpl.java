27b692abf5ebd7602b1b998ef53bd80b7da41d8a
Oleksandr Karpiuk <karpo99old@gmail.com>
2023-02-20 11:00:08 +0200

    Change names, remove braces

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,45 +3,45 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    public static final Integer STORE_SIZE = 10;
-    private Object[] keyArr;
-    private Object[] valueArr;
-    private int lengthOfActualValue = 0;
+    public static final int MAX_SIZE = 10;
+    private Object[] key;
+    private Object[] value;
+    private int size = 0;
 
     public StorageImpl() {
-        valueArr = (V[]) new Object[STORE_SIZE];
-        keyArr = (K[]) new Object[STORE_SIZE];
+        value = (V[]) new Object[MAX_SIZE];
+        key = (K[]) new Object[MAX_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < lengthOfActualValue; i++) {
-            if (compareValue(keyArr[i], key)) {
-                valueArr[i] = value;
+        for (int i = 0; i < size; i++) {
+            if (compareValue(this.key[i], key)) {
+                this.value[i] = value;
                 return;
             }
         }
-        keyArr[lengthOfActualValue] = key;
-        valueArr[lengthOfActualValue] = value;
-        lengthOfActualValue++;
+        this.key[size] = key;
+        this.value[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < lengthOfActualValue; i++) {
-            if (compareValue(keyArr[i], key)) {
-                return (V) valueArr[i];
+        for (int i = 0; i < size; i++) {
+            if (compareValue(this.key[i], key)) {
+                return (V) value[i];
             }
         }
         return null;
     }
 
     private boolean compareValue(Object obj1, Object obj2) {
-        return (obj1 == obj2) || (obj1 != null && obj1.equals(obj2));
+        return obj1 == obj2 || obj1 != null && obj1.equals(obj2);
     }
 
     @Override
     public int size() {
-        return lengthOfActualValue;
+        return size;
     }
 }
