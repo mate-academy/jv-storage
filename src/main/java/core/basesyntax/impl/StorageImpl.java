ec939e06d65e04cb903e1cc1f283f4d2c39f8d20
IvanZuravlev228 <courses.zhuravlev.ivan@gmail.com>
2023-02-20 13:48:14 +0200

    fixed checkstyle problem

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,31 +6,30 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int ARRAY_SIZE = 10;
     private K[] keys;
     private V[] values;
-    private int lastElement;
+    private int size;
 
     public StorageImpl() {
         keys = (K[]) new Object[ARRAY_SIZE];
         values = (V[]) new Object[ARRAY_SIZE];
-        lastElement = 0;
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < lastElement; i++) {
-            if ((keys[i] == key) || keys[i] != null && keys[i].equals(key)) {
+        for (int i = 0; i < size; i++) {
+            if (equals(keys[i], key)) {
                 values[i] = value;
                 return;
             }
         }
-        keys[lastElement] = key;
-        values[lastElement] = value;
-        lastElement++;
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < lastElement; i++) {
-            if ((keys[i] == key) || keys[i] != null && keys[i].equals(key)) {
+        for (int i = 0; i < size; i++) {
+            if (equals(keys[i], key)) {
                 return values[i];
             }
         }
@@ -39,6 +38,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return lastElement;
+        return size;
+    }
+
+    private boolean equals(Object a, Object b) {
+        return (a == b) || a != null && a.equals(b);
     }
 }
