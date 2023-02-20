d0a29c4ece4153c8535e52fbed2d385d1f9ff3ac
Vasyl Sagaidak <saikopixie@gmail.com>
2023-02-20 12:34:47 +0200

    Erased redundant comments

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -8,14 +8,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private V[] values;
     private int size;
 
-    //Implement the Storage of key-value type that is parameterized with two types of data.
-
     public StorageImpl() {
         keys = (K[]) new Object[MAX_NUMBER_OF_ELEMENTS];
         values = (V[]) new Object[MAX_NUMBER_OF_ELEMENTS];
     }
-
-    // You should be able to put new key-value pair
 
     @Override
     public void put(K key, V value) {
@@ -30,8 +26,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         size++;
     }
 
-    //and to get value by key.
-
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
@@ -41,10 +35,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         }
         return null;
     }
-
-    // If you are given the key-value pair,
-    // and the storage already contains this key,
-    // you should replace the value.
 
     private boolean compareKeys(K a, K b) {
         return (a == b) || a != null && a.equals(b);
