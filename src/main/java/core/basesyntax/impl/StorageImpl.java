5a108286fd4a4bc1c9ca27d0b7e80609851ef264
Denys Tyryshkin <tyryshkin1997@gmail.com>
2023-02-20 19:27:47 +0200

    fix mistakes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,20 +3,20 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int ARRAY_LENGTH = 10;
+    private static final int ARRAY_MAX_LENGTH = 10;
     private K[] keyStore;
     private V[] valueStore;
     private int size;
 
     public StorageImpl() {
-        keyStore = (K[]) new Object[ARRAY_LENGTH];
-        valueStore = (V[]) new Object[ARRAY_LENGTH];
+        keyStore = (K[]) new Object[ARRAY_MAX_LENGTH];
+        valueStore = (V[]) new Object[ARRAY_MAX_LENGTH];
     }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if ((key == keyStore[i]) || (key != null && key.equals(keyStore[i]))) {
+            if (resultBoolan(key, keyStore[i])) {
                 valueStore[i] = value;
                 return;
             }
@@ -29,7 +29,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if ((key == keyStore[i]) || (key != null && key.equals(keyStore[i]))) {
+            if (resultBoolan(key, keyStore[i])) {
                 return valueStore[i];
             }
         }
@@ -39,5 +39,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return size;
+    }
+
+    private boolean resultBoolan(K key, K keyStore) {
+        return ((key == keyStore) || (key != null && key.equals(keyStore)));
     }
 }
