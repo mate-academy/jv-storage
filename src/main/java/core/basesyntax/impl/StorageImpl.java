bbd1d34628ce15cf3daaeee45e596ae4824b6648
De1eF <jatfun58@gmail.com>
2023-02-20 18:43:26 +0200

    Implemented additional changes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,22 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private K[] keys = (K[]) new Object[10];
-    private V[] values = (V[]) new Object[10];
+    private static final int DEFAULT_ARRAY_LENGTH = 10;
+    private K[] keys;
+    private V[] values;
     private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[DEFAULT_ARRAY_LENGTH];
+        values = (V[]) new Object[DEFAULT_ARRAY_LENGTH];
+    }
 
     @Override
     public void put(K key, V value) {
-        if (this.keys != null) {
+        if (keys != null) {
             for (int i = 0; i < size; i++) {
-                if (key == this.keys[i]
-                        || key != null && key.equals(this.keys[i])) {
-                    this.values[i] = value;
+                if (compare(key, i)) {
+                    values[i] = value;
                     return;
                 }
             }
@@ -21,22 +26,25 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         keys[size] = key;
         values[size] = value;
         size++;
-        //addCell(key, value);
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (key == this.keys[i]
-                    || key != null && key.equals(this.keys[i])) {
+            if (compare(key, i)) {
                 return values[i];
             }
         }
         return null;
     }
 
+    private boolean compare(K key, int i) {
+        return key == this.keys[i]
+                || key != null && key.equals(this.keys[i]);
+    }
+
     @Override
     public int size() {
-        return keys == null ? 0 : size;
+        return size;
     }
 }
