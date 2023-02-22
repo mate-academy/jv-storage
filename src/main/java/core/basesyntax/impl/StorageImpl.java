771d050d205bd3e1d3b24709864b84be5f9930fd
Andrew kluchnik <andrey.kluchnik.10@gmail.com>
2023-02-22 15:40:13 +0200

    resolved issues of Artur

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,35 +1,43 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int BOUND_OF_ARRAY = 10;
-    private final K[] keysArray = (K[]) new Object[BOUND_OF_ARRAY];
-    private final V[] valuesArray = (V[]) new Object[BOUND_OF_ARRAY];
+    private final K[] keys;
+    private final V[] values;
     private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[BOUND_OF_ARRAY];
+        values = (V[]) new Object[BOUND_OF_ARRAY];
+    }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(key, keysArray[i])) {
-                valuesArray[i] = value;
+            if (isSameKeys(key, keys[i])) {
+                values[i] = value;
                 return;
             }
         }
-        keysArray[size] = key;
-        valuesArray[size] = value;
+        keys[size] = key;
+        values[size] = value;
         size++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(key, keysArray[i])) {
-                return valuesArray[i];
+            if (isSameKeys(key, keys[i])) {
+                return values[i];
             }
         }
         return null;
+    }
+
+    private boolean isSameKeys(K key1, K key2) {
+        return (key1 == key2) || (key1 != null && key1.equals(key2));
     }
 
     @Override
