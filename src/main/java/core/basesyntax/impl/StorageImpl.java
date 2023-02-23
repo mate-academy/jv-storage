f5caa8cb3f32bd10e3c53f2cdb5d07b7836e167f
Valeriy M <valeriym250@gmail.com>
2023-02-23 23:33:50 +0200

    Fixed my program according to your recommendations: 1. deleted size initialization 2. deleted key index from Class field 3. replaced "size" incrementation 4. deleted for cycle from "size" method 5. replaced condition from keys.length to size
    You comment about "size" variable is something great, I don`t know why... I didn`t notice this, It`s very simplify and nice decision

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,25 +6,21 @@ import java.lang.reflect.Array;
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ARRAY_LENGTH = 10;
     private int size;
-
-    private int keyIndex;
     private K[] keys;
     private V[] values;
 
     public StorageImpl() {
         keys = (K[]) Array.newInstance(Object.class, MAX_ARRAY_LENGTH);
         values = (V[]) Array.newInstance(Object.class, MAX_ARRAY_LENGTH);
-        size = -1;
     }
 
     @Override
     public void put(K key, V value) {
-        keyIndex = findKey(key);
+        int keyIndex = findKey(key);
         if (keyIndex == -1) {
-            size += 1;
-            size = (values[size] == null) ? size : size + 1;
             keys[size] = key;
             values[size] = value;
+            size++;
         } else {
             values[keyIndex] = value;
         }
@@ -32,22 +28,17 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        keyIndex = findKey(key);
+        int keyIndex = findKey(key);
         return (keyIndex == -1) ? null : values[keyIndex];
     }
 
     @Override
     public int size() {
-        for (int i = 0; i < values.length; i++) {
-            if (values[i] == null) {
-                return i;
-            }
-        }
         return size;
     }
 
     private int findKey(K key) {
-        for (int i = 0; i < keys.length; i++) {
+        for (int i = 0; i < size; i++) {
             if (key == keys[i] || keys[i] != null && keys[i].equals(key)) {
                 return i;
             }
