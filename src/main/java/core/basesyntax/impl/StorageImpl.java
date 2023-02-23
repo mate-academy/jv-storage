37a9a1f67b4b1f748ad2b7acf90482d952b49a69
Valeriy M <valeriym250@gmail.com>
2023-02-23 19:49:25 +0200

    Fixed my program according to your recommendations: 1. private int size instead static 2. deleted sizeIndex = START_INDEX_LENGTH 3. findKeyIndex used only once and passed this value to variable 4. Fixed findKey mathod 5. Fixed size method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,51 +5,53 @@ import java.lang.reflect.Array;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ARRAY_LENGTH = 10;
-    private static final int START_INDEX_LENGTH = -1;
-    private static int sizeIndex;
+    private int size;
+
+    private int keyIndex;
     private K[] keys;
     private V[] values;
 
     public StorageImpl() {
         keys = (K[]) Array.newInstance(Object.class, MAX_ARRAY_LENGTH);
         values = (V[]) Array.newInstance(Object.class, MAX_ARRAY_LENGTH);
-        sizeIndex = START_INDEX_LENGTH;
+        size = -1;
     }
 
     @Override
     public void put(K key, V value) {
-        if (findKey(key) == -1) {
-            sizeIndex += 1;
-            sizeIndex = (values[sizeIndex] == null) ? sizeIndex : sizeIndex + 1;
-            keys[sizeIndex] = key;
-            values[sizeIndex] = value;
+        keyIndex = findKey(key);
+        if (keyIndex == -1) {
+            size += 1;
+            size = (values[size] == null) ? size : size + 1;
+            keys[size] = key;
+            values[size] = value;
         } else {
-            values[findKey(key)] = value;
+            values[keyIndex] = value;
         }
     }
 
     @Override
     public V get(K key) {
-        return (findKey(key) == -1) ? null : values[findKey(key)];
+        keyIndex = findKey(key);
+        return (keyIndex == -1) ? null : values[keyIndex];
     }
 
     @Override
     public int size() {
         for (int i = 0; i < values.length; i++) {
-            if (keys[i] == null && values[i] == null) {
+            if (values[i] == null) {
                 return i;
             }
         }
-        return 0;
+        return size;
     }
 
     private int findKey(K key) {
-        Integer keyIndex = -1;
         for (int i = 0; i < keys.length; i++) {
             if (key == keys[i] || keys[i] != null && keys[i].equals(key)) {
-                return keyIndex = i;
+                return i;
             }
         }
-        return keyIndex;
+        return -1;
     }
 }
