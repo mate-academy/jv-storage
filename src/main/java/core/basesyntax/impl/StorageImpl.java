816eb635d984f4089bfe8e91d44e3e575288a503
Denys Yastrebov <programmer_p@outlook.com>
2023-02-24 12:17:52 +0200

    firstAttempt

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,14 +6,29 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private K key;
     private V value;
     private int storageCounter = 0;
+    private int maxStorage = 10;
+    private StorageImpl[] storages = new StorageImpl[maxStorage];
+
+    public StorageImpl() {
+    }
+
     @Override
     public void put(K key, V value) {
-        this.key = key;
-        this.value = value;
+        storages[storageCounter].key = key;
+        storages[storageCounter].value = value;
+        storageCounter++;
     }
 
     @Override
     public V get(K key) {
+        V result = null;
+        if (key != null) {
+            for (int i = 0; i < maxStorage; i++) {
+                if (storages[i].key.equals(key)) {
+                    return (V) storages[i].value;
+                }
+            }
+        }
         return null;
     }
 
