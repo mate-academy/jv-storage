ddecfb1653d2afec96d7085ae2d41a00aa81ebd8
Artem Ponomarenko <artemponomar98@gmail.com>
2023-02-28 16:01:32 +0200

    Student review fix

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,12 +1,12 @@
 package core.basesyntax.impl;
 
-import core.basesyntax.Pair;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     public static final int STORAGE_SIZE = 10;
     public static final int NO_RESULT = -1;
     private final Pair<K, V>[] storage;
+    private int size;
 
     public StorageImpl() {
         storage = new Pair[STORAGE_SIZE];
@@ -14,17 +14,14 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        Pair<K, V> toSave = new Pair<>(key, value);
+        final Pair<K, V> newPair = new Pair<>(key, value);
         int position = findExistingPositionByKey(key);
         if (position != NO_RESULT) {
-            storage[position] = toSave;
+            storage[position] = newPair;
+        } else if (size < STORAGE_SIZE) {
+            storage[size++] = newPair;
         } else {
-            position = size();
-            if (position != NO_RESULT) {
-                storage[position] = toSave;
-            } else {
-                throw new ArrayStoreException("Storage is out of free space!");
-            }
+            throw new ArrayStoreException("Storage is out of free space!");
         }
     }
 
@@ -39,25 +36,56 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        for (int i = 0; i < STORAGE_SIZE; i++) {
-            if (storage[i] == null) {
-                return i;
-            }
-        }
-        return NO_RESULT;
+        return size;
     }
 
     private int findExistingPositionByKey(K key) {
-        for (int i = 0; i < STORAGE_SIZE; i++) {
-            if (storage[i] == null) {
-                return NO_RESULT;
-            }
-            K storageKey = storage[i].getKey();
+        for (int i = 0; i < size; i++) {
+            final K storageKey = storage[i].getKey();
             if (storageKey == null && key == null
                     || storageKey != null && storageKey.equals(key)) {
                 return i;
             }
         }
         return NO_RESULT;
+    }
+
+    public static class Pair<K, V> {
+        private final K key;
+        private final V value;
+
+        public Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Pair<K, V> pair = (Pair<K, V>) o;
+            return (getKey() == pair.getKey() || getKey() != null
+                    && getKey().equals(pair.getKey()))
+                    && (getValue() == pair.getValue() || getValue() != null
+                    && getValue().equals(pair.getValue()));
+        }
+
+        @Override
+        public int hashCode() {
+            return (getKey() == null ? 0 : getKey().hashCode() / 7)
+                    + (getValue() == null ? 0 : getValue().hashCode() / 7);
+        }
     }
 }
