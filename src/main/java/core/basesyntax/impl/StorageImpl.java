ca54214ff0e09e7f86862d0df425dc9a573ad403
Artem Fedoseev <afedoseev359@gmail.com>
2023-03-01 14:12:24 +0200

    Removed redundant default value for currentSize

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,10 +4,9 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
-    private static final int STARTING_SIZE = 0;
     private final K[] keys = (K[]) new Object[MAX_SIZE];
     private final V[] values = (V[]) new Object[MAX_SIZE];
-    private int currentSize = STARTING_SIZE;
+    private int currentSize;
 
     @Override
     public void put(K key, V value) {
