5498fbbfb4c4ac52cbef3ab3351973fce9b0aecc
Nataliia <ladna1984@gmail.com>
2023-03-04 23:44:51 +0200

    added new  private fields for arrays and a size. Implemented methods.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,37 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ARRAY_SIZE = 10;
+    private K[] keyArray = (K[]) new Object[MAX_ARRAY_SIZE];
+    private V[] valueArray = (V[]) new Object[MAX_ARRAY_SIZE];
+    private int size;
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (key == keyArray[i] || key != null && key.equals(keyArray[i])) {
+                valueArray[i] = value;
+                return;
+            }
+        }
+        keyArray[size] = key;
+        valueArray[size] = value;
+        size++;
+
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < MAX_ARRAY_SIZE; i++) {
+            if (key == keyArray[i] || key != null && key.equals(keyArray[i])) {
+                return valueArray[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
