dc9acc010eaa5f0c92a5532e2d7941eefde0f4a3
Lena <elenastepanenko@gmail.com>
2023-03-08 20:34:16 +0200

    HW-jv-storage-solved

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,51 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_AMOUNT = 10;
+    private int size = 0;
+    private K[] keys;
+    private V[] values;
+
+    public StorageImpl() {
+        this.keys = (K[]) new Object[MAX_AMOUNT];
+        this.values = (V[]) new Object[MAX_AMOUNT];
+    }
+
+    public boolean keyEquals(K keyFirst, K keySecond) {
+        if (keyFirst != null && keyFirst.equals(keySecond)) {
+            return true;
+        }
+        if (keyFirst == keySecond) {
+            return true;
+        }
+        return false;
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (keyEquals(key, keys[i])) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keyEquals(key, keys[i])) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
