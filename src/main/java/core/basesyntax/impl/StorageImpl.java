158ebcc58fb1e0e3be17b9e68871a10966bf304b
Oleksandr Fedchik <fed4iksasha@gmail.com>
2023-03-12 16:06:49 +0200

    fixed code and implement Storage by arrays without collections.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,32 +1,51 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private Map<K,V> map;
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private final K[] keyArray;
+    private final V[] valueArray;
+    private int itemsNumber = 0;
 
     public StorageImpl() {
-        this.map = new HashMap<>();
+        keyArray = (K[]) new Object[MAX_ITEMS_NUMBER];
+        valueArray = (V[]) new Object[MAX_ITEMS_NUMBER];
     }
 
     @Override
     public void put(K key, V value) {
+        if (putItemsCopy(key, value)) {
+            return;
+        }
+        keyArray[itemsNumber] = key;
+        valueArray[itemsNumber] = value;
+        itemsNumber++;
+    }
 
-        map.put(key, value);
+    private boolean putItemsCopy(K key, V value) {
+        for (int i = 0; i < itemsNumber; i++) {
+            if (Objects.equals(keyArray[i], key)) {
+                valueArray[i] = value;
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public V get(K key) {
-
-        return map.get(key);
+        for (int i = 0; i < itemsNumber; i++) {
+            if (Objects.equals(keyArray[i], key)) {
+                return valueArray[i];
+            }
+        }
+        return null;
     }
 
     @Override
     public int size() {
-
-        return map.size();
+        return itemsNumber;
     }
-
 }
