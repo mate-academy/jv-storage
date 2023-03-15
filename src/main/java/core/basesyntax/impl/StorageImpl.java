d016412c342a80de28fff6b98993527eb2fb7098
Anatolii Nazarov <anatoly_n@ukr.net>
2023-03-15 12:06:55 +0200

    Ready for check

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -17,36 +17,36 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public void put(K key, V value) {
         for (int i = 0; i < arrayKey.length; i++) {
             if (key == arrayKey[i] || key != null && key.equals(arrayKey[i])) {
-               if (key == null && arrayKey[i] == null && arrayValue[i] == null) {
-                   size++;
-               }
-               arrayValue[i] = value;
-               break;
+                if (key == null && arrayKey[i] == null && arrayValue[i] == null) {
+                    size++;
+                }
+                arrayValue[i] = value;
+                break;
             }
-           if (arrayKey[i] == null && arrayValue[i] == null) {
-               arrayKey[i] = key;
-               arrayValue[i] = value;
-               size++;
-               break;
-           }
-      }
+            if (arrayKey[i] == null && arrayValue[i] == null) {
+                arrayKey[i] = key;
+                arrayValue[i] = value;
+                size++;
+                break;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
-       for (int i = 0; i < arrayKey.length; i++) {
-           if (key == arrayKey[i] || key != null && key.equals(arrayKey[i])) {
-               return arrayValue[i];
-           }
-       }
-       return null;
+        for (int i = 0; i < arrayKey.length; i++) {
+            if (key == arrayKey[i] || key != null && key.equals(arrayKey[i])) {
+                return arrayValue[i];
+            }
+        }
+        return null;
     }
 
     @Override
     public int size() {
-       if (size > MAX_SIZE) {
-           return MAX_SIZE;
-       }
-       return size;
+        if (size > MAX_SIZE) {
+            return MAX_SIZE;
+        }
+        return size;
     }
 }
