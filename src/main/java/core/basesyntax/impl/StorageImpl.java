d4db0b5d61febc0a79b6fd4996f6f5795e5b1c15
Anatolii Nazarov <anatoly_n@ukr.net>
2023-03-15 11:50:24 +0200

    Ready for check

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,50 +3,50 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private int size;
     private static final int MAX_SIZE = 10;
+    private int size;
     private K[] arrayKey;
     private V[] arrayValue;
 
     public StorageImpl() {
-      arrayKey = (K[]) new Object[MAX_SIZE];
-      arrayValue = (V[]) new Object[MAX_SIZE];
+        arrayKey = (K[]) new Object[MAX_SIZE];
+        arrayValue = (V[]) new Object[MAX_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
-      for (int i = 0; i < arrayKey.length; i++) {
-        if (key == arrayKey[i] || key != null && key.equals(arrayKey[i]))  {
-          if (key == null && arrayKey[i] == null && arrayValue[i] == null) {
-            size++;
-          }
-          arrayValue[i] = value;
-          break;
-        }
-        if (arrayKey[i] == null && arrayValue[i] == null) {
-          arrayKey[i] = key;
-          arrayValue[i] = value;
-          size++;
-          break;
-        }
+        for (int i = 0; i < arrayKey.length; i++) {
+            if (key == arrayKey[i] || key != null && key.equals(arrayKey[i])) {
+               if (key == null && arrayKey[i] == null && arrayValue[i] == null) {
+                size++;
+               }
+               arrayValue[i] = value;
+               break;
+            }
+           if (arrayKey[i] == null && arrayValue[i] == null) {
+               arrayKey[i] = key;
+               arrayValue[i] = value;
+               size++;
+               break;
+           }
       }
     }
 
     @Override
     public V get(K key) {
-      for (int i = 0; i < arrayKey.length; i++) {
-        if (key == arrayKey[i] || key != null && key.equals(arrayKey[i])) {
-          return arrayValue[i];
-        }
-      }
-    return null;
+       for (int i = 0; i < arrayKey.length; i++) {
+           if (key == arrayKey[i] || key != null && key.equals(arrayKey[i])) {
+               return arrayValue[i];
+           }
+       }
+       return null;
     }
 
     @Override
     public int size() {
-      if (size > MAX_SIZE) {
-        return MAX_SIZE;
-      }
-      return size;
+       if (size > MAX_SIZE) {
+           return MAX_SIZE;
+       }
+       return size;
     }
 }
