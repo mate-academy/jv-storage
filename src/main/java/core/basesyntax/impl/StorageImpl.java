21146ae889e3320ef5fb627d49cd83732b122112
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-03-16 01:37:19 +0200

    added and tested solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,37 +3,44 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ARRAY_LENGTH = 10;
     private K key;
     private V value;
     private int sizeInt = 0;
 
-    public StorageImpl[] array =  new StorageImpl[10];
+    private ArrayRepresenting[] array = new ArrayRepresenting[MAX_ARRAY_LENGTH];
 
     @Override
     public void put(K key, V value) {
-        this.key = key;
-        this.value = value;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] != null && array[i].key == key) {
-                array[i] = this;
-                break;
+        boolean trigger = true;
+            for(int i = 0; i < array.length; i++) {
+                if (array[i] != null) {
+                    if ((key == null && this.array[i].keyJ == null) || (this.array[i].keyJ != null && array[i].keyJ.equals(key)))  {
+                        array[i] = new ArrayRepresenting<>(key, value);
+                        trigger = false;
+                        break;
+                    }
+                }
             }
-        }
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == null) {
-                array[i] = this;
-                sizeInt++;
-                break;
+            if (trigger == true) {
+                for (int i = 0; i < array.length; i++) {
+                    if (array[i] == null) {
+                        array[i] = new ArrayRepresenting<>(key, value);
+                        sizeInt++;
+                        break;
+                    }
+                }
             }
-        }
-
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < array.length; i++) {
-            if (array[i] != null && array[i].key == key) {
-                return (V) array[i].value;
+            if (array[i] != null && array[i].keyJ == null && key == null) {
+                return (V) array[i].valueB;
+            }
+            else if (array[i] != null && this.array[i].keyJ != null &&array[i].keyJ.equals(key)) {
+                return (V) array[i].valueB;
             }
         }
         return null;
@@ -43,4 +50,15 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public int size() {
         return sizeInt;
     }
+
 }
+class ArrayRepresenting<J,B> {
+     J keyJ;
+     B valueB;
+
+    public ArrayRepresenting(J keyJ, B valueB) {
+        this.keyJ = keyJ;
+        this.valueB = valueB;
+    }
+}
+
