a88976ad1f6d968d801b156cb6a5f90b80dee62d
Volodymyr Davidov <volodymyrdavidov1@gmail.com>
2023-03-17 16:52:47 +0200

    jv-storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,42 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private final int SIZE_OF_STORAGE = 10;
+    private final K[] keysOfStorage;
+    private final V[] valuesOfStorage;
+    private int sizeOfStorage;
+
+    public StorageImpl() {
+        this.keysOfStorage = (K[]) new Object[SIZE_OF_STORAGE];
+        this.valuesOfStorage = (V[]) new Object[SIZE_OF_STORAGE];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < sizeOfStorage;i++) {
+            if (key == keysOfStorage[i] || key != null && key.equals(keysOfStorage[i])) {
+                valuesOfStorage[i] = value;
+                return;
+            }
+
+        }
+        keysOfStorage[sizeOfStorage] = key;
+        valuesOfStorage[sizeOfStorage] = value;
+        sizeOfStorage++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < sizeOfStorage; i++) {
+            if (key == keysOfStorage[i] || key != null && key.equals(keysOfStorage[i])) {
+                return valuesOfStorage[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return sizeOfStorage;
     }
 }
