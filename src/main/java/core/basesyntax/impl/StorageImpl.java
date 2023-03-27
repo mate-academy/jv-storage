4d2e902aad603ffb4c4de6e866c9ee787daed94a
Artem <artempharm83@gmail.com>
2023-03-27 15:24:59 +0300

    added a new method public int getIndex This method returns the index of the match, or -1 if no match is found.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,35 +3,46 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int SIZE_ARRAY = 10;
+    private static final int SIZE = 10;
     private final K[] keys;
     private final V[] values;
     private int size;
 
     public StorageImpl() {
-        this.keys = (K[]) new Object[SIZE_ARRAY];
-        this.values = (V[]) new Object[SIZE_ARRAY];
+        this.keys = (K[]) new Object[SIZE];
+        this.values = (V[]) new Object[SIZE];
+    }
+
+    public int getIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < size; i++) {
-            if (key == keys[i] || key != null && key.equals(keys[i])) {
-                values[i] = value;
-                return;
+        int index = getIndex(key);
+        if (index != -1) {
+            values[index] = value;
+        } else {
+            if (size == SIZE) {
+                throw new RuntimeException("Storage is full");
             }
+            keys[size] = key;
+            values[size] = value;
+            size++;
         }
-        keys[size] = key;
-        values[size] = value;
-        size++;
+
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < size; i++) {
-            if (key == keys[i] || key != null && key.equals(keys[i])) {
-                return values[i];
-            }
+        int index = getIndex(key);
+        if (index != -1) {
+            return values[index];
         }
         return null;
     }
