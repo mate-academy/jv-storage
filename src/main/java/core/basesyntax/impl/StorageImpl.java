dd0b6279118d84fb034c6b61ebc8376985b69694
Artem <artempharm83@gmail.com>
2023-03-27 13:59:38 +0300

    Test failed set up JDK 11

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,14 +3,14 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int MAX_SIZE_ARRAY = 10;
+    private static final int SIZE_ARRAY = 10;
     private final K[] keys;
     private final V[] values;
     private int size;
 
     public StorageImpl() {
-        this.keys = (K[]) new Object[MAX_SIZE_ARRAY];
-        this.values = (V[]) new Object[MAX_SIZE_ARRAY];
+        this.keys = (K[]) new Object[SIZE_ARRAY];
+        this.values = (V[]) new Object[SIZE_ARRAY];
     }
 
     @Override
