c121737cbc82d85c37208a1df7c60be44cd6f7f4
Oles <a.vykhovanok@gmail.com>
2023-03-29 21:48:20 +0300

    solved task

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,23 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private final Map<K,V> map = new LinkedHashMap<>();
     @Override
     public void put(K key, V value) {
+        map.put(key, value);
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return map.get(key);
     }
 
     @Override
     public int size() {
-        return -1;
+        return map.size();
     }
 }
