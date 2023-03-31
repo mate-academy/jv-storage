00d409a291c803d9ee1ab00aaf5d7e6531f47976
Oles <a.vykhovanok@gmail.com>
2023-03-31 17:22:49 +0300

    fix checkstyle

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,24 +1,56 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private final Map<K,V> map = new LinkedHashMap<>();
+    private static final int MAX_NUMBER_OF_ELEMENTS = 10;
+    private final Node<K, V>[] nodeArray = new Node[MAX_NUMBER_OF_ELEMENTS];
+    private int size = 0;
+
+    private int getIndex(K key) {
+        int index = -1;
+        for (int i = 0; i < size; i++) {
+            if (key == nodeArray[i].key
+                || nodeArray[i].key != null && nodeArray[i].key.equals(key)) {
+                index = i;
+            }
+        }
+        return index;
+    }
 
     @Override
     public void put(K key, V value) {
-        map.put(key, value);
+        int index = getIndex(key);
+        if (index != -1) {
+            nodeArray[index].value = value;
+            return;
+        }
+        nodeArray[size] = new Node<>(key, value);
+        size++;
     }
 
     @Override
     public V get(K key) {
-        return map.get(key);
+        V value = null;
+        int index = getIndex(key);
+        if (index != -1) {
+            value = nodeArray[index].value;
+        }
+        return value;
     }
 
     @Override
     public int size() {
-        return map.size();
+        return size;
+    }
+
+    private static class Node<K, V> {
+        private final K key;
+        private V value;
+
+        private Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }
