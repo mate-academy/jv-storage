0fbf53502ea88aa6f3e8a2ee0989ea229a5e4532
Tim <tymurprokhorov@gmail.com>
2023-04-03 15:41:21 +0300

    Fixes provided. Redundant equals and hashCode removed, class Obq

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,39 +1,37 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Arrays;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
-    private int currentSize;
-    private Object[] keys;
-    private Object[] values;
+    private int size;
+    private K[] keys;
+    private V[] values;
 
     public StorageImpl() {
-        keys = new Object[MAX_SIZE];
-        values = new Object[MAX_SIZE];
+        keys = (K[]) new Object[MAX_SIZE];
+        values = (V[]) new Object[MAX_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
         sizeCheck();
-        for (int i = 0; i < currentSize; i++) {
-            if (Objects.equals(keys[i], key)) {
+        for (int i = 0; i < size; i++) {
+            if ((keys[i] == key) || (keys[i] != null && keys[i].equals(key))) {
                 values[i] = value;
                 return;
             }
         }
-        keys[currentSize] = key;
-        values[currentSize] = value;
-        currentSize++;
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
         V value = null;
-        for (int i = 0; i < currentSize; i++) {
-            if (Objects.equals(keys[i], key)) {
+        for (int i = 0; i < size; i++) {
+            if ((keys[i] == key) || (keys[i] != null && keys[i].equals(key))) {
                 value = (V) values[i];
             }
         }
@@ -42,35 +40,13 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return currentSize;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        StorageImpl<?, ?> storage = (StorageImpl<?, ?>) o;
-        return currentSize == storage.currentSize
-                && Arrays.equals(keys, storage.keys)
-                && Arrays.equals(values, storage.values);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Objects.hash(currentSize);
-        result = 31 * result + Arrays.hashCode(keys);
-        result = 31 * result + Arrays.hashCode(values);
-        return result;
+        return size;
     }
 
     private void sizeCheck() {
-        if (currentSize <= MAX_SIZE) {
+        if (size <= MAX_SIZE) {
             return;
         }
-        throw new ArrayIndexOutOfBoundsException("Error. Storage is full.");
+        throw new ArrayIndexOutOfBoundsException("Error. Storage is full. Size: " + size);
     }
 }
