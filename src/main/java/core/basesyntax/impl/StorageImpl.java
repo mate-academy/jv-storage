42a38d96be6dea0b3d0ffc1c11fd41ecb898d758
Bakhmet Ivan <bahmetvanya1290@gmail.com>
2023-04-04 21:57:13 +0300

    fixed problems

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,25 +4,24 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ARRAY_LENGTH = 10;
-    private Object[] keysArr;
-    private Object[] valuesArr;
-    private int lastArrayIndex;
+    private K[] keys;
+    private V[] values;
+    private int size;
 
     public StorageImpl() {
-        keysArr = new Object[MAX_ARRAY_LENGTH];
-        valuesArr = new Object[MAX_ARRAY_LENGTH];
-        lastArrayIndex = 0;
+        keys = (K[])new Object[MAX_ARRAY_LENGTH];
+        values = (V[])new Object[MAX_ARRAY_LENGTH];
     }
 
     @Override
     public void put(K key, V value) {
         int index = getIndex(key);
         if (index == -1) {
-            keysArr[lastArrayIndex] = key;
-            valuesArr[lastArrayIndex] = value;
-            lastArrayIndex++;
+            keys[size] = key;
+            values[size] = value;
+            size++;
         } else {
-            valuesArr[index] = value;
+            values[index] = value;
         }
     }
 
@@ -32,22 +31,22 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         if (index == -1) {
             return null;
         } else {
-            V searchResult = (V) valuesArr[index];
+            V searchResult = (V) values[index];
             return searchResult;
         }
     }
 
+    @Override
+    public int size() {
+        return size;
+    }
+
     private int getIndex(K key) {
-        for (int i = 0; i < lastArrayIndex; i++) {
-            if (keysArr[i] == key || keysArr[i] != null && keysArr[i].equals(key)) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
                 return i;
             }
         }
         return -1;
-    }
-
-    @Override
-    public int size() {
-        return lastArrayIndex;
     }
 }
