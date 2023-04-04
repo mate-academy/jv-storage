3fd3d7bbc3d3e62d8932e5f6090f1e47d5d114fe
Sergey Shestaka <sergey.shestaka@gmail.com>
2023-04-04 13:59:57 +0200

    Defined the storage size (10) in the separate constant value / All non-constant fields initialized in the constructor / Removed all System.out.println() methods

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,8 +4,9 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
-    private int storageIndex = 0;
-    private final Pair<K, V>[] storageArray = new Pair[10];
+    private static final int STORAGE_SIZE = 10;
+    private int storageIndex;
+    private final Pair<K, V>[] storageArray;
 
     private static class Pair<K, V> {
         private K key;
@@ -18,23 +19,18 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     }
 
     public StorageImpl() {
+        storageIndex = 0;
+        storageArray = new Pair[STORAGE_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
         Pair<K, V> newObject = new Pair<>(key, value);
         int maxElementNumber = 10;
-        if (storageIndex == maxElementNumber) {
-            System.out.println("Can't add new element. There is no free space");
-            return;
-        }
         for (int i = 0; i < storageIndex; i++) {
             if ((storageArray[i] != null) && ((storageArray[i].key == key)
                     || (storageArray[i].key != null
                     && storageArray[i].key.equals(key)))) {
-                System.out.println("kArray["
-                        + i + "] is " + storageArray[i].key
-                        + " and is equals with key " + key);
                 storageArray[i].value = value;
                 return;
             }
@@ -48,8 +44,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         for (Pair<K, V> si : storageArray) {
             if ((si != null) && ((si.key == key) || (si.key != null
                     && si.key.equals(key)))) {
-                System.out.println("with key \""
-                        + key + "\" value is \"" + si.value + "\"");
                 return si.value;
             }
         }
@@ -65,16 +59,5 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             }
         }
         return arraySize;
-    }
-
-    @Override
-    public String toString() {
-        for (int i = 0; i < storageArray.length; i++) {
-            if (storageArray[i] != null) {
-                System.out.println("kArray [" + i + "] = "
-                        + storageArray[i].key + " " + storageArray[i].value);
-            }
-        }
-        return "";
     }
 }
