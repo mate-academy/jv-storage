00f47d6fefbac01088f14d53a4744f0ef9ead5cc
Artem Kostenko <a_kostenco@yahoo.com>
2023-04-08 18:14:49 +0300

    fix after mentor review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,43 +4,37 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private Pair<K, V>[] storage;
+    private int indexFreePosition;
 
     public StorageImpl() {
-        storage = new Pair[0];
+        storage = new Pair[10];
+        indexFreePosition = 0;
     }
 
     @Override
     public void put(K key, V value) {
-        int lengthStorage = storage.length;
-        for (int i = 0; i < lengthStorage; i++) {
-            if (storage[i].getKey() == null && key == null) {
-                storage[i].setValue(value);
-                return;
-            } else if (storage[i].getKey() == null || key == null) {
-                continue;
-            }
-            if (storage[i].getKey().equals(key)) {
+        for (int i = 0; i < indexFreePosition; i++) {
+            if (storage[i].getKey() == key
+                    || storage[i].getKey() != null
+                    && storage[i].getKey().equals(key)) {
                 storage[i].setValue(value);
                 return;
             }
+            if (storage[i] == null) {
+                break;
+            }
         }
-        Pair<K, V>[] localStorage = new Pair[lengthStorage + 1];
-        for (int i = 0; i < lengthStorage; i++) {
-            localStorage[i] = storage[i];
-        }
-        localStorage[lengthStorage] = new Pair<>(key, value);
-        storage = localStorage;
+
+        storage[indexFreePosition] = new Pair<>(key, value);
+        indexFreePosition++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i].getKey() == null && key == null) {
-                return storage[i].getValue();
-            } else if (storage[i].getKey() == null || key == null) {
-                continue;
-            }
-            if (storage[i].getKey().equals(key)) {
+        for (int i = 0; i < indexFreePosition; i++) {
+            if (storage[i].getKey() == key
+                    || storage[i].getKey() != null
+                    && storage[i].getKey().equals(key)) {
                 return storage[i].getValue();
             }
         }
@@ -49,6 +43,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return storage.length;
+        return indexFreePosition;
     }
 }
