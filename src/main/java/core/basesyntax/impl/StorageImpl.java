504e6b3381c6dca7cd6695b05c3a25b96127cd7a
Serhii Serdiuk <serhii.serdiuk@gmail.com>
2023-04-13 01:15:00 +0200

    Solving the task

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,43 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_STORAGE_SIZE = 10;
+    private int size;
+    private final Pair<K, V>[] storage;
+
+    public StorageImpl() {
+        size = 0;
+        storage = new Pair[MAX_STORAGE_SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        Pair<K, V> newPair = new Pair<>(key, value);
+        for (int i = 0; i < size; i++) {
+            if ((storage[i] != null) && (storage[i].getKey() == key
+                    || storage[i].getKey() != null && storage[i].getKey().equals(key))) {
+                storage[i].setValue(value);
+                return;
+            }
+        }
+        storage[size] = newPair;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (Pair<K, V> pair : storage) {
+            if (pair != null) {
+                if (pair.getKey() == key || (pair.getKey() != null && pair.getKey().equals(key))) {
+                    return pair.getValue();
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
