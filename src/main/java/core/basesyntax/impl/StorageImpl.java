c065b2d9786809c3bd6c4bc80c85f4cab79dfe8d
Ros <rospsix@gmail.com>
2023-04-29 16:09:03 +0100

    Created two arrays and size variable : First array "keys" store keys value; Second array "values" store value; I use "size" variable to check how many keys store in array
    Override equals method to check does the key exist in the keys array.If the key has been found, I replace its value in the values array.
    
    If the key is not present, I add a new key and its corresponding value to the end of the keys and values arrays, using the variable size to determine the position in the array.
    
    In the "get()" method, I locate the key in the keys array in the same way as in the "put()" method, and return the corresponding value from the values array for the found key.
    
    In the size method, I return size.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,80 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[10];
+        values = (V[]) new Object[10];
+        size = 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        StorageImpl current = (StorageImpl) o;
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(keys[i], current.keys[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(size);
+        result = 31 * result + Arrays.hashCode(keys);
+        result = 31 * result + Arrays.hashCode(values);
+        return result;
+    }
+
     @Override
     public void put(K key, V value) {
+        boolean found = false;
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(keys[i], key)) {
+                values[i] = value;
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
+        if (size == keys.length) {
+            keys = Arrays.copyOf(keys, size * 2);
+            values = Arrays.copyOf(values, size * 2);
+        }
+        keys[size] = key;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(keys[i], key)) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
+
 }
