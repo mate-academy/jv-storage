fd9f098067fb6893f7e5941aaf0bacc3580f0af0
ASnazar <36624661+ASnazar@users.noreply.github.com>
2023-04-30 17:26:28 +0300

    Done task

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,51 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private static final int START_ITEMS_NUMBER = -1;
+    private final Object[] keys;
+    private final Object[] values;
+    private int occupiedValues;
+
+    public StorageImpl() {
+        keys = new Object[MAX_ITEMS_NUMBER];
+        values = new Object[MAX_ITEMS_NUMBER];
+        occupiedValues = START_ITEMS_NUMBER;
+    }
+
     @Override
     public void put(K key, V value) {
+        if (checkKey(key,value)) {
+            return;
+        } else if (occupiedValues < MAX_ITEMS_NUMBER) {
+            occupiedValues++;
+            keys[occupiedValues] = key;
+            values[occupiedValues] = value;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i <= occupiedValues; i++) {
+            if (keys[i] != null && keys[i].equals(key) || key == keys[i]) {
+                return (V)values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return occupiedValues - START_ITEMS_NUMBER;
+    }
+
+    private boolean checkKey(K keyForCheck, V value) {
+        for (int i = 0; i <= occupiedValues; i++) {
+            if (keys[i] != null && keys[i].equals(keyForCheck) || keyForCheck == keys[i]) {
+                values[i] = value;
+                return true;
+            }
+        }
+        return false;
     }
 }
