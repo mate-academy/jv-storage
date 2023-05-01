24e479f03417c135f4ce2667e0d9f8061754eecc
Ros <rospsix@gmail.com>
2023-05-01 20:12:01 +0100

    The code has been fixed in two methods: put() and get(). Now the code compares keys by value, not by whether they are equal to null.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -26,29 +26,15 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     }
 
     public void put(K key, V value) {
-        boolean found = false;
-        if (key == null) {
-            for (int i = 0; i < size; i++) {
-                if (keys[i] == null) {
-                    values[i] = value;
-                    found = true;
-                    break;
-                }
-            }
-        } else {
-            for (int i = 0; i < size; i++) {
-                if (key.equals(keys[i])) {
-                    values[i] = value;
-                    found = true;
-                    break;
-                }
+        for (int i = 0; i < size; i++) {
+            if ((key == keys[i]) || (key != null && key.equals(keys[i]))) {
+                values[i] = value;
+                return;
             }
         }
-        if (!found) {
-            keys[size] = key;
-            values[size] = value;
-            size++;
-        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
         if (size == keys.length) {
             keys = Arrays.copyOf(keys, size * ARRAY_MULTIPLIER);
             values = Arrays.copyOf(values, size * ARRAY_MULTIPLIER);
@@ -56,17 +42,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     }
 
     public V get(K key) {
-        if (key == null) {
-            for (int i = 0; i < size; i++) {
-                if (keys[i] == null) {
-                    return values[i];
-                }
-            }
-        } else {
-            for (int i = 0; i < size; i++) {
-                if (key.equals(keys[i])) {
-                    return values[i];
-                }
+        for (int i = 0; i < size; i++) {
+            if ((key == keys[i]) || (key != null && key.equals(keys[i]))) {
+                return values[i];
             }
         }
         return null;
@@ -76,5 +54,4 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public int size() {
         return size;
     }
-
 }
