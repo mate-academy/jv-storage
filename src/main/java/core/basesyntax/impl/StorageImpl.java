d92288c46bc88c1e00786aab2728d7fa7af3f76c
Anastasiia Variichuk <stacyvariuchuk@gmail.com>
2023-05-02 10:17:58 +0300

    fixed solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,22 +5,28 @@ import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ARRAY_SIZE = 10;
-    private final K[] keysArray = (K[]) new Object[MAX_ARRAY_SIZE];
-    private final V[] valuesArray = (V[]) new Object[MAX_ARRAY_SIZE];
+    private final K[] keysArray;
+    private final V[] valuesArray;
     private int size;
 
     public StorageImpl() {
+        keysArray = (K[]) new Object[MAX_ARRAY_SIZE];
+        valuesArray = (V[]) new Object[MAX_ARRAY_SIZE];
+    }
+
+    private boolean isEqualsKeys(Object first, Object second) {
+        return (first == second) || (first != null && first.equals(second));
     }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < keysArray.length; i++) {
             if (keysArray[i] == null && valuesArray[i] == null) {
-                size++;
+                size = size() + 1;
                 keysArray[i] = key;
                 valuesArray[i] = value;
                 return;
-            } else if (Objects.equals(key, keysArray[i])) {
+            } else if (isEqualsKeys(keysArray[i], key)){
                 valuesArray[i] = value;
                 return;
             }
