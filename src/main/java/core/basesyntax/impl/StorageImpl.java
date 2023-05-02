58c8b5457fc28519e0b30c0b38a03aed3d08e363
ASnazar <36624661+ASnazar@users.noreply.github.com>
2023-05-02 15:32:14 +0300

    Fix all problem after Review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,35 +4,30 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ITEMS_NUMBER = 10;
-    private static final int START_ITEMS_NUMBER = -1;
-    private static final int START_SIZE_NUMBER = 0;
     private final Object[] keys;
     private final Object[] values;
-    private int occupiedValues;
     private int size;
 
     public StorageImpl() {
         keys = new Object[MAX_ITEMS_NUMBER];
         values = new Object[MAX_ITEMS_NUMBER];
-        occupiedValues = START_ITEMS_NUMBER;
-        size = START_SIZE_NUMBER;
     }
 
     @Override
     public void put(K key, V value) {
-        if (checkKey(key, value)) {
-            return;
-        } else if (occupiedValues < MAX_ITEMS_NUMBER) {
-            occupiedValues++;
+        int keyForReplace;
+        if ((keyForReplace = getExistKey(key)) != -1) {
+            replaceElement(value,keyForReplace);
+        } else if (size <= MAX_ITEMS_NUMBER) {
+            keys[size] = key;
+            values[size] = value;
             size++;
-            keys[occupiedValues] = key;
-            values[occupiedValues] = value;
         }
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i <= occupiedValues; i++) {
+        for (int i = 0; i <= size; i++) {
             if (check(key,keys[i])) {
                 return (V)values[i];
             }
@@ -45,14 +40,17 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         return size;
     }
 
-    private boolean checkKey(K keyForCheck, V value) {
-        for (int i = 0; i <= occupiedValues; i++) {
+    private int getExistKey(K keyForCheck) {
+        for (int i = 0; i < size; i++) {
             if (check(keyForCheck,keys[i])) {
-                values[i] = value;
-                return true;
+                return i;
             }
         }
-        return false;
+        return -1;
+    }
+
+    private void replaceElement(V value,int index) {
+        values[index] = value;
     }
 
     private boolean check(K keyForCheck, Object value) {
