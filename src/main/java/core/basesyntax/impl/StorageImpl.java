cf1381b134791cacd69f4c465d0fe93804452c99
Petro Kobevko <kobevkopi@gmail.com>
2023-05-03 01:35:30 +0200

    Added tast realization

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,44 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAXIMUM_NUMBER_OF_ELEMENTS = 10;
+    private static final int INITIAL_AMOUNT_OF_ELEMENTS = 0;
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAXIMUM_NUMBER_OF_ELEMENTS];
+        values = (V[]) new Object[MAXIMUM_NUMBER_OF_ELEMENTS];
+        size = INITIAL_AMOUNT_OF_ELEMENTS;
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if ((key == null && keys[i] == null) || (keys[i] != null && keys[i].equals(key))) {
+                keys[i] = key;
+                values[i] = value;
+                return;
+            }
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if ((key == null && keys[i] == null) || (keys[i] != null && keys[i].equals(key))) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
