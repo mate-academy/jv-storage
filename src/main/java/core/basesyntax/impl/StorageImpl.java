281772fb4d698d66c4353070d61123b867de7fd7
Mykyta Naumov <nneed4use@gmail.com>
2023-05-03 00:52:58 +0300

    Code was optimised as much as possible for me, tests passed, checkstyle done

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,10 +4,10 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
-    private Object[] keys;
-    private Object[] values;
+    private static final int INDEX_OF_NOTHING = -1;
+    private final Object[] keys;
+    private final Object[] values;
     private int size;
-    private boolean isKeyEqual;
 
     public StorageImpl() {
         keys = new Object[MAX_SIZE];
@@ -17,45 +17,19 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        int index = getKeyIndex(key);
         if (get(key) != null) {
-            values[index] = value;
-            keys[index] = key;
-            return;
+            reWrite(key, value);
         } else {
-            for (int i = 0; i < values.length; i++) {
-                if (keys[i] == null && values[i] == null) {
-                    values[i] = value;
-                    keys[i] = key;
-                    size++;
-                    return;
-                }
-            }
+            newWrite(key, value);
         }
     }
-
-
-        /*for (int i = 0; i < values.length; i++) {
-            if (keys[i] != null && keys[i].equals(key)
-                    || (key == null && keys[i] == null)) {
-                values[i] = value;
-                keys[i] = key;
-                return;
-            } else if (keys[i] == null && values[i] == null) {
-                values[i] = value;
-                keys[i] = key;
-                size++;
-                return;
-            }
-        }
-    }*/
 
     @SuppressWarnings("unchecked")
     @Override
     public V get(K key) {
         int index = getKeyIndex(key);
-        return (index != -1) ?
-                (V) values[index] : null;
+        return (index != INDEX_OF_NOTHING)
+                ? (V) values[index] : null;
     }
 
     @Override
@@ -70,15 +44,23 @@ public class StorageImpl<K, V> implements Storage<K, V> {
                 return i;
             }
         }
-        return -1;
+        return INDEX_OF_NOTHING;
     }
 
-    /*public int getNullKeyIndex(K key) {
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i] == null) {
-                return i;
+    public void newWrite(K key, V value) {
+        for (int i = 0; i < values.length; i++) {
+            if (keys[i] == null && values[i] == null) {
+                values[i] = value;
+                keys[i] = key;
+                size++;
+                return;
             }
         }
-        return -1;
-    }*/
+    }
+
+    public void reWrite(K key, V value) {
+        int index = getKeyIndex(key);
+        values[index] = value;
+        keys[index] = key;
+    }
 }
