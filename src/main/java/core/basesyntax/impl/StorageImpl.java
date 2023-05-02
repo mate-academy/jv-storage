0241882c70193680fe13c6456684988895011aa9
Maryna Hryshchenko <mari.gryshchenko54@gmail.com>
2023-05-02 09:59:21 +0300

    changes after review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,26 +4,26 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ELEMENTS_IN_STORAGE = 10;
-    private final K[] key;
-    private final V[] value;
+    private final K[] keys;
+    private final V[] values;
     private int size;
 
     public StorageImpl() {
-        this.key = (K[]) new Object[MAX_ELEMENTS_IN_STORAGE];
-        this.value = (V[]) new Object[MAX_ELEMENTS_IN_STORAGE];
+        this.keys = (K[]) new Object[MAX_ELEMENTS_IN_STORAGE];
+        this.values = (V[]) new Object[MAX_ELEMENTS_IN_STORAGE];
     }
 
     @Override
     public void put(K key, V value) {
         int index = indexElement(key);
         if (index >= 0) {
-            this.value[index] = value;
+            values[index] = value;
         } else {
-            if (size == this.key.length) {
-                throw new OutOfMemoryError("Storage is completely full");
+            if (size == keys.length) {
+                throw new RuntimeException("Storage is completely full");
             }
-            this.key[size] = key;
-            this.value[size] = value;
+            keys[size] = key;
+            values[size] = value;
             size++;
         }
     }
@@ -31,11 +31,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         int index = indexElement(key);
-        if (index >= 0) {
-            return value[index];
-        } else {
-            return null;
-        }
+        return index >= 0 ? values[index] : null;
+
     }
 
     @Override
@@ -45,9 +42,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     private int indexElement(K key) {
         for (int i = 0; i < size; i++) {
-            if (this.key[i] != null && this.key[i].equals(key)) {
-                return i;
-            } else if (key == null && this.key[i] == null) {
+            if ((keys[i] != null && keys[i].equals(key)) || (key == null && keys[i] == null)) {
                 return i;
             }
         }
