89849aa1d07298a8bdcf9a297fc32d8c8ffeee25
Anastasiia Variichuk <stacyvariuchuk@gmail.com>
2023-05-02 17:13:51 +0300

    fixed solution 3.0

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,25 +4,25 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ARRAY_SIZE = 10;
-    private final K[] keysArray;
-    private final V[] valuesArray;
+    private final K[] keys;
+    private final V[] values;
     private int size;
 
     public StorageImpl() {
-        keysArray = (K[]) new Object[MAX_ARRAY_SIZE];
-        valuesArray = (V[]) new Object[MAX_ARRAY_SIZE];
+        keys = (K[]) new Object[MAX_ARRAY_SIZE];
+        values = (V[]) new Object[MAX_ARRAY_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < keysArray.length; i++) {
-            if (keysArray[i] == null && valuesArray[i] == null) {
-                size = size() + 1;
-                keysArray[i] = key;
-                valuesArray[i] = value;
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] == null && values[i] == null) {
+                keys[i] = key;
+                values[i] = value;
+                size++;
                 return;
-            } else if (isEqualsKeys(keysArray[i], key)) {
-                valuesArray[i] = value;
+            } else if (areEqualsKeys(keys[i], key)) {
+                values[i] = value;
                 return;
             }
         }
@@ -30,9 +30,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keysArray.length; i++) {
-            if (isEqualsKeys(key, keysArray[i])) {
-                return valuesArray[i];
+        for (int i = 0; i < keys.length; i++) {
+            if (areEqualsKeys(key, keys[i])) {
+                return values[i];
             }
         }
         return null;
@@ -43,7 +43,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         return size;
     }
 
-    private boolean isEqualsKeys(K first, K second) {
+    private boolean areEqualsKeys(K first, K second) {
         return first == second || first != null && first.equals(second);
     }
 }
