b08988337d849fbc643af5471e3e92dfd34e38e8
Mykyta Naumov <nneed4use@gmail.com>
2023-05-03 00:13:59 +0300

    Finally it's working, after 8 hours of constant brainstorm

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,82 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    @Override
-    public void put(K key, V value) {
+    private static final int MAX_SIZE = 10;
+    private Object[] keys;
+    private Object[] values;
+    private int size;
+    private boolean isKeyEqual;
+
+    public StorageImpl() {
+        keys = new Object[MAX_SIZE];
+        values = new Object[MAX_SIZE];
+        size = 0;
     }
 
     @Override
+    public void put(K key, V value) {
+        int index = getKeyIndex(key);
+        if (get(key) != null) {
+            values[index] = value;
+            keys[index] = key;
+            return;
+        } else {
+            for (int i = 0; i < values.length; i++) {
+                if (keys[i] == null && values[i] == null) {
+                    values[i] = value;
+                    keys[i] = key;
+                    size++;
+                    return;
+                }
+            }
+        }
+    }
+
+
+        /*for (int i = 0; i < values.length; i++) {
+            if (keys[i] != null && keys[i].equals(key)
+                    || (key == null && keys[i] == null)) {
+                values[i] = value;
+                keys[i] = key;
+                return;
+            } else if (keys[i] == null && values[i] == null) {
+                values[i] = value;
+                keys[i] = key;
+                size++;
+                return;
+            }
+        }
+    }*/
+
+    @SuppressWarnings("unchecked")
+    @Override
     public V get(K key) {
-        return null;
+        int index = getKeyIndex(key);
+        return (index != -1) ?
+                (V) values[index] : null;
     }
 
     @Override
     public int size() {
+        return size;
+    }
+
+    public int getKeyIndex(K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if ((key == null && keys[i] == null)
+                    || (keys[i] != null && keys[i].equals(key))) {
+                return i;
+            }
+        }
         return -1;
     }
+
+    /*public int getNullKeyIndex(K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] == null) {
+                return i;
+            }
+        }
+        return -1;
+    }*/
 }
