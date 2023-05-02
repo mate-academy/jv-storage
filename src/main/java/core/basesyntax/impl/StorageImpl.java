685e8fd3b47489badc68ff889fabae2bb56d9012
Yaroslav Khmaruk <yarik9656422@gmail.com>
2023-05-02 17:19:04 +0300

    added fixes after student review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,14 +4,13 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_NUMBER_OF_ARRAY_LENGTH = 10;
+    private int size;
     private final K[] keys;
     private final V[] values;
-    private int size;
 
     public StorageImpl() {
         keys = (K[]) new Object[MAX_NUMBER_OF_ARRAY_LENGTH];
         values = (V[]) new Object[MAX_NUMBER_OF_ARRAY_LENGTH];
-        size = 0;
     }
 
     @Override
@@ -40,12 +39,12 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         return null;
     }
 
-    private boolean isKeyEquals(K firstKey, K secondKey) {
-        return firstKey == secondKey || (firstKey != null) && firstKey.equals(secondKey);
-    }
-
     @Override
     public int size() {
         return size;
+    }
+
+    private boolean isKeyEquals(K firstKey, K secondKey) {
+        return firstKey == secondKey || (firstKey != null) && firstKey.equals(secondKey);
     }
 }
