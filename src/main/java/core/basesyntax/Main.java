63d1be1610653b00a41261fa1c823636ca35c0a7
Petro Kobevko <kobevkopi@gmail.com>
2023-05-03 01:55:41 +0200

    Deleted redundant Main class

--- src/main/java/core/basesyntax/Main.java
+++
@@ -1,9 +0,0 @@
-package core.basesyntax;
-
-import core.basesyntax.impl.StorageImpl;
-
-public class Main {
-    public static void main(String[] args) {
-        Storage<Integer, String> storage = new StorageImpl<>();
-    }
-}
