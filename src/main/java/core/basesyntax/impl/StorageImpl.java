491c7adc9da7a37d8cb012fb1630b008573a6cfd
Viktoriia Semenchenko <vickysemenchenko@gmail.com>
2023-05-02 12:47:07 +0300

    Revert "fixed mistake in compare() method"
    This reverts commit 6eb2d923081c1b7b73ff5847431dcccfca1bdbef.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,6 +1,7 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_LENGTH = 10;
