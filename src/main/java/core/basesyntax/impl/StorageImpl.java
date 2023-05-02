b133df0ba97f43984143626a7f20454d3d264c75
viprko <pr.and.vi@gmail.com>
2023-05-02 18:27:35 +0300

    Fixed issues with naming and useless method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,27 +3,34 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    public static final int MAX_ARRAY_LENGTH = 10;
-    public static final int START_INDEX = 0;
-    private final K[] keys = (K[]) new Object[MAX_ARRAY_LENGTH];
-    private final V[] values = (V[]) new Object[MAX_ARRAY_LENGTH];
+    private static final int MAX_ARRAY_LENGTH = 10;
+    private static final int START_INDEX = 0;
+    private static final int NO_MATCH_INDEX = -1;
+    private final K[] keys;
+    private final V[] values;
     private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_ARRAY_LENGTH];
+        values = (V[]) new Object[MAX_ARRAY_LENGTH];
+    }
 
     @Override
     public void put(K key, V value) {
-        int index = indexOfProperCell(key);
-        if (index == -1) {
+        int index = getIndexForPutting(key);
+        if (index == NO_MATCH_INDEX) {
             keys[size] = key;
             values[size] = value;
             size++;
-            return;
+        } else {
+            values[index] = value;
         }
-        values[index] = value;
     }
 
     @Override
     public V get(K key) {
-        return (V) lookingForKeyValue(key);
+        return getIndexForPutting(key) == NO_MATCH_INDEX ? null
+                : values[getIndexForPutting(key)];
     }
 
     @Override
@@ -31,18 +38,14 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         return size;
     }
 
-    private int indexOfProperCell(K key) {
+    private int getIndexForPutting(K key) {
         for (int i = START_INDEX; i < size; i++) {
             if ((key == keys[i]) || key != null && key.equals(keys[i])) {
                 return i;
             }
         }
-        return -1;
+        return NO_MATCH_INDEX;
     }
-
-    private Object lookingForKeyValue(K key) {
-        return indexOfProperCell(key) == -1 ? null
-                : values[indexOfProperCell(key)];
-    }
-
 }
+
+
