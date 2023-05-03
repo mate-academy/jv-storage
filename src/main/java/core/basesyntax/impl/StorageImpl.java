e0815dca0e4dbc94f2ccec9295ed6580e6401c51
Petro Kobevko <kobevkopi@gmail.com>
2023-05-03 10:47:57 +0200

    Fixed code according to the mentors comments

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -18,7 +18,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if ((key == null && keys[i] == null) || (keys[i] != null && keys[i].equals(key))) {
+            if (keysAreNullsOrEquals(key, keys[i])) {
                 keys[i] = key;
                 values[i] = value;
                 return;
@@ -32,11 +32,15 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if ((key == null && keys[i] == null) || (keys[i] != null && keys[i].equals(key))) {
+            if (keysAreNullsOrEquals(key, keys[i])) {
                 return values[i];
             }
         }
         return null;
+    }
+
+    private boolean keysAreNullsOrEquals(K key1, K key2) {
+        return (key1 == null && key2 == null) || (key2 != null && key2.equals(key1));
     }
 
     @Override
