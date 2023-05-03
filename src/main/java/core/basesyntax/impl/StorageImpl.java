a7f313490719d02ef03f88573b7c1c54dd5ddd81
Petro Kobevko <kobevkopi@gmail.com>
2023-05-03 11:17:46 +0200

    Fixed keyAreEquals method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -16,7 +16,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (keysAreNullsOrEquals(key, keys[i])) {
+            if (keysAreEquals(key, keys[i])) {
                 keys[i] = key;
                 values[i] = value;
                 return;
@@ -30,15 +30,15 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (keysAreNullsOrEquals(key, keys[i])) {
+            if (keysAreEquals(key, keys[i])) {
                 return values[i];
             }
         }
         return null;
     }
 
-    private boolean keysAreNullsOrEquals(K key1, K key2) {
-        return (key1 == null && key2 == null) || (key2 != null && key2.equals(key1));
+    private boolean keysAreEquals(K key1, K key2) {
+        return (key1 == key2) || (key2 != null && key2.equals(key1));
     }
 
     @Override
