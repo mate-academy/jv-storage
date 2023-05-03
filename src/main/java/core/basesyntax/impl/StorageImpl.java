96349b10bc4e0ebac452552a37fcd6d464cd941c
Arina Polna <polna.arina@gmail.com>
2023-05-03 08:52:59 +0300

    changed values and keys arrays type and parameters for equals method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,20 +4,20 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ITEM_SIZE = 10;
-    private Object[] keys;
-    private Object[] values;
+    private K[] keys;
+    private V[] values;
     private int size;
     private int index;
 
     public StorageImpl() {
-        keys = new Object[MAX_ITEM_SIZE];
-        values = new Object[MAX_ITEM_SIZE];
+        keys = (K[]) new Object[MAX_ITEM_SIZE];
+        values = (V[]) new Object[MAX_ITEM_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (equalsObjects(keys[i], key)) {
+            if (equalsKeys(keys[i], key)) {
                 values[i] = value;
                 return;
             }
@@ -32,7 +32,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (equalsObjects(keys[i], key)) {
+            if (equalsKeys(keys[i], key)) {
                 return (V) values[i];
             }
         }
@@ -44,7 +44,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         return size;
     }
 
-    private boolean equalsObjects(Object first, Object second) {
+    private boolean equalsKeys(K first, K second) {
         return (first == second || (first != null && first.equals(second)));
     }
 }
