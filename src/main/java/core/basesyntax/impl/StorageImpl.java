9ab9cca8a865714980ee34e7e6037bff1ffc8ed7
Mykyta Oleksin <32339836+nacenik@users.noreply.github.com>
2023-05-12 10:35:20 +0300

    Update ci.yml to java 17

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,50 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE_ARRAY = 10;
+    private K[] keysArray;
+    private V[] valuesArray;
+    private int size;
+
+    public StorageImpl() {
+        this.keysArray = (K[]) new Object[MAX_SIZE_ARRAY];
+        this.valuesArray = (V[]) new Object[MAX_SIZE_ARRAY];
+        size = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (isequals(keysArray[i],key)) {
+                valuesArray[i] = value;
+                return;
+            }
+        }
+        if (size < MAX_SIZE_ARRAY) {
+            keysArray[size] = key;
+            valuesArray[size] = value;
+            size++;
+        } else {
+            throw new RuntimeException("Storage is full");
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (isequals(keysArray[i], key)) {
+                return valuesArray[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    private boolean isequals(Object obj1, Object obj2) {
+        return obj2 == obj1 || obj2 != null && obj2.equals(obj1);
     }
 }
