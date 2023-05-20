87ebcfb6fcc29e8ab53c14d955760828ac7d650b
pochtalon <pochtalon1010@gmail.com>
2023-05-20 13:38:26 +0300

    Created Pair class Created 2 constants, variables Pair[] and elementsInArray in StorageImpl Implemented methods of Storage interface Created methods isKeyEquals and getPosition

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,51 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int arraySize = 10;
+    private static final int empty = 0;
+    private Pair[] store;
+    private int elementsInArray;
+
+    public StorageImpl() {
+        store = new Pair[arraySize];
+        elementsInArray = empty;
+    }
+
     @Override
     public void put(K key, V value) {
+        int position = getPosition(key);
+        store[position] = new Pair<>(key, value);
+        if (position == elementsInArray) {
+            elementsInArray++;
+        }
+    }
+
+    private int getPosition (K key) {
+        if (elementsInArray == empty) {return empty;}
+        for (int i = 0; i < elementsInArray; i++) {
+            if (isKeyEquals((K) store[i].getKey(), key)) {
+                return i;
+            }
+        }
+        return elementsInArray;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < elementsInArray; i++) {
+            if (isKeyEquals((K) store[i].getKey(), key)) {
+                return (V) store[i].getValue();
+            }
+        }
         return null;
+    }
+
+    private boolean isKeyEquals(K key1, K key2) {
+        return key1 == key2 || key1 != null && key1.equals(key2);
     }
 
     @Override
     public int size() {
-        return -1;
+        return elementsInArray;
     }
 }
