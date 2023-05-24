5b7730d9d2485e0776828f27dcb2c6a875c35f9f
Sieryi D <hunterst@ukr.net>
2023-05-25 01:59:27 +0300

    version with ArrayList's

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,36 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.List;
+import java.util.ArrayList;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private List<K> keys = new ArrayList<>();
+    private List<V> values = new ArrayList<>();
+
     @Override
     public void put(K key, V value) {
+        int numberElement = keys.indexOf(key);
+        if(numberElement == -1) {
+            keys.add(key);
+            values.add(value);
+        } else {
+            values.add(numberElement, value);
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int numberElement = keys.indexOf(key);
+        if(numberElement == -1) {
+            return null;
+        } else {
+            return values.get(numberElement);
+        }
     }
 
     @Override
     public int size() {
-        return -1;
+        return keys.size();
     }
 }
