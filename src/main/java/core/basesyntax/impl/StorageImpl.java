43e95472e07969d83a18a7276f06aa8258e45431
Sieryi D <hunterst@ukr.net>
2023-05-25 17:10:51 +0300

    with Array's instead ArrayList's

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,36 +1,52 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.ArrayList;
-import java.util.List;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private List<K> keys = new ArrayList<>();
-    private List<V> values = new ArrayList<>();
+    private static final int DEFAULT_LENGTH = 10;
+    private K[] keys = (K[]) new Object[DEFAULT_LENGTH];
+    private V[] values = (V[]) new Object[DEFAULT_LENGTH];
 
     @Override
     public void put(K key, V value) {
-        int numberElement = keys.indexOf(key);
-        if (numberElement == -1) {
-            keys.add(key);
-            values.add(value);
-        } else {
-            values.add(numberElement, value);
+        boolean keySearches = false;
+        for (int a = 0; a < DEFAULT_LENGTH; a++) {
+            if (keys[a] == key || keys[a] != null && keys[a].equals(key)) {
+                values[a] = value;
+                keySearches = true;
+                break;
+            }
+        }
+        if (keySearches == false) {
+            for (int a = 0; a < DEFAULT_LENGTH; a++) {
+                if (keys[a] == null && values[a] == null) {
+                    keys[a] = key;
+                    values[a] = value;
+                    break;
+                }
+            }
         }
     }
 
     @Override
     public V get(K key) {
-        int numberElement = keys.indexOf(key);
-        if (numberElement == -1) {
-            return null;
-        } else {
-            return values.get(numberElement);
+        for (int a = 0; a < DEFAULT_LENGTH; a++) {
+            if (keys[a] == key || keys[a] != null && keys[a].equals(key)) {
+                return values[a];
+            }
         }
+        return null;
     }
 
     @Override
     public int size() {
-        return keys.size();
+        int size = 0;
+        for (int a = 0; a < DEFAULT_LENGTH; a++) {
+            if (keys[a] == null && values[a] == null) {
+                return size;
+            }
+            size++;
+        }
+        return DEFAULT_LENGTH;
     }
 }
