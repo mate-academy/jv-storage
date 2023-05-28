6b132d8ac1518a8191e32dcd6eec8e5c3b220f7f
testSouth <southparaplan@gmail.com>
2023-05-28 18:19:42 +0300

    fixed style mistakes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -11,19 +11,16 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public StorageImpl() {
         keys = new Object[DEFAULT_SIZE];
         values = new Object[DEFAULT_SIZE];
-
     }
 
     @Override
     public void put(K key, V value) {
-
         for (int i = 0; i < size; i++) {
             if (key == keys[i] || key != null && key.equals(keys[i])) {
                 values[i] = value;
                 return;
             }
         }
-
         if (size == keys.length) {
             int newSize = size * 2;
             resizeArrays(newSize);
@@ -31,15 +28,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         keys[size] = key;
         values[size] = value;
         size++;
-    }
-
-    private void resizeArrays(int newSize) {
-        Object[] newKeys = new Object[newSize];
-        Object[] newValues = new Object[newSize];
-        System.arraycopy(keys, 0, newKeys, 0, size);
-        System.arraycopy(values, 0, newValues, 0, size);
-        keys = newKeys;
-        values = newValues;
     }
 
     @Override
@@ -55,5 +43,14 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return size;
+    }
+
+    private void resizeArrays(int newSize) {
+        Object[] newKeys = new Object[newSize];
+        Object[] newValues = new Object[newSize];
+        System.arraycopy(keys, 0, newKeys, 0, size);
+        System.arraycopy(values, 0, newValues, 0, size);
+        keys = newKeys;
+        values = newValues;
     }
 }
