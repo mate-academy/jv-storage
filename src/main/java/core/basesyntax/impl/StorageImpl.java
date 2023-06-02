7cd522c27588105a9eb3d3a264c58c74f6a095e5
Dmytro Zarudnyi <dmytro.zarudnyi@gmail.com>
2023-06-03 00:14:16 +0300

    finished class structure, added private method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,32 +3,62 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    //створити масив об'єктів, в який зберігати під час put
     private static final int MAX_ARRAY_LENGTH = 10;
 
-    private K key;
-    private V value;
+    private K[] keysArray;
     private V[] valuesArray;
 
     public StorageImpl() {
+        keysArray = (K[]) new Object[MAX_ARRAY_LENGTH];
+        valuesArray = (V[]) new Object[MAX_ARRAY_LENGTH];
     }
 
     @Override
     public void put(K key, V value) {
+        if (key != null) {
+            int valuesArrayLength = getArrayRealLength(valuesArray);
+            for (int i = 0; i < valuesArrayLength; i++) {
+                if (keysArray[i] != null && keysArray[i].equals(key)) {
+                    valuesArray[i] = value;
+                    return;
+                }
+            }
+            keysArray[valuesArrayLength] = key;
+            valuesArray[valuesArrayLength] = value;
+            return;
+        }
+
+        for (int i = 0; i < MAX_ARRAY_LENGTH; i++) {
+            if (keysArray[i] == null) {
+                valuesArray[i] = value;
+                return;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < getArrayRealLength(valuesArray); i++) {
+            if (keysArray[i] == key || (keysArray[i] != null && keysArray[i].equals(key))) {
+                return valuesArray[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        for (int i = 0; i < valuesArray.length; i++) {
-            if (valuesArray[i] == null) {
-                return i - 1;
+        return getArrayRealLength(valuesArray);
+    }
+
+    private int getArrayRealLength(Object[] incomingArray) {
+        int realLength = 0;
+        for (int i = 0; i < MAX_ARRAY_LENGTH; i++) {
+            if (incomingArray[i] == null) {
+                return realLength;
             }
-            return valuesArray.length;
+            realLength++;
         }
+        return realLength;
     }
 }
