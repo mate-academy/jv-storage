f205f3c3f890878c7aa3c2f3121c877fe4aa2c51
Taras Zakala <zakalataras4@gmail.com>
2023-06-07 23:06:40 +0300

    fixed task

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,19 +4,18 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
-    private Object[] keys;
-    private Object[] values;
+    private K[] keys;
+    private V[] values;
     private int size;
 
     public StorageImpl() {
-        keys = new Object[MAX_SIZE];
-        values = new Object[MAX_SIZE];
-        size = 0;
+        keys = (K[]) new Object[MAX_SIZE];
+        values = (V[]) new Object[MAX_SIZE];
     }
 
     public void put(K key, V value) {
         int index = findKeyIndex(key);
-        if (index >= 0) {
+        if (index != -1) {
             values[index] = value;
         } else {
             if (size < MAX_SIZE) {
@@ -44,14 +43,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     private int findKeyIndex(K key) {
         for (int i = 0; i < size; i++) {
-            if (key == null) {
-                if (keys[i] == null) {
-                    return i;
-                }
-            } else {
-                if (key.equals(keys[i])) {
-                    return i;
-                }
+            if (keys[i] == key || key != null && key.equals(keys[i])) {
+                return i;
             }
         }
         return -1;
