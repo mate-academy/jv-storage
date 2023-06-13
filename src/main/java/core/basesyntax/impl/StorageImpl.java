9b119382f56ddb4908f25939718808f014be3ebd
Sergey Bogatkin <daniltisenko22@gmail.com>
2023-06-13 18:58:19 +0300

    Refactoring

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,33 +3,37 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int START_ARRAY_SIZE = 0;
-    private int size = 0;
-    private Object[] keys = new Object[START_ARRAY_SIZE];
-    private Object[] values = new Object[START_ARRAY_SIZE];
+    private static final int STORAGE_SIZE = 10;
+    private Object[] keys = new Object[STORAGE_SIZE];
+    private Object[] values = new Object[STORAGE_SIZE];
+    private int size;
+    private int index;
+
+    public StorageImpl() {
+
+    }
+
+    public StorageImpl(Object[] keys, Object[] values, int size) {
+        this.keys = keys;
+        this.values = values;
+        this.size = size;
+    }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < size; i++) {
-            if ((key == keys[i]) || key != null && key.equals(keys[i])) {
-                values[i] = value;
-                return;
-            }
+        if (hasAKey(key)) {
+            values[index] = value;
+            return;
         }
-        if (keys.length == size) {
-            increaseArray(size + 1);
-            keys[size] = key;
-            values[size] = value;
-            size++;
-        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < size; i++) {
-            if ((keys[i] == key) || keys[i] != null && keys[i].equals(key)) {
-                return (V) values[i];
-            }
+        if (hasAKey(key)) {
+            return (V) values[index];
         }
         return null;
     }
@@ -39,12 +43,13 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         return size;
     }
 
-    private void increaseArray(int sizePlusOne) {
-        Object[] tempKeys = new Object[sizePlusOne];
-        Object[] tempValues = new Object[sizePlusOne];
-        System.arraycopy(keys,0, tempKeys,0, size);
-        System.arraycopy(values,0, tempValues,0, size);
-        keys = tempKeys;
-        values = tempValues;
+    private boolean hasAKey(K key) {
+        for (int i = 0; i < size; i++) {
+            index = i;
+            if ((key == keys[i]) || key != null && key.equals(keys[i])) {
+                return true;
+            }
+        }
+        return false;
     }
 }
