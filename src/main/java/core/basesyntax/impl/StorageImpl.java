67f187503c2e06fb4dd643667e5792863c405c91
Malinskyi Daniil <danmalina2707@gmail.com>
2023-06-14 19:35:09 +0300

    jv-storage done; implementing method put which checks if the key is null and if the storage already contains such element; if storage already contains such element it rewrites data with value if not and storage is less than MAX_SIZE we add new pair and make size++, when the storage is full we output message and throw a stateException, so do with keys. In size() we gain current size of the storage and in get() we gain value by key. IN equals() and HashCode() we compare two objects StorageImpl

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,72 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE = 10;
+    private final Map<K, V> storageMap;
+    private int size;
+
+    public StorageImpl() {
+        this.storageMap = new HashMap<>();
+        this.size = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        if (key == null) {
+            if (storageMap.containsKey(null)) {
+                storageMap.put(null, value);
+            } else {
+                if (size < MAX_SIZE) {
+                    storageMap.put(null, value);
+                    size++;
+                } else {
+                    throw new IllegalStateException("Storage is full");
+                }
+            }
+        } else {
+            if (storageMap.containsKey(key)) {
+                storageMap.put(key, value);
+            } else {
+                if (size < MAX_SIZE) {
+                    storageMap.put(key, value);
+                    size++;
+                } else {
+                    throw new IllegalStateException("Storage is full");
+                }
+            }
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return storageMap.get(key);
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        StorageImpl<?, ?> other = (StorageImpl<?, ?>) obj;
+        return Objects.equals(storageMap, other.storageMap);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(storageMap);
     }
 }
+
