ed286053ac783673f242c73780115fca53ab33a1
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2023-06-15 21:50:54 +0300

    initiate homework

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,51 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ARRAY_SIZE = 10;
+    private final K[] keys;
+    private final V[] values;
+    private int lastPosition = 0;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_ARRAY_SIZE];
+        values = (V[]) new Object[MAX_ARRAY_SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = indexOf(key);
+        if (index != -1) {
+            values[index] = value;
+            return;
+        }
+        keys[lastPosition] = key;
+        values[lastPosition] = value;
+        lastPosition++;
     }
 
     @Override
     public V get(K key) {
+        int index = indexOf(key);
+        if (index != -1) {
+            return values[index];
+        }
         return null;
     }
 
     @Override
     public int size() {
+        return lastPosition;
+    }
+
+    public int indexOf(K key) {
+        for (int i = 0; i < size(); i++) {
+            if (Objects.equals(keys[i], key)) {
+                return i;
+            }
+        }
         return -1;
     }
 }
