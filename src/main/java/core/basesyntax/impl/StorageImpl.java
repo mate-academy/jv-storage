a49884f9b5b809d1002cde5019112190d3c83b41
happytroya <happytroya2001@gmail.com>
2023-06-19 14:54:11 +0300

    refactored task by usind two arreys instead of HashMap

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,25 +1,57 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private Map<K, V> storage = new HashMap<>();
+    private static final int MAX_CAPACITY = 10;
+    private Object[] keys;
+    private Object[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = new Object[MAX_CAPACITY];
+        values = new Object[MAX_CAPACITY];
+        size = 0;
+    }
 
     @Override
     public void put(K key, V value) {
-        storage.put(key, value);
+        int index = getIndex(key);
+
+        if (index != -1) {
+            values[index] = value;
+        } else {
+            if (size == MAX_CAPACITY) {
+                throw new IllegalStateException("Storage is full");
+            }
+
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
     }
 
     @Override
     public V get(K key) {
-
-        return storage.get(key);
+        int index = getIndex(key);
+        if (index != -1) {
+            return (V) values[index];
+        }
+        return null;
     }
 
     @Override
     public int size() {
-        return storage.size();
+        return size;
+    }
+
+    private int getIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(keys[i], key)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
