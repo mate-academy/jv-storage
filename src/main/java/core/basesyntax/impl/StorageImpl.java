2e0130c7cf7dee03f365e1bb06ce868a6745716e
Olena Protsailo <olenaprotsailo@gmail.com>
2023-06-20 09:48:26 +0300

    Fixed array initialization

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,21 +5,14 @@ import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE_ARRAY = 10;
-    private K key;
-    private V value;
-    private final Object[] keyKeep = new Object[MAX_SIZE_ARRAY];
-    private final Object[] valueKeep = new Object[MAX_SIZE_ARRAY];
+    private final K [] keys = (K[]) new Object[MAX_SIZE_ARRAY];
+    private final V [] values = (V[]) new Object[MAX_SIZE_ARRAY];
     private int storageIndex = -1;
-
-    public StorageImpl(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
 
     public StorageImpl() {
     }
 
-    private int getFoundIndex(Object[] keyArray, K key, int lengthKeyArray) {
+    private int getFoundIndex(K[] keyArray, K key, int lengthKeyArray) {
         for (int i = 0; i < lengthKeyArray; i++) {
             if (Objects.equals(key,keyArray[i])) {
                 return i;
@@ -30,26 +23,26 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
      public void put(K key, V value) {
-        int lengthKeyArray = storageIndex + 1;
-        int foundIndex = getFoundIndex(keyKeep,key,lengthKeyArray);
+        int lengthKeyArray = size();
+        int foundIndex = getFoundIndex(keys,key,lengthKeyArray);
         if (foundIndex == MAX_SIZE_ARRAY) {
             storageIndex = size();
-            keyKeep [storageIndex] = key;
-            valueKeep [storageIndex] = value;
+            keys [storageIndex] = key;
+            values [storageIndex] = value;
         } else {
-            valueKeep[foundIndex] = value;
-            keyKeep[foundIndex] = key;
+            values[foundIndex] = value;
+            keys[foundIndex] = key;
         }
     }
 
     @Override
     public V get(K key) {
         int lengthKeyArray = storageIndex + 1;
-        int foundIndex = getFoundIndex(keyKeep, key,lengthKeyArray);
+        int foundIndex = getFoundIndex(keys, key,lengthKeyArray);
         if (foundIndex == MAX_SIZE_ARRAY) {
             return null;
         }
-        return (V) valueKeep[foundIndex];
+        return values[foundIndex];
     }
 
     @Override
