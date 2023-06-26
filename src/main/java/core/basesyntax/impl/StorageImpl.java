cad37f535ea41fac375033308cd40e52be054bc4
barbequeue <p99kab00@gmail.com>
2023-06-26 14:30:06 +0300

    Implement StorageImpl class to store key-value data

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,66 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private K key;
+    private V value;
+    private StorageImpl<K, V> next;
+
+    public StorageImpl() {
+        next = this;
+    }
+
     @Override
     public void put(K key, V value) {
+        if (next == this) {
+            this.key = key;
+            this.value = value;
+            next = null;
+            return;
+        }
+
+        // Overriding the value if the keys match
+        if (Objects.equals(this.key, key)) {
+            this.value = value;
+            return;
+        }
+
+        if (next == null) {
+            next = new StorageImpl<>();
+        }
+
+        next.put(key, value);
     }
 
     @Override
     public V get(K key) {
-        return null;
+        if (next == this) {
+            return null;
+        }
+
+        if (Objects.equals(this.key, key)) {
+            return this.value;
+        }
+
+        if (this.next == null) {
+            return null;
+        } else {
+            return this.next.get(key);
+        }
     }
 
     @Override
     public int size() {
-        return -1;
+        if (next == this) {
+            return 0;
+        }
+
+        if (this.next == null) {
+            return 1;
+        } else {
+            return 1 + this.next.size();
+        }
     }
 }
