08f6d4cd34ef07ead386c3ff68aeb81057e24c89
Sergei Kudelin <chertterner686@gmail.com>
2023-06-26 14:36:26 +0300

    realisation without HashMap

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,24 +1,55 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.HashMap;
-import java.util.Map;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private final Map<K, V> storage = new HashMap<>();
+    private final Node<K, V>[] nodesArray = new Node[10];
+    private int size = 0;
+    private int indexEqualsKey = 0;
 
     @Override
     public void put(K key, V value) {
-        storage.put(key, value);
+
+        if (size == 0) {
+            nodesArray[size] = getNewNode(key, value);
+        } else if (isEqualKey(key)) {
+            nodesArray[indexEqualsKey].setValue(value);
+            return;
+        } else {
+            nodesArray[size] = getNewNode(key, value);
+        }
+        ++size;
     }
 
     @Override
     public V get(K key) {
-        return storage.get(key);
+        if (isEqualKey(key)) {
+            return nodesArray[indexEqualsKey].getValue();
+        }
+        return null;
     }
 
     @Override
     public int size() {
-        return storage.size();
+        return size;
+    }
+
+    private boolean isEqualKey(K key) {
+        for (int index = 0; index < size; ++index) {
+            if (nodesArray[index].getKey() == null) {
+                if (key == null) {
+                    indexEqualsKey = index;
+                    return true;
+                }
+            } else if (nodesArray[index].getKey().equals(key)) {
+                indexEqualsKey = index;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Node<K, V> getNewNode(K key, V value) {
+        return new Node<>(key, value);
     }
 }
