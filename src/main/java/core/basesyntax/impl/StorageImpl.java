e3ca382d2c43a925c19d59ebb3672c2cb47a9a32
Denys-Maksym Kachalup <denys.k82@gmail.com>
2023-06-26 12:02:56 +0300

    implemented tasks

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,44 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int STORAGE_SIZE = 10;
+    private Object[] keys = new Object[STORAGE_SIZE];
+    private Object[] values = new Object[STORAGE_SIZE];
+    private int size = 0;
+
     @Override
     public void put(K key, V value) {
+        int keyIndex = findKeyIndex(key);
+        if (keyIndex != -1) {
+            values[keyIndex] = value;
+        } else {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+
+        }
     }
 
     @Override
     public V get(K key) {
+        int keyIndex = findKeyIndex(key);
+        if (keyIndex != -1) {
+            return (V) values[keyIndex];
+        }
         return null;
     }
 
     @Override
     public int size() {
+        return size;
+    }
+
+    private int findKeyIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == null && key == null || keys[i] != null && keys[i].equals(key)) {
+                return i;
+            }
+        }
         return -1;
     }
 }
