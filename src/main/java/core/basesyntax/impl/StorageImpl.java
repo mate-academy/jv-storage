a2ffe4bb734820c802cc9937bcd90421497c55cd
Andrii Pikhur <andrii.pikhur@gmail.com>
2023-06-27 13:06:41 +0300

    Logic is changed by Anastasia after revision Maven cleaned package All tests are passed

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,55 +4,36 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int STORAGE_SIZE = 10;
-    private Object[] keyElements = new Object[STORAGE_SIZE];
+    private Object[] keyElements = new Object[STORAGE_SIZE]; //
     private Object[] valueElements = new Object[STORAGE_SIZE];
+    private int size;
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < 10; i++) {
-            if (key == null && keyElements[i] == null) {
-                valueElements[i] = value;
-                return;
-            }
-            if (key != null && key.equals(keyElements[i])) { //replacement
-                valueElements[i] = value;
-                return;
-            }
-            if (key != null && keyElements[i] == null && valueElements[i] == null) {
-                keyElements[i] = key;
+        for (int i = 0; i < size; i++) {
+            if (key == keyElements[i] || key != null && key.equals(keyElements[i])) {
                 valueElements[i] = value;
                 return;
             }
         }
+        valueElements[size] = value;
+        keyElements[size] = key;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        if (key != null) {
-            for (int i = 0; i < 10; i++) {
-                if (key.equals(keyElements[i])) {
-                    return (V) valueElements[i];
-                }
+        for (int i = 0; i < size; i++) {
+            if (keyElements[i] == key || key != null && key.equals(keyElements[i])) {
+                return (V) valueElements[i];
             }
         }
-        if (key == null) {
-            for (int i = 0; i < 10; i++) {
-                if (keyElements[i] == null) {
-                    return (V) valueElements[i];
-                }
-            }
-        }
+
         return null;
     }
 
     @Override
     public int size() {
-        int size = 0; // Size of array 0-10 can be 0 if all elements are NULL
-        for (int i = 0; i < 10; i++) {
-            if (valueElements[i] != null) {
-                size++;
-            }
-        }
         return size;
     }
 }
