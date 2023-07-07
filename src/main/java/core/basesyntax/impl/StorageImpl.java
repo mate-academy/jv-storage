ff04208f34afef5a0001c84caccf460d214a01d0
Visqe <veetee@tutanota.com>
2023-07-07 20:47:02 +0300

    added solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,43 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.ArrayList;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private final ArrayList<K> keys = new ArrayList<>();
+    private final ArrayList<V> values = new ArrayList<>();
+
     @Override
     public void put(K key, V value) {
+        int keyIndex = indexOfKey(key);
+        if (keyIndex == -1) {
+            keys.add(key);
+            values.add(value);
+        } else {
+            values.set(keyIndex, value);
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int keyIndex = indexOfKey(key);
+        if (keyIndex == -1) {
+            return null;
+        }
+        return values.get(indexOfKey(key));
     }
 
     @Override
     public int size() {
+        return keys.size();
+    }
+
+    private int indexOfKey(K key) {
+        for (int i = 0; i < keys.size(); i++) {
+            if (key == keys.get(i) || key != null && key.equals(keys.get(i))) {
+                return i;
+            }
+        }
         return -1;
     }
 }
