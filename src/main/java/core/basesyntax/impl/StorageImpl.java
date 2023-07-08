5b33ce6a72203fb2e4d5d075b1e547bfa2422078
Hlib Bykovskyi <minexgleb@gmail.com>
2023-07-08 19:34:57 +0300

    move private methods below public methods; rename variables

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,13 +4,13 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int STORAGE_SIZE = 10;
-    private K[] keyDatabase;
-    private V[] valueDatabase;
+    private K[] keyStorage;
+    private V[] valueStorage;
     private int size;
 
     public StorageImpl() {
-        keyDatabase = (K[]) new Object[STORAGE_SIZE];
-        valueDatabase = (V[]) new Object[STORAGE_SIZE];
+        keyStorage = (K[]) new Object[STORAGE_SIZE];
+        valueStorage = (V[]) new Object[STORAGE_SIZE];
     }
 
     @Override
@@ -23,9 +23,26 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         addValue(key, value);
     }
 
+    @Override
+    public V get(K key) {
+
+        for (int i = 0; i < keyStorage.length; i++) {
+            if (equals(key, keyStorage[i])) {
+                return valueStorage[i];
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
     private boolean doesKeyExist(K key) {
         for (int i = 0; i < size; i++) {
-            if (equals(key, keyDatabase[i])) {
+            if (equals(key, keyStorage[i])) {
                 return true;
             }
         }
@@ -35,8 +52,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     private void replaceValue(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (equals(key, keyDatabase[i])) {
-                valueDatabase[i] = value;
+            if (equals(key, keyStorage[i])) {
+                valueStorage[i] = value;
                 return;
             }
         }
@@ -47,30 +64,13 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             throw new RuntimeException("Storage is full, cannot add a new value");
         }
 
-        keyDatabase[size] = key;
-        valueDatabase[size] = value;
+        keyStorage[size] = key;
+        valueStorage[size] = value;
         size++;
-    }
-
-    @Override
-    public V get(K key) {
-
-        for (int i = 0; i < keyDatabase.length; i++) {
-            if (equals(key, keyDatabase[i])) {
-                return valueDatabase[i];
-            }
-        }
-
-        return null;
     }
 
     private boolean equals(Object firstObject, Object secondObject) {
         return firstObject == secondObject
                 || (firstObject != null && firstObject.equals(secondObject));
-    }
-
-    @Override
-    public int size() {
-        return size;
     }
 }
