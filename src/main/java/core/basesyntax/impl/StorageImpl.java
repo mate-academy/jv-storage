c90dc7dcdecbd49bc5eb4288130c0e7507779898
Vladyslav Ihnatiuk <123470878+Someboty@users.noreply.github.com>
2023-07-10 12:24:23 +0300

    removed magic numbers

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,13 +3,14 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int ARRAY_MAX_SIZE = 10;
     private final K[] keys;
     private final V[] values;
     private int size;
 
     public StorageImpl() {
-        keys = (K[]) new Object[10];
-        values = (V[]) new Object[10];
+        keys = (K[]) new Object[ARRAY_MAX_SIZE];
+        values = (V[]) new Object[ARRAY_MAX_SIZE];
         size = 0;
     }
 
