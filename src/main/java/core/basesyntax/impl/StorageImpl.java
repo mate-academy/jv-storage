296c67c2e10b7e59bcc264b0b8a8076925b1660a
Olexander Parkhomovskyi <sanekparkhomovsky@gmail.com>
2023-07-10 19:19:05 +0300

    mvn fixes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,29 +3,29 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    public static final int MAX_ARRAY_SIZE = 10;
+    public static final int MAXIMUM_ARRAY_SIZE = 10;
+    private int size = 0;
     private K[] keys;
     private V[] values;
-    private int indexCount = 0;
 
     public StorageImpl() {
-        keys = (K[]) new Object[MAX_ARRAY_SIZE];
-        values = (V[]) new Object[MAX_ARRAY_SIZE];
+        keys = (K[]) new Object[MAXIMUM_ARRAY_SIZE];
+        values = (V[]) new Object[MAXIMUM_ARRAY_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
         if (get(key) != null) {
-            for (int i = 0; i < indexCount; i++) {
+            for (int i = 0; i < size; i++) {
                 if ((keys[i] == key)
                         || (keys[i] != null && keys[i].equals(key))) {
                     values[i] = value;
                 }
             }
-        } else if (indexCount <= keys.length) {
-            keys[indexCount] = key;
-            values[indexCount] = value;
-            indexCount++;
+        } else if (size <= keys.length) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
         } else {
             throw new RuntimeException("Storage is already filled. Can not add new elements");
         }
@@ -33,7 +33,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < indexCount; i++) {
+        for (int i = 0; i < size; i++) {
             if ((keys[i] == key)
                     || (keys[i] != null && keys[i].equals(key))) {
                 return values[i];
@@ -44,6 +44,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return indexCount;
+        return size;
     }
 }
