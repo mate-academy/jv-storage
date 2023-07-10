37350f576f31afc9f7f4274cb97931c7f47e5a1a
satori1111 <romakuchmii@gmail.com>
2023-07-10 12:02:34 +0300

    removed redundant expressions

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -17,12 +17,12 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public void put(K key, V value) {
         if (get(key) != null) {
             for (int i = 0; i < size; i++) {
-                if ((keys[i] == null && keys[i] == key)
+                if ((keys[i] == key)
                         || (keys[i] != null && keys[i].equals(key))) {
                     values[i] = value;
                 }
             }
-        } else if (size <= keys.length && size <= values.length) {
+        } else if (size <= keys.length) {
             keys[size] = key;
             values[size] = value;
             size++;
@@ -34,7 +34,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if ((keys[i] == null && keys[i] == key)
+            if ((keys[i] == key)
                     || (keys[i] != null && keys[i].equals(key))) {
                 return values[i];
             }
