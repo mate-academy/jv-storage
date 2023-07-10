462bd9452a0f1635bc3c2f99640b30524c67ac71
Iryna Mekh <iryna.mekh@gmail.com>
2023-07-10 19:36:31 +0300

    implemented storage interface

---
+++ src/main/java/core/basesyntax/impl/Element.java
@@ -0,0 +1,23 @@
+package core.basesyntax.impl;
+
+public class Element<K, V> {
+    private K key;
+    private V value;
+
+    public Element(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public V getValue() {
+        return value;
+    }
+}
--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,46 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ELEMENTS_NUMBER = 10;
+    private final Element<K, V>[] elements = new Element[MAX_ELEMENTS_NUMBER];
+    private int elementCounter = 0;
+
     @Override
     public void put(K key, V value) {
+        Element<K, V> existingElement = findElementByKey(key);
+        if (existingElement != null) {
+            existingElement.setValue(value);
+        } else {
+            Element<K, V> element = new Element<>(key, value);
+            elements[elementCounter] = element;
+            elementCounter++;
+        }
     }
 
     @Override
     public V get(K key) {
+        Element<K, V> existingElement = findElementByKey(key);
+        if (existingElement != null) {
+            return existingElement.getValue();
+        }
+        return null;
+    }
+
+    private Element<K, V> findElementByKey(K key) {
+        for (int i = 0; i < elementCounter; i++) {
+            Element<K, V> existingElement = elements[i];
+            if (Objects.equals(existingElement.getKey(), key)) {
+                return existingElement;
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return elementCounter;
     }
 }
