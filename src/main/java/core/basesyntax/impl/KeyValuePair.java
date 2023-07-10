16533a8689218c4956e3e5077857f274244d8537
Vladyslav Keda <dakadofordisluv@gmail.com>
2023-07-10 17:01:52 +0300

    Added 'KeyValuePair' class

---
+++ src/main/java/core/basesyntax/impl/KeyValuePair.java
@@ -0,0 +1,27 @@
+package core.basesyntax.impl;
+
+public class KeyValuePair<K, V> {
+    private final K key;
+    private V value;
+
+    public KeyValuePair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public boolean equalsByKey(K key) {
+        return (this.key == null) ? key == null : this.key.equals(key);
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public V getValue() {
+        return value;
+    }
+}
