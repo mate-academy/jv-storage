dccbc96aaa9d003ebd22f793b1b89456c7fd6177
Oleg Turyk <oleh.turyk.ki.2019@lpnu.ua>
2023-07-10 15:35:06 +0300

    implemented StorageImpl class

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,36 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.ArrayList;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private ArrayList<K> keys;
+    private ArrayList<V> values;
+
+    public StorageImpl() {
+        keys = new ArrayList<>();
+        values = new ArrayList<>();
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = keys.indexOf(key);
+        if (index >= 0) {
+            values.set(index, value);
+        } else {
+            keys.add(key);
+            values.add(value);
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int index = keys.indexOf(key);
+        return index != -1 ? values.get(index) : null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return keys.size();
     }
 }
