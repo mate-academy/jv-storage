912f215959156aef3cc1fbdbaa7b25d800076341
Ihor Vasylenko <vasylenkoihor23@gmail.com>
2023-07-10 09:27:34 +0300

    implemented task

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,83 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    @Override
-    public void put(K key, V value) {
+    private static final int MAX_SIZE = 10;
+    private final Object [] keys;
+    private final Object [] values;
+    private int size;
+
+    public StorageImpl() {
+        this.keys = new Object[MAX_SIZE];
+        this.values = new Object[MAX_SIZE];
+        this.size = 0;
     }
 
-    @Override
+    public void put(K key, V value) {
+        if (key == null) {
+            putNullKey(value);
+            return;
+        }
+        int index = getIndex(key);
+        if (index != -1) {
+            values[index] = value;
+        } else {
+            if (size < MAX_SIZE) {
+                keys[size] = key;
+                values[size] = value;
+                size++;
+            } else {
+                throw new IllegalStateException("Storage is full");
+            }
+        }
+    }
+
     public V get(K key) {
+        if (key == null) {
+            return getNullKey();
+        }
+
+        int index = getIndex(key);
+        if (index != -1) {
+            return (V) values[index];
+        }
         return null;
     }
 
-    @Override
+    private void putNullKey(V value) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == null) {
+                values[i] = value;
+                return;
+            }
+        }
+        if (size < MAX_SIZE) {
+            keys[size] = null;
+            values[size] = value;
+            size++;
+        } else {
+            throw new IllegalStateException("Storage is full");
+        }
+    }
+
+    private V getNullKey() {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == null) {
+                return (V) values[i];
+            }
+        }
+        return null;
+    }
+
     public int size() {
+        return size;
+    }
+
+    private int getIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key != null && key.equals(keys[i])) {
+                return i;
+            }
+        }
         return -1;
     }
 }
