b4f6a394bc61cb5bc59792d63223a630c22d298d
Ihor <kalaurdelta96@gmail.com>
2023-07-10 20:42:21 +0300

    added solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,64 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private static final int MAX_SIZE = 10;
+    private static final String PUT_EXCEPTION_MESSAGE =
+            "Can't add new data, the storage is full";
+    private K[] keys;
+    private V[] values;
+    private int itemsInStorage = 0;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_SIZE];
+        values = (V []) new Object[MAX_SIZE];
+
+    }
+
     @Override
     public void put(K key, V value) {
+        if (indexOfKey(key) > -1) {
+            keys[indexOfKey(key)] = key;
+            values[indexOfKey(key)] = value;
+        } else if (indexOfKey(key) == -1 && itemsInStorage < MAX_SIZE) {
+            keys[itemsInStorage] = key;
+            values[itemsInStorage] = value;
+            itemsInStorage++;
+        } else {
+            throw new RuntimeException(PUT_EXCEPTION_MESSAGE);
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        if (!containKey(key)) {
+            return null;
+        }
+        return values[indexOfKey(key)];
     }
 
     @Override
     public int size() {
+        return itemsInStorage;
+    }
+
+    private int indexOfKey(K key) {
+        for (int i = 0; i < itemsInStorage; i++) {
+            if (key == keys[i] || (key != null && key.equals(keys[i]))) {
+                return i;
+            }
+        }
         return -1;
+    }
+
+    private boolean containKey(K key) {
+        boolean contain = false;
+        for (int i = 0; i < itemsInStorage; i++) {
+            if (key == keys[i] || (key != null && key.equals(keys[i]))) {
+                contain = true;
+                break;
+            }
+        }
+        return contain;
     }
 }
