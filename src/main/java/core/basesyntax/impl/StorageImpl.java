0fcbba0bad6513c8e8136071163d48ea8a2fe47b
Volodymyr Lazarchuk <lazarchuk.v.u@gmail.com>
2023-07-10 18:16:40 +0300

    Initial commit

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,37 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int ELEMENTS_COUNT = 10;
+    private K[] keys = (K[]) new Object[ELEMENTS_COUNT];
+    private V[] values = (V[]) new Object[ELEMENTS_COUNT];
+    private int size = 0;
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (key == null ? keys[i] == null : key.equals(keys[i])) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            K k = keys[i];
+            if (key == null ? keys[i] == null : key.equals(keys[i])) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
