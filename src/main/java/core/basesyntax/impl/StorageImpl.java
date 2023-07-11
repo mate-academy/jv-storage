6bf63adedebab06237965e3db959e67c4aa28e32
Oleg Turyk <oleh.turyk.ki.2019@lpnu.ua>
2023-07-11 13:04:27 +0300

    Fixed errors

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,36 +1,52 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.ArrayList;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private ArrayList<K> keys;
-    private ArrayList<V> values;
+    private static final int MAX_ELEMENTS = 10;
+    private final K[] keys;
+    private final V[] values;
+    private int size;
 
     public StorageImpl() {
-        keys = new ArrayList<>();
-        values = new ArrayList<>();
+        keys = (K[]) new Object[MAX_ELEMENTS];
+        values = (V[]) new Object[MAX_ELEMENTS];
     }
 
     @Override
     public void put(K key, V value) {
-        int index = keys.indexOf(key);
-        if (index >= 0) {
-            values.set(index, value);
+        int index = findIndex(key);
+        if (size < MAX_ELEMENTS) {
+            if (index < 0) {
+                keys[size] = key;
+                values[size] = value;
+                size++;
+            } else {
+                values[index] = value;
+            }
+
         } else {
-            keys.add(key);
-            values.add(value);
+            throw new RuntimeException("Storage is full");
         }
     }
 
     @Override
     public V get(K key) {
-        int index = keys.indexOf(key);
-        return index != -1 ? values.get(index) : null;
+        int index = findIndex(key);
+        return index != -1 ? values[index] : null;
     }
 
     @Override
     public int size() {
-        return keys.size();
+        return size;
+    }
+
+    private int findIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if ((keys[i] != null && keys[i].equals(key)) || key == keys[i]) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
