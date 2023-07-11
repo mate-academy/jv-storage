44fadcd74222889f6463f45aa68d675ccae92181
Oleg Stashkiv <stashkivolegg@gmail.com>
2023-07-11 12:12:25 +0300

    Fixed mentor review: Made variables final.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,9 +5,9 @@ import core.basesyntax.Storage;
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
 
-    private K[] keys;
-    private V[] values;
-    private int size = 0;
+    private final K[] keys;
+    private final V[] values;
+    private int size;
 
     public StorageImpl() {
         keys = (K[]) new Object[MAX_SIZE];
