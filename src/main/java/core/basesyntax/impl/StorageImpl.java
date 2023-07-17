d58b00f789324f7429c1773ee8278c91b93ed297
Taras Nedaskivskyi <nadashkivskyitv@gmail.com>
2023-07-17 14:43:09 +0300

    task solution - Implemented methods put(), get() and size(), added getPositionByKey() method of StorageImpl class

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,47 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    public static final int KEY_MISSING = -1;
+    private K[] keys;
+    private V[] values;
+    private int sizeArray;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[10];
+        values = (V[]) new Object[10];
+        sizeArray = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        int keyPosition = getPositionByKey(key);
+        if (keyPosition != KEY_MISSING) {
+            values[keyPosition] = value;
+            return;
+        }
+
+        this.keys[sizeArray] = key;
+        this.values[sizeArray] = value;
+        ++sizeArray;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int valuePosition = getPositionByKey(key);
+        return valuePosition == KEY_MISSING ? null : values[valuePosition];
     }
 
     @Override
     public int size() {
-        return -1;
+        return sizeArray;
+    }
+
+    private int getPositionByKey(K key) {
+        for (int i = 0; i < sizeArray; i++) {
+            if ((keys[i] == key) || (keys[i] != null && keys[i].equals(key))) {
+                return i;
+            }
+        }
+        return KEY_MISSING;
     }
 }
