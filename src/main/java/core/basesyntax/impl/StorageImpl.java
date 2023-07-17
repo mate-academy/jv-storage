d41ff2938a821d54405145e050962725c512ead5
literandltx <romanskovronn@gmail.com>
2023-07-17 15:16:43 +0300

    remove redundant empty line

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,7 +3,6 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-
     private K[] storageKey;
     private V[] storageValue;
 
