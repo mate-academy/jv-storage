75b55025fe8ea383352c91a41fae34c8c6935f61
Oksana Slovachevska <slovachevska.ok@gmail.com>
2023-07-19 12:56:09 +0300

    added solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,40 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_STORAGE_SIZE = 10;
+    private K[] keyArray = (K[]) new Object [MAX_STORAGE_SIZE];
+    private V[] valueArray = (V[]) new Object [MAX_STORAGE_SIZE];
+    private int size;
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (key == keyArray[i] || key != null && key.equals(keyArray[i])) {
+                valueArray[i] = value;
+                return;
+            }
+        }
+        if (size >= MAX_STORAGE_SIZE) {
+            throw new RuntimeException("Storage is full");
+        }
+        keyArray[size] = key;
+        valueArray[size] = value;
+        size++;
+
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == keyArray[i] || key != null && key.equals(keyArray[i])) {
+                return valueArray[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
