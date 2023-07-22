3223f15f66afc9fad7c015cd5902828058122041
Oleh <popovich158@gmail.com>
2023-07-22 23:36:36 +0300

    implemented methods, put() - for creating and checking for(isExistKey), get() and size().

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,38 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE = 10;
+    private int arrayLength = 0;
+    private Object[] keyItems = new Object[MAX_SIZE];
+    private Object[] valueItems = new Object[MAX_SIZE];
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < arrayLength; i++) {
+            if ((keyItems[i] != null && (keyItems[i].equals(key)) || keyItems[i] == key)) {
+                valueItems[i] = value;
+                arrayLength--;
+            }
+        }
+        keyItems[arrayLength] = key;
+        valueItems[arrayLength] = value;
+        arrayLength++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < arrayLength; i++) {
+            if (keyItems[i] != null && (keyItems[i].equals(key) || keyItems[i] == key)) {
+                return (V) valueItems[i];
+            } else if (keyItems[i] == key) {
+                return (V) valueItems[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return arrayLength;
     }
 }
