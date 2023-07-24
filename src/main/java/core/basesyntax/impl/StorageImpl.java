f21a0e3012f474d5c4fe0053a2a5f58eabf48179
yermilov.m <yermilov.m@hotmail.com>
2023-07-24 15:45:15 +0200

    removed unnecessфÐary initialization

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -12,7 +12,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public StorageImpl() {
         keys = (K[]) new Object[MAX_SIZE];
         values = (V[]) new Object[MAX_SIZE];
-        size = 0;
     }
 
     @Override
