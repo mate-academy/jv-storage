4ba68f3488dff635e73074a83cc85a5a0b8962ee
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-07-25 19:49:12 +0300

    added changes to the class StorageImpl

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,53 +2,55 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
-    private Object[] keys;
-    private Object[] values;
-    private int size;
+    private final List<K> keys;
+    private final List<V> values;
 
     public StorageImpl() {
-        keys = new Object[MAX_SIZE];
-        values = new Object[MAX_SIZE];
-        size = 0;
+        keys = new ArrayList<>();
+        values = new ArrayList<>();
     }
 
     @Override
     public void put(K key, V value) {
         if (key == null) {
-            throw new IllegalArgumentException("Key cannot be null");
-        }
-        for (int i = 0; i < size; i++) {
-            if (keys[i].equals(key)) {
-                values[i] = value;
-                return;
+            int nullKeyIndex = keys.indexOf(null);
+            if (nullKeyIndex != -1) {
+                values.set(nullKeyIndex, value);
+            } else {
+                keys.add(null);
+                values.add(value);
             }
-        }
-        if (size < MAX_SIZE) {
-            keys[size] = key;
-            values[size] = value;
-            size++;
         } else {
-            throw new IllegalStateException("Storage is full");
+            int index = keys.indexOf(key);
+            if (index != -1) {
+                values.set(index, value);
+            } else {
+                if (keys.size() == MAX_SIZE) {
+                    throw new IllegalStateException("Storage is full.");
+                }
+                keys.add(key);
+                values.add(value);
+            }
         }
     }
 
     @Override
     public V get(K key) {
         if (key == null) {
-            throw new IllegalArgumentException("Key cannot be null");
+            int index = keys.indexOf(null);
+            return (index != -1) ? values.get(index) : null;
         }
-        for (int i = 0; i < size; i++) {
-            if (keys[i].equals(key)) {
-                return (V) values[i];
-            }
-        }
-        return null;
+        int index = keys.indexOf(key);
+        return (index != -1) ? values.get(index) : null;
     }
 
     @Override
     public int size() {
-        return size;
+        return keys.size();
     }
 }
