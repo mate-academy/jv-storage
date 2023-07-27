5b8f7d44427202a4bbd8a0fb176bb6f806112424
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-07-27 18:22:03 +0300

    added changes to the class StorageImpl

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,55 +2,70 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
-    private final Object[] keys;
-    private final Object[] values;
-    private int size;
+    private final List<Entry<K, V>> entries;
 
     public StorageImpl() {
-        this.keys = new Object[MAX_SIZE];
-        this.values = new Object[MAX_SIZE];
+        this.entries = new ArrayList<>();
     }
 
     @Override
     public void put(K key, V value) {
         int index = findKeyIndex(key);
         if (index != -1) {
-            values[index] = value;
+            entries.get(index).setValue(value);
         } else {
-            if (size == MAX_SIZE) {
+            if (entries.size() == MAX_SIZE) {
                 throw new IllegalStateException("Storage is full.");
             }
-            keys[size] = key;
-            values[size] = value;
-            size++;
+            entries.add(new Entry<>(key, value));
         }
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public V get(K key) {
-        if (key == null) {
-            int nullKeyIndex = findKeyIndex(null);
-            return (nullKeyIndex != -1) ? (V) values[nullKeyIndex] : null;
-        } else {
-            int index = findKeyIndex(key);
-            return (index != -1) ? (V) values[index] : null;
-        }
+        int index = findKeyIndex(key);
+        return index != -1 ? entries.get(index).getValue() : null;
     }
 
     @Override
     public int size() {
-        return size;
+        return entries.size();
     }
 
     private int findKeyIndex(K key) {
-        for (int i = 0; i < size; i++) {
-            if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
+        for (int i = 0; i < entries.size(); i++) {
+            Entry<K, V> entry = entries.get(i);
+            if ((key == null && entry.getKey() == null) || (key != null && key.equals(entry.getKey()))) {
                 return i;
             }
         }
         return -1;
+    }
+
+    private static class Entry<K, V> {
+        private final K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
     }
 }
