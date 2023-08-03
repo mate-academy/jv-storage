b7a580ec4d380eebf095a4104f2ed9234e7ad2e4
Andrii Riaboshapka <postagama@gmail.com>
2023-08-03 22:49:38 +0300

    Implement Storage interface methods in StorageImpl class

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,53 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    @Override
-    public void put(K key, V value) {
+    private static final int MAX_CAPACITY = 10;
+    private Object[] keys;
+    private Object[] values;
+    private int size;
+
+    public StorageImpl() {
+        this.keys = new Object[MAX_CAPACITY];
+        this.values = new Object[MAX_CAPACITY];
+        this.size = 0;
     }
 
     @Override
+    public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (equalsKey(keys[i], key)) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (equalsKey(keys[i], key)) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    private boolean equalsKey(Object key1, Object key2) {
+        if (key1 == null && key2 == null) {
+            return true;
+        }
+        if (key1 == null || key2 == null) {
+            return false;
+        }
+        return key1.equals(key2);
     }
 }
