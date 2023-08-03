ac5a35ee60f0215833cb28aec16ab41b770b9252
Krasnov-Maksim <maksim.v.krasnov@gmail.com>
2023-08-03 14:09:27 +0300

    fixed:  changed "private int size = 0;" to "private int size;"

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -7,7 +7,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int NOT_EXISTING_INDEX = -1;
     private final K[] keys;
     private final V[] values;
-    private int size = 0;
+    private int size;
 
     public StorageImpl() {
         keys = (K[]) new Object[MAXIMUM_NUMBER_OF_ELEMENTS];
