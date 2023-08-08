238054b99b34fb0cadd85e05decdce1dab38b5dd
Kolinko Oleksii <alexkolinko22@gmail.com>
2023-08-08 07:59:39 +0200

    HW10-Without StringBuilder

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,44 +3,35 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static String[] arraysValue;
-    private static String[] arraysKey;
-    private static final String SPLIT_INDEX = "___";
-    private final StringBuilder keys;
-    private final StringBuilder values;
+    private static final int MAX_SIZE = 10;
+    private final K[] keys;
+    private final V[] values;
     private int size;
 
     public StorageImpl() {
-        keys = new StringBuilder();
-        values = new StringBuilder();
-        size = 0;
+        keys = (K[]) new Object[MAX_SIZE];
+        values = (V[]) new Object[MAX_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
-        keys.append(key).append(SPLIT_INDEX);
-        values.append(value).append(SPLIT_INDEX);
-        arraysKey = keys.toString().split(SPLIT_INDEX);
-        arraysValue = values.toString().split(SPLIT_INDEX);
-        size = arraysKey.length;
-        for (int i = 0; i < arraysKey.length; i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if ((arraysKey[j] != null && arraysKey[j].equals(arraysKey[i]))
-                        || (arraysKey[j] == null && arraysKey[i] == null)) {
-                    arraysKey[j] = arraysKey[i];
-                    arraysValue[j] = arraysValue[i];
-                    size--;
-                }
+        keys[size] = key;
+        values[size] = value;
+        for (int i = size - 1; i >= 0; i--) {
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
+                keys[i] = key;
+                values[i] = value;
+                size--;
             }
         }
+        size = size + 1;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < arraysKey.length; i++) {
-            if (key != null && arraysKey[i].equals(key.toString())
-                    || arraysKey[i].equals("null") && key == null) {
-                return (V) arraysValue[i];
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
+                return values[i];
             }
         }
         return null;
