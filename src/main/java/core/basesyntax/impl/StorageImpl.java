757152324bb9861db5ab945dd1cdc14b0d7488e1
Oleksii Borysenko <343502597s@gmail.com>
2023-08-10 13:53:32 +0200

    fixed 2 arrays

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,27 +3,29 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private Object [] [] massiv = new Object[5][2];
+    private Object [] massivKey = new Object[10];
+    private Object [] massivValue = new Object[10];
     private int count;
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < count; i++) {
-            //if (massiv[i][0].equals(key)) {
-            if ((massiv[i][0] == key) || (massiv[i][0] != null && massiv[i][0].equals(key))) {
-                massiv[i] = new Object[]{key, value};
+            if ((massivKey[i] == key) || (massivKey[i] != null && massivKey[i].equals(key))) {
+                massivKey[i] = key;
+                massivValue[i] = value;
                 return;
             }
         }
-        massiv[count] = new Object[]{key, value};
+        massivKey[count] = key;
+        massivValue[count] = value;
         count++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < count; i++) {
-            if ((massiv[i][0] == key) || (massiv[i][0] != null && massiv[i][0].equals(key))) {
-                return (V) massiv[i][1];
+            if ((massivKey[i] == key) || (massivKey[i] != null && massivKey[i].equals(key))) {
+                return (V) massivValue[i];
             }
         }
         return null;
