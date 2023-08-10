1be6670f7c891964cd2e615fbf95635e17d5c432
gordgelin <gordgelin@s69a.su>
2023-08-10 06:59:35 +0300

    Fixed logic for null key and existing key

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,6 +2,7 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int INITIAL_ARRAY_SIZE = 10;
@@ -14,9 +15,17 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < currentIndex * 2; i += 2) {
+            if (Objects.equals(key, storage[i])) {
+                storage[i + 1] = value;
+                return;
+            }
+        }
+
         storage[currentIndex * 2] = key;
         storage[currentIndex * 2 + 1] = value;
         currentIndex++;
+
         if (currentIndex == storage.length / 2) {
             storage = Arrays.copyOf(storage, storage.length * 2);
         }
@@ -24,8 +33,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < storage.length; i += 2) {
-            if (key.equals(storage[i])) {
+        for (int i = 0; i < currentIndex * 2; i += 2) {
+            if (Objects.equals(key, storage[i])) {
                 return (V) storage[i + 1];
             }
         }
