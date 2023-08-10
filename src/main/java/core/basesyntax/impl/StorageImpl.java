6ee764908d830f3df7f11315deee5f76584eea17
Oleksii Borysenko <343502597s@gmail.com>
2023-08-10 13:05:00 +0200

    2 мерный массив все работает

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,8 +2,6 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
-import java.util.Arrays;
-
 public class StorageImpl<K, V> implements Storage<K, V> {
     private Object [] [] massiv = new Object[5][2];
     private int count;
@@ -11,7 +9,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < count; i++) {
-            if (massiv[i][0].equals(key)) {
+            //if (massiv[i][0].equals(key)) {
+            if ((massiv[i][0] == key) || (massiv[i][0] != null && massiv[i][0].equals(key))) {
                 massiv[i] = new Object[]{key, value};
                 return;
             }
@@ -23,7 +22,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < count; i++) {
-            if (massiv[i][0] == null || massiv[i][0].equals(key)) {
+            if ((massiv[i][0] == key) || (massiv[i][0] != null && massiv[i][0].equals(key))) {
                 return (V) massiv[i][1];
             }
         }
@@ -32,9 +31,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        for (Object [] mass : massiv) {
-            System.out.println(Arrays.toString(mass));
-        }
         return count;
     }
 }
