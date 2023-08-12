d2db0b9e87d1de86fe165e32340dfed0ab00b1cd
Andrey Polezhaiev <polezhaievandrey28@gmail.com>
2023-08-12 21:33:26 +0300

    deleted dublication in get and put

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -11,12 +11,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] == null && key == null) {
-                values[i] = value;
-                return;
-            }
-
-            if (keys[i] != null && keys[i].equals(key)) {
+            if (keys[i] != null && keys[i].equals(key) || keys[i] == null && key == null) {
                 values[i] = value;
                 return;
             }
@@ -29,10 +24,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] == null && keys[i] == key) {
-                return (V) values[i];
-            }
-            if (keys[i] != null && keys[i].equals(key)) {
+            if (keys[i] != null && keys[i].equals(key) || keys[i] == null && keys[i] == key) {
                 return (V) values[i];
             }
         }
