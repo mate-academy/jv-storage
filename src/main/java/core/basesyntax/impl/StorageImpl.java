a2862611d2dc6a0ac6f18370ebda7da687651ac0
Maksym Fedorenko <fedorenko.max02@gmail.com>
2023-08-13 18:15:23 +0300

    implemented storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,45 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int CAPACITY = 10;
+    private final Pair<K, V>[] data;
+    private int size;
+
+    public StorageImpl() {
+        size = -1;
+        data = new Pair[CAPACITY];
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = getIndexByKey(key);
+
+        if (index != -1) {
+            data[index].setValue(value);
+        } else {
+            data[++size] = new Pair<>(key, value);
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int index = getIndexByKey(key);
+        return index != -1 ? data[index].getValue() : null;
     }
 
     @Override
     public int size() {
+        return size + 1;
+    }
+
+    private int getIndexByKey(K key) {
+        for (int i = 0; i <= size; i++) {
+            if ((data[i].getKey() != null && data[i].getKey().equals(key))
+                    || (data[i].getKey() == null && key == null)) {
+                return i;
+            }
+        }
+
         return -1;
     }
 }
