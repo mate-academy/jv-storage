b8bc46c05a95a87f57bd8017b1f57150abef8fe8
Olena Stankevych <yelenastankevych@gmail.com>
2023-08-14 16:00:30 +0300

    added fixes after students review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,12 +5,12 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int STORAGE_CAPACITY = 10;
-    private final Pair[] pairs;
+    private final Pair<K, V>[] pairs;
     private int index;
 
     public StorageImpl() {
         pairs = new Pair[STORAGE_CAPACITY];
-        index = -1;
+        index = 0;
     }
 
     @Override
@@ -20,26 +20,28 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             if (keyIndex >= 0) {
                 pairs[keyIndex].setValue(value);
             } else {
-                pairs[++index] = new Pair<>(key, value);
+                pairs[index++] = new Pair<>(key, value);
             }
         }
     }
 
     @Override
     public V get(K key) {
-        return getKeyIndex(key) >= 0 ? (V) pairs[getKeyIndex(key)].getValue() :
+        int keyIndex = getKeyIndex(key);
+        return keyIndex >= 0 ? pairs[keyIndex].getValue() :
                 null;
     }
 
     @Override
     public int size() {
-        return index + 1;
+        return index;
     }
 
     private int getKeyIndex(K key) {
-        for (int i = 0; i <= index; i++) {
-            if ((pairs[i].getKey() != null && pairs[i].getKey().equals(key))
-                    || (pairs[i].getKey() == null && key == null)) {
+        for (int i = 0; i < size(); i++) {
+            K current = pairs[i].getKey();
+            if ((current != null && current.equals(key))
+                    || (current == null && key == null)) {
                 return i;
             }
         }
