a4bb98244af8d0a03e73f97ecf6bcedacc045082
Sonia Baranova <sofiasapsan@gmail.com>
2023-08-15 06:58:34 +0300

    implementation of storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,51 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE = 10;
+    private static final int AMOUNT_OF_OBJECTS = 2;
+    private static final int KEY_INDEX = 0;
+    private static final int VALUE_INDEX = 1;
+    private int currentIndex = 0;
+
+    private Object[][] array = new Object[MAX_SIZE][AMOUNT_OF_OBJECTS];
+
     @Override
     public void put(K key, V value) {
+        if (currentIndex < MAX_SIZE) {
+            int index = findKeyIndex(key);
+            if (index == -1) {
+                array[currentIndex][KEY_INDEX] = key;
+                array[currentIndex][VALUE_INDEX] = value;
+                currentIndex++;
+            } else {
+                array[index][VALUE_INDEX] = value;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < currentIndex; i++) {
+            if ((key == null && array[i][KEY_INDEX] == null)
+                    || (key != null && key.equals(array[i][KEY_INDEX]))) {
+                return (V) array[i][VALUE_INDEX];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
+        return currentIndex;
+    }
+
+    private int findKeyIndex(K key) {
+        for (int i = 0; i < currentIndex; i++) {
+            if ((key == null && array[i][KEY_INDEX] == null)
+                    || (key != null && key.equals(array[i][KEY_INDEX]))) {
+                return i;
+            }
+        }
         return -1;
     }
 }
