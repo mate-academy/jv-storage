a0dfb6976d2c9b52549222c93dfed9020ccd495e
Alex-zzzz <141009519+Alex-zzzz@users.noreply.github.com>
2023-08-15 15:09:06 +0300

    new version without HashMap ;-)

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,27 +1,55 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
-    private final Map<K,V> states = new HashMap<K, V>();
+    private final List<K> keys = new ArrayList<>();
+    private final List<V> values = new ArrayList<>();
 
     @Override
     public void put(K key, V value) {
+        boolean isNewKey = true;
+        for (int i = 0; i < keys.size(); i++) {
+            if (keys.get(i) != null && keys.get(i).equals(key)) {
+                values.remove(values.get(i));
+                values.add(i, value);
+                isNewKey = false;
+                break;
 
-        states.put(key,value);
+            } else if (keys.get(i) == null && keys.get(i) == (key)) {
+                values.remove(values.get(i));
+                values.add(i, value);
+                isNewKey = false;
+                break;
+            }
+        }
+        if (isNewKey) {
+            keys.add(key);
+            values.add(value);
+        }
     }
 
     @Override
     public V get(K key) {
-        return states.get(key);
+        for (int i = 0; i < keys.size(); i++) {
+            if (keys.get(i) == (key)) {
+                return values.get(i);
+            }
+            if (keys.get(i) != null && !(keys.get(i).getClass().isPrimitive())) {
+                if (keys.get(i).equals(key)) {
+                    return values.get(i);
+                }
+            }
+        }
+        return null;
     }
 
     @Override
     public int size() {
 
-        return states.size();
+        return keys.size();
     }
 }
