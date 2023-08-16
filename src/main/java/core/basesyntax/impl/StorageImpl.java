20c207e46c55da524c60c99bf3affd7c325a4c80
Alex-zzzz <141009519+Alex-zzzz@users.noreply.github.com>
2023-08-16 10:17:08 +0300

    new version without any List/Map ;-)

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,46 +1,53 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.ArrayList;
-import java.util.List;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
-    private final List<K> keys = new ArrayList<>();
-    private final List<V> values = new ArrayList<>();
+    private static final int MAX_ELEMENTS = 10;
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_ELEMENTS];
+        values = (V[]) new Object[MAX_ELEMENTS];
+    }
 
     @Override
     public void put(K key, V value) {
         boolean isNewKey = true;
-        for (int i = 0; i < keys.size(); i++) {
-            if (keys.get(i) != null && keys.get(i).equals(key)) {
-                values.remove(values.get(i));
-                values.add(i, value);
+        for (int i = 0; i < size; i++) {
+            if (keys[i] != null && keys[i].equals(key)) {
+                values[i] = value;
+                //size++;
                 isNewKey = false;
                 break;
 
-            } else if (keys.get(i) == null && keys.get(i) == (key)) {
-                values.remove(values.get(i));
-                values.add(i, value);
+            } else if (keys[i] == null && keys[i] == (key)) {
+                values[i] = value;
                 isNewKey = false;
+                //size++;
                 break;
             }
         }
         if (isNewKey) {
-            keys.add(key);
-            values.add(value);
+            keys[size] = key;
+            values[size] = value;
+            size++;
         }
+
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keys.size(); i++) {
-            if (keys.get(i) == (key)) {
-                return values.get(i);
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == (key)) {
+                return values[i];
             }
-            if (keys.get(i) != null && !(keys.get(i).getClass().isPrimitive())) {
-                if (keys.get(i).equals(key)) {
-                    return values.get(i);
+            if (keys[i] != null && !(keys[i].getClass().isPrimitive())) {
+                if (keys[i].equals(key)) {
+                    return values[i];
                 }
             }
         }
@@ -50,6 +57,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
 
-        return keys.size();
+        return size;
     }
 }
