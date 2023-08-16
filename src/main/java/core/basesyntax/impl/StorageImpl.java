e6dfc4255eaaef73f5221f5d805a16d9f8f16957
Mykyta Pavlov <pavlov2021mf12@student.karazin.ua>
2023-08-16 18:06:23 +0300

    renamed checkKey method to getKeyIndex and refactored some common mistakes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -7,12 +7,17 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int AMOUNT_OF_ITEMS = 10;
     private static final int INDEX_OF_KEYS = 0;
     private static final int INDEX_OF_VALUES = 1;
-    private Object[][] dictionary = new Object[AMOUNT_OF_ARRAYS][AMOUNT_OF_ITEMS];
-    private int size = 0;
+    private Object[][] dictionary;
+    private int size;
+
+    public StorageImpl() {
+        dictionary = new Object[AMOUNT_OF_ARRAYS][AMOUNT_OF_ITEMS];
+        size = 0;
+    }
 
     @Override
     public void put(K key, V value) {
-        int index = checkKey(key);
+        int index = getKeyIndex(key);
         if (index != -1) {
             dictionary[INDEX_OF_VALUES][index] = value;
             return;
@@ -25,7 +30,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        int index = checkKey(key);
+        int index = getKeyIndex(key);
         if (index != -1) {
             return (V) dictionary[INDEX_OF_VALUES][index];
         }
@@ -37,7 +42,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         return size;
     }
 
-    private int checkKey(K key) {
+    private int getKeyIndex(K key) {
         for (int i = 0; i < size; i++) {
             if (dictionary[INDEX_OF_KEYS][i] == key
                     || key != null
