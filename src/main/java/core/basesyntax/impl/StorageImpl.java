a9e15774ed0cd1fea53ea91be4cced8164ac5d3e
Oleksandr Yablunovskyi <briva.study@gmail.com>
2023-08-17 17:13:51 +0300

    Redundant code is deleted. Repeated logic is moved to the separate private method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,6 +6,7 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ELEMENTS_QUANTITY = 10;
+    private final int indexNotFound = -1;
     private int currentSize;
     private KeyValuePair<K, V>[] pairsArray;
 
@@ -16,37 +17,33 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        if (currentSize == 0) {
+        int index = checkIndex(key);
+        if (index == indexNotFound) {
             pairsArray[currentSize++] = new KeyValuePair<>(key, value);
         } else {
-            boolean reWrite = false;
-            for (int i = 0; i < currentSize; i++) {
-                if (pairsArray[i].getKey() == null ? pairsArray[i].getKey() == key
-                        : pairsArray[i].getKey().equals(key)) {
-                    pairsArray[i].setValue(value);
-                    reWrite = true;
-                    break;
-                }
-            }
-            if (!reWrite) {
-                pairsArray[currentSize++] = new KeyValuePair<>(key, value);
-            }
+            pairsArray[index].setValue(value);
         }
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < currentSize; i++) {
-            if (pairsArray[i].getKey() == null ? pairsArray[i].getKey() == key
-                    : pairsArray[i].getKey().equals(key)) {
-                return pairsArray[i].getValue();
-            }
-        }
-        return null;
+        int index = checkIndex(key);
+        return index == indexNotFound ? null : pairsArray[index].getValue();
     }
 
     @Override
     public int size() {
         return currentSize;
+    }
+
+    private int checkIndex(K key) {
+        int index = indexNotFound;
+        for (int i = 0; i < currentSize; i++) {
+            if (pairsArray[i].getKey() == null ? pairsArray[i].getKey() == key
+                    : pairsArray[i].getKey().equals(key)) {
+                index = i;
+            }
+        }
+        return index;
     }
 }
