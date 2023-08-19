fc50310b5c6cd7db94eed5bda761e080b636c34a
Teract10s <trs.leonov@gmail.com>
2023-08-19 13:05:57 +0300

    added inner class pair

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -51,4 +51,22 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public int size() {
         return pairs.length;
     }
+
+    private class Pair<K, V> {
+        private K key;
+        private V value;
+
+        public Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+    }
 }
