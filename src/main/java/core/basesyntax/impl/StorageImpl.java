6e49605f142025269e8855265e42f4029ee5f95b
Dmytro Cherepov <dmytro.cherepov.ukr@gmail.com>
2023-08-21 01:15:28 +0300

    added fixes after review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,15 +3,15 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int MAX_OF_ELEMENTS = 10;
-    private final Object[] keys;
-    private final Object[] values;
+    private static final int MAX_ELEMENTS = 10;
+    private final K[] keys;
+    private final V[] values;
     private int size;
 
     public StorageImpl() {
         size = 0;
-        keys = new Object[MAX_OF_ELEMENTS];
-        values = new Object[MAX_OF_ELEMENTS];
+        keys = (K[]) new Object[MAX_ELEMENTS];
+        values = (V[]) new Object[MAX_ELEMENTS];
     }
 
     @Override
@@ -20,7 +20,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         if (index == -1) {
             keys[size] = key;
             values[size++] = value;
-
         } else {
             values[index] = value;
         }
@@ -29,7 +28,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         int index = getKeyIndex(key);
-        return index == -1 ? null : (V) values[index];
+        return index == -1 ? null : values[index];
     }
 
     @Override
@@ -37,9 +36,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         return size;
     }
 
-    public int getKeyIndex(K key) {
+    private int getKeyIndex(K key) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] != null && keys[i].equals(key) || keys[i] == key) {
+            K currentKey = keys[i];
+            if (currentKey != null && currentKey.equals(key) || currentKey == key) {
                 return i;
             }
         }
