a416544d78521442e71ee172dae7c64e11111c4d
Svyatoslav Tsytsyura <svatik.bos@gmail.com>
2023-08-21 15:48:56 +0300

    implement methodes get, put, size

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,53 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ARRAY_SIZE = 10;
+    private Object[] keysArray = new Object[MAX_ARRAY_SIZE];
+    private Object[] valuesArray = new Object[MAX_ARRAY_SIZE];
+    private int size = 0;
+
     @Override
     public void put(K key, V value) {
+        if (checkKeysDublicate(key,value)) {
+            if (size < MAX_ARRAY_SIZE) {
+                keysArray[size] = key;
+                valuesArray[size] = value;
+                size++;
+            } else {
+                throw new RuntimeException("Array run out of memory");
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keysArray[i] == null && key == null) {
+                return (V) valuesArray[i];
+            }
+            if (keysArray[i] != null && keysArray[i].equals(key)) {
+                return (V) valuesArray[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    public boolean checkKeysDublicate(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (keysArray[i] != null && keysArray[i].equals(key)) {
+                valuesArray[i] = value;
+                return false;
+            }
+            if (keysArray[i] == null && key == null) {
+                valuesArray[i] = value;
+                return false;
+            }
+        }
+        return true;
     }
 }
