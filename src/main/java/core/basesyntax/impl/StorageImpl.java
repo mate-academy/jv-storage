46c2284aa7952a46001dbfc3e42ca753d2f5efed
Yura Novak <yuranovakr@gmail.com>
2023-08-22 16:54:22 +0300

    sorage solution.05

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,12 +3,9 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-
     private static final int MAX_STORAGE_NUMBER = 10;
     private K[] keys;
-
     private V[] values;
-
     private int size;
 
     public StorageImpl() {
