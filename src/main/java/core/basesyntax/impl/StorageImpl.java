d1706700d0aefd1e32a753b2073299927dc28ae4
Anastasiia <anastasiiafi7@gmail.com>
2023-08-22 15:27:48 +0300

    solution added

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,50 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE = 10;
+    private final K[] keys;
+    private final V[] values;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public StorageImpl() {
+        keys = (K[])new Object[MAX_SIZE];
+        values = (V[])new Object[MAX_SIZE];
+        size = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        if (keyExists(key)) {
+            values[findKeyIndex(key)] = value;
+        } else {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return keyExists(key) ? values[findKeyIndex(key)] : null;
     }
 
     @Override
     public int size() {
+        return size;
+    }
+
+    public int findKeyIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
+                return i;
+            }
+        }
         return -1;
+    }
+
+    public boolean keyExists(K key) {
+        int keyIndex = findKeyIndex(key);
+        return keyIndex >= 0;
     }
 }
