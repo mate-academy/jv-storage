14e3d3f32e078574bd85a4386cd98ee68e1221c4
hp <volodya.gata@gmail.com>
2023-08-22 15:53:56 +0300

    added constants, then initialized them, implemented findKeyIndex, then put and get methods

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,18 +2,55 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.Objects;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int AMOUNT_OF_ELEMENTS = 10;
+    private final K[] keys;
+    private final V[] values;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public StorageImpl() {
+        keys = (K[]) new Object[AMOUNT_OF_ELEMENTS];
+        values = (V[])new Object[AMOUNT_OF_ELEMENTS];
+        size = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        if (size == AMOUNT_OF_ELEMENTS) {
+            return;
+        }
+        int keyIndex = findKeyIndex(key);
+        if (keyIndex >= 0) {
+            values[keyIndex] = value;
+        } else {
+
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int keyIndex = findKeyIndex(key);
+        return keyIndex == -1 ? null : values[keyIndex];
     }
 
     @Override
     public int size() {
+        return size;
+    }
+
+    private int findKeyIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if ((key == keys[i]) || key != null && key.equals(keys[i])) {
+                return i;
+            }
+        }
         return -1;
     }
+
 }
