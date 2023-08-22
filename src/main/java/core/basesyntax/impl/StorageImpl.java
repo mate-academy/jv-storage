85a74a3673aad24d79f608e823b0c3438baf4b37
Tetiana Denchyk <tanushka.den7@gmail.com>
2023-08-22 15:57:41 +0300

    fixed errors

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,6 +4,7 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
+    private static final int NO_INDEX = -1;
     private int size;
     private final K[] keys;
     private final V[] values;
@@ -20,32 +21,32 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             return;
         }
         int index = indexOf(key);
-        if (index != -1) {
+        if (index != NO_INDEX) {
             values[index] = value;
-        } else if (index == -1) {
+        } else if (index == NO_INDEX) {
             keys[size] = key;
             values[size] = value;
             size++;
         }
     }
 
-    public int indexOf(K key) {
-        for (int i = 0; i < size; i++) {
-            if (key == keys[i] || key != null && key.equals(keys[i])) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
     @Override
     public V get(K key) {
         int index = indexOf(key);
-        return index == -1 ? null : values[index];
+        return index == NO_INDEX ? null : values[index];
     }
 
     @Override
     public int size() {
         return size;
+    }
+
+    private int indexOf(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
+                return i;
+            }
+        }
+        return NO_INDEX;
     }
 }
