be332f4f4b7604298e0069b144f1130ea26aae3d
Parashchuk <Andry.parashchuk@gmail.com>
2023-08-22 11:29:18 +0200

    implement storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,52 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE = 10;
+    private final K[] keys;
+    private final V[] values;
+    private int size = 0;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_SIZE];
+        values = (V[]) new Object[MAX_SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (size >= MAX_SIZE) {
+            throw new RuntimeException("Storage is out of memory");
+        }
+
+        int index = findKey(key);
+        if (index == -1) {
+            keys[size] = key;
+            values[size++] = value;
+        } else {
+            values[index] = value;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int index = findKey(key);
+        return index != -1 ? values[index] : null;
     }
 
     @Override
     public int size() {
+        return size;
+    }
+
+    private int findKey(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(key, keys[i])) {
+                return i;
+            }
+        }
+
         return -1;
     }
 }
