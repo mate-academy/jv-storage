e3c99d96a6c2602154d0e5d5b4ddfa025d86c1c2
Denis Gorda <dnsgorda@gmail.com>
2023-08-22 13:40:04 +0300

    fixed mistakes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,13 +5,22 @@ import core.basesyntax.Storage;
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 9;
     private int size;
-    private Object[] keys;
-    private Object[] values;
+    private K[] keys;
+    private V[] values;
 
     public StorageImpl() {
         size = 0;
-        keys = new Object[MAX_SIZE];
-        values = new Object[MAX_SIZE];
+        keys = (K[]) new Object[MAX_SIZE];
+        values = (V[]) new Object[MAX_SIZE];
+    }
+
+    private int getIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] != null && keys[i].equals(key) || keys[i] == key) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
@@ -23,22 +32,12 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         } else {
             values[index] = value;
         }
-
     }
 
     @Override
     public V get(K key) {
         int index = getIndex(key);
-        return index == -1 ? null : (V) values[index];
-    }
-
-    private int getIndex(K key) {
-        for (int i = 0; i < size; i++) {
-            if (keys[i] != null && keys[i].equals(key) || keys[i] == key) {
-                return i;
-            }
-        }
-        return -1;
+        return index == -1 ? null : values[index];
     }
 
     @Override
