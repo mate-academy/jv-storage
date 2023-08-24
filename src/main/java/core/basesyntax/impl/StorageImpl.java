6c5937b4fb73cb46cdef1a19e39480875a6f0cf8
Vyacheslav <torbin.official@gmail.com>
2023-08-24 13:53:39 +0300

    Errors fixed

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,13 +3,46 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    public static final int MAXIMUM_SIZE = 10;
+    private static final int MAXIMUM_SIZE = 10;
     private Pair<K, V>[] storage;
-    private int size = 0;
+    private int size;
 
-    @SuppressWarnings("unchecked")
     public StorageImpl() {
         storage = new Pair[MAXIMUM_SIZE];
+    }
+
+    @Override
+    public void put(K key, V value) {
+        int index = getIndex(key);
+        if (index == -1) {
+            if (size < MAXIMUM_SIZE) {
+                storage[size] = new Pair<>(key, value);
+                size++;
+            }
+        } else {
+            storage[index].setValue(value);
+        }
+    }
+
+    @Override
+    public V get(K key) {
+        int index = getIndex(key);
+        return index != -1 ? storage[index].getValue() : null;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    private int getIndex(K key) {
+        for (int i = 0; i < size(); i++) {
+            if (key != null && key.equals(storage[i].getKey())
+                    || key == storage[i].getKey()) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     private static class Pair<K, V> {
@@ -20,7 +53,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             this.key = key;
             this.value = value;
         }
-        
+
         public void setValue(V value) {
             this.value = value;
         }
@@ -34,37 +67,4 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         }
     }
 
-    @Override
-    public void put(K key, V value) {
-        if (size < MAXIMUM_SIZE) {
-            int index = getIndex(key);
-            if (index == -1) {
-                storage[size] = new Pair<>(key, value);
-                size++;
-            } else {
-                storage[index].setValue(value);
-            }
-        }
-    }
-
-    public int getIndex(K key) {
-        for (int i = 0; i < size(); i++) {
-            if (key != null && key.equals(storage[i].getKey())
-                    || key == storage[i].getKey()) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    @Override
-    public V get(K key) {
-        int index = getIndex(key);
-        return (index != -1) ? storage[index].getValue() : null;
-    }
-
-    @Override
-    public int size() {
-        return size;
-    }
 }
