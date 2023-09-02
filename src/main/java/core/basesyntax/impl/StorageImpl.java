8a5282b2dbcf6d4c4d7c737759ce7292baa51b13
mister.Sokor <mister.sokor@gmail.com>
2023-09-02 11:04:22 -0500

    project jv-storage is ready for code review.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,38 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE = 10;
+    private final List<K> keys;
+    private final List<V> values;
+
+    public StorageImpl() {
+        keys = new ArrayList<>(MAX_SIZE);
+        values = new ArrayList<>(MAX_SIZE);
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = keys.indexOf(key);
+        if (index != -1) {
+            values.set(index, value);
+        } else if (keys.size() < MAX_SIZE) {
+            keys.add(key);
+            values.add(value);
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int index = keys.indexOf(key);
+        return (index != -1) ? values.get(index) : null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return keys.size();
     }
 }
