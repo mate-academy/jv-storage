7ee580bb315777f6dd3afd5da48d3327f3bcf910
Petro Smoliar <smp22pl@gmail.com>
2023-09-03 17:10:55 +0200

    Implemented StorageImpl class

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,7 +3,6 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int INDEX_KEY = 0;
     private static final int MAX_SIZE = 10;
     private final Object[] keys;
     private final Object[] values;
@@ -13,6 +12,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         this.keys = new Object[MAX_SIZE];
         this.values = new Object[MAX_SIZE];
     }
+
     @Override
     public void put(K key, V value) {
         int indexKey = indexOf(key);
