0ebbb99487972210ff62a55e3670bdb1ca2e4d2c
Petro Smoliar <smp22pl@gmail.com>
2023-09-04 13:43:36 +0200

    Implemented StorageImpl class

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,7 +6,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
     private final K[] keys;
     private final V[] values;
-    private int size = 0;
+    private int size;
 
     public StorageImpl() {
         this.keys = (K[]) new Object[MAX_SIZE];
