9da8f67ee56a9ee104aa087248d12124ad45a3b9
Petro Smoliar <smp22pl@gmail.com>
2023-09-04 13:39:44 +0200

    Implemented StorageImpl class

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -22,9 +22,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             size++;
         } else {
             values[indexKey] = value;
-            if (indexKey == size) {
-                size++;
-            }
         }
     }
 
@@ -43,14 +40,13 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     }
 
     private int indexOf(K key) {
-        for (int i = 0; i < keys.length; i++) {
+        for (int i = 0; i < size; i++) {
             boolean keysAreEqual = key == null ? key == keys[i]
                     : key.equals(keys[i]);
-            if (keysAreEqual) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
                 return i;
             }
         }
         return -1;
     }
 }
-
