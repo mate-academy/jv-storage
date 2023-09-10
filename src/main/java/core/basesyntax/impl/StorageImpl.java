3881a7a42b7055f216b81cd55812bb4981f6a3f4
Vlad Rublenko <vladik.rar@ukr.net>
2023-09-10 18:45:11 +0300

    added access types to arrays

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,9 +5,9 @@ import core.basesyntax.Storage;
 public class StorageImpl<K, V> implements Storage<K, V> {
 
     private static final int MAX_SIZE = 10;
-    K[] keys;
-    V[] values;
-    int size;
+    private K[] keys;
+    private V[] values;
+    private int size;
 
     public StorageImpl() {
         keys = (K[]) new Object[MAX_SIZE];
