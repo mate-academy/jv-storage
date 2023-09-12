fa7f992590e75859ea8d074784ab7b8badc6ac91
Lukian Muzyka <lukianmuzyka@gmail.com>
2023-09-12 10:26:05 +0300

    added solution using arrays with max storage number, fixed some checkstyle issues

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,44 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_STORAGE_SIZE = 10;
+    private int storageSize;
+    private V[] valueStorage;
+    private K[] keyStorage;
+
+    public StorageImpl() {
+        valueStorage = (V[]) new Object[MAX_STORAGE_SIZE];
+        keyStorage = (K[]) new Object[MAX_STORAGE_SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < storageSize; i++) {
+            if (Objects.equals(keyStorage[i], key)) {
+                valueStorage[i] = value;
+                return;
+            }
+        }
+        valueStorage[storageSize] = value;
+        keyStorage[storageSize] = key;
+        storageSize++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < storageSize; i++) {
+            if (Objects.equals(keyStorage[i], key)) {
+                return valueStorage[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return storageSize;
     }
 }
