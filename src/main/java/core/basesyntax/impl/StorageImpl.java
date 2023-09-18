e9e2c3428b210a286781d5a75e8cdb7f97711169
Vladyslav Herasymenko <dalv23092005@gmail.com>
2023-09-18 15:47:12 +0200

    Cleaned up code and did small fixes to improve readability

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,40 +5,36 @@ import core.basesyntax.Storage;
 public class StorageImpl<K, V> implements Storage<K, V> {
     public static final int ARRAY_SIZE = 10;
 
-    private final K[] keyArr = (K[])new Object[ARRAY_SIZE];
-    private final V[] valArr = (V[])new Object[ARRAY_SIZE];
-    private int pos = 0;
+    private final K[] keys = (K[])new Object[ARRAY_SIZE];
+    private final V[] values = (V[])new Object[ARRAY_SIZE];
+    private int size = 0;
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < pos; i++) {
-            if (key != null && keyArr[i] != null) {
-                if (keyArr[i].equals(key)) {
-                    valArr[i] = value;
-                    return;
-                }
+        for (int i = 0; i < size; i++) {
+            if (key != null && keys[i] != null && keys[i].equals(key)) {
+                values[i] = value;
+                return;
             } else {
-                if (keyArr[i] == null && key == null) {
-                    valArr[i] = value;
+                if (keys[i] == null && key == null) {
+                    values[i] = value;
                     return;
                 }
             }
         }
-        keyArr[pos] = key;
-        valArr[pos] = value;
-        pos++;
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < pos; i++) {
-            if (keyArr[i] != null && key != null) {
-                if (keyArr[i].equals(key)) {
-                    return valArr[i];
-                }
+        for (int i = 0; i < size; i++) {
+            if (keys[i] != null && key != null && keys[i].equals(key)) {
+                return values[i];
             } else {
-                if (keyArr[i] == null && key == null) {
-                    return valArr[i];
+                if (keys[i] == null && key == null) {
+                    return values[i];
                 }
             }
         }
@@ -47,6 +43,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return pos;
+        return size;
     }
 }
