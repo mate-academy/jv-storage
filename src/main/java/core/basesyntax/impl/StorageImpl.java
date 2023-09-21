bf7aa390d280983019ee75cc753e48507bf3dc19
Ivan Kusurhashev <ivankusur@gmail.com>
2023-09-21 11:46:18 +0300

    Solution for jv-storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,53 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    @Override
-    public void put(K key, V value) {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private final K[] keys;
+    private final V[] values;
+    private int size = 0;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_ITEMS_NUMBER];
+        values = (V[]) new Object[MAX_ITEMS_NUMBER];
     }
 
     @Override
+    public void put(K key, V value) {
+        int index = findKeyIndex(keys, key);
+        if (index != -1) {
+            values[index] = value;
+            return;
+        }
+
+        if (size < MAX_ITEMS_NUMBER) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        } else {
+            keys[MAX_ITEMS_NUMBER - 1] = key;
+            values[MAX_ITEMS_NUMBER - 1] = value;
+        }
+    }
+
     public V get(K key) {
+        int index = findKeyIndex(keys, key);
+        if (index != -1) {
+            return values[index];
+        }
         return null;
     }
 
     @Override
     public int size() {
+        return size;
+    }
+
+    private int findKeyIndex(K[] keys, K key) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == null ? key == null : keys[i].equals(key)) {
+                return i;
+            }
+        }
         return -1;
     }
 }
