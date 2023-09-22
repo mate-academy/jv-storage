f57d269163acb76a0e8ad96d401f58e3b70a437e
Dima Semeniuk <semeniuk.dima@ukr.net>
2023-09-22 12:39:19 +0300

    Implemented storage with its methods

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,55 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    @Override
+    private static final int STORAGE_SIZE = 10;
+    private Element<K, V>[] storage = null;
+    private int position = 0;
+
+    private static class Element<K, V> {
+        private K key;
+        private V value;
+
+        public Element(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
     public void put(K key, V value) {
+        if (size() == 0) {
+            storage = new Element[STORAGE_SIZE];
+        }
+        for (int i = 0; i < size(); i++) {
+            if (compareKeyAndStorageKey(key, storage[i].key)) {
+                storage[i].value = value;
+                return;
+            }
+        }
+        if (position < STORAGE_SIZE) {
+            Element<K, V> element = new Element<>(key, value);
+            storage[position++] = element;
+        }
     }
 
     @Override
     public V get(K key) {
+        if (size() > 0) {
+            for (int i = 0; i < size(); i++) {
+                if (compareKeyAndStorageKey(key, storage[i].key)) {
+                    return storage[i].value;
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return position;
+    }
+
+    private boolean compareKeyAndStorageKey(K key, K storageKey) {
+        return ((key == null && storageKey == null)
+                || (storageKey != null && storageKey.equals(key)));
     }
 }
