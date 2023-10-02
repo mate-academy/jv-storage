4beb8287048b260ec8c5538ae18f5136bc20510e
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2023-10-02 10:23:40 +0300

    removed Objects

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,7 +1,6 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_CAPACITY = 10;
@@ -17,7 +16,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(key, keys[i])) {
+            if (key == null && keys[i] == null || key != null && key.equals(keys[i])) {
                 values[i] = value;
                 return;
             }
@@ -33,7 +32,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(key, keys[i])) {
+            if (key == null && keys[i] == null || key != null && key.equals(keys[i])) {
                 return values[i];
             }
         }
