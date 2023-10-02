959cc05f676dda09a00fe7f3a3c1d0b34db6a361
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-10-02 17:46:28 +0300

    Override put method. if key to add is not represented - add new key/value pair, other case rewrite value. Override the "size" method. For now return size of keys/value array. Create method findValuePositionByKey - return index in the "values" array , where value will be place. Create method isKeyExist which check is key already represented in storage. Create method isKeysEqual similar with Objects.equals, to compare keys.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,63 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int INITIAL_SIZE_NUMBER = 0;
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private final K[] keys;
+    private final V[] values;
+    private int size;
+
+    public StorageImpl() {
+        size = INITIAL_SIZE_NUMBER;
+        keys = (K[]) new Object[MAX_ITEMS_NUMBER];
+        values = (V[]) new Object[MAX_ITEMS_NUMBER];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (isKeyExist(key)) {
+            int insertPosition = findValuePositionByKey(key);
+            values[insertPosition] = value;
+        } else {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        if (!isKeyExist(key)) {
+            return null;
+        }
+        V valueToFind = values[findValuePositionByKey(key)];
+        return valueToFind;
     }
 
     @Override
     public int size() {
+        return size;
+    }
+
+    private boolean isKeyExist(K keyToFind) {
+        for (int i = 0; i < size; i++) {
+            if (isKeysEqual(keys[i], keyToFind)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int findValuePositionByKey(K keyToFind) {
+        for (int i = 0; i < size; i++) {
+            if (isKeysEqual(keys[i], keyToFind)) {
+                return i;
+            }
+        }
         return -1;
+    }
+
+    private boolean isKeysEqual(K firstKey, K secondKey) {
+        return (firstKey == secondKey) || (firstKey != null && firstKey.equals(secondKey));
     }
 }
