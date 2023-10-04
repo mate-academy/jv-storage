4c545664fcab28bc38c50088ad0d476281c889eb
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-10-04 14:54:18 +0300

    fixed initialization mistakes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,27 +5,35 @@ import core.basesyntax.Storage;
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ITEMS_NUMBER = 10;
     private static final int ITEM_FIELDS = 2;
-    private Object[][] items = new Object[MAX_ITEMS_NUMBER][ITEM_FIELDS];
-    private int size = 0;
+    private static final int KEY_COLUMN_INDEX = 0;
+    private static final int VALUE_COLUMN_INDEX = 1;
+    private static final int DEFAULT_STORAGE_SIZE = 0;
+    private Object[][] items;
+    private int size;
+
+    public StorageImpl() {
+        items = new Object[MAX_ITEMS_NUMBER][ITEM_FIELDS];
+        size = DEFAULT_STORAGE_SIZE;
+    }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (isKeyEquals(key, (K) items[i][0])) {
-                items[i][1] = value;
+            if (isKeyEquals(key, (K) items[i][KEY_COLUMN_INDEX])) {
+                items[i][VALUE_COLUMN_INDEX] = value;
                 return;
             }
         }
-        items[size][0] = key;
-        items[size][1] = value;
+        items[size][KEY_COLUMN_INDEX] = key;
+        items[size][VALUE_COLUMN_INDEX] = value;
         size++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < items.length; i++) {
-            if (isKeyEquals(key, (K) items[i][0])) {
-                return (V) items[i][1];
+            if (isKeyEquals(key, (K) items[i][KEY_COLUMN_INDEX])) {
+                return (V) items[i][VALUE_COLUMN_INDEX];
             }
         }
         return null;
