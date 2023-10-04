e63662fccea726b5e9cdeb29487ee479eac79089
Tetiana Koval <tetyanak0102@gmail.com>
2023-10-04 18:15:00 +0300

    resolved mistakes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,27 +3,33 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private K[] keys = (K[]) new Object[10];
-    private V[] values = (V[]) new Object[10];
+    private static final int ARRAY_SIZE = 10;
+    private K[] keys = (K[]) new Object[ARRAY_SIZE];
+    private V[] values = (V[]) new Object[ARRAY_SIZE];
     private int size = 0;
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if ((keys[i] != null && keys[i].equals(key)) || (keys[i] == null && key == null)) {
+            if (keysAreEqual(i, key)) {
                 values[i] = value;
                 return;
             }
         }
-        keys[size] = key;
-        values[size] = value;
-        size++;
+        if (size < 10) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        } else {
+            throw new RuntimeException(
+                    String.format("Can't store more than %d elements", ARRAY_SIZE));
+        }
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if ((keys[i] != null && keys[i].equals(key)) || (keys[i] == null && key == null)) {
+            if (keysAreEqual(i, key)) {
                 return values[i];
             }
         }
@@ -33,5 +39,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return size;
+    }
+
+    private boolean keysAreEqual(int i, K key) {
+        return (keys[i] != null && keys[i].equals(key)) || (keys[i] == null && key == null);
     }
 }
