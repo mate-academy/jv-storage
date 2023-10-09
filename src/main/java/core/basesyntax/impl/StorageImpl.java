6a9088ea9f01f98f8f422df6b199dbb34d6be295
Anton Dudar <dudar.anton4uk@gmail.com>
2023-10-09 16:49:30 +0300

    implemented the needed methods v1

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,62 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private static final int MAX_STORAGE_CAP = 10;
+    private int counter = 0;
+    private boolean hasKey = false;
+    private K[] arrayOfKeys = (K[]) new Object[MAX_STORAGE_CAP];
+    private V[] arrayOfValues = (V[]) new Object[MAX_STORAGE_CAP];
+
     @Override
     public void put(K key, V value) {
+        hasKey = false;
+        if (counter == 0) {
+            arrayOfKeys[counter] = key;
+            arrayOfValues[counter] = value;
+            counter++;
+            return;
+        } else if (counter > 0) {
+            for (int i = 0; i < counter; i++) {
+                if ((key != null && key.equals(arrayOfKeys[i]))
+                        || (key == null && key == arrayOfKeys[i])) {
+                    arrayOfValues[i] = value;
+                    hasKey = true;
+                }
+            }
+        }
+        if (!hasKey) {
+            arrayOfKeys[counter] = key;
+            arrayOfValues[counter] = value;
+            counter++;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        V result = null;
+        for (int i = 0; i < counter; i++) {
+            if ((key != null && key.equals(arrayOfKeys[i]))
+                    || (key == null && key == arrayOfKeys[i])) {
+                result = arrayOfValues[i];
+            }
+        }
+        return result;
+    }
+
+    private int keyLocator(K key) {
+        int result = 0;
+        for (int i = 0; i < counter; i++) {
+            if ((key != null && key.equals(arrayOfKeys[i]))
+                    || (key == null && key == arrayOfKeys[i])) {
+                result = i;
+            }
+        }
+        return result;
     }
 
     @Override
     public int size() {
-        return -1;
+        return counter;
     }
 }
