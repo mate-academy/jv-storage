bb0888b493ac5632c87268c86b4786180f022e0f
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-10-09 23:05:33 +0300

    implemented storage items save logic

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,45 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    public static final int STORAGE_LENGTH = 10;
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    public StorageImpl() {
+        this.keys = (K[]) new Object[STORAGE_LENGTH];
+        this.values = (V[]) new Object[STORAGE_LENGTH];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (checkKey(key, i)) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (checkKey(key, i)) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    private boolean checkKey(K key, int index) {
+        return key == keys[index] || keys[index] != null && keys[index].equals(key);
     }
 }
