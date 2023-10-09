f51edba9ea680bb31803fab842adeb9d753b2a42
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-10-09 11:59:28 +0300

    Remove redundant constant Remove isKeyExist method Rework findValuePositionByKey method. Rework put, get methods. For now, logic depends on value that we get from findValuePositionByKey method, not from isKeyExist method.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,51 +3,40 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int INITIAL_SIZE_NUMBER = 0;
     private static final int MAX_ITEMS_NUMBER = 10;
     private final K[] keys;
     private final V[] values;
     private int size;
 
     public StorageImpl() {
-        size = INITIAL_SIZE_NUMBER;
         keys = (K[]) new Object[MAX_ITEMS_NUMBER];
         values = (V[]) new Object[MAX_ITEMS_NUMBER];
     }
 
     @Override
     public void put(K key, V value) {
-        if (isKeyExist(key)) {
-            int insertPosition = findValuePositionByKey(key);
+        int insertPosition = findValuePositionByKey(key);
+        if (insertPosition != -1) {
             values[insertPosition] = value;
-        } else {
-            keys[size] = key;
-            values[size] = value;
-            size++;
+            return;
         }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        if (!isKeyExist(key)) {
-            return null;
+        int valuePosition = findValuePositionByKey(key);
+        if (valuePosition != -1) {
+            return values[valuePosition];
         }
-        V valueToFind = values[findValuePositionByKey(key)];
-        return valueToFind;
+        return null;
     }
 
     @Override
     public int size() {
         return size;
-    }
-
-    private boolean isKeyExist(K keyToFind) {
-        for (int i = 0; i < size; i++) {
-            if (isKeysEqual(keys[i], keyToFind)) {
-                return true;
-            }
-        }
-        return false;
     }
 
     private int findValuePositionByKey(K keyToFind) {
