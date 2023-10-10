eaf8ff51fd04df5798da852e05f6d8a3b87e2f49
Nataliia Shtykh <forstudying93@gmail.com>
2023-10-10 19:25:36 +0300

    added new method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,7 +4,6 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ELEMENT_COUNT = 10;
-    private static final int ZERO_VALUE = 0;
     private final Object[] keys;
     private final Object[] values;
     private int index;
@@ -12,14 +11,12 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public StorageImpl() {
         this.keys = new Object[MAX_ELEMENT_COUNT];
         this.values = new Object[MAX_ELEMENT_COUNT];
-        index = ZERO_VALUE;
     }
 
     @Override
     public void put(K key, V value) {
-
         for (int i = 0; i < index; i++) {
-            if (key == keys[i] || key != null && key.equals(keys[i])) {
+            if (equalsCheck(i, key)) {
                 values[i] = value;
                 return;
             }
@@ -33,8 +30,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < MAX_ELEMENT_COUNT; i++) {
-            if (key == keys[i] || key != null && key.equals(keys[i])) {
+        for (int i = 0; i < index; i++) {
+            if (equalsCheck(i, key)) {
                 return (V) values[i];
             }
         }
@@ -43,12 +40,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        int counter = ZERO_VALUE;
-        for (int i = 0; i < MAX_ELEMENT_COUNT; i++) {
-            if (values[i] != null) {
-                counter++;
-            }
-        }
-        return counter;
+        return index;
+    }
+
+    public boolean equalsCheck(int i, K key) {
+        return key == keys[i] || key != null && key.equals(keys[i]);
     }
 }
