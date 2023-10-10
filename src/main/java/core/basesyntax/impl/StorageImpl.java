4f6bfd3262660f53f1958ce5bea68d6b758f75f4
Mateusz Rostek <17rostek17@gmail.com>
2023-10-10 22:12:05 +0200

    Implemented the solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,45 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_STORAGE_ELEMENTS = 10;
+
+    private final K[] keys = (K[]) new Object[MAX_STORAGE_ELEMENTS];
+    private final V[] values = (V[]) new Object[MAX_STORAGE_ELEMENTS];
+    private int counter = 0;
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < MAX_STORAGE_ELEMENTS; i++) {
+            if ((keys[i] == null && key == null) || (keys[i] == null && values[i] == null)) {
+                keys[i] = key;
+                if (values[i] == null) {
+                    counter++;
+                }
+                values[i] = value;
+                return;
+            }
+            if (keys[i] != null) {
+                if (keys[i].equals(key)) {
+                    keys[i] = key;
+                    values[i] = value;
+                    return;
+                }
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < MAX_STORAGE_ELEMENTS; i++) {
+            if ((keys[i] == null && key == null) || keys[i] != null && keys[i].equals(key)) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return counter;
     }
 }
