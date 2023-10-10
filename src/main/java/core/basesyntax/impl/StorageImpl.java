c0bb2ee2872cd806943a945c508e2dbc8ca6a51f
Volodymyr Ozemblovskyi <Vladimir.ozem.ps@gmail.com>
2023-10-10 11:42:11 +0300

    Solve tasks from Readme file

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,54 @@
 package core.basesyntax.impl;
 
+import core.basesyntax.Pair;
 import core.basesyntax.Storage;
+import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_STORAGE_SIZE = 10;
+    private Pair[] content;
+
+    public StorageImpl() {
+        this.content = new Pair[0];
+    }
+
+    private int findKeysIndex(K key) {
+        for (int i = 0; i < content.length; i++) {
+            Object oldKey = content[i].getKey();
+            boolean firstCondition = (oldKey == null && key == null);
+            boolean secondCondition = (key != null && key.equals(oldKey));
+            if (firstCondition || secondCondition) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     @Override
     public void put(K key, V value) {
+        int keyIndex = findKeysIndex(key);
+        if (keyIndex >= 0) {
+            content[keyIndex] = new Pair(key, value);
+            return;
+        }
+        if (content.length < MAX_STORAGE_SIZE) {
+            int newLength = content.length + 1;
+            this.content = Arrays.copyOf(content, newLength);
+            content[newLength - 1] = new Pair(key, value);
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int keyIndex = findKeysIndex(key);
+        if (keyIndex == -1) {
+            return null;
+        }
+        return (V) content[keyIndex].getValue();
     }
 
     @Override
     public int size() {
-        return -1;
+        return content.length;
     }
 }
