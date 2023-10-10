c578b2185a927f9b6de3d7a9fcc1ab87474b8955
Igor Korolevich <igorkorolj@gmail.com>
2023-10-10 10:20:45 +0300

    added growIfArrayFull metod and minor changes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,6 +4,10 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ITEMS_NUMBER = 10;
+    private static final int DONT_HAVE_INDEX = -1;
+    private static final int ACCURACY_OF_ONE = 1;
+    private static final int GROWTH_FACTOR = 1;
+    private static final int INITIAL_POSITION = 0;
     private K[] keys;
     private V[] values;
     private int usedSpace;
@@ -16,20 +20,20 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         int index = findKeyIndex(key);
-        if (index != -1) {
+        if (index != DONT_HAVE_INDEX) {
             values[index] = value;
             return;
         }
         keys[usedSpace] = key;
         values[usedSpace] = value;
         usedSpace++;
+        growIfArrayFull();
     }
 
     @Override
     public V get(K key) {
         int index = findKeyIndex(key);
-        V value = index == -1 ? null : values[index];
-        return value;
+        return index == DONT_HAVE_INDEX ? null : values[index];
     }
 
     @Override
@@ -43,6 +47,21 @@ public class StorageImpl<K, V> implements Storage<K, V> {
                 return i;
             }
         }
-        return -1;
+        return DONT_HAVE_INDEX;
+    }
+
+    private void growIfArrayFull() {
+        if (usedSpace == values.length - ACCURACY_OF_ONE) {
+            int oldCapacity = values.length;
+            int newCapacity = oldCapacity + (oldCapacity >> GROWTH_FACTOR);
+            Object[] tempValues = new Object[newCapacity];
+            Object[] tempKeys = new Object[newCapacity];
+            System.arraycopy(values, INITIAL_POSITION, tempValues, INITIAL_POSITION, usedSpace);
+            System.arraycopy(keys, INITIAL_POSITION, tempKeys, INITIAL_POSITION, usedSpace);
+            values = (V[])new Object[newCapacity];
+            values = (V[]) tempValues;
+            keys = (K[])new Object[newCapacity];
+            keys = (K[]) tempKeys;
+        }
     }
 }
