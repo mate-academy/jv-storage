a74c5a2e264fba4549069b11b240f46d33fd74c5
Влад <vladlesn86@gmail.com>
2023-10-10 17:40:12 +0300

    did a StorageImpl

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,14 +3,14 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int maxSize = 10;
+    private static final int MAX_SIZE = 10;
     private final Object[] keys;
     private final Object[] values;
     private int size;
 
     public StorageImpl() {
-        keys = new Object[maxSize];
-        values = new Object[maxSize];
+        keys = new Object[MAX_SIZE];
+        values = new Object[MAX_SIZE];
         size = 0;
     }
 
@@ -20,7 +20,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         if (index != -1) {
             values[index] = value;
         } else {
-            if (size >= maxSize) {
+            if (size >= MAX_SIZE) {
                 throw new IllegalStateException("Storage is full.");
             }
             keys[size] = key;
