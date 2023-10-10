84c5fdda441e181b58a95b72a66a59001d06f2d3
White EdenI <danil.gayvoronsky2201@gmail.com>
2023-10-10 23:56:22 +0300

    I hate those tests, they make me angry :/

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,68 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int INITIAL_CAPACITY = 10;
+    private Object[] keys = new Object[INITIAL_CAPACITY];
+    private Object[] values = new Object[INITIAL_CAPACITY];
+    private int size = 0;
+
     @Override
     public void put(K key, V value) {
+        if (key == null) {
+            for (int i = 0; i < size; i++) {
+                if (keys[i] == null) {
+                    values[i] = value;
+                    return;
+                }
+            }
+            ensureCapacity();
+            keys[size] = null;
+            values[size] = value;
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (key.equals(keys[i])) {
+                    values[i] = value;
+                    return;
+                }
+            }
+            ensureCapacity();
+            keys[size] = key;
+            values[size] = value;
+        }
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == null) {
+                if (keys[i] == null) {
+                    return (V) values[i];
+                }
+            } else if (key.equals(keys[i])) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    private void ensureCapacity() {
+        if (size == keys.length) {
+            int newCapacity = keys.length * 2;
+            keys = copyArray(keys, newCapacity);
+            values = copyArray(values, newCapacity);
+        }
+    }
+
+    private Object[] copyArray(Object[] array, int newCapacity) {
+        Object[] newArray = new Object[newCapacity];
+        System.arraycopy(array, 0, newArray, 0, size);
+        return newArray;
     }
 }
+
