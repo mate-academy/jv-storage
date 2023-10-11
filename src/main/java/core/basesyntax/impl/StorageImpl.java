1339e0f1452ff826f3c7fd9a3cfdd11be88b07f2
Denys Okhrymovych <denys.ohrymovych.2015@gmail.com>
2023-10-11 15:59:35 +0300

    implement methods for putting key-value pairs and getting value by key

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,49 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_STORAGE_SIZE = 10;
+    private int currentStorageSize;
+    private K[] keys;
+    private V[] values;
+
+    public StorageImpl() {
+        this.currentStorageSize = 0;
+        this.keys = (K[]) new Object[MAX_STORAGE_SIZE];
+        this.values = (V[]) new Object[MAX_STORAGE_SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = -1;
+        for (int i = 0; i < currentStorageSize; i++) {
+            if (key == keys[i] || (key != null && key.equals(keys[i]))) {
+                index = i;
+                break;
+            }
+        }
+        if (index != -1) {
+            values[index] = value;
+        } else if (currentStorageSize < MAX_STORAGE_SIZE) {
+            keys[currentStorageSize] = key;
+            values[currentStorageSize] = value;
+            currentStorageSize++;
+        } else {
+            System.out.println("Storage size is limited to 10");
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < currentStorageSize; i++) {
+            if (key == keys[i] || (key != null && key.equals(keys[i]))) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return currentStorageSize;
     }
 }
