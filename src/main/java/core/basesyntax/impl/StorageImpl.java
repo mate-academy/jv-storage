7516105ee152218ef24497c40886b99cae1fb6ab
Oleksandr Voitenko <129169030+OlexVoit@users.noreply.github.com>
2023-10-11 08:14:16 +0200

    add a new method findKeyIndex

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,41 +4,40 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
-    private K[] arrayOfKeys;
-    private V[] arrayOfValues;
+    private K[] keys;
+    private V[] values;
     private int size;
 
     public StorageImpl() {
-        this.arrayOfKeys = (K[]) new Object[MAX_SIZE];
-        this.arrayOfValues = (V[]) new Object[MAX_SIZE];
-        this.size = 0;
+        this.keys = (K[]) new Object[MAX_SIZE];
+        this.values = (V[]) new Object[MAX_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < size; i++) {
-            if (key == null && arrayOfKeys[i] == null
-                    || key != null && key.equals(arrayOfKeys[i])) {
-                arrayOfValues[i] = value;
-                return;
-            }
-        }
-        if (size < MAX_SIZE) {
-            arrayOfKeys[size] = key;
-            arrayOfValues[size] = value;
+        int index = findKeyIndex(key);
+        if (index != -1) {
+            values[index] = value;
+        } else if (size < MAX_SIZE) {
+            keys[size] = key;
+            values[size] = value;
             size++;
         }
     }
 
     @Override
     public V get(K key) {
+        int index = findKeyIndex(key);
+        return (index != -1) ? (V) values[index] : null;
+    }
+
+    private int findKeyIndex(K key) {
         for (int i = 0; i < size; i++) {
-            if (key == null && arrayOfKeys[i] == null
-                    || key != null && key.equals(arrayOfKeys[i])) {
-                return (V) arrayOfValues[i];
+            if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
+                return i;
             }
         }
-        return null;
+        return -1;
     }
 
     @Override
