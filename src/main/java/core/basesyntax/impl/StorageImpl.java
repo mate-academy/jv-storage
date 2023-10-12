959df7fecfce9d9b32771d2ce2ab95556b3f8ecc
White EdenI <danil.gayvoronsky2201@gmail.com>
2023-10-12 09:45:33 +0300

    Optimized code

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -10,38 +10,23 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        if (key == null) {
-            for (int i = 0; i < size; i++) {
-                if (keys[i] == null) {
-                    values[i] = value;
-                    return;
-                }
+        for (int i = 0; i < size; i++) {
+            if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
+                values[i] = value;
+                return;
             }
-            ensureCapacity();
-            keys[size] = null;
-            values[size] = value;
-        } else {
-            for (int i = 0; i < size; i++) {
-                if (key.equals(keys[i])) {
-                    values[i] = value;
-                    return;
-                }
-            }
-            ensureCapacity();
-            keys[size] = key;
-            values[size] = value;
         }
+
+        ensureCapacity();
+        keys[size] = key;
+        values[size] = value;
         size++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (key == null) {
-                if (keys[i] == null) {
-                    return (V) values[i];
-                }
-            } else if (key.equals(keys[i])) {
+            if ((keys[i] == null && key == null) || (key != null && key.equals(keys[i]))) {
                 return (V) values[i];
             }
         }
