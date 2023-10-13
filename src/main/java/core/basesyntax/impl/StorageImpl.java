08feec9fbce372c464a6c58b789e80d2315ff8ad
Denys Okhrymovych <denys.ohrymovych.2015@gmail.com>
2023-10-13 22:59:48 +0300

    create method for finding key index

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,49 +3,52 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int MAX_STORAGE_SIZE = 10;
-    private int currentStorageSize;
+    private static final int MAX_SIZE = 10;
+    private int size;
     private K[] keys;
     private V[] values;
 
     public StorageImpl() {
-        this.currentStorageSize = 0;
-        this.keys = (K[]) new Object[MAX_STORAGE_SIZE];
-        this.values = (V[]) new Object[MAX_STORAGE_SIZE];
+        this.size = 0;
+        this.keys = (K[]) new Object[MAX_SIZE];
+        this.values = (V[]) new Object[MAX_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
-        int index = -1;
-        for (int i = 0; i < currentStorageSize; i++) {
-            if (key == keys[i] || (key != null && key.equals(keys[i]))) {
-                index = i;
-                break;
-            }
-        }
+        int index = findKeyIndex(key);
         if (index != -1) {
             values[index] = value;
-        } else if (currentStorageSize < MAX_STORAGE_SIZE) {
-            keys[currentStorageSize] = key;
-            values[currentStorageSize] = value;
-            currentStorageSize++;
+        } else if (size < MAX_SIZE) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
         } else {
-            System.out.println("Storage size is limited to 10");
+            System.out.println("Storage size is limited to " + MAX_SIZE);
         }
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < currentStorageSize; i++) {
-            if (key == keys[i] || (key != null && key.equals(keys[i]))) {
-                return values[i];
-            }
+        int index = findKeyIndex(key);
+        if (index != -1) {
+            return values[index];
         }
         return null;
     }
 
     @Override
     public int size() {
-        return currentStorageSize;
+        return size;
+    }
+
+    private int findKeyIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            K currentKey = keys[i];
+            if (key == currentKey || (key != null && key.equals(currentKey))) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
