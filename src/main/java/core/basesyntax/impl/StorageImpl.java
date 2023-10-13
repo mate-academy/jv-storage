83d547835474e524d23de0731f8770140f3247f5
Nataliia <gzodchie@gmail.com>
2023-10-13 14:37:51 +0300

    refactorind jv-storage solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,53 +3,55 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int MAXIMUM_CAPACITY = 10;
+    private static final int MAX_CAPACITY = 10;
     private Object[] keys;
     private Object[] values;
     private int numberOfElements;
 
     public StorageImpl() {
-        this.keys = new Object[MAXIMUM_CAPACITY];
-        this.values = new Object[MAXIMUM_CAPACITY];
+        this.keys = new Object[MAX_CAPACITY];
+        this.values = new Object[MAX_CAPACITY];
         this.numberOfElements = 0;
     }
 
     @Override
     public void put(K key, V value) {
-        if (findKeyIndex(key) != -1) {
-            values[findKeyIndex(key)] = value;
+        int index = findKeyIndex(key);
+        if (index != -1) {
+            values[index] = value;
+            return;
+        }
+        if (numberOfElements < MAX_CAPACITY) {
+            keys[numberOfElements] = key;
+            values[numberOfElements] = value;
+            numberOfElements++;
         } else {
-            if (numberOfElements < MAXIMUM_CAPACITY) {
-                keys[numberOfElements] = key;
-                values[numberOfElements] = value;
-                numberOfElements++;
-            } else {
-                throw new IndexOutOfBoundsException("Storage is full");
-            }
+            throw new IndexOutOfBoundsException("Storage is full");
         }
-    }
-
-    private int findKeyIndex(K key) {
-        for (int i = 0; i < numberOfElements; i++) {
-            if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
-                return i;
-            }
-        }
-        return -1;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < numberOfElements; i++) {
-            if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
-                return (V) values[i];
-            }
+        int index = findKeyIndex(key);
+        if (index == -1) {
+            return null;
+        } else {
+            return (V) values[index];
         }
-        return null;
     }
 
     @Override
     public int size() {
         return numberOfElements;
+    }
+
+    private int findKeyIndex(K key) {
+        for (int i = 0; i < numberOfElements; i++) {
+            Object currentKey = keys[i];
+            if ((key == null && currentKey == null) || (key != null && key.equals(currentKey))) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
