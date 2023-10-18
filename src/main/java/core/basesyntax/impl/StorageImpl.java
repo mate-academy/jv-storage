14c1ccd5e6c29ade9d64810eb9b61118d5426647
Anton Babych <antonbabi13@gmail.com>
2023-10-18 21:52:21 +0300

    Develop first version of task. Override methods from Storage interface.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,41 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    public static final int MAX_ARRAY_SIZE = 10;
+    private int curretElement = 0;
+    private Object[] keyArray = new Object[MAX_ARRAY_SIZE];
+    private Object[] valueArray = new Object[MAX_ARRAY_SIZE];
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < curretElement; i++) {
+            if (checkEquals(key, keyArray[i])) {
+                valueArray[i] = value;
+                curretElement--;
+            }
+        }
+        keyArray[curretElement] = key;
+        valueArray[curretElement] = value;
+        curretElement++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < curretElement; i++) {
+            if (checkEquals(key, keyArray[i])) {
+                return (V) valueArray[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return curretElement;
+    }
+
+    private boolean checkEquals(K key, Object keyArray) {
+        return ((key == null && keyArray == null)
+                || (keyArray != null && keyArray.equals(key)));
     }
 }
