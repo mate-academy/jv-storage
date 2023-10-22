7068031bf835f8ce01c30792e1eecb298eb9ec5e
Tony Hut <tonnyhut@outlook.com>
2023-10-22 22:37:43 +0200

    Realized storageImpl

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,61 @@
 package core.basesyntax.impl;
 
+import core.basesyntax.Pair;
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private final Pair<K, V>[] arrOfPair = new Pair[MAX_ITEMS_NUMBER];
+    private int size = 0;
+
     @Override
     public void put(K key, V value) {
+        Pair<K, V> pair = new Pair<>(key, value);
+        int elementIndex = getIndexOfElement(key);
+        if (elementIndex == -1) {
+            for (int i = 0; i < arrOfPair.length; i++) {
+                if (arrOfPair[i] == null) {
+                    arrOfPair[i] = new Pair<>(key, value);
+                    size++;
+                    break;
+                }
+            }
+        } else {
+            arrOfPair[elementIndex].setValue(pair.getValue());
+        }
+
     }
 
     @Override
     public V get(K key) {
-        return null;
+        V result = null;
+        for (int i = 0; i < arrOfPair.length; i++) {
+            if (i == size) {
+                break;
+            }
+            if (Objects.equals(arrOfPair[i].getKey(), key)) {
+                result = arrOfPair[i].getValue();
+                break;
+            }
+        }
+        return result;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    public int getIndexOfElement(K key) {
+        int index = -1;
+        for (int i = 0; i < size; i++) {
+            if (arrOfPair[i] != null && Objects.equals(arrOfPair[i].getKey(), key)) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+
     }
 }
