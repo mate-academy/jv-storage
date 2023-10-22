1ab3156df8765f67259ae84f98d4466812ec221d
Piotr Kaliszuk <p_kaliszuk@tlen.pl>
2023-10-22 17:12:00 +0200

    solution after suggestions

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,7 +1,6 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
@@ -12,44 +11,28 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public StorageImpl() {
         keys = new Object[MAX_SIZE];
         values = new Object[MAX_SIZE];
-        size = 0;
     }
 
     @Override
     public void put(K key, V value) {
-        if (key == null) {
-            for (int i = 0; i < size; i++) {
-                if (keys[i] == null) {
-                    values[i] = value;
-                    return;
-                }
+        for (int i = 0; i < size; i++) {
+            if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
+                values[i] = value;
+                return;
             }
-            if (size >= MAX_SIZE) {
-                throw new IllegalStateException("Storage is full");
-            }
-            keys[size] = null;
-            values[size] = value;
-            size++;
-        } else {
-            for (int i = 0; i < size; i++) {
-                if (Objects.equals(key, keys[i])) {
-                    values[i] = value;
-                    return;
-                }
-            }
-            if (size >= MAX_SIZE) {
-                throw new IllegalStateException("Storage is full");
-            }
-            keys[size] = key;
-            values[size] = value;
-            size++;
         }
+        if (size >= MAX_SIZE) {
+            throw new IllegalStateException("Storage is full");
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(key, keys[i])) {
+            if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
                 return (V) values[i];
             }
         }
