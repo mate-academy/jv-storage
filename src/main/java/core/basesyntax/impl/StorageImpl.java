51a6de9ccfd6bf5e7ec6a4e3043ce99983da4e36
AdamMudrak <mudrets2000@gmail.com>
2023-10-28 14:35:07 +0300

    Add solution(I know I shouldn't write messages like this)

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,18 +2,53 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+@SuppressWarnings("unchecked")
 public class StorageImpl<K, V> implements Storage<K, V> {
+    K[] keys = (K[]) new Object[10];
+    V[] values = (V[]) new Object[10];
+    int size;
+
     @Override
     public void put(K key, V value) {
+        int index = putValidator(keys, values, key);
+        if (index == -1) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        } else if (index >= 0) {
+            keys[index] = key;
+            values[index] = value;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return getValidator(key);
     }
 
     @Override
     public int size() {
+        return size;
+    }
+
+    private int putValidator(K[] keys, V[] values, K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if ((((key == null && keys[i] == null) || (keys[i] != null && keys[i].equals(key))) && values[i] != null)) {
+                return i;
+            } else if ((key == null && keys[i] == null) || (key != null && keys[i] == null)) {
+                return -1;
+            }
+        }
         return -1;
     }
+
+    private V getValidator(K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] == key || (keys[i] != null && keys[i].equals(key))) {
+                return values[i];
+            }
+        }
+        return null;
+    }
 }
+
