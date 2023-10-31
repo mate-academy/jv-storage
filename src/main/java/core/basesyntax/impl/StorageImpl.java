d9217bef10227c1e29b0bd937b897b605e889c86
Vitalii Kazymyrovych <impero44@gmail.com>
2023-10-31 14:43:57 +0200

    Resolved style issues

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,39 +3,36 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-
     private static final int FIRST_ELEMENT = 0;
     private static final int STORAGE_LENGTH = 10;
     private final Pair<K, V>[] storage;
-    private int index;
+    private int size;
 
     public StorageImpl() {
         storage = new Pair[STORAGE_LENGTH];
+        for (int i = 0; i < STORAGE_LENGTH; i++) {
+            storage[i] = new Pair<>();
+        }
     }
 
     @Override
     public void put(K key, V value) {
-        if (storage[FIRST_ELEMENT] == null) {
-            for (int i = 0; i < STORAGE_LENGTH; i++) {
-                storage[i] = new Pair<>();
-            }
-        }
-        for (int i = 0; i < index; i++) {
+        for (int i = 0; i < size; i++) {
             if (equalKey(storage[i].key, key)) {
                 storage[i].value = value;
                 return;
             }
         }
-        storage[index].key = key;
-        storage[index].value = value;
-        index++;
+        storage[size].key = key;
+        storage[size].value = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        for (Pair<K, V> element : storage) {
-            if (equalKey(element.key, key)) {
-                return element.value;
+        for (int i = 0; i < storage.length; i++) {
+            if (equalKey(storage[i].key, key)) {
+                return storage[i].value;
             }
         }
         return null;
@@ -43,17 +40,16 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return index;
+        return size;
     }
 
     public boolean equalKey(K firstKey, K secondKey) {
-        return firstKey == null
-                && secondKey == null
+        return firstKey == secondKey
                 || (firstKey != null
                 && firstKey.equals(secondKey));
     }
 
-    public static class Pair<K, V> {
+    private static class Pair<K, V> {
         private K key;
         private V value;
     }
