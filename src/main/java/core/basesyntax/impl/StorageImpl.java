7ce3bf57ee1e00b81ca46018ec681c2febe421a3
Roman Globa <globaroman@gmail.com>
2023-11-01 16:25:02 +0200

    The implemented solution to the task according to the README

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,54 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int SIZE_ARRAY = 10;
+    private final Object[] storeK = new Object[SIZE_ARRAY];
+    private final Object[] storeV = new Object[SIZE_ARRAY];
+    private int countIndex = 0;
+
     @Override
     public void put(K key, V value) {
+        int indexKey = getIndexAndCheckExistKey(key);
+        if (indexKey >= 0) {
+            storeV[indexKey] = value;
+        } else {
+            storeK[countIndex] = key;
+            storeV[countIndex++] = value;
+        }
+
+    }
+
+    private int getIndexAndCheckExistKey(K key) {
+        if (key == null) {
+            for (int i = 0; i < countIndex; i++) {
+                if (storeK[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < countIndex; i++) {
+                if (key.equals(storeK[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int indexKey = getIndexAndCheckExistKey(key);
+        if (indexKey >= 0) {
+            return (V) storeV[indexKey];
+        } else {
+            return null;
+        }
     }
 
     @Override
     public int size() {
-        return -1;
+        return countIndex;
     }
 }
+
+
