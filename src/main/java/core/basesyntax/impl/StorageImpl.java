3d40a3c3ad10904696d75c77af39f1f8d857e6ff
Roman Globa <globaroman@gmail.com>
2023-11-01 22:30:47 +0200

    Added constructors and implemented init of arrays in the constructor. Also rename arrays

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,44 +4,35 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int SIZE_ARRAY = 10;
-    private final Object[] storeK = new Object[SIZE_ARRAY];
-    private final Object[] storeV = new Object[SIZE_ARRAY];
+    private final Object[] keyStorage;
+    private final Object[] valueStorage;
     private int countIndex = 0;
+
+    public StorageImpl(int size) {
+        keyStorage = new Object[size];
+        valueStorage = new Object[size];
+    }
+
+    public StorageImpl() {
+        this(SIZE_ARRAY);
+    }
 
     @Override
     public void put(K key, V value) {
         int indexKey = getIndexAndCheckExistKey(key);
         if (indexKey >= 0) {
-            storeV[indexKey] = value;
+            valueStorage[indexKey] = value;
         } else {
-            storeK[countIndex] = key;
-            storeV[countIndex++] = value;
+            keyStorage[countIndex] = key;
+            valueStorage[countIndex++] = value;
         }
-
-    }
-
-    private int getIndexAndCheckExistKey(K key) {
-        if (key == null) {
-            for (int i = 0; i < countIndex; i++) {
-                if (storeK[i] == null) {
-                    return i;
-                }
-            }
-        } else {
-            for (int i = 0; i < countIndex; i++) {
-                if (key.equals(storeK[i])) {
-                    return i;
-                }
-            }
-        }
-        return -1;
     }
 
     @Override
     public V get(K key) {
         int indexKey = getIndexAndCheckExistKey(key);
         if (indexKey >= 0) {
-            return (V) storeV[indexKey];
+            return (V) valueStorage[indexKey];
         } else {
             return null;
         }
@@ -50,6 +41,23 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return countIndex;
+    }
+
+    private int getIndexAndCheckExistKey(K key) {
+        if (key == null) {
+            for (int i = 0; i < countIndex; i++) {
+                if (keyStorage[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < countIndex; i++) {
+                if (key.equals(keyStorage[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 }
 
