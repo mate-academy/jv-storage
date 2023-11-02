34d49b4ea58058c5f40fc71c3e82007702024910
Roman Kuzyk <kuzya228337@mail.com>
2023-11-02 18:52:32 +0200

    Fixed errors

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,33 +4,38 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
-    private Object[] keys = new Object[MAX_SIZE];
-    private Object[] values = new Object[MAX_SIZE];
+    private K[] keys;
+    private V[] values;
     private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_SIZE];
+        values = (V[]) new Object[MAX_SIZE];
+    }
 
     @Override
     public void put(K key, V value) {
+        if (size >= MAX_SIZE) {
+            throw new RuntimeException("Maximum size exceeded");
+
+        }
         for (int i = 0; i < size; i++) {
-            if (key == keys[i] || key != null && key.equals(keys[i])) {
+            if (key == keys[i] || (key != null && key.equals(keys[i]))) {
                 values[i] = value;
                 return;
             }
         }
 
-        if (size < MAX_SIZE) {
-            keys[size] = key;
-            values[size] = value;
-            size++;
-        } else {
-            throw new RuntimeException("Maximum size exceeded");
-        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (key == keys[i] || key != null && key.equals(keys[i])) {
-                return (V) values[i];
+            if (key == keys[i] || (key != null && key.equals(keys[i]))) {
+                return values[i];
             }
         }
         return null;
