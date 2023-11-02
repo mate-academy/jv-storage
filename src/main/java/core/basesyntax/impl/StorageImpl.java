779f53215b3e21781b7ea219c50c9eee34bf5fb1
Vadim <waterfallkadas@gmail.com>
2023-11-02 08:32:09 +0200

    solution 02/11 - 1

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -7,44 +7,45 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int INDEX_VALUE = 1;
     private int arraySize;
     private int arrayIndex;
-    private final Object[][] tempValuesArray = new Object[10][2];
-    private Object[][] resultValuesArray = new Object[0][0];
+    private final Object[][] valuesArray;
+
+    public StorageImpl() {
+        valuesArray = new Object[10][2];
+    }
 
     @Override
     public void put(K key, V value) {
-        tempValuesArray[arrayIndex][INDEX_KEY] = key;
-        tempValuesArray[arrayIndex][INDEX_VALUE] = value;
+        valuesArray[arrayIndex][INDEX_KEY] = key;
+        valuesArray[arrayIndex][INDEX_VALUE] = value;
         arrayIndex++;
         for (int i = 0; i < arraySize; i++) {
-            if ((tempValuesArray[i][INDEX_KEY] == null && key == null)
-                    || (tempValuesArray[i][INDEX_KEY] != null && key != null
-                    && tempValuesArray[i][INDEX_KEY].toString().equals(key.toString()))) {
-                tempValuesArray[i][INDEX_VALUE] = value;
+            if (equals(key,(K)valuesArray[i][0])) {
+                valuesArray[i][INDEX_VALUE] = value;
                 arraySize--;
             }
         }
         arraySize++;
-        resultValuesArray = new Object[arraySize][2];
-        for (int i = 0; i < arraySize; i++) {
-            resultValuesArray[i][INDEX_KEY] = tempValuesArray[i][INDEX_KEY];
-            resultValuesArray[i][INDEX_VALUE] = tempValuesArray[i][INDEX_VALUE];
-        }
+    }
+
+    private boolean equals(K keyOne, K keyTwo) {
+        return keyOne == null && keyTwo == null || keyOne != null && keyOne.equals(keyTwo);
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < resultValuesArray.length; i++) {
-            if ((resultValuesArray[i][INDEX_KEY] == null && key == null)
-                    || (tempValuesArray[i][INDEX_KEY] != null && key != null
-                    && tempValuesArray[i][INDEX_KEY].toString().equals(key.toString()))) {
-                return (V)resultValuesArray[i][INDEX_VALUE];
+        for (int i = 0; i < valuesArray.length; i++) {
+            if (equals(key,(K)valuesArray[i][INDEX_KEY])) {
+                return (V) valuesArray[i][INDEX_VALUE];
             }
         }
+
         return null;
     }
 
     @Override
     public int size() {
-        return resultValuesArray.length;
+        return arraySize;
     }
 }
+
+
