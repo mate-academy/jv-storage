a681dc4f7056f68da25cbd098aaa50edf6ffc91d
Dmitriy Sumaroka <sumaroka.dmitriy@gmail.com>
2023-11-06 12:36:54 +0200

    without fixed two tests: addTwoElementsWithSameKey,addTwoElementsWithNullKey

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,46 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    public static final int DEFAULT_ITEMS_NUMBER = 10;
+
+    private K[] keys;
+
+    private V[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[DEFAULT_ITEMS_NUMBER];
+        values = (V[]) new Object[DEFAULT_ITEMS_NUMBER];
+    }
+
+    public K[] getKeys() {
+        return keys;
+    }
+
+    public V[] getValues() {
+        return values;
+    }
+
     @Override
     public void put(K key, V value) {
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == key || keys[i] != null &&
+                    keys[i].equals(key)) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return this.size;
     }
 }
