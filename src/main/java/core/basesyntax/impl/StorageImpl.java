6361c8a730a32f84d592b0ae981a2b986cd79850
Dmitriy Sumaroka <sumaroka.dmitriy@gmail.com>
2023-11-06 16:30:19 +0200

    created isSame method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,9 +4,7 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     public static final int DEFAULT_ITEMS_NUMBER = 10;
-
     private K[] keys;
-
     private V[] values;
     private int size;
 
@@ -15,20 +13,16 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         values = (V[]) new Object[DEFAULT_ITEMS_NUMBER];
     }
 
-    public K[] getKeys() {
-        return keys;
-    }
-
-    public V[] getValues() {
-        return values;
+    private boolean isSame(K key, int i) {
+        return keys[i] == key || keys[i] != null && keys[i].equals(key);
     }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
+            if (isSame(key, i)) {
                 values[i] = value;
-                size--;
+                return;
             }
         }
         keys[size] = key;
@@ -39,7 +33,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
+            if (isSame(key, i)) {
                 return values[i];
             }
         }
@@ -48,6 +42,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return this.size;
+        return size;
     }
 }
