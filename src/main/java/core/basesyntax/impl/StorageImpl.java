75a142621c4767a41e94f6ab9df92f96f442d57f
Bohdan <bohdan.g.54@gmail.com>
2023-11-09 19:42:14 +0200

    successfully passed all tests

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,42 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.ArrayList;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private final ArrayList<K> kArray;
+    private final ArrayList<V> vArray;
+
+    public StorageImpl() {
+        kArray = new ArrayList<>();
+        vArray = new ArrayList<>();
+    }
+
     @Override
     public void put(K key, V value) {
+        if (kArray.contains(key)) {
+            int index = kArray.indexOf(key);
+            vArray.set(index, value);
+        } else {
+            kArray.add(key);
+            vArray.add(value);
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int index = kArray.indexOf(key);
+        return (index != -1) ? vArray.get(index) : null;
     }
 
     @Override
     public int size() {
-        return -1;
+        if (kArray.size() == vArray.size()) {
+            return kArray.size();
+        } else {
+            throw new RuntimeException("Size dont match");
+        }
     }
+
+
 }
