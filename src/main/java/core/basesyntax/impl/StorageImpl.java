070b982ff231109ff1f11b273a3e8b0c6160e58b
Bohdan <bohdan.g.54@gmail.com>
2023-11-10 14:03:58 +0200

    reformatting

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,18 +1,17 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-
 import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private K[] kArray;
-    private V[] vArray;
+    private K[] keysArray;
+    private V[] valuesArray;
     private V valueIfKeyIsNull = null;
 
     public StorageImpl() {
         // Unchecked cast warning !
-        kArray = (K[]) new Object[0];
-        vArray = (V[]) new Object[0];
+        keysArray = (K[]) new Object[0];
+        valuesArray = (V[]) new Object[0];
     }
 
     @Override
@@ -21,12 +20,12 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             valueIfKeyIsNull = value;
             return;
         }
-        int index = findElement(kArray, key);
+        int index = findElement(keysArray, key);
         if (index != -1) {
-            vArray[index] = value;
+            valuesArray[index] = value;
         } else {
-            kArray = addElem(kArray, key);
-            vArray = addElem(vArray, value);
+            keysArray = addElem(keysArray, key);
+            valuesArray = addElem(valuesArray, value);
         }
     }
 
@@ -35,23 +34,25 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         if (key == null) {
             return valueIfKeyIsNull;
         }
-        int index = findElement(kArray, key);
-        return (index != -1) ? vArray[index] : null;
+        int index = findElement(keysArray, key);
+        return (index != -1) ? valuesArray[index] : null;
     }
 
     @Override
     public int size() {
         int size;
-        if (kArray.length == vArray.length) {
-            size = kArray.length;
+        if (keysArray.length == valuesArray.length) {
+            size = keysArray.length;
         } else {
             throw new RuntimeException("Size dont match");
         }
 
         // We save value for null key in other cell, not to break array structure
-        // If this cell is not empty (not null), so we have additional value in our array technically
+        // If cell is not empty (not null), so we have additional value in our array technically
         // So we should increment size
-        if (valueIfKeyIsNull != null) size++;
+        if (valueIfKeyIsNull != null) {
+            size++;
+        }
         return size;
     }
 
