cb85c7a3c8a632bc10477f0148a2e2ec864fb7cd
Bohdan Chupika <boroda4436@users.noreply.github.com>
2023-11-13 13:06:05 +0200

    remove encoding config from checkstyle plugin

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,46 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private final V[] values;
+    private final K[] keys;
+    private int size;
+
+    public StorageImpl() {
+        this.values = (V[]) new Object[MAX_ITEMS_NUMBER];
+        this.keys = (K[]) new Object[MAX_ITEMS_NUMBER];
+        this.size = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if ( key == keys[i] && key != null || key.equals(keys[i])) {
+                values[i] = value;
+                return;
+            }
+        }
+
+        if (size >= MAX_ITEMS_NUMBER) {
+            throw new RuntimeException("Storage is full. Cannot add more elements");
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] && key != null && key.equals(keys[i])) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
