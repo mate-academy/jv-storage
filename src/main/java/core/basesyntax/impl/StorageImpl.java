5424486cc836011cd9ba3e7cb40b9ac99b77f8a2
Ivan <lavrinenkoivan51@gmail.com>
2023-11-13 14:53:04 +0100

    corrected initialization in constructor and simplified check for existing key in put method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,27 +4,24 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_STORAGE_SIZE = 10;
-    private final Object[] keys;
-    private final Object[] values;
+    private final K[] keys;
+    private final V[] values;
     private int actualStorageSize;
 
     public StorageImpl() {
-        keys = new Object[MAX_STORAGE_SIZE];
-        values = new Object[MAX_STORAGE_SIZE];
-        actualStorageSize = 0;
+        keys = (K[]) new Object[MAX_STORAGE_SIZE];
+        values = (V[]) new Object[MAX_STORAGE_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
-        boolean keyExists = false;
         for (int i = 0; i < actualStorageSize; i++) {
             if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
-                keyExists = true;
                 values[i] = value;
-                break;
+                return;
             }
         }
-        if (actualStorageSize < MAX_STORAGE_SIZE && !keyExists) {
+        if (actualStorageSize < MAX_STORAGE_SIZE) {
             keys[actualStorageSize] = key;
             values[actualStorageSize] = value;
             actualStorageSize++;
@@ -36,7 +33,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         if (actualStorageSize > 0) {
             for (int i = 0; i < actualStorageSize; i++) {
                 if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
-                    return (V) values[i];
+                    return values[i];
                 }
             }
         }
