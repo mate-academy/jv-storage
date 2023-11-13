358f4d5912915a55c569606646dcf9b5262b45b1
Bohdan Chupika <boroda4436@users.noreply.github.com>
2023-11-13 13:06:05 +0200

    Changed order of private static to static private.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,8 +5,8 @@ import core.basesyntax.Storage;
 public class StorageImpl<K, V> implements Storage<K, V> {
     private K[] keys;
     private V[] values;
-    private static int DEFAULT_SIZE = 10;
-    private static int RESIZE_COEFFICIENT = 2;
+    static private int DEFAULT_SIZE = 10;
+    static private int RESIZE_COEFFICIENT = 2;
     private int size;
     
     public StorageImpl() {
