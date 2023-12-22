2e9b36d7c4b360f1cc7d9d3e1f081293b64bd3b1
Tetiana Khrestina <khrestinatatiana@gmail.com>
2023-12-22 21:17:38 +0200

    1. Initialized arrays in constructor 2. Avoided duplicating by creating getIndex method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,70 +4,40 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
-    private final Object[] keys = new Object[MAX_SIZE];
-    private final Object[] values = new Object[MAX_SIZE];
+    private final K[] keys;
+    private final V[] values;
     private int size = 0;
 
-    @Override
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_SIZE];
+        values = (V[]) new Object[MAX_SIZE];
+    }
+
     public void put(K key, V value) {
-        if (key == null) {
-            putNullKey(value);
-            return;
-        }
-
-        for (int i = 0; i < size; i++) {
-            if (keys[i] != null && keys[i].equals(key)) {
-                values[i] = value;
-                return;
-            }
-        }
-
-        if (size < MAX_SIZE) {
+        int index = getIndex(key);
+        if (index != -1) {
+            values[index] = value;
+        } else if (size < MAX_SIZE) {
             keys[size] = key;
             values[size] = value;
             size++;
         }
     }
 
-    private void putNullKey(V value) {
-        for (int i = 0; i < size; i++) {
-            if (keys[i] == null) {
-                values[i] = value;
-                return;
-            }
-        }
-
-        if (size < MAX_SIZE) {
-            keys[size] = null;
-            values[size] = value;
-            size++;
-        }
-    }
-
-    @Override
     public V get(K key) {
-        if (key == null) {
-            return getNullKey();
-        }
-
-        for (int i = 0; i < size; i++) {
-            if (keys[i] != null && keys[i].equals(key)) {
-                return (V) values[i];
-            }
-        }
-        return null;
+        int index = getIndex(key);
+        return index != -1 ? values[index] : null;
     }
 
-    private V getNullKey() {
+    private int getIndex(K key) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] == null) {
-                return (V) values[i];
+            if ((keys[i] == null && key == null) || (keys[i] != null && keys[i].equals(key))) {
+                return i;
             }
         }
-        return null;
+        return -1;
     }
 
-    @Override
     public int size() {
         return size;
     }
