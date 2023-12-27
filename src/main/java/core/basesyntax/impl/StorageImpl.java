0ee8910574abb350332b7ce301618b8f677e70e1
Illya Pantazi <illios.piv@gmail.com>
2023-12-28 00:31:06 +0100

    Implemented the `Storage` of the key-value type. Created the loops in the methods `Put` and `Get` to compare, write and read the key-value pair. Implemented calculation of the Storage size in the method `Size`.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,48 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private int counter = 0;
+    private final Object[] arrayK;
+    private final Object[] arrayV;
+
+    public StorageImpl() {
+        arrayK = new Object[MAX_ITEMS_NUMBER];
+        arrayV = new Object[MAX_ITEMS_NUMBER];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < MAX_ITEMS_NUMBER; i++) {
+            if (arrayK[i] == null && arrayV[i] == null) {
+                arrayK[i] = key;
+                arrayV[i] = value;
+                counter++;
+                return;
+            }
+            if (Objects.equals(arrayK[i], key)) {
+                arrayV[i] = value;
+                return;
+            }
+
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < MAX_ITEMS_NUMBER; i++) {
+            if (Objects.equals(arrayK[i], key)) {
+                return (V) arrayV[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return counter;
     }
 }
