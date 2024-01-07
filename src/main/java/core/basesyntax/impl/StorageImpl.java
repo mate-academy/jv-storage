9db50a3317295f88c0bd26f9b5e10182f00760ac
Anton <StarAntonU@gmail.com>
2024-01-08 01:05:26 +0200

    add constant MAX_ITEMS_NUMBER

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,7 +2,15 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.lang.reflect.Array;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+
+    public StorageImpl() {
+
+    }
+
     @Override
     public void put(K key, V value) {
     }
