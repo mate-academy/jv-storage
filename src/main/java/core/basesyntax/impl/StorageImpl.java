4dd211829261ea73ffcf97c5c9be06bc65c790cc
Vova Maryniak <volodimirmarinak27@gmail.com>
2024-01-11 17:18:09 +0200

    hw-jv-storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,24 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private final Map<K, V> storageMap = new HashMap<>();
+
     @Override
     public void put(K key, V value) {
+        storageMap.put(key, value);
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return storageMap.get(key);
     }
 
     @Override
     public int size() {
-        return -1;
+        return storageMap.size();
     }
 }
