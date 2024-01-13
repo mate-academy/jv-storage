e20ee7231ac6b6416285e85c01d3eea75607c724
Yana Stepanova <yanna.stepanova@gmail.com>
2024-01-13 22:44:52 +0200

    fixed after review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,52 +1,42 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_NUMBER = 10;
-    private Pair<K, V>[] pairs;
+    private static final int ZERO = 0;
+    private int size;
+    private K[] keys;
+    private V[] values;
 
     public StorageImpl() {
-        this.pairs = new Pair[MAX_NUMBER];
+        this.size = ZERO;
+        this.keys = (K[]) new Object[MAX_NUMBER];
+        this.values = (V[]) new Object[MAX_NUMBER];
+    }
+
+    private boolean compare(K keyLeft, K keyRight) {
+        return keyLeft == keyRight || keyLeft != null && keyLeft.equals(keyRight);
     }
 
     @Override
     public void put(K key,V value) {
-        int filledSize = size();
-        if (filledSize == 0) {
-            pairs[filledSize] = new Pair(key, value);
-        }
-        if (filledSize <= MAX_NUMBER) {
-            if (get(key) == null) {
-                for (int i = 0; i < pairs.length; i++) {
-                    if (pairs[i] == null) {
-                        pairs[i] = new Pair(key, value);
-                        break;
-                    }
-                }
-            }
-            if (get(key) != null) {
-                for (int i = 0; i < filledSize; i++) {
-                    if ((pairs[i].myKey == null && key == null)
-                            || ((pairs[i].myKey != null && key != null)
-                                    && pairs[i].myKey.equals(key))) {
-                        pairs[i].myValue = value;
-                    }
-                }
+        for (int i = 0; i < size; i++) {
+            if (compare(keys[i], key)) {
+                values[i] = value;
+                return;
             }
         }
-
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        int length = size();
-        for (int i = 0; i < length; i++) {
-            if ((pairs[i].myKey == null && key == null)
-                    || ((pairs[i].myKey != null && key != null)
-                           && pairs[i].myKey.equals(key))) {
-                return (V) pairs[i].myValue;
+        for (int i = 0; i < size; i++) {
+            if (compare(keys[i], key)) {
+                return (V) values[i];
             }
         }
         return null;
@@ -54,32 +44,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        int count = 0;
-        for (Pair pair : pairs) {
-            if (pair != null) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    private static class Pair<K, V> {
-        private K myKey;
-        private V myValue;
-
-        Pair(K key, V value) {
-            this.myKey = key;
-            this.myValue = value;
-        }
-
-        @Override
-        public boolean equals(Object key) {
-            K rightObj = (K) key;
-            if (this.myKey == null && rightObj == null) {
-                return true;
-            } else {
-                return Objects.equals(this.myKey, rightObj);
-            }
-        }
+        return size;
     }
 }
