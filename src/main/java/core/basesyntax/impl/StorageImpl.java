c18f2f3976a4571377d6b80209ebf840c0cb95c9
Dominik Gągało <cliffcamer@gmail.com>
2024-01-18 17:03:15 +0100

    StorageImpl HW

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,54 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_CAPACITY = 10;
+    private Object[] keys;
+    private Object[] values;
+    private int size;
+
+    private int indexOfKey(K key) {
+        for(int i = 0; i < size; i++) {
+            if(key != null && key.equals(keys[i]) || key == null && keys[i] == null) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public StorageImpl() {
+        keys = new Object[MAX_CAPACITY];
+        values = new Object[MAX_CAPACITY];
+        size = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = indexOfKey(key);
+
+        if (index != -1) {
+            values[index] = value;
+        } else {
+            if (size < MAX_CAPACITY) {
+                keys[size] = key;
+                values[size] = value;
+                size++;
+            } else {
+                throw new IllegalStateException("Storage is full");
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        int index = indexOfKey(key);
+        if (index != -1) {
+            return (V) values[index];
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
