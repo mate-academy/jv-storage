bd29dba5a01ea260c07bf236b235cf5cecf867b1
breakingtumble <yaroslavglavatsky@gmail.com>
2024-01-22 21:34:03 +0200

    fix: remove useless constants and non readable expressions

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,35 +3,32 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int DOUBLE_ARRAY_SIZE = 20;
     private static final int ARRAY_SIZE = 10;
-    private Object[] array = new Object[DOUBLE_ARRAY_SIZE];
-    private int arrayIterator = 0;
-    private int sizeCounter = 0;
+    private Object[] array = new Object[ARRAY_SIZE * 2];
+    private int iterator = 0;
+    private int size = 0;
 
     @Override
     public void put(K key, V value) {
         if (get(key) != null) {
-            int currentArraySize = sizeCounter * 2 - 1;
-            for (int i = 0; i < currentArraySize; i += 2) {
+            for (int i = 0; i < array.length; i += 2) {
                 if (key == array[i] || (key != null && key.equals(array[i]))) {
                     array[i + 1] = value;
                     break;
                 }
             }
         } else {
-            if (sizeCounter <= ARRAY_SIZE) {
-                array[arrayIterator++] = key;
-                array[arrayIterator++] = value;
-                sizeCounter++;
+            if (size <= ARRAY_SIZE) {
+                array[iterator++] = key;
+                array[iterator++] = value;
+                size++;
             }
         }
     }
 
     @Override
     public V get(K key) {
-        int currentArraySize = sizeCounter * 2 - 1;
-        for (int i = 0; i < currentArraySize; i += 2) {
+        for (int i = 0; i < array.length; i += 2) {
             if (key == array[i] || (key != null && key.equals(array[i]))) {
                 return (V) array[i + 1];
             }
@@ -41,6 +38,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return sizeCounter;
+        return size;
     }
 }
