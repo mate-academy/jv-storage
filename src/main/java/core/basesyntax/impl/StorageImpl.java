55ebb6b8ccb105e15e19f131c82dba00f6a7768f
breakingtumble <yaroslavglavatsky@gmail.com>
2024-01-24 17:59:22 +0200

    fix: remove iterator variable, refactored code

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,26 +1,27 @@
 package core.basesyntax.impl;
-
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int ARRAY_SIZE = 10;
     private Object[] array = new Object[ARRAY_SIZE * 2];
-    private int iterator = 0;
     private int size = 0;
 
     @Override
     public void put(K key, V value) {
-        if (get(key) != null) {
-            for (int i = 0; i < array.length; i += 2) {
+        boolean keyIsNotPresent = true;
+        for (int i = 0; i < array.length; i += 2) {
+            if (i < size * 2) {
                 if (key == array[i] || (key != null && key.equals(array[i]))) {
                     array[i + 1] = value;
+                    keyIsNotPresent = false;
                     break;
                 }
             }
-        } else {
+        }
+        if (keyIsNotPresent) {
             if (size <= ARRAY_SIZE) {
-                array[iterator++] = key;
-                array[iterator++] = value;
+                array[size * 2] = key;
+                array[size * 2 + 1] = value;
                 size++;
             }
         }
