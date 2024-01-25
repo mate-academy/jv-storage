ec241bfd5e5837011ea82b85e60a6b60888fa368
breakingtumble <yaroslavglavatsky@gmail.com>
2024-01-25 13:53:23 +0200

    fix: fix issues

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -9,27 +9,22 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     public StorageImpl() {
         array = new Object[ARRAY_SIZE * 2];
-        size = 0;
     }
 
     @Override
     public void put(K key, V value) {
-        boolean keyIsNotPresent = true;
         for (int i = 0; i < array.length; i += 2) {
             if (i < size * 2) {
                 if (key == array[i] || (key != null && key.equals(array[i]))) {
                     array[i + 1] = value;
-                    keyIsNotPresent = false;
-                    break;
+                    return;
                 }
             }
         }
-        if (keyIsNotPresent) {
-            if (size <= ARRAY_SIZE) {
-                array[size * 2] = key;
-                array[size * 2 + 1] = value;
-                size++;
-            }
+        if (size <= ARRAY_SIZE) {
+            array[size * 2] = key;
+            array[size * 2 + 1] = value;
+            size++;
         }
     }
 
