e2fdf69feb9a639120a675b98321361401676f65
Zorii Bohdan <bodkazorii@gmail.com>
2024-01-29 13:20:09 +0200

    implemented custom map storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,47 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    public static final int CAPACITY = 10;
+
+    private Object[] keys;
+    private Object[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = new Object[CAPACITY];
+        values = new Object[CAPACITY];
+        size = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        int indexOfKey = getKeyIndex(key);
+        if (indexOfKey == -1) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        } else {
+            values[indexOfKey] = value;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int indexOfKey = getKeyIndex(key);
+        return indexOfKey == -1 ? null : (V) values[indexOfKey];
     }
 
     @Override
     public int size() {
+        return size;
+    }
+
+    private int getKeyIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || keys[i] != null && keys[i].equals(key)) {
+                return i;
+            }
+        }
         return -1;
     }
 }
