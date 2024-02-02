f544ae335da4371af9f492fe423d3cbbaf1b825b
Maryna Bodina <bananafox2002@gmail.com>
2024-02-02 15:54:12 +0200

    "done"

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -17,29 +17,44 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         if (key == null) {
-            return;
-        }
-        for (int i = 0; i < size; i++) {
-            if (key.equals(keys[i])) {
-                values[i] = value;
-                return;
+            for (int i = 0; i < size; i++) {
+                if (keys[i] == null) {
+                    values[i] = value;
+                    return;
+                }
             }
-        }
-        if (size < MAX_SIZE) {
             keys[size] = key;
             values[size] = value;
             size++;
         } else {
-            throw new IllegalStateException("Storage is full");
+            for (int i = 0; i < size; i++) {
+                if (key.equals(keys[i])) {
+                    values[i] = value;
+                    return;
+                }
+            }
+
+            if (size < MAX_SIZE) {
+                keys[size] = key;
+                values[size] = value;
+                size++;
+            } else {
+                throw new IllegalStateException("Storage is full");
+            }
         }
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public V get(K key) {
         if (key == null) {
+            for (int i = 0; i < size; i++) {
+                if (keys[i] == null) {
+                    return (V) values[i];
+                }
+            }
             return null;
         }
+
         for (int i = 0; i < size; i++) {
             if (key.equals(keys[i])) {
                 return (V) values[i];
