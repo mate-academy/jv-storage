c651e80ed9289bda97ffb620893b5be99dbdb768
Vlad <mazafackery@gmail.com>
2024-02-04 14:04:54 +0200

    fixed all problem

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,50 +3,50 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int ROW_COUNT = 10;
-    private static final int COLUMN_COUNT = 2;
-    private static final int INITIAL_VALUE = 0;
-    private Object[][] storageArray;
+    private static final int MAX_VALUE = 10;
+    private K[] keyList;
+    private V[] valueList;
+    private int count = 0;
 
     public StorageImpl() {
-        storageArray = new Object[ROW_COUNT][COLUMN_COUNT];
+        keyList = (K[]) new Object[MAX_VALUE];
+        valueList = (V[]) new Object[MAX_VALUE];
     }
 
     @Override
     public void put(K key, V value) {
-        if (key != null && value != null) {
-            for (int i = 0; i < storageArray.length; i++) {
-                if ((storageArray[i][0] == null) || storageArray[i][0].equals(key)) {
-                    storageArray[i][0] = key;
-                    storageArray[i][1] = value;
-                    break;
-                }
-            }
+        int index = getIndex(key);
+        if (index != -1) {
+            valueList[index] = value;
+        } else {
+            keyList[count] = key;
+            valueList[count] = value;
+            count++;
         }
     }
 
     @Override
     public V get(K key) {
-        int number = 0;
-        for (int i = 0; i < storageArray.length; i++) {
-            if (key.equals(storageArray[i][0]) || (key == null && storageArray[i][0] == null)) {
-                number = i;
-                return (V) storageArray[i][1];
-            }
+        int index = getIndex(key);
+        if (index != -1) {
+            return (V) valueList[index];
+        } else {
+            return null;
         }
-        return null;
     }
 
     @Override
     public int size() {
-        int count = 0;
-        for (int i = 0; i < storageArray.length; i++) {
-            if (storageArray[i][0] == null && storageArray[i][1] == null) {
-                break;
-            } else {
-                count++;
+        return count;
+    }
+
+    private int getIndex(K key) {
+        for (int i = 0; i < size(); i++) {
+            if ((keyList[i] == null && key == null)
+                    || (keyList[i] != null && keyList[i].equals(key))) {
+                return i;
             }
         }
-        return count;
+        return -1;
     }
 }
