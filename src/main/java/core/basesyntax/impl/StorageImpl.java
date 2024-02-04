f1670aac241458e4531b8dac91ee87e7233fd415
Kacper <kacperkukula97@gmail.com>
2024-02-04 16:02:32 +0100

    First version. Implemented generic class functionality.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,61 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private static final int MAXIMUM_STORAGE = 10;
+    private static final int KEY_NOT_FOUND = -1;
+    private int currentIndex = 0;
+    private Object[] keys = new Object[1];
+    private Object[] values = new Object[1];
+
     @Override
     public void put(K key, V value) {
+        boolean isValueReplaced = replaceIfKeyExists(key, value);
+
+        if (!isValueReplaced) {
+            widenTheArrayIfNeeded();
+            keys[currentIndex] = key;
+            values[currentIndex] = value;
+            currentIndex++;
+        }
+    }
+
+    private boolean replaceIfKeyExists(K key, V value) {
+        int indexOfKey = findIndexOfKey(key);
+        if (indexOfKey != KEY_NOT_FOUND) {
+            values[indexOfKey] = value;
+            return true;
+        }
+        return false;
+    }
+
+    private void widenTheArrayIfNeeded() {
+        if (currentIndex > 0 && currentIndex < MAXIMUM_STORAGE) {
+            keys = Arrays.copyOf(keys, keys.length + 1);
+            values = Arrays.copyOf(values, values.length + 1);
+        }
+    }
+
+    private int findIndexOfKey(K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if ((keys[i] == key) || (keys[i] != null && keys[i].equals(key))) {
+                return i;
+            }
+        }
+        return KEY_NOT_FOUND;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int indexOfKey = findIndexOfKey(key);
+        return (indexOfKey != KEY_NOT_FOUND) ? (V) values[indexOfKey] : null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return currentIndex;
     }
 }
