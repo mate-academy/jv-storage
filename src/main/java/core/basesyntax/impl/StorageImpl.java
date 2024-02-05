c32d5142aa52532952de1f0a56886ba3d0ed7096
Kacper <kacperkukula97@gmail.com>
2024-02-05 22:23:25 +0100

    Second version. Minor fixes, added constructor.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,12 +4,20 @@ import core.basesyntax.Storage;
 import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-
+    private static final int DEFAULT_CURRENT_INDEX = 0;
+    private static final int DEFAULT_STORAGE = 1;
     private static final int MAXIMUM_STORAGE = 10;
     private static final int KEY_NOT_FOUND = -1;
-    private int currentIndex = 0;
-    private Object[] keys = new Object[1];
-    private Object[] values = new Object[1];
+
+    private int currentIndex;
+    private K[] keys;
+    private V[] values;
+
+    public StorageImpl() {
+        currentIndex = DEFAULT_CURRENT_INDEX;
+        keys = (K[]) new Object[DEFAULT_STORAGE];
+        values = (V[]) new Object[DEFAULT_STORAGE];
+    }
 
     @Override
     public void put(K key, V value) {
@@ -18,18 +26,19 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         if (!isValueReplaced) {
             widenTheArrayIfNeeded();
             keys[currentIndex] = key;
-            values[currentIndex] = value;
-            currentIndex++;
+            values[currentIndex++] = value;
         }
     }
 
     private boolean replaceIfKeyExists(K key, V value) {
         int indexOfKey = findIndexOfKey(key);
-        if (indexOfKey != KEY_NOT_FOUND) {
-            values[indexOfKey] = value;
-            return true;
+
+        if (indexOfKey == KEY_NOT_FOUND) {
+            return false;
         }
-        return false;
+
+        values[indexOfKey] = value;
+        return true;
     }
 
     private void widenTheArrayIfNeeded() {
@@ -51,7 +60,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         int indexOfKey = findIndexOfKey(key);
-        return (indexOfKey != KEY_NOT_FOUND) ? (V) values[indexOfKey] : null;
+        return (indexOfKey == KEY_NOT_FOUND) ? null : (V) values[indexOfKey];
     }
 
     @Override
