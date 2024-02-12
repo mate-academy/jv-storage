a5edded4ffbc334d097f5db2fab9b61bc1c7d191
Evhen Timofeiev <timofeevzhenya2107@gmail.com>
2024-02-12 02:21:06 +0200

    replaced Oblects class methods

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,24 +1,25 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
     private static final int KEY_INDEX = 0;
     private static final int VALUE_INDEX = 1;
+    private static final int VALUE_AND_INDEX = 2;
     private final Object[][] keyValuePairs;
     private int size;
 
     public StorageImpl() {
-        keyValuePairs = new Object[MAX_SIZE][2];
+        keyValuePairs = new Object[MAX_SIZE][VALUE_AND_INDEX];
         size = 0;
     }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(key, keyValuePairs[i][KEY_INDEX])) {
+            if (keyValuePairs[i][KEY_INDEX] == null ? key == null :
+                    keyValuePairs[i][KEY_INDEX].equals(key)) {
                 keyValuePairs[i][VALUE_INDEX] = value;
                 return;
             }
@@ -35,7 +36,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(key, keyValuePairs[i][KEY_INDEX])) {
+            if (keyValuePairs[i][KEY_INDEX] == null ? key == null :
+                    keyValuePairs[i][KEY_INDEX].equals(key)) {
                 return (V) keyValuePairs[i][VALUE_INDEX];
             }
         }
