52688548312ad645e6aca0c5770a41d3a3809c2c
DmitryLazyuka <83352819+DmitryLazyuka@users.noreply.github.com>
2024-02-13 13:36:21 +0200

    Added checking the repository key for null when adding a new item

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -16,7 +16,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < keys.length; i++) {
-            if (values[i] == null) {
+            if (keys[i] == null && values[i] == null) {
                 keys[i] = key;
                 values[i] = value;
                 size++;
