92b20ddbe3de49503fb033c06ac685f7d0ff59a3
Bohdan Markatov <bogdanmarkatov@gmail.com>
2024-02-14 18:15:13 +0200

    removed redundant methodsÐ

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,43 +3,15 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int DEFAULT_INITIAL_CAPACITY = 10;
-    private static final float DEFAULT_LOAD_FACTOR = 0.5f;
-    private static final float DEFAULT_CAPACITY_INCREASE_FACTOR = 1.5f;
-
-    private int initialCapacity;
-    private float loadFactor;
-    private float capacityIncreaseFactor;
-    private StorageObject<K, V>[] storage;
+    private static final int DEFAULT_LENGTH = 10;
     private int size;
+    private StorageObject<K, V>[] storage;
 
     public StorageImpl() {
-        initialCapacity = DEFAULT_INITIAL_CAPACITY;
-        loadFactor = DEFAULT_LOAD_FACTOR;
-        capacityIncreaseFactor = DEFAULT_CAPACITY_INCREASE_FACTOR;
-        this.storage = new StorageObject[initialCapacity];
+        storage = new StorageObject[DEFAULT_LENGTH];
     }
 
-    public StorageImpl(int initialCapacity, float loadFactor, float capacityIncreaseFactor) {
-        if (initialCapacity > 0) {
-            this.initialCapacity = initialCapacity;
-        } else {
-            this.initialCapacity = DEFAULT_INITIAL_CAPACITY;
-        }
-        if (loadFactor >= 0.1f && loadFactor <= 0.9f) {
-            this.loadFactor = loadFactor;
-        } else {
-            this.loadFactor = DEFAULT_LOAD_FACTOR;
-        }
-        if (capacityIncreaseFactor >= 1.1f && capacityIncreaseFactor <= 2) {
-            this.capacityIncreaseFactor = capacityIncreaseFactor;
-        } else {
-            this.capacityIncreaseFactor = DEFAULT_CAPACITY_INCREASE_FACTOR;
-        }
-        this.storage = new StorageObject[this.initialCapacity];
-    }
-
-    public class StorageObject<K, V> {
+    private class StorageObject<K, V> {
         private K key;
         private V value;
 
@@ -48,7 +20,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             this.value = value;
         }
 
-        public boolean keyCheck(K key) {
+        private boolean keyCheck(K key) {
             return ((key == null && this.key == null)
                     || (this.key != null && this.key.equals(key)));
         }
@@ -62,9 +34,12 @@ public class StorageImpl<K, V> implements Storage<K, V> {
                 return;
             }
         }
-        this.checkingForCompleteness();
-        storage[size] = new StorageObject<>(key, value);
-        size++;
+        if (size < DEFAULT_LENGTH) {
+            storage[size] = new StorageObject<>(key, value);
+            size++;
+            return;
+        }
+        throw new StorageIndexOutOfBoundsException(DEFAULT_LENGTH);
     }
 
     @Override
@@ -75,55 +50,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             }
         }
         return null;
-    }
-
-    public V getByHashCode(K key) {
-        for (int i = 0; i < size; i++) {
-            if ((key == null && storage[i].key == null)
-                    || (storage[i].key != null && key != null
-                    && storage[i].key.hashCode() == key.hashCode())) {
-                return storage[i].value;
-            }
-        }
-        return null;
-    }
-
-    private void checkingForCompleteness() {
-        if (((float) size / storage.length) > loadFactor) {
-            StorageObject<K, V>[] newStorage
-                    = new StorageObject[(int) (storage.length * capacityIncreaseFactor)];
-            for (int i = 0; i < size; i++) {
-                newStorage[i] = storage[i];
-            }
-            storage = newStorage;
-        }
-    }
-
-    public void remove(K key) {
-        int index = -1;
-        boolean indexFound = false;
-        for (int i = 0; i < size; i++) {
-            if (storage[i].keyCheck(key)) {
-                index = i;
-                break;
-            }
-        }
-        if (index != -1) {
-            StorageObject<K, V>[] newStorage = new StorageObject[storage.length - 1];
-            for (int i = 0; i < size; i++) {
-                if (i == index) {
-                    indexFound = true;
-                } else {
-                    if (indexFound) {
-                        newStorage[i - 1] = storage[i];
-                    } else {
-                        newStorage[i] = storage[i];
-                    }
-                }
-            }
-            storage = newStorage;
-            size--;
-        }
     }
 
     @Override
