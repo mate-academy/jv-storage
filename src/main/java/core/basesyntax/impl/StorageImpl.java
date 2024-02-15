4d489f94bb381ed2fc9fdb489e35fed44aac6b5d
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-02-15 16:44:15 +0200

    added hw-solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,44 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int CAPACITY = 10;
+    private int size = 0;
+    private Object[] keyArray;
+    private Object[] valueArray;
+
+    public StorageImpl() {
+        keyArray = new Object[CAPACITY];
+        valueArray = new Object[CAPACITY];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (key == keyArray[i] || key != null && key.equals(keyArray[i])) {
+                valueArray[i] = value;
+                return;
+            }
+        }
+
+        if (size < CAPACITY) {
+            keyArray[size] = key;
+            valueArray[size] = value;
+            size++;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == keyArray[i] || key != null && key.equals(keyArray[i])) {
+                return (V) valueArray[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
