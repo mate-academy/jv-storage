da1a056f9062088a02fa9033697b474f9cbfa4f9
Oleksandr Farion <sashafarion@gmail.com>
2024-02-19 17:21:29 +0200

    added constants and two arrays for keys and values.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,49 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_STORAGE_SIZE = 10;
+    private static final int NOT_FOUND = -1;
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_STORAGE_SIZE];
+        values = (V[]) new Object[MAX_STORAGE_SIZE];
+        size = 0;
+    }
     @Override
     public void put(K key, V value) {
+        int index = 0;
+        if (size != 0) {
+            index = findIndex(key);
+        }
+        if (index == NOT_FOUND || index == 0) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        } else {
+            values[index] = value;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+            return values[findIndex(key)];
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    private int findIndex(K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] != null && keys[i].equals(key)) {
+                return i;
+            }
+        }
+        return NOT_FOUND;
     }
 }
