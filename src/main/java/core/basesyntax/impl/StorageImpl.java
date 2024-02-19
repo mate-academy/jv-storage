109f8a7eb80df40e7cb4c80b0200efc185a89b70
HAIOVIY <gaevoywork@gmail.com>
2024-02-19 15:55:32 +0200

    implemented methods from Storage interface

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,44 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ELEMENT = 10;
+    private K[] keyArray = (K[]) new Object[MAX_ELEMENT];
+    private V[] valueArray = (V[]) new Object[MAX_ELEMENT];
+    private int size;
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < keyArray.length; i++) {
+            if (keyArray[i] == null && valueArray[i] == null) {
+                keyArray[i] = key;
+                valueArray[i] = value;
+                size++;
+                return;
+            } else if (key != null && keyArray[i] != null && keyArray[i].equals(key)) {
+                valueArray[i] = value;
+                return;
+            } else if (key == null && keyArray[i] == key) {
+                valueArray[i] = value;
+                return;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keyArray.length; i++) {
+            if (key == null && keyArray[i] == null) {
+                return valueArray[i];
+            }
+            if (key != null && keyArray[i] != null && keyArray[i].equals(key)) {
+                return valueArray[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
