55a5f91dec2e108d85bbbd4da1221155cf122226
Shr1mpa <aminakrevetka@gmail.com>
2024-02-21 15:31:03 +0200

    hw solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,57 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static int STOREGE_CAPACITY = 10;
+    private static int NOT_FOUND = -1;
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[STOREGE_CAPACITY];
+        values = (V[]) new Object[STOREGE_CAPACITY];
+        this.size = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        if (size == STOREGE_CAPACITY) {
+            return;
+        }
+        if (get(key) == null) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        } else {
+            int index = getIndex(key);
+            keys[index] = key;
+            values[index] = value;
+        }
     }
 
     @Override
     public V get(K key) {
+        int index = getIndex(key);
+        if (index != NOT_FOUND) {
+            return values[index];
+        }
         return null;
+    }
+
+    private int getIndex(K key) {
+        for (int i = 0; i < STOREGE_CAPACITY; i++) {
+            if (Objects.equals(keys[i], key)) {
+                return i;
+            }
+        }
+        return NOT_FOUND;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
