fcebe31358d112952c6f34eba299a7b06d570ee8
Vasyl Arabchuk <arabon3@gmail.com>
2024-02-22 21:37:28 +0200

    Optimization of key-value storage code

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,33 +6,30 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int SIZE_OF_ARRAY = 10;
     private K[] keys;
     private V[] values;
-    private int counter;
+    private int size;
 
     public StorageImpl() {
-        this.keys = (K[])(new Object[SIZE_OF_ARRAY]);
-        this.values = (V[])(new Object[SIZE_OF_ARRAY]);
+        keys = (K[])(new Object[SIZE_OF_ARRAY]);
+        values = (V[])(new Object[SIZE_OF_ARRAY]);
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < keys.length; i++) {
-            if ((key == keys[i]
-                    || keys[i] != null && keys[i].equals(key)) && values[i] != null) {
-                keys[i] = key;
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
                 values[i] = value;
                 return;
             }
         }
-        keys[counter] = key;
-        values[counter] = value;
-        counter++;
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keys.length; i++) {
-            if ((key == keys[i]
-                    || keys[i] != null && keys[i].equals(key)) && values[i] != null) {
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
                 return values[i];
             }
         }
@@ -41,6 +38,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return counter;
+        return size;
     }
 }
