d71eaa0c8d1957d7bcb02ac221a9ad8fc649041b
Chopyk Mykola <record7654@gmail.com>
2024-02-24 00:15:54 +0200

    Created storage implementation

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,52 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int NOT_EXISTS = -1;
+    private static final int MAX_SIZE = 10;
+    private K[] keys;
+    private V[] values;
+    private int currentSize;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_SIZE];
+        values = (V[]) new Object[MAX_SIZE];
+        currentSize = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        int isAvailablePosition = availableKey(key);
+        if (isAvailablePosition != NOT_EXISTS) {
+            values[isAvailablePosition] = value;
+            return;
+        }
+        if (currentSize < MAX_SIZE) {
+            keys[currentSize] = key;
+            values[currentSize] = value;
+            currentSize++;
+        }
     }
 
     @Override
     public V get(K key) {
+        int availablePosition = availableKey(key);
+        if (availablePosition != NOT_EXISTS) {
+            return values[availablePosition];
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return currentSize;
+    }
+
+    private int availableKey(K key) {
+        for (int i = 0; i < currentSize; i++) {
+            if (keys[i] == null && key == null || keys[i] != null && keys[i].equals(key)) {
+                return i;
+            }
+        }
+        return NOT_EXISTS;
     }
 }
