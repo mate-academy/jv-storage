d45e14f95d713af11ef5d0b869fc3b9d0ddbba76
Valentyn Sharshon <woosay333@gmail.com>
2024-02-23 14:47:36 +0200

    Implemented the Storage of the key-value type that is parameterized with two types of data

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,67 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.StringJoiner;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final String DELIMITER = ", ";
+    private static final String PREFIX = "[";
+    private static final String SUFFIX = "]";
+    private static final String STRING_FORMAT = "%s : %s";
+
+    private final Pair<K, V>[] storage;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public StorageImpl() {
+        storage = new Pair[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = findPairByKey(key);
+        if (index == -1) {
+            storage[size++] = new Pair<>(key, value);
+        } else {
+            storage[index] = new Pair<>(key, value);
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int index = findPairByKey(key);
+        if (index == -1) {
+            return null;
+        } else {
+            return storage[index].value();
+        }
+    }
+
+    private int findPairByKey(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == storage[i].key() || (key != null && key.equals(storage[i].key()))) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    private record Pair<K, V>(K key, V value) {
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner stringJoiner = new StringJoiner(DELIMITER, PREFIX, SUFFIX);
+        for (int i = 0; i < size; i++) {
+            stringJoiner.add(String.format(STRING_FORMAT, storage[i].key(), storage[i].value));
+        }
+        return stringJoiner.toString();
     }
 }
