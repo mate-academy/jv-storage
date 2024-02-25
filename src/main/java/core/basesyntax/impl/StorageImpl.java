10aedc2b8b8531d80eed7897f0944452e0eacded
bodya4243 <105318502+bodya4243@users.noreply.github.com>
2024-02-25 18:45:09 +0200

    1)Set up "put", "get" method 2)Create constant 3)Create "indexCheck" method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,46 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int SIZE_ARR = 10;
+    private int size = 0;
+    private final Object[] keyArr = new Object[SIZE_ARR];
+    private final Object[] valueArr = new Object[SIZE_ARR];
+
     @Override
     public void put(K key, V value) {
+        int keyIndex = indexCheck(key);
+
+        if (keyIndex == -1) {
+            keyArr[size] = key;
+            valueArr[size] = value;
+            size++;
+        } else {
+            valueArr[keyIndex] = value;
+        }
     }
 
     @Override
     public V get(K key) {
+        int keyIndex = indexCheck(key);
+
+        if (keyIndex != -1) {
+            return (V) valueArr[keyIndex];
+        }
         return null;
+    }
+
+    private int indexCheck(K key) {
+        for (int i = 0; i < size; i++) {
+            if ((key == null && keyArr[i] == null)
+                    || (keyArr[i] != null && keyArr[i].equals(key))) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
