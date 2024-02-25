dd6b13b4f698ac196e14bc1145430e9b1a064cb3
artemklishch <artemklishch@gmail.com>
2024-02-25 14:24:10 +0200

    fix: should make some changes
    BREAKING CHANGES:
    
    *Remade putting data method
    *Remade size method
    
    Task: storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,48 +3,52 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int MAX_STORAGE_ITEMS_NUMBER = 10;
-
+    private static final int MAX_STORAGE_LENGTH = 10;
     private Data[] storage;
+    private int freeIndex;
 
     public StorageImpl() {
-        this.storage = new Data[0];
+        this.storage = new Data[MAX_STORAGE_LENGTH];
+        this.freeIndex = 0;
+    }
+
+    public Data[] getStorage() {
+        return storage;
     }
 
     @Override
     public void put(K key, V value) {
         Data newData = new Data(key, value);
-        Data[] updatedStorage;
-        if (storage.length == 0) {
-            updatedStorage = new Data[1];
-            updatedStorage[0] = newData;
-            storage = updatedStorage;
-            return;
-        }
 
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i].getKey() != null && storage[i].getKey().equals(key)) {
+        for (int i = 0; i < MAX_STORAGE_LENGTH; i++) {
+            if (key == null && storage[i] != null && storage[i].getKey() == null
+                    || (storage[i] != null
+                    && storage[i].getKey() != null
+                    && storage[i].getKey().equals(key))
+            ) {
                 storage[i].setValue(value);
                 return;
             }
         }
 
-        if (storage.length == MAX_STORAGE_ITEMS_NUMBER) {
-            return;
+        if (freeIndex == MAX_STORAGE_LENGTH) {
+            throw new RuntimeException(
+                    "The storage is totally filled - impossible to add new data"
+            );
+        } else {
+            storage[freeIndex] = newData;
+            freeIndex++;
         }
-
-        updatedStorage = new Data[storage.length + 1];
-        for (int i = 0; i < storage.length; i++) {
-            updatedStorage[i] = storage[i];
-        }
-        updatedStorage[storage.length] = newData;
-        storage = updatedStorage;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < storage.length; i++) {
-            if (storage[i].getKey() != null && storage[i].getKey().equals(key)) {
+            if ((key == null && storage[i] != null && storage[i].getKey() == null)
+                    || (storage[i] != null
+                    && storage[i].getKey() != null
+                    && storage[i].getKey().equals(key))
+            ) {
                 return (V) storage[i].getValue();
             }
         }
@@ -53,6 +57,12 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return storage.length;
+        int size = 0;
+        for (int i = 0; i < MAX_STORAGE_LENGTH; i++) {
+            if (storage[i] != null) {
+                size++;
+            }
+        }
+        return size;
     }
 }
