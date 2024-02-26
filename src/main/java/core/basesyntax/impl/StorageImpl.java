736337c267935e12c3df100e96d67652c4367665
John <pandastars535@gmail.com>
2024-02-26 13:01:53 +0200

    added method  isEquals to invoke  it in get () and put() method  .also deleted redundant line from size method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -16,7 +16,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] == key || (keys[i] != null && keys[i].equals(key))) {
+            if (isEqual(keys[i], key)) {
                 values[i] = value;
                 return;
             }
@@ -32,11 +32,18 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] == key || (keys[i] != null && keys[i].equals(key))) {
+            if (isEqual(keys[i], key)) {
                 return values[i];
             }
         }
         return null;
+    }
+
+    private boolean isEqual(K obj1, K obj2) {
+        if (obj1 == null) {
+            return obj2 == null;
+        }
+        return obj1.equals(obj2);
     }
 
     @Override
