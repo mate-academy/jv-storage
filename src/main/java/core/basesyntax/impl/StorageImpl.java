04b17ac36547a2a79b0f8ecb9e8c59b4b791b912
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-02-26 16:10:36 +0200

    K[] and V[] - now final and this removed initialization of size = 0 removed reworked V get()

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,14 +4,14 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int ARRAY_MAX_SIZE = 10;
-    private K[] keys;
-    private V[] values;
+
+    private final K[] keys;
+    private final V[] values;
     private int size;
 
     public StorageImpl() {
-        this.keys = (K[]) new Object[ARRAY_MAX_SIZE];
-        this.values = (V[]) new Object[ARRAY_MAX_SIZE];
-        this.size = 0;
+        keys = (K[]) new Object[ARRAY_MAX_SIZE];
+        values = (V[]) new Object[ARRAY_MAX_SIZE];
     }
 
     private int findIndexKey(K key) {
@@ -26,19 +26,23 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         int index = findIndexKey(key);
-        if (index != -1) {
-            values[index] = value;
-        } else if (size < ARRAY_MAX_SIZE) {
-            keys[size] = key;
-            values[size] = value;
-            size++;
+        try {
+            if (index != -1) {
+                values[index] = value;
+            } else if (size < ARRAY_MAX_SIZE) {
+                keys[size] = key;
+                values[size] = value;
+                size++;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Vault out of size" + e);
         }
     }
 
     @Override
     public V get(K key) {
         int index = findIndexKey(key);
-        return index != -1 ? (V) values[index] : null;
+        return index == -1 ? null : (V) values[index];
     }
 
     @Override
