68fed065d9fc171d1a9459113a76fb0be3e66c95
John <pandastars535@gmail.com>
2024-02-26 21:54:45 +0200

    Deleted if (size == keys.length) {                     size();                 } block and changed name for constant added separated method to find index of the key

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,40 +3,41 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int ARRAY_CAPACITY = 10;
+    private static final int DEFAULT_CAPACITY = 10;
     private final K[] keys;
     private final V[] values;
     private int size;
 
     public StorageImpl() {
-        keys = (K[]) new Object[ARRAY_CAPACITY];
-        values = (V[]) new Object[ARRAY_CAPACITY];
+        keys = (K[]) new Object[DEFAULT_CAPACITY];
+        values = (V[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public int findKeyIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (isEqual(keys[i], key)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < size; i++) {
-            if (isEqual(keys[i], key)) {
-                values[i] = value;
-                return;
-            }
+        int index = findKeyIndex(key);
+        if (index != -1) {
+            values[index] = value;
+        } else {
+            keys[size] = key;
+            values[size] = value;
+            size++;
         }
-        if (size == keys.length) {
-            size();
-        }
-        keys[size] = key;
-        values[size] = value;
-        size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < size; i++) {
-            if (isEqual(keys[i], key)) {
-                return values[i];
-            }
-        }
-        return null;
+        int index = findKeyIndex(key);
+        return (index != -1) ? values[index] : null;
     }
 
     private boolean isEqual(K obj1, K obj2) {
