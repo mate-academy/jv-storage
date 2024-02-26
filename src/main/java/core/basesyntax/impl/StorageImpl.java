dfa8592ea17ab0158a9ab7f9d4ebf380f6098cd7
Mykola Skrypalov <skripalov1@gmail.com>
2024-02-26 14:19:23 +0200

    implemented the methods in StorageImpl class

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,41 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE = 10;
+    private final K[] keysArray;
+    private final V[] valuesArray;
+    private int size = 0;
+
+    public StorageImpl() {
+        keysArray = (K[]) new Object[MAX_SIZE];
+        valuesArray = (V[]) new Object[MAX_SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (key == keysArray[i] || keysArray[i] != null && keysArray[i].equals(key)) {
+                valuesArray[i] = value;
+                return;
+            }
+        }
+        valuesArray[size] = value;
+        keysArray[size] = key;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == keysArray[i] || keysArray[i] != null && keysArray[i].equals(key)) {
+                return valuesArray[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
