b23d344834ed0c31102d162440cf61c2b9b1abe1
Yarolsav <kiritok712@gmail.com>
2024-02-26 11:37:42 +0100

    HomeWork

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,66 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE = 10;
+    private Object[] keys;
+    private Object[] values;
+    private int size;
+
+    public StorageImpl() {
+        this.keys = new Object[MAX_SIZE];
+        this.values = new Object[MAX_SIZE];
+        this.size = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = findIndex(key);
+        if (index != -1) {
+            // Key already exists, replace the value
+            values[index] = value;
+        } else {
+            // Key doesn't exist, add a new entry
+            if (size < MAX_SIZE) {
+                keys[size] = key;
+                values[size] = value;
+                size++;
+            } else {
+                throw new RuntimeException("Storage is full");
+            }
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int index = findIndex(key);
+        if (index != -1) {
+            // Key found, return the corresponding value
+            return (V) values[index];
+        } else {
+            // Key not found
+            return null;
+        }
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    private int findIndex(K key) {
+        if (key == null) {
+            for (int i = 0; i < size; i++) {
+                if (keys[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (key.equals(keys[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1; // Key not found
     }
 }
