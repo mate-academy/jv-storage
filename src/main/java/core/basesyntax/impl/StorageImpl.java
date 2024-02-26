14b4df9ec53e4a15a229c9f80e0d9a0dd2bab7e5
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-02-26 13:32:18 +0200

    added findIndexKey method to search index of key decrease amount of lines Object[] -> K[] and V[] size refactured now we just increase number not calling method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,80 +4,45 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int ARRAY_MAX_SIZE = 10;
-    private Object[] keys;
-    private Object[] values;
+    private K[] keys;
+    private V[] values;
     private int size;
-    private int result;
-    private boolean keyFound;
 
     public StorageImpl() {
-        this.keys = new Object[ARRAY_MAX_SIZE];
-        this.values = new Object[ARRAY_MAX_SIZE];
+        this.keys = (K[]) new Object[ARRAY_MAX_SIZE];
+        this.values = (V[]) new Object[ARRAY_MAX_SIZE];
         this.size = 0;
+    }
+
+    private int findIndexKey(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == null ? key == null : keys[i].equals(key)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public void put(K key, V value) {
-        if (size < ARRAY_MAX_SIZE) {
-            keyFound = false;
-            for (int i = 0; i < size; i++) {
-                if (keys[i] == null ? key == null : keys[i].equals(key)) {
-                    values[i] = value;
-                    keyFound = true;
-                    break;
-                }
-            }
-            if (!keyFound) {
-                keys[size] = key;
-                values[size] = value;
-                size();
-            }
+        int index = findIndexKey(key);
+        if (index != -1) {
+            values[index] = value;
+        } else if (size < ARRAY_MAX_SIZE) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
         }
     }
 
     @Override
     public V get(K key) {
-        if (key == null) {
-            for (int i = 0; i < size; i++) {
-                if (keys[i] == null) {
-                    return (V) values[i];
-                }
-            }
-        } else {
-            for (int i = 0; i < size; i++) {
-                if (key.equals(keys[i])) {
-                    return (V) values[i];
-                }
-            }
-        }
-        return null;
+        int index = findIndexKey(key);
+        return index != -1 ? (V) values[index] : null;
     }
 
     @Override
     public int size() {
-        return size++;
-    }
-
-    public boolean equals(Object object) {
-        if (this == object) {
-            return true;
-        }
-        if (object == null || this.getClass() != object.getClass()) {
-            return false;
-        }
-        StorageImpl<K, V> storageImpl = (StorageImpl<K, V>) object;
-        return (keys == null
-                ? storageImpl.keys == null
-                : keys.equals(storageImpl.keys))
-                && (values == null
-                ? storageImpl.values == null
-                : values.equals(storageImpl.values));
-    }
-
-    public int hashCode() {
-        result = 17;
-        result = 31 * result + (keys != null ? keys.hashCode() : 0);
-        result = 31 * result + (values != null ? values.hashCode() : 0);
-        return result;
+        return size;
     }
 }
