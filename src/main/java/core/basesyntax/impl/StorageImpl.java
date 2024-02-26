cbb0a9d563c5a66315b5dc577ed72ea2c97c18bd
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-02-26 12:57:43 +0200

    фÐadded exception throw to the put() method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,6 +4,7 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ARRAY_SIZE = 10;
+    private static final String FULL_ARRAY_EXCEPTION_MESSAGE = "The storage is full";
     private final K[] keys = (K[]) new Object[MAX_ARRAY_SIZE];
     private final V[] values = (V[]) new Object[MAX_ARRAY_SIZE];
     private int size;
@@ -20,6 +21,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
+        if (size == MAX_ARRAY_SIZE && !containsKey(key)) {
+            throw new IllegalStateException(FULL_ARRAY_EXCEPTION_MESSAGE);
+        }
         for (int i = 0; i < size; i++) {
             if (compareProvidedKey(key, i)) {
                 values[i] = value;
@@ -38,5 +42,15 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     private boolean compareProvidedKey(K key, int index) {
         return ((key == keys[index]) || (key != null && key.equals(keys[index])));
+    }
+
+    private boolean containsKey(K key) {
+        boolean result = false;
+        for (K storedKey : this.keys) {
+            if (storedKey.equals(key)) {
+                result = true;
+            }
+        }
+        return result;
     }
 }
