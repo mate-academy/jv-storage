7412bba9fb2ebcdb54c56046fe91dcf4a1fb77be
Yarolsav <kiritok712@gmail.com>
2024-02-26 16:25:16 +0100

    fix_build

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,47 +1,37 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
-    private Object[] keys;
-    private Object[] values;
+    private final K[] keys;
+    private final V[] values;
     private int size;
 
+    @SuppressWarnings("unchecked")
     public StorageImpl() {
-        this.keys = new Object[MAX_SIZE];
-        this.values = new Object[MAX_SIZE];
-        this.size = 0;
+        keys = (K[]) new Object[MAX_SIZE];
+        values = (V[]) new Object[MAX_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
         int index = findIndex(key);
         if (index != -1) {
-            // Key already exists, replace the value
             values[index] = value;
+        } else if (size < MAX_SIZE) {
+            keys[size] = key;
+            values[size++] = value;
         } else {
-            // Key doesn't exist, add a new entry
-            if (size < MAX_SIZE) {
-                keys[size] = key;
-                values[size] = value;
-                size++;
-            } else {
-                throw new RuntimeException("Storage is full");
-            }
+            throw new RuntimeException("Storage is full");
         }
     }
 
     @Override
     public V get(K key) {
         int index = findIndex(key);
-        if (index != -1) {
-            // Key found, return the corresponding value
-            return (V) values[index];
-        } else {
-            // Key not found
-            return null;
-        }
+        return index != -1 ? values[index] : null;
     }
 
     @Override
@@ -50,19 +40,11 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     }
 
     private int findIndex(K key) {
-        if (key == null) {
-            for (int i = 0; i < size; i++) {
-                if (keys[i] == null) {
-                    return i;
-                }
-            }
-        } else {
-            for (int i = 0; i < size; i++) {
-                if (key.equals(keys[i])) {
-                    return i;
-                }
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(key, keys[i])) {
+                return i;
             }
         }
-        return -1; // Key not found
+        return -1;
     }
 }
