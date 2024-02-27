49433d283e1d082db7a013f52dfdf8bc095f26f3
Pavlo Betsa <bigworld2303@gmail.com>
2024-02-27 15:40:31 +0200

    Third solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,45 +4,44 @@ import core.basesyntax.Storage;
 import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int INVALID_INDEX = -1;
     private static final int MAX_SIZE = 10;
     private K[] keyArray = (K[]) new Object[MAX_SIZE];
     private V[] valueArray = (V[]) new Object[MAX_SIZE];
+    private int size;
+
+    public int getIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(keyArray[i], key)) {
+                return i;
+            }
+        }
+        return INVALID_INDEX;
+    }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < keyArray.length; i++) {
-            if (Objects.equals(keyArray[i], key)) {
-                valueArray[i] = value;
-                return;
-            }
+        int index = getIndex(key);
+        if (index != INVALID_INDEX) {
+            valueArray[index] =value;
+            return;
         }
-        for (int j = 0; j < keyArray.length; j++) {
-            if (keyArray[j] == null && valueArray[j] == null) {
-                keyArray[j] = key;
-                valueArray[j] = value;
-                return;
-            }
-        }
+        valueArray[size] = value;
+        keyArray[size] = key;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keyArray.length; i++) {
-            if (Objects.equals(keyArray[i], key)) {
-                return valueArray[i];
-            }
+        int index = getIndex(key);
+        if (index != INVALID_INDEX) {
+            return valueArray[index];
         }
         return null;
     }
 
     @Override
     public int size() {
-        int counter = 0;
-        for (V element : valueArray) {
-            if (element != null) {
-                counter++;
-            }
-        }
-        return counter;
+        return size;
     }
 }
