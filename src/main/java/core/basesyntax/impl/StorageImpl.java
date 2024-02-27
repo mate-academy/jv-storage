12527f3f9fc08dce5024e5a8b993a3570b4d3de1
TonyH277 <robhobs50@gmail.com>
2024-02-27 14:32:49 +0200

    Update code

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,42 +6,42 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int STORAGE_SIZE = 10;
     private K[] keyArray = (K[]) new Object[STORAGE_SIZE];
     private V[] valueArray = (V[]) new Object[STORAGE_SIZE];
+    private int size = 0;
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < keyArray.length; i++) {
-            if (keyArray[i] == null ? key == null : keyArray[i].equals(key)) {
+        for (int i = 0; i < size; i++) {
+            if (isKeyAtStorage(key, i)) {
                 valueArray[i] = value;
                 return;
             }
         }
-        for (int j = 0; j < keyArray.length; j++) {
-            if (keyArray[j] == null && valueArray[j] == null) {
-                keyArray[j] = key;
-                valueArray[j] = value;
-                return;
-            }
-        }
+
+        keyArray[size] = key;
+        valueArray[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < keyArray.length; i++) {
-            if (keyArray[i] == null ? key == null : keyArray[i].equals(key)) {
+            if (isKeyAtStorage(key, i)) {
                 return valueArray[i];
             }
         }
         return null;
     }
 
+    private boolean isKeyAtStorage(K key, int i) {
+        if ((keyArray[i] == null && key == null)
+                || (keyArray[i] != null && keyArray[i].equals(key))) {
+            return true;
+        }
+        return false;
+    }
+
     @Override
     public int size() {
-        int counter = 0;
-        for (V element : valueArray) {
-            if (element != null) {
-                counter++;
-            }
-        }
-        return counter;
+        return size;
     }
 }
