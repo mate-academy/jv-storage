fe8d0e6a3f52376f1661872838d12f23074f825c
rk0v <rz20@ukr.net>
2024-03-10 23:19:55 +0200

    implements put()

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,8 +3,12 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private K key;
+    private V value;
     @Override
     public void put(K key, V value) {
+        this.key = key;
+        this.value = value;
     }
 
     @Override
