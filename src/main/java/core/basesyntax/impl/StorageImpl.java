d11a0b69f142a011b6b4d5e81374f2aa5a234629
1 <glebpashko2004@gmail.com>
2024-03-25 16:47:40 +0200

    Changed class StorageImpl

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -7,7 +7,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     private final K[] arrayKey;
     private final V[] arrayValue;
-    private int size = 0;
+    private int size;
 
     public StorageImpl() {
         arrayKey = (K[]) new Object[ARRAY_MAX_SIZE];
@@ -17,7 +17,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if ((key == null && arrayKey[i] == null)
+            if ((key == arrayKey[i])
                     || (arrayKey[i] != null && arrayKey[i].equals(key))) {
                 arrayValue[i] = value;
                 return;
