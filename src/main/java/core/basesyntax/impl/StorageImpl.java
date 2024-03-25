4eb4a1d92a553cfe0b833a3f30e20d1161c8109f
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-03-26 00:38:33 +0200

    Added implementation of storage with put/get items by keys method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,54 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import core.basesyntax.exeptions.StorageException;
+
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAXIMUM_STORAGE_LENGTH = 10;
+    private Pair<K, V>[] storage;
+
+    public StorageImpl() {
+        this.storage = new Pair[0];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (Pair pair : storage) {
+            Object pairKey = pair.getKey();
+            if (Objects.equals(pairKey, key)) {
+                pair.setValue(value);
+                return;
+            }
+        }
+        if (storage.length >= MAXIMUM_STORAGE_LENGTH) {
+            throw new StorageException("Storage is out of capacity");
+        }
+        Pair<K, V>[] newStorage = createCopyOfStorageWithIncreasedCapacity(storage);
+        newStorage[newStorage.length - 1] = new Pair<>(key, value);
+        this.storage = newStorage;
+    }
+
+    private Pair<K, V>[] createCopyOfStorageWithIncreasedCapacity(Pair<K, V>[] previousStorage) {
+        Pair<K, V>[] newStorage = new Pair[previousStorage.length + 1];
+        System.arraycopy(previousStorage, 0, newStorage, 0, previousStorage.length);
+        return newStorage;
     }
 
     @Override
     public V get(K key) {
+        for (Pair<K, V> pair : storage) {
+            K pairKey = pair.getKey();
+            if (Objects.equals(pairKey, key)) {
+                return pair.getValue();
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return storage.length;
     }
 }
