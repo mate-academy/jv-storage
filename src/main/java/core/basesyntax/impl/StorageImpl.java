eb58b8d550257ecc6cec88ebd075a50ebddad114
Wojtas1996PL <mat.wojtkowiak@gmail.com>
2024-04-07 21:18:18 +0200

    Fifth solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,27 +1,33 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int ARRAY_SIZE = 10;
-    private K[] keys = (K[]) new Object[ARRAY_SIZE];
-    private V[] values = (V[]) new Object[ARRAY_SIZE];
+    private K[] keys;
+    private V[] values;
+    private int counter;
     private int index;
     private int size;
     private K key;
 
+    public StorageImpl() {
+        keys = (K[]) new Object[ARRAY_SIZE];
+        values = (V[]) new Object[ARRAY_SIZE];
+        counter++;
+    }
+
     @Override
     public void put(K key, V value) {
-        if (size >= ARRAY_SIZE) {
-            throw new RuntimeException("Size is full");
+        if (counter == 0) {
+            new StorageImpl<>();
         }
         for (int i = 0; i < keys.length; i++) {
             if (this.key == null && key == null && i > 0) {
                 values[i] = value;
                 return;
             }
-            if (Objects.equals(keys[i], key) && key != null) {
+            if (keys[i] != null && keys[i].equals(key)) {
                 values[i] = value;
                 this.key = key;
                 return;
@@ -37,7 +43,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < keys.length; i++) {
-            if (Objects.equals(keys[i], key)) {
+            if ((keys[i] == null && key == null)
+                    || (keys[i] != null && keys[i].equals(key))) {
                 return (V) values[i];
             }
         }
