72ab03c82ea1b747b988d512f18600a48ffa7b4d
Yurii Soliar <us1585707@gmail.com>
2024-04-10 01:37:43 +0300

    implemented mentor`s recommendations

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,54 +1,58 @@
 package core.basesyntax.impl;
 
+import core.basesyntax.KV;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int STORAGE_SIZE = 10;
-    private K[] keyStorage = (K[]) new Object[STORAGE_SIZE];
-    private V[] valueStorage = (V[]) new Object[STORAGE_SIZE];
+    private KV<K, V>[] storage;
 
-    @Override
-    public void put(K key, V value) {
-        for (int i = 0; i < STORAGE_SIZE; i++) {
-
-            if ((keyStorage[i] != null && keyStorage[i].equals(key))
-                    || (keyStorage[i] == null && valueStorage[i] != null && key == null)) {
-                valueStorage[i] = value;
-                break;
-            }
-
-            if (keyStorage[i] == null && valueStorage[i] == null) {
-                keyStorage[i] = key;
-                valueStorage[i] = value;
-                break;
-            }
-        }
+    public StorageImpl() {
+        storage = (KV<K, V>[]) new KV<?, ?>[STORAGE_SIZE];
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < STORAGE_SIZE; i++) {
-            if (keyStorage[i] == null && key == null && valueStorage[i] != null) {
-                return valueStorage[i];
-            }
+        int index = this.findIndex(key);
 
-            if (keyStorage[i] != null) {
-                if (keyStorage[i].equals(key)) {
-                    return valueStorage[i];
+        return index == -1 ? null : storage[index].getValue();
+    }
+
+    @Override
+    public void put(K key, V value) {
+        int index = this.findIndex(key);
+
+        if (index != -1) {
+            storage[index].setValue(value);
+            return;
+        }
+
+        for (int i = 0; i < STORAGE_SIZE; i++) {
+            if (storage[i] == null) {
+                storage[i] = new KV(key, value);
+                return;
+            }
+        }
+    }
+
+    private int findIndex(K key) {
+        for (int i = 0; i < STORAGE_SIZE; i++) {
+            if (storage[i] != null) {
+                if ((storage[i].getKey() == null && key == null)
+                        || (storage[i].getKey() != null && storage[i].getKey().equals(key))) {
+                    return i;
                 }
             }
         }
-        return null;
+        return -1;
     }
 
     @Override
     public int size() {
         int counter = 0;
         for (int i = 0; i < STORAGE_SIZE; i++) {
-            if (keyStorage[i] == null && valueStorage[i] == null) {
-                return counter;
-            } else {
-                ++counter;
+            if (storage[i] != null) {
+                counter++;
             }
         }
         return counter;
