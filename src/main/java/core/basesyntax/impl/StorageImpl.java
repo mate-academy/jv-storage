29ec3bae86f0ace61c38756359e8573f700a04dc
Wojtas1996PL <mat.wojtkowiak@gmail.com>
2024-04-09 18:17:09 +0200

    Sixth solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,45 +6,40 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int ARRAY_SIZE = 10;
     private K[] keys;
     private V[] values;
-    private int counter;
-    private int index;
+    private boolean executedOnce;
     private int size;
     private K key;
 
     public StorageImpl() {
         keys = (K[]) new Object[ARRAY_SIZE];
         values = (V[]) new Object[ARRAY_SIZE];
-        counter++;
     }
 
     @Override
     public void put(K key, V value) {
-        if (counter == 0) {
+        if (executedOnce) {
             new StorageImpl<>();
+            executedOnce = true;
         }
         for (int i = 0; i < keys.length; i++) {
-            if (this.key == null && key == null && i > 0) {
-                values[i] = value;
-                return;
-            }
-            if (keys[i] != null && keys[i].equals(key)) {
+            if (keysEquals(key, keys[i]) && key != null || (key == this.key && i > 0)) {
                 values[i] = value;
                 this.key = key;
                 return;
             }
         }
-        keys[index] = key;
-        values[index] = value;
-        index++;
-        size++;
-        this.key = key;
+        if (size < ARRAY_SIZE) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+            this.key = key;
+        }
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < keys.length; i++) {
-            if ((keys[i] == null && key == null)
-                    || (keys[i] != null && keys[i].equals(key))) {
+            if (keysEquals(key, keys[i])) {
                 return (V) values[i];
             }
         }
@@ -54,5 +49,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return size;
+    }
+
+    private boolean keysEquals(K key, K storageKey) {
+        return ((storageKey == key)
+                || storageKey != null && storageKey.equals(key));
     }
 }
