65c5136213eee8e573c6feb902c68f98a49a9de5
SviatoslavTataryn <www.t.svyatoslav55@gmail.com>
2024-04-10 19:30:09 +0200

    i try fixed. I don't understand what is wrong here

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -11,14 +11,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < size; i++) {
-            if (Objects.equals(key, keys[i])) {
-                values[i] = value;
-                return;
-            }
-        }
-
-        if (size < MAX_SIZE) {
+        int index = findKeyIndex(key);
+        if (index != -1) {
+            values[index] = value;
+        } else if (size < MAX_SIZE) {
             keys[size] = key;
             values[size] = value;
             size++;
@@ -28,16 +24,22 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     @SuppressWarnings("unchecked")
     public V get(K key) {
-        for (int i = 0; i < size; i++) {
-            if (Objects.equals(key, keys[i])) {
-                return (V) values[i];
-            }
-        }
-        return null;
+        int index = findKeyIndex(key);
+        return index != -1 ? (V) values[index] : null;
     }
 
     @Override
     public int size() {
         return size;
     }
+
+    private int findKeyIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(key, keys[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
+
