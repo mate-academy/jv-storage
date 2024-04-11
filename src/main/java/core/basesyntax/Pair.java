f06a1db1333ac6b8e11d4ba1fe99111cac181158
Yurii Soliar <us1585707@gmail.com>
2024-04-11 13:02:05 +0300

    update size method, rename KV class to Pair

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,10 +1,10 @@
 package core.basesyntax;
 
-public class KV<K, V> {
+public class Pair<K, V> {
     private K key;
     private V value;
 
-    public KV(K key, V value) {
+    public Pair(K key, V value) {
         this.key = key;
         this.value = value;
     }
--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,42 +1,43 @@
 package core.basesyntax.impl;
 
-import core.basesyntax.KV;
+import core.basesyntax.Pair;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int STORAGE_SIZE = 10;
-    private KV<K, V>[] storage;
+    private Pair<K, V>[] storage;
+    private int size = 0;
 
     public StorageImpl() {
-        storage = (KV<K, V>[]) new KV<?, ?>[STORAGE_SIZE];
+        storage = (Pair<K, V>[]) new Pair<?, ?>[STORAGE_SIZE];
     }
 
     @Override
     public V get(K key) {
-        int index = this.findIndex(key);
-
+        int index = findIndex(key);
         return index == -1 ? null : storage[index].getValue();
     }
 
     @Override
     public void put(K key, V value) {
-        int index = this.findIndex(key);
+        int index = findIndex(key);
 
         if (index != -1) {
             storage[index].setValue(value);
             return;
         }
 
-        for (int i = 0; i < STORAGE_SIZE; i++) {
+        for (int i = 0; i < STORAGE_SIZE; ++i) {
             if (storage[i] == null) {
-                storage[i] = new KV(key, value);
+                storage[i] = new Pair(key, value);
+                ++size;
                 return;
             }
         }
     }
 
     private int findIndex(K key) {
-        for (int i = 0; i < STORAGE_SIZE; i++) {
+        for (int i = 0; i < STORAGE_SIZE; ++i) {
             if (storage[i] != null) {
                 if ((storage[i].getKey() == null && key == null)
                         || (storage[i].getKey() != null && storage[i].getKey().equals(key))) {
@@ -49,12 +50,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        int counter = 0;
-        for (int i = 0; i < STORAGE_SIZE; i++) {
-            if (storage[i] != null) {
-                counter++;
-            }
-        }
-        return counter;
+        return size;
     }
 }
