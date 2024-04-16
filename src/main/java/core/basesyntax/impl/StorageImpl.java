120b45b7fcabff21c755a493278209d7f32af13f
trokhim03 <trokhimchuk.003@gmail.com>
2024-04-16 19:16:12 +0300

    adding helper methods

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,27 +3,39 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int MAX_NUM = 10;
+    private static final int MAX_SIZE = 10;
     private int size;
-    private K[] keys;
-    private V[] values;
+    private final K[] keys;
+    private final V[] values;
 
     public StorageImpl() {
-        keys = (K[]) new Object[MAX_NUM];
-        values = (V[]) new Object[MAX_NUM];
+        keys = (K[]) new Object[MAX_SIZE];
+        values = (V[]) new Object[MAX_SIZE];
         size = 0;
+    }
+
+    public void ensureCapacity() {
+        if (MAX_SIZE < size) {
+            throw new RuntimeException("Storage is full!");
+        }
+    }
+
+    public int getIndex(K key, K[] keys) {
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || (key != null && key.equals(keys[i]))) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public void put(K key, V value) {
-        if (MAX_NUM < size) {
-            throw new RuntimeException("Storage is full!");
-        }
-        for (int i = 0; i < size; i++) {
-            if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
-                values[i] = value;
-                return;
-            }
+        ensureCapacity();
+        int index = getIndex(key, keys);
+        if (index != -1) {
+            values[index] = value;
+            return;
         }
         values[size] = value;
         keys[size] = key;
@@ -32,12 +44,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < size; i++) {
-            if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
-                return values[i];
-            }
-        }
-        return null;
+        int index = getIndex(key, keys);
+        return index == -1 ? null : values[index];
     }
 
     @Override
