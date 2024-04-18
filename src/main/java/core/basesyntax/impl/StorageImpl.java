e0255dbdb4cde85479fa2eeeb4ce82bbf225fafc
oleksandr-saulko <aleksandrsaulko@gmail.com>
2024-04-18 21:50:19 +0300

    fixes after code review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,51 +4,43 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_CAPACITY = 10;
-    private Object[] keys;
-    private Object[] values;
+    private static final int NO_INDEX = -1;
+    private K[] keys;
+    private V[] values;
     private int size;
 
     public StorageImpl() {
-        keys = new Object[MAX_CAPACITY];
-        values = new Object[MAX_CAPACITY];
+        keys = (K[]) new Object[MAX_CAPACITY];
+        values = (V[]) new Object[MAX_CAPACITY];
         size = 0;
+    }
+
+    private int indexOf(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public void put(K key, V value) {
-        if (key == null) {
-            for (int i = 0; i < size; i++) {
-                if (keys[i] == null) {
-                    values[i] = value; // Replace the value if key is null
-                    return;
-                }
-            }
-        } else {
-            for (int i = 0; i < size; i++) {
-                if (key.equals(keys[i])) {
-                    values[i] = value; // Replace the value if key already exists
-                    return;
-                }
-            }
-        }
+        int index = indexOf(key);
 
-        if (size < MAX_CAPACITY) {
+        if (index != NO_INDEX) {
+            values[index] = value;
+        } else {
             keys[size] = key;
             values[size] = value;
             size++;
-        } else {
-            System.out.println("Storage is full, cannot add more elements.");
         }
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < size; i++) {
-            if (key == null && keys[i] == null || key != null && key.equals(keys[i])) {
-                return (V) values[i];
-            }
-        }
-        return null;
+        int index = indexOf(key);
+        return index == NO_INDEX ? null : values[index];
     }
 
     @Override
