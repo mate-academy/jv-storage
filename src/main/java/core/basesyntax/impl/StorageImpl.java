83ca5e8496e0090d9c3606641bf8ea156cd3d985
BelitskyiAlexandr <vilisapett@gmail.com>
2024-04-19 17:28:27 +0300

    + null case

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -16,6 +16,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
+        if (key == null) {
+            putNullKey(value);
+            return;
+        }
         int index = indexOf(key);
         if (index != -1) {
             values[index] = value;
@@ -39,11 +43,39 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         return -1;
     }
 
+    private void putNullKey(V value) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == null) {
+                values[i] = value;
+                return;
+            }
+        }
+        if (size < STORAGE_CAPACITY) {
+            keys[size] = null;
+            values[size] = value;
+            size++;
+        } else {
+            throw new ArrayIndexOutOfBoundsException("Storage is full");
+        }
+    }
+
     @Override
     public V get(K key) {
+        if (key == null) {
+            return getNullKey();
+        }
         int index = indexOf(key);
         if (index != -1) {
             return (V) values[index];
+        }
+        return null;
+    }
+
+    private V getNullKey() {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == null) {
+                return (V) values[i];
+            }
         }
         return null;
     }
