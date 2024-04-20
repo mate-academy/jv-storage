5f2f4c46252af38d0c208b754c595fe5a5ebfeb1
BelitskyiAlexandr <vilisapett@gmail.com>
2024-04-20 12:50:54 +0300

    change null logic

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -15,29 +15,21 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        if (key == null) {
-            putNullKey(value);
+        if (size == STORAGE_CAPACITY) {
             return;
         }
         int index = indexOf(key);
         if (index != -1) {
             values[index] = value;
         } else {
-            if (size < STORAGE_CAPACITY) {
-                keys[size] = key;
-                values[size] = value;
-                size++;
-            } else {
-                throw new ArrayIndexOutOfBoundsException("Storage is full");
-            }
+            keys[size] = key;
+            values[size] = value;
+            size++;
         }
     }
 
     @Override
     public V get(K key) {
-        if (key == null) {
-            return getNullKey();
-        }
         int index = indexOf(key);
         if (index != -1) {
             return values[index];
@@ -52,35 +44,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     private int indexOf(K key) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] != null && keys[i].equals(key)) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
                 return i;
             }
         }
         return -1;
-    }
-
-    private void putNullKey(V value) {
-        for (int i = 0; i < size; i++) {
-            if (keys[i] == null) {
-                values[i] = value;
-                return;
-            }
-        }
-        if (size < STORAGE_CAPACITY) {
-            keys[size] = null;
-            values[size] = value;
-            size++;
-        } else {
-            throw new ArrayIndexOutOfBoundsException("Storage is full");
-        }
-    }
-
-    private V getNullKey() {
-        for (int i = 0; i < size; i++) {
-            if (keys[i] == null) {
-                return values[i];
-            }
-        }
-        return null;
     }
 }
