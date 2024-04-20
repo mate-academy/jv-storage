1ea8058cdf5235e3f9ef78394a21f877463e06db
Oleksandr Fetisenko <alexandr.fetisenko@gmail.com>
2024-04-20 20:54:35 +0300

    Solved the task.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,54 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE = 10;
+    private static final int NO_INDEX = -1;
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_SIZE];
+        values = (V[]) new Object[MAX_SIZE];
+        size = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = indexOf(key);
+        if (index != NO_INDEX) {
+            values[index] = value;
+        } else {
+            if (size < MAX_SIZE) {
+                keys[size] = key;
+                values[size] = value;
+                size++;
+            } else {
+                throw new IllegalStateException("Storage is full");
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        int index = indexOf(key);
+        if (index != NO_INDEX) {
+            return (V) values[index];
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    private int indexOf(K key) {
+        for (int i = 0; i < size; i++) {
+            if ((key == null && keys[i] == null) || ((key != null) && key.equals(keys[i]))) {
+                return i;
+            }
+        }
+        return NO_INDEX;
     }
 }
