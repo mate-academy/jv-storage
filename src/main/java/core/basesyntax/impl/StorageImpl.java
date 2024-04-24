a4ae3a362d0a625067fa89a539d9f474cbc06503
Svyatoslav Nadorozhnyi <svyatoslavnadorozhniy@gmail.com>
2024-04-24 10:50:41 -0400

    Replaced Objects.equals method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,7 +1,6 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
