cb951dc7d9b31fcbfc175f89dd1eab034faa8c68
Anton <antchaycka@gmail.com>
2024-04-24 18:27:40 +0200

    made fixes according to review(removed 2 private methods and added a new one "indexOf", changed methods "get" and "put")

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,83 +5,55 @@ import core.basesyntax.Storage;
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_STORAGE = 10;
     private static final int NULL_PLACEHOLDER = 99;
-    private Object[] valueArr;
-    private Object[] keyArr;
-    private int itemsNumber;
+    private Object[] values;
+    private Object[] keys;
+    private int size;
 
     public StorageImpl() {
-        valueArr = new Object[MAX_STORAGE];
-        keyArr = new Object[MAX_STORAGE];
+        values = new Object[MAX_STORAGE];
+        keys = new Object[MAX_STORAGE];
+
     }
 
     @Override
     public void put(K key, V value) {
-        if (key == null && !nullCheck()) {
-            keyArr[itemsNumber] = NULL_PLACEHOLDER;
-            valueArr[itemsNumber] = value;
-            itemsNumber++;
+        if (key == null && indexOf(key) == -1) {
+            keys[size] = NULL_PLACEHOLDER;
+            values[size] = value;
+            size++;
             return;
         }
 
-        if (duplicateCheck(key, value)) {
+        if (indexOf(key) != -1) {
+            values[indexOf(key)] = value;
             return;
         }
 
-        keyArr[itemsNumber] = key;
-        valueArr[itemsNumber] = value;
-        itemsNumber++;
-    }
-
-    private boolean nullCheck() {
-        try {
-            for (Object o : keyArr) {
-                if (o.equals(NULL_PLACEHOLDER)) {
-                    return true;
-                }
-            }
-        } catch (NullPointerException e) {
-            System.out.println(e.getMessage());
-        }
-        return false;
-    }
-
-    private boolean duplicateCheck(K key, V value) {
-        try {
-            for (int i = 0; i < keyArr.length; i++) {
-                if (key == null && keyArr[i].equals(NULL_PLACEHOLDER)) {
-                    valueArr[i] = value;
-                    return true;
-                }
-                if (keyArr[i].equals(key)) {
-                    valueArr[i] = value;
-                    return true;
-                }
-            }
-        } catch (NullPointerException e) {
-            System.out.println(e.getMessage());
-        }
-        return false;
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        try {
-            for (int i = 0; i < keyArr.length; i++) {
-                if (key == null && keyArr[i].equals(NULL_PLACEHOLDER)) {
-                    return (V) valueArr[i];
-                }
-                if (keyArr[i].equals(key)) {
-                    return (V) valueArr[i];
-                }
-            }
-        } catch (NullPointerException e) {
-            System.out.println(e.getMessage());
+        if (indexOf(key) != -1) {
+            return (V) values[indexOf(key)];
         }
         return null;
     }
 
     @Override
     public int size() {
-        return itemsNumber;
+        return size;
+    }
+
+    private int indexOf(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == null && keys[i].equals(NULL_PLACEHOLDER)
+                    || key != null && key.equals(keys[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
