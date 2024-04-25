efd02ede718b19956449cf1abcaf3db2bfa38e02
krudeN <wetwettew@gmail.com>
2024-04-25 13:48:18 +0300

    made changes based on comments

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,22 +3,30 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private K[] keysArrays = (K[]) new Object[10];
-    private V[] valueArrays = (V[]) new Object[10];
-    private int size = 0;
+    private static final int MAX_ARRAY_SIZE = 10;
+    private final K[] keys;
+    private final V[] values;
+    private int size;
+
+    public StorageImpl() {
+        this.keys = (K[]) new Object[MAX_ARRAY_SIZE];
+        this.values = (V[]) new Object[MAX_ARRAY_SIZE];
+    }
+
+    private boolean isValid(K key, int index) {
+        return key == null && keys[index] == null || key != null && key.equals(keys[index]);
+    }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if ((key == null && keysArrays[i] == null)
-                    ||
-                    (key != null && key.equals(keysArrays[i]))) {
-                valueArrays[i] = value;
+            if (isValid(key,i)) {
+                values[i] = value;
                 return;
             }
         }
-        keysArrays[size] = key;
-        valueArrays[size] = value;
+        keys[size] = key;
+        values[size] = value;
         size++;
 
     }
@@ -26,10 +34,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if ((key == null && keysArrays[i] == null)
-                    ||
-                    (key != null && key.equals(keysArrays[i]))) {
-                return valueArrays[i];
+            if (isValid(key,i)) {
+                return values[i];
             }
         }
         return null;
