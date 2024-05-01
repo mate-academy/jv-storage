46a245c632f8cef299da34250213094e4699b8e3
ruslanPonomarev <ruslan.ponomarev@wearebrain.com>
2024-05-01 15:58:51 +0300

    fix task

--- src/main/java/core/basesyntax/Storage.java
+++ src/main/java/core/basesyntax/Storage.java
@@ -1,9 +1,19 @@
-package core.basesyntax;
+import static java.lang.StringLatin1.indexOf;
 
-public interface Storage<K, V> {
-    void put(K key, V value);
-
-    V get(K key);
-
-    int size();
+public interface Storage {
+    @Override
+    public void put(K key, V value) {
+        int index = indexOf(key);
+        if (index != -1) {
+            values[index] = value; // Replace value if key already exists
+        } else {
+            if (size < MAX_SIZE) {
+                keys[size] = key;
+                values[size] = value;
+                size++;
+            } else {
+                throw new IllegalStateException("Storage is full");
+            }
+        }
+    }
 }
