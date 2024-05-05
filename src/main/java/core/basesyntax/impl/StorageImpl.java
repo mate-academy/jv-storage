e177d0b9b8eb36d1ab4423306559d97d100e1d18
Svitlana Galicka <svitlanakozak12332@gmail.com>
2024-05-05 19:47:51 +0300

    corrected the Storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,37 +4,49 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ELEMENTS_NUMBER = 10;
-    private K[] keys = (K[])new Object[MAX_ELEMENTS_NUMBER];
-    private V[] values = (V[]) new Object[MAX_ELEMENTS_NUMBER];
-    private int sizeOfStorage;
+    private final K[] keys;
+    private final V[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_ELEMENTS_NUMBER];
+        values = (V[]) new Object[MAX_ELEMENTS_NUMBER];
+    }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < sizeOfStorage; i++) {
-            if ((keys[i] != null && keys[i].equals(key)) || keys[i] == key) {
-                values[i] = value;
-                return;
-            }
+        int number = getKey(key);
+        if (number != -1) {
+            values[number] = value;
+            return;
         }
-        if (sizeOfStorage < MAX_ELEMENTS_NUMBER) {
-            keys[sizeOfStorage] = key;
-            values[sizeOfStorage] = value;
-            sizeOfStorage++;
+        if (size < MAX_ELEMENTS_NUMBER) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
         }
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < sizeOfStorage; i++) {
-            if ((keys[i] != null && keys[i].equals(key)) || keys[i] == key) {
-                return values[i];
-            }
+        int number = getKey(key);
+        if (number != -1) {
+            return values[number];
         }
         return null;
     }
 
+    public int getKey(K key) {
+        for (int i = 0; i < size; i++) {
+            if ((keys[i] != null && keys[i].equals(key)) || keys[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     @Override
     public int size() {
-        return sizeOfStorage;
+        return size;
     }
 }
