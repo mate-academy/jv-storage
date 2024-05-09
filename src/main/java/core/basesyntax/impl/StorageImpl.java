848d4e6b1e1f6bbcc5d52232db0ea46cc0b476f0
Ppllupold <kotnazar2005@gmail.com>
2024-05-09 13:32:05 +0300

    jv-storage fixed

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,80 +4,48 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
-    private int indexForNexElem = 0;
     private int size;
-    private K[] keyStorage;
-    private V[] valueStorage;
+    private K[] keys;
+    private V[] values;
 
     public StorageImpl() {
-        size = 0;
-        keyStorage = (K[]) new Object[MAX_SIZE];
-        valueStorage = (V[]) new Object[MAX_SIZE];
+        keys = (K[]) new Object[MAX_SIZE];
+        values = (V[]) new Object[MAX_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
-
-        if (containsKey(key)) {
-            valueStorage[getKeyIndex(key)] = value;
+        if (size == MAX_SIZE) {
+            throw new RuntimeException("The storage is full");
+        }
+        int index = getKeyIndex(key);
+        if (index != -1) {
+            values[getKeyIndex(key)] = value;
         } else {
-            if (size == MAX_SIZE) {
-                throw new RuntimeException("The storage is full");
-            }
-            keyStorage[indexForNexElem] = key;
-            valueStorage[indexForNexElem] = value;
-            indexForNexElem++;
+            keys[size] = key;
+            values[size] = value;
             size++;
         }
     }
 
     @Override
     public V get(K key) {
-        if (key == null) {
-            return valueStorage[getKeyIndex(key)];
-        }
-        if (!containsKey(key)) {
-            return null;
-        }
-        return valueStorage[getKeyIndex(key)];
+        int index = getKeyIndex(key);
+        return index == -1 ? null : values[index];
     }
 
     @Override
     public int size() {
-        return this.size;
-    }
-
-    private boolean containsKey(K key) {
-        for (int i = 0; i < size; i++) {
-            if (key == null ? keyStorage[i] == null
-                    : keyStorage[i] != null && keyStorage[i].equals(key)) {
-                return true;
-            }
-        }
-        return false;
+        return size;
     }
 
     private int getKeyIndex(K key) {
         for (int i = 0; i < size; i++) {
-            if (key == null ? keyStorage[i] == null
-                    : keyStorage[i] != null && keyStorage[i].equals(key)) {
+            if (key == null ? keys[i] == null
+                    : keys[i] != null && keys[i].equals(key)) {
                 return i;
             }
         }
         return -1;
-    }
-
-    private void putWithNullKey(K key, V value) {
-        if (value == null) {
-            keyStorage[indexForNexElem] = null;
-            valueStorage[indexForNexElem] = null;
-            indexForNexElem++;
-            size++;
-        } else {
-            keyStorage[indexForNexElem] = null;
-            valueStorage[indexForNexElem] = value;
-            indexForNexElem++;
-            size++;
-        }
     }
 }
