2cd29ca2a066ff8fc331933d685d2a049ff7c535
Valeria <valeriaua97@gmail.com>
2024-05-17 12:35:54 +0200

    Fixed cod

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -15,24 +15,32 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
+        int index = indexOf(key);
+        if (index != -1) {
+            values[index] = value;
+        } else {
+            if (size >= MAX_CAPACITY) {
+                throw new RuntimeException("Storage is full!");
+            }
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
+    }
+
+    private int indexOf(K key) {
         for (int i = 0; i < size; i++) {
-            if (key == keys[i] || key != null && key.equals(keys[i])) {
-                values[i] = value;
-                return;
+            if ((key == keys[i]) || (key != null && key.equals(keys[i]))) {
+                return i;
             }
         }
-        if (size >= MAX_CAPACITY) {
-            throw new RuntimeException("Storage is full!");
-        }
-        keys[size] = key;
-        values[size] = value;
-        size++;
+        return -1;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if ((key == keys[i]) || (key != null && key.equals(keys[i]))) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
                 return values[i];
             }
         }
