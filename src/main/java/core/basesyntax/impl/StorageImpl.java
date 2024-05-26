a0f3bf45a58591004accd3fb9fbf98f9911e3446
Nataliya Plastun <natalya21753@gmail.com>
2024-05-26 09:44:13 +0300

    create private helper method  and change value

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,44 +6,47 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
     private K[] keys;
     private V[] values;
-    private int currentSize;
+    private int size;
 
     @SuppressWarnings("unchecked")
     public StorageImpl() {
         keys = (K[]) new Object[MAX_SIZE];
         values = (V[]) new Object[MAX_SIZE];
-        currentSize = 0;
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < currentSize; i++) {
-            if ((keys[i] == null && key == null) || (keys[i] != null && keys[i].equals(key))) {
-                values[i] = value;
-                return;
-            }
-        }
-        if (currentSize < MAX_SIZE) {
-            keys[currentSize] = key;
-            values[currentSize] = value;
-            currentSize++;
+        int index = findKeyIndex(key);
+        if (index != -1) {
+            values[index] = value;
         } else {
-            throw new IllegalStateException("Storage is full");
+            if (size < MAX_SIZE) {
+                keys[size] = key;
+                values[size] = value;
+                size++;
+            } else {
+                throw new IllegalStateException("Storage is full");
+            }
         }
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < currentSize; i++) {
-            if ((keys[i] == null && key == null) || (keys[i] != null && keys[i].equals(key))) {
-                return values[i];
-            }
-        }
-        return null;
+        int index = findKeyIndex(key);
+        return index != -1 ? values[index] : null;
     }
 
     @Override
     public int size() {
-        return currentSize;
+        return size;
+    }
+
+    private int findKeyIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if ((keys[i] == null && key == null) || (keys[i] != null && keys[i].equals(key))) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
