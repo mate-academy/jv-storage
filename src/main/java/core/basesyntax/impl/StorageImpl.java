53b6a962aa1e0a3dd918913ec2f6fcd4d4ab9223
Ivan Demchenko <experywow@gmail.com>
2024-06-12 22:56:36 +0100

    Lines: 7, 8:   Arrays type changed from Object to Generics. Removed instantiation of field size to 0. Line 16:   Added method indexOf for remove duplication in methods put & get.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,42 +4,40 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int STORAGE_MAX_CAPACITY = 10;
-    private final Object[] keys;
-    private final Object[] values;
+    private final K[] keys;
+    private final V[] values;
     private int size;
 
     public StorageImpl() {
-        keys = new Object[STORAGE_MAX_CAPACITY];
-        values = new Object[STORAGE_MAX_CAPACITY];
-        size = 0;
+        keys = (K[]) new Object[STORAGE_MAX_CAPACITY];
+        values = (V[]) new Object[STORAGE_MAX_CAPACITY];
+    }
+
+    private int indexOf(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < size; i++) {
-            if ((keys[i] == null && key == null) || (keys[i] != null && keys[i].equals(key))) {
-                values[i] = value;
-                return;
-            }
-        }
-        if (size < STORAGE_MAX_CAPACITY) {
+        int index = indexOf(key);
+        if (index != -1) {
+            values[index] = value;
+        } else if (index == -1) {
             keys[size] = key;
             values[size] = value;
             size++;
-
-        } else {
-            throw new RuntimeException("Storage is overfilled, maximum capacity is 10");
         }
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < keys.length; i++) {
-            if ((keys[i] == null && key == null) || (keys[i] != null && keys[i].equals(key))) {
-                return (V) values[i];
-            }
-        }
-        return null;
+        int index = indexOf(key);
+        return index == -1 ? null : values[index];
     }
 
     @Override
