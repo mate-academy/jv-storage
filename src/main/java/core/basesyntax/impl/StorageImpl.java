a482fc3858f5575441291ba28b390be5dceede01
JaverJaver <mopasaj205@arpizol.com>
2024-06-14 17:27:37 +0300

    add getKeyIndex method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,10 +1,10 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+
 import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-
     private static final int MAX_CAPACITY = 10;
     private final K[] keys;
     private final V[] values;
@@ -18,12 +18,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         boolean isReplaced = false;
-        for (int i = 0; i < size; i++) {
-            if (Objects.equals(keys[i], key)) {
-                values[i] = value;
-                isReplaced = true;
-                break;
-            }
+        int keyIndex = getKeyIndex(key);
+        if (keyIndex != -1) {
+            values[keyIndex] = value;
+            isReplaced = true;
         }
         if (!isReplaced) {
             keys[size] = key;
@@ -35,10 +33,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         V foundValueByKey = null;
-        for (int i = 0; i < size; i++) {
-            if (Objects.equals(keys[i], key)) {
-                return values[i];
-            }
+        int keyIndex = getKeyIndex(key);
+        if (keyIndex != -1) {
+            return values[keyIndex];
         }
         return foundValueByKey;
     }
@@ -46,5 +43,15 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return size;
+    }
+
+    private int getKeyIndex(K key) {
+        int index = -1;
+        for (int i = 0; i < size; i++) {
+            if ((keys[i] == key) || (keys[i] != null && keys[i].equals(key))) {
+                index = i;
+            }
+        }
+        return index;
     }
 }
