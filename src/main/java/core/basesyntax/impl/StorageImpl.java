35fbc43be7f6af03eeef7ee0b918fff0711a3b1b
JaverJaver <mopasaj205@arpizol.com>
2024-06-14 17:29:20 +0300

    fix checkstyle

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,8 +2,6 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
-import java.util.Objects;
-
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_CAPACITY = 10;
     private final K[] keys;
