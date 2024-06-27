b0cc29c68b71ecbbe252a7e9b256c5224900c34f
Oleh <olehliashyk@gmail.com>
2024-06-27 21:09:17 +0300

    added-rendering-obj-1.1

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -12,10 +12,14 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         this.size = 0;
     }
 
+    private boolean keysEqual(K key1, K key2) {
+        return key1 == null ? key2 == null : key1.equals(key2);
+    }
+
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] == null && key == null || keys[i] != null && keys[i].equals(key)) {
+            if (keysEqual((K) keys[i], key)) {
                 values[i] = value;
                 return;
             }
@@ -32,7 +36,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] == null && key == null || keys[i] != null && keys[i].equals(key)) {
+            if (keysEqual((K) keys[i], key)) {
                 return (V) values[i];
             }
         }
