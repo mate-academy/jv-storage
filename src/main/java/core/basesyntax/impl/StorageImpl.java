0c61807a9cd7b4baa591fa065600e240d3eec7df
jakibczukw <jakibczukvlad@gmail.com>
2024-06-27 16:24:23 +0200

    deleted redundant this operator, created private method indexOfKey, inverted if

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -9,35 +9,40 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private int size;
 
     public StorageImpl() {
-        this.keys = (K[]) new Object[MAX_SIZE];
-        this.values = (V[]) new Object[MAX_SIZE];
+        keys = (K[]) new Object[MAX_SIZE];
+        values = (V[]) new Object[MAX_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < size; i++) {
-            if (key == keys[i] || key != null && key.equals(keys[i])) {
-                values[i] = value;
-                return;
-            }
+        if (indexOfKey(key) != -1) {
+            values[indexOfKey(key)] = value;
+            return;
         }
-        if (size < MAX_SIZE) {
-            keys[size] = key;
-            values[size] = value;
-            size++;
-        } else {
+        if (size > MAX_SIZE) {
             throw new IndexOutOfBoundsException("Storage is full");
         }
+
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        if (indexOfKey(key) == -1) {
+            return null;
+        }
+        return values[indexOfKey(key)];
+    }
+
+    private int indexOfKey(K key) {
         for (int i = 0; i < size; i++) {
             if (key == keys[i] || key != null && key.equals(keys[i])) {
-                return values[i];
+                return i;
             }
         }
-        return null;
+        return -1;
     }
 
     @Override
