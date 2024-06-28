2f89bd77ba6d10fb25d91b99c97dcfc854364ee6
Artem Hurievskyi <gurievsky.artem@gmail.com>
2024-06-28 10:58:11 +0300

    replaced unnesesary methods isNull and isAlreadyExist and create new correct method to find array index

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,45 +3,36 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static final int ARRAY_LENGTH = 5;
+    private static final int ARRAY_LENGTH = 10;
     private int size;
-    private K[] keyArray;
-    private V[] valueArray;
+    private K[] keys;
+    private V[] values;
 
     public StorageImpl() {
-        valueArray = (V[])new Object[ARRAY_LENGTH];
-        keyArray = (K[]) new Object[ARRAY_LENGTH];
+        values = (V[])new Object[ARRAY_LENGTH];
+        keys = (K[]) new Object[ARRAY_LENGTH];
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < keyArray.length; i++) {
-            if (isNull(key, i)) {
-                valueArray[i] = value;
-                size++;
-                return;
-            } else if (isAlreadyExistKey(key, i)) {
-                valueArray[i] = value;
-                return;
-            }
-        }
-        if (size < ARRAY_LENGTH) {
-            keyArray[size] = key;
-            valueArray[size] = value;
-            size++;
+        int index = getIndex(key);
+        if (index != -1) {
+            values[index] = value;
         } else {
-            throw new IllegalStateException("Storage is full");
+            if (size == ARRAY_LENGTH) {
+                throw new ArrayIndexOutOfBoundsException("Storage is full");
+            }
+            keys[size] = key;
+            values[size] = value;
+            size++;
         }
+
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < size; i++) {
-            if ((key == null && keyArray[i] == null) || (key != null && key.equals(keyArray[i]))) {
-                return valueArray[i];
-            }
-        }
-        return null;
+        int index = getIndex(key);
+        return index == -1 ? null : values[index];
     }
 
     @Override
@@ -49,12 +40,12 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         return size;
     }
 
-    private boolean isNull(K key, int index) {
-        return key == null && keyArray[index] == null && valueArray[index] == null;
-    }
-
-    private boolean isAlreadyExistKey(K key, int index) {
-        return (key == null && keyArray[index] == null && valueArray[index] != null)
-                || (key != null && key.equals(keyArray[index]));
+    private int getIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || (key != null && key.equals(keys[i]))) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
