f1152a1f2a10ca79b54d31b2b6b9cab4b9e9ca6d
maaarynet <mashajazz.mj@gmail.com>
2024-07-02 19:44:37 +0300

    implemented Storage interface

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,42 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private int size;
+    private K[] keys;
+    private V[] values;
+
+    @SuppressWarnings("unchecked")
+    public StorageImpl() {
+        size = 0;
+        keys = (K[]) new Object[10];
+        values = (V[]) new Object[10];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == null && key == null || (keys[i] != null && keys[i].equals(key))) {
+                values[i] = value;
+                return;
+            }
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] == null && key == null || (keys[i] != null && keys[i].equals(key))) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
