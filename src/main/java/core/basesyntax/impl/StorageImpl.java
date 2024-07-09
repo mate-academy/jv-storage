1b8da858d2cda53cafc46d8325846da44b8c1195
Oleksandr Prots <sashaproc2289@gmail.com>
2024-07-09 18:56:25 +0300

    jv-storage fixed some mistakes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,31 +1,33 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int SIZEOF = 10;
     private K[] keys;
     private V[] values;
     private int size;
 
     public StorageImpl() {
-        keys = (K[]) new Object[10];
-        values = (V[]) new Object[10];
+        keys = (K[]) new Object[SIZEOF];
+        values = (V[]) new Object[SIZEOF];
         size = 0;
     }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (key == null && keys[i] == null) {
+            if (Objects.equals(key, keys[i]) && keys[i] == null) {
                 values[i] = value;
                 return;
             }
-            if (key != null && key.equals(keys[i])) {
+            if (key != null && Objects.equals(key, keys[i])) {
                 values[i] = value;
                 return;
             }
         }
-        if (size < 10) {
+        if (size < SIZEOF) {
             keys[size] = key;
             values[size] = value;
             size++;
@@ -35,10 +37,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (key == null && keys[i] == null) {
+            if (Objects.equals(key, keys[i]) && keys[i] == null) {
                 return values[i];
             }
-            if (key != null && key.equals(keys[i])) {
+            if (key != null && Objects.equals(key, keys[i])) {
                 return values[i];
             }
         }
