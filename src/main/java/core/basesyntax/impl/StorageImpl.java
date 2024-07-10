cd3ebb48f7f5b105266e8e8263596087459c9fbe
Oleksandr Prots <sashaproc2289@gmail.com>
2024-07-10 14:58:42 +0300

    jv-storage fixed if

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -16,11 +16,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (key == null && keys[i] == null) {
-                values[i] = value;
-                return;
-            }
-            if (key != null && key.equals(keys[i])) {
+            if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
                 values[i] = value;
                 return;
             }
@@ -35,10 +31,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (key == null && keys[i] == null) {
-                return values[i];
-            }
-            if (key != null && key.equals(keys[i])) {
+            if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
                 return values[i];
             }
         }
