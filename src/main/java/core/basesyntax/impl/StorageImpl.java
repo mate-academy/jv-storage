bf9fde04a527756fa4807c4ca04d4379307820a0
Oleksandr Prots <sashaproc2289@gmail.com>
2024-07-10 13:53:30 +0300

    jv-storage fixed some mistakes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,7 +1,6 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
@@ -17,7 +16,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (key.equals(keys[i]) && keys[i] == null) {
+            if (key == null && keys[i] == null) {
                 values[i] = value;
                 return;
             }
@@ -36,7 +35,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (key.equals(keys[i]) && keys[i] == null) {
+            if (key == null && keys[i] == null) {
                 return values[i];
             }
             if (key != null && key.equals(keys[i])) {
