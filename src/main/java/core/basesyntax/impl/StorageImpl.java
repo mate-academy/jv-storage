65738f0503eea5fb4e019c2603bb126070440553
OD <sdenisenko055@gmail.com>
2024-07-13 16:29:31 +0300

    Result

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,67 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int ARRAY_SIZE = 10;
+    private static final int START_SIZE = 0;
+    private int size;
+    private K[] keyArray;
+    private V[] valueArray;
+
+    public StorageImpl() {
+        keyArray = (K[]) new Object[ARRAY_SIZE];
+        valueArray = (V[]) new Object[ARRAY_SIZE];
+        size = START_SIZE;
+    }
+
     @Override
     public void put(K key, V value) {
+        if (checkKey(key, value)) {
+            return;
+        }
+        keyArray[size] = key;
+        valueArray[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        if (key == null) {
+            for (int i = 0; i < size; i++) {
+                if (key == keyArray[i]) {
+                    return valueArray[i];
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (key.equals(keyArray[i])) {
+                    return valueArray[i];
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    public boolean checkKey(K key, V value) {
+        if (key == null) {
+            for (int i = 0; i < size; i++) {
+                if (key == keyArray[i]) {
+                    valueArray[i] = value;
+                    return true;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (key.equals(keyArray[i])) {
+                    valueArray[i] = value;
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
