291995338208db4ef6273e0cfee3284ca108a3e9
OD <sdenisenko055@gmail.com>
2024-07-14 12:26:47 +0300

    Created method indexOf() and place it in put() and get() Created constant NOT_FOUND (= -1)

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,6 +4,7 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int ARRAY_SIZE = 10;
+    private static final int NOT_FOUND = -1;
     private int size;
     private K[] keyArray;
     private V[] valueArray;
@@ -15,29 +16,33 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < size; i++) {
-            if ((key == null && keyArray[i] == null) || (key != null && key.equals(keyArray[i]))) {
-                valueArray[i] = value;
-                return;
-            }
+        int index = indexOf(key);
+        if (index == NOT_FOUND) {
+            keyArray[size] = key;
+            valueArray[size] = value;
+            size++;
+        } else {
+            valueArray[index] = value;
         }
-        keyArray[size] = key;
-        valueArray[size] = value;
-        size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < size; i++) {
-            if ((key == null && keyArray[i] == null) || (key != null && key.equals(keyArray[i]))) {
-                return valueArray[i];
-            }
-        }
-        return null;
+        int index = indexOf(key);
+        return (index == NOT_FOUND) ? null : valueArray[index];
     }
 
     @Override
     public int size() {
         return size;
+    }
+
+    private int indexOf(K key) {
+        for (int i = 0; i < size; i++) {
+            if ((key == null && keyArray[i] == null) || (key != null && key.equals(keyArray[i]))) {
+                return i;
+            }
+        }
+        return NOT_FOUND;
     }
 }
