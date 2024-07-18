7b0c8142acab407aca66d291ef9bc056a66b2662
Denys Mazurenko <0gur4ik3992@gmail.com>
2024-07-18 16:12:08 +0300

    created a condition without using Object-equals

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,7 +2,6 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 import java.util.Arrays;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ARRAY_SIZE = 10;
@@ -46,7 +45,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     private int indexOf(K key) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(key,keys[i])) {
+            if (keys[i] == key || key != null && key.equals(keys[i])) {
                 return i;
             }
         }
