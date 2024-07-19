35415cc6a22380c3981783449d63b555958026da
YevhenSrdk <eugeneserdyuk23@gmail.com>
2024-07-19 18:45:47 +0300

    Moved private method after public ones

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -13,18 +13,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         this.values = new Object[MAX_ARRAY_SIZE];
     }
 
-    private int findIndex(K key) {
-        for (int i = 0; i < size; i++) {
-            if (key != null && key.equals(keys[i])) {
-                return i;
-            }
-            if (key == null && keys[i] == null) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
     @Override
     public void put(K key, V value) {
         int index = findIndex(key);
@@ -52,5 +40,17 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return size;
+    }
+
+    private int findIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key != null && key.equals(keys[i])) {
+                return i;
+            }
+            if (key == null && keys[i] == null) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
