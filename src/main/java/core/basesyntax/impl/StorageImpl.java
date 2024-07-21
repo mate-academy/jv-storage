4e5d323a629be87b801b718a618fa4592fb4da84
Petro Shchur <pepe2.98.12@icloud.com>
2024-07-21 09:11:15 -0400

    all done? second try

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,63 +1,37 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
-    private Object[] keys;
-    private Object[] values;
-    private boolean hasNullKey;
-    private V nullKeyValue;
+    private K[] keys;
+    private V[] values;
     private int size;
 
     @SuppressWarnings("unchecked")
     public StorageImpl() {
-        this.keys = new Object[MAX_SIZE];
-        this.values = new Object[MAX_SIZE];
-        this.hasNullKey = false;
-        this.nullKeyValue = null;
+        this.keys = (K[]) new Object[MAX_SIZE];
+        this.values = (V[]) new Object[MAX_SIZE];
         this.size = 0;
     }
 
     @Override
     public void put(K key, V value) {
-        if (key == null) {
-            if (!hasNullKey) {
-                hasNullKey = true;
-                nullKeyValue = value;
-                size++;
-            } else {
-                nullKeyValue = value;
-            }
-            return;
+        int index = indexOf(key);
+        if (index != -1) {
+            values[index] = value;
+        } else if (index == -1) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
         }
-        for (int i = 0; i < size; i++) {
-            if (keys[i] != null && keys[i].equals(key)) {
-                values[i] = value;
-                return;
-            }
-        }
-        if (size == keys.length) {
-            resize();
-        }
-        keys[size] = key;
-        values[size] = value;
-        size++;
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public V get(K key) {
-        if (key == null) {
-            return nullKeyValue;
-        }
-        for (int i = 0; i < size; i++) {
-            if (keys[i] != null && keys[i].equals(key)) {
-                return (V) values[i];
-            }
-        }
-        return null;
+        int index = indexOf(key);
+        return index == -1 ? null : values[index];
     }
 
     @Override
@@ -65,9 +39,16 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         return size;
     }
 
-    private void resize() {
-        int newSize = keys.length * 2;
-        keys = Arrays.copyOf(keys, newSize);
-        values = Arrays.copyOf(values, newSize);
+    private int indexOf(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == null) {
+                if (keys[i] == null) {
+                    return i;
+                }
+            } else if (key.equals(keys[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
