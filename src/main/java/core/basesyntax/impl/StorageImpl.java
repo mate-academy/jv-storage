9b4310d21c1ac70ca0aa2c6b76a2d154475015a7
homeworkfirst <zookotelif@gmail.com>
2024-07-25 22:11:02 +0300

    aded class indexOf

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -8,34 +8,46 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private V[] values;
     private int size;
 
+    @SuppressWarnings("unchecked")
     public StorageImpl() {
         keys = (K[]) new Object[MAX_SIZE];
         values = (V[]) new Object[MAX_SIZE];
     }
 
+    @Override
     public void put(K key, V value) {
-        for (int i = 0; i < size; i++) {
-            if (keysEqual(keys[i], key)) {
-                values[i] = value;
-                return;
-            }
+        int index = indexOf(key);
+        if (index != -1) {
+            values[index] = value;
+        } else {
+            keys[size] = key;
+            values[size] = value;
+            size++;
         }
-        keys[size] = key;
-        values[size] = value;
-        size++;
     }
 
+    @Override
     public V get(K key) {
-        for (int i = 0; i < size; i++) {
-            if (keysEqual(keys[i], key)) {
-                return values[i];
-            }
+        int index = indexOf(key);
+        if (index != -1) {
+            return values[index];
+        } else {
+            return null;
         }
-        return null;
     }
 
+    @Override
     public int size() {
         return size;
+    }
+
+    private int indexOf(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keysEqual(keys[i], key)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     private boolean keysEqual(K key1, K key2) {
