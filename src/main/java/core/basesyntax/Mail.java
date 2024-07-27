8d8fdb17defee08b9d94896ab50e944ddccf4402
kozma <kozmakruckov@gmail.com>
2024-07-27 21:38:07 +0300

    added jv-storage-solution

---
+++ src/main/java/core/basesyntax/Mail.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+import core.basesyntax.impl.StorageImpl;
+
+public class Mail {
+    public static void main(String[] args) {
+
+
+    }
+}
--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,8 +3,13 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private K key;
+    private V value;
+
     @Override
     public void put(K key, V value) {
+        this.key = key;
+        this.value = value;
     }
 
     @Override
