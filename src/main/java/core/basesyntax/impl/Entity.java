5e48f853d5c8e7817c2f675ea1aef4810d95dc8a
Hetalova <nhetalova@luxoft.com>
2024-08-01 13:57:21 +0300

    Added solution for homework

---
+++ src/main/java/core/basesyntax/impl/Entity.java
@@ -0,0 +1,36 @@
+package core.basesyntax.impl;
+
+public class Entity<K, V> {
+    private K key;
+    private V value;
+
+    public Entity(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "StorageImpl{" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
+    }
+
+}
--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,63 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int DEFAULT_SIZE_OF_ARRAY = 10;
+    private int size;
+    private Entity<K, V> entity;
+    private Entity<K, V>[] entityArr;
+
+    public StorageImpl() {
+        this.entityArr = new Entity[DEFAULT_SIZE_OF_ARRAY];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (!checkIfKeyPresent(key)) {
+            entity = new Entity<>(key, value);
+            entityArr[size] = entity;
+            size++;
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (entityArr[i].getKey() == null || entityArr[i].getKey().equals(key)) {
+                    entityArr[i].setValue(value);
+                }
+
+            }
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        V result = null;
+        for (int i = 0; i < size; i++) {
+            if (entityArr[i].getKey() == null && key != null) {
+                continue;
+            }
+            if (entityArr[i].getKey() == key || entityArr[i].getKey().equals(key)) {
+                result = entityArr[i].getValue();
+            }
+
+        }
+        return result;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
+
+    public boolean checkIfKeyPresent(K key) {
+        for (int i = 0; i < size; i++) {
+            if (entityArr[i].getKey() == null && key != null) {
+                continue;
+            }
+            if (entityArr[i].getKey() == key || entityArr[i].getKey().equals(key)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
+
+
