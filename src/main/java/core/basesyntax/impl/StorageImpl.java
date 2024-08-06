592d85c3b4a99b8353e851963d8c461bd1359c8f
Andrii Datko <a.p.datko@gmail.com>
2024-08-06 12:47:46 +0300

    Created indexOf method to eliminate code duplication. The size field is removed from the constructor and its initial value is set to 0 by default.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,23 +6,28 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
     private final K[] keys;
     private final V[] values;
-    private int size;
+    private int size = 0;
 
     public StorageImpl() {
         keys = (K[]) new Object[MAX_SIZE];
         values = (V[]) new Object[MAX_SIZE];
-        size = 0;
+    }
+
+    private int indexOf(K key) {
+        for (int i = 0; i < size; i++) {
+            if ((keys[i] == null && key == null) || (keys[i] != null && keys[i].equals(key))) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < size; i++) {
-            if ((keys[i] == null && key == null) || (keys[i] != null && keys[i].equals(key))) {
-                values[i] = value;
-                return;
-            }
-        }
-        if (size < MAX_SIZE) {
+        int index = indexOf(key);
+        if (index != -1) {
+            values[index] = value;
+        } else if (size < MAX_SIZE) {
             keys[size] = key;
             values[size] = value;
             size++;
@@ -31,12 +36,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < size; i++) {
-            if ((keys[i] == null && key == null) || (keys[i] != null && keys[i].equals(key))) {
-                return values[i];
-            }
-        }
-        return null;
+        int index = indexOf(key);
+        return index != -1 ? values[index] : null;
     }
 
     @Override
