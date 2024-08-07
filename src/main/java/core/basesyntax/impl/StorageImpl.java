ffc8d568f338f6c244ce6b7e293ea2c09a268c00
Nikita Khilobok <nicktich284@gmail.com>
2024-08-07 17:44:17 +0300

    Updated after review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,7 +4,6 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     public static final int MAX_SIZE = 10;
-    public static final int HASH_RESULT = 17;
     private K[] keys;
     private V[] values;
     private int size;
@@ -12,13 +11,12 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public StorageImpl() {
         keys = (K[]) new Object[MAX_SIZE];
         values = (V[]) new Object[MAX_SIZE];
-        size = 0;
     }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] == key || (keys[i] != null && keys[i].equals(key))) {
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
                 values[i] = value;
                 return;
             }
@@ -35,7 +33,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] == key || (keys[i] != null && keys[i].equals(key))) {
+            if (keys[i] == key || keys[i] != null && keys[i].equals(key)) {
                 return values[i];
             }
         }
@@ -45,31 +43,5 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return size;
-    }
-
-    @Override
-    public boolean equals(Object storage) {
-        if (storage == this) {
-            return true;
-        }
-        if (storage == null) {
-            return false;
-        }
-        if (storage.getClass().equals(StorageImpl.class)) {
-            StorageImpl current = (StorageImpl) storage;
-            return (keys != null ? this.keys.equals(current.keys)
-                    : current.keys == null)
-                    && (values != null ? this.values.equals(current.values)
-                    : current.values == null);
-        }
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = HASH_RESULT;
-        result = HASH_RESULT * result + (keys == null ? 0 : keys.hashCode());
-        result = HASH_RESULT * result + (values == null ? 0 : values.hashCode());
-        return result;
     }
 }
