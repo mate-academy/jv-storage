c59daacbb782b123cd5f7d5185ee6a8a64dbf79f
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2024-08-09 11:59:38 +0200

    please help

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,50 +1,56 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
-    private K[] keys;
-    private V[] values;
+    private final K[] keys;
+    private final V[] values;
     private int size;
 
     @SuppressWarnings("unchecked")
     public StorageImpl() {
         keys = (K[]) new Object[MAX_SIZE];
         values = (V[]) new Object[MAX_SIZE];
-        size = 0;
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i] == null ? key == null : keys[i].equals(key)) {
-                values[i] = value;
-                return;
-            }
-        }
-
-        if (size < MAX_SIZE) {
-            keys[size] = key;
-            values[size] = value;
-            size++;
+        int index = indexOf(key);
+        if (index != -1) {
+            values[index] = value;
         } else {
-            throw new IllegalStateException("Storage is Full");
+            if (size < MAX_SIZE) {
+                keys[size] = key;
+                values[size] = value;
+                size++;
+            } else {
+                throw new IllegalStateException("Storage is Full");
+            }
         }
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < size; i++) {
-            if (keys[i] == null ? key == null : keys[i].equals(key)) {
-                return values[i];
-            }
+        {
+            int index = indexOf(key);
+            return index != -1 ? values[index] : null;
         }
-        return null;
     }
 
     @Override
     public int size() {
         return 0;
     }
+
+    private int indexOf(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(keys[i], key)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
+
