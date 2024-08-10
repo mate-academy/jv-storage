c8595820a6e63301a6bc0c0bb59381a0a54fa2dc
Nikita Lafinskiy <niklafinskiy@gmail.com>
2024-08-10 13:44:58 +0200

    implemented the storage functionality

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,54 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_STORAGE_SIZE = 10;
+    private static final int NO_SUCH_INDEX = -1;
+
+    private final K[] keys;
+    private final V[] values;
+    private int size = 0;
+
+    @SuppressWarnings("unchecked")
+    public StorageImpl() {
+        this.keys = (K[]) new Object[MAX_STORAGE_SIZE];
+        this.values = (V[]) new Object[MAX_STORAGE_SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = findIndex(key);
+        if (index != NO_SUCH_INDEX) {
+            values[index] = value;
+        } else if (size < MAX_STORAGE_SIZE) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int index = findIndex(key);
+
+        if (index == NO_SUCH_INDEX) {
+            return null;
+        }
+
+        return values[index];
     }
 
     @Override
     public int size() {
-        return -1;
+        return this.size;
+    }
+
+    private int findIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
+                return i;
+            }
+        }
+
+        return NO_SUCH_INDEX;
     }
 }
