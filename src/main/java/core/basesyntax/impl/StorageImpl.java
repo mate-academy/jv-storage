789d9e9a29e6cad3eb163c08837b0671a0f96234
KostiaO <kostiaomelcenko@gmail.com>
2024-08-12 01:44:46 +0200

    remade

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,19 +5,25 @@ import core.basesyntax.Storage;
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_STORAGE_LENGTH = 10;
 
-    private K[] keys = (K[]) new Object[MAX_STORAGE_LENGTH];
+    private K[] keys;
 
-    private V[] values = (V[]) new Object[MAX_STORAGE_LENGTH];
+    private V[] values;
 
-    private int storageIndex = 0;
+    private int storageIndex;
+
+    public StorageImpl() {
+        this.values = (V[]) new Object[MAX_STORAGE_LENGTH];
+        this.keys = (K[]) new Object[MAX_STORAGE_LENGTH];
+        this.storageIndex = 0;
+    }
 
     @Override
     public void put(K key, V value) {
-        if (isNoMoreSpace()) {
-            return;
+        if (isIndexOutOfBounds()) {
+            throw new RuntimeException("Storage is out of bounds");
         }
 
-        if (doesItemExist(key, value)) {
+        if (isItemExistsInArray(key, value)) {
             return;
         }
 
@@ -29,10 +35,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < storageIndex; i++) {
-            if (key != null ? key.equals(keys[i]) : keys[i] == null) {
-                return values[i];
-            }
+        int index = getValueIndex(key);
+
+        if (index >= 0) {
+            return values[index];
         }
 
         return null;
@@ -43,19 +49,29 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         return storageIndex;
     }
 
-    private boolean isNoMoreSpace() {
+    private boolean isIndexOutOfBounds() {
         return storageIndex - 1 >= MAX_STORAGE_LENGTH;
     }
 
-    private boolean doesItemExist(K key, V value) {
-        for (int i = 0; i < storageIndex; i++) {
-            if (key != null ? key.equals(keys[i]) : keys[i] == null) {
-                this.values[i] = value;
+    private boolean isItemExistsInArray(K key, V value) {
+        int index = getValueIndex(key);
 
-                return true;
-            }
+        if (index >= 0) {
+            this.values[index] = value;
+
+            return true;
         }
 
         return false;
+    }
+
+    private int getValueIndex(K key) {
+        for (int i = 0; i < storageIndex; i++) {
+            if (key != null ? key.equals(keys[i]) : keys[i] == null) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
