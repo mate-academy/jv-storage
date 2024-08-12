cd6c36f8a9c9ceb2d6736a3432996fa51fb8aa29
KostyaOmelchenko <kostiantyn.omelchenko@enjoyit.at>
2024-08-12 17:11:55 +0200

    sadsad

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,57 +5,30 @@ import core.basesyntax.Storage;
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_STORAGE_LENGTH = 10;
 
-    private K[] keys = (K[]) new Object[MAX_STORAGE_LENGTH];
-
-    private V[] values = (V[]) new Object[MAX_STORAGE_LENGTH];
-
-    private int storageIndex = 0;
+    private StorageItem[] storageItems = (StorageItem[]) new Object[MAX_STORAGE_LENGTH];
 
     @Override
     public void put(K key, V value) {
-        if (isNoMoreSpace()) {
-            return;
-        }
 
-        if (doesItemExist(key, value)) {
-            return;
-        }
-
-        this.keys[storageIndex] = key;
-        this.values[storageIndex] = value;
-
-        storageIndex++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < storageIndex; i++) {
-            if (key != null ? key.equals(keys[i]) : keys[i] == null) {
-                return values[i];
-            }
-        }
-
         return null;
     }
 
     @Override
     public int size() {
-        return storageIndex;
+        return 0;
     }
 
-    private boolean isNoMoreSpace() {
-        return storageIndex - 1 >= MAX_STORAGE_LENGTH;
-    }
+    private class StorageItem {
+        private K key;
+        private V value;
 
-    private boolean doesItemExist(K key, V value) {
-        for (int i = 0; i < storageIndex; i++) {
-            if (key != null ? key.equals(keys[i]) : keys[i] == null) {
-                this.values[i] = value;
-
-                return true;
-            }
+        protected StorageItem(K key, V value) {
+            this.key = key;
+            this.value = value;
         }
-
-        return false;
     }
 }
