26a368061c1a88418df7b0facccc6f06341c1b19
skrypkasv <skrypkasv@gmail.com>
2024-08-15 01:04:22 +0300

    create Storage implementation

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,52 @@
 package core.basesyntax.impl;
 
+import core.basesyntax.KeyValue;
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    public static final int MAX_SIZE = 10;
+    public static final int ZERO = 0;
+    private KeyValue<K, V>[] store;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public StorageImpl() {
+        this.store = new KeyValue[MAX_SIZE];
+        this.size = ZERO;
+    }
+
     @Override
     public void put(K key, V value) {
+        if (size < store.length) {
+            for (int i = 0; i < store.length; i++) {
+                if (store[i] == null) {
+                    store[i] = new KeyValue<>(key, value);
+                    size++;
+                    return;
+                }
+                if (store[i] != null && Objects.equals(store[i].getKey(), key)) {
+                    store[i].setValue(value);
+                    return;
+                }
+            }
+        } else {
+            System.out.println("Full storage!");
+        }
     }
 
     @Override
     public V get(K key) {
+        for (KeyValue<K, V> element : store) {
+            if (element != null && Objects.equals(element.getKey(), key)) {
+                return element.getValue();
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
