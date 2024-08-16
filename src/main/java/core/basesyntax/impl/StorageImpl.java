9682ee589a5064cc0507de12b572308a8389f879
Nikita Kravtsov <deez999deez@gmail.com>
2024-08-16 17:59:24 +0300

    Implement StorageImpl service to store the key-value data by using two Lists with key and value and update index if needed.
    Implement the put method to store data to storage and update value if new key-value pair have key that equal to key that already exist in storage.
    
    Implement get method to get value by key.
    
    Implement size method to return size of storage.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,43 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ARRAY_VALUE = 10;
+    private final List<K> keys;
+    private final List<V> values;
+
+    public StorageImpl() {
+        keys = new ArrayList<>(MAX_ARRAY_VALUE);
+        values = new ArrayList<>(MAX_ARRAY_VALUE);
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < keys.size(); i++) {
+            if (keys.get(i) == null ? key == null : keys.get(i).equals(key)) {
+                values.set(i, value);
+                return;
+            }
+        }
+        keys.add(key);
+        values.add(value);
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keys.size(); i++) {
+            if (keys.get(i) == null ? key == null : keys.get(i).equals(key)) {
+                return values.get(i);
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return keys.size();
     }
 }
