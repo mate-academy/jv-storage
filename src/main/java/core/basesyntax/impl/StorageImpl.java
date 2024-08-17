1b55785b3038c59b15ed623224949008233f9f38
Bohdan Stratiychuk <vasyahunta@gmail.com>
2024-08-17 12:07:04 +0300

    corected method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,18 +2,28 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private Map<K, V> storage;
+
+    public StorageImpl() {
+        this.storage = new HashMap<>();
+    }
+
     @Override
     public void put(K key, V value) {
+        storage.put(key, value);
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return storage.get(key);
     }
 
     @Override
     public int size() {
-        return -1;
+        return storage.size();
     }
 }
