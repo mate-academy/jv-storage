19adc0140ebd951c9dc1c8f79ff06a04f302b762
Bohdan Stratiychuk <vasyahunta@gmail.com>
2024-08-20 17:29:57 +0300

    checkstyling

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,28 +1,56 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.HashMap;
-import java.util.Map;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private Map<K, V> storage;
+    private static final int INITIAL_CAPACITY = 10;
+    private K[] keys;
+    private V[] values;
+    private int size;
 
     public StorageImpl() {
-        this.storage = new HashMap<>();
+        keys = (K[]) new Object[INITIAL_CAPACITY];
+        values = (V[]) new Object[INITIAL_CAPACITY];
+        size = 0;
     }
 
     @Override
     public void put(K key, V value) {
-        storage.put(key, value);
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == key || (keys[i] != null && keys[i].equals(key))) {
+                values[i] = value;
+                return;
+            }
+        }
+        if (size == keys.length) {
+            resize();
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        return storage.get(key);
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == key || (keys[i] != null && keys[i].equals(key))) {
+                return values[i];
+            }
+        }
+        return null;
     }
 
     @Override
     public int size() {
-        return storage.size();
+        return size;
+    }
+
+    private void resize() {
+        K[] newKeys = (K[]) new Object[keys.length * 2];
+        V[] newValues = (V[]) new Object[values.length * 2];
+        System.arraycopy(keys, 0, newKeys, 0, keys.length);
+        System.arraycopy(values, 0, newValues, 0, values.length);
+        keys = newKeys;
+        values = newValues;
     }
 }
