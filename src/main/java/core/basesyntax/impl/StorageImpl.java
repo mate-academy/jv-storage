2a472e520ca6bbfa9baa03f6c61ebca9cd1c599f
Nikita Kravtsov <deez999deez@gmail.com>
2024-08-20 13:54:55 +0300

    Make changes in equals checks in Put and Get methods, avoiding use object class

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,11 +1,9 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ARRAY_VALUE = 10;
-    private static final int START_SIZE_VALUE = 0;
     private final K[] keys;
     private final V[] values;
     private int size;
@@ -14,13 +12,13 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     public StorageImpl() {
         keys = (K[]) new Object[MAX_ARRAY_VALUE];
         values = (V[]) new Object[MAX_ARRAY_VALUE];
-        size = START_SIZE_VALUE;
     }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(keys[i], key)) {
+            if ((keys[i] == null && key == null)
+                    || (keys[i] != null && keys[i].equals(key))) {
                 values[i] = value;
                 return;
             }
@@ -35,7 +33,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < keys.length; i++) {
-            if (Objects.equals(keys[i], key)) {
+            if ((keys[i] == null && key == null)
+                    || (keys[i] != null && keys[i].equals(key))) {
                 return values[i];
             }
         }
