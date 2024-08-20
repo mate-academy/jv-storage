769923e197ef41af2e3f816ac0c2276888139aaf
tbasy <t.basylkevych@gmail.com>
2024-08-20 14:32:20 +0300

    Implemented the Storage of the key-value type

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,64 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ELEMENTS = 10;
+    private K[] keys;
+    private V[] values;
+    private K key;
+    private V value;
+    private int sizeStorage = 0;
+
+    public StorageImpl() {
+        this.keys = (K[]) new Object[MAX_ELEMENTS];
+        this.values = (V[]) new Object[MAX_ELEMENTS];
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < sizeStorage; i++) {
+            if (key == null) {
+                if (keys[i] == null) {
+                    values[i] = value;
+                    return;
+                }
+            }
+            if (keys[i] != null && keys[i].equals(key)) {
+                values[i] = value;
+                return;
+            }
+        }
+        if (sizeStorage < MAX_ELEMENTS) {
+            keys[sizeStorage] = key;
+            values[sizeStorage] = value;
+            sizeStorage++;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < sizeStorage; i++) {
+            if (key == null) {
+                if (keys[i] == null) {
+                    return values[i];
+                }
+            }
+            if (keys[i] != null && keys[i].equals(key)) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return sizeStorage;
     }
 }
