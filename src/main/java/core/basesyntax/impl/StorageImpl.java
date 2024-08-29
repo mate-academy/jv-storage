d791b58b2e0238cb83b6db789e1c4a70825d4e00
Dima Davydov <Kilo6559@gmail.com>
2024-08-29 12:34:26 +0300

    Defects have been eliminated

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,13 +4,15 @@ import core.basesyntax.Storage;
 import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    // Initialize an array of fixed size (10 elements) to store key-value pairs of type Entry<K, V>.
     private Entry<K, V>[] entries = (Entry<K, V>[]) new Entry[10];
+    // The (Entry<K, V>[]) cast is used to remove type incompatibility warnings.
     private int size = 0;
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < entries.length; i++) {
-            if (entries[i] != null && Objects.equals(entries[i].key, key)) {
+            if (entries[i] != null && keyComparison(entries[i].key, key)) {
                 entries[i].value = value;
                 return;
             }
@@ -19,14 +21,14 @@ public class StorageImpl<K, V> implements Storage<K, V> {
             entries[size] = new Entry<>(key, value);
             size++;
         } else {
-            throw new IllegalStateException("Array is full");
+            throw new IllegalStateException("Storage has reached maximum number of items");
         }
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (entries[i] != null && Objects.equals(entries[i].key, key)) {
+            if (entries[i] != null && keyComparison(entries[i].key, key)) {
                 return entries[i].value;
             }
         }
@@ -38,24 +40,22 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         return size;
     }
 
+    private boolean keyComparison(K key1, K key2) {
+        if (key1 == key2) {
+            return true;
+        }
+        if (key1 == null || key2 == null) {
+            return false;
+        }
+        return key1.equals(key2);
+    }
+
     private class Entry<K, E> {
         private final K key;
         private V value;
 
         public Entry(K key, V value) {
             this.key = key;
-            this.value = value;
-        }
-
-        public K getKey() {
-            return key;
-        }
-
-        public V getValue() {
-            return value;
-        }
-
-        public void setValue(V value) {
             this.value = value;
         }
     }
