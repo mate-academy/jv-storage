85e35240c5da82be2a8e9926049f58af096e2604
Alinka Lyn <alinka.lyn@ukr.net>
2024-08-31 15:52:53 +0300

    solved task

---
+++ src/main/java/core/basesyntax/StorageImpl.java
@@ -0,0 +1,44 @@
+package core.basesyntax;
+
+public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE = 10;
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_SIZE];
+        values = (V[]) new Object[MAX_SIZE];
+        size = 0;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i].equals(key)) {
+                values[i] = value;
+                return;
+            }
+        }
+        if (size < MAX_SIZE) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
+    }
+
+    @Override
+    public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i].equals(key)) {
+                return values[i];
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+}
