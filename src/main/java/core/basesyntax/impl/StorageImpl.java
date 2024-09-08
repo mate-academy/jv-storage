6c1b92e991d386ced282ae75d565f1bafbf80ce2
Anton Pekariev <litsss30071989@gmail.com>
2024-09-08 21:10:41 +0300

    jv-storage task is complete

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,45 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int ARRAYS_LENGTH = 10;
+    private Object[] arrayOfKeys = new Object[ARRAYS_LENGTH];
+    private Object[] arrayOfValues = new Object[ARRAYS_LENGTH];
+    private int lastIndex = 0;
+
+    public StorageImpl() {
+        Arrays.fill(this.arrayOfKeys, new Object());
+        Arrays.fill(this.arrayOfValues, new Object());
+    }
+    
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < ARRAYS_LENGTH - 1; i++) {
+            if (Objects.equals(key, arrayOfKeys[i])) {
+                arrayOfValues[i] = value;
+                return;
+            }
+        }
+        arrayOfKeys[lastIndex] = key;
+        arrayOfValues[lastIndex] = value;
+        lastIndex++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < ARRAYS_LENGTH - 1; i++) {
+            if (Objects.equals(key, arrayOfKeys[i])) {
+                return (V) arrayOfValues[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return lastIndex;
     }
 }
