483cf4452d0d73eaf42ac991f2b9e62b72e6bf70
Anton Pekariev <litsss30071989@gmail.com>
2024-09-12 11:50:13 +0300

    jv-storage: a constructor has been added and other remarks amended.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,38 +1,39 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Arrays;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int ARRAYS_LENGTH = 10;
-    private Object[] arrayOfKeys = new Object[ARRAYS_LENGTH];
-    private Object[] arrayOfValues = new Object[ARRAYS_LENGTH];
-    private int lastIndex = 0;
+    private K[] arrayOfKeys;
+    private V[] arrayOfValues;
+    private int size;
 
     public StorageImpl() {
-        Arrays.fill(this.arrayOfKeys, new Object());
-        Arrays.fill(this.arrayOfValues, new Object());
+        arrayOfKeys = (K[]) new Object[ARRAYS_LENGTH];
+        arrayOfValues = (V[]) new Object[ARRAYS_LENGTH];
     }
-    
+
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < ARRAYS_LENGTH - 1; i++) {
-            if (Objects.equals(key, arrayOfKeys[i])) {
+        for (int i = 0; i < size; i++) {
+            if (key == arrayOfKeys[i] || (key != null && key.equals(arrayOfKeys[i]))) {
+                if (arrayOfValues[i] == null) {
+                    size++;
+                }
                 arrayOfValues[i] = value;
                 return;
             }
         }
-        arrayOfKeys[lastIndex] = key;
-        arrayOfValues[lastIndex] = value;
-        lastIndex++;
+        arrayOfKeys[size] = key;
+        arrayOfValues[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < ARRAYS_LENGTH - 1; i++) {
-            if (Objects.equals(key, arrayOfKeys[i])) {
-                return (V) arrayOfValues[i];
+        for (int i = 0; i < size; i++) {
+            if (key == arrayOfKeys[i] || (key != null && key.equals(arrayOfKeys[i]))) {
+                return arrayOfValues[i];
             }
         }
         return null;
@@ -40,6 +41,6 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return lastIndex;
+        return size;
     }
 }
