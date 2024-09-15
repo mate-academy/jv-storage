302866dc6b8fc13915c714661111b5a0b265718e
kozma <kozmakruckov@gmail.com>
2024-09-15 21:23:13 +0300

    added jv-storage-solution 3

--- src/main/java/core/basesyntax/Mail.java
+++ src/main/java/core/basesyntax/Mail.java
@@ -1,10 +1,7 @@
 package core.basesyntax;
 
-import core.basesyntax.impl.StorageImpl;
-
 public class Mail {
     public static void main(String[] args) {
-        Storage<Integer, String> integerStringStorage = new StorageImpl<>();
 
     }
 }
--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,14 +4,30 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int CAPACITY = 10;
-    private Object[] keys;
-    private Object[] values;
-    private int size;
+    private K[] keys;
+    private V[] values;
+    private int size = 0;
 
     public StorageImpl() {
-        keys = new Object[CAPACITY];
-        values = new Object[CAPACITY];
-        size = 0;
+        keys = (K[]) new Object[CAPACITY];
+        values = (V[]) new Object[CAPACITY];
+    }
+
+    private int indexOf(K key) {
+        if (key == null) {
+            for (int i = 0; i < size; i++) {
+                if (keys[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (keys[i] != null && keys[i].equals(key)) {
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 
     @Override
@@ -19,40 +35,21 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         if (size >= CAPACITY) {
             throw new IllegalStateException("Storage is full");
         }
-        if (key == null) {
-            for (int i = 0; i < size; i++) {
-                if (keys[i] == null) {
-                    values[i] = value;
-                    return;
-                }
-            }
+        int index = indexOf(key);
+        if (index != -1) {
+            values[index] = value;
         } else {
-            for (int i = 0; i < size; i++) {
-                if (key.equals(keys[i])) {
-                    values[i] = value;
-                    return;
-                }
-            }
+            keys[size] = key;
+            values[size] = value;
+            size++;
         }
-        keys[size] = key;
-        values[size] = value;
-        size++;
     }
 
     @Override
     public V get(K key) {
-        if (key == null) {
-            for (int i = 0; i < size; i++) {
-                if (keys[i] == null) {
-                    return (V) values[i];
-                }
-            }
-        } else {
-            for (int i = 0; i < size; i++) {
-                if (key.equals(keys[i])) {
-                    return (V) values[i];
-                }
-            }
+        int index = indexOf(key);
+        if (index != -1) {
+            return values[index];
         }
         return null;
     }
