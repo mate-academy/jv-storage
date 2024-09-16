fa63fd37930a27339aa57a5148dcfbdff77e798d
Maks <petliukofficial@gmail.com>
2024-09-16 21:48:16 +0300

    implemented methods 'put', get and 'size' in the StorageImpl class without Map

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,24 +1,46 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.HashMap;
-import java.util.Map;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private final Map<K, V> map = new HashMap<>();
+    private static final int MAX_SIZE = 10;
+    @SuppressWarnings("unchecked")
+    private final K[] keys = (K[]) new Object[MAX_SIZE];
+    @SuppressWarnings("unchecked")
+    private final V[] values = (V[]) new Object[MAX_SIZE];
+    private int size = 0;
 
     @Override
     public void put(K key, V value) {
-        map.put(key, value);
+        int index = findKeyIndex(key);
+        if (index != -1) {
+            values[index] = value;
+        } else if (size < MAX_SIZE) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
     }
 
     @Override
     public V get(K key) {
-        return map.get(key);
+        int index = findKeyIndex(key);
+        return index != -1 ? values[index] : null;
     }
 
     @Override
     public int size() {
-        return map.size();
+        return size;
+    }
+
+    private int findKeyIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            boolean isKeyNullAndMatch = (key == null && keys[i] == null);
+            boolean isKeyNonNullAndMatch = (key != null && key.equals(keys[i]));
+            if (isKeyNullAndMatch || isKeyNonNullAndMatch) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
