c8a96d8a35a0659a99058c1cda7a8463d9a73f62
Mykola Kovbasiuk <nikolya.cr@gmail.com>
2024-09-16 04:12:13 +0200

    Third try

--- src/main/java/core/basesyntax/impl/Main.java
+++ src/main/java/core/basesyntax/impl/Main.java
@@ -5,19 +5,27 @@ import core.basesyntax.Storage;
 public class Main {
     public static void main(String[] args) {
         Storage<Integer, Box> storage = new StorageImpl<>();
-        Box box = new Box();
+        Box box = new Box("beautiful1", 4, 8);
+        Box box2 = new Box("beautiful2", 6, 9);
+        Box box3 = new Box("beautiful3", 5, 7);
         storage.put(22, box);
+        storage.put(23, box2);
+        storage.put(24, box3);
         Box value = storage.get(22); // returns the Box
+        Box value2 = storage.get(23);
+        Box value3 = storage.get(24);
         int size = storage.size(); // returns storage size
         System.out.println(value);
+        System.out.println(value2);
+        System.out.println(value3);
         System.out.println(size);
 
-        Storage<Integer, String> storage2 = new StorageImpl<>();
-        storage2.put(23, "Hello");
-        storage2.put(1, "Hello3");
-        String value2 = storage2.get(23);
-        int size2 = storage2.size();
-        System.out.println(value2);
-        System.out.println(size2);
+//        Storage<Integer, String> storage2 = new StorageImpl<>();
+//        storage2.put(23, "Hello");
+//        storage2.put(1, "Hello3");
+//        String value3 = storage2.get(23);
+//        int size2 = storage2.size();
+//        System.out.println(value3);
+//        System.out.println(size2);
     }
 }
--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -8,55 +8,61 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private K key;
     private V value;
     @SuppressWarnings("unchecked")
-    private K[] originalKeys = (K[]) new Object[0];
+    private K[] originalKeys = (K[]) new Object[1];
     @SuppressWarnings("unchecked")
     private K[] newKeys = (K[]) new Object[0];
 
     @SuppressWarnings("unchecked")
-    private V originalValues[] = (V[]) new Object[0];
+    private V[] originalValues = (V[]) new Object[1];
 
     @SuppressWarnings("unchecked")
-    private V newValues[] = (V[]) new Object[0];
+    private V[] newValues = (V[]) new Object[0];
     private int count = 0;
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < originalKeys.length; i++) {
+        for (int i = 0; i < count; i++) {
             if (key == originalKeys[i]) {
-                newValues = (V[]) new Object[originalValues.length + 1];
-                for (int j = 0; j < originalValues.length; j++) {
-                    newValues[j] = originalValues[j];
-                }
-            } else {
-                newKeys = (K[]) new Object[originalKeys.length + 1];
-
-                for (int c = 0; c < originalKeys.length; c++) {
-                    newKeys[c] = originalKeys[c];
-                }
-                newKeys[originalKeys.length] = key;
-
-                newValues = (V[]) new Object[originalValues.length + 1];
-
-                for (int j = 0; j < originalValues.length; j++) {
-                    newValues[j] = originalValues[j];
-                }
-                newValues[originalValues.length] = value;
+                originalValues[i] = value;
+                break;
             }
         }
+
+        newKeys = (K[]) new Object[newKeys.length + 1];
+        for (int c = 0; c < originalKeys.length; c++) {
+            newKeys[c] = originalKeys[c];
+        }
+        newKeys[newKeys.length - 1] = key;
+
+        originalKeys = (K[]) new Object[newKeys.length];
+        for (int c = 0; c < originalKeys.length; c++) {
+            originalKeys[c] = newKeys[c];
+        }
+
+        newValues = (V[]) new Object[newValues.length + 1];
+
+        for (int j = 0; j < originalValues.length; j++) {
+            newValues[j] = originalValues[j];
+        }
+        newValues[newValues.length - 1] = value;
+
+        originalValues = (V[]) new Object[newValues.length];
+
+        for (int j = 0; j < originalValues.length; j++) {
+            originalValues[j] = newValues[j];
+        }
+        count++;
     }
 
     @Override
     public V get(K key) {
-        V value2 = null;
         for (int i = 0; i < newKeys.length; i++) {
-            if(Objects.equals(newKeys[i], key)) {
-                value2 = newValues[i];
-            } else {
-                value2 = null;
-                System.out.println("There is no such key");
+            if (Objects.equals(newKeys[i], key)) {
+                return newValues[i];
             }
         }
-        return value2;
+        System.out.println("There is no such key");
+        return null;
     }
 
     @Override
