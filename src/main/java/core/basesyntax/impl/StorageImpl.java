3dafc8a08a996f3b142e95207ae507442a5a81e3
clo3eX <pansydaisy83@gmail.com>
2024-09-19 14:00:22 +0200

    removed "Objects" and renamed currentSize to size

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,18 +1,16 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_ARRAY_SIZE = 10;
     private final K[] keys;
     private final V[] values;
-    private int currentSize;
+    private int size;
 
     public StorageImpl() {
         keys = (K[]) new Object[MAX_ARRAY_SIZE];
         values = (V[]) new Object[MAX_ARRAY_SIZE];
-        currentSize = 0;
     }
 
     @Override
@@ -20,10 +18,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         int index = findKeyIndex(key);
         if (index != -1) {
             values[index] = value;
-        } else if (currentSize < MAX_ARRAY_SIZE) {
-            keys[currentSize] = key;
-            values[currentSize] = value;
-            currentSize++;
+        } else if (size < MAX_ARRAY_SIZE) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
         } else {
             throw new RuntimeException("Storage is full");
         }
@@ -40,12 +38,12 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
-        return currentSize;
+        return size;
     }
 
     private int findKeyIndex(K key) {
-        for (int i = 0; i < currentSize; i++) {
-            if ((keys[i] == null && key == null) || Objects.equals(keys[i], key)) {
+        for (int i = 0; i < size; i++) {
+            if ((keys[i] == key) || (key != null && key.equals(keys[i]))) {
                 return i;
             }
         }
