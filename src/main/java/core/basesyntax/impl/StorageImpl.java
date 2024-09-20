1beb0efb757c371746afa72dc28b692e1dd205df
B4rt1omiej <bartlomiej.karol.gacek@gmail.com>
2024-09-20 10:16:49 +0200

    First solution of the task, with two arrays T nad V, that are implements and chacked by for loop.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,46 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_ITEMS_NUMBER];
+        values = (V[]) new Object[MAX_ITEMS_NUMBER];
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < MAX_ITEMS_NUMBER; i++) {
+            if (keys[i] == key || (keys[i] == null && values[i] == null)) {
+                keys[i] = key;
+                if (values[i] == null) {
+                    size++;
+                }
+                values[i] = value;
+                return;
+            }
+            if (keys[i] != null && keys[i].equals(key)) {
+                values[i] = value;
+                return;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < MAX_ITEMS_NUMBER; i++) {
+            if (keys[i] != null && keys[i].equals(key) || key == keys[i]) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
