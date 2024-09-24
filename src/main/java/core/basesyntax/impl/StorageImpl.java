8e1d8d006ad2da6fdeaebb39d579394b34b368f5
Ihor Tkachuk <iono19031@gmail.com>
2024-09-24 21:36:37 +0300

    corrected issues after AI review

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,61 +3,33 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private K key;
-    private V value;
+    private static final int MAXIMUM_NUMBER_ELEMENTS_IN_STORAGE = 10;
     private int index = 0;
-    private final StorageImpl<K, V>[] pairStorage = new StorageImpl[10];
-
-    public void setKey(K key) {
-        this.key = key;
-    }
-
-    public void setValue(V value) {
-        this.value = value;
-    }
-
-    public K getKey() {
-        return key;
-    }
-
-    public V getValue() {
-        return value;
-    }
+    private final Pair<K, V>[] pairStorage = new Pair[MAXIMUM_NUMBER_ELEMENTS_IN_STORAGE];
 
     @Override
     public void put(K key, V value) {
         boolean flag = true;
 
-        if (index == 0) {
-            pairStorage[0] = new StorageImpl<>();
-            pairStorage[0].setKey(key);
-            pairStorage[0].setValue(value);
-        }
+        for (int i = 1; i <= index; i++) {
 
-        if (index > 0) {
-            for (int i = 1; i <= index; i++) {
-                if (pairStorage[i - 1].getKey() == null && key == null
-                        || pairStorage[i - 1].getKey() != null
-                        && pairStorage[i - 1].getKey().equals(key)) {
-                    pairStorage[i - 1].setValue(value);
-                    flag = false;
-                    index--;
-                    break;
-                }
-            }
-
-            if (flag) {
-                pairStorage[index] = new StorageImpl<>();
-                pairStorage[index].setKey(key);
-                pairStorage[index].setValue(value);
+            if (index > 0 && pairStorage[i - 1].getKey() == null && key == null
+                    || pairStorage[i - 1].getKey() != null
+                    && pairStorage[i - 1].getKey().equals(key)) {
+                pairStorage[i - 1].setValue(value);
+                flag = false;
             }
         }
-        index++;
+
+        if (flag) {
+            pairStorage[index] = new Pair<>(key, value);
+            index++;
+        }
     }
 
     @Override
     public V get(K key) {
-        for (StorageImpl<K, V> storage: pairStorage) {
+        for (Pair<K, V> storage: pairStorage) {
             if (storage == null) {
                 return null;
             }
@@ -78,3 +50,4 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         return index;
     }
 }
+
