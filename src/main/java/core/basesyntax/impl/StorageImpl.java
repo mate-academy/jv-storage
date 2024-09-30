b26826cf68631e918e5fdca814367f4743c465bf
Roman <romanvoynahiy@gmail.com>
2024-09-30 17:58:47 +0300

    Implement key-value storage with put, get and size methods.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,44 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private K[] keysArray;
+    private V[] valuesArray;
+    private int valuesSize;
+
+    public StorageImpl() {
+        keysArray = (K[]) new Object[10];
+        valuesArray = (V[]) new Object[10];
+        valuesSize = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < valuesSize; i++) {
+            if (keysArray[i] == null ? key == null : keysArray[i].equals(key)) {
+                valuesArray[i] = value;
+                return;
+            }
+        }
+
+        if (valuesSize < 10) {
+            keysArray[valuesSize] = key;
+            valuesArray[valuesSize] = value;
+            valuesSize++;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < valuesSize; i++) {
+            if (keysArray[i] == null ? key == null : keysArray[i].equals(key)) {
+                return valuesArray[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return valuesSize;
     }
 }
