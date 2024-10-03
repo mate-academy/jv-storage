7b3d69b3279bd933f3f5c08bcc9454e387a45c3a
Loiev Roman <robolito0420@gmail.com>
2024-10-03 22:52:49 +0300

    Implemented StorageImpl. Also added model Item as class.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,54 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import core.basesyntax.models.Item;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private final Item<K, V>[] items = new Item[MAX_ITEMS_NUMBER];
+    private int cursor = 0;
+
     @Override
     public void put(K key, V value) {
+        try {
+            items[findIndexToPut(key)] = new Item<>(key, value);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("There is no space left"
+                    + " in the warehouse, and the item has not been added to storage");
+        }
+
     }
 
     @Override
     public V get(K key) {
+        for (Item<K, V> item : items) {
+            if (item != null && key == null && item.getKey() == null) {
+                return item.getValue();
+            }
+            if (item != null && item.getKey() != null && item.getKey().equals(key)) {
+                return item.getValue();
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return cursor;
+    }
+
+    private int findIndexToPut(K key) {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                return cursor++;
+            }
+            if (items[i] != null && items[i].getKey() == null && key == null) {
+                return i;
+            }
+            if (items[i] != null && items[i].getKey() != null && items[i].getKey().equals(key)) {
+                return i;
+            }
+        }
+        return cursor++;
     }
 }
