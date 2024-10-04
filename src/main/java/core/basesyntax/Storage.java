743d83f3e50ab74e61cb9633b23a72cc1064699c
Valeriy M <valeriym250@gmail.com>
2024-10-04 22:18:16 +0300

    fixed bugs

--- src/main/java/core/basesyntax/Storage.java
+++ src/main/java/core/basesyntax/Storage.java
@@ -1,6 +1,10 @@
 package core.basesyntax;
 
 public interface Storage<K, V> {
+    Object[] getKeyObjects();
+
+    Object[] getStorageObjects();
+
     void put(K key, V value);
 
     V get(K key);
--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,52 +4,40 @@ import core.basesyntax.Storage;
 import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    public static final int ARRAY_SIZE = 10;
+    private static final int ARRAY_SIZE = 10;
     private Object[] keyObjects = new Object[ARRAY_SIZE];
     private Object[] storageObjects = new Object[ARRAY_SIZE];
 
+    @Override
     public Object[] getKeyObjects() {
         return Arrays.copyOf(keyObjects, keyObjects.length);
     }
 
+    @Override
     public Object[] getStorageObjects() {
         return Arrays.copyOf(storageObjects, storageObjects.length);
     }
 
     @Override
     public void put(K key, V value) {
-        int resultOfNullFinder = 0;
-        int resultOfEqualFinder = 0;
-        resultOfNullFinder = findFirstNullIndex(key, keyObjects);
-        if (resultOfNullFinder >= 0) {
-            storageObjects[resultOfNullFinder] = value;
+        int resultOfIndexFinder = 0;
+        resultOfIndexFinder = findByIndex(key, keyObjects);
+
+        if (resultOfIndexFinder >= 0) {
+            storageObjects[resultOfIndexFinder] = value;
         } else {
-            resultOfEqualFinder = findIndexOfKey(key, keyObjects);
-            if (resultOfEqualFinder >= 0) {
-                storageObjects[resultOfEqualFinder] = value;
-            } else {
-                for (int i = 0; i < keyObjects.length; i++) {
-                    if (keyObjects[i] == null && storageObjects[i] == null) {
-                        keyObjects[i] = key;
-                        storageObjects[i] = value;
-                        break;
-                    }
-                }
-            }
+            resultOfIndexFinder = defineEmptyCell();
+            keyObjects[resultOfIndexFinder] = key;
+            storageObjects[resultOfIndexFinder] = value;
         }
+
     }
 
     @Override
     public V get(K key) {
-        int resultOfNullFinder = 0;
-        int resultOfEqualFinder = 0;
-        resultOfNullFinder = findFirstNullIndex(key, keyObjects);
-        if (resultOfNullFinder >= 0) {
-            return (V) (storageObjects[resultOfNullFinder]);
-        } else {
-            resultOfEqualFinder = findIndexOfKey(key, keyObjects);
-            return resultOfEqualFinder >= 0 ? (V) (storageObjects[resultOfEqualFinder]) : null;
-        }
+        int resultOfIndexFinder = 0;
+        resultOfIndexFinder = findByIndex(key, keyObjects);
+        return resultOfIndexFinder >= 0 ? (V) (storageObjects[resultOfIndexFinder]) : null;
     }
 
     @Override
@@ -64,7 +52,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
         return index;
     }
 
-    private int findFirstNullIndex(K key, Object[] objects) {
+    private int findByIndex(K key, Object[] objects) {
+        int index = 0;
         if (key == null) {
             for (int i = 0; i < objects.length; i++) {
                 if (objects[i] == null) {
@@ -72,12 +61,22 @@ public class StorageImpl<K, V> implements Storage<K, V> {
                 }
             }
         }
-        return -1;
+        index = findIndexOfKey(key, objects);
+        return index >= 0 ? index : -1;
     }
 
     private int findIndexOfKey(K key, Object [] objects) {
         for (int i = 0; i < objects.length; i++) {
             if (key.equals((objects[i]))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private int defineEmptyCell() {
+        for (int i = 0; i < keyObjects.length; i++) {
+            if (keyObjects[i] == null && storageObjects[i] == null) {
                 return i;
             }
         }
