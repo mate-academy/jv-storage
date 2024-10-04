8cdc10a89268eddb4298a6b54feb3812cf71f8b4
Olha Oleshchuk <olha.oleshchuk@gmail.com>
2024-10-04 23:29:30 +0300

    first try

---
+++ src/main/java/core/basesyntax/impl/Main.java
@@ -0,0 +1,19 @@
+package core.basesyntax.impl;
+
+import core.basesyntax.Storage;
+import core.basesyntax.StorageImpl;
+
+public class Main {
+    StorageImpl storageImpl = new StorageImpl();
+
+    public static void main (String[] args) {
+        public Storage (K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+
+        for (int i = 0; i <10; i++) {
+        }
+    }
+}
