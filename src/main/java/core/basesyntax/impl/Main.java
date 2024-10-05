d7ae5b192be195be0482bddfdd91bc1b04dfb5cb
Olha Oleshchuk <olha.oleshchuk@gmail.com>
2024-10-05 23:14:50 +0300

    second try

--- src/main/java/core/basesyntax/impl/Main.java
+++ src/main/java/core/basesyntax/impl/Main.java
@@ -1,19 +1,11 @@
 package core.basesyntax.impl;
 
-import core.basesyntax.Storage;
-import core.basesyntax.StorageImpl;
-
 public class Main {
-    StorageImpl storageImpl = new StorageImpl();
+    public static void main(String[] args) {
+        StorageImpl<String, Integer> storageImpl = new StorageImpl<>();
 
-    public static void main (String[] args) {
-        public Storage (K key, V value) {
-            this.key = key;
-            this.value = value;
-        }
-
-
-        for (int i = 0; i <10; i++) {
+        for (int i = 0; i < 10; i++) {
+            storageImpl.put("key" + i, i);
         }
     }
 }
--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,22 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private K key;
+    private V value;
+
     @Override
     public void put(K key, V value) {
+        this.key = key;
+        this.value = value;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return value;
     }
 
     @Override
     public int size() {
-        return -1;
+        return (Integer) key;
     }
 }
