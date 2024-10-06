40f138c36b477891f23c32a9cdc45bd3c36db77f
Olha Oleshchuk <olha.oleshchuk@gmail.com>
2024-10-06 15:59:26 +0300

    third try

--- src/main/java/core/basesyntax/impl/Main.java
+++ src/main/java/core/basesyntax/impl/Main.java
@@ -3,9 +3,5 @@ package core.basesyntax.impl;
 public class Main {
     public static void main(String[] args) {
         StorageImpl<String, Integer> storageImpl = new StorageImpl<>();
-
-        for (int i = 0; i < 10; i++) {
-            storageImpl.put("key" + i, i);
-        }
     }
 }
--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,22 +3,46 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private K key;
-    private V value;
+    private K[] keys;
+    private V[] values;
+    private int count;
+
+    @SuppressWarnings("unchecked")
+    public StorageImpl() {
+        keys = (K[]) new Object[10];
+        values = (V[]) new Object[10];
+        count = 0;
+    }
 
     @Override
     public void put(K key, V value) {
-        this.key = key;
-        this.value = value;
+        for (int i = 0; i < count; i++) {
+            if (keys[i] == null && key == null || keys[i] != null && keys[i].equals(key)) {
+                values[i] = value;
+                return;
+            }
+        }
+        if (count < 10) {
+            keys[count] = key;
+            values[count] = value;
+            count++;
+        } else {
+            System.out.println("Storage is full");
+        }
     }
 
     @Override
     public V get(K key) {
-        return value;
+        for (int i = 0; i < count; i++) {
+            if (keys[i] == null && key == null || keys[i] != null && keys[i].equals(key)) {
+                return values[i];
+            }
+        }
+        return null;
     }
 
     @Override
     public int size() {
-        return (Integer) key;
+        return count;
     }
 }
