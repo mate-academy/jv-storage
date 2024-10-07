3c260dddea6efa2491f8d33c3f1c906319f06dd6
ttikr <ia03.danchuk.volodymyr@gmail.com>
2024-10-07 19:40:38 +0300

    done

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,50 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private final Object[] keys;
+    private final Object[] values;
+    private int cursor = 0;
+
+    public StorageImpl() {
+        this.keys = new Object[MAX_ITEMS_NUMBER];
+        this.values = new Object[MAX_ITEMS_NUMBER];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (get(key) == null) {
+            keys[cursor] = key;
+            values[cursor] = value;
+            cursor++;
+        } else {
+            int index = findKeyIndex(key);
+            values[index] = value;
+        }
     }
 
     @Override
     public V get(K key) {
+        int index = findKeyIndex(key);
+        if (index != -1) {
+            return (V) values[index];
+        }
         return null;
     }
 
     @Override
     public int size() {
+        return cursor;
+    }
+
+    private int findKeyIndex(K key) {
+        int index = 0;
+        while (index < keys.length) {
+            if (key == keys[index] || (key != null && key.equals(keys[index]))) {
+                return index;
+            }
+            index++;
+        }
         return -1;
     }
 }
