6b4ea6e5167101c14fb3e3f79384810c13856a2f
opdhk <ahhsigjl@gmail.com>
2024-10-14 00:32:29 +0300

    changed: * increaseStorageSize() added to prevent ArrayIndexOutOfBoundsException in case if a Pair is put in full Storage; * separate methods areBothKeysNull(...) and areKeysEqual(...) created for checking keys to avoid code duplication

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,6 +1,7 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int STORAGE_DEFAULT_CAPACITY = 10;
@@ -14,39 +15,49 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (items[i].getKey() == null && key == null) {
+            if (areBothKeysNull(items[i].getKey(), key)) {
                 items[i].setValue(value);
                 return;
             }
-            if (items[i].getKey() != null && items[i].getKey().equals(key)) {
+            if (areKeysEqual(key, items[i].getKey())) {
                 items[i] = new Pair<>(key, value);
                 return;
             }
         }
-        if (size < STORAGE_DEFAULT_CAPACITY - 1) {
-            items[size] = new Pair<>(key, value);
-            size++;
+        if (size == STORAGE_DEFAULT_CAPACITY - 1) {
+            increaseStorageSize();
         }
+        items[size] = new Pair<>(key, value);
+        size++;
+    }
+
+    private boolean areBothKeysNull(K firstKey, K secondKey) {
+        return secondKey == null && firstKey == null;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (key == null && items[i].getKey() == null) {
+            if (areKeysEqual(key, items[i].getKey())) {
                 return items[i].getValue();
             }
-            if (items[i].getKey() == null && key != null) {
-                continue;
-            }
-            if (items[i].getKey().equals(key)) {
+            if (items[i].getKey() == null && key == null) {
                 return items[i].getValue();
             }
         }
         return null;
     }
 
+    private boolean areKeysEqual(K firstKey, K secondKey) {
+        return secondKey != null && secondKey.equals(firstKey);
+    }
+
     @Override
     public int size() {
         return size;
+    }
+
+    private void increaseStorageSize() {
+        items = Arrays.copyOf(items, items.length * 2);
     }
 }
