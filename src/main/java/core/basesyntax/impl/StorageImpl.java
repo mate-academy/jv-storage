a5579e973a7e82892243dbb894156ef6fb209528
Anton <windsweptofficial@gmail.com>
2024-10-16 19:02:37 +0200

    solved task

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,18 +2,31 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.*;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private HashMap<K, V> kvMap = new HashMap<>();
+
     @Override
     public void put(K key, V value) {
+        kvMap.put(key, value);
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return kvMap.get(key);
     }
 
     @Override
     public int size() {
-        return -1;
+        return kvMap.size();
+    }
+
+    public Map<K, V> getKvMap() {
+        return kvMap;
+    }
+
+    public void setKvMap(HashMap<K, V> kvMap) {
+        this.kvMap = kvMap;
     }
 }
