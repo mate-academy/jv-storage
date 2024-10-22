76341f4da04e4e52fb7a416e1bdee32c21c4ce69
karma.misha <karma.misha@gmail.com>
2024-10-22 22:28:44 +0300

    implements solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,55 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEM_NUMBER = 10;
+    private Object[] keysRepository;
+    private Object[] valuesRepository;
+    private int count = 0;
+
+    public StorageImpl() {
+        keysRepository = new Object[MAX_ITEM_NUMBER];
+        valuesRepository = new Object[MAX_ITEM_NUMBER];
+    }
+
     @Override
     public void put(K key, V value) {
+       if (size() == MAX_ITEM_NUMBER) {
+           return;
+       }
+
+       for (int i = 0; i < size(); i++) {
+           K currentKey = (K) keysRepository[i];
+
+           if (key == currentKey || (key != null && key.equals(currentKey))) {
+               valuesRepository[i] = value;
+               return;
+           }
+       }
+
+       int index = size();
+
+       keysRepository[index] = key;
+       valuesRepository[index] = value;
+
+       count++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size(); i++) {
+            K currentKey = (K) keysRepository[i];
+
+            if (key == currentKey || (key != null && key.equals(currentKey))) {
+                V value = (V) valuesRepository[i];
+                return value;
+            }
+        };
+
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return count;
     }
 }
