0f30fd443f0069c092d5eae04ffe9b4d27061e2b
Vadym Dmytriv <vadymdmytriv@gmail.com>
2024-10-24 17:15:54 +0200

    Implements put, get, size methods in StorageImpl class

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,47 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int CAPACITY = 10;
+    private int index = 0;
+    private K[] keys;
+    private V[] values;
+
+    {
+        keys = (K[]) new Object[CAPACITY];
+        values = (V[]) new Object[CAPACITY];
+    }
+
     @Override
     public void put(K key, V value) {
+        int numberOfKey = findKey(key);
+        if (numberOfKey < 0) {
+            keys[index] = key;
+            values[index] = value;
+            index++;
+        } else {
+            values[numberOfKey] = value;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int numberOfKey = findKey(key);
+        return (numberOfKey >= 0) ? values[numberOfKey] : null;
     }
 
     @Override
     public int size() {
+        return index;
+    }
+
+    private int findKey(K key) {
+        for (int i = 0; i < index; i++) {
+            if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
+                return i;
+            }
+        }
         return -1;
     }
 }
+
+
