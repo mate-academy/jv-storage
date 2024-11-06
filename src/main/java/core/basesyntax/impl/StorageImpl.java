d66d18b41723ae73ed7ea0efb5c168c1312bdecf
Vasyl <vasiliy.azarov.vl1@gmail.com>
2024-11-06 11:38:20 +0100

    implemented base task logic

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,8 +3,28 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int STORAGE_MAX_SIZE = 10;
+    private static int size = 0;
+    private static int cursor = 0;
+    private K[] keyArr;
+    private V[] valueArr;
+
+    @SuppressWarnings("unchecked")
+    public StorageImpl() {
+        keyArr = (K[]) new Object[STORAGE_MAX_SIZE];
+        valueArr = (V[]) new Object[STORAGE_MAX_SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        if (getKeyPlaceIfContains(key) != -1) {
+            putDirectly(key, value, getKeyPlaceIfContains(key));
+        } else if (isFull()) {
+            putDirectly(key, value, cursor);
+            updateCursor();
+        } else {
+            putDirectly(key, value, size());
+        }
     }
 
     @Override
@@ -14,6 +34,33 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public int size() {
+        return size;
+    }
+
+    private int getKeyPlaceIfContains(K key) {
+        for (int i = 0; i < size(); i++) {
+            if (keyArr[i] == key) {
+                return i;
+            }
+        }
         return -1;
+    }
+
+    private void putDirectly(K key, V value, int place) {
+        keyArr[place] = key;
+        valueArr[place] = value;
+        size++;
+    }
+
+    private boolean isFull() {
+        return size() == STORAGE_MAX_SIZE;
+    }
+
+    private void updateCursor() {
+        if (cursor == STORAGE_MAX_SIZE) {
+            cursor = 0;
+        } else {
+            cursor++;
+        }
     }
 }
