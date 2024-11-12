47f65bdeef848f5795a6cf04e76009185860c805
Alinka Lyn <alinka.lyn@ukr.net>
2024-11-12 20:51:08 +0200

    fixes

--- src/main/java/core/basesyntax/StorageImpl.java
+++ src/main/java/core/basesyntax/StorageImpl.java
@@ -14,10 +14,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] == null && key == null) {
-                values[i] = value;
-                return;
-            } else if (keys[i] != null && keys[i].equals(key)) {
+            if ((keys[i] == null && key == null) || (keys[i] != null && keys[i].equals(key))) {
                 values[i] = value;
                 return;
             }
@@ -34,9 +31,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] == null && key == null) {
-                return values[i];
-            } else if (keys[i] != null && keys[i].equals(key)) {
+            if ((keys[i] == null && key == null) || (keys[i] != null && keys[i].equals(key))) {
                 return values[i];
             }
         }
