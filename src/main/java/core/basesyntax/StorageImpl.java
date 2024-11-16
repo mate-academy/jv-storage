e48eb5703475ffc1bbc5ac71a6e1834fe704da1f
Alinka Lyn <alinka.lyn@ukr.net>
2024-11-16 15:14:13 +0200

    fixes

--- src/main/java/core/basesyntax/StorageImpl.java
+++ src/main/java/core/basesyntax/StorageImpl.java
@@ -1,39 +1,50 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int INITIAL_CAPACITY = 10;
+    private static final int MULTIPLIER = 2;
     private K[] keys;
     private V[] values;
     private int size;
 
-    @SuppressWarnings("unchecked")
     public StorageImpl() {
-        this.keys = (K[]) new Object[INITIAL_CAPACITY];
-        this.values = (V[]) new Object[INITIAL_CAPACITY];
-        size = 0;
+        keys = createKeyArray();
+        values = createValueArray();
+    }
+
+    @SuppressWarnings("unchecked")
+    private K[] createKeyArray() {
+        return (K[]) new Object[StorageImpl.INITIAL_CAPACITY];
+    }
+
+    @SuppressWarnings("unchecked")
+    private V[] createValueArray() {
+        return (V[]) new Object[StorageImpl.INITIAL_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if ((keys[i] == null && key == null) || (keys[i] != null && keys[i].equals(key))) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
                 values[i] = value;
                 return;
             }
         }
-        if (size < INITIAL_CAPACITY) {
-            keys[size] = key;
-            values[size] = value;
-            size++;
-        } else {
-            throw new RuntimeException("Storage is completely full");
+        keys[size] = key;
+        values[size] = value;
+        size++;
+        if (size == keys.length) {
+            keys = Arrays.copyOf(keys, size * MULTIPLIER);
+            values = Arrays.copyOf(values, size * MULTIPLIER);
         }
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if ((keys[i] == null && key == null) || (keys[i] != null && keys[i].equals(key))) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
                 return values[i];
             }
         }
