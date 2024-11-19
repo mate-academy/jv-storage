9e221e24db9d04fafe5b5535eed1f69b93b89260
Sasha Koval <kovalsana40@gmail.com>
2024-11-19 18:13:03 +0200

    i want to give some explain

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,18 +2,37 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.security.Key;
+import java.util.ArrayList;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
+    ArrayList<K> keys = new ArrayList<>();
+    ArrayList<V> values = new ArrayList<>();
+    private int count = 0;
     @Override
     public void put(K key, V value) {
+        if (key == null) {
+            values.add(count,value);
+        }
+        keys.add(count, key);
+        values.add(count,value);
+        count++;
+
+
     }
 
     @Override
     public V get(K key) {
-        return null;
+        try {
+            return values.get(keys.indexOf(key));
+        } catch (IndexOutOfBoundsException e) {
+            return null;
+        }
+
     }
 
     @Override
     public int size() {
-        return -1;
+        return values.size();
     }
 }
