422a7404f916958610f59d6a6b0b9a80b09a7eec
YevhenSrdk <eugeneserdyuk23@gmail.com>
2024-12-03 23:29:46 +0200

    Implemented the Storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,51 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ARRAY_SIZE = 10;
+    private K[] keys;
+    private V[] values;
+    private int size = 0;
+
+    public StorageImpl() {
+        this.keys = (K[]) new Object[MAX_ARRAY_SIZE];
+        this.values = (V[]) new Object[MAX_ARRAY_SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = indexOf(key);
+        if (index == -1) {
+            keys[size] = key;
+            values[size] = value;
+            size += 1;
+        } else {
+            if (size >= MAX_ARRAY_SIZE) {
+                throw new RuntimeException("Storage is full");
+            }
+            values[index] = value;
+        }
     }
 
     @Override
     public V get(K key) {
+        int index = indexOf(key);
+        if (index != -1) {
+            return (V) values[index];
+        }
         return null;
     }
 
     @Override
     public int size() {
+        return size;
+    }
+
+    private int indexOf(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == keys[i] || (key != null && key.equals(keys[i]))) {
+                return i;
+            }
+        }
         return -1;
     }
 }
