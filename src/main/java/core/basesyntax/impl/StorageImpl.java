74858f964e273f65b9edb8bf25cb43bbeb01e668
Teslenko Daniil <burgalhashem@gmail.com>
2024-12-03 15:48:57 +0200

    dev

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,36 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.ArrayList;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    ArrayList<K> keys = new ArrayList<K>();
+    ArrayList<V> data = new ArrayList<V>();
+
     @Override
     public void put(K key, V value) {
+        for(int i = 0; i < keys.size(); i++) {
+            if(keys.get(i).equals(key)) {
+                data.set(i, value);
+                return;
+            }
+        }
+        keys.add(key);
+        data.add(value);
     }
 
     @Override
     public V get(K key) {
+        for(int i = 0; i < keys.size(); i++) {
+            if(keys.get(i).equals(key)) {
+                return data.get(i);
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return keys.size();
     }
 }
