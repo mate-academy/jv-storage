ee7db08af72908369d45558ad00624559b26e519
Bohdan_SSX <mr.dimov77@gmail.com>
2024-12-10 23:03:56 +0200

    Refactored with Node implementation

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,73 +1,57 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Map;
 import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
 
-    private K[] keys;
-    private V[] values;
-    private int size;
+    private Node<K, V>[] nodes;
+    private int size = 0;
     private int capacity = 10;
-    private int index = 0;
-    private boolean nullKeyExists = false;
+    private double capacityIncrement = 1.5;
 
     public StorageImpl() {
-        this.size = 0;
-        this.keys = (K[]) new Object[capacity];
-        this.values = (V[]) new Object[capacity];
+        nodes = new Node[capacity];
     }
 
-    public boolean containsKey(K addingKey) {
-        for (K key : keys) {
-            if (Objects.equals(key, addingKey)) {
-                return true;
+    public int containsKey(K addingKey) {
+        for (int i = 0; i < this.size; i++) {
+            if (Objects.equals(nodes[i].getKey(), addingKey)) {
+                return i;
             }
         }
-        return false;
+        return -1;
     }
 
     private <T> T[] resizeArray(T[] array) {
-        T[] newArray = (T[]) new Object[capacity];
+        capacity = (int) (capacity * capacityIncrement);
+        T[] newArray = (T[]) new Node[(int) (capacity)];
         System.arraycopy(array, 0, newArray, 0, size);
         return newArray;
     }
 
-    public int indexOfKey(K key) {
-        for (int i = 0; i < size; i++) {
-            if (Objects.equals(key, keys[i])) {
-                return i;
-            }
-        }
-        return size;
-    }
-
     @Override
     public void put(K key, V value) {
-        if (containsKey(key)) {
-            index = indexOfKey(key);
-            values[index] = value;
-            if (key == null && nullKeyExists == false) {
-                size++;
-                nullKeyExists = true;
-            }
+        Node<K, V> node = new Node<>(key, value);
+        int index = containsKey(key);
+        if (index != -1) {
+            nodes[index].setValue(value);
         } else {
             if (size == capacity) {
-                capacity = capacity * 2;
-                keys = resizeArray(keys);
-                values = resizeArray(values);
+                nodes = resizeArray(nodes);
             }
-            keys[size] = key;
-            values[size] = value;
+            nodes[size] = node;
             size++;
         }
     }
 
     @Override
     public V get(K key) {
-        if (containsKey(key)) {
-            index = indexOfKey(key);
-            return values[index];
+        for (int i = 0; i < this.size; i++) {
+            if (Objects.equals(nodes[i].getKey(), key)) {
+                return nodes[i].getValue();
+            }
         }
         return null;
     }
@@ -75,5 +59,45 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public int size() {
         return this.size;
+    }
+
+    static class Node<K,V> implements Map.Entry<K,V> {
+        private final K key;
+        private V value;
+
+        Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public final K getKey() {
+            return key;
+        }
+
+        public final V getValue() {
+            return value;
+        }
+
+        public final String toString() {
+            return key + "=" + value;
+        }
+
+        public final int hashCode() {
+            return Objects.hashCode(key) ^ Objects.hashCode(value);
+        }
+
+        public final V setValue(V newValue) {
+            V oldValue = value;
+            value = newValue;
+            return oldValue;
+        }
+
+        public final boolean equals(Object o) {
+            if (o == this) {
+                return true;
+            }
+            return o instanceof Map.Entry<?, ?> e
+                    && Objects.equals(key, e.getKey());
+        }
     }
 }
