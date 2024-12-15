39e1a6408420ecce7bd7b07d994a2812436ad400
eulu <anegamega@gmail.com>
2024-12-15 19:04:29 +0200

    Remove Objects class and List usage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,43 +1,47 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private final List<K> keys;
-    private final List<V> values;
+    public static final int MAX_STORAGE_SIZE = 10;
+    private final Object[] keys;
+    private final Object[] values;
+    private int size;
 
     public StorageImpl() {
-        this.keys = new ArrayList<>();
-        this.values = new ArrayList<>();
+        this.keys = new Object[MAX_STORAGE_SIZE];
+        this.values = new Object[MAX_STORAGE_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < keys.size(); i++) {
-            if (Objects.equals(keys.get(i), key)) {
-                values.set(i, value);
+        if (size > MAX_STORAGE_SIZE) {
+            throw new RuntimeException("The storage is full");
+        }
+        for (int i = 0; i < this.size; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
+                values[i] = value;
                 return;
             }
         }
-
-        keys.add(key);
-        values.add(value);
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public V get(K key) {
-        int keyPosition = this.keys.indexOf(key);
-        if (keyPosition == -1) {
-            return null;
+        for (int i = 0; i < this.size; i++) {
+            if (key == keys[i] || key != null && key.equals(keys[i])) {
+                return (V) values[i];
+            }
         }
-        return this.values.get(keyPosition);
+        return null;
     }
 
     @Override
     public int size() {
-        return this.keys.size();
+        return this.size;
     }
 }
