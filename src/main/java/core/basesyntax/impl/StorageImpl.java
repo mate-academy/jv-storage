db10f99bf6d73b8bbdcad8a205450b6001303932
Piotr Hic <piotrhic@gmail.com>
2024-12-30 18:32:55 +0100

    v1.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,36 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private static final int MAX_ITEMS_NUMBER = 10;
+    V [] array = (V[]) new Object[MAX_ITEMS_NUMBER];
+
+    public StorageImpl() {
+    }
+
     @Override
     public void put(K key, V value) {
+        Integer key1 = (Integer) key;
+        if(key1 < 1 || key1 > 10){
+            throw new IllegalArgumentException("Number is out of range! - Try something between 1 and 10");
+        }
+        array[key1] = value;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        Integer key1 = (Integer) key;
+        if(key1 < 1 || key1 > 10){
+            throw new IllegalArgumentException("Number is out of range! - Try something between 1 and 10");
+        }
+        return array[key1];
     }
 
     @Override
     public int size() {
-        return -1;
+        if(array.length == 0){
+            return -1;
+        }
+        return array.length;
     }
 }
