c695f395e08ae6b98e5b134c2bd1180bbf7a9360
Mykola <mykola.turak.ki.2023@lpnu.ua>
2025-01-06 01:44:12 +0200

    make storage generic class

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,18 +2,40 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.security.KeyStore;
+import java.util.ArrayList;
+import java.util.List;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private List<K> keyStorage = new ArrayList<>();
+    private List<V> valueStorage = new ArrayList<>();
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < keyStorage.size(); i++) {
+            if ((key == null && keyStorage.get(i) == null) ||
+                    (key != null && key.equals(keyStorage.get(i)))) {
+                valueStorage.set(i, value);
+                return;
+            }
+        }
+        keyStorage.add(key);
+        valueStorage.add(value);
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keyStorage.size(); i++) {
+            if ((key == null && keyStorage.get(i) == null) ||
+                    (key != null && key.equals(keyStorage.get(i)))) {
+                return valueStorage.get(i);
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return keyStorage.size();
     }
 }
