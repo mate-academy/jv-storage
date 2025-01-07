abe5caf9e8f783d096af42d53af474134cc4cd30
Your_Name Your_Surname <your@email>
2025-01-07 12:16:56 +0200

    Implemented the Storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,48 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_ITEMS_NUMBER = 10;
+    private K[] dataOfKeys;
+    private V[] dataOfValues;
+    private int counter;
+
+    @SuppressWarnings("unchecked")
+    public StorageImpl() {
+        dataOfKeys = (K[]) new Object[MAX_ITEMS_NUMBER];
+        dataOfValues = (V[]) new Object[MAX_ITEMS_NUMBER];
+        counter = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < counter; i++) {
+            if (key == dataOfKeys[i]) {
+                dataOfValues[i] = value;
+                return;
+            } else if (key != null && key.equals(dataOfKeys[i])) {
+                dataOfValues[i] = value;
+                return;
+            }
+        }
+        dataOfKeys[counter] = key;
+        dataOfValues[counter] = value;
+        counter++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < counter; i++) {
+            if (key == dataOfKeys[i]) {
+                return dataOfValues[i];
+            } else if (key != null && key.equals(dataOfKeys[i])) {
+                return dataOfValues[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return counter;
     }
 }
