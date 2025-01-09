9b63ee27684a6a9ccdda92082d8db7710cdce191
Wojtek <wojka66@gmail.com>
2025-01-09 17:04:13 +0100

    Removing Objects class.

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,7 +1,6 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_LENGTH = 10;
@@ -19,7 +18,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = MIN_SIZE; i < size; i++) {
-            if (Objects.equals(key, this.key[i])) {
+            if ((key != null && key.equals(this.key[i])) || key == this.key[i]) {
                 this.value[i] = value;
                 return;
             }
@@ -34,7 +33,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = MIN_SIZE; i < size; i++) {
-            if (Objects.equals(key, this.key[i])) {
+            if ((key != null && key.equals(this.key[i])) || key == this.key[i]) {
                 return this.value[i];
             }
         }
