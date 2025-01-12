6bd793b8d041734ec7504dfc2fcbc067d0b32daf
Piotr Hic <piotrhic@gmail.com>
2025-01-12 13:46:16 +0100

    fourth try

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,7 +1,6 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,12 +8,14 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     private static final int MAX_ITEMS_NUMBER = 10;
     private Map<K, V> map;
+
     public StorageImpl() {
-        map = new HashMap<> ();
+        map = new HashMap<>();
     }
+
     @Override
     public void put(K key, V value) {
-        if(map.size() <= 10 || map.containsKey(key)){
+        if(map.size() <= 10 || map.containsKey(key)) {
             map.put(key,value);
             return;
         }
@@ -23,7 +24,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        if(map.containsKey(key)){
+        if (map.containsKey(key)) {
             return map.get(key);
         }
         return null;
