047380b1a35af7abc877286905e6c42c57086346
xMaXD <max260505.mx22@gmail.com>
2025-01-19 22:09:39 +0100

    done

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,57 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int defaultCapacity = 10;
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[defaultCapacity];
+        values = (V[]) new Object[defaultCapacity];
+        size = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
+                values[i] = value;
+                return;
+            }
+        }
+        if (size == keys.length) {
+            expandCapacity();
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if ((key == null && keys[i] == null) || (key != null && key.equals(keys[i]))) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    private void expandCapacity() {
+        int newCapacity = defaultCapacity * 2;
+        K[] newKeys = (K[]) new Object[newCapacity];
+        V[] newValues = (V[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newKeys[i] = keys[i];
+            newValues[i] = values[i];
+        }
+        keys = newKeys;
+        values = newValues;
     }
 }
