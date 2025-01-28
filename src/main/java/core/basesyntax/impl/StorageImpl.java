f49962644d15169e7b74595457ad0d1b754f4c00
Petro <petroipatiy@gmail.com>
2025-01-28 00:25:17 +0000

    storage first update

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,92 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    public static final int MAX_VALUE = 10;
+    private K[] keys;
+    private V[] values;
+    private int size;
+    private boolean nullKeyPresent;
+
+    @SuppressWarnings("unchecked")
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_VALUE];
+        values = (V[]) new Object[MAX_VALUE];
+        size = 0;
+        nullKeyPresent = false;
+    }
+
     @Override
     public void put(K key, V value) {
+        if (key == null) {
+            nullKeyPresent = true; // Установлюємо прапорець
+            for (int i = 0; i < size; i++) {
+                if (keys[i] == null) {
+                    values[i] = value; // Заміна значення для ключа null
+                    return;
+                }
+            }
+            if (size < MAX_VALUE) {
+                keys[size] = null;
+                values[size] = value;
+                size++;
+            } else {
+                throw new RuntimeException("Storage is full");
+            }
+            return;
+        }
+        for (int i = 0;i < size;i++) {
+            if (keys[i] != null && keys[i].equals(key)) {
+                values[i] = value;
+                return;
+            }
+        }
+        if (size < MAX_VALUE) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        } else {
+            throw new RuntimeException("Storage is full!");
+        }
     }
 
     @Override
     public V get(K key) {
+        if (key == null) {
+            for (int i = 0; i < size; i++) {
+                if (keys[i] == null) {
+                    return values[i];
+                }
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            if (keys[i] != null && keys[i].equals(key)) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        StorageImpl<K, V> storage = (StorageImpl<K, V>) obj;
+        return Objects.deepEquals(keys, storage.keys)
+                && Objects.deepEquals(values, storage.values);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Arrays.hashCode(keys), Arrays.hashCode(values));
     }
 }
