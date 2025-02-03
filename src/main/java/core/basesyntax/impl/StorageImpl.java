116e8f39e102155551e0e1e60e0e5cfd55be7821
Nikita Chaikovskyi <cha9n.1488@gmail.com>
2025-02-03 16:05:35 +0200

    Done2

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,14 +4,9 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
-    private K[] keys;
-    private V[] values;
+    private K[] keys = (K[]) new Object[MAX_SIZE];
+    private V[] values = (V[]) new Object[MAX_SIZE];
     private int size = 0;
-
-    public StorageImpl() {
-        keys = (K[]) new Object[MAX_SIZE];
-        values = (V[]) new Object[MAX_SIZE];
-    }
 
     @Override
     public void put(K key, V value) {
