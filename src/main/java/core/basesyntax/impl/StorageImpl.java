c525ce3c8dbb48ffce9f6c83aabe06c03255910d
Hanna Voloshko <aa.voloshko09@gmail.com>
2025-02-09 23:35:36 +0200

    implemented method get

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -5,15 +5,27 @@ import core.basesyntax.Storage;
 public class StorageImpl<K, V> implements Storage<K, V> {
     private K key;
     private V value;
+    private static int index = 0;
+    private K[] keyValues = new K[10];
+    private V[] valueValues = new V[10];
     @Override
     public void put(K key, V value) {
         this.key = key;
         this.value = value;
+        keyValues[index] = key;
+        valueValues[index] = value;
+        index++;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int keyIndex = 0;
+        for (int i = 0; i < keyValues.length; i++) {
+            if (keyValues[i].equals(key)) {
+                keyIndex = i;
+            }
+        }
+        return valueValues[keyIndex];
     }
 
     @Override
