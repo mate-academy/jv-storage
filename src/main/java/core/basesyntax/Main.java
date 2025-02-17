2522a43455cf0d2a025050d717e2f8e6ec34da5a
Mateusz <mateusz.kiryk@o2.pl>
2025-02-17 22:06:59 +0100

    First solution

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,21 @@
+package core.basesyntax;
+
+import core.basesyntax.Storage;
+
+public class Main {
+    public static void main(String[] args) {
+        Storage<Integer, Box> storage = new StorageImpl<>();
+
+        Box box = new Box();
+
+        storage.put(22, box);
+
+        Box value = storage.get(22);
+        System.out.println(value);
+
+        int size = storage.size();
+        System.out.println("Size: " + size);
+    }
+}
+class Box {
+}
--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,49 @@
-package core.basesyntax.impl;
+package core.basesyntax;
 
-import core.basesyntax.Storage;
+import java.util.Arrays;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE = 10;
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_SIZE];
+        values = (V[]) new Object[MAX_SIZE];
+        size = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i].equals(key)) {
+                values[i] = value;
+                return;
+            }
+        }
+
+        if (size < MAX_SIZE) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        } else {
+            throw new IllegalStateException("Storage is full");
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i].equals(key)) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
