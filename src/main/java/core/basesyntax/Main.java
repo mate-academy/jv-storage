62a3a9ed6d052e912ef27cbb8c4f40a11b4019bc
Mateusz <mateusz.kiryk@o2.pl>
2025-02-17 22:14:19 +0100

    Third solution

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,21 +1,21 @@
-package core.basesyntax;
+import core.basesyntax.Box;
+import core.basesyntax.Storage;
+import core.basesyntax.StorageImpl;
 
 public class Main {
 
     public static void main(String[] args) {
         Storage<Integer, Box> storage = new StorageImpl<>();
 
-        Box box = new Box();
+        Box box = new Box("Apple");
+        storage.put(1, box);
+        Box box2 = new Box("Banana");
+        storage.put(2, box2);
 
-        storage.put(22, box);
-
-        Box value = storage.get(22);
-        System.out.println(value);
+        Box value = storage.get(1); // Zwróci "Apple"
+        System.out.println(value.getContent());
 
         int size = storage.size();
-        System.out.println("Size: " + size);
+        System.out.println("Size of storage: " + size);
     }
-}
-
-class Box {
 }
