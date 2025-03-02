50a7e2d9a6dc214c1e6251aed11b6837cd2f15e5
DmitryShmerchuk <Dmitryvin1992@gmail.com>
2025-03-02 23:03:38 +0200

    Ready-2

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -15,19 +15,18 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     }
 
     @Override
-        public void put(K key, V value) {
-            for (int i = 0; i < size; i++) {
-                if ((keys[i] == null && key == null)
-                        || (keys[i] != null && keys[i].equals(key))) {
-                    values[i] = value;
-                    return;
-                }
+    public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if ((keys[i] == null && key == null)
+                    || (keys[i] != null && keys[i].equals(key))) {
+                values[i] = value;
+                return;
             }
+        }
         keys[size] = key;
         values[size] = value;
         size++;
-        }
-
+    }
 
     @Override
     public V get(K key) {
