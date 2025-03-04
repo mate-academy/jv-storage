5d7a41f4daf0078920513d7d1ec0834a30c39fee
Vladyslav Bedryk <vladikbedrikl@gmail.com>
2025-03-04 18:44:53 +0100

    Implemented the Storage of the key-value type that is parametrized with two types of data

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,11 +1,12 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
-public class StorageImpl<K, V > implements Storage<K, V> {
+public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
-    private K[] keys;
-    private V[] values;
+    private final K[] keys;
+    private final V[] values;
     private int size = 0;
 
     public StorageImpl() {
@@ -13,43 +14,42 @@ public class StorageImpl<K, V > implements Storage<K, V> {
         values = (V[]) new Object[MAX_SIZE];
     }
 
-
     @Override
     public void put(K key, V value) {
-        int index = 0;
-        for (K currentKey : keys){
-            if (currentKey != null && currentKey.equals(key)) {
-                values[index] = value;
-            }
-
-            index++;
-
-            }
-         if (size() < MAX_SIZE){
-            keys[size()] = key;
-            values[size()] = value;
-            size++;
+        int index = indexOf(key);
+        if (index != -1) {
+            values[index] = value;
         } else {
-
+            if (size < MAX_SIZE) {
+                keys[size] = key;
+                values[size] = value;
+                size++;
+            }
         }
+
     }
 
     @Override
     public V get(K key) {
-        int index = 0;
-        for (K currentKey : keys){
-            if (currentKey != null && currentKey.equals(key)) {
-                return values[index];
-            }else if (currentKey == null && size != 0){
-                return values[index];
-            }
-            index++;
+        int index = indexOf(key);
+        if (index != -1) {
+            return values[index];
+        } else {
+            return null;
         }
-        return null;
     }
 
     @Override
     public int size() {
         return size;
+    }
+
+    public int indexOf(K key) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(keys[i], key)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
