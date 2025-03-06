1c95fa99e3acbdb038a2f2d297e8d2bc8d14799a
Povch Anastasia <povkhnastiya@gmail.com>
2025-03-06 12:19:56 +0200

    cat relax3

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,6 +2,8 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.Objects;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
     private final K[] keys;
@@ -17,7 +19,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] == null && key == null) {
+            if (Objects.equals(keys[i], key)) {
                 values[i] = value;
                 return;
             }
@@ -36,7 +38,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] == null && key == null) {
+            if (Objects.equals(keys[i], key)) {
                 return values[i];
             }
             if (keys[i] != null && keys[i].equals(key)) {
