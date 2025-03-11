52e56f020e2ab7e3eb1f03c43ea8d5bda0fa5433
Roman Zaharii <romazagariy2003@gmail.com>
2025-03-11 08:01:24 +0200

    implement put(), get(), and size() in StorageImpl

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,39 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private final K[] keys = (K[]) new Object[10];
+    private final V[] values = (V[]) new Object[10];
+    private int count = 0;
+
     @Override
     public void put(K key, V value) {
+        boolean fl = true;
+        for (int i = 0; i < count; i++) {
+            if ((key == null && keys[i] == null) || (keys[i] != null && keys[i].equals(key))) {
+                values[i] = value;
+                fl = false;
+            }
+        }
+        if (fl) {
+            keys[count] = key;
+            values[count] = value;
+            count++;
+        }
+
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < count; i++) {
+            if ((key == null && keys[i] == null) || (keys[i] != null && keys[i].equals(key))) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return count;
     }
 }
