276d589482258529a466eaa58ddc208bd4b3e867
bilsinas <Nevnerzhitskaiadaryna@gmail.com>
2025-03-12 20:56:39 +0200

    new

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,8 +1,7 @@
 package core.basesyntax.impl;
 
-import core.basesyntax.Storage;
-
 import java.util.Objects;
+import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
