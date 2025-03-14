34ca3ac1d2ef5214b9eec492a1f10459eb549ac1
Liliya <klv.liliya.klv@gmail.com>
2025-03-14 23:22:11 +0200

    fixed initialize arrays and object comparison

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,18 +1,25 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private static int MAX_NUMBER_OF_ELEMENTS = 10;
-    private K[] key = (K[]) new Object[MAX_NUMBER_OF_ELEMENTS];
-    private V[] value = (V[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+    private static final int MAX_NUMBER_OF_ELEMENTS = 10;
+    private K[] key;
+    private V[] value;
     private int size = 0;
+
+    public StorageImpl() {
+        key = (K[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+        value = (V[]) new Object[MAX_NUMBER_OF_ELEMENTS];
+    }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(this.key[i], key)) {
+            if (this.key[i] == key
+                    || (this.key[i] != null
+                    && key != null
+                    && this.key[i].equals(key))) {
                 this.value[i] = value;
                 return;
             }
@@ -25,7 +32,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (Objects.equals(this.key[i], key)) {
+            if (this.key[i] == key
+                    || (this.key[i] != null
+                    && key != null
+                    && this.key[i].equals(key))) {
                 return this.value[i];
             }
         }
