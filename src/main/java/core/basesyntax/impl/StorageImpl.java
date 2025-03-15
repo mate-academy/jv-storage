9aee157b713d8b52c19a3d70facec957bb4b0b3f
DZotsenko <diana.zotsenko@gmail.com>
2025-03-15 18:21:09 +0200

    implemented get() method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,18 +2,40 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.Arrays;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private K[] keys;
+    private V[] values;
+
     @Override
     public void put(K key, V value) {
+
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int index = 0;
+        for (int i = 0; i < keys.length; i++) {
+           if (keys[i].equals(key)) {
+               index = i;
+           }
+        }
+        return values[index];
     }
 
     @Override
     public int size() {
-        return -1;
+        return keys.length;
+    }
+
+    public static void main(String[] args) {
+        Storage<Integer, Box> storage = new StorageImpl<>();
+        Box box = new Box();
+        storage.put(22, box);
+        Box value = storage.get(22); // returns the Box
+        int size = storage.size(); // returns storage size
+        System.out.println(size);
+        System.out.println(value);
     }
 }
