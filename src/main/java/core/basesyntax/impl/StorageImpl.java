325a5d861932432b24ad6769b8526f94f78bdf3b
DZotsenko <diana.zotsenko@gmail.com>
2025-03-16 19:36:17 +0200

    added getKeyIndex() method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,6 +4,7 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_STORAGE_CAPACITY = 10;
+    private static final int NOT_FOUND = -1;
     private K[] keys;
     private V[] values;
     private int size;
@@ -15,11 +16,10 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < size; i++) {
-            if ((keys[i] == null && key == null) || (keys[i] != null && keys[i].equals(key))) {
-                values[i] = value;
-                return;
-            }
+        int index = getKeyIndex(key);
+        if (index != NOT_FOUND) {
+            values[index] = value;
+            return;
         }
         if (size < MAX_STORAGE_CAPACITY) {
             keys[size] = key;
@@ -30,16 +30,21 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < size; i++) {
-            if ((keys[i] == null && key == null) || (keys[i] != null && keys[i].equals(key))) {
-                return values[i];
-            }
-        }
-        return null;
+        int index = getKeyIndex(key);
+        return index == NOT_FOUND ? null : values[index];
     }
 
     @Override
     public int size() {
         return size;
+    }
+
+    private int getKeyIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == null && key == null || keys[i] != null && keys[i].equals(key)) {
+                return i;
+            }
+        }
+        return NOT_FOUND;
     }
 }
