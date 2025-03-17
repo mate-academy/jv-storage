f3db9d2381abdbafaaf2ae5063704453359acbb0
Max Krup <tochnonemaks@gmail.com>
2025-03-17 11:01:51 +0200

    add solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,54 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE = 10;
+
+    private final Object[] keys;
+    private final Object[] values;
+    private int size;
+
+    public StorageImpl() {
+        this.keys = new Object[MAX_SIZE];
+        this.values = new Object[MAX_SIZE];
+        this.size = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        if (key == null) {
+            throw new IllegalArgumentException("Can't be null");
+        }
+        for (int i = 0; i < size; i++) {
+            if (key.equals(keys[i])) {
+                // Replace existing value
+                values[i] = value;
+                return;
+            }
+        }
+        if (size < MAX_SIZE) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        } else {
+            throw new IllegalStateException("Storage is full (max size: " + MAX_SIZE + ")");
+        }
     }
 
     @Override
     public V get(K key) {
+        if (key == null) {
+            return null;
+        }
+        for ( int i = 0; i < size; i++) {
+            if (key.equals(keys[i])) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size-1;
     }
 }
