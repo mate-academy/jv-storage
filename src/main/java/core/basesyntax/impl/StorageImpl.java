b580622419de2a0b9d1dc94ba051470b705e1513
Illia Dobrovolskyi <illia.dobrovolskyi2@gmail.com>
2025-03-25 22:56:01 +0200

    changed the condition in get() method

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -32,7 +32,9 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (key == keys[i] || key != null && key.equals(keys[i])) {
+            if (key != null && key.equals(keys[i])) {
+                return values[i];
+            } else if (key == null && keys[i] == null) {
                 return values[i];
             }
         }
