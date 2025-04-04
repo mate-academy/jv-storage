73ac608a4d6430babbdc6ed27b79a099788f7f17
acid <deadfoxua1337@gmail.com>
2025-04-04 10:25:28 +0200

    loop fix

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -20,7 +20,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < STORAGE_MAX_SIZE; i++) {
+        for (int i = 0; i < STORAGE_MAX_SIZE - 1; i++) {
             if (keys[i] == null && values[i] == null) {
                 keys[i] = key;
                 values[i] = value;
@@ -37,7 +37,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < STORAGE_MAX_SIZE; i++) {
+        for (int i = 0; i < STORAGE_MAX_SIZE - 1; i++) {
             if ((keys[i] != null && keys[i].equals(key))
                     || (keys[i] == null && key == null)) {
                 return (V) values[i];
