9f5d2b56297b14869eacc93227d88947e0da2712
Maxim Golyak <maximgoliak@outlook.com>
2025-05-07 20:52:45 +0300

    add

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,12 +3,27 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int CAPACITY = 10;
+
+    private K[] kees = (K[]) new Object[CAPACITY];
+    private V[] value = (V[]) new Object[CAPACITY];
+
+    public K[] getKees() {
+        return kees;
+    }
+
+    public V[] getValue() {
+        return value;
+    }
+
     @Override
     public void put(K key, V value) {
+
     }
 
     @Override
     public V get(K key) {
+
         return null;
     }
 
