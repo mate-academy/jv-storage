36e22dda053912baa6310890c44fbd8db6d0ac9c
Biletska Liudmyla <bileckaludmila74@gmail.com>
2025-05-19 15:05:16 +0200

    Update ci.yml

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -18,33 +18,22 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] != null && keys[i].equals(key) || keys[i] == key) {
-                values[i] = value;
-
             if (key == null ? keys[i] == null : key.equals(keys[i])) {
                 values[i] = value;
-
                 return;
             }
         }
-        if (size < MAX_CAPACITY) {
-            keys[size] = key;
-            values[size] = value;
-            size++;
-        } else {
+        if (size >= MAX_CAPACITY) {
             throw new IllegalStateException("Storage is full!");
         }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            if (keys[i] != null && keys[i].equals(key) || keys[i] == key) {
-                return values[i];
-            }
-        }
-        return null;
-
             if (key == null ? keys[i] == null : key.equals(keys[i])) {
                 return values[i];
             }
